4f1f0866ece4387ce6be6373602d0804f9887bac
Eloy Durán <eloy.de.enige@gmail.com>
2015-05-01 16:05:03 +0200

    [Test] Move view hosting window into test helper.

--- Artsy Tests/ARTestHelper.h
+++ Artsy Tests/ARTestHelper.h
@@ -1,5 +1,8 @@
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface ARTestHelper : NSObject
+@end
 
+@interface ARTestViewHostingWindow : UIWindow
++ (void)hostView:(UIView *)view;
 @end
