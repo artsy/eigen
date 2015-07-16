fef051ab28be64e3b214b1827fc761bdd5aea238
Eloy Durán <eloy.de.enige@gmail.com>
2015-07-16 18:46:18 +0200

    [Warnings] Make banner look as it should.

--- Artsy/Views/Utilities/ARCustomEigenLabels.m
+++ Artsy/Views/Utilities/ARCustomEigenLabels.m
@@ -1,5 +1,6 @@
 
 
+
 @interface ARLabel (Private)
 - (void)setup;
 @end
@@ -56,6 +57,13 @@
 @end
 
 
+@interface ARWarningView ()
+@property (readwrite, nonatomic, strong) UIImageView *attentionSign;
+@end
+
+static CGFloat ARWarningViewInset = 60;
+
+
 @implementation ARWarningView
 
 - (void)setup
@@ -63,12 +71,25 @@
     [super setup];
     self.textAlignment = NSTextAlignmentCenter;
     self.backgroundColor = [UIColor artsyAttention];
+    self.attentionSign = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"AttentionIcon"]];
+    [self addSubview:self.attentionSign];
 }
 
 - (void)drawTextInRect:(CGRect)rect
 {
-    UIEdgeInsets insets = {0, 60, 0, 60};
+    CGSize size = self.attentionSign.image.size;
+    UIEdgeInsets insets = {0, ARWarningViewInset + 8 + size.width, 0, ARWarningViewInset};
     [super drawTextInRect:UIEdgeInsetsInsetRect(rect, insets)];
+}
+
+- (void)layoutSubviews;
+{
+    [super layoutSubviews];
+
+    CGRect frame = self.attentionSign.bounds;
+    frame.origin.x = ((CGRectGetWidth(self.bounds) - self.intrinsicContentSize.width) / 2) - ARWarningViewInset - CGRectGetWidth(frame) - 8;
+    frame.origin.y = (CGRectGetHeight(self.bounds) - CGRectGetHeight(frame)) / 2;
+    self.attentionSign.frame = frame;
 }
 
 @end
