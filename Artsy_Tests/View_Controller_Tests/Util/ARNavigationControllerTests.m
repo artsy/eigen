7b9d459131bc23b9883c8dbfd7b086d726d726db
Ash Furrow <ash@ashfurrow.com>
2015-07-22 14:56:14 -0400

    [View in Room] Hides back button when presented on iPhone via rotation.

--- Artsy_Tests/View_Controller_Tests/Util/ARNavigationControllerTests.m
+++ Artsy_Tests/View_Controller_Tests/Util/ARNavigationControllerTests.m
@@ -5,6 +5,16 @@
 #import "ARTopMenuViewController.h"
 #import "ARAppSearchViewController.h"
 
+@interface ARTestingHidesBackButtonViewController : UIViewController <ARMenuAwareViewController>
+@property (readwrite, nonatomic, assign) BOOL hidesBackButton;
+@end
+
+@interface ARTestingHidesNavigationButtonsViewController : UIViewController <ARMenuAwareViewController>
+@property (readwrite, nonatomic, assign) BOOL hidesNavigationButtons;
+@end
+
+@implementation ARTestingHidesNavigationButtonsViewController @end
+@implementation ARTestingHidesBackButtonViewController @end
 
 @interface ARNavigationController (Testing)
 - (IBAction)back:(id)sender;
@@ -12,11 +22,23 @@
 @property (readwrite, nonatomic, strong) ARAppSearchViewController *searchViewController;
 @end
 
+@implementation ARNavigationController (PrivateTesting)
+
+-(void)callDidShowVCDelegateMethod
+{
+    // For some reason, this is not called during tests unless the actual view hierarchy is rendered.
+    [(id<UINavigationControllerDelegate>)self navigationController:self
+                                             didShowViewController:self.topViewController
+                                                          animated:NO];
+}
+
+@end
+
 SpecBegin(ARNavigationController);
 
 __block ARNavigationController *navigationController;
 
-before(^{
+beforeEach(^{
     UIViewController *viewController = [[UIViewController alloc] init];
     navigationController = [[ARNavigationController alloc] initWithRootViewController:viewController];
 });
@@ -136,10 +158,7 @@ describe(@"search", ^{
 
     it(@"presents the search VC", ^{
         [navigationController search:nil];
-        // This is normally only called when on-screen
-        [(id<UINavigationControllerDelegate>)navigationController navigationController:navigationController
-                                                                 didShowViewController:navigationController.searchViewController
-                                                                              animated:NO];
+        [navigationController callDidShowVCDelegateMethod];
         expect(navigationController.topViewController).to.equal(navigationController.searchViewController);
     });
 
@@ -147,10 +166,7 @@ describe(@"search", ^{
         [navigationController search:nil];
         UIViewController *other = [UIViewController new];
         [navigationController pushViewController:other animated:NO];
-        // This is normally only called when on-screen
-        [(id<UINavigationControllerDelegate>)navigationController navigationController:navigationController
-                                                                 didShowViewController:other
-                                                                              animated:NO];
+        [navigationController callDidShowVCDelegateMethod];
         expect(navigationController.viewControllers).to.equal(@[navigationController.rootViewController, other]);
     });
 
@@ -159,6 +175,54 @@ describe(@"search", ^{
         [navigationController search:nil];
         expect(navigationController.searchViewController.navigationController).to.equal(navigationController);
         expect(other.viewControllers).to.beEmpty();
+    });
+});
+
+describe(@"back button", ^{
+    it(@"should hide back", ^{
+        ARTestingHidesBackButtonViewController *viewController = [[ARTestingHidesBackButtonViewController alloc] init];
+        viewController.hidesBackButton = YES;
+        [navigationController pushViewController:viewController animated:NO];
+
+        [navigationController callDidShowVCDelegateMethod];
+
+        viewController.hidesBackButton = NO;
+        expect(navigationController.backButton.layer.opacity).to.equal(1);
+    });
+
+    it(@"should show back", ^{
+        ARTestingHidesBackButtonViewController *viewController = [[ARTestingHidesBackButtonViewController alloc] init];
+        viewController.hidesBackButton = NO;
+        [navigationController pushViewController:viewController animated:NO];
+
+        [navigationController callDidShowVCDelegateMethod];
+
+        viewController.hidesBackButton = YES;
+        expect(navigationController.backButton.layer.opacity).to.equal(0);
+    });
+
+    describe(@"hidesBackButton not implemented", ^{
+        it(@"falls back to hidesNavigationButtons to show back button ", ^{
+            ARTestingHidesNavigationButtonsViewController *viewController = [[ARTestingHidesNavigationButtonsViewController alloc] init];
+            viewController.hidesNavigationButtons = NO;
+            [navigationController pushViewController:viewController animated:NO];
+
+            [navigationController callDidShowVCDelegateMethod];
+
+            viewController.hidesNavigationButtons = YES;
+            expect(navigationController.backButton.layer.opacity).to.equal(0);
+        });
+
+        it(@"falls back to hidesNavigationButtons to hide back button ", ^{
+            ARTestingHidesNavigationButtonsViewController *viewController = [[ARTestingHidesNavigationButtonsViewController alloc] init];
+            viewController.hidesNavigationButtons = YES;
+            [navigationController pushViewController:viewController animated:NO];
+
+            [navigationController callDidShowVCDelegateMethod];
+
+            viewController.hidesNavigationButtons = NO;
+            expect(navigationController.backButton.layer.opacity).to.equal(1);
+        });
     });
 });
 
