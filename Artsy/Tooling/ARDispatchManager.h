28897da572e0d9b63c40fd66cbd4335a7236feb5
Jory Stiefel <jory@stiefeldigital.com>
2015-08-17 18:12:09 -0400

    Remove + fix erroneous comments

--- Artsy/Tooling/ARDispatchManager.h
+++ Artsy/Tooling/ARDispatchManager.h
@@ -1,10 +1,8 @@
-// Dispatches asyncronously unless ARPerformWorkSynchronously is set on the shared dispatch manager
 extern void ar_dispatch_async(dispatch_block_t block);
 
-// Dispatches to the main queue unless ARPerformWorkSynchronously is set on the shared dispatch manager
 extern void ar_dispatch_main_queue(dispatch_block_t block);
 
-// Dispatches to a queue unless ARPerformWorkSynchronously is set on the shared dispatch manager
+// Dispatches to a queue unless ARPerformWorkAsynchronously is set to false
 extern void ar_dispatch_on_queue(dispatch_queue_t queue, dispatch_block_t block);
 
 extern void ar_dispatch_after_on_queue(float seconds, dispatch_queue_t queue, dispatch_block_t block);
