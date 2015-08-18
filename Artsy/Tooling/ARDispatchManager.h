f24d7526f47743d537cb6bcc6fcf71c109982692
Jory Stiefel <jory@stiefeldigital.com>
2015-08-18 10:41:49 -0400

    Update documentation

--- Artsy/Tooling/ARDispatchManager.h
+++ Artsy/Tooling/ARDispatchManager.h
@@ -1,8 +1,11 @@
+
+/// Dispatches asynchronously when ARPerformWorkAsynchronously is set to true, and synchronously otherwise
 extern void ar_dispatch_async(dispatch_block_t block);
 
+/// Dispatches to the main queue unless ARPerformWorkAsynchronously is set to false
 extern void ar_dispatch_main_queue(dispatch_block_t block);
 
-// Dispatches to a queue unless ARPerformWorkAsynchronously is set to false
+/// Dispatches to a queue unless ARPerformWorkAsynchronously is set to false
 extern void ar_dispatch_on_queue(dispatch_queue_t queue, dispatch_block_t block);
 
 extern void ar_dispatch_after_on_queue(float seconds, dispatch_queue_t queue, dispatch_block_t block);
