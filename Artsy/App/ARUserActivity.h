203e9870f3cef12204b5ae142d782a7f3045fcac
Eloy Durán <eloy.de.enige@gmail.com>
2015-09-16 17:29:57 +0200

    [ARUserActivity] Make Spotlight attribute methods public.

--- Artsy/App/ARUserActivity.h
+++ Artsy/App/ARUserActivity.h
@@ -1,5 +1,8 @@
 #import <Foundation/Foundation.h>
 
+@class CSSearchableItemAttributeSet;
+
+typedef void (^ARSearchAttributesCompletionBlock)(CSSearchableItemAttributeSet *attributeSet);
 
 @interface ARUserActivity : NSUserActivity
 
@@ -10,5 +13,15 @@
 + (instancetype)activityWithGene:(Gene *)gene becomeCurrent:(BOOL)becomeCurrent;
 + (instancetype)activityWithFair:(Fair *)fair withProfile:(Profile *)fairProfile becomeCurrent:(BOOL)becomeCurrent;
 + (instancetype)activityWithShow:(PartnerShow *)show inFair:(Fair *)fair becomeCurrent:(BOOL)becomeCurrent;
+
+/// Creates new CSSearchableItemAttributeSet objects, including thumbnails.
+///
+/// This work is performed on a background queue, the caller is responsible for dispatching onto the main queue (or any
+/// other required queue) from the completion block.
++ (void)searchAttributesWithArtwork:(Artwork *)artwork completion:(ARSearchAttributesCompletionBlock)completion;
++ (void)searchAttributesWithArtist:(Artist *)artist completion:(ARSearchAttributesCompletionBlock)completion;
++ (void)searchAttributesWithGene:(Gene *)gene completion:(ARSearchAttributesCompletionBlock)completion;
++ (void)searchAttributesWithFair:(Fair *)fair withProfile:(Profile *)fairProfile completion:(ARSearchAttributesCompletionBlock)completion;
++ (void)searchAttributesWithShow:(PartnerShow *)show inFair:(Fair *)fair completion:(ARSearchAttributesCompletionBlock)completion;
 
 @end
