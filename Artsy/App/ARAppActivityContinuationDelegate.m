663d41db75d503bd690c543145f8af7ad687ea80
Eloy Durán <eloy.de.enige@gmail.com>
2015-09-24 14:47:19 +0200

    [Handoff] Clarify symbol availability checking.

--- Artsy/App/ARAppActivityContinuationDelegate.m
+++ Artsy/App/ARAppActivityContinuationDelegate.m
@@ -1,24 +1,28 @@
 #import "ARAppActivityContinuationDelegate.h"
 @import CoreSpotlight;
 
+// Only available on iOS 9.
+static BOOL IsSpotlightActionTypeAvailable = NO;
+
 @implementation ARAppActivityContinuationDelegate
 
 + (void)load
 {
+    IsSpotlightActionTypeAvailable = &CSSearchableItemActionType != NULL;
     [JSDecoupledAppDelegate sharedAppDelegate].activityContinuationDelegate = [[self alloc] init];
 }
 
 - (BOOL)application:(UIApplication *)application willContinueUserActivityWithType:(NSString *)userActivityType;
 {
     return [userActivityType isEqualToString:NSUserActivityTypeBrowsingWeb]
-            || (&CSSearchableItemActionType != NULL && [userActivityType isEqualToString:CSSearchableItemActionType])
+            || (IsSpotlightActionTypeAvailable && [userActivityType isEqualToString:CSSearchableItemActionType])
                 || [userActivityType hasPrefix:@"net.artsy.artsy."];
 }
 
 - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *restorableObjects))restorationHandler;
 {
     NSURL *URL = nil;
-    if ((&CSSearchableItemActionType != NULL && [userActivity.activityType isEqualToString:CSSearchableItemActionType])) {
+    if (IsSpotlightActionTypeAvailable && [userActivity.activityType isEqualToString:CSSearchableItemActionType]) {
         URL = [NSURL URLWithString:userActivity.userInfo[CSSearchableItemActivityIdentifier]];
     } else {
         URL = userActivity.webpageURL;
