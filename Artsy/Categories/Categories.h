82a803aece00cba87de26cde2efe972de5cd7f81
nbfuhao <nbfuhao@gmail.com>
2015-10-09 11:53:31 -0400

    add if __include to Categories.h file to remove font file not found error

--- Artsy/Categories/Categories.h
+++ Artsy/Categories/Categories.h
@@ -1,6 +1,11 @@
 #import <Artsy+UIColors/UIColor+ArtsyColors.h>
+#if __has_include(<Artsy+UIFonts/UIFont+ArtsyFonts.h>)
 #import <Artsy+UIFonts/UIFont+ArtsyFonts.h>
+#endif
 
+#if __has_include(<Artsy+OSSUIFonts/UIFont+OSSArtsyFonts.h>)
+#import <Artsy+OSSUIFonts/UIFont+OSSArtsyFonts.h>
+#endif
 #import "UIImage+ImageFromColor.h"
 #import "UIImageView+AsyncImageLoading.h"
 #import "UILabel+Typography.h"
