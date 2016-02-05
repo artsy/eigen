2fb053209f0875dfc3e49010caa174236da60a9f
Orta Therox <orta.therox@gmail.com>
2016-02-05 09:29:44 +0000

    Initial work on nav

---
+++ Artsy/View_Controllers/ARSerifNavigationViewController.m
@@ -0,0 +1,39 @@
+#import "ARSerifNavigationViewController.h"
+#import "ARFonts.h"
+
+
+@interface ARSerifNavigationViewController ()
+
+@end
+
+
+@implementation ARSerifNavigationViewController
+
+- (instancetype)initWithRootViewController:(UIViewController *)rootViewController
+{
+    static dispatch_once_t dispatchOnceLocker = 0;
+    dispatch_once(&dispatchOnceLocker, ^{
+        [ARSerifNavigationViewController setupAppearance];
+    });
+
+
+    return [super initWithRootViewController:rootViewController];
+}
+
++ (void)setupAppearance
+{
+    UIBarButtonItem *appearance = [UIBarButtonItem appearanceWhenContainedIn:self.class, nil];
+
+    [appearance setBackgroundVerticalPositionAdjustment:-15 forBarMetrics:UIBarMetricsDefault];
+    [appearance setTitleTextAttributes:@{
+        NSForegroundColorAttributeName : [UIColor artsyHeavyGrey],
+        NSFontAttributeName : [UIFont sansSerifFontWithSize:16]
+
+    } forState:UIControlStateNormal & UIControlStateHighlighted & UIControlStateDisabled];
+
+    UIImage *backImage = [UIImage imageNamed:@""];
+    [appearance setBackButtonBackgroundImage:backImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [appearance setBackgroundImage:backImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+}
+
+@end
