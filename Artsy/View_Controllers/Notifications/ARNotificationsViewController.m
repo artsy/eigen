4f335030e3483e5b60fcbf437d5d15d50384bef0
Maxim Cramer <mennenia@gmail.com>
2016-02-16 11:55:33 +0000

    native vc now being pushed when clicking bell

---
+++ Artsy/View_Controllers/Notifications/ARNotificationsViewController.m
@@ -0,0 +1,21 @@
+//
+//  ARNotificationsViewController.m
+//  Artsy
+//
+//  Created by Maxim Cramer on 16/02/2016.
+//  Copyright © 2016 Artsy. All rights reserved.
+//
+
+#import "ARNotificationsViewController.h"
+
+
+@implementation ARNotificationsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor purpleColor];
+}
+
+
+@end
