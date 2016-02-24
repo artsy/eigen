ebaf2b54f5e1d0e81f0b3bde1ccb2e61c8c0e62c
Eloy Durán <eloy.de.enige@gmail.com>
2016-02-24 13:31:03 +0100

    [Pods] Setup skeleton.

---
+++ Emission.podspec
@@ -0,0 +1,12 @@
+Pod::Spec.new do |s|
+  s.name         = "Emission"
+  s.version      = "0.1.0"
+  s.summary      = "React Native Components used by Eigen."
+  s.homepage     = "https://github.com/artsy/emission"
+  s.license      = 'MIT'
+  s.author       = { "Eloy Durán" => "eloy.de.enige@gmail.com" }
+  s.source       = { :git => "https://github.com/artsy/emission.git", :tag => s.version.to_s }
+  s.platform     = :ios, '8.0'
+  s.requires_arc = true
+  s.source_files = 'Pod/Classes/**/*'
+end
---
+++ Example/Emission/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Emission
+//
+//  Created by Eloy Durán on 02/24/2016.
+//  Copyright (c) 2016 Eloy Durán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
