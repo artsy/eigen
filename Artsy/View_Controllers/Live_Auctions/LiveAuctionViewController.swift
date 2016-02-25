e4db3aff9bffbc512079ceebe848ca95377fadbd
Orta Therox <orta.therox@gmail.com>
2016-02-25 12:48:53 -0500

    [auctions] Add icons for the top of native auctions

--- Artsy/View_Controllers/Live_Auctions/LiveAuctionViewController.swift
+++ Artsy/View_Controllers/Live_Auctions/LiveAuctionViewController.swift
@@ -4,11 +4,8 @@ class LiveAuctionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let exit
+        
     }
 }
