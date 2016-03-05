e4cac5a095e83f2ce6471abd59f855b0fa1039da
Orta Therox <orta.therox@gmail.com>
2016-03-05 10:53:59 -0500

    Support code injection !m!

--- Artsy/View_Controllers/App_Navigation/ARTopMenuViewController+DeveloperExtras.m
+++ Artsy/View_Controllers/App_Navigation/ARTopMenuViewController+DeveloperExtras.m
@@ -1,14 +1,41 @@
 #import "ARTopMenuViewController+DeveloperExtras.h"
+#import "Artsy-Swift.h"
 
-// You can tell git to ignore changes to this file by running
-//
-//   git update-index --assume-unchanged Artsy/View_Controllers/App_Navigation/ARTopMenuViewController+DeveloperExtras.m
+// This file is for running developer specific code, either when
+// the app is loaded, or when you have injected fresh code in via
+// InjectionForXcode (which you can install via Alcatraz.)
+
+// See: https://github.com/artsy/eigen/pull/1236
+
+// It is put in gitignore as the current state of what you see now,
+// any changes you make will be erased when you switch branches.
 
 
 @implementation ARTopMenuViewController (DeveloperExtras)
 
-// Use this function to run code once the app is loaded, useful for pushing a
-// specific VC etc.
+// Called when the app has been re-injected with some code,
+// the default here will pop the top view controller and
+// re-run the developerExtras
+
+- (void)appHasBeenInjected:(NSNotification *)notification
+{
+    [self.rootNavigationController popViewControllerAnimated:NO];
+    [self runDeveloperExtras];
+}
+
+// Use this to create a new ViewController and push it on to the stack
+//
+// @example
+//
+//  id viewController = [[LiveAuctionViewController alloc] init];
+//  [self pushViewController:viewController animated:YES];
+//
+// @example
+//
+//  NSString *path = @"/artwork/helidon-xhixha-energia-delle-forme";
+//  id viewController = [[ARSwitchBoard sharedInstance] loadPath:path];
+//  [self pushViewController:viewController animated:YES];
+//
 
 - (void)runDeveloperExtras
 {
