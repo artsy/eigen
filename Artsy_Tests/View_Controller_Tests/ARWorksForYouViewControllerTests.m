62d60698a3568fe9778e7e0ca051145537a74b06
Eloy Durán <eloy.de.enige@gmail.com>
2016-03-16 13:44:06 +0100

    [WorksForYou] Move stubbed network model out of single test file.

--- Artsy_Tests/View_Controller_Tests/ARWorksForYouViewControllerTests.m
+++ Artsy_Tests/View_Controller_Tests/ARWorksForYouViewControllerTests.m
@@ -1,23 +1,9 @@
 #import "ARWorksForYouViewController.h"
-#import "ARWorksForYouNetworkModel.h"
-#import "ARWorksForYouNotificationItem.h"
-#import "Artist.h"
-#import "Artwork.h"
-#import "Artwork+Extensions.h"
-
-ARWorksForYouNotificationItem *stubbedNotificationItemWithNumberAndArtworks(int number, int nArtworks);
+#import "ARStubbedWorksForYouNetworkModel.h"
 
 
 @interface ARWorksForYouViewController ()
 @property (nonatomic, strong, readwrite) id<ARWorksForYouNetworkModelable> worksForYouNetworkModel;
-@end
-
-
-@interface ARStubbedWorksForYouNetworkModel : NSObject <ARWorksForYouNetworkModelable>
-@property (nonatomic, assign) BOOL allDownloaded;
-@property (nonatomic, copy, readwrite) NSArray<ARWorksForYouNotificationItem *> *notificationItems;
-@property (readwrite, nonatomic, assign) NSInteger currentPage;
-@property (readwrite, nonatomic, strong) NSArray *downloadedArtworkIDs;
 @end
 
 
@@ -32,20 +18,20 @@ beforeEach(^{
 });
 
 describe(@"visually", ^{
-    
     it(@"looks right with one notification", ^{
-        stubbedNetworkModel.notificationItems = @[ stubbedNotificationItemWithNumberAndArtworks(0, 2) ];
+        [stubbedNetworkModel stubNotificationItemWithNumberOfArtworks:2];
         subject.worksForYouNetworkModel = stubbedNetworkModel;
 
         /// This line has to be included for ORStackScrollView to record snapshots properly
         [subject ar_presentWithFrame:[[UIScreen mainScreen] bounds]];
         expect(subject).to.haveValidSnapshot();
     });
-    
+
     it(@"looks right with several notifications", ^{
-        stubbedNetworkModel.notificationItems = @[ stubbedNotificationItemWithNumberAndArtworks(0, 2), stubbedNotificationItemWithNumberAndArtworks(1, 1) ];
+        [stubbedNetworkModel stubNotificationItemWithNumberOfArtworks:2];
+        [stubbedNetworkModel stubNotificationItemWithNumberOfArtworks:1];
         subject.worksForYouNetworkModel = stubbedNetworkModel;
-        
+
         /// This line has to be included for ORStackScrollView to record snapshots properly
         [subject ar_presentWithFrame:[[UIScreen mainScreen] bounds]];
         expect(subject).to.haveValidSnapshot();
@@ -56,17 +42,17 @@ describe(@"marking notifications as read", ^{
     it(@"sends a network request", ^{
         subject.worksForYouNetworkModel = stubbedNetworkModel;
         id networkModelStub = [OCMockObject partialMockForObject:subject.worksForYouNetworkModel];
-        
+
         [[networkModelStub expect] markNotificationsRead];
         [subject beginAppearanceTransition:YES animated:NO];
         [subject endAppearanceTransition];
         [networkModelStub verify];
     });
-    
+
     it(@"tells the top menu vc to update its bell", ^{
         subject.worksForYouNetworkModel = stubbedNetworkModel;
         id topMenuStub = [OCMockObject partialMockForObject:[ARTopMenuViewController sharedController]];
-        
+
         [[topMenuStub expect] setNotificationCount:0 forControllerAtIndex:ARTopTabControllerIndexNotifications];
         [subject beginAppearanceTransition:YES animated:NO];
         [subject endAppearanceTransition];
@@ -74,76 +60,12 @@ describe(@"marking notifications as read", ^{
     });
 });
 
-
 itHasSnapshotsForDevicesWithName(@"looks right when user has no notifications", ^{
     subject.worksForYouNetworkModel = stubbedNetworkModel;
-    stubbedNetworkModel.notificationItems = @[];
-    
+
     [subject ar_presentWithFrame:[[UIScreen mainScreen] bounds]];
     return subject;
 });
 
-SpecEnd
+SpecEnd;
 
-
-    ARWorksForYouNotificationItem *
-    stubbedNotificationItemWithNumberAndArtworks(int number, int nArtworks)
-{
-    Artist *artist = [[Artist alloc] initWithDictionary:@{
-        @"artistID" : NSStringWithFormat(@"stubbed_%d", number),
-        @"name" : NSStringWithFormat(@"Artist %d", number)
-    } error:nil];
-
-    NSMutableArray *artworks = [[NSMutableArray alloc] initWithCapacity:nArtworks];
-    for (int i = 0; i < nArtworks; i++) {
-        Artwork *artwork = [[Artwork alloc] initWithDictionary:@{
-            @"artworkID" : @"stubbed",
-            @"title" : NSStringWithFormat(@"Artwork %d", i)
-        } error:nil];
-        artwork.artist = artist;
-        [artworks addObject:artwork];
-    }
-
-    return [[ARWorksForYouNotificationItem alloc] initWithArtist:artist artworks:artworks date:[NSDate dateWithTimeIntervalSince1970:2]];
-}
-
-
-@implementation ARStubbedWorksForYouNetworkModel
-
-- (instancetype)init
-{
-    self = [super init];
-    if (!self) {
-        return nil;
-    }
-
-    _currentPage = 1;
-    _downloadedArtworkIDs = [[NSMutableArray alloc] init];
-
-    return self;
-}
-
-- (void)getWorksForYou:(void (^_Nonnull)(NSArray<ARWorksForYouNotificationItem *> *_Nonnull))success failure:(void (^_Nullable)(NSError *_Nullable error))failure
-{
-    self.allDownloaded = YES;
-    self.currentPage++;
-    self.downloadedArtworkIDs = [[self.notificationItems map:^id(ARWorksForYouNotificationItem *item) {
-        return [item.artworks map:^id(Artwork *artwork) {
-            return artwork.artworkID;
-        }];
-    }] flatten];
-
-    success(self.notificationItems);
-}
-
-- (BOOL)didReceiveNotifications
-{
-    return self.allDownloaded && self.downloadedArtworkIDs.count;
-}
-
-- (void)markNotificationsRead
-{
-    // don't actually make the request; do nothing
-}
-
-@end
