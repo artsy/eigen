f932f4421870559972326250b1f7132620f77b2d
Ash Furrow <ash@ashfurrow.com>
2016-03-31 16:47:42 -0400

    [State Reconciliation] Adds skeleton tests.

---
+++ Artsy_Tests/Networking_Tests/Live_Auctions/LiveAuctionStateReconcilerSpec.swift
@@ -0,0 +1,78 @@
+import Quick
+import Nimble
+import Interstellar
+@testable
+import Artsy
+
+
+class LiveAuctionStateReconcilerSpec: QuickSpec {
+    override func spec() {
+
+        pending("doesn't do anything if there are no lots") {
+
+        }
+
+        pending("doesn't do anything if there is no sale") {
+
+        }
+
+        pending("doesn't do anything if there are no sale's lots") {
+
+        }
+
+        pending("doesn't do anything if there is no current lot") {
+
+        }
+
+        pending("doesn't do anything if there are no lot events (empty is 👌)") {
+
+        }
+
+        describe("first update") {
+
+            pending("sends fresh lots on first update") {
+
+            }
+
+
+            describe("second update") {
+
+                pending("does not send lots") {
+
+                }
+
+                pending("does not send sale") {
+
+                }
+
+                pending("sends new current lot") {
+
+                }
+
+                pending("updates lot view model with new events") {
+
+                }
+
+                pending("updates lot view model with online asking price") {
+
+                }
+
+                pending("updates lot view model with reserve status") {
+
+                }
+
+                pending("sends new sale when start time changes") {
+
+                }
+
+                pending("sends new sale when end time changes") {
+
+                }
+
+                pending("sends lots when number of lots change") {
+
+                }
+            }
+        }
+    }
+}
