e13ea74cf203d05ee46944fc63e0aca6f48409a8
Orta Therox <orta.therox@gmail.com>
2016-04-04 18:38:42 +0100

    Initial work on the live bid VC

---
+++ Artsy/View_Controllers/Live_Auctions/LiveAuctionBidViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class LiveAuctionBidViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
