97d3dc4db895853810674ec28f7dbc394b0bb90c
Orta Therox <orta.therox@gmail.com>
2016-04-11 16:31:44 +0100

    Improve the lot metadata tests + add a note about view things

--- Artsy_Tests/View_Controller_Tests/Auction/Views/AuctionLotMetadataStackScrollViewTests.swift
+++ Artsy_Tests/View_Controller_Tests/Auction/Views/AuctionLotMetadataStackScrollViewTests.swift
@@ -30,7 +30,7 @@ class AuctionLotMetadataStackScrollViewTests: QuickSpec {
         }
 
         it("looks right when expanded") {
-            let wrapper = UIView(frame: CGRect(x: 0, y: 0, width: 280, height: 1000)
+            let wrapper = UIView(frame: CGRect(x: 0, y: 0, width: 280, height: 1000))
             let testVM = Test_LiveAuctionLotViewModel()
             let subject = AuctionLotMetadataStackScrollView()
 
@@ -41,13 +41,7 @@ class AuctionLotMetadataStackScrollViewTests: QuickSpec {
             subject.viewModel.update(testVM)
             subject.showFullMetadata(false)
 
-            guard let stack = subject.stackView as? TextStack else {
-                return fail("Nope")
-            }
-
-            stack.addBodyText("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-
-            expect(wrapper) == recordSnapshot()
+            expect(wrapper) == snapshot()
         }
 
     }
