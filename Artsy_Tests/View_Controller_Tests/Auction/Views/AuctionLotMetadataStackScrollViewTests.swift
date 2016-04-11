e91e14803f06252d554e38b508710b4b587d4fb0
Orta Therox <orta.therox@gmail.com>
2016-04-11 16:30:07 +0100

    Added a note about secondary IB views

--- Artsy_Tests/View_Controller_Tests/Auction/Views/AuctionLotMetadataStackScrollViewTests.swift
+++ Artsy_Tests/View_Controller_Tests/Auction/Views/AuctionLotMetadataStackScrollViewTests.swift
@@ -30,12 +30,16 @@ class AuctionLotMetadataStackScrollViewTests: QuickSpec {
         }
 
         it("looks right when expanded") {
+            let wrapper = UIView(frame: CGRect(x: 0, y: 0, width: 280, height: 1000)
             let testVM = Test_LiveAuctionLotViewModel()
             let subject = AuctionLotMetadataStackScrollView()
 
+            wrapper.addSubview(subject)
+            subject.alignToView(wrapper)
+
             subject.backgroundColor = .whiteColor()
-            subject.constrainWidth("280")
             subject.viewModel.update(testVM)
+            subject.showFullMetadata(false)
 
             guard let stack = subject.stackView as? TextStack else {
                 return fail("Nope")
@@ -43,7 +47,7 @@ class AuctionLotMetadataStackScrollViewTests: QuickSpec {
 
             stack.addBodyText("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
 
-            expect(subject) == snapshot()
+            expect(wrapper) == recordSnapshot()
         }
 
     }
