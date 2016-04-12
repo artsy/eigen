14ef78cff77e9f53239ad1013e0dbec6a4393371
Maxim Cramer <mennenia@gmail.com>
2016-04-11 21:34:00 -0400

    [Onboarding] commented out tests

--- Artsy_Tests/View_Controller_Tests/Login_and_Onboarding/AROnboardingViewControllerTests.m
+++ Artsy_Tests/View_Controller_Tests/Login_and_Onboarding/AROnboardingViewControllerTests.m
@@ -11,49 +11,55 @@
 
 SpecBegin(AROnboardingViewController);
 
-__block OCMockObject *mock;
-__block AROnboardingViewController *vc;
+// commenting this out for now
+// because we no longer will have web onboarding nor do we have the collector level question
+// I want to redo this once the sequence is correct
 
-describe(@"signup splash", ^{
-    describe(@"signupDone", ^{
+// TODO: implement new onboarding signup sequence
 
-        before(^{
-            vc = [[AROnboardingViewController alloc] init];
-            mock = [OCMockObject partialMockForObject:vc];
-        });
-
-        after(^{
-            [mock stopMocking];
-            [ARTestContext stopStubbing];
-        });
-
-        it(@"ipad", ^{
-            [ARTestContext stubDevice:ARDeviceTypePad];
-            [[mock reject] presentCollectorLevel];
-            [[mock expect] presentWebOnboarding];
-            [vc didSignUpAndLogin];
-            [mock verify];
-        });
-
-        it (@"not ipad", ^{
-            [ARTestContext stubDevice:ARDeviceTypePhone5];
-            [[mock reject] presentWebOnboarding];
-            [[mock expect] presentCollectorLevel];
-            [vc didSignUpAndLogin];
-            [mock verify];
-        });
-        
-        describe(@"when registering to bid", ^{
-            it(@"does not onboard on iPhone", ^{
-                [ARTestContext stubDevice:ARDeviceTypePhone5];
-                vc.trialContext = ARTrialContextAuctionBid;
-                [[mock reject] presentOnboarding];
-                [[mock expect] dismissOnboardingWithVoidAnimation:YES];
-                [vc didSignUpAndLogin];
-                [mock verify];
-            });
-        });
-    });
-});
+//__block OCMockObject *mock;
+//__block AROnboardingViewController *vc;
+//
+//describe(@"signup splash", ^{
+//    describe(@"signupDone", ^{
+//
+//        before(^{
+//            vc = [[AROnboardingViewController alloc] init];
+//            mock = [OCMockObject partialMockForObject:vc];
+//        });
+//
+//        after(^{
+//            [mock stopMocking];
+//            [ARTestContext stopStubbing];
+//        });
+//
+//        it(@"ipad", ^{
+//            [ARTestContext stubDevice:ARDeviceTypePad];
+//            [[mock reject] presentCollectorLevel];
+//            [[mock expect] presentWebOnboarding];
+//            [vc didSignUpAndLogin];
+//            [mock verify];
+//        });
+//
+//        it (@"not ipad", ^{
+//            [ARTestContext stubDevice:ARDeviceTypePhone5];
+//            [[mock reject] presentWebOnboarding];
+//            [[mock expect] presentCollectorLevel];
+//            [vc didSignUpAndLogin];
+//            [mock verify];
+//        });
+//
+//        describe(@"when registering to bid", ^{
+//            it(@"does not onboard on iPhone", ^{
+//                [ARTestContext stubDevice:ARDeviceTypePhone5];
+//                vc.trialContext = ARTrialContextAuctionBid;
+//                [[mock reject] presentOnboarding];
+//                [[mock expect] dismissOnboardingWithVoidAnimation:YES];
+//                [vc didSignUpAndLogin];
+//                [mock verify];
+//            });
+//        });
+//    });
+//});
 
 SpecEnd;
