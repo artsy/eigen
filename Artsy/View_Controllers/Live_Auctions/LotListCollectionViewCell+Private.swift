ec3104d40479a4d6d2784139b0b21d919eaaca18
Ash Furrow <ash@ashfurrow.com>
2016-04-14 19:21:12 -0400

    [Lot List] Improved layout code.

--- Artsy/View_Controllers/Live_Auctions/LotListCollectionViewCell+Private.swift
+++ Artsy/View_Controllers/Live_Auctions/LotListCollectionViewCell+Private.swift
@@ -6,13 +6,14 @@ import Then
 private typealias PrivateFunctions = LotListCollectionViewCell
 extension PrivateFunctions {
 
-    // TODO: Layout user interface
     func setup() {
-        translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.alignToView(self)
 
         contentView.addSubview(imageView)
         imageView.constrainWidth("60", height: "60")
-        imageView.alignTop("10", leading: "20", bottom: "10", trailing: nil, toView: contentView)
+        imageView.alignTop("10", bottom: "-10", toView: contentView)
+        imageView.alignLeadingEdgeWithView(contentView, predicate: "20")
 
         contentView.addSubview(labelContainerView)
         [lotNumberLabel, artistsNamesLabel, currentAskingPriceLabel].forEach { label in
@@ -21,28 +22,36 @@ extension PrivateFunctions {
         }
         lotNumberLabel.alignTopEdgeWithView(labelContainerView, predicate: "0")
         artistsNamesLabel.alignAttribute(.Top, toAttribute: .Bottom, ofView: lotNumberLabel, predicate: "0")
-        artistsNamesLabel.constrainBottomSpaceToView(labelContainerView, predicate: ">= 0")
-        artistsNamesLabel.constrainBottomSpaceToView(currentAskingPriceLabel, predicate: ">= 10")
-
+        artistsNamesLabel.alignBottomEdgeWithView(labelContainerView, predicate: "<= 0")
+        artistsNamesLabel.constrainBottomSpaceToView(currentAskingPriceLabel, predicate: "  0")
         currentAskingPriceLabel.alignBottomEdgeWithView(labelContainerView, predicate: "0")
 
-        labelContainerView.alignLeading("10", trailing: nil, toView: imageView)
-        labelContainerView.alignTop("10", bottom: "10", toView: contentView)
+        labelContainerView.constrainLeadingSpaceToView(imageView, predicate: "10")
+        labelContainerView.constrainHeight("<= 60")
+//        labelContainerView.alignTop("10", bottom: nil, toView: contentView)
+        labelContainerView.alignCenterYWithView(imageView, predicate: "0")
     }
 
     func setLotState(lotState: LotState) {
         // Reset to defaults
         contentView.alpha = 1
 
+        let color: UIColor
+
         switch lotState {
         case .ClosedLot:
+            currentAskingPriceLabel.removeFromSuperview()
             contentView.alpha = 0.5
+            color = .blackColor()
         case .LiveLot:
             labelContainerView.addSubview(currentAskingPriceLabel)
+            color = .whiteColor()
         case .UpcomingLot:
             currentAskingPriceLabel.removeFromSuperview()
+            color = .blackColor()
         }
 
+        [lotNumberLabel, artistsNamesLabel, currentAskingPriceLabel].forEach { $0.textColor = color }
         contentView.setNeedsLayout()
     }
 }
@@ -53,14 +62,27 @@ extension ClassFunctions {
         return ARSansSerifLabel().then {
             $0.font = UIFont.sansSerifFontWithSize(12)
             $0.numberOfLines = 1
+            $0.backgroundColor = .clearColor()
         }
     }
 
     class func _artistNamesLabel() -> UILabel {
-        return UILabel()
+        return ARSerifLabel().then {
+            $0.font = UIFont.serifFontWithSize(14)
+            $0.backgroundColor = .clearColor()
+        }
     }
 
     class func _currentAskingPriceLabel() -> UILabel {
-        return UILabel()
+        return ARSansSerifLabel().then {
+            $0.font = UIFont.sansSerifFontWithSize(16)
+            $0.backgroundColor = .clearColor()
+        }
+    }
+
+    class func _labelContainerView() -> UIView {
+        return UIView().then {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
     }
 }
--- Artsy/View_Controllers/Live_Auctions/LotListCollectionViewCell.swift
+++ Artsy/View_Controllers/Live_Auctions/LotListCollectionViewCell.swift
@@ -9,7 +9,7 @@ class LotListCollectionViewCell: UICollectionViewCell {
     static let Height: CGFloat = 80
 
     let imageView = UIImageView()
-    let labelContainerView = UIView()
+    let labelContainerView = LotListCollectionViewCell._labelContainerView()
     let currentLotIndicatorImageView = UIImageView()
     let lotNumberLabel = LotListCollectionViewCell._lotNumberLabel()
     let artistsNamesLabel = LotListCollectionViewCell._artistNamesLabel()
@@ -17,19 +17,22 @@ class LotListCollectionViewCell: UICollectionViewCell {
 
     private var needsSetup = true
 
-    override func didMoveToSuperview() {
-        super.didMoveToSuperview()
-
-        guard needsSetup else { return }
-        needsSetup = false
-        setup()
-    }
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//
+//    }
 }
 
 private typealias PublicFunctions = LotListCollectionViewCell
 extension PublicFunctions {
     func configureForViewModel(viewModel: LiveAuctionLotViewModelType, auctionViewModel: LiveAuctionViewModelType) {
 
+        if needsSetup {
+            needsSetup = false
+            setup()
+        }
+
+        // TODO: These subscriptions require disposal.
         viewModel
             .computedLotStateSignal(auctionViewModel)
             .next { [weak self] state in
@@ -39,11 +42,10 @@ extension PublicFunctions {
         viewModel
             .askingPriceSignal
             .next { [weak self] askingPrice in
-                self?.currentAskingPriceLabel.text = "$DOLLARS"
+                self?.currentAskingPriceLabel.text = "$30,000"
             }
 
         imageView.ar_setImageWithURL(viewModel.urlForThumbnail)
-
         lotNumberLabel.text = "Lot \(viewModel.lotIndex)"
         artistsNamesLabel.text = viewModel.lotArtist
     }
@@ -59,7 +61,7 @@ extension Overrides {
         if layoutAttributes.selected {
             backgroundColor = .artsyPurpleRegular()
         } else {
-            backgroundColor = .grayColor()
+            backgroundColor = .whiteColor()
         }
     }
 }
