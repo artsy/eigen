7efb1b9b205a667857d12ec4db4e7763778f672f
Ash Furrow <ash@ashfurrow.com>
2016-04-15 14:06:21 -0400

    [Lot List] Adds tests for #1411.

---
+++ Artsy_Tests/View_Controller_Tests/Live_Auction/LiveAuctionLotListViewControllerTests.swift
@@ -0,0 +1,20 @@
+import Quick
+import Nimble
+import Nimble_Snapshots
+import UIKit
+
+@testable
+import Artsy
+
+
+class LiveAuctionLotListViewControllerTests: QuickSpec {
+    override func spec() {
+        it("looks good by default") {
+            let fake = stub_auctionSalesPerson()
+            let subject = LiveAuctionLotListViewController(lots: fake.lots, currentLotSignal: fake.currentLotSignal, auctionViewModel: fake.auctionViewModel!)
+
+            expect(subject) == snapshot()
+        }
+
+    }
+}
