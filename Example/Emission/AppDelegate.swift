31dc808cf853c8d8ef059a7613dcac99ae051684
Eloy Durán <eloy.de.enige@gmail.com>
2016-04-16 15:09:30 +0200

    [Example] Show Artist component.

--- Example/Emission/AppDelegate.swift
+++ Example/Emission/AppDelegate.swift
@@ -12,11 +12,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    let URL = NSURL(string: "http://localhost:8081/index.ios.bundle?platform=ios")
+    let URL = NSURL(string: "http://localhost:8081/Emission/index.ios.bundle?platform=ios&dev=true")
     self.reactBridge = RCTBridge(bundleURL: URL, moduleProvider: nil, launchOptions: nil)
 
     self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-    window?.rootViewController = UINavigationController(rootViewController: ARComponentViewController(bridge: self.reactBridge, moduleName: "ArtworksGrid"))
+    window?.rootViewController = UINavigationController(rootViewController: ARComponentViewController(bridge: self.reactBridge, moduleName: "Artist"))
     window?.makeKeyAndVisible()
 
     return true
--- Example/Emission/index.ios.js
+++ Example/Emission/index.ios.js
@@ -1,23 +1,58 @@
+/* @flow */
 'use strict';
 
-var React = require('react-native');
-var {
-  Text,
-  //TouchableHighlight,
-  View,
-  //ScrollView,
-  NativeModules,
-} = React;
+import React from 'react-native';
+import Relay from 'react-relay';
+import Emission from 'emission';
 
-class ArtworksGrid extends React.Component {
+//
+// Config
+//
+
+Relay.injectNetworkLayer(
+  new Relay.DefaultNetworkLayer('https://metaphysics-staging.artsy.net')
+    // headers: {
+    //   Authorization: 'Basic SECRET'
+    // })
+);
+
+// Disable the native polyfill during development, which will make network requests show-up in the Chrome dev-tools.
+// Specifically, in our case, we get to see the Relay requests.
+//
+// It will be `undefined` unless running inside Chrome.
+//
+// See:
+// * https://github.com/facebook/react-native/blob/2d0051f21371e8a51fde836c62a474a941023dd4/Libraries/JavaScriptAppEngine/Initialization/InitializeJavaScriptAppEngine.js#L41-L56
+// * https://github.com/facebook/react-native/issues/934
+//
+if (__DEV__ && global.originalXMLHttpRequest != undefined) {
+  global.XMLHttpRequest = global.originalXMLHttpRequest;
+}
+
+//
+// App
+//
+
+let artistRoute = new Emission.Routes.Artist({ artistID: 'kevin-beasley' });
+
+class Artist extends React.Component {
   render() {
-    return (
-      <View style={{ flex: 1, backgroundColor: 'red' }}>
-        <Text>Oh ja</Text>
-      </View>
-    );
+    // https://facebook.github.io/relay/docs/guides-root-container.html
+    return (<Relay.RootContainer
+             Component={Emission.Containers.Artist}
+             route={artistRoute}
+             renderLoading={() => {
+               return (<React.ActivityIndicatorIOS
+                        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+                        size="large"
+                        color="black"
+                      />);
+             }}
+             renderFailure={(error, retry) => {
+               return (<Text>Failed to load.</Text>);
+             }}
+           />);
   }
 }
 
-React.AppRegistry.registerComponent('ArtworksGrid', () => ArtworksGrid);
-
+React.AppRegistry.registerComponent('Artist', () => Artist);
