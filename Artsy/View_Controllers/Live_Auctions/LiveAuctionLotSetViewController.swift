b1def5eec5f024805ab90905ab77b933ad2328df
Ash Furrow <ash@ashfurrow.com>
2016-05-18 16:46:24 -0400

    [Lot View] Wrap up.

--- Artsy/View_Controllers/Live_Auctions/LiveAuctionLotSetViewController.swift
+++ Artsy/View_Controllers/Live_Auctions/LiveAuctionLotSetViewController.swift
@@ -84,10 +84,19 @@ class LiveAuctionLotSetViewController: UIViewController {
         guard firstAppearance else { return }
         firstAppearance = true
 
-        // The collection view "rests" at a non-zero index. We need to set it, but doing so immediately is too soon, so we dispatch to the next runloop invocation.
-        ar_dispatch_main_queue {
+        ar_dispatch_after(1) { 
             let restingIndexPath = NSIndexPath(forItem: LiveAuctionLotCollectionViewDataSource.RestingIndex, inSection: 0)
             self.lotImageCollectionView.scrollToItemAtIndexPath(restingIndexPath, atScrollPosition: .CenteredHorizontally, animated: false)
+        }
+        // The collection view "rests" at a non-zero index. We need to set it, but doing so immediately is too soon, so we dispatch to the next runloop invocation.
+        ar_dispatch_main_queue {
+            let initialRect = CGRect(
+                x: CGRectGetWidth(self.view.frame),
+                y: 0,
+                width: CGRectGetWidth(self.lotImageCollectionView.frame),
+                height: CGRectGetHeight(self.lotImageCollectionView.frame)
+            )
+            self.lotImageCollectionView.scrollRectToVisible(initialRect, animated: false)
         }
     }
 
