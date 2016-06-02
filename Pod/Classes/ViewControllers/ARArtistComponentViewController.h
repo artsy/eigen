2c2dba61219a20c4535711f9293505a3a53c35ef
Eloy Durán <eloy.de.enige@gmail.com>
2016-06-02 22:41:09 +0200

    [Events] Actually fetch required event data and expose the artist VC’s artist ID.

--- Pod/Classes/ViewControllers/ARArtistComponentViewController.h
+++ Pod/Classes/ViewControllers/ARArtistComponentViewController.h
@@ -5,6 +5,8 @@ NS_ASSUME_NONNULL_BEGIN
 // TODO: Rename to ARArtistViewController once the old one has been removed from Eigen.
 @interface ARArtistComponentViewController : ARComponentViewController
 
+@property (nonatomic, strong, readonly) NSString *artistID;
+
 - (instancetype)initWithArtistID:(NSString *)artistID;
 - (instancetype)initWithArtistID:(NSString *)artistID
                         emission:(nullable AREmission *)emission NS_DESIGNATED_INITIALIZER;
