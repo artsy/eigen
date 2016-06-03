7277075dccb78a76aef71cea222eaa70f119f3e7
Sarah Scott <sarahoscott@gmail.com>
2016-06-03 14:50:50 -0700

    [TabView] Hides tab view when there would only be one tab

--- Example/Emission/AppDelegate.m
+++ Example/Emission/AppDelegate.m
@@ -10,7 +10,7 @@
 #import <React/RCTUtils.h>
 #import <TargetConditionals.h>
 
-#define ARTIST @"alex-katz"
+#define ARTIST @"luis-enrique-toledo-del-rio"
 
 #if TARGET_OS_SIMULATOR
 #define ENABLE_DEV_MODE
