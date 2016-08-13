3f6c47b5029dc3e5cb79db8493c272ef71a56a2f
Orta Therox <orta.therox@gmail.com>
2016-08-13 16:54:27 -0400

    Remove semi-colons

--- test/support/common.js
+++ test/support/common.js
@@ -1,14 +1,14 @@
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import React from 'react';
-import ReactTestUtils from 'react-addons-test-utils';
-import { MockComponents } from './mocks/react-native';
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import React from 'react'
+import ReactTestUtils from 'react-addons-test-utils'
+import { MockComponents } from './mocks/react-native'
 
-chai.use(chaiAsPromised);
+chai.use(chaiAsPromised)
 
 export default {
   expect: chai.expect,
   createShallowRenderer: ReactTestUtils.createRenderer,
   React,
   MockComponents,
-};
+}
