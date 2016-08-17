2695d20e856b7431bac3934d5fa61e1e61152863
Orta Therox <orta.therox@gmail.com>
2016-08-17 09:34:56 -0400

    Refactored the Dangerfile with mr @anandaroop

--- Dangerfile.js
+++ Dangerfile.js
@@ -3,25 +3,21 @@ if (_.includes(git.modified_files, "CHANGELOG.md") === false) {
   fail("No CHANGELOG added.")
 }
 
-// Modifed files from inside Emission itself
-const modified_app_paths = _.filter(git.modified_files, function(path) {
-  return _.includes(path, "lib/") && _.includes(path, ".js") && !_.includes(path, "__tests__")
+const test_files = _.filter(git.modified_files, function(path){
+  return _.includes(path, '__tests__/');
 })
 
-// Looking for corresponding `__tests__`
-function hasCorrelatedTestChange(filename) {
-    return _.find(git.modified_files, function(modified_path) {
-      return _.endsWith(modified_path, filename.replace(".js", "-tests.js"))
-    } ) != null
-}
+const logical_test_paths = _.map(test_files, function(path){
+  // turns "lib/__tests__/i-am-good-tests.js" into "lib/i-am-good.js"
+  return path.replace(/__tests__\//, '').replace(/-tests\./, '.')
+})
 
-// Check to see whether there's a test change for every modified emission file
-const app_code_without_tests = _.reject(modified_app_paths, function(path){
-  var filename = path.split("/").pop()
-  return hasCorrelatedTestChange(filename)
+const source_paths = _.filter(git.modified_files, function(path){
+  return _.includes(path, 'lib/') &&  !_.includes(path, '__tests__/');
 })
 
 // Check that any new file has a corresponding tests file
-if (app_code_without_tests.length > 0) {
-  fail("The following files do not have tests: " + github.html_link(app_code_without_tests))
+const untested_files = _.difference(source_paths, logical_test_paths)
+if (untested_files.length > 0) {
+  warn("The following files do not have tests: " + github.html_link(untested_files))
 }
