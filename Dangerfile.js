84e70c92324dba5f9883f58278d3f8b5e5ffd5c2
Felix Krause <KrauseFx@users.noreply.github.com>
2016-08-25 14:53:17 -0700

    Improve code style
    Me being a JS expert

--- Dangerfile.js
+++ Dangerfile.js
@@ -1,5 +1,5 @@
 // CHANGELOG check
-const hasAppChanges = _.filter(git.modified_files, function(path){
+const hasAppChanges = _.filter(git.modified_files, function(path) {
   return _.includes(path, 'lib/');
 }).length > 0
 
@@ -7,16 +7,16 @@ if (hasAppChanges && _.includes(git.modified_files, "CHANGELOG.md") === false) {
   fail("No CHANGELOG added.")
 }
 
-const testFiles = _.filter(git.modified_files, function(path){
+const testFiles = _.filter(git.modified_files, function(path) {
   return _.includes(path, '__tests__/');
 })
 
-const logicalTestPaths = _.map(testFiles, function(path){
+const logicalTestPaths = _.map(testFiles, function(path) {
   // turns "lib/__tests__/i-am-good-tests.js" into "lib/i-am-good.js"
   return path.replace(/__tests__\//, '').replace(/-tests\./, '.')
 })
 
-const sourcePaths = _.filter(git.modified_files, function(path){
+const sourcePaths = _.filter(git.modified_files, function(path) {
   return _.includes(path, 'lib/') &&  !_.includes(path, '__tests__/');
 })
 
