c48727f7c476ddcf99b41e2f3faa655a202f3cf8
Orta Therox <orta.therox@gmail.com>
2016-09-05 15:49:18 -0400

    Remove my fork of apphubdeploy

--- scripts/deploy_apphub.sh
+++ scripts/deploy_apphub.sh
@@ -3,8 +3,7 @@
 # Create a .apphub credentials file
 echo "{\"appHubId\":\"Z6IwqK52JBXrKLI4kpvJ\",\"appHubSecret\":\"$APP_HUB_SECRET\"}" > .apphub
 
-# while this PR is active: https://github.com/joshuapinter/apphubdeploy/pull/5
-npm install --global orta/apphubdeploy
+npm install --global apphubdeploy
 
 # Ship a debug build of our current RN
 apphubdeploy --plist-file ./Example/Emission/Info.plist --target debug
