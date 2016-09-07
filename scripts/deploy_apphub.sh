df6a9158b18e1c31b6ac0048a042eeb3074e3ba1
Orta Therox <orta.therox@gmail.com>
2016-09-07 09:44:39 -0400

    Remove node modules after deploying

--- scripts/deploy_apphub.sh
+++ scripts/deploy_apphub.sh
@@ -10,3 +10,7 @@ echo "{\"appHubId\":\"Z6IwqK52JBXrKLI4kpvJ\",\"appHubSecret\":\"$APP_HUB_SECRET\
 
 # Ship a debug build of our current RN
 apphubdeploy --plist-file ./Example/Emission/Info.plist --target debug
+
+# To avoid build failures, kill the node_modules folder
+# e.g. https://travis-ci.org/artsy/emission/builds/158175879
+rm -rf node_modules
