bc517c128c5b484d341ce90b8900044adb3dedea
Orta Therox <orta.therox@gmail.com>
2016-09-07 09:31:53 -0400

    Do an npm install before the deploy

--- scripts/deploy_apphub.sh
+++ scripts/deploy_apphub.sh
@@ -1,9 +1,12 @@
 # See: https://github.com/joshuapinter/apphubdeploy
 #
+
+# Set up our npm environment again
+npm install
+npm install --global apphubdeploy
+
 # Create a .apphub credentials file
 echo "{\"appHubId\":\"Z6IwqK52JBXrKLI4kpvJ\",\"appHubSecret\":\"$APP_HUB_SECRET\"}" > .apphub
-
-npm install --global apphubdeploy
 
 # Ship a debug build of our current RN
 apphubdeploy --plist-file ./Example/Emission/Info.plist --target debug
