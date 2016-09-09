941548d4f5304dcfd8286aa2c55c9e6231649788
Orta Therox <orta.therox@gmail.com>
2016-09-09 11:09:46 -0400

    Make green

--- Artsy_Tests/View_Controller_Tests/Fair/ARFairSearchViewControllerTests.m
+++ Artsy_Tests/View_Controller_Tests/Fair/ARFairSearchViewControllerTests.m
@@ -2,6 +2,10 @@
 #import <ORStackView/ORStackScrollView.h>
 #import "SearchResult.h"
 
+@interface Fair()
+@property (nonatomic, strong) NSMutableSet *shows;
+@end
+
 SpecBegin(ARFairSearchViewController);
 
 describe(@"init", ^{
@@ -88,15 +92,41 @@ describe(@"init", ^{
             [OHHTTPStubs removeAllStubs];
         });
         
-        it(@"combines results from local and remote search", ^{
+        it(@"with local fair shows - combines results from local and remote search ignoring remote shows", ^{
+            fair.shows = [NSMutableSet set];
             [fairSearchVC fetchSearchResults:@"Leila"];
-            
+
             // the results are a combination of the local Leila Heller and the remote Leila Pazooki
             expect(fairSearchVC.searchResults.count).will.equal(2);
             expect(fairSearchVC.searchResults.firstObject).to.beKindOf([SearchResult class]);
             SearchResult *result = (id) fairSearchVC.searchResults.firstObject;
             expect(result.modelID).to.equal(@"leila-heller-gallery-show");
         });
+    });
+
+    it(@"without local fair shows - combines results from local and remote search", ^{
+        [OHHTTPStubs stubJSONResponseAtPath:@"/api/v1/match"
+                                 withParams:@{ @"term" : @"Leila", @"fair_id" : @"fair-id" }
+                               withResponse:@[
+          @{
+              @"model" : @"artist",
+              @"id" : @"leila-pazooki",
+              @"display": @"Leila Pazooki",
+              @"label": @"Artist",
+              @"published": @YES,
+              }, @{
+              @"model": @"partnershow",
+              @"id": @"leila-heller-gallery-leila-heller-gallery-at-the-armory-show-2014",
+              @"display": @"Leila Heller Gallery at The Armory Show 2014",
+              @"published": @YES
+          }] andStatusCode:200];
+
+        [fairSearchVC fetchSearchResults:@"Leila"];
+
+        expect(fairSearchVC.searchResults.count).will.equal(2);
+        expect(fairSearchVC.searchResults.firstObject).to.beKindOf([SearchResult class]);
+        SearchResult *result = (id) fairSearchVC.searchResults.firstObject;
+        expect(result.modelID).to.equal(@"leila-pazooki");
     });
 
 });
