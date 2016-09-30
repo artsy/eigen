6c6440db2f6c8f1480a25e055445fe7d352ae45c
Maxim Cramer <mennenia@gmail.com>
2016-09-30 17:30:14 +0100

    [Onboarding] Fix margins everywhere. So pretty. Much space. Wow.

--- Artsy/View_Controllers/Login_and_Onboarding/Onboarding_stages/Views/AROnboardingHeaderView.m
+++ Artsy/View_Controllers/Login_and_Onboarding/Onboarding_stages/Views/AROnboardingHeaderView.m
@@ -31,7 +31,7 @@
 - (void)setupHeaderViewWithTitle:(NSString *)title withLargeLayout:(BOOL)useLargeLayout
 {
     self.titleLabel.textColor = [UIColor blackColor];
-    self.titleLabel.font = [UIFont serifFontWithSize:useLargeLayout ? 32.0 : 30.0];
+    self.titleLabel.font = [UIFont serifFontWithSize:useLargeLayout ? 40.0 : 30.0];
     self.titleLabel.textAlignment = useLargeLayout ? NSTextAlignmentCenter : NSTextAlignmentLeft;
     self.titleLabel.text = title;
     self.titleLabel.numberOfLines = 0;
@@ -42,7 +42,7 @@
         [self.titleLabel alignCenterXWithView:self predicate:@"0"];
     } else {
         [self.titleLabel constrainWidthToView:self predicate:@"*.8"];
-        [self.titleLabel alignLeadingEdgeWithView:self predicate:@"10"];
+        [self.titleLabel alignLeadingEdgeWithView:self predicate:@"20"];
     }
     [self.titleLabel alignTopEdgeWithView:self predicate:@"30"];
     [self.titleLabel constrainHeight:@"60"];
@@ -51,8 +51,8 @@
 
     self.searchHeightConstraint = [self.searchField constrainHeight:@"40"];
     [self.searchField constrainTopSpaceToView:self.titleLabel predicate:@"20"];
-    [self.searchField alignLeadingEdgeWithView:self predicate:@"10"];
-    [self.searchField alignTrailingEdgeWithView:self predicate:@"-10"];
+    [self.searchField alignLeadingEdgeWithView:self predicate:@"20"];
+    [self.searchField alignTrailingEdgeWithView:self predicate:@"-20"];
     self.searchField.tintColor = [UIColor blackColor];
 }
 
