052ca51440bd58cd1c841f4e228b5804101ae4bf
Ash Furrow <ash@ashfurrow.com>
2016-10-17 14:22:39 -0400

    [Onboarding] Fixes blank gene images.

---
+++ Artsy_Tests/View_Controller_Tests/Login_and_Onboarding/AROnboardingPersonalizationGeneImageStateReconcilerTests.m
@@ -0,0 +1,40 @@
+#import "AROnboardingPersonalizationGeneImageStateReconciler.h"
+
+SpecBegin(AROnboardingPersonalizationGeneImageStateReconciler);
+
+__block AROnboardingPersonalizationGeneImageStateReconciler *subject;
+NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+Gene *gene = [Gene modelWithJSON:@{
+    @"id" : @"some_gene",
+    @"image_url" : @"http://example.com/:version.jpg",
+}];
+
+beforeEach(^{
+    subject = [AROnboardingPersonalizationGeneImageStateReconciler new];
+});
+
+it(@"normally returns the onboarding URL", ^{
+    expect([subject imageURLForGene:gene atIndexPath:indexPath]).to.equal([NSURL URLWithString:@"https://d7hftxdivxxvm.cloudfront.net/?resize_to=fill&width=100&height=100&quality=85&src=http://example.com/:version.jpg"]);
+});
+
+describe(@"added index path", ^{
+    beforeEach(^{
+        [subject addReplacedGene:indexPath];
+    });
+
+    it(@"returns the small URL", ^{
+        expect([subject imageURLForGene:gene atIndexPath:indexPath]).to.equal([NSURL URLWithString:@"http://example.com/thumb.jpg"]);
+    });
+
+    describe(@"after being reset", ^{
+        beforeEach(^{
+            [subject reset];
+        });
+
+        it(@"returns the onboarding URL", ^{
+            expect([subject imageURLForGene:gene atIndexPath:indexPath]).to.equal([NSURL URLWithString:@"https://d7hftxdivxxvm.cloudfront.net/?resize_to=fill&width=100&height=100&quality=85&src=http://example.com/:version.jpg"]);
+        });
+    });
+});
+
+SpecEnd
