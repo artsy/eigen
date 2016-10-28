b30afdb4ce3f52dd273a6b4b64e71368e04b01e4
Orta Therox <orta.therox@gmail.com>
2016-10-28 11:35:20 +0100

    [Infra] Add Danger.js

---
+++ dangerfile.js
@@ -0,0 +1,32 @@
+var _ = require('underscore')
+import { danger, fail } from 'danger'
+
+// CHANGELOG check
+const hasAppChanges = _.filter(danger.git.modified_files, function(path) {
+  return _.includes(path, 'lib/')
+}).length > 0
+
+if (hasAppChanges && _.includes(danger.git.modified_files, 'CHANGELOG.md') === false) {
+  fail('No CHANGELOG added.')
+}
+
+// Danger JS doesn't support warn yet.
+
+//  const testFiles = _.filter(git.modified_files, function(path) {
+//    return _.includes(path, '__tests__/');
+//  })
+//
+//  const logicalTestPaths = _.map(testFiles, function(path) {
+//    // turns "lib/__tests__/i-am-good-tests.js" into "lib/i-am-good.js"
+//    return path.replace(/__tests__\//, '').replace(/-tests\./, '.')
+//  })
+//
+//  const sourcePaths = _.filter(git.modified_files, function(path) {
+//    return _.includes(path, 'lib/') &&  !_.includes(path, '__tests__/');
+//  })
+//
+//  // Check that any new file has a corresponding tests file
+//  const untestedFiles = _.difference(sourcePaths, logicalTestPaths)
+//  if (untestedFiles.length > 0) {
+//    warn("The following files do not have tests: " + github.html_link//(untestedFiles))
+//  }
