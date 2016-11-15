2dac9f5ac9b0952bf3b8aeb6911aec6f9a7d97f9
Ash Furrow <ash@ashfurrow.com>
2016-11-15 12:28:23 -0500

    Revert "Updates CI/Unit Tests for iOS 10."
    This reverts commit b83feb70b39cb60878aae09c726496f7a8f24e1f.

--- Artsy_Tests/View_Controller_Tests/Auction/AuctionInformationViewControllerTests.swift
+++ Artsy_Tests/View_Controller_Tests/Auction/AuctionInformationViewControllerTests.swift
@@ -7,8 +7,6 @@ import OHHTTPStubs
 @testable
 import Artsy
 
-let window = UIWindow()
-
 class AuctionInformationViewControllerSpec: QuickSpec {
     override func spec() {
         let start = ARStandardDateFormatter.shared().date(from: "2016-02-18T10:00:00+00:00")!
@@ -21,11 +19,6 @@ class AuctionInformationViewControllerSpec: QuickSpec {
 
         var navigationController: ARSerifNavigationViewController!
         var informationController: AuctionInformationViewController!
-
-        beforeSuite {
-            // Required for all snapshot test cases, otherwise it blows up: https://github.com/facebook/ios-snapshot-test-case/blob/fbb2d277cda66350487a88e318cd5a3457738ddd/FBSnapshotTestCase/Categories/UIApplication%2BStrictKeyWindow.m#L18-L23
-            window.makeKeyAndVisible()
-        }
 
         beforeEach {
             informationController = AuctionInformationViewController(saleViewModel: saleViewModel)
