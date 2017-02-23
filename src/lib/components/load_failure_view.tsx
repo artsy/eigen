98853ca81aacd798cc19a2b84a41ddf8a6afc38e
Sarah Scott <sarahoscott@gmail.com>
2017-02-23 12:43:52 +0100

    [TypeScript] Converted load failure view to ts

--- src/lib/components/load_failure_view.tsx
+++ src/lib/components/load_failure_view.tsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import * as React from 'react'
 import {
   requireNativeComponent,
 } from 'react-native'
 
-export default class LoadFailureView extends React.Component {
+export default class LoadFailureView extends React.Component<any,any> {
+  static propTypes = {
+    // A callback that is called when the user requests a retry.
+    onRetry: React.PropTypes.func,
+  }
+
   render() {
     return <NativeLoadFailureView {...this.props} />
   }
-}
-
-LoadFailureView.propTypes = {
-  /**
-   * A callback that is called when the user requests a retry.
-   */
-  onRetry: React.PropTypes.func,
 }
 
 const NativeLoadFailureView = requireNativeComponent('ARLoadFailureView', LoadFailureView)
