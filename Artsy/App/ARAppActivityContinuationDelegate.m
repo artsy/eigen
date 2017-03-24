1cd2077cd339cf1346a2d29ae74a43c90e9566a5
Yusuke Mito <y310.1984@gmail.com>
2017-03-24 22:13:16 +0100

    Remove iOS9 checking Current minimum OS version is iOS9

--- Artsy/App/ARAppActivityContinuationDelegate.m
+++ Artsy/App/ARAppActivityContinuationDelegate.m
@@ -8,27 +8,22 @@
 
 #import <CoreSpotlight/CoreSpotlight.h>
 
-// Only available on iOS 9.
-static BOOL IsSpotlightActionTypeAvailable = NO;
-
-
 @implementation ARAppActivityContinuationDelegate
 
 + (void)load
 {
-    IsSpotlightActionTypeAvailable = &CSSearchableItemActionType != NULL;
     [JSDecoupledAppDelegate sharedAppDelegate].activityContinuationDelegate = [[self alloc] init];
 }
 
 - (BOOL)application:(UIApplication *)application willContinueUserActivityWithType:(NSString *)userActivityType;
 {
-    return [userActivityType isEqualToString:NSUserActivityTypeBrowsingWeb] || (IsSpotlightActionTypeAvailable && [userActivityType isEqualToString:CSSearchableItemActionType]) || [userActivityType hasPrefix:@"net.artsy.artsy."];
+    return [userActivityType isEqualToString:NSUserActivityTypeBrowsingWeb] || ([userActivityType isEqualToString:CSSearchableItemActionType]) || [userActivityType hasPrefix:@"net.artsy.artsy."];
 }
 
 - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *restorableObjects))restorationHandler;
 {
     NSURL *URL = nil;
-    if (IsSpotlightActionTypeAvailable && [userActivity.activityType isEqualToString:CSSearchableItemActionType]) {
+    if ([userActivity.activityType isEqualToString:CSSearchableItemActionType]) {
         URL = [NSURL URLWithString:userActivity.userInfo[CSSearchableItemActivityIdentifier]];
     } else {
         URL = userActivity.webpageURL;
