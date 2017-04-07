dd2439998c158127117f1f97607bccefe5060ac1
Ash Furrow <ash@ashfurrow.com>
2017-04-07 13:35:31 -0400

    [testing] more debug statements.

--- Artsy/App/ArtsyEcho.m
+++ Artsy/App/ArtsyEcho.m
@@ -2,6 +2,10 @@
 #import <Keys/ArtsyKeys.h>
 #import <Foundation/Foundation.h>
 
+@interface Aerodramus ()
+- (void)updateWithJSONData:(NSData *)JSONdata;
+@end
+
 @implementation ArtsyEcho
 
 - (instancetype)init {
@@ -9,6 +13,22 @@
     NSURL *url = [[NSURL alloc] initWithString:@"https://echo-api-production.herokuapp.com/"];
     self = [self initWithServerURL:url accountID:1 APIKey:[keys artsyEchoProductionToken] localFilename:@"Echo"];
     return self;
+}
+
+- (void)setup {
+    [super setup];
+
+    NSLog(@"Aerodamus setup, routes: %@", self.routes);
+
+    if (self.routes.count == 0) {
+        NSLog(@"Routes are empty");
+    }
+}
+
+- (void)updateWithJSONData:(NSData *)JSONdata {
+    [super updateWithJSONData:JSONdata];
+
+    NSLog(@"Aerodramus update, json string: %@", [[NSString alloc] initWithData:JSONdata encoding:NSUTF8StringEncoding]);
 }
 
 @end
