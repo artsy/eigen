904105835ffeadb544dae9206f671be343f8b4aa
Eloy Durán <eloy.de.enige@gmail.com>
2017-04-29 00:02:46 +0200

    [ARUserManager] Re-enable save creds in iCloud Keychain.

--- Artsy/Networking/API_Modules/ARUserManager.m
+++ Artsy/Networking/API_Modules/ARUserManager.m
@@ -157,7 +157,7 @@ static BOOL ARUserManagerDisableSharedWebCredentials = NO;
                          gotUser:gotUser
            authenticationFailure:authenticationFailure
                   networkFailure:networkFailure
-        saveSharedWebCredentials:NO]; // This has been changed to NO because in the current onboarding flow we do not request this
+        saveSharedWebCredentials:YES];
 }
 
 - (void)loginWithUsername:(NSString *)username
