e41a1416643c6037dc5f16f14f7de2c1e4bffe40
Ash Furrow <ash@ashfurrow.com>
2017-05-10 12:33:05 -0400

    Adds tests for showing inline bid amounts.

--- src/lib/components/artwork_grids/__tests__/artwork-tests.tsx
+++ src/lib/components/artwork_grids/__tests__/artwork-tests.tsx
@@ -6,12 +6,39 @@ import * as renderer from "react-test-renderer"
 import Artwork from "../artwork"
 
 it("renders properly", () => {
-  const artworkProps = {
+  const artwork = renderer.create(<Artwork artwork={artworkProps()} />).toJSON()
+  expect(artwork).toMatchSnapshot()
+})
+
+describe("in a sale", () => {
+  it("renders with starting bid", () => {
+    const saleArtwork = {
+      opening_bid: { display: "$100" },
+      current_bid: null,
+      bidder_positions_count: 0,
+    }
+    const artwork = renderer.create(<Artwork artwork={artworkProps(saleArtwork)} />).toJSON()
+    expect(artwork).toMatchSnapshot()
+  })
+
+  it("renders with current bid", () => {
+    const saleArtwork = {
+      opening_bid: { display: "$100" },
+      current_bid: { display: "$200" },
+      bidder_positions_count: 1,
+    }
+    const artwork = renderer.create(<Artwork artwork={artworkProps(saleArtwork)} />).toJSON()
+    expect(artwork).toMatchSnapshot()
+  })
+})
+
+let artworkProps = (saleArtwork = null) => {
+  return {
     title: "Some Kind of Dinosaur",
     date: "2015",
     sale_message: "$875",
-    is_in_auction: false,
-    sale_artwork: null,
+    is_in_auction: (saleArtwork !== null),
+    sale_artwork: saleArtwork,
     image: {
       url: "artsy.net/image-url",
       aspect_ratio: 0.74,
@@ -26,7 +53,4 @@ it("renders properly", () => {
     },
     href: "/artwork/mikael-olson-some-kind-of-dinosaur",
   }
-
-  const artwork = renderer.create(<Artwork artwork={artworkProps} />).toJSON()
-  expect(artwork).toMatchSnapshot()
-})
+}
