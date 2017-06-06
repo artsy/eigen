76334fad8c5b1c058d3a6d57293790fb88e4b4c6
Orta Therox <orta.therox@gmail.com>
2017-06-06 12:03:25 +0100

    [Dev] Adds jest-snapshots-svg and implements a single test

--- src/lib/components/consignments/__stories__/consignments_form_elements.story.tsx
+++ src/lib/components/consignments/__stories__/consignments_form_elements.story.tsx
@@ -3,18 +3,12 @@ import { View } from "react-native"
 
 import { storiesOf } from "@storybook/react-native"
 
-import BooleanSelector from "../components/boolean-button"
+import BooleanSelector from "../components/boolean_button"
 
 const Wrapper = (p) => <View style={{backgroundColor: "black", flex: 1, paddingTop: 20}}>{p.children}</View>
 
 storiesOf("Consignments - Form Elements")
-  .add("Selected", () =>
-    <Wrapper>
-      <BooleanSelector selected={true}/>
-      <BooleanSelector selected={false}/>
-    </Wrapper>,
-  )
-  .add("Unselected", () =>
+  .add("Boolean selector", () =>
     <Wrapper>
       <BooleanSelector selected={true}/>
       <BooleanSelector selected={false}/>
