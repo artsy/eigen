2c008e38988d54d6ffeffc8f82025e2a98964f23
Orta Therox <orta.therox@gmail.com>
2017-06-08 21:53:33 +0100

    [Consignments] More button work

--- src/lib/components/consignments/__stories__/style.story.tsx
+++ src/lib/components/consignments/__stories__/style.story.tsx
@@ -7,7 +7,7 @@ import BooleanSelector from "../components/boolean_button"
 
 const Wrapper = p => <View style={{ backgroundColor: "black", flex: 1, paddingTop: 20 }}>{p.children}</View>
 
-storiesOf("Consignments - Type")
+storiesOf("Consignments - Styling")
   .add("Type Reference", () =>
     <Wrapper>
       <T.LargeHeadline>Large Headline</T.LargeHeadline>
@@ -22,7 +22,7 @@ storiesOf("Consignments - Type")
   )
   .add("Boolean selector", () =>
     <Wrapper>
-      <BooleanSelector selected={true} />
-      <BooleanSelector selected={false} />
+      <BooleanSelector selected={true} left="YE" right="NO" />
+      <BooleanSelector selected={false} left="IN" right="WW" />
     </Wrapper>
   )
--- src/lib/components/consignments/components/boolean_button.tsx
+++ src/lib/components/consignments/components/boolean_button.tsx
@@ -4,53 +4,58 @@ import styled from "styled-components/native"
 import fonts from "../../../../data/fonts"
 
 const Title = styled.Text`
-  color: white
-  font-family: "${fonts["avant-garde-regular"]}"
-  font-size: 11
-  flex: 1
+  color: white;
+  font-family: "${fonts["avant-garde-regular"]}";
+  font-size: 11;
 `
+
 const Background = styled.TouchableHighlight`
-  background-color: green
-  height: 34
-  border-radius: 16
-  border: white
-  align-items: center
-  width: 64
+  background-color: green;
+  height: 34;
+  border-radius: 16;
+  border-color: white;
+  border-width: 1;
+  align-items: center;
+  width: 64;
 `
 
 const TextBackground = styled.View`
-  justify-content: space-between
-  flex-direction: row
-  margin-top: 9
-  margin-left: 8
-  margin-right: 8
+  justify-content: space-between;
+  flex-direction: row;
+  margin-top: 9;
+  margin-left: 8;
+  margin-right: 8;
 `
 
 const BlackCircle = styled.View`
-  width: 24
-  height: 24
-  border-radius: 12
-  background-color: black
-  border: black
+  width: 24;
+  height: 24;
+  border-radius: 12;
+  background-color: black;
+  border-color: black;
+  border-width: 1;
 `
 
 const WhiteCircle = styled.View`
-  width: 24
-  height: 24
-  border-radius: 12
-  background-color: black
-  border: white
+  width: 24;
+  height: 24;
+  border-radius: 12;
+  background-color: black;
+  border-color: white;
+  border-width: 1;
 `
 
 const CircleSpacer = styled.View`
-  position: absolute
-  top: 4
-  left: 6
-  right: 6
+  position: absolute;
+  top: 4;
+  left: 0;
+  right: 0;
 `
 
 interface BooleanButton {
   selected: boolean
+  left: string
+  right: string
   onPress?: () => void
 }
 
@@ -67,11 +72,11 @@ const render = (props: BooleanButton) => {
     <Background style={{ backgroundColor: mainBGColor }} onPress={props.onPress}>
       <View>
         <TextBackground>
-          <Title style={{ color: leftTextColor }}>YES</Title>
-          <Title style={{ textAlign: "right", color: rightTextColor }}>NO</Title>
+          <Title style={{ color: leftTextColor }}>{props.left}</Title>
+          <Title style={{ textAlign: "right", color: rightTextColor }}>{props.right}</Title>
         </TextBackground>
         <CircleSpacer style={{ flexDirection: dotDirection }}>
-          <Circle style={{ border: dotBorder }} />
+          <Circle style={{ borderColor: dotBorder }} />
         </CircleSpacer>
       </View>
     </Background>
