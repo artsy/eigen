9eac1ad9d9c5d0fdd9f13a38757ee308d48f96d2
Orta Therox <orta.therox@gmail.com>
2017-06-14 15:36:18 +0100

    [Consignments] More test coverage

--- src/lib/components/consignments/components/__tests__/bottom_aligned_button-tests.tsx
+++ src/lib/components/consignments/components/__tests__/bottom_aligned_button-tests.tsx
@@ -2,3 +2,19 @@ import * as BottomAligned from "../../__stories__/bottom_aligned.story"
 import storyRunner from "./runner"
 
 storyRunner("Bottom-Aligned states: ", BottomAligned)
+
+import "jest-snapshots-svg"
+import * as React from "react"
+import { View } from "react-native"
+import * as renderer from "react-test-renderer"
+import BottomAlignedButton from "../bottom_aligned_button"
+
+it("shows an activity indicator when searching ", () => {
+  const sut = (
+    <BottomAlignedButton onPress={() => ""}>
+      <View />
+    </BottomAlignedButton>
+  )
+  const component = renderer.create(sut).toJSON()
+  expect(component).toMatchSVGSnapshot(375, 667)
+})
