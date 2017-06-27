bacf861e28dc5fea10261146b4e16175984f311f
Matthew Zikherman <matt@bestvendor.com>
2017-06-27 13:27:58 -0400

    Remove metaphysics URL override

--- src/lib/relay/config.tsx
+++ src/lib/relay/config.tsx
@@ -9,8 +9,6 @@ if (Emission && Emission.useStagingEnvironment) {
   metaphysicsURL = "https://metaphysics-production.artsy.net"
 }
 
-metaphysicsURL = "http://localhost:5001"
-
 export { metaphysicsURL }
 
 if (Emission) {
