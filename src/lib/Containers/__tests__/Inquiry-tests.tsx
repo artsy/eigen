caf2d6540ce14bf9e33fa40ea398dd87c6ce05cd
Maxim Cramer <mennenia@gmail.com>
2017-06-30 13:30:34 +0100

    [Tests] Add Inquiry tests

---
+++ src/lib/Containers/__tests__/Inquiry-tests.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+import "react-native"
+import * as renderer from "react-test-renderer"
+
+import Inquiry from "../Inquiry"
+import StubContainer from "react-storybooks-relay-container"
+
+it("renders correctly", () => {
+  const tree = renderer.create(<StubContainer Component={Inquiry} props={inquiryProps} />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+const inquiryProps = {
+  inquiry: {
+    id: "bradley-theodore-karl-and-anna-face-off-diptych",
+    href: "/artwork/bradley-theodore-karl-and-anna-face-off-diptych",
+    title: "Karl and Anna Face Off (Diptych)",
+    date: "2016",
+    artist_names: "Bradley Theodore",
+    image: {
+      url: "https://d32dm0rphc51dk.cloudfront.net/bJ9I_vJX9ksaKFJAkOAIKg/normalized.jpg",
+    },
+    partner: {
+      name: "Other Criteria",
+      contact_message:
+        "Hi, I'm interested in purchasing this work. Could you please provide me with more information about the piece?",
+    },
+  },
+}
