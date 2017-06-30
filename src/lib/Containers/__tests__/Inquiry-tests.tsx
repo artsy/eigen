01f33d32185d0d3b4587752f53f7af8afd67c5fa
Luc Succes <l2succes@gmail.com>
2017-06-30 20:26:44 +0200

    [MSG] fixes Inquiry test

--- src/lib/Containers/__tests__/Inquiry-tests.tsx
+++ src/lib/Containers/__tests__/Inquiry-tests.tsx
@@ -1,29 +1,25 @@
 import * as React from "react"
 import "react-native"
 import * as renderer from "react-test-renderer"
-
-import StubContainer from "react-storybooks-relay-container"
 import Inquiry from "../Inquiry"
 
 it("renders correctly", () => {
-  const tree = renderer.create(<StubContainer Component={Inquiry} props={inquiryProps} />).toJSON()
+  const tree = renderer.create(<Inquiry inquiryArtwork={inquiryProps} />).toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 const inquiryProps = {
-  inquiryArtwork: {
-    id: "bradley-theodore-karl-and-anna-face-off-diptych",
-    contact_message:
-      "Hi, I'm interested in purchasing this work. Could you please provide me with more information about the piece?",
-    href: "/artwork/bradley-theodore-karl-and-anna-face-off-diptych",
-    title: "Karl and Anna Face Off (Diptych)",
-    date: "2016",
-    artist_names: "Bradley Theodore",
-    image: {
-      url: "https://d32dm0rphc51dk.cloudfront.net/bJ9I_vJX9ksaKFJAkOAIKg/normalized.jpg",
-    },
-    partner: {
-      name: "Other Criteria",
-    },
+  id: "bradley-theodore-karl-and-anna-face-off-diptych",
+  contact_message:
+    "Hi, I'm interested in purchasing this work. Could you please provide me with more information about the piece?",
+  href: "/artwork/bradley-theodore-karl-and-anna-face-off-diptych",
+  title: "Karl and Anna Face Off (Diptych)",
+  date: "2016",
+  artist_names: "Bradley Theodore",
+  image: {
+    url: "https://d32dm0rphc51dk.cloudfront.net/bJ9I_vJX9ksaKFJAkOAIKg/normalized.jpg",
+  },
+  partner: {
+    name: "Other Criteria",
   },
 }
