17a034501b2e96e0ebc1b92f417e8fd98072dee6
Maxim Cramer <mennenia@gmail.com>
2017-06-30 13:14:47 +0100

    [MSG] Add status bar to inquiry modal

--- src/lib/Containers/Inquiry.tsx
+++ src/lib/Containers/Inquiry.tsx
@@ -8,6 +8,7 @@ import {
   ImageURISource,
   KeyboardAvoidingView,
   NativeModules,
+  StatusBar,
   Text,
   TextInput,
   TouchableOpacity,
@@ -152,6 +153,7 @@ export class Inquiry extends React.Component<RelayProps, any> {
 
     return (
       <Container>
+        <StatusBar />
         <BottomAlignedButton
           onPress={this.sendInquiry.bind(this)}
           bodyStyle={doneButtonStyles}
