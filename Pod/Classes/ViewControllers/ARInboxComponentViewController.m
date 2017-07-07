8211213ca9545fff2d3d98a4f5c220e2edf026ed
Orta Therox <orta.therox@gmail.com>
2017-07-07 13:07:32 -0400

    [Dev] Remove unused code

--- Pod/Classes/ViewControllers/ARInboxComponentViewController.m
+++ Pod/Classes/ViewControllers/ARInboxComponentViewController.m
@@ -16,28 +16,6 @@
     return self;
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-
-    int somevar __attribute__((unused));
-
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored "-Wunused-variable"
-    RCTRootView *rootView = self.view.subviews.firstObject;
-    #pragma clang diagnostic pop
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-    [super viewDidDisappear:animated];
-
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored "-Wunused-variable"
-    RCTRootView *rootView = self.view.subviews.firstObject;
-    #pragma clang diagnostic pop
-}
-
 - (UIStatusBarStyle)preferredStatusBarStyle
 {
     return UIStatusBarStyleLightContent;
