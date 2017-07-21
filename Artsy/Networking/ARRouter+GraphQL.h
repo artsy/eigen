0d643d1e996854fd30f2b4eece99d3f4e70a20f8
Ash Furrow <ash@ashfurrow.com>
2017-07-21 14:53:02 -0400

    Stores GraphQL queries in files, adds favorites query.

--- Artsy/Networking/ARRouter+GraphQL.h
+++ Artsy/Networking/ARRouter+GraphQL.h
@@ -2,6 +2,9 @@
 
 @interface ARRouter (GraphQL)
 
++ (NSString *)graphQueryForFavorites;
++ (NSString *)graphQueryForFavoritesAfter:(NSString *)cursor;
+
 + (NSString *)graphQLQueryForLiveSaleStaticData:(NSString *)saleID role:(NSString *)causalityRole;
 
 @end
