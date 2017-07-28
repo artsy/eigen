f80b2616d13e0d6afdb417e62ac21c45a821a90e
Ash Furrow <ash@ashfurrow.com>
2017-07-28 16:56:54 -0400

    Gives TextArea manual placeholder text.

--- src/lib/Components/Consignments/Components/TextArea.tsx
+++ src/lib/Components/Consignments/Components/TextArea.tsx
@@ -9,27 +9,68 @@ export interface TextAreaProps extends ViewProperties {
   text?: TextInputProperties
 }
 
+interface State {
+  text: string
+}
+
+// We need to use our own placeholder as there is a bug with multiline placeholders we're working around.
+// See discussion in https://github.com/artsy/emission/pull/699
+const Placeholder = styled.Text`
+  position: absolute;
+  z-index: -1;
+  color: ${colors["gray-medium"]};
+  font-family: "${fonts["garamond-regular"]}";
+  font-size: 20;
+  margin-top: 5px;
+  width: 100%;
+`
+
 const Input = styled.TextInput`
   height: 100%;
-  background-color: black;
+  background-color: transparent;
   color: white;
   font-family: "${fonts["garamond-regular"]}";
   font-size: 20;
   flex: 1;
 `
 
-const render = (props: TextAreaProps) =>
-  <View style={{ flex: 1, ...props.style }}>
-    <View style={{ flexDirection: "row" }}>
-      <Input
-        autoCapitalize={"sentences"}
-        keyboardAppearance="dark"
-        placeholderTextColor={colors["gray-medium"]} // TODO: Placeholder isn't multiline.
-        selectionColor={colors["gray-medium"]}
-        multiline={true}
-        {...props.text}
-      />
-    </View>
-  </View>
+export default class TextArea extends React.Component<TextAreaProps, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: props.text.value ? props.text.value : "",
+    }
+  }
 
-export default render
+  onChangeText = text => {
+    this.props.text.onChangeText(text)
+    this.setState({ text })
+  }
+
+  render() {
+    const displayPlaceholder = this.state.text.length === 0
+
+    const placeholderText = this.props.text.placeholder
+    delete this.props.text.placeholder
+
+    return (
+      <View style={{ flex: 1, ...this.props.style }}>
+        <View style={{ flexDirection: "row" }}>
+          {displayPlaceholder
+            ? <Placeholder>
+                {placeholderText}
+              </Placeholder>
+            : null}
+          <Input
+            autoCapitalize={"sentences"}
+            keyboardAppearance="dark"
+            selectionColor={colors["gray-medium"]}
+            multiline={true}
+            {...this.props.text}
+            onChangeText={this.onChangeText}
+          />
+        </View>
+      </View>
+    )
+  }
+}
---
+++ src/lib/Components/Consignments/Components/__tests__/TextArea-tests.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import * as renderer from "react-test-renderer"
+import TextArea from "../TextArea"
+
+it("shows the placeholder when text is empty", () => {
+  const component = renderer.create(<TextArea text={{ placeholder: "some placeholder text" }} />).toJSON()
+  expect(component).toMatchSnapshot()
+})
+
+it("doesn't show placeholder when initial text is present", () => {
+  const component = renderer
+    .create(<TextArea text={{ placeholder: "some placeholder text", value: "any text at all" }} />)
+    .toJSON()
+  expect(component).toMatchSnapshot()
+})
