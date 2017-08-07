8c811b7f1d6d95dedceb59737530eb09a0ed1d0a
Orta Therox <orta.therox@gmail.com>
2017-08-07 16:49:09 -0400

    Use the trigger camera module inside Emission

--- src/lib/NativeModules/triggerCamera.tsx
+++ src/lib/NativeModules/triggerCamera.tsx
@@ -1,15 +1,15 @@
 import { findNodeHandle, NativeModules } from "react-native"
-const { ARTakeCameraModule } = NativeModules
+const { ARTakeCameraPhotoModule } = NativeModules
 
 async function triggerCamera(component: React.Component<any, any>): Promise<any> {
   let reactTag
   try {
     reactTag = findNodeHandle(component)
   } catch (err) {
-    console.error("could not find tag for ")
+    console.error("could not find tag for the component calling ARTakeCameraModule.triggerCameraModal")
     return
   }
-  return ARTakeCameraModule.triggerCameraModal(reactTag)
+  return ARTakeCameraPhotoModule.triggerCameraModal(reactTag)
 }
 
 export default triggerCamera
