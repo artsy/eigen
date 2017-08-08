5a5e468a8b00f6efdea08aaa5e7aceb134e8f8d7
Orta Therox <orta.therox@gmail.com>
2017-08-08 12:13:22 -0400

    Add the native work to show a camera an return whether the user has taken a photo or not

---
+++ Example/Emission/Modules/TakePhotoPromisable.h
@@ -0,0 +1,13 @@
+//
+//  TakePhotoPromisable.h
+//  Emission
+//
+//  Created by Orta Therox on 08/08/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TakePhotoPromisable : NSObject
+
+@end
---
+++ Example/Emission/Modules/TakePhotoPromisable.m
@@ -0,0 +1,13 @@
+//
+//  TakePhotoPromisable.m
+//  Emission
+//
+//  Created by Orta Therox on 08/08/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import "TakePhotoPromisable.h"
+
+@implementation TakePhotoPromisable
+
+@end
