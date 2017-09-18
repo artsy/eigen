74d7f5217153af07fd7bf49ede446bb1731b2d94
Ash Furrow <ash@ashfurrow.com>
2017-09-18 09:59:54 -0400

    Removes NSNulls from response serialization.

--- Artsy/Categories/AFNetworking/AFHTTPRequestOperation+JSON.m
+++ Artsy/Categories/AFNetworking/AFHTTPRequestOperation+JSON.m
@@ -6,7 +6,9 @@
 + (instancetype)JSONRequestOperationWithRequest:(NSURLRequest *)urlRequest success:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, id JSON))success failure:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON))failure
 {
     AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:urlRequest];
-    operation.responseSerializer = [[AFJSONResponseSerializer alloc] init];
+    AFJSONResponseSerializer *responseSerializer = [[AFJSONResponseSerializer alloc] init];
+    responseSerializer.removesKeysWithNullValues = YES;
+    operation.responseSerializer = responseSerializer;
     [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
         if (success) { success(operation.request, operation.response, responseObject); }
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
