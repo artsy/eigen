faf0bda47d1d761523ea28d86aa9cfac3b3bcdec
Ash Furrow <ash@ashfurrow.com>
2017-10-16 18:07:19 -0400

    Cleanup for #2432.

--- Artsy_Tests/View_Controller_Tests/SaleOnHoldOverlayViewTests.swift
+++ Artsy_Tests/View_Controller_Tests/SaleOnHoldOverlayViewTests.swift
@@ -7,35 +7,42 @@ import Then
 @testable
 import Artsy
 
+func runTheTest(message: String? = nil) {
+    let containerView = UIView().then {
+        $0.backgroundColor = .gray
+        $0.frame = UIScreen.main.bounds
+    }
+
+    let subject = SaleOnHoldOverlayView(message: message)
+    containerView.addSubview(subject)
+    subject.align(toView: containerView)
+
+    expect(containerView).to( haveValidSnapshot() )
+}
+
 class SaleOnHoldOverlayViewTests: QuickSpec {
     override func spec() {
         it("looks right by default on iPhone") {
             ARTestContext.use(.phone6) {
-                let containerView = UIView().then {
-                    $0.backgroundColor = .gray
-                    $0.frame = UIScreen.main.bounds
-                }
-
-                let subject = SaleOnHoldOverlayView()
-                containerView.addSubview(subject)
-                subject.align(toView: containerView)
-
-                expect(containerView).to( haveValidSnapshot() )
+                runTheTest()
             }
         }
 
         it("looks right by default on iPad") {
             ARTestContext.use(.pad) {
-                let containerView = UIView().then {
-                    $0.backgroundColor = .gray
-                    $0.frame = UIScreen.main.bounds
-                }
+                runTheTest()
+            }
+        }
 
-                let subject = SaleOnHoldOverlayView()
-                containerView.addSubview(subject)
-                subject.align(toView: containerView)
+        it("looks right with a custom message on iPhone") {
+            ARTestContext.use(.phone6) {
+                runTheTest(message: "This is a custom sale-on-hold message")
+            }
+        }
 
-                expect(containerView).to( haveValidSnapshot() )
+        it("looks right with a custom message on iPad") {
+            ARTestContext.use(.pad) {
+                runTheTest(message: "This is a custom sale-on-hold message")
             }
         }
     }
