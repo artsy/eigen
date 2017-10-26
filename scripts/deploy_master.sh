a05d32e2efc3fe2902bbd5caf03171d20a8f61b0
Orta Therox <orta.therox@gmail.com>
2017-10-26 15:39:36 -0400

    Add deploy master command

---
+++ scripts/deploy_master.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Give us a CLI to work with
+yarn global add s3-cli
+
+# Createa bundle with embedded storybooks also into Emission.js
+yarn bundle-with-storybooks
+
+# Uploads the file to our s3 bucket - credentials are in the ENV vars for same-repo PRs
+s3-cli put Pod/Assets/Emission.js "s3://artsy-emission-js/Emission-master.js"
+
+# Get the most recent PR commit
+SHA=`git rev-list --min-parents=2 --max-count=1 HEAD`
+
+# Get the PR number out of the merge commit title
+PR_NUM=`git log --format=%B -n 1 $SHA | grep -Eo '#[0-9]+' | tail -n 1`
+
+# Create a metadata file
+echo "{\"title\":\"Downloading ${SHA}\",\"subtitle\":\"$PR_DESC\"}" > head_metadata.json
+
+# Uploads the metadata so that the app can show some info
+s3-cli put head_metadata.json "s3://artsy-emission-js/master-metadata.json"
