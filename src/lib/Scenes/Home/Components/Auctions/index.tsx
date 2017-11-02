8fe3901c25816872eb4f8f74e88b652faaab4159
Luc Succes <l2succes@gmail.com>
2017-11-02 15:10:31 +0800

    [Auctions] adds live and timed auctions

--- src/lib/Scenes/Home/Components/Auctions/index.tsx
+++ src/lib/Scenes/Home/Components/Auctions/index.tsx
@@ -12,31 +12,60 @@ const Container = styled.View`
 `
 
 class Auctions extends React.Component<any, any> {
+  renderList(itemData) {
+    return (
+      <FlatList
+        data={itemData.data}
+        numColumns={2}
+        keyExtractor={(item, index) => item.id}
+        renderItem={d => {
+          return <AuctionItem key={d.index} auction={d.item} />
+        }}
+      />
+    )
+  }
+
   render() {
     const auctions = this.props.auctions
+    const liveAuctions = auctions.filter(a => !!a.live_start_at)
+    const timedAuctions = auctions.filter(a => !a.live_start_at)
 
     return (
       <Container>
         <SectionList
-          contentContainerStyle={{ justifyContent: "center", padding: 5 }}
+          contentContainerStyle={{ justifyContent: "center", padding: 5, display: "flex" }}
           sections={[
             {
-              data: auctions,
-              title: "Timed Live Auctions",
+              data: [
+                {
+                  data: liveAuctions,
+                },
+              ],
+              title: "Current Live Auctions",
+            },
+            {
+              data: [
+                {
+                  data: timedAuctions,
+                },
+              ],
+              title: "Current Timed Auctions",
             },
           ]}
           keyExtractor={(item, index) => item.id}
           renderItem={itemData => {
             return (
-              <View style={{ flex: 1, margin: 1, alignContent: "center" }}>
-                <AuctionItem key={itemData.index} auction={itemData.item} />
+              <View style={{ flex: 1, alignContent: "center" }}>
+                {this.renderList(itemData.item)}
               </View>
             )
           }}
           renderSectionHeader={({ section }) =>
-            <SectionTitle>
-              {section.title}
-            </SectionTitle>}
+            <View style={{ paddingTop: 15, backgroundColor: "white" }}>
+              <SectionTitle>
+                {section.title}
+              </SectionTitle>
+            </View>}
         />
       </Container>
     )
