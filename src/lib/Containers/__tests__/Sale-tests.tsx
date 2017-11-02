d2faaf87895aac012524dfcbe50953110a68041c
Ash Furrow <ash@ashfurrow.com>
2017-11-02 15:44:03 -0400

    Adds tests for Sale container.

---
+++ src/lib/Containers/__tests__/Sale-tests.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import * as renderer from "react-test-renderer"
+
+// Stub out these views for simplicity sake
+jest.mock("../../Components/Sale/Header", () => "Header")
+
+import { Sale } from "../Sale"
+
+import artwork from "../../Components/ArtworkGrids/__tests__/__fixtures__/artwork"
+
+describe("state", () => {
+  it("looks okay by default", () => {
+    const sale = {
+      saleArtworks: [
+        {
+          artwork: artwork(),
+        },
+      ],
+    }
+    const tree = renderer.create(<Sale sale={sale} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
