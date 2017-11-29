381d85cdd5785cdcaf0c1aadf072407dd034ce4d
Christopher Pappas <damassi.pappas@gmail.com>
2017-11-29 00:00:05 -0800

    [Home] Add Pagination support

--- src/lib/Scenes/Home/Components/Sales/Components/LotsByFollowedArtists.tsx
+++ src/lib/Scenes/Home/Components/Sales/Components/LotsByFollowedArtists.tsx
@@ -1,32 +1,71 @@
 import GenericGrid from "lib/Components/ArtworkGrids/GenericGrid"
 import createEnvironment from "lib/relay/createEnvironment"
 import renderWithLoadProgress from "lib/utils/renderWithLoadProgress"
+import { get } from "lodash"
 import React, { Component } from "react"
-import { Text, View } from "react-native"
+import { Button, Text, View } from "react-native"
+import { RelayPaginationProp } from "react-relay"
 import { createPaginationContainer, graphql, QueryRenderer, QueryRendererProps } from "react-relay"
 
+const PAGE_SIZE = 2
+
 interface RelayProps {
+  relay: RelayPaginationProp
   sale_artworks: {
-    edges: Array<{
-      node: {
-        artwork: {
-          __id: string
-          image: {
-            aspect_ratio: number
+    connection: {
+      pageInfo: {
+        hasNextPage: boolean
+      }
+      edges: Array<{
+        node: {
+          artwork: {
+            __id: string
+            image: {
+              aspect_ratio: number
+            }
           }
         }
-      }
-    }>
+      }>
+    }
   }
 }
 
 const Query = graphql.experimental`
-  query LotsByFollowedArtistsQuery {
+  query LotsByFollowedArtistsQuery($count: Int!, $cursor: String) {
     sale_artworks {
-      ...LotsByFollowedArtists_sale_artworks
+      ...LotsByFollowedArtists_sale_artworks @arguments(count: $count, cursor: $cursor)
     }
   }
 `
+
+const Pagination = createPaginationContainer(
+  GridContainer,
+  graphql.experimental`
+    fragment LotsByFollowedArtists_sale_artworks on SaleArtworks
+      @argumentDefinitions(count: { type: "Int" }, cursor: { type: "String" }) {
+      connection(first: $count, after: $cursor) @connection(key: "LotsByFollowedArtists_connection") {
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        edges {
+          cursor
+          node {
+            artwork {
+              ...GenericGrid_artworks
+            }
+          }
+        }
+      }
+    }
+  `,
+  {
+    getConnectionFromProps: ({ sale_artworks }) => sale_artworks && sale_artworks.connection,
+    getFragmentVariables: (prevVars, totalCount) => ({ ...prevVars, count: totalCount }),
+    getVariables: (props, { count, cursor }) => ({ count, cursor }),
+    query: Query,
+  }
+)
 
 export class LotsByFollowedArtists extends Component<any> {
   render() {
@@ -35,7 +74,7 @@ export class LotsByFollowedArtists extends Component<any> {
         environment={createEnvironment()}
         query={Query}
         variables={{
-          count: 5,
+          count: PAGE_SIZE,
         }}
         render={renderWithLoadProgress(Pagination)}
       />
@@ -43,40 +82,13 @@ export class LotsByFollowedArtists extends Component<any> {
   }
 }
 
-const Pagination = createPaginationContainer(
-  GridContainer,
-  graphql.experimental`
-    fragment LotsByFollowedArtists_sale_artworks on SaleArtworksConnection {
-      edges {
-        node {
-          artwork {
-            __id
-            image {
-              aspect_ratio
-            }
-            ...Artwork_artwork @relay(mask: false)
-          }
-        }
-      }
-    }
-  `,
-  {
-    direction: "forward",
-    getConnectionFromProps: props => {
-      return {}
-    },
-    getFragmentVariables: (prevVars, totalCount) => {
-      return {}
-    },
-    getVariables: (props, { count, cursor }, fragmentVariables) => {
-      return {}
-    },
-    query: Query,
-  }
-)
-
 function GridContainer(props: RelayProps) {
-  const { sale_artworks: { edges } } = props
-  const artworks = edges.map(({ node }) => node.artwork)
-  return <GenericGrid artworks={artworks} />
+  const artworks = get(props, "sale_artworks.connection.edges", []).map(({ node }) => node.artwork)
+
+  return (
+    <View>
+      <Button title="Load More" onPress={() => props.relay.loadMore(PAGE_SIZE, x => x)} />
+      <GenericGrid artworks={artworks} />
+    </View>
+  )
 }
