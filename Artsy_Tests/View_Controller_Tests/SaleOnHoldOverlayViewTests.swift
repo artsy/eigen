b6983c7b48f8f035859824ecc2c6195715e8584a
Ash Furrow <ash@ashfurrow.com>
2017-12-05 13:17:46 -0500

    Adds tests for updated messages.

--- Artsy_Tests/View_Controller_Tests/SaleOnHoldOverlayViewTests.swift
+++ Artsy_Tests/View_Controller_Tests/SaleOnHoldOverlayViewTests.swift
@@ -46,5 +46,28 @@ class SaleOnHoldOverlayViewTests: QuickSpec {
                 runTheTest(message: "This is a custom sale-on-hold message")
             }
         }
+
+        describe("updated sale-on-hold messages") {
+            var containerView: UIView!
+            var messages: Observable<String?>!
+            beforeEach {
+                containerView = UIView().then {
+                    $0.backgroundColor = .gray
+                    $0.frame = UIScreen.main.bounds
+                }
+                messages = Observable("initial message")
+                let subject = SaleOnHoldOverlayView(messages: messages)
+                containerView.addSubview(subject)
+                subject.align(toView: containerView)
+            }
+
+            it("handles subsequent message updates") {
+                expect(containerView).to( haveValidSnapshot(named: "initial message") )
+
+                messages.update("subsequently updated message")
+                // This expectation uses drawRect to delay the snapshot until after a subsequent render pass (for Autolayout to do its thing).
+                expect(containerView).to( haveValidSnapshot(named: "updated message", usesDrawRect: true) )
+            }
+        }
     }
 }
