c1de2d9a8823c4b42c47427892d6cabe535f76b3
Luc Succes <l2succes@gmail.com>
2017-12-12 22:45:21 +0700

    [Home/Auctions] fixes tests

--- src/lib/Scenes/Home/Components/Sales/__tests__/index-tests.tsx
+++ src/lib/Scenes/Home/Components/Sales/__tests__/index-tests.tsx
@@ -3,6 +3,9 @@ import "react-native"
 import * as renderer from "react-test-renderer"
 import Sales from "../index"
 
+jest.mock("WebView", () => "WebView")
+jest.mock("../Components/ZeroState/index.html", () => "")
+
 it("looks correct when rendered", () => {
   const auctions = renderer.create(<Sales {...props} />)
   expect(auctions).toMatchSnapshot()
