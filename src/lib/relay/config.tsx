f8df3d37e6100279ae43b7f26795343ed843495c
Eloy Durán <eloy.de.enige@gmail.com>
2018-03-16 17:18:19 +0100

    [relay] Remove enabling network debugging by default.
    Instead, use the ‘Enable Network Inspector’ from the React Native
    Debugger app’s contextual menu.
    
    https://github.com/jhen0409/react-native-debugger/blob/master/docs/network-inspect-of-chrome-devtools.md

--- src/lib/relay/config.tsx
+++ src/lib/relay/config.tsx
@@ -14,20 +14,7 @@ if (Emission && Emission.gravityAPIHost && Emission.metaphysicsAPIHost) {
 
 export { metaphysicsURL, gravityURL }
 
-// Disable the native polyfill during development, which will make network requests show-up in the Chrome dev-tools.
-// Specifically, in our case, we get to see the Relay requests.
-//
-// It will be `undefined` unless running inside Chrome.
-//
-declare var global: any
-if (__DEV__ && global.originalXMLHttpRequest !== undefined) {
-  /**
-   * TODO: Recording network access in Chrome Dev Tools is disabled for now.
-   *
-   * @see https://github.com/jhen0409/react-native-debugger/issues/209
-   */
-  // global.XMLHttpRequest = global.originalXMLHttpRequest
-
+if (__DEV__) {
   // tslint:disable-next-line:no-var-requires
   require("react-relay/lib/RelayNetworkDebug").init()
 }
