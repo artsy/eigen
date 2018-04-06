c842df18c24e647c0535c1c09af16bb09a43c225
Eloy Durán <eloy.de.enige@gmail.com>
2018-04-06 17:17:20 +0200

    [QueryPreloader] Integrate into JS side.

--- src/lib/relay/fetchQuery.ts
+++ src/lib/relay/fetchQuery.ts
@@ -1,62 +1,55 @@
-import query, { metaphysics } from "../metaphysics"
+import { request } from "../metaphysics"
+import * as cache from "../NativeModules/GraphQLQueryCache"
 
 import { FetchFunction } from "relay-runtime"
-import RelayQueryResponseCache from "relay-runtime/lib/RelayQueryResponseCache"
-
-/**
- * Cache requests/responses for 2 minutes, which is the average session time people spend in the app in recent versions.
- *
- * @see https://artsy.looker.com/sql/mj6pnhjdsxfhqv
- */
-const cache = new RelayQueryResponseCache({ size: 250, ttl: 2 * 60 * 1000 })
-
-// TODO: Look on disk for cached data
-class Cache {
-  _responseCache: RelayQueryResponseCache
-
-  constructor() {
-    this._responseCache = new RelayQueryResponseCache({ size: 250, ttl: 2 * 60 * 1000 })
-  }
-
-  get(text, variables) {
-    this._responseCache.get(text, variables)
-  }
-
-  set(text, variables, json) {
-    this._responseCache.set(text, variables, json)
-  }
-
-  clear() {
-    this._responseCache.clear()
-  }
-}
 
 export const fetchQuery: FetchFunction = (operation, variables, cacheConfig, _uploadables) => {
   const isQuery = operation.operationKind === "query"
-  let queryOrID
-  let body
-
-  if (__DEV__) {
-    queryOrID = require("../../__generated__/complete.queryMap.json")[operation.id]
-    body = { query: queryOrID, variables }
-  } else {
-    queryOrID = operation.id
-    body = { documentID: queryOrID, variables }
-  }
+  const queryID = operation.id
 
   if (isQuery && !cacheConfig.force) {
-    const fromCache = cache.get(queryOrID, variables)
-    if (fromCache) {
-      return fromCache
-    }
+    return cache.get(queryID, variables).then(fromCache => {
+      if (fromCache) {
+        return JSON.parse(fromCache)
+      }
+      return _fetchQuery(queryID, variables, isQuery)
+    })
+  } else {
+    return _fetchQuery(queryID, variables, isQuery)
+  }
+}
+
+function _fetchQuery(queryID: string, variables: object, isQuery: boolean) {
+  // Mark queryID as in-flight
+  cache.set(queryID, variables, null)
+
+  let body
+  if (__DEV__) {
+    body = { query: require("../../__generated__/complete.queryMap.json")[queryID], variables }
+  } else {
+    body = { documentID: queryID, variables }
   }
 
-  return metaphysics(body).then(json => {
-    if (json && isQuery) {
-      cache.set(queryOrID, variables, json)
-    } else if (!isQuery) {
-      cache.clear()
-    }
-    return json
-  })
+  return request(body)
+    .then(response => response.text())
+    .then(responseBody => {
+      const json: { errors: any[] } = JSON.parse(responseBody)
+      if (json.errors) {
+        // Unmark as in-flight
+        cache.clear(queryID, variables)
+        // Log to console/Sentry
+        json.errors.forEach(console.error)
+        // Throw here so that our error view gets shown.
+        // See https://github.com/facebook/relay/issues/1913
+        throw new Error("Server-side error occurred")
+      }
+      if (isQuery) {
+        // Fullfil in-flight request
+        cache.set(queryID, variables, responseBody)
+      } else if (!isQuery) {
+        // In case of a mutation clear all.
+        cache.clearAll()
+      }
+      return json
+    })
 }
