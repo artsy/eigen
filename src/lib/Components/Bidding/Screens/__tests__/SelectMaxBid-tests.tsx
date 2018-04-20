18c4a6f4767e15890ac69f39527fd8c3290208c0
Ash Furrow <ash@ashfurrow.com>
2018-04-20 14:16:08 -0400

    Updates tests to use preformatted bid increments.

--- src/lib/Components/Bidding/Screens/__tests__/SelectMaxBid-tests.tsx
+++ src/lib/Components/Bidding/Screens/__tests__/SelectMaxBid-tests.tsx
@@ -5,7 +5,28 @@ import * as renderer from "react-test-renderer"
 import { SelectMaxBid } from "../SelectMaxBid"
 
 const SaleArtwork = {
-  bid_increments: [3500000, 4000000, 4500000, 5000000, 5500000],
+  increments: [
+    {
+      display: "$35,000",
+      cents: 3500000,
+    },
+    {
+      display: "$40,000",
+      cents: 4000000,
+    },
+    {
+      display: "$45,000",
+      cents: 4500000,
+    },
+    {
+      display: "$50,000",
+      cents: 5000000,
+    },
+    {
+      display: "$55,000",
+      cents: 5500000,
+    },
+  ],
 }
 
 it("renders properly", () => {
