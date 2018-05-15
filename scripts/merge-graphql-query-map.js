71325d43cb606a23dade5ff9d12c1c6eb325502b
Ash Furrow <ash@ashfurrow.com>
2018-05-15 12:21:27 -0400

    Updates release with new script to merge query maps.

---
+++ scripts/merge-graphql-query-map.js
@@ -0,0 +1,20 @@
+const fs = require("fs")
+
+const queryMap = require("../src/__generated__/complete.queryMap.json")
+const mpQueryMapFilename = "../metaphysics/src/data/complete.queryMap.json"
+
+if (!fs.existsSync(mpQueryMapFilename)) {
+  console.error("Couldn't read local metaphysics query map.")
+  return 1
+}
+
+const mpQueryMap = JSON.parse(fs.readFileSync(mpQueryMapFilename))
+
+for (const key in queryMap) {
+  if (!mpQueryMap[key]) {
+    mpQueryMap[key] = queryMap[key]
+  }
+}
+
+fs.writeFileSync(mpQueryMapFilename, JSON.stringify(mpQueryMap, null, 2))
+console.write("IMPORTANT: Changes have been made to your local metaphysics repo, please PR them.")
