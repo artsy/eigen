009fbc911cd93d3f5d98b0803eefb9d49c625e53
Sarah Weir <sweir27@gmail.com>
2018-06-27 09:14:25 -0400

    update metaphysics schema;

--- src/__generated__/Registration_me.graphql.ts
+++ src/__generated__/Registration_me.graphql.ts
@@ -2,71 +2,33 @@
 
 import { ConcreteFragment } from "relay-runtime";
 export type Registration_me = {
-    readonly has_qualified_credit_cards: boolean | null;
-    readonly bidders: ReadonlyArray<({
-            readonly qualified_for_bidding: boolean | null;
-        }) | null> | null;
+    readonly has_credit_cards: boolean | null;
 };
 
 
 
-const node: ConcreteFragment = (function(){
-var v0 = {
-  "kind": "ScalarField",
-  "alias": null,
-  "name": "__id",
-  "args": null,
-  "storageKey": null
-};
-return {
+const node: ConcreteFragment = {
   "kind": "Fragment",
   "name": "Registration_me",
   "type": "Me",
   "metadata": null,
-  "argumentDefinitions": [
-    {
-      "kind": "RootArgument",
-      "name": "saleID",
-      "type": "String"
-    }
-  ],
+  "argumentDefinitions": [],
   "selections": [
     {
       "kind": "ScalarField",
       "alias": null,
-      "name": "has_qualified_credit_cards",
+      "name": "has_credit_cards",
       "args": null,
       "storageKey": null
     },
     {
-      "kind": "LinkedField",
+      "kind": "ScalarField",
       "alias": null,
-      "name": "bidders",
-      "storageKey": null,
-      "args": [
-        {
-          "kind": "Variable",
-          "name": "sale_id",
-          "variableName": "saleID",
-          "type": "String"
-        }
-      ],
-      "concreteType": "Bidder",
-      "plural": true,
-      "selections": [
-        {
-          "kind": "ScalarField",
-          "alias": null,
-          "name": "qualified_for_bidding",
-          "args": null,
-          "storageKey": null
-        },
-        v0
-      ]
-    },
-    v0
+      "name": "__id",
+      "args": null,
+      "storageKey": null
+    }
   ]
 };
-})();
-(node as any).hash = '2cc1e990b6a0dae57dd6ec9149f7257d';
+(node as any).hash = 'b3bea4b4a77d36d250a717c700bcff51';
 export default node;
