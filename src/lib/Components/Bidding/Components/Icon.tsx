679e44b7aaa7ce7c4769a8b2d964a356872f7289
Yuki Nishijima <yk.nishijima@gmail.com>
2018-07-02 16:20:59 -0400

    Refactor the <Icon> component using the styled <Image> component

--- src/lib/Components/Bidding/Components/Icon.tsx
+++ src/lib/Components/Bidding/Components/Icon.tsx
@@ -1,15 +1,4 @@
 import React from "react"
-import styled from "styled-components/native"
-import { height, space, textAlign, width } from "styled-system"
-import { HeightProps, SpaceProps, TextAlignProps, WidthProps } from "./types"
+import { Image } from "../Elements/Image"
 
-interface IconProps extends SpaceProps, WidthProps, HeightProps, SpaceProps, TextAlignProps {}
-
-const Icon = styled.Image.attrs<IconProps>({})`
-  ${width};
-  ${height};
-  ${space};
-  ${textAlign};
-`
-
-export const Icon20 = props => <Icon width={20} height={20} {...props} />
+export const Icon20 = props => <Image width={20} height={20} {...props} />
