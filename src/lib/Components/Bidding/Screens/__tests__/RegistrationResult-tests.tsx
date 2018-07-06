b405f18655d83075ff3ac510401eff02f93e9409
Sarah Weir <sweir27@gmail.com>
2018-07-06 17:01:38 -0400

    add functionality to the continue button on the reg flow

--- src/lib/Components/Bidding/Screens/__tests__/RegistrationResult-tests.tsx
+++ src/lib/Components/Bidding/Screens/__tests__/RegistrationResult-tests.tsx
@@ -1,7 +1,14 @@
 import React from "react"
 import * as renderer from "react-test-renderer"
 
+import { BidGhostButton } from "lib/Components/Bidding/Components/Button"
 import { RegistrationResult, RegistrationStatus } from "../RegistrationResult"
+
+jest.mock("lib/NativeModules/SwitchBoard", () => ({
+  dismissModalViewController: jest.fn(),
+  presentModalViewController: jest.fn(),
+}))
+import SwitchBoard from "lib/NativeModules/SwitchBoard"
 
 describe("Registration result component", () => {
   it("renders registration pending properly", () => {
@@ -21,5 +28,18 @@ describe("Registration result component", () => {
       .create(<RegistrationResult status={RegistrationStatus.RegistrationStatusError} />)
       .toJSON()
     expect(component).toMatchSnapshot()
+  })
+
+  it("dismisses the controller when the continue button is pressed", () => {
+    jest.useFakeTimers()
+    const component = renderer.create(<RegistrationResult status={RegistrationStatus.RegistrationStatusComplete} />)
+    const mockDismiss = SwitchBoard.dismissModalViewController as jest.Mock<any>
+    mockDismiss.mockReturnValueOnce(Promise.resolve())
+
+    component.root.findByType(BidGhostButton).instance.props.onPress()
+    jest.runAllTicks()
+
+    expect(SwitchBoard.dismissModalViewController).toHaveBeenCalled()
+    expect(SwitchBoard.presentModalViewController).not.toHaveBeenCalled()
   })
 })
