ebf7dc9feffddab73dd13a7c7e6c9cc450b33874
Orta Therox <orta.therox@gmail.com>
2018-10-09 11:35:16 -0400

    Fixes the status bar in the inquiry modal

---
+++ Artsy/View_Controllers/Artwork/AREigenInquiryComponentViewController.m
@@ -0,0 +1,13 @@
+#import "AREigenInquiryComponentViewController.h"
+
+@implementation AREigenInquiryComponentViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+@end
+
