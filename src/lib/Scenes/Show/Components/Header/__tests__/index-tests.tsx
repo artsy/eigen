0f7c0169b906c96bdb87846e2c09d3f676ba39eb
Ashley Jelks <arjelks@gmail.com>
2018-10-30 14:51:27 -0400

    removing Header dir

--- src/lib/Scenes/Show/Components/Header/__tests__/index-tests.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import "react-native"
-import * as renderer from "react-test-renderer"
-import { Header } from "../index"
-
-it("looks correct when rendered", () => {
-  const comp = renderer.create(<Header />)
-  expect(comp).toMatchSnapshot()
-})
