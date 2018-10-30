fc9901414ad3d4481aba90e0b62c191d57637b4e
Ashley Jelks <arjelks@gmail.com>
2018-10-30 12:43:23 -0400

    update snapshot test

--- src/lib/Scenes/Show/Components/Artworks/__tests__/index-tests.tsx
+++ src/lib/Scenes/Show/Components/Artworks/__tests__/index-tests.tsx
@@ -1,16 +1,14 @@
-import GenericGrid from "lib/Components/ArtworkGrids/GenericGrid"
 import React from "react"
 import "react-native"
 import * as renderer from "react-test-renderer"
-import { ShallowRenderer } from "react-test-renderer/shallow"
 import { Artworks } from "../index"
 
 it("looks correct when rendered", () => {
-  const artwork = [{ id: "12345" }]
-  const comp = renderer.create(
-    <Artworks>
-      <GenericGrid artworks={artwork} />
-    </Artworks>
-  )
+  const data = {
+    artworks: [],
+    __id: "12345",
+    $refType: {},
+  }
+  const comp = renderer.create(<Artworks show={data as any} />)
   expect(comp).toMatchSnapshot()
 })
