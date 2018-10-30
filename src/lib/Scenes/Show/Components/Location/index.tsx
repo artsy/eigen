de5856287b2086030aaaa31f291afd419cccddfe
Luc Succes <l2succes@gmail.com>
2018-10-29 23:22:35 -0400

    [Show] update map library + view

--- src/lib/Scenes/Show/Components/Location/index.tsx
+++ src/lib/Scenes/Show/Components/Location/index.tsx
@@ -1,45 +1,41 @@
 import { color, Flex, Sans } from "@artsy/palette"
+import Mapbox from "@mapbox/react-native-mapbox-gl"
 import { Location_show } from "__generated__/Location_show.graphql"
-import qs from "query-string"
 import React from "react"
-
-import OpaqueImageView from "lib/Components/OpaqueImageView"
 import { createFragmentContainer, graphql } from "react-relay"
 import styled from "styled-components/native"
 
-const Image = styled(OpaqueImageView)`
-  width: 400;
-  height: 300;
+// TODO: This is a testing access token, will need to regenerate and move to cocoapods-keys
+Mapbox.setAccessToken("pk.eyJ1IjoiYXJ0c3lpdCIsImEiOiJjam51dTJibTEwNHNpM3BxamV4dDRibzJhIn0.bhFMJt1mqxNDrE1xSAtxSw")
+
+const Map = styled(Mapbox.MapView)`
+  flex: 1;
+  height: 200;
 `
 
 interface Props {
   show: Location_show
 }
 
-// Move to cocoapods-keys
-const GoogleMapsAPIKey = "<API KEY HERE>"
-
 export class Location extends React.Component<Props> {
   render() {
     const { location, partner } = this.props.show
     const { lat, lng } = location.coordinates
-    const ImageParams = {
-      size: "600x800",
-      center: `${lat},${lng}`,
-      key: GoogleMapsAPIKey,
-    }
-    const imageURL = "https://maps.googleapis.com/maps/api/staticmap?" + qs.stringify(ImageParams)
-    console.log(imageURL)
 
     return (
       <Flex>
-        <Image imageURL={imageURL} />
+        <Map
+          styleURL={Mapbox.StyleURL.Light}
+          centerCoordinate={[lng, lat]}
+          zoomLevel={13}
+          logoEnabled={false}
+          scrollEnabled={false}
+        />
         <Sans size="2" color={color("black100")} weight="medium">
           {partner.name}
         </Sans>
         <Sans size="1">{location.address}</Sans>
         <Sans size="1">{location.address_2}</Sans>
-        <Sans size="1">{location.day_schedules.start_time!}</Sans>
       </Flex>
     )
   }
