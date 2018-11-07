4ff00064c5ad7b5cfba20ca03cda2dccb97a2188
Luc Succes <l2succes@gmail.com>
2018-11-07 14:17:32 -0500

    Cleanup

--- src/lib/Scenes/Fair/__tests__/index-tests.tsx
+++ src/lib/Scenes/Fair/__tests__/index-tests.tsx
@@ -1,18 +1,15 @@
-// import { renderRelayTree } from "lib/tests/renderRelayTree"
 import { MockRelayRenderer } from "lib/tests/MockRelayRenderer"
 import { renderUntil } from "lib/tests/renderUntil"
 import React from "react"
 import { graphql } from "react-relay"
 import Fair from "../"
 import { FairFixture } from "../__fixtures__"
-// import { FairHeaderContainer as FairHeader } from "../Components/FairHeader"
 
 jest.unmock("react-relay")
 
 it("renders properly", async () => {
   const tree = await renderUntil(
     wrapper => {
-      // console.log(wrapper.debug())
       return wrapper.find("FairHeader").length > 0
     },
     <MockRelayRenderer
