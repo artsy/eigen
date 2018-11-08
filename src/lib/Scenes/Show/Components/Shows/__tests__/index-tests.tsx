110443c2dc371d9ba31ef0f2b8e4a80260322bce
Ashley Jelks <arjelks@gmail.com>
2018-11-07 19:24:29 -0500

    added/updated tests

--- src/lib/Scenes/Show/Components/Shows/__tests__/index-tests.tsx
+++ src/lib/Scenes/Show/Components/Shows/__tests__/index-tests.tsx
@@ -5,10 +5,11 @@ import { Shows } from "../index"
 import { NearbyShows as _nearbyShows } from "../nearbyShows"
 
 it("looks correct when rendered", () => {
-  const data = {
-    city: "New York",
-    nearbyShows: _nearbyShows,
-  }
   const comp = renderer.create(<Shows show={data as any} />)
   expect(comp).toMatchSnapshot()
 })
+
+const data = {
+  city: "New York",
+  nearbyShows: _nearbyShows,
+}
