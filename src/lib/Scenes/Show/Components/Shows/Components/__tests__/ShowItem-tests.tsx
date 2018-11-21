dbb22714e9e12d3baf7ead06b7146dc7eeef86cb
Luc Succes <l2succes@gmail.com>
2018-11-21 16:31:12 -0500

    Updates snapshots

--- src/lib/Scenes/Show/Components/Shows/Components/__tests__/ShowItem-tests.tsx
+++ src/lib/Scenes/Show/Components/Shows/Components/__tests__/ShowItem-tests.tsx
@@ -9,29 +9,21 @@ it("looks correct when rendered", () => {
 })
 
 const data = {
-  nearbyShows: {
-    edges: [
-      {
-        node: {
-          id: "j-cacciola-gallery-delicate-power-the-delicacy-and-complexity-of-egg-tempera",
-          name: "Delicate Power: The Delicacy and Complexity of Egg Tempera",
-          images: [
-            {
-              url: "https://d32dm0rphc51dk.cloudfront.net/6HYZ-tKaXLrcqXCfYxNZQQ/large.jpg",
-              aspect_ratio: 0.99,
-            },
-          ],
-          partner: {
-            name: "J. Cacciola Gallery",
-          },
-          location: {
-            address: "35 Mill Street",
-            address_2: "",
-            state: "NJ",
-            postal_code: "07924",
-          },
-        },
-      },
-    ],
+  id: "j-cacciola-gallery-delicate-power-the-delicacy-and-complexity-of-egg-tempera",
+  name: "Delicate Power: The Delicacy and Complexity of Egg Tempera",
+  images: [
+    {
+      url: "https://d32dm0rphc51dk.cloudfront.net/6HYZ-tKaXLrcqXCfYxNZQQ/large.jpg",
+      aspect_ratio: 0.99,
+    },
+  ],
+  partner: {
+    name: "J. Cacciola Gallery",
+  },
+  location: {
+    address: "35 Mill Street",
+    address_2: "",
+    state: "NJ",
+    postal_code: "07924",
   },
 }
