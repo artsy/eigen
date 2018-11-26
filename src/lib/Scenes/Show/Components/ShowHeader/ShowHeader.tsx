81536cbd47edbbb6574623ca101c982873e2e08f
Luc Succes <l2succes@gmail.com>
2018-11-26 17:24:00 -0500

    Show artist list in order

--- src/lib/Scenes/Show/Components/ShowHeader/ShowHeader.tsx
+++ src/lib/Scenes/Show/Components/ShowHeader/ShowHeader.tsx
@@ -3,8 +3,11 @@ import React from "react"
 import { Box, Sans, Serif, Spacer } from "@artsy/palette"
 import { ShowHeader_show } from "__generated__/ShowHeader_show.graphql"
 import { InvertedButton } from "lib/Components/Buttons"
+import { Dimensions } from "react-native"
 import { createFragmentContainer, graphql } from "react-relay"
 
+import { CaretButton } from "lib/Components/Buttons/CaretButton"
+import { ArtistNamesList } from "./Components/ArtistNamesList"
 import { Carousel } from "./Components/Carousel"
 
 interface Props {
@@ -13,18 +16,22 @@ interface Props {
   onMoreInformationPressed: () => void
 }
 
+const { height: windowHeight } = Dimensions.get("window")
+
 export class ShowHeader extends React.Component<Props> {
   render() {
     const {
-      show: { images, name, partner, exhibition_period, description },
+      show: { artists, images, name, partner, exhibition_period, description },
+      onMoreInformationPressed,
       onSaveShowPressed,
     } = this.props
 
     const hasImages = !!images.length
+    const noImagesPadding = windowHeight / 2 - 200
 
     return (
       <>
-        <Box px={2} pt={hasImages ? 3 : 150} pb={hasImages ? 0 : 150}>
+        <Box px={2} pt={hasImages ? 3 : noImagesPadding} pb={hasImages ? 0 : noImagesPadding}>
           <Spacer m={2} />
           <Sans size="3" mb={0.5}>
             {partner.name}
@@ -40,7 +47,8 @@ export class ShowHeader extends React.Component<Props> {
           />
         )}
         <Box px={2}>
-          <Spacer m={2} />
+          <ArtistNamesList artists={artists} />
+          <Spacer mt={1} />
           <InvertedButton
             text="Save show"
             onPress={() => {
@@ -50,9 +58,7 @@ export class ShowHeader extends React.Component<Props> {
           <Spacer m={1} />
           <Serif size="3t">{description}</Serif>
           <Spacer m={1} />
-          <Sans size="3" weight="medium">
-            View more information
-          </Sans>
+          <CaretButton text="View more information" onPress={onMoreInformationPressed} />
         </Box>
       </>
     )
@@ -79,6 +85,9 @@ export const ShowHeaderContainer = createFragmentContainer(
       images {
         url
         aspect_ratio
+      }
+      artists {
+        name
       }
     }
   `
