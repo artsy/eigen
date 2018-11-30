1e356985e3db8cf1ac13fb882e3f77996932e44a
Kieran Gillen <kgillen@gmail.com>
2018-11-30 15:31:52 -0500

    Adds view on map button to Artworks component

--- src/lib/Scenes/Show/Components/Shows/index.tsx
+++ src/lib/Scenes/Show/Components/Shows/index.tsx
@@ -1,19 +1,35 @@
-import { Serif } from "@artsy/palette"
+import { Box, Flex, Sans, Serif } from "@artsy/palette"
 import { Shows_show } from "__generated__/Shows_show.graphql"
 import React from "react"
-import { FlatList } from "react-native"
+import { FlatList, Image, TouchableOpacity } from "react-native"
 import { createFragmentContainer, graphql } from "react-relay"
 import { ShowItemContainer as ShowItem } from "./Components/ShowItem"
 
 interface Props {
   show: Shows_show
+  handleViewOnMap: () => void
 }
 
 export const Shows: React.SFC<Props> = ({ show }) => {
+  const handleViewOnMap = () => {
+    // TODO: Show view on map view when clicked
+    console.log("clicked view on map")
+  }
+
   const { edges } = show.nearbyShows
   return (
     <>
-      <Serif size="5">More Shows</Serif>
+      <Flex justifyContent="space-between" alignItems="center" flexDirection="row">
+        <Serif size="5">More Shows</Serif>
+        <TouchableOpacity onPress={() => handleViewOnMap()}>
+          <Flex alignItems="center" flexDirection="row">
+            <Sans size="2">View on map</Sans>
+            <Box ml={1} mr={1}>
+              <Image style={{ width: 20, height: 22 }} source={require("../../../../../../images/map.png")} />
+            </Box>
+          </Flex>
+        </TouchableOpacity>
+      </Flex>
       <FlatList
         horizontal
         data={edges}
