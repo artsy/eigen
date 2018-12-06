71188b76355149141e9c479248255a864b77a192
Luc Succes <l2succes@gmail.com>
2018-12-06 16:29:54 -0500

    [Fair] adds pagination container on booths section

--- src/lib/Scenes/Fair/index.tsx
+++ src/lib/Scenes/Fair/index.tsx
@@ -2,16 +2,18 @@ import { Box, Separator, Theme } from "@artsy/palette"
 import { Fair_fair } from "__generated__/Fair_fair.graphql"
 import React from "react"
 import { FlatList, ViewProperties } from "react-native"
-import { createFragmentContainer, graphql } from "react-relay"
+import { createPaginationContainer, graphql, RelayPaginationProp } from "react-relay"
 
 import { HoursCollapsible } from "lib/Components/HoursCollapsible"
 import { LocationMapContainer as LocationMap, PartnerType } from "lib/Components/LocationMap"
+import { PAGE_SIZE } from "lib/data/constants"
 import { FairBoothContainer as FairBooth } from "./Components/FairBooth"
 import { FairHeaderContainer as FairHeader } from "./Components/FairHeader"
 import { SearchLink } from "./Components/SearchLink"
 
 interface Props extends ViewProperties {
   fair: Fair_fair
+  relay: RelayPaginationProp
 }
 
 interface State {
@@ -20,14 +22,26 @@ interface State {
     data: any
   }>
   extraData?: { animatedValue: { height: number } }
+  boothCount: number
 }
 
 export class Fair extends React.Component<Props, State> {
   state: State = {
     sections: [],
+    boothCount: 0,
+  }
+
+  componentWillReceiveProps({ fair }: Props) {
+    if (this.state.boothCount !== fair.shows.edges.length) {
+      this.updateSections()
+    }
   }
 
   componentDidMount() {
+    this.updateSections()
+  }
+
+  updateSections = () => {
     const { fair } = this.props
     const sections = []
 
@@ -54,14 +68,14 @@ export class Fair extends React.Component<Props, State> {
       },
     })
 
-    fair.shows_connection.edges.forEach(showData => {
+    fair.shows.edges.forEach(showData => {
       sections.push({
         type: "booth",
         data: showData.node,
       })
     })
 
-    this.setState({ sections })
+    this.setState({ sections, boothCount: fair.shows.edges.length })
   }
 
   renderItemSeparator = () => (
@@ -83,6 +97,20 @@ export class Fair extends React.Component<Props, State> {
       default:
         return null
     }
+  }
+
+  fetchNextPage = () => {
+    const { relay } = this.props
+
+    if (!relay.hasMore() || relay.isLoading()) {
+      return
+    }
+
+    relay.loadMore(PAGE_SIZE, error => {
+      if (!error) {
+        this.updateSections()
+      }
+    })
   }
 
   handleAnimationFrame = animatedValue => {
@@ -118,36 +146,72 @@ export class Fair extends React.Component<Props, State> {
               {this.renderItem(item)}
             </Box>
           )}
+          onEndReached={this.fetchNextPage}
         />
       </Theme>
     )
   }
 }
 
-export default createFragmentContainer(
+export default createPaginationContainer(
   Fair,
-  graphql`
-    fragment Fair_fair on Fair {
-      ...FairHeader_fair
-      id
-      name
-      hours
-      location {
-        ...LocationMap_location
-      }
-
-      profile {
+  {
+    fair: graphql`
+      fragment Fair_fair on Fair
+        @argumentDefinitions(count: { type: "Int", defaultValue: 10 }, cursor: { type: "String" }) {
+        ...FairHeader_fair
+        id
         name
-      }
+        hours
+        location {
+          ...LocationMap_location
+        }
 
-      shows_connection(first: 4) {
-        edges {
-          cursor
-          node {
-            ...FairBooth_show
+        profile {
+          name
+        }
+
+        shows: shows_connection(first: $count, after: $cursor) @connection(key: "Fair_shows") {
+          pageInfo {
+            hasNextPage
+            startCursor
+            endCursor
+          }
+          edges {
+            cursor
+            node {
+              ...FairBooth_show
+            }
           }
         }
       }
-    }
-  `
+    `,
+  },
+  {
+    direction: "forward",
+    getConnectionFromProps(props) {
+      return props.fair && props.fair.shows
+    },
+    getFragmentVariables(prevVars, totalCount) {
+      return {
+        ...prevVars,
+        count: totalCount,
+      }
+    },
+    getVariables(props, { count, cursor }, { filter }) {
+      return {
+        id: props.fair.id,
+        count,
+        cursor,
+        filter,
+      }
+    },
+    query: graphql`
+      query FairShowsQuery($id: String!, $count: Int!, $cursor: String) {
+        fair(id: $id) {
+          ...Fair_fair @arguments(count: $count, cursor: $cursor)
+        }
+      }
+    `,
+  }
 )
