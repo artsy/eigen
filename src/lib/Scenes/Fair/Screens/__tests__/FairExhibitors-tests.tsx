1c6d61522139a94c166fd9cda94188d62fc984cd
Ashley Jelks <arjelks@gmail.com>
2018-12-12 15:05:36 -0500

    LD-30 snapshot tests

---
+++ src/lib/Scenes/Fair/Screens/__tests__/FairExhibitors-tests.tsx
@@ -0,0 +1,33 @@
+import { MockRelayRenderer } from "lib/tests/MockRelayRenderer"
+import { renderUntil } from "lib/tests/renderUntil"
+import React from "react"
+import { graphql } from "react-relay"
+import { FairFixture } from "../../__fixtures__"
+import { FairExhibitorsContainer as FairExhibitors } from "../FairExhibitors"
+
+jest.unmock("react-relay")
+
+it("renders properly", async () => {
+  const tree = await renderUntil(
+    wrapper => {
+      return wrapper.find("FairExhibitors").length > 0
+    },
+    <MockRelayRenderer
+      Component={FairExhibitors}
+      query={graphql`
+        query FairExhibitorsTestsQuery {
+          fair(id: "art-basel-in-miami-beach-2018") {
+            ...FairExhibitors_fair
+          }
+        }
+      `}
+      mockResolvers={{
+        Fair: () => FairFixture,
+      }}
+    />
+  )
+
+  console.log("​tree.html()", tree.html())
+
+  expect(tree.html()).toMatchSnapshot()
+})
