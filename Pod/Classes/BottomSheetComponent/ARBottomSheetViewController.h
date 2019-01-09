51ab813a6d120f7e427208f450ffa0cab8becb40
Luc Succes <l2succes@gmail.com>
2019-01-09 19:25:54 +0200

    Add ObjC version of BottomSheetVC

---
+++ Pod/Classes/BottomSheetComponent/ARBottomSheetViewController.h
@@ -0,0 +1,16 @@
+//
+//  ARBottomSheetViewController.h
+//  Emission
+//
+//  Created by Luc Succes on 1/3/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ARBottomSheetViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pod/Classes/BottomSheetComponent/ARBottomSheetViewController.m
@@ -0,0 +1,35 @@
+//
+//  ARBottomSheetViewController.m
+//  Emission
+//
+//  Created by Luc Succes on 1/3/19.
+//
+
+#import "ARBottomSheetViewController.h"
+
+@interface ARBottomSheetViewController ()
+
+@property (nonatomic) CGFloat lastY;
+
+
+
+@end
+
+@implementation ARBottomSheetViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
