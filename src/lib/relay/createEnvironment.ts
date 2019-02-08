3e6fd97be7e8e3c268f5779c2805cc3fa0b1cc84
Luc Succes <l2succes@gmail.com>
2019-02-08 16:44:16 -0500

    Update createEnvironment

--- src/lib/relay/createEnvironment.ts
+++ src/lib/relay/createEnvironment.ts
@@ -1,8 +1,32 @@
-import { Environment, Network, RecordSource, Store } from "relay-runtime"
-import { fetchQuery } from "./fetchQuery"
+import { NativeModules } from "react-native"
+import { errorMiddleware, loggerMiddleware, RelayNetworkLayer, urlMiddleware } from "react-relay-network-modern/node8"
+import { Environment, RecordSource, Store } from "relay-runtime"
+
+import { metaphysicsURL } from "./config"
+import { cacheMiddleware } from "./middlewares/cacheMiddleware"
+import { metaphysicsExtensionsLoggerMiddleware } from "./middlewares/metaphysicsMiddleware"
+
+const Emission = NativeModules.Emission || {}
 
 export default function createEnvironment() {
-  const network = Network.create(fetchQuery)
+  const network = new RelayNetworkLayer([
+    cacheMiddleware(),
+    urlMiddleware({
+      url: metaphysicsURL,
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent": Emission.userAgent,
+        "X-USER-ID": Emission.userID,
+        "X-ACCESS-TOKEN": Emission.authenticationToken,
+      },
+    }),
+    loggerMiddleware(),
+    errorMiddleware({
+      disableServerMiddlewareTip: true,
+    }),
+    metaphysicsExtensionsLoggerMiddleware(),
+  ])
+
   const source = new RecordSource()
   const store = new Store(source)
   return new Environment({
