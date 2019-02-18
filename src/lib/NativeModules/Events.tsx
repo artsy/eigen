2bf6da0acf4f4635f99b313c12ef683badf001ed
Luc Succes <l2succes@gmail.com>
2019-02-18 18:29:29 -0500

    print formatted JSON instead

--- src/lib/NativeModules/Events.tsx
+++ src/lib/NativeModules/Events.tsx
@@ -3,7 +3,7 @@ const { AREventsModule } = NativeModules
 
 function postEvent(info: any) {
   if (__DEV__) {
-    console.log("[Event tracked]", info)
+    console.log("[Event tracked]", JSON.stringify(info, null, 2))
   }
   AREventsModule.postEvent(info)
 }
