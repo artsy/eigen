1d2673927f0741f7dff01695d12f7be5a305a2e6
Luc Succes <l2succes@gmail.com>
2019-02-18 17:15:16 -0500

    Add logging events in Dev mode

--- src/lib/NativeModules/Events.tsx
+++ src/lib/NativeModules/Events.tsx
@@ -2,6 +2,9 @@ import { NativeModules } from "react-native"
 const { AREventsModule } = NativeModules
 
 function postEvent(info: any) {
+  if (__DEV__) {
+    console.log("[Event tracked]", info)
+  }
   AREventsModule.postEvent(info)
 }
 
