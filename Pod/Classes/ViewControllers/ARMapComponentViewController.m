a8cb10630765b02fafd671827953fc6f74d0f385
Orta Therox <orta.therox@gmail.com>
2019-02-26 07:19:10 -0500

    Fix feedback

--- Pod/Classes/ViewControllers/ARMapComponentViewController.m
+++ Pod/Classes/ViewControllers/ARMapComponentViewController.m
@@ -7,21 +7,6 @@
     return [super initWithEmission:nil moduleName:@"Map" initialProperties:nil];
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    [self.view addConstraints:@[
-                                [NSLayoutConstraint constraintWithItem:self.rootView
-                                                             attribute:NSLayoutAttributeTop
-                                                             relatedBy:NSLayoutRelationEqual
-                                                                toItem:self.view
-                                                             attribute:NSLayoutAttributeTop
-                                                            multiplier:1
-                                                              constant:0]
-                                ]];
-}
-
 - (UIStatusBarStyle)preferredStatusBarStyle
 {
     return UIStatusBarStyleDefault;
