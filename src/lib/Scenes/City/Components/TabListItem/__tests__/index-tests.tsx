172d70584589b7c655fcae9b2bd7fbff8081d2aa
kierangillen <kgillen@gmail.com>
2019-02-27 14:49:30 -0500

    Fixes test

--- src/lib/Scenes/City/Components/TabListItem/__tests__/index-tests.tsx
+++ src/lib/Scenes/City/Components/TabListItem/__tests__/index-tests.tsx
@@ -3,23 +3,22 @@ import { mount } from "enzyme"
 import React from "react"
 import { TabListItem } from "../index"
 
-const fairData = [
-  {
-    node: {
-      counts: {
-        partners: 3,
-      },
-      __id: "RmFpcjp0ZWZhZi1uZXcteW9yay1zcHJpbmctMjAxOQ==",
-      id: "tefaf-new-york-spring-2019",
-      image: {
-        url: "https://d32dm0rphc51dk.cloudfront.net/uSlVwLet3RIOno8LxJGn2g/wide.jpg",
-      },
-      end_at: "2019-05-07T12:00:00+00:00",
-      start_at: "2019-05-03T12:00:00+00:00",
-      name: "TEFAF New York Spring 2019",
+const fairData = {
+  node: {
+    counts: {
+      partners: 3,
     },
+    __id: "RmFpcjp0ZWZhZi1uZXcteW9yay1zcHJpbmctMjAxOQ==",
+    id: "tefaf-new-york-spring-2019",
+    image: {
+      aspect_ratio: 1,
+      url: "https://d32dm0rphc51dk.cloudfront.net/uSlVwLet3RIOno8LxJGn2g/wide.jpg",
+    },
+    end_at: "2019-05-07T12:00:00+00:00",
+    start_at: "2019-05-03T12:00:00+00:00",
+    name: "TEFAF New York Spring 2019",
   },
-]
+}
 
 describe("TabListItem", () => {
   it("renders Fair properly", () => {
