37ea6386d91a80b0896210e8f2374efe69e0eb29
Ashley Jelks <arjelks@gmail.com>
2019-03-01 10:29:29 -0500

    city picker flex styling

--- src/lib/Scenes/City/CityPicker.tsx
+++ src/lib/Scenes/City/CityPicker.tsx
@@ -1,5 +1,4 @@
 import { Box, color, Flex, Sans, Separator, Serif, space } from "@artsy/palette"
-import { cityList } from "lib/Scenes/City/cities"
 import React, { Component } from "react"
 import { Modal, ScrollView, TouchableOpacity } from "react-native"
 import styled from "styled-components/native"
@@ -11,6 +10,8 @@ interface State {
 interface Props {
   overlayVisible: boolean
 }
+
+const cityList = ["New York", "Los Angeles", "London", "Berlin", "Paris", "Hong Kong"]
 
 export class CityPicker extends Component<Props, State> {
   state = {
@@ -29,40 +30,39 @@ export class CityPicker extends Component<Props, State> {
       <Modal visible={overlayVisible} animationType="slide" transparent={true}>
         <Overlay>
           <ScrollView>
-            <Box>
-              <Box mt={2} ml={2}>
-                <Sans size="3t">Select a city</Sans>
-              </Box>
-              {cityList.map((city, i) => (
-                <Box key={i} mt={2} mx={2}>
-                  <TouchableOpacity onPress={() => this.selectCity()}>
-                    <Flex
-                      flexDirection="row"
-                      ref={el => {
-                        if (el) {
-                          this.selectionView = el as any
-                        }
-                      }}
-                    >
-                      <Serif size="10">{city.name}</Serif>
-                    </Flex>
-                  </TouchableOpacity>
-                  <Separator />
-                </Box>
-              ))}
-              <Box mx={2}>
-                <Flex flexDirection="row" py={1} alignItems="flex-start">
-                  <Logo
-                    style={{ height: 32, width: 32 }}
-                    source={require("../../../../Pod/Assets/assets/images/BMW-logo.jpg")}
-                  />
-                  <Sans size="3" weight="medium" ml={1} mt={1}>
-                    Presented in Partnership with BMW
-                  </Sans>
-                </Flex>
-              </Box>
+            <Box mt={2} ml={2}>
+              <Sans size="3" weight="medium">
+                Select a city
+              </Sans>
             </Box>
+            {cityList.map((city, i) => (
+              <Box key={i} mt={2} mx={2}>
+                <TouchableOpacity onPress={() => this.selectCity()}>
+                  <Flex
+                    flexDirection="row"
+                    ref={el => {
+                      if (el) {
+                        this.selectionView = el as any
+                      }
+                    }}
+                  >
+                    <Serif size="8" lineHeight="45">
+                      {city}
+                    </Serif>
+                  </Flex>
+                </TouchableOpacity>
+                <Separator />
+              </Box>
+            ))}
           </ScrollView>
+          <LogoContainer>
+            <Flex flexDirection="row" py={1} alignItems="center">
+              <Logo source={require("../../../../Pod/Assets/assets/images/BMW-logo.jpg")} />
+              <Sans size="3" weight="medium" ml={1} mt={1}>
+                Presented in Partnership with BMW
+              </Sans>
+            </Flex>
+          </LogoContainer>
         </Overlay>
       </Modal>
     )
@@ -72,7 +72,7 @@ export class CityPicker extends Component<Props, State> {
 const Logo = styled.Image`
   height: 32;
   width: 32;
-  margin-top: ${space(0.3)};
+  margin-top: ${space(1)};
 `
 const Overlay = styled.View`
   flex: 1;
@@ -82,4 +82,9 @@ const Overlay = styled.View`
   margin-left: ${space(2)};
   margin-right: ${space(2)};
   border-radius: 25;
+  flex-direction: column;
+`
+const LogoContainer = styled.View`
+  margin-left: ${space(2)};
+  margin-right: ${space(2)};
 `
