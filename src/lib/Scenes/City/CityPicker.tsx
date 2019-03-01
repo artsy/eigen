6caa5de226cf906e3aac55a318a42d90e820f17b
Ash Furrow <ash@ashfurrow.com>
2019-03-01 16:05:54 -0500

    Integrates with CityPicker component.

--- src/lib/Scenes/City/CityPicker.tsx
+++ src/lib/Scenes/City/CityPicker.tsx
@@ -1,85 +1,59 @@
 import { Box, color, Flex, Sans, Separator, Serif, space } from "@artsy/palette"
 import { CircleWhiteCheckIcon } from "lib/Icons/CircleWhiteCheckIcon"
 import React, { Component } from "react"
-import { Modal, ScrollView, TouchableOpacity } from "react-native"
+import { Modal, NativeModules, ScrollView, TouchableOpacity } from "react-native"
 import styled from "styled-components/native"
+import { cityList as cities } from "./cities"
 
 interface State {
-  overlayVisible: boolean
   selectedCity: string
 }
 
-interface Props {
-  overlayVisible: boolean
-}
-
-const cityList = ["New York", "Los Angeles", "London", "Berlin", "Paris", "Hong Kong"]
-export class CityPicker extends Component<Props, State> {
+const cityList = cities.map(city => city.name)
+export class CityPicker extends Component<{}, State> {
   state = {
-    overlayVisible: null,
     selectedCity: null,
   }
 
-  _mounted = null
-
-  componentDidMount() {
-    this._mounted = true
-  }
-
-  componentWillUnmount() {
-    this._mounted = false
-  }
-
-  selectCity(city) {
-    const isCityPickerMounted = this._mounted
-
+  selectCity(city: string, index: number) {
     this.setState({ selectedCity: city })
-
-    // checks that comp is mounted before calling timeout method here to prevent timer
-    // from firing after component is unmounted
-    if (isCityPickerMounted) {
-      setTimeout(() => {
-        this.setState({ overlayVisible: false })
-      }, 1000)
-    }
+    NativeModules.ARNotificationsManager.postNotificationName("ARLocalDiscoveryUserSelectedCity", { cityIndex: index })
   }
 
   render() {
-    const { overlayVisible, selectedCity } = this.state
+    const { selectedCity } = this.state
 
     return (
-      <Modal visible={overlayVisible} animationType="slide" transparent={true}>
-        <Overlay>
-          <ScrollView>
-            <Box mt={2} ml={2}>
-              <Sans size="3" weight="medium">
-                Select a city
-              </Sans>
+      <Overlay>
+        <ScrollView>
+          <Box mt={2} ml={2}>
+            <Sans size="3" weight="medium">
+              Select a city
+            </Sans>
+          </Box>
+          {cityList.map((city, i) => (
+            <Box key={i} mt={2} mx={2}>
+              <TouchableOpacity onPress={() => this.selectCity(city, i)}>
+                <Flex flexDirection="row" justifyContent="space-between" alignItems="flex-start">
+                  <Serif size="8" lineHeight={45}>
+                    {city}
+                  </Serif>
+                  {selectedCity === city && <CircleWhiteCheckIcon width={26} height={26} />}
+                </Flex>
+              </TouchableOpacity>
+              <Separator />
             </Box>
-            {cityList.map((city, i) => (
-              <Box key={i} mt={2} mx={2}>
-                <TouchableOpacity onPress={() => this.selectCity(city)}>
-                  <Flex flexDirection="row" justifyContent="space-between" alignItems="flex-start">
-                    <Serif size="8" lineHeight={45}>
-                      {city}
-                    </Serif>
-                    {selectedCity === city && <CircleWhiteCheckIcon width={26} height={26} />}
-                  </Flex>
-                </TouchableOpacity>
-                <Separator />
-              </Box>
-            ))}
-          </ScrollView>
-          <LogoContainer>
-            <Flex flexDirection="row" py={1} alignItems="center">
-              <Logo source={require("../../../../Pod/Assets/assets/images/BMW-logo.jpg")} />
-              <Sans size="3" weight="medium" ml={1} mt={1}>
-                Presented in Partnership with BMW
-              </Sans>
-            </Flex>
-          </LogoContainer>
-        </Overlay>
-      </Modal>
+          ))}
+        </ScrollView>
+        <LogoContainer>
+          <Flex flexDirection="row" py={1} alignItems="center">
+            <Logo source={require("../../../../Pod/Assets/assets/images/BMW-logo.jpg")} />
+            <Sans size="3" weight="medium" ml={1} mt={1}>
+              Presented in Partnership with BMW
+            </Sans>
+          </Flex>
+        </LogoContainer>
+      </Overlay>
     )
   }
 }
