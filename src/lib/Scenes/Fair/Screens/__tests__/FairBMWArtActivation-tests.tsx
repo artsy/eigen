ea542cc0fe3efd666c7ebd43fa6baeaf29edcc04
Ashley Jelks <arjelks@gmail.com>
2019-03-07 15:17:26 -0500

    updates tests

---
+++ src/lib/Scenes/Fair/Screens/__tests__/FairBMWArtActivation-tests.tsx
@@ -0,0 +1,41 @@
+import SwitchBoard from "lib/NativeModules/SwitchBoard"
+import { MockRelayRenderer } from "lib/tests/MockRelayRenderer"
+import { renderUntil } from "lib/tests/renderUntil"
+import React from "react"
+import { graphql } from "react-relay"
+import { fairFixture } from "../../__fixtures__"
+import { FairBMWArtActivation } from "../FairBMWArtActivation"
+
+jest.unmock("react-relay")
+
+it("renders properly", async () => {
+  const tree = await renderUntil(
+    wrapper => {
+      return wrapper.find("FairBMWArtActivation").length > 0
+    },
+    <MockRelayRenderer
+      Component={FairBMWArtActivation}
+      query={graphql`
+        query FairBMWArtActivationTestsQuery {
+          fair(id: "art-basel-in-miami-beach-2018") {
+            id
+            _id
+            sponsoredContent {
+              activationText
+              pressReleaseUrl
+            }
+          }
+        }
+      `}
+      mockResolvers={{
+        Fair: () => fairFixture,
+      }}
+    />
+  )
+  const dom = tree.text()
+
+  expect(dom).toContain("BMW Art Activations")
+  expect(dom).toContain(fairFixture.sponsoredContent.activationText)
+
+  // @TODO: Add test here for View Press Release button
+})
