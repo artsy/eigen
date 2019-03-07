0f8115fcda304bbcc26dec2efe04c2e52a971ae2
kierangillen <kgillen@gmail.com>
2019-03-07 13:27:57 -0500

    Adds relay to tests

--- src/lib/Scenes/City/Components/Event/__tests__/index-tests.tsx
+++ src/lib/Scenes/City/Components/Event/__tests__/index-tests.tsx
@@ -1,11 +1,13 @@
 import { Theme } from "@artsy/palette"
 import { mount } from "enzyme"
 import React from "react"
+import { RelayProp } from "react-relay"
 import { Event } from "../index"
 
 const eventData = {
   node: {
     name: "PALAY, Trapunto Murals by Pacita Abad",
+    _id: "1234567",
     __id: "U2hvdzpwYWNpdGEtYWJhZC1hcnQtZXN0YXRlLXBhbGF5LXRyYXB1bnRvLW11cmFscy1ieS1wYWNpdGEtYWJhZA==",
     id: "pacita-abad-art-estate-palay-trapunto-murals-by-pacita-abad",
     cover_image: {
@@ -24,7 +26,7 @@ describe("CityEvent", () => {
   it("renders properly", () => {
     const comp = mount(
       <Theme>
-        <Event event={eventData} />
+        <Event event={eventData} relay={{ environment: {} } as RelayProp} />
       </Theme>
     )
 
