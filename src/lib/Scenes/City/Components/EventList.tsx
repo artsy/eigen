d2765e28c056488af11d255aae43449aa8c367d3
Ash Furrow <ash@ashfurrow.com>
2019-03-11 15:55:16 -0400

    linter cleanup.

--- src/lib/Scenes/City/Components/EventList.tsx
+++ src/lib/Scenes/City/Components/EventList.tsx
@@ -1,5 +1,4 @@
 import { Box, Message, Separator, Theme } from "@artsy/palette"
-import { CitySectionList_city } from "__generated__/CitySectionList_city.graphql"
 import { ShowItemRow } from "lib/Components/Lists/ShowItemRow"
 import Spinner from "lib/Components/Spinner"
 import { BucketKey } from "lib/Scenes/Map/bucketCityResults"
