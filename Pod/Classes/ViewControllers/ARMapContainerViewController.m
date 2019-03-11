baf6a63451ac30c141f2b1818c42befb86d6b87e
Luc Succes <l2succes@gmail.com>
2019-03-11 14:01:38 -0400

    Add iOS 11 check

--- Pod/Classes/ViewControllers/ARMapContainerViewController.m
+++ Pod/Classes/ViewControllers/ARMapContainerViewController.m
@@ -233,14 +233,16 @@ Since this controller already has to do the above logic, having it handle the Ci
 
 - (void)updateSafeAreaInsets
 {
-    UIEdgeInsets safeAreaInsets = self.view.safeAreaInsets;
-    [self.mapVC setProperty:@{
-                              @"top": @(safeAreaInsets.top),
-                              @"bottom": @(safeAreaInsets.bottom),
-                              @"left": @(safeAreaInsets.left),
-                              @"right": @(safeAreaInsets.right)
-                              }
-                     forKey:@"safeAreaInsets"];
+    if (@available(iOS 11.0, *)) {
+        UIEdgeInsets safeAreaInsets = self.view.safeAreaInsets;
+        [self.mapVC setProperty:@{
+                                  @"top": @(safeAreaInsets.top),
+                                  @"bottom": @(safeAreaInsets.bottom),
+                                  @"left": @(safeAreaInsets.left),
+                                  @"right": @(safeAreaInsets.right)
+                                  }
+                         forKey:@"safeAreaInsets"];
+    }
 }
 
 # pragma mark - PulleyDelegate Methods
