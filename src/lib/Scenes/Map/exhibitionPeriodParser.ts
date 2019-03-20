cfd6c313f0849b103c9b36b14a226be70ccb45b1
kierangillen <kgillen@gmail.com>
2019-03-20 11:10:11 -0400

    Changes oneYearFromToday to 2 years

--- src/lib/Scenes/Map/exhibitionPeriodParser.ts
+++ src/lib/Scenes/Map/exhibitionPeriodParser.ts
@@ -2,7 +2,7 @@ import moment from "moment"
 
 export const ExhibitionDates = (dateRange: string, endDate: string) => {
   const oneYearFromToday = moment()
-    .add(1, "years")
+    .add(2, "years")
     .utc()
   const exhibitionEndDate = moment(endDate).utc()
   const shouldDisplayOngoing = moment(exhibitionEndDate).isSameOrAfter(oneYearFromToday)
