fa12bf2ee8aee5716db579033f10f7d116e1f2c2
kierangillen <kgillen@gmail.com>
2019-03-22 15:55:12 -0400

    Adds missing )

--- src/lib/Scenes/Show/Components/ShowHeader/Components/Carousel.tsx
+++ src/lib/Scenes/Show/Components/ShowHeader/Components/Carousel.tsx
@@ -4,26 +4,6 @@ import React from "react"
 import { Dimensions, ScrollView, ScrollViewProperties } from "react-native"
 import styled from "styled-components/native"
 
-const ITEM_HEIGHT = 350
-
-const { width: windowWidth } = Dimensions.get("window")
-
-const ImageView = styled(OpaqueImageView)<{ isFirst: boolean; aspectRatio: number }>`
-  height: ${ITEM_HEIGHT};
-  margin-top: ${p => (p.isFirst ? 150 : 0)};
-  ${p =>
-    p.isFirst &&
-    `
-      height: 200;
-      width: ${windowWidth - space(2) - 80};
-    `};
-`
-
-const PageList = styled(ScrollView)`
-  height: ${ITEM_HEIGHT}px;
-  padding-left: ${space(2)};
-`
-
 interface Props extends ScrollViewProperties {
   sources: Array<{
     imageURL: string
@@ -42,16 +22,11 @@ export class Carousel extends React.Component<Props> {
     return sources.map((source, i) => {
       const lastImage = i === sources.length - 1
       return (
-        <Flex
-          key={this.keyForSource(source)}
-          mr={lastImage ? 2 : 1}
-          pr={lastImage ? 2 : 0}
-          alignItems="flex-start"
-        >
+        <Flex key={this.keyForSource(source)} mr={lastImage ? 2 : 1} pr={lastImage ? 2 : 0} alignItems="flex-start">
           <ImageView {...source} isFirst={i === 0} />
         </Flex>
       )
-    }
+    })
   }
 
   render() {
@@ -72,3 +47,23 @@ export class Carousel extends React.Component<Props> {
     )
   }
 }
+
+const ITEM_HEIGHT = 350
+
+const { width: windowWidth } = Dimensions.get("window")
+
+const ImageView = styled(OpaqueImageView)<{ isFirst: boolean; aspectRatio: number }>`
+  height: ${ITEM_HEIGHT};
+  margin-top: ${p => (p.isFirst ? 150 : 0)};
+  ${p =>
+    p.isFirst &&
+    `
+      height: 200;
+      width: ${windowWidth - space(2) - 80};
+    `};
+`
+
+const PageList = styled(ScrollView)`
+  height: ${ITEM_HEIGHT}px;
+  padding-left: ${space(2)};
+`
