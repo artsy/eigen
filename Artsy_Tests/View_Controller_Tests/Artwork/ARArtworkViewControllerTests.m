7d2064dc0134dfb3f238d9f8b1f78f811cd791d1
Eloy Durán <eloy.de.enige@gmail.com>
2019-05-14 17:33:14 +0200

    [Artwork] Test that RN VC is only shown under right circumstances

---
+++ Artsy_Tests/View_Controller_Tests/Artwork/ARArtworkViewControllerTests.m
@@ -0,0 +1,103 @@
+#import "Artwork.h"
+#import "ARArtworkViewController.h"
+#import "ARLegacyArtworkViewController.h"
+#import <Emission/ARArtworkComponentViewController.h>
+
+@interface _ARLegacyArtworkViewControllerMock : UIViewController
+@end
+@implementation _ARLegacyArtworkViewControllerMock
+@end
+
+
+@interface _ARArtworkComponentViewControllerMock : UIViewController
+@end
+@implementation _ARArtworkComponentViewControllerMock
+@end
+
+static void
+StubArtworkWithAvailability(NSString *availability)
+{
+  NSDictionary *response = @{
+                             @"data": @{
+                                 @"artwork": @{
+                                     @"id": @"some-artwork",
+                                     @"title": @"Some Title",
+                                     @"availability": availability
+                                     }
+                                 }
+                             };
+  [OHHTTPStubs stubJSONResponseForHost:@"metaphysics-staging.artsy.net" withResponse:response];
+}
+
+SpecBegin(ARArtworkViewController);
+
+describe(@"ARArtworkViewController", ^{
+  NSArray *legacyAvailabilityStates = @[@"for sale", @"on hold"];
+  NSArray *componentAvailabilityStates = @[@"not for sale", @"on loan", @"permanent collection", @"sold"];
+
+  __block Artwork *artwork = nil;
+  __block ARArtworkViewController *vc = nil;
+
+  __block id mockLegacyVCClass = nil;
+  __block _ARLegacyArtworkViewControllerMock *mockLegacyVC = nil;
+
+  __block id mockComponentVCClass = nil;
+  __block _ARArtworkComponentViewControllerMock *mockComponentVC = nil;
+
+  beforeEach(^{
+    artwork = [[Artwork alloc] initWithArtworkID:@"some-artwork"];
+
+    mockLegacyVC = [_ARLegacyArtworkViewControllerMock new];
+    mockLegacyVCClass = [OCMockObject mockForClass:ARLegacyArtworkViewController.class];
+    (void)[[[mockLegacyVCClass stub] andReturn:mockLegacyVCClass] alloc];
+    (void)[[[mockLegacyVCClass stub] andReturn:mockLegacyVC] initWithArtwork:artwork fair:OCMOCK_ANY];
+
+    mockComponentVC = [_ARArtworkComponentViewControllerMock new];
+    mockComponentVCClass = [OCMockObject mockForClass:ARArtworkComponentViewController.class];
+    (void)[[[mockComponentVCClass stub] andReturn:mockComponentVCClass] alloc];
+    (void)[[[mockComponentVCClass stub] andReturn:mockComponentVC] initWithArtworkID:artwork.artworkID];
+
+    vc = [[ARArtworkViewController alloc] initWithArtwork:artwork fair:nil];
+  });
+
+  afterEach(^{
+    [mockLegacyVCClass stopMocking];
+    [mockComponentVCClass stopMocking];
+  });
+
+  describe(@"concerning artworks for which to show the legacy view", ^{
+    for (NSString *availability in legacyAvailabilityStates) {
+      it([NSString stringWithFormat:@"shows it with a `%@` artwork", availability], ^{
+        StubArtworkWithAvailability(availability);
+        (void)vc.view;
+        expect(vc.childViewControllers[0]).to.equal(mockLegacyVC);
+      });
+    }
+
+    describe(@"when in the context of a fair", ^{
+      beforeEach(^{
+        vc = [[ARArtworkViewController alloc] initWithArtwork:artwork fair:(id)[NSObject new]];
+      });
+
+      for (NSString *availability in componentAvailabilityStates) {
+        it([NSString stringWithFormat:@"shows it with a `%@` artwork", availability], ^{
+          StubArtworkWithAvailability(availability);
+          (void)vc.view;
+          expect(vc.childViewControllers[0]).to.equal(mockLegacyVC);
+        });
+      }
+    });
+  });
+
+  describe(@"concerning artworks for which to show the new component view", ^{
+    for (NSString *availability in componentAvailabilityStates) {
+      it([NSString stringWithFormat:@"shows it with a `%@` artwork", availability], ^{
+        StubArtworkWithAvailability(availability);
+        (void)vc.view;
+        expect(vc.childViewControllers[0]).to.equal(mockComponentVC);
+      });
+    }
+  });
+});
+
+SpecEnd;
