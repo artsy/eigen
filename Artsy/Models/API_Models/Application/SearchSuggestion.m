61cb0521090bcff3107acf616b91d427b4652131
Ash Furrow <ash@ashfurrow.com>
2019-05-16 10:10:15 -0400

    Fixes search result selection for features.

--- Artsy/Models/API_Models/Application/SearchSuggestion.m
+++ Artsy/Models/API_Models/Application/SearchSuggestion.m
@@ -51,7 +51,7 @@
     } else if ([self.model isEqualToString:@"sale"]) {
         return NSStringWithFormat(@"/auction/%@", self.modelID);
     } else if ([self.model isEqualToString:@"feature"]) {
-        return NSStringWithFormat(@"/auction/%@", self.modelID);
+        return NSStringWithFormat(@"/feature/%@", self.modelID);
     }
 
     NSAssert(NO, @"Got an unknown model from search");
