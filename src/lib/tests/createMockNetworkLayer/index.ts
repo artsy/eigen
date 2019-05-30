ce42d1e15a4cbcc2e6cf24099f68cc65e8734163
Eloy Durán <eloy.de.enige@gmail.com>
2019-05-30 14:28:45 +0200

    [createMockNetworkLayer] Update fallback for ID fields

--- src/lib/tests/createMockNetworkLayer/index.ts
+++ src/lib/tests/createMockNetworkLayer/index.ts
@@ -82,8 +82,14 @@ export const createMockFetchQuery = ({
         return inferUnionOrInterfaceType(checkLeafType(source[info.fieldName], info), info)
       }
 
-      if (info.fieldName === "id" || info.fieldName === "id") {
-        // if relay is looking for `id` but we only supplied `id`
+      if (info.fieldName === "id" || info.fieldName === "gravityID" || info.fieldName === "internalID") {
+        // if relay is looking for `id`, `gravityID`, or `internalID`, but we only supplied one of the other
+        if ("gravityID" in source) {
+          return source.gravityID
+        }
+        if ("internalID" in source) {
+          return source.internalID
+        }
         if ("id" in source) {
           return source.id
         }
