1ef6417dd0aa1237e09d88b4255a9e11cb73db5f
Lily Pacegit <lilyfpace@gmail.com>
2019-06-03 17:42:38 -0400

    ts fixes and mock relay render attempt

--- src/lib/Scenes/Artwork/Components/__stories__/ArtworkTombstone.story.tsx
+++ src/lib/Scenes/Artwork/Components/__stories__/ArtworkTombstone.story.tsx
@@ -1,148 +1,44 @@
 import { storiesOf } from "@storybook/react-native"
-import { RelayStubProvider } from "lib/tests/RelayStubProvider"
+import { ArtworkTombstone_artwork } from "__generated__/ArtworkTombstone_artwork.graphql"
+import { MockRelayRenderer } from "lib/tests/MockRelayRenderer"
 import React from "react"
-import { ArtworkTombstone } from "../ArtworkTombstone"
-
-storiesOf("Artwork/Components")
-  .add("Tombstone with 1 artist", () => {
-    return (
-      <RelayStubProvider>
-        <ArtworkTombstone
-          artwork={
-            {
-              title: "Hello im a title",
-              medium: "Painting",
-              date: "1992",
-              artists: [
-                {
-                  name: "Andy Warhol",
-                  href: "/artist/andy-warhol",
-                  id: "1234",
-                  gravityID: "andy-warhol",
-                  is_followed: false,
-                },
-              ],
-              cultural_maker: null,
-              dimensions: {
-                in: "15 × 20 in",
-                cm: "38.1 × 50.8 cm",
-              },
-              edition_of: "Edition 100/200",
-              attribution_class: {
-                short_description: "This is an edition of something",
-              },
-            } as any
+import { graphql } from "react-relay"
+import { ArtworkTombstoneFragmentContainer } from "../ArtworkTombstone"
+const MockArtworkTombstone = ({ artwork }: { artwork: ArtworkTombstone_artwork }) => {
+  return (
+    <MockRelayRenderer
+      Component={ArtworkTombstoneFragmentContainer}
+      mockData={{ artwork }}
+      query={graphql`
+        query ArtworkTombstoneStoryQuery {
+          artwork(id: "unused") {
+            ...ArtworkTombstone_artwork
           }
-        />
-      </RelayStubProvider>
-    )
-  })
-  .add("Tombstone with many artists", () => {
-    return (
-      <ArtworkTombstone
-        artwork={
-          {
-            title: "Hello im a title",
-            medium: "Painting",
-            date: "1992",
-            artists: [
-              { name: "Alex Katz", href: "/artist/alex-katz", id: "4354", gravityID: "alex-katz", is_followed: false },
-              {
-                name: "Pablo Picasso",
-                href: "/artist/pablo-picasso",
-                id: "3433",
-                gravityID: "pablo-picasso",
-                is_followed: false,
-              },
-              { name: "Banksy", href: "/artist/banksy", id: "3468", gravityID: "banksy", is_followed: false },
-              {
-                name: "Barbara Kruger",
-                href: "/artist/barbara-kruger",
-                id: "9874",
-                gravityID: "barbara-kruger",
-                is_followed: false,
-              },
-              {
-                name: "Robert Trundelbed Bananahammock Verylongname",
-                href: "/artist/some-rando",
-                id: "9874",
-                gravityID: "some-rando",
-                is_followed: false,
-              },
-            ],
-            cultural_maker: null,
-            dimensions: {
-              in: "15 × 20 in",
-              cm: "38.1 × 50.8 cm",
-            },
-            edition_of: "Edition 100/200",
-            attribution_class: {
-              short_description: "This is an edition of something",
-            },
-          } as any
         }
-      />
-    )
-  })
-  .add("Tombstone with 3 artists", () => {
-    return (
-      <ArtworkTombstone
-        artwork={
-          {
-            title: "Hello im a title",
-            medium: "Painting",
-            date: "1992",
-            artists: [
-              {
-                name: "Andy Warhol",
-                href: "/artist/andy-warhol",
-                id: "1234",
-                gravityID: "andy-warhol",
-                is_followed: false,
-              },
-              { name: "Alex Katz", href: "/artist/alex-katz", id: "4354", gravityID: "alex-katz", is_followed: false },
-              {
-                name: "Pablo Picasso",
-                href: "/artist/pablo-picasso",
-                id: "3433",
-                gravityID: "pablo-picasso",
-                is_followed: false,
-              },
-            ],
-            cultural_maker: null,
-            dimensions: {
-              in: "15 × 20 in",
-              cm: "38.1 × 50.8 cm",
-            },
-            edition_of: "Edition 100/200",
-            attribution_class: {
-              short_description: "This is an edition of something",
-            },
-          } as any
-        }
-      />
-    )
-  })
-  .add("tombstone with cultural maker", () => {
-    return (
-      <ArtworkTombstone
-        artwork={
-          {
-            title: "Hello im a title",
-            medium: "Painting",
-            date: "1992",
-            artists: [],
-            cultural_maker: "some guy idk",
-            dimensions: {
-              in: "15 × 20 in",
-              cm: "38.1 × 50.8 cm",
-            },
-            edition_of: "Edition 100/200",
-            attribution_class: {
-              short_description: "This is an edition of something",
-            },
-          } as any
-        }
-      />
-    )
-  })
+      `}
+    />
+  )
+}
+const artworkTombstoneArtwork = {
+  title: "Hello im a title",
+  medium: "Painting",
+  date: "1992",
+  artists: [
+    {
+      name: "Andy Warhol",
+      href: "/artist/andy-warhol",
+      id: "1234",
+      gravityID: "andy-warhol",
+      is_followed: false,
+      " $fragmentRefs": null,
+    },
+  ],
+  cultural_maker: null,
+  dimensions: { in: "15 × 20 in", cm: "38.1 × 50.8 cm" },
+  edition_of: "Edition 100/200",
+  attribution_class: { short_description: "This is an edition of something" },
+  " $refType": null,
+}
+storiesOf("Artwork/Components").add("New Component", () => {
+  return <MockArtworkTombstone artwork={artworkTombstoneArtwork} />
+})
