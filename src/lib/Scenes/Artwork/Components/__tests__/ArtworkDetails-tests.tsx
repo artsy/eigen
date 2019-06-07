44ad3f62ec0874440176439a5089ba6bbfdc1f8e
saolsen <steve.olsen@gmail.com>
2019-06-07 11:28:35 -0400

    test the expand

--- src/lib/Scenes/Artwork/Components/__tests__/ArtworkDetails-tests.tsx
+++ src/lib/Scenes/Artwork/Components/__tests__/ArtworkDetails-tests.tsx
@@ -5,27 +5,58 @@ import { ArtworkDetails } from "../ArtworkDetails"
 jest.unmock("react-relay")
 
 describe("Artwork Details", () => {
-  it("renders fields correctly", () => {
+  it("shows 3 or less fields inline", () => {
+    const artworkDetailsInfo = {
+      artwork: {
+        " $refType": null,
+        medium: "Oil on canvas",
+        conditionDescription: null,
+        signature: null,
+        signatureInfo: null,
+        certificateOfAuthenticity: null,
+        framed: { label: "oh yeah", details: "real nice one" },
+        series: null,
+        publisher: null,
+        manufacturer: null,
+        image_rights: "Scala / Art Resource, NY / Picasso, Pablo (1881-1973) © ARS, NY",
+      },
+    }
+
     const component = mount(<ArtworkDetails artwork={artworkDetailsInfo.artwork} />)
     expect(component.text()).toContain("Artwork Details")
     expect(component.text()).toContain("MediumOil")
     expect(component.text()).toContain("Certificate of AuthenticityNot Included")
     expect(component.text()).toContain("FrameIncluded")
   })
-})
 
-const artworkDetailsInfo = {
-  artwork: {
-    " $refType": null,
-    medium: "Oil on canvas",
-    conditionDescription: null,
-    signature: null,
-    signatureInfo: null,
-    certificateOfAuthenticity: null,
-    framed: { label: "oh yeah", details: "real nice one" },
-    series: null,
-    publisher: null,
-    manufacturer: null,
-    image_rights: "Scala / Art Resource, NY / Picasso, Pablo (1881-1973) © ARS, NY",
-  },
-}
+  it("shows top 3 fields if >3 and show more button to reveal the rest", () => {
+    const artworkDetailsInfo = {
+      artwork: {
+        " $refType": null,
+        medium: "Oil on canvas",
+        conditionDescription: { label: "nice", details: "real nice one" },
+        signature: null,
+        signatureInfo: null,
+        certificateOfAuthenticity: null,
+        framed: { label: "oh yeah", details: "real nice one" },
+        series: null,
+        publisher: "steve",
+        manufacturer: null,
+        image_rights: "Scala / Art Resource, NY / Picasso, Pablo (1881-1973) © ARS, NY",
+      },
+    }
+
+    const component = mount(<ArtworkDetails artwork={artworkDetailsInfo.artwork} />)
+    expect(component.text()).toContain("Artwork Details")
+    expect(component.text()).toContain("MediumOil")
+    expect(component.text()).toContain("Conditionnice")
+    expect(component.text()).toContain("Certificate of AuthenticityNot Included")
+    expect(component.text()).not.toContain("FrameIncluded")
+
+    const button = component.find("Button")
+    expect(button.text()).toEqual("Show more artwork details")
+    button.props().onPress()
+    expect(component.text()).toContain("FrameIncluded")
+    expect(component.text()).not.toContain("Show more artwork details")
+  })
+})
