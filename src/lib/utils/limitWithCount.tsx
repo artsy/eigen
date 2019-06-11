4e60dba141f5b18dbfae492657a32510ceace2f2
Lily Pacegit <lilyfpace@gmail.com>
2019-06-11 15:42:09 -0400

    ci fixes

--- src/lib/utils/limitWithCount.tsx
+++ src/lib/utils/limitWithCount.tsx
@@ -2,15 +2,6 @@
  * Helper function to limit array of strings for shorter displays
  * @param initial array of strings to chech for the limit
  * @param limit number of elements to keep
- *
- * @example
-   ```tsx
-   it("returns array of limit number of elements and count of the rest", () => {
-    const initial = ["let", "there", "be", "light"]
-    const limit = 2
-    const limited = limitWithCount(initial, limit)
-    expect(limited).toEqual(["let", "there", "+2 more"]) })
-   ```
  */
 
 export const limitWithCount = (initial: ReadonlyArray<string>, limit: number) => {
