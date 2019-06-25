9b1d45d0eeb09cbe911760c03bc7386e4b8e2445
Lily Pacegit <lilyfpace@gmail.com>
2019-06-25 12:58:00 -0400

    test fixes

--- src/lib/Scenes/Artwork/Components/__tests__/ArtworkInformation-tests.tsx
+++ src/lib/Scenes/Artwork/Components/__tests__/ArtworkInformation-tests.tsx
@@ -11,12 +11,38 @@ describe("ArtworkInformation", () => {
   it("renders all information when the data is present", () => {
     const component = mount(
       <Theme>
-        <ArtworkInformation artwork={ArtworkInformationArtwork as any} />
+        <ArtworkInformation artwork={ArtworkInformationArtwork} />
       </Theme>
     )
     expect(component.text()).toContain("Sold")
     expect(component.text()).toContain("I'm a Gallery")
     expect(component.text()).toContain("Consign with Artsy.")
+  })
+
+  it("doesn't render information when the data is not present", () => {
+    const ArtworkInformationArtworkNoData = {
+      availability: null,
+      partner: {
+        name: null,
+        " $refType": null,
+      },
+      artists: [
+        {
+          is_consignable: false,
+          " $fragmentRefs": null,
+        },
+      ],
+      " $fragmentRefs": null,
+      " $refType": null,
+    }
+    const component = mount(
+      <Theme>
+        <ArtworkInformation artwork={ArtworkInformationArtworkNoData} />
+      </Theme>
+    )
+    expect(component.text()).not.toContain("Sold")
+    expect(component.text()).not.toContain("I'm a Gallery")
+    expect(component.text()).not.toContain("Consign with Artsy.")
   })
 })
 
@@ -24,6 +50,7 @@ const ArtworkInformationArtwork = {
   availability: "Sold",
   partner: {
     name: "I'm a Gallery",
+    " $refType": null,
   },
   artists: [
     {
@@ -31,5 +58,6 @@ const ArtworkInformationArtwork = {
       " $fragmentRefs": null,
     },
   ],
+  " $fragmentRefs": null,
   " $refType": null,
 }
