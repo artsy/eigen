9da8818bd5c7101cec72b089062232df6b910591
saolsen <steve.olsen@gmail.com>
2019-06-25 13:05:08 -0500

    another other test

--- src/lib/Scenes/Artwork/Components/__tests__/ArtworkHistory-tests.tsx
+++ src/lib/Scenes/Artwork/Components/__tests__/ArtworkHistory-tests.tsx
@@ -25,9 +25,7 @@ describe("Artwork History", () => {
     expect(component.text()).toContain("Exhibition History")
     expect(component.text()).toContain("Bibliography")
   })
-})
 
-describe("Artwork History", () => {
   it("renders only set keys", () => {
     const artworkHistoryInfo = {
       artwork: {
@@ -46,5 +44,25 @@ describe("Artwork History", () => {
     expect(component.text()).toContain("Provenance")
     expect(component.text()).not.toContain("Exhibition History")
     expect(component.text()).toContain("Bibliography")
+  })
+
+  it("doesn't render without data", () => {
+    const artworkHistoryInfo = {
+      artwork: {
+        $refType: null,
+        provenance: null,
+        exhibition_history: null,
+        literature: null,
+      },
+    }
+
+    const component = mount(
+      <Theme>
+        <ArtworkHistory artwork={artworkHistoryInfo.artwork} />
+      </Theme>
+    )
+    expect(component.text()).not.toContain("Provenance")
+    expect(component.text()).not.toContain("Exhibition History")
+    expect(component.text()).not.toContain("Bibliography")
   })
 })
