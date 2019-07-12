8321ae0c0f500163026ba81c7f0510d7415fc921
Lily Pacegit <lilyfpace@gmail.com>
2019-07-12 12:59:16 -0400

    artwork details casing fix

--- src/lib/Scenes/Artwork/Components/__tests__/ArtworkDetails-tests.tsx
+++ src/lib/Scenes/Artwork/Components/__tests__/ArtworkDetails-tests.tsx
@@ -23,7 +23,7 @@ describe("Artwork Details", () => {
     }
 
     const component = mount(<ArtworkDetails artwork={artworkDetailsInfo.artwork} />)
-    expect(component.text()).toContain("Artwork Details")
+    expect(component.text()).toContain("Artwork details")
     expect(component.text()).toContain("MediumOil")
     expect(component.text()).toContain("Certificate of AuthenticityNot included")
     expect(component.text()).toContain("FrameIncluded")
@@ -47,7 +47,7 @@ describe("Artwork Details", () => {
     }
 
     const component = mount(<ArtworkDetails artwork={artworkDetailsInfo.artwork} />)
-    expect(component.text()).toContain("Artwork Details")
+    expect(component.text()).toContain("Artwork details")
     expect(component.text()).toContain("MediumOil")
     expect(component.text()).toContain("Conditionnice")
     expect(component.text()).toContain("Certificate of AuthenticityNot included")
@@ -74,7 +74,7 @@ describe("Artwork Details", () => {
     }
 
     const component = mount(<ArtworkDetails artwork={artworkDetailsInfo.artwork} />)
-    expect(component.text()).toContain("Artwork Details")
+    expect(component.text()).toContain("Artwork details")
     expect(component.text()).toContain("MediumOil")
     expect(component.text()).toContain("Conditionnice")
     expect(component.text()).toContain("Certificate of AuthenticityNot included")
