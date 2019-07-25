ba9441a5839f0fd15eb293413b839c7581daeb2a
David Sheldrick <d.j.sheldrick@gmail.com>
2019-07-25 15:45:17 -0400

    increase hit area on close button

--- src/lib/Scenes/Artwork/Components/ImageCarousel/FullScreen/ImageCarouselCloseButton.tsx
+++ src/lib/Scenes/Artwork/Components/ImageCarousel/FullScreen/ImageCarouselCloseButton.tsx
@@ -6,7 +6,7 @@ import { boxShadow } from "styled-system"
 import { SafeAreaInsetsContext } from "../../SafeAreaInsetsContext"
 import { useSpringFade } from "./useSpringFade"
 
-// taken from https://github.com/ds300/eigen/blob/0831853cb574566415f3bd8b3908b26b61f61eec/Artsy/View_Controllers/Util/ARNavigationController.m#L125
+// taken from https://github.com/artsy/eigen/blob/0831853cb574566415f3bd8b3908b26b61f61eec/Artsy/View_Controllers/Util/ARNavigationController.m#L125
 const CLOSE_BUTTON_MARGIN = 12
 
 export const ImageCarouselCloseButton: React.FC<{ onClose(): void }> = ({ onClose }) => {
@@ -18,31 +18,36 @@ export const ImageCarouselCloseButton: React.FC<{ onClose(): void }> = ({ onClos
         position: "absolute",
         left: 0,
         top,
-        width: 40,
-        height: 40,
-        paddingLeft: CLOSE_BUTTON_MARGIN,
-        paddingTop: CLOSE_BUTTON_MARGIN,
-        paddingRight: 20,
-        paddingBottom: 20,
       }}
     >
       <TouchableOpacity onPress={onClose}>
-        <Animated.View
-          style={[
-            boxShadow,
-            {
-              opacity,
-              width: 40,
-              height: 40,
-              borderRadius: 20,
-              backgroundColor: "white",
-              alignItems: "center",
-              justifyContent: "center",
-            },
-          ]}
+        <View
+          style={{
+            width: 40,
+            height: 40,
+            paddingLeft: CLOSE_BUTTON_MARGIN,
+            paddingTop: CLOSE_BUTTON_MARGIN,
+            paddingRight: 20,
+            paddingBottom: 20,
+          }}
         >
-          <CloseIcon />
-        </Animated.View>
+          <Animated.View
+            style={[
+              boxShadow,
+              {
+                opacity,
+                width: 40,
+                height: 40,
+                borderRadius: 20,
+                backgroundColor: "white",
+                alignItems: "center",
+                justifyContent: "center",
+              },
+            ]}
+          >
+            <CloseIcon />
+          </Animated.View>
+        </View>
       </TouchableOpacity>
     </View>
   )
