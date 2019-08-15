4955a0a9f50838f3bb922e8e236b1e87d9347d0f
Luc Succes <l2succes@gmail.com>
2019-08-15 14:47:01 +0200

    Fixes login errors

--- src/lib/Scenes/Collection/Collection.tsx
+++ src/lib/Scenes/Collection/Collection.tsx
@@ -1,13 +1,23 @@
-import { Sans, Theme } from "@artsy/palette"
-import React from "react"
+import { Box, Sans } from "@artsy/palette"
+import { Schema, track } from "lib/utils/track"
+import React, { Component } from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 
-export class Collection extends React.Component<any> {
+interface CollectionProps {
+  collection: any
+}
+
+@track<any>(props => ({
+  context_module: Schema.ContextModules.AboutTheArtist,
+  context_page_owner_slug: props.collection.slug,
+  context_page_owner_id: props.collection.id,
+}))
+export class Collection extends Component<CollectionProps> {
   render() {
     return (
-      <Theme>
+      <Box>
         <Sans size="3t">Hello world</Sans>
-      </Theme>
+      </Box>
     )
   }
 }
@@ -17,6 +27,10 @@ export const CollectionContainer = createFragmentContainer(Collection, {
     fragment Collection_collection on MarketingCollection {
       slug
       title
+      description
+      headerImage
+      category
+      credit
     }
   `,
 })
