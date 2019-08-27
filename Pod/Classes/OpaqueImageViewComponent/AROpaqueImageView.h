92555b100f9ae28385a2cd37109f0378e32d06c2
David Sheldrick <d.j.sheldrick@gmail.com>
2019-08-27 14:19:54 +0100

    de-pointerify new bool props on AROpaqueImageView

--- Pod/Classes/OpaqueImageViewComponent/AROpaqueImageView.h
+++ Pod/Classes/OpaqueImageViewComponent/AROpaqueImageView.h
@@ -3,7 +3,7 @@
 @interface AROpaqueImageView : UIImageView
 @property (nonatomic, strong, readwrite) NSURL *imageURL;
 @property (nonatomic, strong, readwrite) UIColor *placeholderBackgroundColor;
-@property (nonatomic, readwrite) BOOL *noAnimation;
-@property (nonatomic, readwrite) BOOL *failSilently;
-@property (nonatomic, readwrite) BOOL *highPriority;
+@property (nonatomic, assign, readwrite) BOOL noAnimation;
+@property (nonatomic, assign, readwrite) BOOL failSilently;
+@property (nonatomic, assign, readwrite) BOOL highPriority;
 @end
