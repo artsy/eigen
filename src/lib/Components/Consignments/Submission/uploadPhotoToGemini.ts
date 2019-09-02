c6b834ec88f76f3bd9ef52ed0cd2b1f8f1c0f58b
Eloy Durán <eloy.de.enige@gmail.com>
2019-09-02 20:49:16 +0200

    [Consignments] Use Relay for addAssetToConsignment

--- src/lib/Components/Consignments/Submission/uploadPhotoToGemini.ts
+++ src/lib/Components/Consignments/Submission/uploadPhotoToGemini.ts
@@ -1,38 +1,10 @@
-import { metaphysics } from "../../../metaphysics"
+import { addAssetToConsignment } from "./addAssetToConsignment"
 import {
   createGeminiAssetWithS3Credentials,
   getConvectionGeminiKey,
   getGeminiCredentialsForEnvironment,
   uploadFileToS3,
 } from "./geminiUploadToS3"
-import objectToGraphQL from "./objectToGraphQL"
-
-interface ConvectionAssetSubmissionInput {
-  asset_type: string
-  gemini_token: string
-  submission_id: string
-  clientMutationId?: string
-}
-
-export interface GeminiEntryCreationResonse {
-  data: {
-    createGeminiEntryForAsset: {
-      asset: {
-        token: string
-      }
-    }
-  }
-}
-
-export interface GeminiTokenResonse {
-  data: {
-    services: {
-      convection: {
-        geminiTemplateKey: string
-      }
-    }
-  }
-}
 
 export const uploadImageAndPassToGemini = async (file: string, acl: string, submissionID: string) => {
   const convectionKey = await getConvectionGeminiKey()
@@ -44,7 +16,7 @@ export const uploadImageAndPassToGemini = async (file: string, acl: string, subm
   const s3 = await uploadFileToS3(file, acl, assetCredentials)
 
   // Let Gemini know that this file exists and should be processed
-  const geminiAssetToken = await createGeminiAssetWithS3Credentials({
+  const geminiToken = await createGeminiAssetWithS3Credentials({
     sourceKey: s3.key,
     templateKey: convectionKey,
     sourceBucket: assetCredentials.policyDocument.conditions.bucket,
@@ -56,23 +28,8 @@ export const uploadImageAndPassToGemini = async (file: string, acl: string, subm
 
   // Let Convection know that the Gemini asset should be attached to the consignment
   await addAssetToConsignment({
-    asset_type: "image",
-    gemini_token: geminiAssetToken,
-    submission_id: submissionID,
+    assetType: "image",
+    geminiToken,
+    submissionID,
   })
-}
-
-export const addAssetToConsignment = async (options: ConvectionAssetSubmissionInput) => {
-  options.clientMutationId = Math.random().toString(8)
-
-  const input = objectToGraphQL(options, [])
-  const query = `
-    mutation {
-      addAssetToConsignmentSubmission(input: ${input}) {
-        asset {
-          submission_id
-        }
-      }
-    }`
-  return metaphysics<GeminiEntryCreationResonse>({ query, variables: {} })
 }
