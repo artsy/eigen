2d4617d60f2c78e59f13f069061155a33f8459a6
kierangillen <kgillen@gmail.com>
2019-12-16 13:36:29 -0500

    Deletes FormButton

--- src/lib/Components/Consignments/Components/FormElements.tsx
+++ src/lib/Components/Consignments/Components/FormElements.tsx
@@ -1,16 +1,6 @@
-import { Box, Button } from "@artsy/palette"
 import React from "react"
 import { ScrollView, View, ViewProperties } from "react-native"
 import { LargeHeadline } from "../Typography/index"
-
-export interface FormButtonProps extends ViewProperties {
-  /** The text value on the string */
-  text: string
-  /** Optional callback function, not including it implies the button is not enabled */
-  onPress?: React.TouchEventHandler<Button>
-  /** Disables the button from executing onPress and shows in distable styling */
-  disabled?: boolean
-}
 
 /** A re-usable full-screen form with a scrollview */
 export const Form: React.SFC<{ title?: string }> = props => (
@@ -27,12 +17,4 @@ export const Row: React.SFC<ViewProperties> = ({ children, ...props }) => (
   <View {...props} style={[props.style, { flexDirection: "row", paddingVertical: 6, alignItems: "center" }]}>
     {children}
   </View>
-)
-
-export const FormButton: React.SFC<FormButtonProps> = props => (
-  <Box style={Object.assign({ width: 174, marginTop: 20 }, props.style)}>
-    <Button block width={100} variant="primaryWhite" onPress={props.onPress} disabled={props.disabled}>
-      {props.text}
-    </Button>
-  </Box>
 )
