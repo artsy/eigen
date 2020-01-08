3484b110b2f24e1a030abe88b33b364a06db181e
Ash Furrow <ash@ashfurrow.com>
2020-01-08 16:05:47 -0500

    Work-in-progress for Settings UI.

--- src/lib/Scenes/Settings/MyProfile.tsx
+++ src/lib/Scenes/Settings/MyProfile.tsx
@@ -1,24 +1,55 @@
-import { Button } from "@artsy/palette"
-import Serif from "lib/Components/Text/Serif"
+import { Box, Button, Flex, Join, Separator, Serif, Spacer } from "@artsy/palette"
 import SwitchBoard from "lib/NativeModules/SwitchBoard"
 import React from "react"
-import { NativeModules, TouchableWithoutFeedback } from "react-native"
+import { Image, NativeModules, TouchableWithoutFeedback } from "react-native"
 
 export default class MyProfile extends React.Component {
   render() {
     return (
-      <>
-        <Serif>TODO: Implement this according to designs. See: MX-141.</Serif>
-        <TouchableWithoutFeedback onPress={() => SwitchBoard.presentNavigationViewController(this, "privacy-request")}>
-          <Serif>Take me to the new privacy view controller</Serif>
-        </TouchableWithoutFeedback>
-        <Button
-          variant="primaryBlack"
-          onPress={() => NativeModules.ARNotificationsManager.postNotificationName("ARUserRequestedLogout", {})}
-        >
-          Logout
-        </Button>
-      </>
+      <Box px={2} py={1} mx={2} my={3}>
+        <Flex alignItems="center">
+          <Serif size="5">Settings</Serif>
+        </Flex>
+        <Separator m={2} />
+        <Join separator={<Spacer mb={2} />}>
+          <TouchableWithoutFeedback>
+            <Flex flexDirection="row" justifyContent="space-between" alignItems="center">
+              <Serif size="3t">Send Feedback</Serif>
+              <Image source={require("../../../../images/horizontal_chevron.png")} />
+            </Flex>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback
+            onPress={() => SwitchBoard.presentNavigationViewController(this, "privacy-request")}
+          >
+            <Flex flexDirection="row" justifyContent="space-between" alignItems="center">
+              <Serif size="3t">Personal Data Request</Serif>
+              <Image source={require("../../../../images/horizontal_chevron.png")} />
+            </Flex>
+          </TouchableWithoutFeedback>
+          <Button
+            variant="primaryBlack"
+            block
+            size="large"
+            mt={3}
+            onPress={() => NativeModules.ARNotificationsManager.postNotificationName("ARUserRequestedLogout", {})}
+          >
+            Logout
+          </Button>
+        </Join>
+      </Box>
     )
   }
 }
+
+// <>
+//   <Serif>TODO: Implement this according to designs. See: MX-141.</Serif>
+//   <TouchableWithoutFeedback onPress={() => SwitchBoard.presentNavigationViewController(this, "privacy-request")}>
+//     <Serif>Take me to the new privacy view controller</Serif>
+//   </TouchableWithoutFeedback>
+//   <Button
+//     variant="primaryBlack"
+//     onPress={() => NativeModules.ARNotificationsManager.postNotificationName("ARUserRequestedLogout", {})}
+//   >
+//     Logout
+//   </Button>
+// </>
