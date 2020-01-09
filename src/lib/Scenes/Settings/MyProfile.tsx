91abd148c6f9d763e4371559c56e918bfbd23047
Ash Furrow <ash@ashfurrow.com>
2020-01-08 19:24:22 -0500

    Refactors UI for settings screen.

--- src/lib/Scenes/Settings/MyProfile.tsx
+++ src/lib/Scenes/Settings/MyProfile.tsx
@@ -6,50 +6,41 @@ import { Image, NativeModules, TouchableWithoutFeedback } from "react-native"
 export default class MyProfile extends React.Component {
   render() {
     return (
-      <Box px={2} py={1} mx={2} my={3}>
-        <Flex alignItems="center">
-          <Serif size="5">Settings</Serif>
-        </Flex>
-        <Separator m={2} />
-        <Join separator={<Spacer mb={2} />}>
-          <TouchableWithoutFeedback>
-            <Flex flexDirection="row" justifyContent="space-between" alignItems="center">
-              <Serif size="3t">Send Feedback</Serif>
-              <Image source={require("../../../../images/horizontal_chevron.png")} />
-            </Flex>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback
-            onPress={() => SwitchBoard.presentNavigationViewController(this, "privacy-request")}
-          >
-            <Flex flexDirection="row" justifyContent="space-between" alignItems="center">
-              <Serif size="3t">Personal Data Request</Serif>
-              <Image source={require("../../../../images/horizontal_chevron.png")} />
-            </Flex>
-          </TouchableWithoutFeedback>
-          <Button
-            variant="primaryBlack"
-            block
-            size="large"
-            mt={3}
-            onPress={() => NativeModules.ARNotificationsManager.postNotificationName("ARUserRequestedLogout", {})}
-          >
-            Logout
-          </Button>
-        </Join>
-      </Box>
+      <>
+        <Box px={2} py={2} mx={2} mb={2} mt={2}>
+          <Flex alignItems="center">
+            <Serif size="5">Settings</Serif>
+          </Flex>
+        </Box>
+        <Separator />
+        <Box px={2} py={1} mx={2} mt={2}>
+          <Join separator={<Spacer mb={2} />}>
+            <Row title="Send Feedback" />
+            <Row
+              title="Personal Data Request"
+              onPress={() => SwitchBoard.presentNavigationViewController(this, "privacy-request")}
+            />
+            <Button
+              variant="primaryBlack"
+              block
+              size="large"
+              mt={3}
+              onPress={() => NativeModules.ARNotificationsManager.postNotificationName("ARUserRequestedLogout", {})}
+            >
+              Logout
+            </Button>
+          </Join>
+        </Box>
+      </>
     )
   }
 }
 
-// <>
-//   <Serif>TODO: Implement this according to designs. See: MX-141.</Serif>
-//   <TouchableWithoutFeedback onPress={() => SwitchBoard.presentNavigationViewController(this, "privacy-request")}>
-//     <Serif>Take me to the new privacy view controller</Serif>
-//   </TouchableWithoutFeedback>
-//   <Button
-//     variant="primaryBlack"
-//     onPress={() => NativeModules.ARNotificationsManager.postNotificationName("ARUserRequestedLogout", {})}
-//   >
-//     Logout
-//   </Button>
-// </>
+const Row: React.FC<{ title: string; onPress?: () => void }> = ({ title, onPress }) => (
+  <TouchableWithoutFeedback onPress={onPress}>
+    <Flex flexDirection="row" justifyContent="space-between" alignItems="center">
+      <Serif size="3t">{title}</Serif>
+      <Image source={require("../../../../images/horizontal_chevron.png")} />
+    </Flex>
+  </TouchableWithoutFeedback>
+)
