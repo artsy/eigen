6a5070d1d1f54ed81d8e41456988ae5aa94f2d05
brainbicycle <brian.beckerle@artsymail.com>
2020-01-15 09:25:21 -0500

    Separate artist show test cases, consolidate props

--- src/lib/Components/Artist/ArtistShows/__tests__/ArtistShow-tests.tsx
+++ src/lib/Components/Artist/ArtistShows/__tests__/ArtistShow-tests.tsx
@@ -7,37 +7,37 @@ import ArtistShow from "../ArtistShow"
 
 import { Theme } from "@artsy/palette"
 
-it("renders properly", () => {
-  const showProps = {
-    href: "artsy.net/show",
-    cover_image: {
-      url: "artsy.net/image-url",
-    },
-    kind: "solo",
-    name: "Expansive Exhibition",
-    exhibition_period: "Jan 1 - March 1",
-    status_update: "Closing in 2 days",
-    status: "running",
-    partner: {
-      name: "Gallery",
-    },
-    location: {
-      city: "Berlin",
-    },
-  }
+const showProps = {
+  href: "artsy.net/show",
+  cover_image: {
+    url: "artsy.net/image-url",
+  },
+  kind: "solo",
+  name: "Expansive Exhibition",
+  exhibition_period: "Jan 1 - March 1",
+  status_update: "Closing in 2 days",
+  status: "running",
+  partner: {
+    name: "Gallery",
+  },
+  location: {
+    city: "Berlin",
+  },
+}
 
-  const showStyles = {
-    container: {
-      margin: 10,
-      marginBottom: 30,
-      width: 100,
-    },
-    image: {
-      width: 50,
-      height: 50,
-    },
-  }
+const showStyles = {
+  container: {
+    margin: 10,
+    marginBottom: 30,
+    width: 100,
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+}
 
+it("renders properly with all props", () => {
   const show = renderer
     .create(
       <Theme>
@@ -46,22 +46,12 @@ it("renders properly", () => {
     )
     .toJSON()
   expect(show).toMatchSnapshot()
+})
 
+it("renders properly with null show kind", () => {
   const showPropsNullKind = {
-    href: "artsy.net/show",
-    cover_image: {
-      url: "artsy.net/image-url",
-    },
-    name: "Expansive Exhibition",
-    exhibition_period: "Jan 1 - March 1",
-    status_update: "Closing in 2 days",
-    status: "running",
-    partner: {
-      name: "Gallery",
-    },
-    location: {
-      city: "Berlin",
-    },
+    ...showProps,
+    kind: null,
   }
 
   const showNullKind = renderer
