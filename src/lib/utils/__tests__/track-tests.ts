b4b9277ae700e94304303192e6d3a719522f025d
David Sheldrick <d.j.sheldrick@gmail.com>
2020-01-16 19:39:01 +0000

    fix tests

--- src/lib/utils/__tests__/track-tests.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from "../track"
-
-describe("analytics constants", () => {
-  it("action names all start with a lowercase", () => {
-    // grab string values from enum
-    const values = Object.keys(Schema.ActionNames).map(k => Schema.ActionNames[k as any])
-    values.map(name => expect(name[0] === name[0].toLowerCase()).toBeTruthy())
-  })
-})
