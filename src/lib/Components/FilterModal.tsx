f4dd7a4a6be6bd94a60100fd84d739850bd135be
Ashley Jelks <arjelks@gmail.com>
2020-02-19 23:14:49 +0100

    wip commit--cleans up styling on flat list

--- src/lib/Components/FilterModal.tsx
+++ src/lib/Components/FilterModal.tsx
@@ -1,30 +1,22 @@
 import { ArrowLeftIcon, ArrowRightIcon, Box, Button, CloseIcon, color, Flex, Sans, Serif } from "@artsy/palette"
 import React from "react"
-import { FlatList, LayoutAnimation, Modal as RNModal, TouchableWithoutFeedback, ViewProperties } from "react-native"
+import {
+  FlatList,
+  LayoutAnimation,
+  Modal as RNModal,
+  ScrollView,
+  TouchableWithoutFeedback,
+  ViewProperties,
+} from "react-native"
 import NavigatorIOS from "react-native-navigator-ios"
 import styled from "styled-components/native"
-// import { SortOptions } from "./SortOptions"
 
-interface ModalProps extends ViewProperties {
-  // visible: boolean
-  closeModal?: () => void
-  navigator?: NavigatorIOS
-  isFilterArtworksModalVisible: boolean
-}
-
-// TODO:  Define a TypeScript interface to represent possible filter states (take a look at the Pick TypeScript generic)
-interface State {
-  isComponentMounted: boolean
-  sortableItems: Array<{ type: string; data: any }>
-  currentSortOrder: SortOptions
-}
-
-export class FilterModalNavigator extends React.Component<ModalProps, State> {
+export class FilterModalNavigator extends React.Component<FilterModalProps, FilterModalState> {
   state = {
     isComponentMounted: false,
     sortableItems: [],
-    currentSortOrder: SortOptions.Default,
   }
+
   componentDidMount() {
     setTimeout(() => {
       LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
@@ -38,15 +30,7 @@ export class FilterModalNavigator extends React.Component<ModalProps, State> {
     }
   }
 
-  getDefaultSort(type) {
-    switch (type) {
-      case "Sort by":
-        return this.state.currentSortOrder
-    }
-  }
-
   render() {
-    const { sortableItems } = this.state
     const { isFilterArtworksModalVisible } = this.props
 
     return (
@@ -82,59 +66,12 @@ export class FilterModalNavigator extends React.Component<ModalProps, State> {
   }
 }
 
-export const FilterArtworkButtonContainer = styled(Flex)`
-  position: absolute;
-  bottom: 50;
-  flex: 1;
-  justify-content: center;
-  width: 100%;
-  flex-direction: row;
-`
-
-export const FilterArtworkButton = styled(Button)`
-  border-radius: 100;
-  width: 110px;
-`
-
-const ClickDefault = styled(Flex)`
-  border: solid 5px hotpink;
-`
-
-const ModalBackgroundView = styled.View`
-  background-color: #00000099;
-  flex: 1;
-  flex-direction: column;
-`
-
-const ModalInnerView = styled.View<{ visible: boolean }>`
-  flex-direction: column;
-  background-color: white;
-  height: 300px;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-`
-
-const SortRowItem = styled(Flex)`
-  flex-direction: row;
-  justify-content: space-between;
-  border: solid 1px ${color("black10")};
-  border-bottom-width: 0;
-  border-right-width: 0;
-  border-left-width: 0;
-  flex: 1;
-  width: 100%;
-`
-
-interface SortOptionsScreenProps {
-  navigator: NavigatorIOS
-}
 class SortOptionsScreen extends React.Component<SortOptionsScreenProps> {
-  goBack() {
+  handleBackNavigation() {
     this.props.navigator.pop()
   }
 
   renderSortOption = ({ item }) => {
-    console.log("TCL: SortOptionsScreen -> item", item)
     return (
       <SortRowItem>
         <Flex p={2} flexDirection="row" justifyContent="space-between" flexGrow={1}>
@@ -146,56 +83,50 @@ class SortOptionsScreen extends React.Component<SortOptionsScreenProps> {
 
   render() {
     return (
-      <Flex>
-        <Flex flexDirection="row" justifyContent="space-between">
+      <Flex flexGrow={1}>
+        <Flex flexDirection="row" justifyContent="space-between" pr={2}>
           <Flex alignItems="flex-end" mt={0.5} mb={2}>
-            <Box ml={2} mt={2} onTouchStart={() => this.goBack()}>
+            <Box ml={2} mt={2} onTouchStart={() => this.handleBackNavigation()}>
               <ArrowLeftIcon fill="black100" />
             </Box>
           </Flex>
           <Sans mt={2} weight="medium" size="4">
             Sort
           </Sans>
+          <Box></Box>
         </Flex>
-        <FlatList
-          keyExtractor={(_item, index) => String(index)}
-          data={SortOptions}
-          renderItem={item => <Box>{this.renderSortOption(item)}</Box>}
-        />
+        <Flex>
+          <FlatList
+            keyExtractor={(_item, index) => String(index)}
+            data={SortOptions}
+            renderItem={item => <Box>{this.renderSortOption(item)}</Box>}
+          />
+        </Flex>
+        <BackgroundFill />
       </Flex>
     )
   }
 }
 
-interface FilterOptionsState {
-  sortableItems: Array<{ type: string; data: any }>
-}
-
-interface FilterOptionsProps {
-  closeModal: () => void
-  navigator: NavigatorIOS
-}
-
 class FilterOptions extends React.Component<FilterOptionsProps, FilterOptionsState> {
   state = {
-    sortableItems: [],
+    filterOptions: [],
   }
 
-  // TODO: Rename "sortableItem" --> Filter Options
   componentDidMount() {
-    const sortableItems = []
+    const filterOptions = []
 
-    sortableItems.push({
+    filterOptions.push({
       type: "Sort by",
-      onTap: this.navigateToSort,
+      onTap: this.handleNavigationToSortScreen,
     })
 
     this.setState({
-      sortableItems,
+      filterOptions,
     })
   }
 
-  navigateToSort = () => {
+  handleNavigationToSortScreen = () => {
     this.props.navigator.push({
       component: SortOptionsScreen,
     })
@@ -218,10 +149,10 @@ class FilterOptions extends React.Component<FilterOptionsProps, FilterOptionsSta
   }
 
   render() {
-    const { sortableItems } = this.state
+    const { filterOptions } = this.state
 
     return (
-      <Flex>
+      <Flex flexGrow={1}>
         <Flex flexDirection="row" justifyContent="space-between">
           <Flex alignItems="flex-end" mt={0.5} mb={2}>
             <Box ml={2} mt={2} onTouchStart={() => this.props.closeModal()}>
@@ -235,15 +166,61 @@ class FilterOptions extends React.Component<FilterOptionsProps, FilterOptionsSta
             Clear all
           </Sans>
         </Flex>
-        <FlatList
-          keyExtractor={(_item, index) => String(index)}
-          data={sortableItems}
-          renderItem={item => <Box>{this.renderFilterOption(item)}</Box>}
-        />
+        <Flex>
+          <FlatList
+            keyExtractor={(_item, index) => String(index)}
+            data={filterOptions}
+            renderItem={item => <Box>{this.renderFilterOption(item)}</Box>}
+          />
+        </Flex>
+        <BackgroundFill />
       </Flex>
     )
   }
 }
+
+const BackgroundFill = styled(Flex)`
+  background-color: ${color("black10")};
+  flex-grow: 1;
+`
+
+export const FilterArtworkButtonContainer = styled(Flex)`
+  position: absolute;
+  bottom: 50;
+  flex: 1;
+  justify-content: center;
+  width: 100%;
+  flex-direction: row;
+`
+
+export const FilterArtworkButton = styled(Button)`
+  border-radius: 100;
+  width: 110px;
+`
+
+const ModalBackgroundView = styled.View`
+  background-color: #00000099;
+  flex: 1;
+  flex-direction: column;
+`
+
+const ModalInnerView = styled.View<{ visible: boolean }>`
+  flex-direction: column;
+  background-color: white;
+  height: 586px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`
+
+const SortRowItem = styled(Flex)`
+  flex-direction: row;
+  justify-content: space-between;
+  border: solid 0.5px ${color("black10")};
+  border-right-width: 0;
+  border-left-width: 0;
+  flex: 1;
+  width: 100%;
+`
 
 const SortOptions = [
   "Default",
@@ -254,3 +231,39 @@ const SortOptions = [
   "Artwork year (descending)",
   "Artwork year (ascending)",
 ]
+
+interface FilterModalProps extends ViewProperties {
+  closeModal?: () => void
+  navigator?: NavigatorIOS
+  isFilterArtworksModalVisible: boolean
+}
+
+interface FilterModalState {
+  isComponentMounted: boolean
+  sortableItems: Array<{ type: string; data: any }>
+}
+
+interface SortOptionsScreenProps {
+  navigator: NavigatorIOS
+}
+
+interface FilterOptionsState {
+  filterOptions: Array<{ type: string; data: any }>
+}
+
+interface FilterOptionsProps {
+  closeModal: () => void
+  navigator: NavigatorIOS
+}
+
+/**
+ * TODOS:
+ *
+ * 1) Fill the remaining sort options modal + filter options modal areas with the grey background
+ * 2) UI Fixes on Sort Screen
+ * 3) Close modal on background click
+ * 4) Add tests  (check out Bid Flow + Consignments flow)
+ *  e.g. when you click sort the correct component renders in the nav stack
+ * 5) Clean up all the old/unused code
+ *
+ */
