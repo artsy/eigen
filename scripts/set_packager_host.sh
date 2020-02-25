bf33d6d338762d505d140384e441046eecf671e8
David Sheldrick <d.j.sheldrick@gmail.com>
2020-02-25 10:27:24 +0000

    intelligent fallbacks for packager hostname

--- scripts/set_packager_host.sh
+++ scripts/set_packager_host.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
+
+# e.g. Davids-MacBook.local
+host=$(hostname)
+
+function can-reach-host () {
+  ping -c1 -t1 $host
+}
+
+# sometimes hostnames are not resolvable.
+if ! can-reach-host
+then
+  # so try local wifi IP instead
+  host=$(ipconfig getifaddr en0)
+
+  # if you're not connected via wifi, and you're feeling retro,
+  # then you might be connected via ethernet?
+  if ! can-reach-host
+  then
+    host=$(ipconfig getifaddr en1)
+
+    # otherwise you're probably not connected to the internet 🤷‍♀️
+    if ! can-reach-host
+    then
+      host=localhost
+    fi
+  fi
+fi
+
+
 OUTPUT=$(cat << EOF
 // this file is generated automatically in a build phase
 #import "ARReactPackagerHost.h"
@@ -10,7 +39,7 @@ OUTPUT=$(cat << EOF
 + (NSString *)hostname
 {
 #if defined(DEBUG)
-    return @"$(hostname)";
+    return @"$host";
 #else
     return @"localhost";
 #endif
