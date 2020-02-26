0d1897d5ab997d94836ac36e0d0a0df2068e2f5d
Ash Furrow <ash@ashfurrow.com>
2020-02-26 15:49:43 -0500

    Cleanup.

--- src/lib/Components/Home/ArtistRails/ArtistCard.tsx
+++ src/lib/Components/Home/ArtistRails/ArtistCard.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { View } from "react-native"
 import { createFragmentContainer, graphql } from "react-relay"
 
-import { Button, Flex, Join, Sans, Spacer } from "@artsy/palette"
+import { Button, Flex, Join, Sans } from "@artsy/palette"
 import { ArtistCard_artist } from "__generated__/ArtistCard_artist.graphql"
 import ImageView from "lib/Components/OpaqueImageView/OpaqueImageView"
 import SwitchBoard from "lib/NativeModules/SwitchBoard"
--- src/lib/Components/Home/CardScrollView/CardScrollView.tsx
+++ src/lib/Components/Home/CardScrollView/CardScrollView.tsx
@@ -1,21 +1,20 @@
 import { Join, Spacer } from "@artsy/palette"
 import React from "react"
-import styled from "styled-components/native"
+import { ScrollView as RNScrollView } from "react-native"
+import styled from "styled-components"
 
 export const CardScrollView: React.FC = ({ children }) => (
-  <ScrollView>
+  <ScrollView
+    horizontal={true}
+    showsHorizontalScrollIndicator={false}
+    scrollsToTop={false}
+    contentContainerStyle={{ paddingHorizontal: 20 }}
+  >
     <Join separator={<Spacer width="15px" />}>{React.Children.toArray(children)}</Join>
   </ScrollView>
 )
 
-const ScrollView = styled.ScrollView.attrs({
-  horizontal: true,
-  showsHorizontalScrollIndicator: false,
-  scrollsToTop: false,
-  contentContainerStyle: {
-    paddingHorizontal: 20,
-  },
-})`
+const ScrollView = styled(RNScrollView)`
   flex-grow: 1;
   flex-direction: row;
   margin-top: 10px;
