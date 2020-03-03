1fbd2511039369ab022e86c2cbfd1ecd4914a894
David Sheldrick <d.j.sheldrick@gmail.com>
2020-03-03 17:10:13 +0000

    suggestions from PR review

--- src/lib/Components/SectionTitle.tsx
+++ src/lib/Components/SectionTitle.tsx
@@ -2,26 +2,31 @@ import { ArrowRightIcon, Flex, Sans, space } from "@artsy/palette"
 import React from "react"
 import { TouchableOpacity, View } from "react-native"
 
+const Wrapper: React.FC<{ onPress(): any }> = ({ onPress, children }) => {
+  if (onPress) {
+    return (
+      <TouchableOpacity onPress={onPress} data-test-id="touchable-wrapper">
+        {children}
+      </TouchableOpacity>
+    )
+  } else {
+    return <>children</>
+  }
+}
+
 export const SectionTitle: React.FC<{ title: React.ReactChild; subtitle?: React.ReactChild; onPress?: () => any }> = ({
   title,
   subtitle,
   onPress,
 }) => {
-  const Wrapper: React.ComponentType = onPress
-    ? ({ children }) => (
-        <TouchableOpacity onPress={onPress} data-test-id="touchable-wrapper">
-          {children}
-        </TouchableOpacity>
-      )
-    : React.Fragment
   return (
-    <Wrapper>
+    <Wrapper onPress={onPress}>
       <Flex mt="3" mb="1" flexDirection="row" alignItems="center">
         <View style={{ overflow: "hidden", flex: 1 }}>
           <Sans size="4" ellipsizeMode="tail" numberOfLines={1} data-test-id="title">
             {title}
           </Sans>
-          {subtitle && (
+          {Boolean(subtitle) && (
             <Sans size="2" color="black60" data-test-id="subtitle">
               {subtitle}
             </Sans>
@@ -29,7 +34,7 @@ export const SectionTitle: React.FC<{ title: React.ReactChild; subtitle?: React.
         </View>
         {onPress && (
           <View style={{ flexShrink: 0, paddingLeft: space(1) }}>
-            <ArrowRightIcon></ArrowRightIcon>
+            <ArrowRightIcon />
           </View>
         )}
       </Flex>
