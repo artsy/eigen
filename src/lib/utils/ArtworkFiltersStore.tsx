5fd1adeb22bc9fafe725b448f4af9c3c5e39343c
Ashley Jelks <arjelks@gmail.com>
2020-03-12 18:05:53 -0400

    refactors apply filter

--- src/lib/utils/ArtworkFiltersStore.tsx
+++ src/lib/utils/ArtworkFiltersStore.tsx
@@ -1,4 +1,4 @@
-import { some, union } from "lodash"
+import { differenceWith, filter, some, union } from "lodash"
 import React, { createContext, Dispatch, Reducer, useReducer } from "react"
 
 const filterState: ArtworkFilterContextState = {
@@ -12,11 +12,27 @@ export const reducer = (
   artworkFilterState: ArtworkFilterContextState,
   action: FilterActions
 ): ArtworkFilterContextState => {
+  let sanitizeFilters = []
+
   switch (action.type) {
     case "applyFilters":
       const previouslyAppliedFilters = artworkFilterState.appliedFilters
-      const filtersToApply = action.payload
-      const appliedFilters = union(previouslyAppliedFilters, filtersToApply)
+
+      // if a filter is applied for the first time update state with the newly applied filter
+      if (previouslyAppliedFilters.length < 1) {
+        return {
+          applyFilters: true,
+          appliedFilters: action.payload,
+          selectedFilters: [],
+          selectedSortOption: artworkFilterState.selectedSortOption,
+        }
+      }
+
+      // otherwise remove filters that can only be selected once, e.g. "sort" and merge those with newly applied filters
+      const payloadFilters = action.payload
+      sanitizeFilters = filter(previouslyAppliedFilters, prop => prop.filter === "sort")
+      const appliedFilters = union(differenceWith(previouslyAppliedFilters, sanitizeFilters), payloadFilters)
+
       return {
         applyFilters: true,
         appliedFilters,
@@ -29,21 +45,24 @@ export const reducer = (
       const currentlySelectedFilter = action.payload
       const isFilterAlreadySelected = some(previouslySelectedFilters, currentlySelectedFilter)
       const selectedFilter: Array<{ type: SortOption; filter: FilterOption }> = []
+      const filtersApplied = artworkFilterState.appliedFilters
 
       if (isFilterAlreadySelected) {
         const mergedFilters = [...previouslySelectedFilters, currentlySelectedFilter]
+
         return {
           applyFilters: false,
           selectedFilters: mergedFilters,
-          appliedFilters: filterState.appliedFilters,
+          appliedFilters: filtersApplied,
           selectedSortOption: currentlySelectedFilter.type,
         }
       } else {
         selectedFilter.push(currentlySelectedFilter)
+
         return {
           applyFilters: false,
           selectedFilters: selectedFilter,
-          appliedFilters: filterState.appliedFilters,
+          appliedFilters: filtersApplied,
           selectedSortOption: currentlySelectedFilter.type,
         }
       }
