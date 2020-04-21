30b1d9890d6936023a97653822bf6ee045c45c65
Ashley Jelks <arjelks@gmail.com>
2020-04-21 16:13:06 -0400

    move artist series rail to common component

---
+++ src/lib/Components/ArtistSeriesRail.tsx
@@ -0,0 +1,87 @@
+import { color, Sans } from "@artsy/palette"
+import { ArtistSeriesRail_collectionGroup } from "__generated__/ArtistSeriesRail_collectionGroup.graphql"
+import {
+  CARD_RAIL_ARTWORKS_HEIGHT as ARTWORKS_HEIGHT,
+  CardRailArtworkImageContainer as ArtworkImageContainer,
+  CardRailCard,
+  CardRailDivision as Division,
+} from "lib/Components/Home/CardRailCard"
+import { CardRailFlatList } from "lib/Components/Home/CardRailFlatList"
+import ImageView from "lib/Components/OpaqueImageView/OpaqueImageView"
+import SwitchBoard from "lib/NativeModules/SwitchBoard"
+import React, { useRef } from "react"
+import { View } from "react-native"
+import styled from "styled-components/native"
+
+interface ArtistSeriesRailProps {
+  collections: ArtistSeriesRail_collectionGroup["members"]
+}
+
+type ArtistSeriesItem = ArtistSeriesRail_collectionGroup["members"][0]
+
+export const GenericArtistSeriesRail: React.FC<ArtistSeriesRailProps> = ({ collections }) => {
+  console.log("collections", collections)
+  const navRef = useRef<any>()
+  const handleNavigation = slug => {
+    return SwitchBoard.presentNavigationViewController(navRef.current, `/collection/${slug}`)
+  }
+
+  return (
+    <CardRailFlatList<ArtistSeriesItem>
+      data={collections}
+      keyExtractor={(_item, index) => String(index)}
+      initialNumToRender={3}
+      renderItem={({ item: result, index }) => {
+        console.log("result", result)
+
+        const artworkImageURLs = result?.artworksConnection?.edges?.map(edge => edge?.node?.image?.url) ?? []
+        return (
+          <CardRailCard
+            ref={navRef}
+            key={index}
+            onPress={() => {
+              handleNavigation(result.slug)
+            }}
+          >
+            <View>
+              <ArtworkImageContainer>
+                <ImageView width={ARTWORKS_HEIGHT} height={ARTWORKS_HEIGHT} imageURL={artworkImageURLs[0]} />
+                <Division />
+                <View>
+                  <ImageView width={ARTWORKS_HEIGHT / 2} height={ARTWORKS_HEIGHT / 2} imageURL={artworkImageURLs[1]} />
+                  <Division horizontal />
+                  <ImageView
+                    width={ARTWORKS_HEIGHT / 2}
+                    height={ARTWORKS_HEIGHT / 2 - 2}
+                    imageURL={artworkImageURLs[2]}
+                  />
+                </View>
+              </ArtworkImageContainer>
+
+              <MetadataContainer>
+                <ArtistSeriesTitle weight="medium" size="3t">
+                  {result.title}
+                </ArtistSeriesTitle>
+                <ArtistSeriesMeta color={color("black60")} size="3t">
+                  {"From $" + `${result.priceGuidance.toLocaleString()}`}
+                </ArtistSeriesMeta>
+              </MetadataContainer>
+            </View>
+          </CardRailCard>
+        )
+      }}
+    />
+  )
+}
+
+export const ArtistSeriesMeta = styled(Sans)`
+  margin: 0px 15px;
+`
+
+export const ArtistSeriesTitle = styled(Sans)`
+  margin: 15px 15px 0px 15px;
+`
+
+const MetadataContainer = styled.View`
+  margin-bottom: 15px;
+`
--- src/lib/Scenes/Collection/Components/CollectionHubsRails/ArtistSeries/ArtistSeriesRail.tsx
+++ src/lib/Scenes/Collection/Components/CollectionHubsRails/ArtistSeries/ArtistSeriesRail.tsx
@@ -1,16 +1,7 @@
-import { color, Flex, Sans } from "@artsy/palette"
+import { Flex, Sans } from "@artsy/palette"
 import { ArtistSeriesRail_collectionGroup } from "__generated__/ArtistSeriesRail_collectionGroup.graphql"
-import {
-  CARD_RAIL_ARTWORKS_HEIGHT as ARTWORKS_HEIGHT,
-  CardRailArtworkImageContainer as ArtworkImageContainer,
-  CardRailCard,
-  CardRailDivision as Division,
-} from "lib/Components/Home/CardRailCard"
-import { CardRailFlatList } from "lib/Components/Home/CardRailFlatList"
-import ImageView from "lib/Components/OpaqueImageView/OpaqueImageView"
-import SwitchBoard from "lib/NativeModules/SwitchBoard"
-import React, { useRef } from "react"
-import { View } from "react-native"
+import { GenericArtistSeriesRail } from "lib/Components/ArtistSeriesRail"
+import React from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import styled from "styled-components/native"
 
@@ -18,10 +9,7 @@ interface ArtistSeriesRailProps {
   collectionGroup: ArtistSeriesRail_collectionGroup
 }
 
-type ArtistSeriesItem = ArtistSeriesRail_collectionGroup["members"][0]
-
 export const ArtistSeriesRail: React.SFC<ArtistSeriesRailProps> = props => {
-  const navRef = useRef<any>()
   const { collectionGroup } = props
 
   return (
@@ -29,51 +17,7 @@ export const ArtistSeriesRail: React.SFC<ArtistSeriesRailProps> = props => {
       <CollectionName size="4" mb={2} ml={2}>
         {collectionGroup.name}
       </CollectionName>
-      <CardRailFlatList<ArtistSeriesItem>
-        data={collectionGroup?.members}
-        keyExtractor={(_item, index) => String(index)}
-        initialNumToRender={3}
-        renderItem={({ item: result, index }) => {
-          const artworkImageURLs = result?.artworksConnection?.edges?.map(edge => edge?.node?.image?.url) ?? []
-
-          return (
-            <CardRailCard
-              ref={navRef}
-              key={index}
-              onPress={() => SwitchBoard.presentNavigationViewController(navRef.current, `/collection/${result.slug}`)}
-            >
-              <View>
-                <ArtworkImageContainer>
-                  <ImageView width={ARTWORKS_HEIGHT} height={ARTWORKS_HEIGHT} imageURL={artworkImageURLs[0]} />
-                  <Division />
-                  <View>
-                    <ImageView
-                      width={ARTWORKS_HEIGHT / 2}
-                      height={ARTWORKS_HEIGHT / 2}
-                      imageURL={artworkImageURLs[1]}
-                    />
-                    <Division horizontal />
-                    <ImageView
-                      width={ARTWORKS_HEIGHT / 2}
-                      height={ARTWORKS_HEIGHT / 2 - 2}
-                      imageURL={artworkImageURLs[2]}
-                    />
-                  </View>
-                </ArtworkImageContainer>
-
-                <MetadataContainer>
-                  <ArtistSeriesTitle weight="medium" size="3t">
-                    {result.title}
-                  </ArtistSeriesTitle>
-                  <ArtistSeriesMeta color={color("black60")} size="3t">
-                    {"From $" + `${result.priceGuidance.toLocaleString()}`}
-                  </ArtistSeriesMeta>
-                </MetadataContainer>
-              </View>
-            </CardRailCard>
-          )
-        }}
-      />
+      <GenericArtistSeriesRail collections={collectionGroup?.members} />
     </ArtistSeriesWrapper>
   )
 }
@@ -88,10 +32,6 @@ export const ArtistSeriesMeta = styled(Sans)`
 
 export const ArtistSeriesTitle = styled(Sans)`
   margin: 15px 15px 0px 15px;
-`
-
-const MetadataContainer = styled.View`
-  margin-bottom: 15px;
 `
 
 export const CollectionName = styled(Sans)``
