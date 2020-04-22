fb36d9a52dadb734f15c3399988f90975066b174
Pavlos Vinieratos <pvinis@gmail.com>
2020-04-22 13:19:16 +0200

    boom! second hunk

--- src/lib/Components/AboveTheFoldFlatList.tsx
+++ src/lib/Components/AboveTheFoldFlatList.tsx
@@ -3,6 +3,7 @@ import { FlatList, FlatListProps } from "react-native"
 
 export function AboveTheFoldFlatList<ItemType>(props: { initialNumToRender: number } & FlatListProps<ItemType>) {
   const [userHasScrolled, setUserHasScrolled] = useState(false)
+
   return (
     <FlatList<ItemType>
       {...props}
