1d04e3f263fb6294ec57ad0b66c3b3db2466b923
David Sheldrick <d.j.sheldrick@gmail.com>
2020-04-30 10:42:52 +0100

    fix sales type in SaleList

--- src/lib/Scenes/Sales/Components/SaleList.tsx
+++ src/lib/Scenes/Sales/Components/SaleList.tsx
@@ -2,11 +2,16 @@ import { isEmpty } from "lodash"
 import React from "react"
 import { View } from "react-native"
 
+import { SaleListItem_sale } from "__generated__/SaleListItem_sale.graphql"
 import { SectionTitle } from "lib/Components/SectionTitle"
 import { useScreenDimensions } from "lib/utils/useScreenDimensions"
+import { FragmentRef } from "react-relay"
 import SaleListItem from "./SaleListItem"
 
-export const SaleList: React.FC<{ sales: any[]; title: string }> = ({ sales, title }) => {
+export const SaleList: React.FC<{ sales: Array<FragmentRef<SaleListItem_sale>>; title: string }> = ({
+  sales,
+  title,
+}) => {
   const { width } = useScreenDimensions()
   const isIPad = width > 700
   const columnCount = isIPad ? 4 : 2
