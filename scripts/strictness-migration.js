f363cee88908dd216860f77947d34bd4a641c731
David Sheldrick <d.j.sheldrick@gmail.com>
2020-05-01 13:25:17 +0100

    better copy for CI strictness tags check

--- scripts/strictness-migration.js
+++ scripts/strictness-migration.js
@@ -4,6 +4,8 @@ const glob = require("glob").sync
 const fs = require("fs")
 const _ = require("lodash")
 const path = require("path")
+const chalk = require("chalk").default
+const { execSync } = require("child_process")
 
 const HERO_HELPER_FILENAME = ".i-am-helping-out-with-the-strictness-migration"
 
@@ -19,15 +21,16 @@ function countIssuesInFile(filePath) {
   )
 }
 
+function countIssuesInWholeProject() {
+  return glob("src/**/*.ts{x,}")
+    .map(countIssuesInFile)
+    .reduce((a, b) => a + b, 0)
+}
+
 switch (process.argv[2]) {
   case "count":
     if (process.argv.length === 3) {
-      console.log(
-        glob("src/**/*.ts{x,}")
-          .map(countIssuesInFile)
-          .reduce((a, b) => a + b, 0)
-          .toString()
-      )
+      console.log(countIssuesInWholeProject().toString())
     } else {
       console.log(
         _.flatten(process.argv.slice(3).map(f => glob(f, { nodir: true })))
@@ -71,4 +74,32 @@ switch (process.argv[2]) {
     }
 
     break
+  case "check-pr":
+    const current = countIssuesInWholeProject()
+    execSync("git checkout origin/master", { stdio: "inherit" })
+    const onMaster = countIssuesInWholeProject()
+    if (current > onMaster) {
+      console.error(
+        chalk.yellow.bold("WARNING:"),
+        `The number of comments with the substring ${chalk.cyan("STRICTNESS_MIGRATION")} has risen from ${chalk.green(
+          onMaster
+        )} in ${chalk.bold("master")} to ${chalk.red(current)} in this PR.`
+      )
+      console.error(
+        `These comments were added when we switched on TypeScript's strict mode, and their number should only ever go down.\n`
+      )
+
+      console.error(
+        `It is safe to ignore this failure if you are in a hurry, but please consider refactoring to remove these tags.`
+      )
+      process.exit(1)
+    } else if (current < onMaster) {
+      console.log(
+        `Yay! Thanks for decreasing the number of strictness migration issues from ${chalk.bold(
+          onMaster
+        )} to ${chalk.bold(current)} 🌟`
+      )
+    } else {
+      console.log(`There's still ${chalk.bold(onMaster)} strictness migration issues to fix.`)
+    }
 }
