81fe01347b0a73a897b5bd5c8df8d64c47efd3f6
Ashley Jelks <arjelks@gmail.com>
2020-05-04 17:26:17 -0400

    link toggle state to selectedFilters state

--- src/lib/Components/ArtworkFilterOptions/FilterToggleButton.tsx
+++ src/lib/Components/ArtworkFilterOptions/FilterToggleButton.tsx
@@ -1,6 +1,7 @@
 import { color } from "@artsy/palette"
 import { WaysToBuyOptions } from "lib/Scenes/Collection/Helpers/FilterArtworksHelpers"
-import React, { useState } from "react"
+import { ArtworkFilterContext } from "lib/utils/ArtworkFiltersStore"
+import React, { useContext } from "react"
 import { Switch, View } from "react-native"
 
 interface FilterToggleButtonProps {
@@ -10,12 +11,8 @@ interface FilterToggleButtonProps {
 
 export const FilterToggleButton: React.FC<FilterToggleButtonProps> = props => {
   const { onSelect, optionSelection } = props
-
-  const [isSwitchEnabled, setIsSwitchEnabled] = useState(false)
-
+  const { state } = useContext(ArtworkFilterContext)
   const handleToggle = (selectOption: (selection: WaysToBuyOptions) => void, selection: WaysToBuyOptions) => {
-    setIsSwitchEnabled(!isSwitchEnabled)
-
     selectOption(selection)
   }
 
@@ -24,7 +21,7 @@ export const FilterToggleButton: React.FC<FilterToggleButtonProps> = props => {
       <Switch
         trackColor={{ false: color("black10"), true: color("black100") }}
         onValueChange={() => handleToggle(onSelect, optionSelection)}
-        value={isSwitchEnabled}
+        value={state.selectedFilters.find(filter => filter.value === optionSelection) === undefined ? false : true}
       />
     </View>
   )
