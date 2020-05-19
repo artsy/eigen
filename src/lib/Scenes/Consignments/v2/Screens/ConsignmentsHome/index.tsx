1f42f21dbf1fd4ff635e4766f29f14d562121aa4
Steven Hicks <steven.j.hicks@gmail.com>
2020-05-19 08:54:17 -0500

    Extract landing page modules to individual components to make it easier to build up a placeholder

--- src/lib/Scenes/Consignments/v2/Screens/ConsignmentsHome/index.tsx
+++ src/lib/Scenes/Consignments/v2/Screens/ConsignmentsHome/index.tsx
@@ -20,7 +20,8 @@ interface Props {
 export const ConsignmentsHome: React.FC<Props> = props => {
   const navRef = useRef<ScrollView>(null)
   const { artists } = props
-  const handlePress = () => {
+
+  const handleCTAPress = () => {
     if (navRef.current) {
       const route = "/collections/my-collection/artworks/new/submissions/new"
       SwitchBoard.presentModalViewController(navRef.current, route)
@@ -29,151 +30,175 @@ export const ConsignmentsHome: React.FC<Props> = props => {
 
   return (
     <ScrollView ref={navRef}>
-      <Box px={2} py={6}>
-        <Box>
-          <Sans size="8" textAlign="center" px={2}>
-            Sell Art From Your Collection
-          </Sans>
-
-          <Spacer my={0.5} />
-
-          <Sans size="4" textAlign="center">
-            Reach art buyers all over the world.
-          </Sans>
-        </Box>
-
-        <Spacer mb={2} />
-
-        <Button variant="primaryBlack" block onPress={handlePress}>
-          <Sans size="3" weight="medium">
-            Start selling
-          </Sans>
-        </Button>
-      </Box>
+      <HeaderCTA handleCTAPress={handleCTAPress} />
 
       <Separator my={3} />
 
-      <Box px={2}>
-        <Sans size="8">How it works</Sans>
-
-        <Spacer mb={2} />
-        <Flex flexDirection="row">
-          <Box pr={2}>
-            <EditIcon width={30} height={30} />
-          </Box>
-
-          <FlexChildThatWontStretchOutsideOfParent>
-            <Sans size="4">Submit Once</Sans>
-            <Spacer mb={0.3} />
-            <Sans color="black60" size="3t">
-              With a single submission, you'll access art buyers around the world.
-            </Sans>
-          </FlexChildThatWontStretchOutsideOfParent>
-        </Flex>
-        <Spacer mb={2} />
-        <Flex flexDirection="row">
-          <Box pr={2}>
-            <AuctionIcon width={30} height={30} />
-          </Box>
-
-          <FlexChildThatWontStretchOutsideOfParent>
-            <Sans size="4">Make Your Sale</Sans>
-            <Spacer mb={0.3} />
-            <Sans color="black60" size="3t">
-              Choose from several Artsy marketplace resale options.
-            </Sans>
-          </FlexChildThatWontStretchOutsideOfParent>
-        </Flex>
-        <Spacer mb={2} />
-        <Flex flexDirection="row">
-          <Box pr={2}>
-            <EnvelopeIcon width={30} height={30} />
-          </Box>
-
-          <FlexChildThatWontStretchOutsideOfParent>
-            <Sans size="4">Receive Payment</Sans>
-            <Spacer mb={0.3} />
-            <Sans color="black60" size="3t">
-              Keep the work until it sells, ship it with our help, and receive payment.
-            </Sans>
-          </FlexChildThatWontStretchOutsideOfParent>
-        </Flex>
-      </Box>
+      <HowItWorks />
 
       <Separator my={3} />
 
-      <Box px={2}>
-        <Box>
-          <Sans size="4">Artists collectors are looking to buy</Sans>
-
-          <Spacer mb={2} />
-
-          <ScrollView horizontal>
-            <ArtistList artists={artists} />
-          </ScrollView>
-        </Box>
-      </Box>
+      <ArtistsCollectorsAreLookingToBuy artists={artists} />
 
       <Separator my={3} />
 
-      <Box px={2} pb={6}>
-        <Sans size="8">Why sell with Artsy?</Sans>
-
-        <Spacer mb={2} />
-
-        <Flex flexDirection="row">
-          <NumberBox pl={0.5} pr={1}>
-            <Sans size="4">1</Sans>
-          </NumberBox>
-
-          <FlexChildThatWontStretchOutsideOfParent>
-            <Sans size="4">Simple Steps</Sans>
-            <Spacer mb={0.3} />
-            <Sans color="black60" size="3t">
-              Submit your work once, pick the best offer, and ship the work when it sells.
-            </Sans>
-          </FlexChildThatWontStretchOutsideOfParent>
-        </Flex>
-
-        <Spacer mb={2} />
-
-        <Flex flexDirection="row">
-          <NumberBox pl={0.5} pr={1}>
-            <Sans size="4">2</Sans>
-          </NumberBox>
-
-          <FlexChildThatWontStretchOutsideOfParent>
-            <Sans size="4">Industry Expertise</Sans>
-            <Spacer mb={0.3} />
-            <Sans color="black60" size="3t">
-              Receive virtual valuation and expert guidance on the best sales strategies.
-            </Sans>
-          </FlexChildThatWontStretchOutsideOfParent>
-        </Flex>
-
-        <Spacer mb={2} />
-
-        <Flex flexDirection="row">
-          <NumberBox pl={0.5} pr={1}>
-            <Sans size="4">3</Sans>
-          </NumberBox>
-
-          <FlexChildThatWontStretchOutsideOfParent>
-            <Sans size="4">Global Reach</Sans>
-            <Spacer mb={0.3} />
-            <Sans color="black60" size="3t">
-              Your work will reach the world's collectors, galleries, and auction houses.
-            </Sans>
-          </FlexChildThatWontStretchOutsideOfParent>
-        </Flex>
-
-        <Spacer mb={3} />
-
-        <Button variant="primaryBlack" block onPress={handlePress}>
-          <Sans size="3">Start selling</Sans>
-        </Button>
-      </Box>
+      <FooterCTA handleCTAPress={handleCTAPress} />
     </ScrollView>
+  )
+}
+
+const HeaderCTA: React.FC<{ handleCTAPress: () => void }> = ({ handleCTAPress }) => {
+  return (
+    <Box px={2} py={6}>
+      <Box>
+        <Sans size="8" textAlign="center" px={2}>
+          Sell Art From Your Collection
+        </Sans>
+
+        <Spacer my={0.5} />
+
+        <Sans size="4" textAlign="center">
+          Reach art buyers all over the world.
+        </Sans>
+      </Box>
+
+      <Spacer mb={2} />
+
+      <Button variant="primaryBlack" block onPress={handleCTAPress}>
+        <Sans size="3" weight="medium">
+          Start selling
+        </Sans>
+      </Button>
+    </Box>
+  )
+}
+
+const HowItWorks: React.FC = () => {
+  return (
+    <Box px={2}>
+      <Sans size="8">How it works</Sans>
+
+      <Spacer mb={2} />
+      <Flex flexDirection="row">
+        <Box pr={2}>
+          <EditIcon width={30} height={30} />
+        </Box>
+
+        <FlexChildThatWontStretchOutsideOfParent>
+          <Sans size="4">Submit Once</Sans>
+          <Spacer mb={0.3} />
+          <Sans color="black60" size="3t">
+            With a single submission, you'll access art buyers around the world.
+          </Sans>
+        </FlexChildThatWontStretchOutsideOfParent>
+      </Flex>
+      <Spacer mb={2} />
+      <Flex flexDirection="row">
+        <Box pr={2}>
+          <AuctionIcon width={30} height={30} />
+        </Box>
+
+        <FlexChildThatWontStretchOutsideOfParent>
+          <Sans size="4">Make Your Sale</Sans>
+          <Spacer mb={0.3} />
+          <Sans color="black60" size="3t">
+            Choose from several Artsy marketplace resale options.
+          </Sans>
+        </FlexChildThatWontStretchOutsideOfParent>
+      </Flex>
+      <Spacer mb={2} />
+      <Flex flexDirection="row">
+        <Box pr={2}>
+          <EnvelopeIcon width={30} height={30} />
+        </Box>
+
+        <FlexChildThatWontStretchOutsideOfParent>
+          <Sans size="4">Receive Payment</Sans>
+          <Spacer mb={0.3} />
+          <Sans color="black60" size="3t">
+            Keep the work until it sells, ship it with our help, and receive payment.
+          </Sans>
+        </FlexChildThatWontStretchOutsideOfParent>
+      </Flex>
+    </Box>
+  )
+}
+
+const ArtistsCollectorsAreLookingToBuy: React.FC<{ artists: ConsignmentsHome_artists }> = ({ artists }) => {
+  return (
+    <Box px={2}>
+      <Box>
+        <Sans size="4">Artists collectors are looking to buy</Sans>
+
+        <Spacer mb={2} />
+
+        <ScrollView horizontal>
+          <ArtistList artists={artists} />
+        </ScrollView>
+      </Box>
+    </Box>
+  )
+}
+
+const FooterCTA: React.FC<{ handleCTAPress: () => void }> = ({ handleCTAPress }) => {
+  return (
+    <Box px={2} pb={6}>
+      <Sans size="8">Why sell with Artsy?</Sans>
+
+      <Spacer mb={2} />
+
+      <Flex flexDirection="row">
+        <NumberBox pl={0.5} pr={1}>
+          <Sans size="4">1</Sans>
+        </NumberBox>
+
+        <FlexChildThatWontStretchOutsideOfParent>
+          <Sans size="4">Simple Steps</Sans>
+          <Spacer mb={0.3} />
+          <Sans color="black60" size="3t">
+            Submit your work once, pick the best offer, and ship the work when it sells.
+          </Sans>
+        </FlexChildThatWontStretchOutsideOfParent>
+      </Flex>
+
+      <Spacer mb={2} />
+
+      <Flex flexDirection="row">
+        <NumberBox pl={0.5} pr={1}>
+          <Sans size="4">2</Sans>
+        </NumberBox>
+
+        <FlexChildThatWontStretchOutsideOfParent>
+          <Sans size="4">Industry Expertise</Sans>
+          <Spacer mb={0.3} />
+          <Sans color="black60" size="3t">
+            Receive virtual valuation and expert guidance on the best sales strategies.
+          </Sans>
+        </FlexChildThatWontStretchOutsideOfParent>
+      </Flex>
+
+      <Spacer mb={2} />
+
+      <Flex flexDirection="row">
+        <NumberBox pl={0.5} pr={1}>
+          <Sans size="4">3</Sans>
+        </NumberBox>
+
+        <FlexChildThatWontStretchOutsideOfParent>
+          <Sans size="4">Global Reach</Sans>
+          <Spacer mb={0.3} />
+          <Sans color="black60" size="3t">
+            Your work will reach the world's collectors, galleries, and auction houses.
+          </Sans>
+        </FlexChildThatWontStretchOutsideOfParent>
+      </Flex>
+
+      <Spacer mb={3} />
+
+      <Button variant="primaryBlack" block onPress={handleCTAPress}>
+        <Sans size="3">Start selling</Sans>
+      </Button>
+    </Box>
   )
 }
 
