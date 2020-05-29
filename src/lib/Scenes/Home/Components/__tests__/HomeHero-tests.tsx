42061eae883c0fc258322b8e8a36657f42cb88ba
David Sheldrick <d.j.sheldrick@gmail.com>
2020-05-29 11:21:29 +0100

    test credit line

--- src/lib/Scenes/Home/Components/__tests__/HomeHero-tests.tsx
+++ src/lib/Scenes/Home/Components/__tests__/HomeHero-tests.tsx
@@ -1,9 +1,8 @@
 import { HomeHeroTestsQuery } from "__generated__/HomeHeroTestsQuery.graphql"
-import OpaqueImageView from "lib/Components/OpaqueImageView/OpaqueImageView"
 import SwitchBoard from "lib/NativeModules/SwitchBoard"
 import { extractText } from "lib/tests/extractText"
 import React from "react"
-import { TouchableOpacity } from "react-native"
+import { Image, TouchableOpacity } from "react-native"
 import { graphql, QueryRenderer } from "react-relay"
 import { create } from "react-test-renderer"
 import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
@@ -40,14 +39,36 @@ describe("HomeHero", () => {
         HomePageHeroUnit() {
           return {
             title: "Art Keeps Going",
+            subtitle: "Art in the time of pandemic",
             linkText: "Learn More",
+            creditLine: "Andy Warhol, 1973",
           }
         },
       })
     )
 
-    expect(tree.root.findAllByType(OpaqueImageView)).toHaveLength(1)
-    expect(extractText(tree.root)).toMatchInlineSnapshot(`"Art Keeps GoingLearn More"`)
+    expect(tree.root.findAllByType(Image)).toHaveLength(1)
+    expect(extractText(tree.root)).toMatchInlineSnapshot(`"Art Keeps GoingArt in the time of pandemicLearn More"`)
+  })
+
+  it(`only shows the credit line after the image has loaded`, () => {
+    const tree = create(<TestRenderer />)
+    environment.mock.resolveMostRecentOperation(op =>
+      MockPayloadGenerator.generate(op, {
+        HomePageHeroUnit() {
+          return {
+            title: "Art Keeps Going",
+            subtitle: "Art in the time of pandemic",
+            linkText: "Learn More",
+            creditLine: "Andy Warhol, 1973",
+          }
+        },
+      })
+    )
+
+    expect(extractText(tree.root)).not.toContain("Warhol")
+    tree.root.findByType(Image).props.onLoad()
+    expect(extractText(tree.root)).toContain("Warhol")
   })
 
   it("is tappable", () => {
