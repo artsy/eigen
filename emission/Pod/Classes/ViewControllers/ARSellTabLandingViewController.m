e1653b99dbb58a471e105eb1c23300564f883185
Steven Hicks <steven.j.hicks@gmail.com>
2020-05-29 14:56:13 -0500

    Implement a standalone route for the landing page

---
+++ emission/Pod/Classes/ViewControllers/ARSellTabLandingViewController.m
@@ -0,0 +1,10 @@
+#import "ARSellTabLandingViewController.h"
+
+@implementation ARSellTabLandingViewController
+
+- (instancetype)init
+{
+    return [super initWithEmission:nil moduleName:@"SellTabLanding" initialProperties:nil];
+}
+
+@end
