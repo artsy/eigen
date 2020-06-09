b27c72fadfb957f53bb689f0754411741d1fa556
Pavlos Vinieratos <pvinis@gmail.com>
2020-06-09 16:33:17 +0200

    add a dev menu option for VR

--- src/lib/AppRegistry.tsx
+++ src/lib/AppRegistry.tsx
@@ -46,7 +46,7 @@ import { ShowArtistsRenderer, ShowArtworksRenderer, ShowMoreInfoRenderer } from 
 import { ShowRenderer } from "./Scenes/Show/Show"
 import { ViewingRoomRenderer } from "./Scenes/ViewingRoom/ViewingRoom"
 import { ViewingRoomArtworksRenderer } from "./Scenes/ViewingRoom/ViewingRoomArtworks"
-import { ViewingRoomsListRenderer } from "./Scenes/ViewingRoom/ViewingRoomsList"
+import { ViewingRoomsListQueryRenderer } from "./Scenes/ViewingRoom/ViewingRoomsList"
 import { Schema, screenTrack, track } from "./utils/track"
 import { ProvideScreenDimensions, useScreenDimensions } from "./utils/useScreenDimensions"
 
@@ -314,7 +314,7 @@ register("Show", ShowRenderer)
 register("ShowArtists", ShowArtists)
 register("ShowArtworks", ShowArtworks)
 register("ShowMoreInfo", ShowMoreInfo)
-register("ViewingRooms", ViewingRoomsListRenderer)
+register("ViewingRooms", ViewingRoomsListQueryRenderer)
 register("ViewingRoom", ViewingRoomRenderer, { fullBleed: true })
 register("ViewingRoomArtworks", ViewingRoomArtworksRenderer)
 register("WorksForYou", WorksForYouRenderer)
