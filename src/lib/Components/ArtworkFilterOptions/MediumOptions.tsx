3cabfb63adbca7ca3fc591a26f5aeb7b278cc3b7
brainbicycle <brian.beckerle@artsymail.com>
2020-06-10 16:44:03 -0400

    Convert mediums to aggregate filters

--- src/lib/Components/ArtworkFilterOptions/MediumOptions.tsx
+++ src/lib/Components/ArtworkFilterOptions/MediumOptions.tsx
@@ -1,7 +1,8 @@
-import { MediumOption, OrderedMediumFilters } from "lib/Scenes/Collection/Helpers/FilterArtworksHelpers"
+import { AggregateOption, FilterParamName, FilterType } from "lib/Scenes/Collection/Helpers/FilterArtworksHelpers"
 import { ArtworkFilterContext, useSelectedOptionsDisplay } from "lib/utils/ArtworkFiltersStore"
 import React, { useContext } from "react"
 import { NavigatorIOS } from "react-native"
+import { aggregationFromFilterType } from "../FilterModal"
 import { SingleSelectOptionScreen } from "./SingleSelectOption"
 
 interface MediumOptionsScreenProps {
@@ -9,22 +10,40 @@ interface MediumOptionsScreenProps {
 }
 
 export const MediumOptionsScreen: React.SFC<MediumOptionsScreenProps> = ({ navigator }) => {
-  const { dispatch } = useContext(ArtworkFilterContext)
+  const { dispatch, aggregations } = useContext(ArtworkFilterContext)
 
-  const filterType = "medium"
+  const filterType = FilterType.medium
+  const aggregationName = aggregationFromFilterType(filterType)
+  const aggregation = aggregations!.filter(value => value.slice === aggregationName)[0]
+  const options = aggregation.counts.map(aggCount => {
+    return {
+      displayText: aggCount.name,
+      paramName: FilterParamName.medium,
+      paramValue: aggCount.value,
+      filterType,
+    }
+  })
 
   const selectedOptions = useSelectedOptionsDisplay()
-  const selectedOption = selectedOptions.find(option => option.filterType === filterType)?.value! as MediumOption
+  const selectedOption = selectedOptions.find(option => option.filterType === filterType)!
 
-  const selectOption = (option: MediumOption) => {
-    dispatch({ type: "selectFilters", payload: { value: option, filterType } })
+  const selectOption = (option: AggregateOption) => {
+    dispatch({
+      type: "selectFilters",
+      payload: {
+        displayText: option.displayText,
+        paramValue: option.paramValue,
+        paramName: FilterParamName.medium,
+        filterType,
+      },
+    })
   }
 
   return (
     <SingleSelectOptionScreen
       onSelect={selectOption}
       filterHeaderText="Medium"
-      filterOptions={OrderedMediumFilters}
+      filterOptions={options}
       selectedOption={selectedOption}
       navigator={navigator}
     />
