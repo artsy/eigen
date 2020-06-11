52d63b624198f0b57de0f827d8ee6357682b6e65
David Sheldrick <d.j.sheldrick@gmail.com>
2020-06-11 10:33:03 +0100

    perf optimisation

--- src/lib/utils/extractNodes.ts
+++ src/lib/utils/extractNodes.ts
@@ -2,5 +2,5 @@ export function extractNodes<Node extends object, T = Node>(
   connection: { readonly edges?: ReadonlyArray<{ readonly node?: Node | null } | null> | null } | undefined | null,
   mapper?: (node: Node) => T
 ): T[] {
-  return (connection?.edges?.map(edge => edge?.node!) ?? []).map(mapper ?? (x => x as any))
+  return connection?.edges?.map(edge => (mapper ? (mapper(edge?.node!) as any) : edge?.node!)) ?? []
 }
