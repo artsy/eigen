a909c458afa0779c12308569c4b5967185de21ed
Christopher Pappas <damassi.pappas@gmail.com>
2020-06-11 14:17:31 -0700

    Revert "Revert "[My Collection] Stub out skeleton nav flow ""

---
+++ emission/Pod/Classes/ViewControllers/ARMyCollectionHomeComponentViewController.m
@@ -0,0 +1,10 @@
+#import "ARMyCollectionHomeComponentViewController.h"
+
+@implementation ARMyCollectionHomeComponentViewController
+
+- (instancetype)init
+{
+    return [super initWithEmission:nil moduleName:@"MyCollectionHome" initialProperties:nil];
+}
+
+@end
--- src/lib/Scenes/Consignments/v2/State/navigationState.ts
+++ src/lib/Scenes/Consignments/v2/State/navigationState.ts
@@ -1,13 +1,40 @@
 import { Action, action } from "easy-peasy"
+import SwitchBoard from "lib/NativeModules/SwitchBoard"
+import { RefObject } from "react"
 
 export interface NavigationState {
-  hello: string
-  setHello: Action<NavigationState, string>
+  navRef: RefObject<any>
+  setupNavigation: Action<NavigationState, RefObject<any>>
+
+  // Nav actions
+  navigateToAddArtwork: Action<NavigationState>
+  navigateToArtworkDetail: Action<NavigationState>
+  navigateToArtworkList: Action<NavigationState>
+  navigateToHome: Action<NavigationState>
 }
 
 export const navigationState: NavigationState = {
-  hello: "world",
-  setHello: action((state, payload) => {
-    state.hello = payload
+  navRef: {
+    current: null,
+  },
+
+  setupNavigation: action((state, navRef) => {
+    state.navRef = navRef
+  }),
+
+  navigateToAddArtwork: action(state => {
+    SwitchBoard.presentNavigationViewController(state.navRef.current, "/my-collection/add-artwork")
+  }),
+
+  navigateToArtworkDetail: action(state => {
+    SwitchBoard.presentNavigationViewController(state.navRef.current, "/my-collection/artwork-detail")
+  }),
+
+  navigateToArtworkList: action(state => {
+    SwitchBoard.presentNavigationViewController(state.navRef.current, "/my-collection/artwork-list")
+  }),
+
+  navigateToHome: action(state => {
+    SwitchBoard.presentNavigationViewController(state.navRef.current, "/my-collection/home")
   }),
 }
