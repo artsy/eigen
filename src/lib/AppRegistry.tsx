fc0cf1838208d208d6658161e1f5bbdb33761e40
Mounir Dhahri <soussouheros@gmail.com>
2020-06-22 11:52:07 +0200

    added change phone number screen

--- src/lib/AppRegistry.tsx
+++ src/lib/AppRegistry.tsx
@@ -47,6 +47,7 @@ import { HomeQueryRenderer } from "./Scenes/Home/Home"
 import { MapContainer } from "./Scenes/Map"
 import { MyAccountQueryRenderer } from "./Scenes/MyAccount/MyAccount"
 import { MyAccountEditNameQueryRenderer } from "./Scenes/MyAccount/MyAccountEditName"
+import { MyAccountEditPhoneQueryRenderer } from "./Scenes/MyAccount/MyAccountEditPhone"
 import { NewSubmissionForm } from "./Scenes/MyCollection/NewSubmissionForm"
 import { PartnerQueryRenderer } from "./Scenes/Partner"
 import { PartnerLocationsQueryRenderer } from "./Scenes/Partner/Screens/PartnerLocations"
@@ -319,6 +320,7 @@ register("Map", MapContainer, { fullBleed: true })
 // My Account screens
 register("MyAccount", MyAccountQueryRenderer)
 register("MyAccountEditName", MyAccountEditNameQueryRenderer)
+register("MyAccountEditPhone", MyAccountEditPhoneQueryRenderer)
 
 // My Collection
 register("Sales", setupMyCollectionScreen(Consignments)) // Placeholder for sales tab!
