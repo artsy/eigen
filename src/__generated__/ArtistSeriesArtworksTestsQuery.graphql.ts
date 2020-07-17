5d1fa2fc7c59b0d6f803b851a37b9f5038cea875
Ashley Jelks <arjelks@gmail.com>
2020-07-17 16:33:11 -0400

    add/update tests

---
+++ src/__generated__/ArtistSeriesArtworksTestsQuery.graphql.ts
@@ -0,0 +1,498 @@
+/* tslint:disable */
+/* eslint-disable */
+/* @relayHash 4773c98e51eee428a953429902ba2e59 */
+
+import { ConcreteRequest } from "relay-runtime";
+import { FragmentRefs } from "relay-runtime";
+export type ArtistSeriesArtworksTestsQueryVariables = {};
+export type ArtistSeriesArtworksTestsQueryResponse = {
+    readonly artistSeries: {
+        readonly " $fragmentRefs": FragmentRefs<"ArtistSeriesArtworks_artistSeries">;
+    } | null;
+};
+export type ArtistSeriesArtworksTestsQueryRawResponse = {
+    readonly artistSeries: ({
+        readonly artistSeriesArtworks: ({
+            readonly edges: ReadonlyArray<({
+                readonly node: ({
+                    readonly id: string;
+                    readonly slug: string;
+                    readonly image: ({
+                        readonly aspectRatio: number;
+                        readonly url: string | null;
+                    }) | null;
+                    readonly title: string | null;
+                    readonly date: string | null;
+                    readonly saleMessage: string | null;
+                    readonly artistNames: string | null;
+                    readonly href: string | null;
+                    readonly sale: ({
+                        readonly isAuction: boolean | null;
+                        readonly isClosed: boolean | null;
+                        readonly displayTimelyAt: string | null;
+                        readonly id: string | null;
+                    }) | null;
+                    readonly saleArtwork: ({
+                        readonly currentBid: ({
+                            readonly display: string | null;
+                        }) | null;
+                        readonly id: string | null;
+                    }) | null;
+                    readonly partner: ({
+                        readonly name: string | null;
+                        readonly id: string | null;
+                    }) | null;
+                    readonly __typename: "Artwork";
+                }) | null;
+                readonly cursor: string;
+                readonly id: string | null;
+            }) | null> | null;
+            readonly counts: ({
+                readonly total: number | null;
+            }) | null;
+            readonly pageInfo: {
+                readonly hasNextPage: boolean;
+                readonly startCursor: string | null;
+                readonly endCursor: string | null;
+            };
+            readonly id: string | null;
+        }) | null;
+    }) | null;
+};
+export type ArtistSeriesArtworksTestsQuery = {
+    readonly response: ArtistSeriesArtworksTestsQueryResponse;
+    readonly variables: ArtistSeriesArtworksTestsQueryVariables;
+    readonly rawResponse: ArtistSeriesArtworksTestsQueryRawResponse;
+};
+
+
+
+/*
+query ArtistSeriesArtworksTestsQuery {
+  artistSeries(id: "pumpkins") {
+    ...ArtistSeriesArtworks_artistSeries
+  }
+}
+
+fragment ArtistSeriesArtworks_artistSeries on ArtistSeries {
+  artistSeriesArtworks: filterArtworksConnection(first: 20, sort: "-decayed_merch") {
+    edges {
+      node {
+        id
+        __typename
+      }
+      cursor
+    }
+    counts {
+      total
+    }
+    ...InfiniteScrollArtworksGrid_connection
+    pageInfo {
+      endCursor
+      hasNextPage
+    }
+    id
+  }
+}
+
+fragment ArtworkGridItem_artwork on Artwork {
+  title
+  date
+  saleMessage
+  slug
+  artistNames
+  href
+  sale {
+    isAuction
+    isClosed
+    displayTimelyAt
+    id
+  }
+  saleArtwork {
+    currentBid {
+      display
+    }
+    id
+  }
+  partner {
+    name
+    id
+  }
+  image {
+    url(version: "large")
+    aspectRatio
+  }
+}
+
+fragment InfiniteScrollArtworksGrid_connection on ArtworkConnectionInterface {
+  pageInfo {
+    hasNextPage
+    startCursor
+    endCursor
+  }
+  edges {
+    __typename
+    node {
+      slug
+      id
+      image {
+        aspectRatio
+      }
+      ...ArtworkGridItem_artwork
+    }
+    ... on Node {
+      id
+    }
+  }
+}
+*/
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "kind": "Literal",
+    "name": "id",
+    "value": "pumpkins"
+  }
+],
+v1 = [
+  {
+    "kind": "Literal",
+    "name": "first",
+    "value": 20
+  },
+  {
+    "kind": "Literal",
+    "name": "sort",
+    "value": "-decayed_merch"
+  }
+],
+v2 = {
+  "kind": "ScalarField",
+  "alias": null,
+  "name": "id",
+  "args": null,
+  "storageKey": null
+};
+return {
+  "kind": "Request",
+  "fragment": {
+    "kind": "Fragment",
+    "name": "ArtistSeriesArtworksTestsQuery",
+    "type": "Query",
+    "metadata": null,
+    "argumentDefinitions": [],
+    "selections": [
+      {
+        "kind": "LinkedField",
+        "alias": null,
+        "name": "artistSeries",
+        "storageKey": "artistSeries(id:\"pumpkins\")",
+        "args": (v0/*: any*/),
+        "concreteType": "ArtistSeries",
+        "plural": false,
+        "selections": [
+          {
+            "kind": "FragmentSpread",
+            "name": "ArtistSeriesArtworks_artistSeries",
+            "args": null
+          }
+        ]
+      }
+    ]
+  },
+  "operation": {
+    "kind": "Operation",
+    "name": "ArtistSeriesArtworksTestsQuery",
+    "argumentDefinitions": [],
+    "selections": [
+      {
+        "kind": "LinkedField",
+        "alias": null,
+        "name": "artistSeries",
+        "storageKey": "artistSeries(id:\"pumpkins\")",
+        "args": (v0/*: any*/),
+        "concreteType": "ArtistSeries",
+        "plural": false,
+        "selections": [
+          {
+            "kind": "LinkedField",
+            "alias": "artistSeriesArtworks",
+            "name": "filterArtworksConnection",
+            "storageKey": "filterArtworksConnection(first:20,sort:\"-decayed_merch\")",
+            "args": (v1/*: any*/),
+            "concreteType": "FilterArtworksConnection",
+            "plural": false,
+            "selections": [
+              {
+                "kind": "LinkedField",
+                "alias": null,
+                "name": "edges",
+                "storageKey": null,
+                "args": null,
+                "concreteType": "FilterArtworksEdge",
+                "plural": true,
+                "selections": [
+                  {
+                    "kind": "LinkedField",
+                    "alias": null,
+                    "name": "node",
+                    "storageKey": null,
+                    "args": null,
+                    "concreteType": "Artwork",
+                    "plural": false,
+                    "selections": [
+                      (v2/*: any*/),
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "slug",
+                        "args": null,
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "LinkedField",
+                        "alias": null,
+                        "name": "image",
+                        "storageKey": null,
+                        "args": null,
+                        "concreteType": "Image",
+                        "plural": false,
+                        "selections": [
+                          {
+                            "kind": "ScalarField",
+                            "alias": null,
+                            "name": "aspectRatio",
+                            "args": null,
+                            "storageKey": null
+                          },
+                          {
+                            "kind": "ScalarField",
+                            "alias": null,
+                            "name": "url",
+                            "args": [
+                              {
+                                "kind": "Literal",
+                                "name": "version",
+                                "value": "large"
+                              }
+                            ],
+                            "storageKey": "url(version:\"large\")"
+                          }
+                        ]
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "title",
+                        "args": null,
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "date",
+                        "args": null,
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "saleMessage",
+                        "args": null,
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "artistNames",
+                        "args": null,
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "href",
+                        "args": null,
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "LinkedField",
+                        "alias": null,
+                        "name": "sale",
+                        "storageKey": null,
+                        "args": null,
+                        "concreteType": "Sale",
+                        "plural": false,
+                        "selections": [
+                          {
+                            "kind": "ScalarField",
+                            "alias": null,
+                            "name": "isAuction",
+                            "args": null,
+                            "storageKey": null
+                          },
+                          {
+                            "kind": "ScalarField",
+                            "alias": null,
+                            "name": "isClosed",
+                            "args": null,
+                            "storageKey": null
+                          },
+                          {
+                            "kind": "ScalarField",
+                            "alias": null,
+                            "name": "displayTimelyAt",
+                            "args": null,
+                            "storageKey": null
+                          },
+                          (v2/*: any*/)
+                        ]
+                      },
+                      {
+                        "kind": "LinkedField",
+                        "alias": null,
+                        "name": "saleArtwork",
+                        "storageKey": null,
+                        "args": null,
+                        "concreteType": "SaleArtwork",
+                        "plural": false,
+                        "selections": [
+                          {
+                            "kind": "LinkedField",
+                            "alias": null,
+                            "name": "currentBid",
+                            "storageKey": null,
+                            "args": null,
+                            "concreteType": "SaleArtworkCurrentBid",
+                            "plural": false,
+                            "selections": [
+                              {
+                                "kind": "ScalarField",
+                                "alias": null,
+                                "name": "display",
+                                "args": null,
+                                "storageKey": null
+                              }
+                            ]
+                          },
+                          (v2/*: any*/)
+                        ]
+                      },
+                      {
+                        "kind": "LinkedField",
+                        "alias": null,
+                        "name": "partner",
+                        "storageKey": null,
+                        "args": null,
+                        "concreteType": "Partner",
+                        "plural": false,
+                        "selections": [
+                          {
+                            "kind": "ScalarField",
+                            "alias": null,
+                            "name": "name",
+                            "args": null,
+                            "storageKey": null
+                          },
+                          (v2/*: any*/)
+                        ]
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "__typename",
+                        "args": null,
+                        "storageKey": null
+                      }
+                    ]
+                  },
+                  {
+                    "kind": "ScalarField",
+                    "alias": null,
+                    "name": "cursor",
+                    "args": null,
+                    "storageKey": null
+                  },
+                  (v2/*: any*/)
+                ]
+              },
+              {
+                "kind": "LinkedField",
+                "alias": null,
+                "name": "counts",
+                "storageKey": null,
+                "args": null,
+                "concreteType": "FilterArtworksCounts",
+                "plural": false,
+                "selections": [
+                  {
+                    "kind": "ScalarField",
+                    "alias": null,
+                    "name": "total",
+                    "args": null,
+                    "storageKey": null
+                  }
+                ]
+              },
+              {
+                "kind": "LinkedField",
+                "alias": null,
+                "name": "pageInfo",
+                "storageKey": null,
+                "args": null,
+                "concreteType": "PageInfo",
+                "plural": false,
+                "selections": [
+                  {
+                    "kind": "ScalarField",
+                    "alias": null,
+                    "name": "hasNextPage",
+                    "args": null,
+                    "storageKey": null
+                  },
+                  {
+                    "kind": "ScalarField",
+                    "alias": null,
+                    "name": "startCursor",
+                    "args": null,
+                    "storageKey": null
+                  },
+                  {
+                    "kind": "ScalarField",
+                    "alias": null,
+                    "name": "endCursor",
+                    "args": null,
+                    "storageKey": null
+                  }
+                ]
+              },
+              (v2/*: any*/)
+            ]
+          },
+          {
+            "kind": "LinkedHandle",
+            "alias": "artistSeriesArtworks",
+            "name": "filterArtworksConnection",
+            "args": (v1/*: any*/),
+            "handle": "connection",
+            "key": "ArtistSeries_artistSeriesArtworks",
+            "filters": [
+              "sort"
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  "params": {
+    "operationKind": "query",
+    "name": "ArtistSeriesArtworksTestsQuery",
+    "id": "f9984475749ea2dcfe170c7876d331fc",
+    "text": null,
+    "metadata": {}
+  }
+};
+})();
+(node as any).hash = 'dd616f7d9adf8f2d10c2990c09ac203d';
+export default node;
---
+++ src/lib/Scenes/ArtistSeries/__tests__/ArtistSeriesArtworks-tests.tsx
@@ -0,0 +1,185 @@
+import { Theme } from "@artsy/palette"
+import {
+  ArtistSeriesArtworksTestsQuery,
+  ArtistSeriesArtworksTestsQueryRawResponse,
+} from "__generated__/ArtistSeriesArtworksTestsQuery.graphql"
+import { InfiniteScrollArtworksGridContainer } from "lib/Components/ArtworkGrids/InfiniteScrollArtworksGrid"
+import { ArtistSeriesArtworksFragmentContainer } from "lib/Scenes/ArtistSeries/ArtistSeriesArtworks"
+import React from "react"
+import { graphql, QueryRenderer } from "react-relay"
+import ReactTestRenderer, { act } from "react-test-renderer"
+import { createMockEnvironment } from "relay-test-utils"
+
+jest.unmock("react-relay")
+
+describe("Artist Series Artworks", () => {
+  let env: ReturnType<typeof createMockEnvironment>
+
+  beforeEach(() => {
+    env = createMockEnvironment()
+  })
+
+  const TestRenderer = () => (
+    <QueryRenderer<ArtistSeriesArtworksTestsQuery>
+      environment={env}
+      query={graphql`
+        query ArtistSeriesArtworksTestsQuery @raw_response_type {
+          artistSeries(id: "pumpkins") {
+            ...ArtistSeriesArtworks_artistSeries
+          }
+        }
+      `}
+      variables={{ artistSeriesID: "pumpkins" }}
+      render={({ props, error }) => {
+        if (props?.artistSeries) {
+          return (
+            <Theme>
+              <ArtistSeriesArtworksFragmentContainer artistSeries={props.artistSeries} />
+            </Theme>
+          )
+        } else if (error) {
+          console.log(error)
+        }
+      }}
+    />
+  )
+
+  it("renders an artwork grid if artworks", () => {
+    const wrapper = () => {
+      const tree = ReactTestRenderer.create(<TestRenderer />)
+      act(() => {
+        env.mock.resolveMostRecentOperation({
+          errors: [],
+          data: {
+            ...ArtistSeriesArtworksFixture,
+          },
+        })
+      })
+      return tree
+    }
+
+    expect(wrapper().root.findAllByType(InfiniteScrollArtworksGridContainer)).toHaveLength(1)
+  })
+
+  it("renders a null component if no artworks", () => {
+    const wrapper = () => {
+      const tree = ReactTestRenderer.create(<TestRenderer />)
+      act(() => {
+        env.mock.resolveMostRecentOperation({
+          errors: [],
+          data: {
+            ...ArtistSeriesZeroArtworksFixture,
+          },
+        })
+      })
+      return tree
+    }
+
+    expect(wrapper().root.findAllByType(InfiniteScrollArtworksGridContainer)).toHaveLength(0)
+  })
+})
+
+const ArtistSeriesArtworksFixture: ArtistSeriesArtworksTestsQueryRawResponse = {
+  artistSeries: {
+    artistSeriesArtworks: {
+      pageInfo: {
+        hasNextPage: false,
+        startCursor: "ajdjabnz81",
+        endCursor: "aknqa9d81",
+      },
+      id: null,
+      counts: { total: 4 },
+      edges: [
+        {
+          node: {
+            id: "12345654321",
+            slug: "pumpkins-1",
+            image: null,
+            title: "Pumpkins 1.0",
+            date: null,
+            saleMessage: null,
+            artistNames: null,
+            href: null,
+            sale: null,
+            saleArtwork: null,
+            partner: null,
+            __typename: "Artwork",
+          },
+          cursor: "123456789",
+          id: "#8989141",
+        },
+        {
+          node: {
+            id: "9874491018",
+            slug: "pumpkins-2",
+            image: null,
+            title: "Pumpkins 2.0",
+            date: null,
+            saleMessage: null,
+            artistNames: null,
+            href: null,
+            sale: null,
+            saleArtwork: null,
+            partner: null,
+            __typename: "Artwork",
+          },
+          cursor: "98761511",
+          id: "faf91kjg8",
+        },
+        {
+          node: {
+            id: "128163456",
+            slug: "pumpkins-3",
+            image: null,
+            title: "Pumpkins 3.0",
+            date: null,
+            saleMessage: null,
+            artistNames: null,
+            href: null,
+            sale: null,
+            saleArtwork: null,
+            partner: null,
+            __typename: "Artwork",
+          },
+          cursor: "19017613",
+          id: "fja91k30v",
+        },
+        {
+          node: {
+            id: "123310456",
+            slug: "pumpkins-4",
+            image: null,
+            title: "Pumpkins 4.0",
+            date: null,
+            saleMessage: null,
+            artistNames: null,
+            href: null,
+            sale: null,
+            saleArtwork: null,
+            partner: null,
+            __typename: "Artwork",
+          },
+          cursor: "01827111",
+          id: "afd7a91m1",
+        },
+      ],
+    },
+  },
+}
+
+const ArtistSeriesZeroArtworksFixture: ArtistSeriesArtworksTestsQueryRawResponse = {
+  artistSeries: {
+    artistSeriesArtworks: {
+      pageInfo: {
+        hasNextPage: false,
+        startCursor: "ajdjabnz81",
+        endCursor: "aknqa9d81",
+      },
+      id: null,
+      counts: {
+        total: 0,
+      },
+      edges: [],
+    },
+  },
+}
