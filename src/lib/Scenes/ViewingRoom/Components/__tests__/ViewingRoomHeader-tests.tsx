bb654ae3d867edee16338cd79da13a937f9875cb
Pavlos Vinieratos <pvinis@gmail.com>
2020-07-27 22:27:24 +0200

    fix test

--- src/lib/Scenes/ViewingRoom/Components/__tests__/ViewingRoomHeader-tests.tsx
+++ src/lib/Scenes/ViewingRoom/Components/__tests__/ViewingRoomHeader-tests.tsx
@@ -36,7 +36,7 @@ describe("ViewingRoomHeader", () => {
     const tree = ReactTestRenderer.create(<TestRenderer />)
     mockEnvironment.mock.resolveMostRecentOperation(operation => {
       const result = MockPayloadGenerator.generate(operation, {
-        ViewingRoom: () => ({ image: { imageURLs: { normalized: "Foo" } } }),
+        ViewingRoom: () => ({ image: { imageURLs: { heroImage: "Foo" } } }),
       })
       return result
     })
