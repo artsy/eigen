44e16c4986f22b9be6e264a2bf4f0e2a69c59096
brainbicycle <brian.beckerle@artsymail.com>
2020-07-30 13:49:24 -0400

    Access permissions in callback

--- emission/Pod/Classes/Core/ARPermissions.m
+++ emission/Pod/Classes/Core/ARPermissions.m
@@ -6,14 +6,26 @@
 //
 
 #import "ARPermissions.h"
+#import <UserNotifications/UserNotifications.h>
 
 @implementation ARPermissions
 
 RCT_EXPORT_MODULE();
 
+// TODO: Believe this should be an observable property in the same way selectedTab was
+// Otherwise when you change notification permissions while on the profile push screen
+// the setting get stale under trigger pull to refresh
 RCT_EXPORT_METHOD(fetchNotificationPermissions:(RCTResponseSenderBlock)callback)
 {
-  callback(@[[NSNull null], @"hereIsWhereIShouldPassPermissions"]);
+    UNUserNotificationCenter *notifCenter = [UNUserNotificationCenter currentNotificationCenter];
+    [notifCenter getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+        UNAuthorizationStatus *status = [settings authorizationStatus];
+        if (status == UNAuthorizationStatusAuthorized) {
+            callback(@[[NSNull null], @YES]);
+        } else {
+            callback(@[[NSNull null], @NO]);
+        }
+    }];
 }
 
 @end
