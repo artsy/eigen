36b49e68c543d0d9cbc30182555b05872ff274ec
Pavlos Vinieratos <pvinis@gmail.com>
2020-07-31 16:36:04 +0200

    adding flex

--- src/palette/elements/Touchable.tsx
+++ src/palette/elements/Touchable.tsx
@@ -1,9 +1,9 @@
-import { color } from "@artsy/palette"
+import { color, Flex } from "@artsy/palette"
 import React from "react"
-import { TouchableHighlight, TouchableHighlightProps, View } from "react-native"
+import { TouchableHighlight, TouchableHighlightProps } from "react-native"
 
-export const Touchable: React.FC<TouchableHighlightProps> = ({ children, ...props }) => (
+export const Touchable: React.FC<TouchableHighlightProps & { flex?: number }> = ({ children, flex, ...props }) => (
   <TouchableHighlight underlayColor={color("white100")} activeOpacity={0.8} {...props}>
-    <View>{children}</View>
+    <Flex flex={flex}>{children}</Flex>
   </TouchableHighlight>
 )
