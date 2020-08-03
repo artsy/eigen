cfe947ce7486badaeb49de955cb31dc6b52e2b36
Yuki Nishijima <yk.nishijima@gmail.com>
2020-08-03 13:48:08 -0400

    Wire up a GraphQL query with <MyBids>

--- src/lib/Scenes/MyBids/helpers/saleTime.tsx
+++ src/lib/Scenes/MyBids/helpers/saleTime.tsx
@@ -1,7 +1,11 @@
 import moment from "moment-timezone"
 
-export const saleTime = (sale: { liveStartAt?: string; endAt?: string }) => {
-  const datetime = (sale.liveStartAt || sale.endAt) as string
+import { MyBids_sales } from "__generated__/MyBids_sales.graphql"
+
+type Sale = NonNullable<NonNullable<MyBids_sales["edges"]>[number]>["node"]
+
+export const saleTime = (sale?: Sale) => {
+  const datetime = (sale?.liveStartAt || sale?.endAt) as string
   const dateInMoment = moment(datetime, moment.ISO_8601).tz(moment.tz.guess(true))
   const now = moment()
 
@@ -14,7 +18,7 @@ export const saleTime = (sale: { liveStartAt?: string; endAt?: string }) => {
     formattedDate = `at ${dateInMoment.format("h:mma")} on ${dateInMoment.format("M/D")}`
   }
 
-  if (sale.liveStartAt) {
+  if (sale?.liveStartAt) {
     return `Live sale opens ${formattedDate}`
   } else {
     return `Closes ${formattedDate}`
