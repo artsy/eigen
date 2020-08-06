0fca4dcb97888444f9f0cada86eb3e2698466217
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-08-06 17:44:46 -0400

    fix: Make ReadMore char limit consistent with other usage in other components

--- src/lib/Scenes/ArtistSeries/ArtistSeriesMeta.tsx
+++ src/lib/Scenes/ArtistSeries/ArtistSeriesMeta.tsx
@@ -3,7 +3,7 @@ import { ArtistSeriesMeta_artistSeries } from "__generated__/ArtistSeriesMeta_ar
 import { ArtistSeriesMetaFollowMutation } from "__generated__/ArtistSeriesMetaFollowMutation.graphql"
 import { ReadMore } from "lib/Components/ReadMore"
 import SwitchBoard from "lib/NativeModules/SwitchBoard"
-import { useScreenDimensions } from "lib/utils/useScreenDimensions"
+import { truncatedTextLimit } from "lib/utils/hardware"
 import React, { useRef } from "react"
 import { TouchableOpacity, TouchableWithoutFeedback, View } from "react-native"
 import { commitMutation, createFragmentContainer, graphql, RelayProp } from "react-relay"
@@ -17,10 +17,7 @@ type ArtistToFollowOrUnfollow = NonNullable<NonNullable<ArtistSeriesMetaProps["a
 
 export const ArtistSeriesMeta: React.SFC<ArtistSeriesMetaProps> = ({ artistSeries, relay }) => {
   const metaRef = useRef<View | null>(null)
-
-  const { width } = useScreenDimensions()
-  const isIPad = width > 700
-  const maxChars = isIPad ? 200 : 120
+  const maxChars = truncatedTextLimit()
   const artist = artistSeries?.artists?.[0]
 
   const followOrUnfollowArtist = (followArtist: ArtistToFollowOrUnfollow) => {
