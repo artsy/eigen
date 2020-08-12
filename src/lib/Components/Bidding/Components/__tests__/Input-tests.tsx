39072f7bbc63181737a30623207bc812182ed9e9
brainbicycle <brian.beckerle@artsymail.com>
2020-08-12 10:19:35 -0400

    Update remaining test with rule disabled

--- src/lib/Components/Bidding/Components/__tests__/Input-tests.tsx
+++ src/lib/Components/Bidding/Components/__tests__/Input-tests.tsx
@@ -1,26 +1,24 @@
 import React from "react"
 import { TextInput } from "react-native"
-import ReactTestRenderer from "react-test-renderer"
 
+import { renderWithWrappers } from "lib/tests/renderWithWrappers"
 import { theme } from "../../Elements/Theme"
 import { BiddingThemeProvider } from "../BiddingThemeProvider"
 import { Input } from "../Input"
 
-/* tslint:disable use-wrapped-components */
-
 it("shows a gray border by default", () => {
-  const component = ReactTestRenderer.create(
+  const component = renderWithWrappers(
     <BiddingThemeProvider>
       <Input />
     </BiddingThemeProvider>
   )
 
-  // @ts-ignore STRICTNESS_MIGRATION
-  expect(component.toJSON().props.style[0].borderColor).toEqual(theme.colors.black10)
+  expect(component.toJSON()).toBeTruthy()
+  expect(component.toJSON()?.props.style[0].borderColor).toEqual(theme.colors.black10)
 })
 
 it("shows a purple border on focus", () => {
-  const component = ReactTestRenderer.create(
+  const component = renderWithWrappers(
     <BiddingThemeProvider>
       <Input />
     </BiddingThemeProvider>
@@ -30,12 +28,12 @@ it("shows a purple border on focus", () => {
 
   inputComponent.onFocus()
 
-  // @ts-ignore STRICTNESS_MIGRATION
-  expect(component.toJSON().props.style[0].borderColor).toEqual(theme.colors.purple100)
+  expect(component.toJSON()).toBeTruthy()
+  expect(component.toJSON()?.props.style[0].borderColor).toEqual(theme.colors.purple100)
 })
 
 it("changes the border color back to gray on blur", () => {
-  const component = ReactTestRenderer.create(
+  const component = renderWithWrappers(
     <BiddingThemeProvider>
       <Input />
     </BiddingThemeProvider>
@@ -46,19 +44,19 @@ it("changes the border color back to gray on blur", () => {
   inputComponent.onFocus()
   inputComponent.onBlur()
 
-  // @ts-ignore STRICTNESS_MIGRATION
-  expect(component.toJSON().props.style[0].borderColor).toEqual(theme.colors.black10)
+  expect(component.toJSON()).toBeTruthy()
+  expect(component.toJSON()?.props.style[0].borderColor).toEqual(theme.colors.black10)
 })
 
 it("shows a red border if error is true", () => {
-  const component = ReactTestRenderer.create(
+  const component = renderWithWrappers(
     <BiddingThemeProvider>
       <Input error />
     </BiddingThemeProvider>
   )
 
-  // @ts-ignore STRICTNESS_MIGRATION
-  expect(component.toJSON().props.style[0].borderColor).toEqual(theme.colors.red100)
+  expect(component.toJSON()).toBeTruthy()
+  expect(component.toJSON()?.props.style[0].borderColor).toEqual(theme.colors.red100)
 })
 
 it("updates the border color when the parent component updates the error prop", () => {
@@ -74,28 +72,29 @@ it("updates the border color when the parent component updates the error prop", 
     }
   }
 
-  const component = ReactTestRenderer.create(<TestFormForInput />)
+  const component = renderWithWrappers(<TestFormForInput />)
 
-  // @ts-ignore STRICTNESS_MIGRATION
-  expect(component.toJSON().props.style[0].borderColor).toEqual(theme.colors.black10)
+  expect(component.toJSON()).toBeTruthy()
+  expect(component.toJSON()?.props.style[0].borderColor).toEqual(theme.colors.black10)
 
   // Explicitly calling setState to force-render the Input component
-  component.root.instance.setState({ error: true })
+  const parentComponent = component.root.findByType(TestFormForInput).instance
+  parentComponent.setState({ error: true })
 
-  // @ts-ignore STRICTNESS_MIGRATION
-  expect(component.toJSON().props.style[0].borderColor).toEqual(theme.colors.red100)
+  expect(component.toJSON()).toBeTruthy()
+  expect(component.toJSON()?.props.style[0].borderColor).toEqual(theme.colors.red100)
 
-  component.root.instance.setState({ error: false })
+  parentComponent.setState({ error: false })
 
-  // @ts-ignore STRICTNESS_MIGRATION
-  expect(component.toJSON().props.style[0].borderColor).toEqual(theme.colors.black10)
+  expect(component.toJSON()).toBeTruthy()
+  expect(component.toJSON()?.props.style[0].borderColor).toEqual(theme.colors.black10)
 })
 
 it("allows for capturing the ref to the actual text input", () => {
   // FXIME: This is a StyledNativeComponent instance. Find the appropriate type and replace any with it.
   let inputRef: any
 
-  const component = ReactTestRenderer.create(
+  const component = renderWithWrappers(
     <BiddingThemeProvider>
       <Input inputRef={element => (inputRef = element)} />
     </BiddingThemeProvider>
