b9e1dc7debe3aee0741b92d4020d567a8d471604
Pavlos Vinieratos <pvinis@gmail.com>
2020-08-20 20:05:51 +0200

    better props

--- src/lib/Components/LazyScrollView.tsx
+++ src/lib/Components/LazyScrollView.tsx
@@ -1,12 +1,17 @@
 import React from "react"
+import { ScrollViewProps } from "react-native"
 import { AboveTheFoldFlatList } from "./AboveTheFoldFlatList"
 
-export const LazyScrollView: React.FC<{ initialNumToRender: number }> = props => {
+export const LazyScrollView: React.FC<{
+  children: Array<React.ReactElement | null> // only allow react elements as children (not string for example like `Text`)
+  initialNumToRender?: number
+} & ScrollViewProps> = ({ initialNumToRender = 1, ...props }) => {
   const sections = React.Children.toArray(props.children) as React.ReactElement[]
 
   return (
     <AboveTheFoldFlatList
       {...props}
+      initialNumToRender={initialNumToRender}
       data={sections}
       renderItem={({ item }) => {
         return item
