4ea0ffed6bc1846d2cd76882fc04b12185315984
brainbicycle <brian.beckerle@artsymail.com>
2020-08-20 10:09:28 -0400

    Starting vanity url changes, broken

---
+++ src/lib/Scenes/VanityURL/VanityURLEntity.tsx
@@ -0,0 +1,52 @@
+import { VanityURLEntityQuery } from "__generated__/VanityURLEntityQuery.graphql"
+import { HeaderTabsGridPlaceholder } from "lib/Components/HeaderTabGridPlaceholder"
+import { defaultEnvironment } from "lib/relay/createEnvironment"
+import { renderWithPlaceholder } from "lib/utils/renderWithPlaceholder"
+import React from "react"
+import { createFragmentContainer, graphql, QueryRenderer } from "react-relay"
+import { Fair } from "../Fair/Fair"
+
+const VanityURLEntity: React.FC<{ fairOrPartner: VanityURLEntity_fairOrPartner }> = ({ fairOrPartner }) => {
+  if (fairOrPartner.__typename === "Fair") {
+    return <Fair fair={fairOrPartner} />
+  } else if (fairOrPartner.__typename === "Partner") {
+    // might need to add `safeAreaInsets.top` top padding here since `Fair` is full-bleed and `Partner` isn't.
+    return null // <Partner partner={fairOrPartner} />
+  }
+  throw new Error(`404`)
+}
+
+const VanityURLEntityContainer = createFragmentContainer(VanityURLEntity, {
+  fairOrPartner: graphql`
+    fragment VanityURLEntity_fairOrPartner on VanityURLEntity {
+      ... on Fair {
+        __typename
+        ...Fair_fair
+      }
+      ... on Partner {
+        __typename
+        ...Partner_partner
+      }
+    }
+  `,
+})
+
+export const VanityURLEntityQueryRenderer: React.SFC<{ entity: "fair" | "partner"; id: string }> = ({ entity, id }) => {
+  return (
+    <QueryRenderer<VanityURLEntityQuery>
+      environment={defaultEnvironment}
+      query={graphql`
+        query VanityURLEntityQuery($id: String!) {
+          vanityURLEntity(id: $id) {
+            __typename
+          }
+        }
+      `}
+      variables={{ id }}
+      render={renderWithPlaceholder({
+        Container: VanityURLEntityContainer,
+        renderPlaceholder: () => (entity === "fair" ? <></> : <HeaderTabsGridPlaceholder />),
+      })}
+    />
+  )
+}
