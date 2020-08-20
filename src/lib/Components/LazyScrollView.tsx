d4bcc1f395a5867f0738066c9ba2c6b19709d81a
Pavlos Vinieratos <pvinis@gmail.com>
2020-08-20 23:34:21 +0200

    testing it using VR as an example

--- src/lib/Components/LazyScrollView.tsx
+++ src/lib/Components/LazyScrollView.tsx
@@ -1,21 +1,51 @@
-import React from "react"
-import { ScrollViewProps } from "react-native"
-import { AboveTheFoldFlatList } from "./AboveTheFoldFlatList"
+import React, { useRef, useState } from "react"
+import { FlatList, FlatListProps, ScrollViewProps, ViewabilityConfig, ViewToken } from "react-native"
 
 export const LazyScrollView: React.FC<{
-  children: Array<React.ReactElement | null> // only allow react elements as children (not string for example like `Text`)
+  children: Array<React.ReactElement | null | React.ReactElement[]> // only allow react elements as children (not string for example like `Text`)
   initialNumToRender?: number
-} & ScrollViewProps> = ({ initialNumToRender = 1, ...props }) => {
-  const sections = React.Children.toArray(props.children) as React.ReactElement[]
+  onVisiblePointsChanged?: ({ visiblePoints, changed }: { visiblePoints: string[]; changed: string[] }) => void
+
+  // FlatList specific stuff
+  windowSize?: FlatListProps<any>["windowSize"]
+  viewabilityConfig?: { itemVisiblePercentThreshold: number }
+} & ScrollViewProps> = ({
+  initialNumToRender = 1,
+  onVisiblePointsChanged,
+  windowSize,
+  viewabilityConfig: propViewabilityConfig,
+  ...restProps
+}) => {
+  const [userHasScrolled, setUserHasScrolled] = useState(false)
+
+  const sections = React.Children.toArray(restProps.children) as React.ReactElement[]
+
+  const viewabilityConfig = useRef<ViewabilityConfig>(propViewabilityConfig ?? { itemVisiblePercentThreshold: 50 })
+  const handleVisiblePoints = useRef(
+    ({ viewableItems, changed }: { viewableItems: ViewToken[]; changed: ViewToken[] }) => {
+      // SOMETHING FUNKY IS GOING ON HERE!!
+      // I should be able to use the func just with `onVisiblePointsChanged?({visible: [], changed: []})`
+      if (!onVisiblePointsChanged) {
+        return
+      }
+
+      onVisiblePointsChanged({
+        visiblePoints: viewableItems.map(token => token.key),
+        changed: changed.map(token => token.key),
+      })
+    }
+  )
 
   return (
-    <AboveTheFoldFlatList
-      {...props}
+    <FlatList
+      {...restProps}
       initialNumToRender={initialNumToRender}
       data={sections}
-      renderItem={({ item }) => {
-        return item
-      }}
+      renderItem={({ item }) => item}
+      onViewableItemsChanged={handleVisiblePoints.current}
+      viewabilityConfig={viewabilityConfig.current}
+      onScrollBeginDrag={() => setUserHasScrolled(true)}
+      windowSize={userHasScrolled ? windowSize || 5 : 1}
     />
   )
 }
--- src/lib/Scenes/ViewingRoom/ViewingRoom.tsx
+++ src/lib/Scenes/ViewingRoom/ViewingRoom.tsx
@@ -1,6 +1,7 @@
 import { Button, Flex, Sans, Spacer, Text, Theme } from "@artsy/palette"
 import { ViewingRoom_viewingRoom } from "__generated__/ViewingRoom_viewingRoom.graphql"
 import { ViewingRoomQuery } from "__generated__/ViewingRoomQuery.graphql"
+import { LazyScrollView } from "lib/Components/LazyScrollView"
 import LoadFailureView from "lib/Components/LoadFailureView"
 import SwitchBoard from "lib/NativeModules/SwitchBoard"
 import { defaultEnvironment } from "lib/relay/createEnvironment"
@@ -9,7 +10,7 @@ import { ProvideScreenTracking, Schema } from "lib/utils/track"
 import { once } from "lodash"
 import { maxWidth } from "palette"
 import React, { useCallback, useRef, useState } from "react"
-import { FlatList, LayoutAnimation, View, ViewToken } from "react-native"
+import { LayoutAnimation, View } from "react-native"
 import { createFragmentContainer, graphql, QueryRenderer } from "react-relay"
 import { useTracking } from "react-tracking"
 import { ViewingRoomArtworkRailContainer } from "./Components/ViewingRoomArtworkRail"
@@ -19,11 +20,6 @@ import { ViewingRoomViewWorksButtonContainer } from "./Components/ViewingRoomVie
 
 interface ViewingRoomProps {
   viewingRoom: ViewingRoom_viewingRoom
-}
-
-interface ViewingRoomSection {
-  key: string
-  content: JSX.Element
 }
 
 // Same as Gravity model viewing_room_status
@@ -76,81 +72,68 @@ export const ViewingRoom: React.FC<ViewingRoomProps> = props => {
     return <LoadFailureView style={{ flex: 1 }} />
   }
 
-  const sections: ViewingRoomSection[] = []
-
-  if (viewingRoom.status === ViewingRoomStatus.CLOSED || viewingRoom.status === ViewingRoomStatus.SCHEDULED) {
-    sections.push({
-      key: "closedNotice",
-      content: <ClosedNotice status={viewingRoom.status} navRef={navRef} partnerHref={viewingRoom.partner!.href!} />,
-    })
-  } else if (viewingRoom.status === ViewingRoomStatus.LIVE) {
-    sections.push(
-      {
-        key: "introStatement",
-        content: (
-          <Flex mt="2" mx="2">
-            <Text data-test-id="intro-statement" mt="2" variant="text" mx="2" style={maxWidth}>
-              {viewingRoom.introStatement}
-            </Text>
-          </Flex>
-        ),
-      },
-      {
-        key: "artworkRail",
-        content: <ViewingRoomArtworkRailContainer viewingRoom={viewingRoom} />,
-      },
-      {
-        key: "pullQuote",
-        content: (
-          <>
-            {!!viewingRoom.pullQuote && (
-              <Flex mx="2">
-                <Text data-test-id="pull-quote" variant="largeTitle" textAlign="center">
-                  {viewingRoom.pullQuote}
-                </Text>
-              </Flex>
-            )}
-          </>
-        ),
-      },
-      {
-        key: "body",
-        content: (
-          <Flex mx="2">
-            <Text data-test-id="body" variant="text" style={maxWidth}>
-              {viewingRoom.body}
-            </Text>
-          </Flex>
-        ),
-      },
-      {
-        key: "subsections",
-        content: <ViewingRoomSubsectionsContainer viewingRoom={viewingRoom} />,
-      }
-    )
-  }
-
   return (
     <ProvideScreenTracking info={tracks.context(viewingRoom.internalID, viewingRoom.slug)}>
       <Theme>
         <View style={{ flex: 1 }} ref={navRef}>
-          <FlatList<ViewingRoomSection>
-            onViewableItemsChanged={useCallback(({ viewableItems }) => {
-              if (viewableItems.find((viewableItem: ViewToken) => viewableItem.item.key === "body")) {
+          <LazyScrollView
+            contentContainerStyle={{ paddingBottom: 80 }}
+            initialNumToRender={1}
+            onVisiblePointsChanged={({ changed }) => {
+              if (changed.find(key => key.includes("body"))) {
                 trackBodyImpression()
                 LayoutAnimation.configureNext({ ...LayoutAnimation.Presets.easeInEaseOut, duration: 150 })
                 setDisplayViewWorksButton(true)
               }
-            }, [])}
-            contentContainerStyle={{ paddingBottom: 80 }}
-            viewabilityConfig={{ itemVisiblePercentThreshold: 15 }}
-            data={sections}
-            ListHeaderComponent={<ViewingRoomHeaderContainer viewingRoom={viewingRoom} />}
-            ItemSeparatorComponent={() => <Spacer mb={3} />}
-            renderItem={({ item }) => {
-              return item.content
             }}
-          />
+            viewabilityConfig={{ itemVisiblePercentThreshold: 15 }}
+          >
+            <ViewingRoomHeaderContainer viewingRoom={viewingRoom} />
+
+            {viewingRoom.status === ViewingRoomStatus.CLOSED || viewingRoom.status === ViewingRoomStatus.SCHEDULED ? (
+              <ClosedNotice status={viewingRoom.status} navRef={navRef} partnerHref={viewingRoom.partner!.href!} />
+            ) : null}
+
+            {viewingRoom.status === ViewingRoomStatus.LIVE
+              ? [
+                  <Flex mt="2" mx="2">
+                    <Text data-test-id="intro-statement" mt="2" variant="text" mx="2" style={maxWidth}>
+                      {viewingRoom.introStatement}
+                    </Text>
+                  </Flex>,
+
+                  <>
+                    <Spacer mb={3} />
+                    <ViewingRoomArtworkRailContainer viewingRoom={viewingRoom} />
+                  </>,
+
+                  <>
+                    <Spacer mb={3} />
+                    {!!viewingRoom.pullQuote && (
+                      <Flex mx="2">
+                        <Text data-test-id="pull-quote" variant="largeTitle" textAlign="center">
+                          {viewingRoom.pullQuote}
+                        </Text>
+                      </Flex>
+                    )}
+                  </>,
+
+                  <React.Fragment key="body">
+                    <Spacer mb={3} />
+                    <Flex mx="2">
+                      <Text data-test-id="body" variant="text" style={maxWidth}>
+                        {viewingRoom.body}
+                      </Text>
+                    </Flex>
+                  </React.Fragment>,
+
+                  <>
+                    <Spacer mb={3} />
+                    <ViewingRoomSubsectionsContainer viewingRoom={viewingRoom} />
+                  </>,
+                ]
+              : null}
+          </LazyScrollView>
           <ViewingRoomViewWorksButtonContainer isVisible={displayViewWorksButton} {...props} />
         </View>
       </Theme>
