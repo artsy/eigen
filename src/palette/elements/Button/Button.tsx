354415a713cc87fafdc9980711a416e6e6e28e48
Pavlos Vinieratos <pvinis@gmail.com>
2020-08-24 18:51:37 +0200

    button

--- src/palette/elements/Button/Button.tsx
+++ src/palette/elements/Button/Button.tsx
@@ -1,291 +1,336 @@
-import React, { Component } from "react"
-import styled, { css } from "styled-components"
+import React, { Component, ReactNode } from "react"
+import { TouchableWithoutFeedback } from "react-native"
+import { animated, Spring } from "react-spring/renderprops-native.cjs"
+import { css } from "styled-components"
+import styled from "styled-components/native"
 import { themeProps } from "../../Theme"
+import { Box, BoxProps } from "../Box"
+import { Flex } from "../Flex"
 import { Spinner } from "../Spinner"
-import { Sans, SansProps } from "../Typography"
+import { Sans } from "../Typography"
 
-import {
-  borderRadius,
-  borders,
-  height,
-  space,
-  textAlign,
-  width,
-} from "styled-system"
-import {
-  ButtonBaseProps,
-  ButtonProps,
-  defaultSize,
-  defaultVariant,
-} from "./Button.shared"
+/** Different theme variations */
+export type ButtonVariant = "primaryBlack" | "primaryWhite" | "secondaryGray" | "secondaryOutline" | "noOutline"
+/** Default button color variant */
+export const defaultVariant: ButtonVariant = "primaryBlack"
 
-export interface WebButtonProps extends ButtonProps {
-  /** The underlying type of button */
-  type?: string
+/** The size of the button */
+export type ButtonSize = "small" | "medium" | "large"
+
+/** Default button size */
+export const defaultSize: ButtonSize = "medium"
+
+export interface ButtonProps extends ButtonBaseProps {
+  children: ReactNode
+  /** The size of the button */
+  size?: ButtonSize
+  /** The theme of the button */
+  variant?: ButtonVariant
+  /** React Native only, Callback on press, use instead of onClick */
+  onPress?: (e) => void
+}
+
+export interface ButtonBaseProps extends BoxProps {
+  /** Size of the button */
+  buttonSize?: ButtonSize
+  /** Displays a loader in the button */
+  loading?: boolean
+  /** Disabled interactions */
+  disabled?: boolean
+  /** Uses inline style for button */
+  inline?: boolean
+  /** Makes button full width */
+  block?: boolean
+  /** Callback on click */
+  onClick?: (e) => void
+  /** Additional styles to apply to the variant */
+  variantStyles?: any // FIXME
+  /** Pass the longest text to the button for the button to keep longest text width */
+  longestText?: string
+}
+
+/**
+ * Returns various colors for each state given a button variant
+ * @param variant
+ */
+export function getColorsForVariant(variant: ButtonVariant) {
+  const {
+    colors: { black100, black10, black30, white100, purple100 },
+  } = themeProps
+
+  switch (variant) {
+    case "primaryBlack":
+      return {
+        default: {
+          backgroundColor: black100,
+          borderColor: black100,
+          color: white100,
+        },
+        hover: {
+          backgroundColor: purple100,
+          borderColor: purple100,
+          color: white100,
+        },
+      }
+    case "primaryWhite":
+      return {
+        default: {
+          backgroundColor: white100,
+          borderColor: white100,
+          color: black100,
+        },
+        hover: {
+          backgroundColor: purple100,
+          borderColor: purple100,
+          color: white100,
+        },
+      }
+    case "secondaryGray":
+      return {
+        default: {
+          backgroundColor: black10,
+          borderColor: black10,
+          color: black100,
+        },
+        hover: {
+          backgroundColor: black30,
+          borderColor: black30,
+          color: black100,
+        },
+      }
+    case "secondaryOutline":
+      return {
+        default: {
+          backgroundColor: white100,
+          borderColor: black10,
+          color: black100,
+        },
+        hover: {
+          backgroundColor: white100,
+          borderColor: black100,
+          color: black100,
+        },
+      }
+    case "noOutline":
+      return {
+        default: {
+          backgroundColor: "rgba(0, 0, 0, 0)",
+          borderColor: "rgba(0, 0, 0, 0)",
+          color: black100,
+        },
+        hover: {
+          backgroundColor: white100,
+          borderColor: black100,
+          color: black100,
+        },
+      }
+    default:
+  }
+}
+
+/**
+ * Returns css related to the passed in variant
+ * @param variant
+ */
+export const getStylesForVariant = (variant: ButtonVariant) => {
+  const { default: enabled, hover } = getColorsForVariant(variant)
+
+  return css`
+    ${() => {
+      return `
+          background-color: ${enabled.backgroundColor};
+          border-color: ${enabled.borderColor};
+          color: ${enabled.color};
+
+          @media ${themeProps.mediaQueries.hover} {
+            &:hover {
+              background-color: ${hover.backgroundColor};
+              border-color: ${hover.borderColor};
+              color: ${hover.color};
+            }
+          }
+        `
+    }};
+  `
+}
+
+enum DisplayState {
+  Enabled = "default",
+  Highlighted = "hover",
+  Disabled = "default",
+}
+
+interface ButtonState {
+  previous: DisplayState
+  current: DisplayState
 }
 
 /** A button with various size and color settings */
-export class Button extends Component<WebButtonProps> {
+export class Button extends Component<ButtonProps, ButtonState> {
   static defaultProps = {
     size: defaultSize,
     variant: defaultVariant,
     theme: themeProps,
   }
 
-  getSize(): { height: string; size: "2" | "3t"; px: number } {
+  state = {
+    previous: DisplayState.Enabled,
+    current: DisplayState.Enabled,
+  }
+
+  getSize(): { height: number | string; size: "2" | "3t"; px: number } {
     const { inline } = this.props
     switch (this.props.size) {
       case "small":
-        return {
-          height: inline ? "17px" : "26px",
-          size: "2",
-          px: inline ? 0 : ("15px" as any),
-        }
+        return { height: inline ? 17 : 26, size: "2", px: inline ? 0 : 1 }
       case "medium":
-        return {
-          height: inline ? "21px" : "41px",
-          size: "3t",
-          px: inline ? 0 : 2,
-        }
+        return { height: inline ? 21 : 41, size: "3t", px: inline ? 0 : 2 }
       case "large":
-        return {
-          height: inline ? "21px" : "50px",
-          size: "3t",
-          px: inline ? 0 : 3,
-        }
+        return { height: inline ? 21 : 50, size: "3t", px: inline ? 0 : 3 }
     }
   }
 
-  getVariant() {
-    switch (this.props.variant) {
-      case "primaryBlack":
-        return css`
-          ${props => {
-            const { colors } = props.theme
+  get loadingStyles() {
+    const { inline, loading } = this.props
 
-            return `
-                background-color: ${colors.black100};
-                border-color: ${colors.black100};
-                color: ${colors.white100};
+    if (!loading) {
+      return {}
+    }
 
-                @media ${themeProps.mediaQueries.hover} {
-                  &:hover {
-                    background-color: ${colors.purple100};
-                    border-color: ${colors.purple100};
-                    color: ${colors.white100};
-                  }
-                }
-              `
-          }};
-        `
-      case "primaryWhite":
-        return css`
-          ${props => {
-            const { colors } = props.theme
+    if (inline) {
+      return {
+        backgroundColor: "rgba(0, 0, 0, 0)",
+        color: "rgba(0, 0, 0, 0)",
+        borderWidth: 0,
+      }
+    }
 
-            return `
-                background-color: ${colors.white100};
-                border-color: ${colors.white100};
-                color: ${colors.black100};
+    const { black100 } = themeProps.colors
 
-                @media ${themeProps.mediaQueries.hover} {
-                  &:hover {
-                    background-color: ${colors.purple100};
-                    border-color: ${colors.purple100};
-                    color: ${colors.white100};
-                  }
-                }
-              `
-          }};
-        `
-      case "secondaryGray":
-        return css`
-          ${props => {
-            const { colors } = props.theme
+    return {
+      backgroundColor: black100,
+      borderColor: black100,
+      color: "rgba(0, 0, 0, 0)",
+    }
+  }
 
-            return `
-                background-color: ${colors.black10};
-                border-color: ${colors.black10};
-                color: ${colors.black100};
+  get spinnerColor() {
+    const { inline, variant } = this.props
 
-                @media ${themeProps.mediaQueries.hover} {
-                  &:hover {
-                    background-color: ${colors.black30};
-                    border-color: ${colors.black30};
-                    color: ${colors.black100};
-                  }
-                }
-              `
-          }};
-        `
-      case "secondaryOutline":
-        return css`
-          ${props => {
-            const { colors } = props.theme
-            return `
-                background-color: ${colors.white100};
-                border-color: ${colors.black10};
-                color: ${colors.black100};
+    if (inline) {
+      return variant === "primaryWhite" ? "white100" : "black100"
+    }
 
-                @media ${themeProps.mediaQueries.hover} {
-                  &:hover {
-                    background-color: ${colors.white100};
-                    border-color: ${colors.black100};
-                    color: ${colors.black100};
-                  }
-                }
-              `
-          }};
-        `
-      case "noOutline":
-        return css`
-          ${props => {
-            const { colors } = props.theme
-            return `
-                background-color: transparent;
-                border-color: transparent;
-                color: ${colors.black100};
-              `
-          }};
-        `
-      default:
+    return "white100"
+  }
+
+  onPress = args => {
+    if (this.props.onPress) {
+      // Did someone tap really fast? Flick the highlighted state
+      const { current } = this.state
+
+      if (this.state.current === DisplayState.Enabled) {
+        this.setState({
+          previous: current,
+          current: DisplayState.Highlighted,
+        })
+        setTimeout(
+          () =>
+            this.setState({
+              previous: current,
+              current: DisplayState.Enabled,
+            }),
+          0.3
+        )
+      } else {
+        // Was already selected
+        this.setState({ current: DisplayState.Enabled })
+      }
+
+      this.props.onPress(args)
     }
   }
 
   render() {
-    const buttonProps = {
-      ...this.props,
-      ...this.getSize(),
-      buttonSize: this.props.size,
-      variantStyles: this.getVariant(),
-    }
+    const { children, loading, disabled, inline, longestText, ...rest } = this.props
+    const { px, size, height } = this.getSize()
+    const variantColors = getColorsForVariant(this.props.variant)
+    const opacity = this.props.disabled ? 0.1 : 1.0
 
-    return <ButtonBase {...buttonProps}>{this.props.children}</ButtonBase>
-  }
-}
+    const { current, previous } = this.state
 
-/** A base from which various button implementations can compose from */
-export class ButtonBase extends Component<ButtonBaseProps & SansProps> {
-  static defaultProps = {
-    border: 1,
-    borderRadius: 3,
-  }
-
-  onClick = event => {
-    if (!this.props.loading && this.props.onClick) {
-      this.props.onClick(event)
-    }
-  }
-
-  render() {
-    const {
-      children,
-      loading,
-      disabled,
-      color,
-      size,
-      longestText,
-      weight,
-      onClick,
-      className,
-      ...rest
-    } = this.props
-
-    const loadingClass = loading ? "loading" : ""
-    const disabledClass = disabled ? "disabled" : ""
+    const from = variantColors[previous]
+    const to = variantColors[current]
 
     return (
-      <Container
-        className={[loadingClass, disabledClass, className]
-          .filter(Boolean)
-          .join(" ")}
-        onClick={this.onClick}
-        disabled={disabled}
-        {...rest}
-      >
-        {loading && <Spinner size={this.props.buttonSize} />}
+      <Spring native from={from} to={to}>
+        {props => (
+          <TouchableWithoutFeedback
+            onPress={this.onPress}
+            onPressIn={() => {
+              this.setState({
+                previous: DisplayState.Enabled,
+                current: DisplayState.Highlighted,
+              })
+            }}
+            onPressOut={() => {
+              this.setState({
+                previous: DisplayState.Highlighted,
+                current: DisplayState.Enabled,
+              })
+            }}
+            disabled={disabled}
+          >
+            <Flex flexDirection="row">
+              <AnimatedContainer
+                {...rest}
+                loading={loading}
+                disabled={disabled}
+                style={{ ...props, ...this.loadingStyles, height, opacity }}
+                px={px}
+              >
+                <VisibleTextContainer>
+                  <Sans weight="medium" color={this.loadingStyles.color || to.color} size={size}>
+                    {children}
+                  </Sans>
+                </VisibleTextContainer>
+                <HiddenText role="presentation" weight="medium" size={size}>
+                  {longestText ? longestText : children}
+                </HiddenText>
 
-        {longestText ? (
-          <>
-            <VisibleText
-              pt="1px"
-              weight={weight || "medium"}
-              color={color}
-              size={size}
-            >
-              {children}
-            </VisibleText>
-            <HiddenText
-              role="presentation"
-              pt="1px"
-              weight={weight || "medium"}
-              size={size}
-            >
-              {longestText}
-            </HiddenText>
-          </>
-        ) : (
-          <Sans pt="1px" weight={weight || "medium"} size={size}>
-            {children}
-          </Sans>
+                {loading && <Spinner size={this.props.size} color={this.spinnerColor} />}
+              </AnimatedContainer>
+            </Flex>
+          </TouchableWithoutFeedback>
         )}
-      </Container>
+      </Spring>
     )
   }
 }
 
-const VisibleText = styled(Sans)`
+/** Base props that construct button */
+
+const VisibleTextContainer = styled(Box)`
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
 `
 
 const HiddenText = styled(Sans)`
   opacity: 0;
-  pointer-events: none;
 `
 
-const Container = styled.button<ButtonBaseProps>`
-  cursor: pointer;
+const Container = styled(Box)<ButtonProps>`
+  align-items: center;
+  justify-content: center;
   position: relative;
-  white-space: nowrap;
-
-  ${borders};
-  ${borderRadius};
-  ${space};
-  ${textAlign};
-  ${width};
-  ${height};
-
-  ${props => {
-    if (!props.loading) {
-      return `
-        transition: 0.25s ease;
-      `
-    }
-  }};
-
-  ${props => props.variantStyles};
-
-  &.loading {
-    transition: none;
-    background-color: transparent;
-    color: transparent;
-    border: 0;
-    cursor: auto;
-  }
-
-  &.disabled {
-    ${props => {
-      const { colors } = props.theme
-
-      return `
-        background-color: ${colors.black10};
-        border-color: ${colors.black10};
-        color: ${colors.white100};
-        pointer-events: none;
-      `
-    }};
-  }
+  border-width: 1;
+  border-radius: 3;
+  width: ${p => (p.block ? "100%" : "auto")};
 `
+
+const AnimatedContainer = animated(Container)
