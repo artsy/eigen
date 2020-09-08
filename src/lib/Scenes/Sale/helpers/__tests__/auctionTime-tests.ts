0223d567fbbceb09a7294d8a90ba8bfb381527b3
Adam Butler <ab@adam-butler.com>
2020-09-08 11:22:11 +0200

    chore: fix tests

---
+++ src/lib/Scenes/Sale/helpers/__tests__/auctionTime-tests.ts
@@ -0,0 +1,84 @@
+// import moment from "moment-timezone"
+
+const TIME_ZONE_NY = "America/New_York"
+const TIME_ZONE_LA = "America/Los_Angeles"
+const TIME_ZONE_DE = "Europe/Berlin"
+
+Date.now = jest.fn(() => new Date("2020-09-04T19:00:00Z"))
+jest.mock("moment-timezone", () => {
+  const momentMock = require.requireActual("moment-timezone")
+  momentMock.tz.setDefault("America/New_York")
+  momentMock.tz.guess = () => "America/New_York"
+  return momentMock
+})
+
+import { saleTime } from "../saleTime"
+
+const liveSaleNY = {
+  liveStartAt: "2020-10-01T15:00:00",
+  timeZone: TIME_ZONE_NY,
+}
+
+const timedSaleNY = {
+  startAt: "2020-10-01T15:00:00",
+  timeZone: TIME_ZONE_NY,
+}
+
+const liveSaleImminentNY = {
+  liveStartAt: "2020-09-04T19:00:00",
+  timeZone: TIME_ZONE_NY,
+}
+
+const liveSaleSoonNY = {
+  startAt: "2020-09-09T15:00:00",
+  timeZone: TIME_ZONE_NY,
+}
+
+const liveSaleSoonLA = {
+  startAt: "2020-09-09T21:00:00",
+  timeZone: TIME_ZONE_LA,
+}
+
+const finishedSaleNY = {
+  startAt: "2020-08-01T15:00:00",
+  endAt: "2020-09-01T15:00:00",
+  timeZone: TIME_ZONE_NY,
+}
+
+const liveSaleDE = {
+  liveStartAt: "2020-10-01T15:00:00",
+  timeZone: TIME_ZONE_DE,
+}
+
+describe("#saleTime.absolute", () => {
+  it("shows the time in the correct timezone", () => {
+    expect(saleTime(liveSaleNY)?.absolute).toEqual("Live bidding begins Oct 1 at 3:00pm EDT")
+    expect(saleTime(liveSaleDE)?.absolute).toEqual("Live bidding begins Oct 1 at 9:00am EDT")
+  })
+  it("recognises whether an auction is live or not", () => {
+    expect(saleTime(liveSaleNY)?.absolute).toEqual("Live bidding begins Oct 1 at 3:00pm EDT")
+    expect(saleTime(timedSaleNY)?.absolute).toEqual("Bidding begins Oct 1 at 3:00pm EDT")
+  })
+  it("recognises whether an auction is over or not", () => {
+    expect(saleTime(finishedSaleNY)?.absolute).toEqual("Closed on Sep 1")
+  })
+})
+
+describe("#saleTime.relative", () => {
+  it("shows the relative time for imminent sales", () => {
+    expect(saleTime(liveSaleImminentNY)?.relative).toEqual("Starts in 4 hours")
+    expect(saleTime(liveSaleSoonNY)?.relative).toEqual("Starts in 5 days")
+  })
+
+  it("doesn't show the relative time for a non-imminent sale", () => {
+    expect(saleTime(liveSaleNY)?.relative).toEqual(null)
+  })
+
+  it("doesn't show the relative time for a recently finished sale", () => {
+    expect(saleTime(finishedSaleNY)?.relative).toEqual(null)
+  })
+
+  it("handles timezone differences across midnight", () => {
+    expect(saleTime(liveSaleSoonLA)?.relative).toEqual("Starts in 6 days")
+  })
+})
---
+++ src/lib/Scenes/Sale/helpers/saleTime.ts
@@ -0,0 +1,68 @@
+import moment from "moment-timezone"
+
+export const saleTime = (sale?: {
+  liveStartAt?: string | null
+  endAt?: string | null
+  startAt?: string | null
+  timeZone?: string | null
+}) => {
+  if (!sale || !sale.timeZone) {
+    return
+  }
+  const startDate = (sale?.liveStartAt || sale?.startAt) as string
+  const endDate = sale?.endAt as string
+  const isLive = !!sale?.liveStartAt
+  const userTimeZone = moment.tz.guess()
+  const startDateMoment = startDate && moment.tz(startDate, moment.ISO_8601, sale.timeZone).tz(userTimeZone)
+  const endDateMoment = endDate && moment.tz(endDate, moment.ISO_8601, sale.timeZone).tz(userTimeZone)
+  const now = moment()
+
+  return {
+    absolute: absolute(now, endDateMoment, isLive, startDateMoment, userTimeZone),
+    relative: relative(now, endDateMoment, isLive, startDateMoment, userTimeZone),
+  }
+}
+
+const absolute = (now: any, endDateMoment: any, isLive: boolean, startDateMoment: any, userTimeZone: string) => {
+  if (endDateMoment && now.diff(endDateMoment) > 0) {
+    return `Closed on ${endDateMoment.format("MMM D")}`
+  } else {
+    return (
+      `${isLive ? "Live bidding" : "Bidding"} ` +
+      (now.diff(startDateMoment) < 0
+        ? `begins ${startDateMoment.format("MMM D")} ` +
+          `at ${startDateMoment.format("h:mma")} ` +
+          moment.tz(userTimeZone).format("z")
+        : `closes ${endDateMoment.format("MMM D")} ` + `at ${endDateMoment.format("h:mma")} `)
+    )
+  }
+}
+
+const relative = (now: any, endDateMoment: any, isLive: boolean, startDateMoment: any, userTimeZone: string) => {
+  const nowUtc = now.utc()
+  const startUtc = startDateMoment.utc()
+  const endUtc = endDateMoment && endDateMoment.utc()
+  if (nowUtc.diff(startDateMoment) < 0) {
+    const hours = startUtc.diff(nowUtc, "hours")
+    const days = startUtc.dayOfYear() - nowUtc.dayOfYear()
+    if (days < 1) {
+      return `Starts in ${hours} hours`
+    } else if (days < 7) {
+      return `Starts in ${days} days`
+    } else {
+      return null
+    }
+  } else if (nowUtc.diff(startUtc) >= 0 && endUtc && now.diff(endUtc) < 0) {
+    console.log(userTimeZone)
+    const hours = endUtc.diff(nowUtc, "hours")
+    const days = endUtc.dayOfYear() - nowUtc.dayOfYear()
+    if (days < 1) {
+      return `Ends in ${hours} hours`
+    } else if (days < 7) {
+      return `Ends in ${days} days`
+    } else {
+      return null
+    }
+  }
+  return null
+}
