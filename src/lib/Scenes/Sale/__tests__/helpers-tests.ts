99e0d48a335334388e09c4ad9f8b3c0c29e76b7e
Pavlos Vinieratos <pvinis@gmail.com>
2020-09-11 14:48:16 +0200

    simplify

--- src/lib/Scenes/Sale/__tests__/helpers-tests.ts
+++ src/lib/Scenes/Sale/__tests__/helpers-tests.ts
@@ -1,18 +1,9 @@
 import moment from "moment"
 
-import { isCurrentlyActive, saleStatus } from "../helpers"
+import { saleStatus } from "../helpers"
 
 const dAdd = (date: number, amount: number, type: "day" | "days") => moment(date).add(amount, type).toISOString()
 const dSub = (date: number, amount: number, type: "day" | "days") => moment(date).subtract(amount, type).toISOString()
-
-describe(isCurrentlyActive, () => {
-  it("calculates correctly", () => {
-    const now = Date.now()
-    expect(isCurrentlyActive(dAdd(now, 1, "day"), dAdd(now, 2, "days"))).toBe(false)
-    expect(isCurrentlyActive(dSub(now, 1, "day"), dAdd(now, 2, "days"))).toBe(true)
-    expect(isCurrentlyActive(dSub(now, 3, "days"), dSub(now, 2, "days"))).toBe(false)
-  })
-})
 
 describe(saleStatus, () => {
   it("calculates correctly", () => {
--- src/lib/Scenes/Sale/helpers.ts
+++ src/lib/Scenes/Sale/helpers.ts
@@ -2,23 +2,18 @@ import moment from "moment"
 
 export type SaleStatus = "notYetOpen" | "active" | "closed"
 
-export const isCurrentlyActive = (startAt: string, endAt: string) => {
-  const now = moment()
-  return moment(startAt) < now && now < moment(endAt)
-}
-
 export const saleStatus = (startAt: string | null, endAt: string | null): SaleStatus => {
   if (startAt === null || endAt === null) {
     return "notYetOpen"
   }
 
-  if (isCurrentlyActive(startAt, endAt)) {
-    return "active"
-  }
-
   const now = moment()
   if (moment(startAt) > now) {
     return "notYetOpen"
+  }
+
+  if (moment(startAt) < now && now < moment(endAt)) {
+    return "active"
   }
 
   return "closed"
