f32eac61595daa05df9a3876b38e3abd74cb06e7
Adam Butler <ab@adam-butler.com>
2020-09-14 17:21:10 +0200

    feat: add FancyModalHeader

--- src/lib/Scenes/Artwork/Components/CommercialButtons/InquiryModal.tsx
+++ src/lib/Scenes/Artwork/Components/CommercialButtons/InquiryModal.tsx
@@ -1,4 +1,5 @@
 import { FancyModal } from "lib/Components/FancyModal/FancyModal"
+import { FancyModalHeader } from "lib/Components/FancyModal/FancyModalHeader"
 import { CloseIcon, Text } from "palette"
 import React from "react"
 import NavigatorIOS from "react-native-navigator-ios"
@@ -18,7 +19,9 @@ export const InquiryModal: React.FC<InquiryModalProps> = (props) => {
 
   return (
     <FancyModal visible={modalIsVisible} onBackgroundPressed={() => toggleVisibility()}>
-      <CloseIcon onPress={() => toggleVisibility()} />
+      <FancyModalHeader leftButtonText="Cancel" onLeftButtonPress={() => toggleVisibility()}>
+        Contact Gallery
+      </FancyModalHeader>
       <ArtworkDetails artwork={props.artwork} />
       <Text m={2} variant="title">
         More here
