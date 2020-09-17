fc0ea9695b613f409e6c0d0c0a6bb517b12713cb
David Sheldrick <d.j.sheldrick@gmail.com>
2020-09-17 10:36:12 +0100

    sticky scroll header prototype

--- src/lib/Scenes/MyCollection/Screens/ConsignmentsHome/ConsignmentsHome.tsx
+++ src/lib/Scenes/MyCollection/Screens/ConsignmentsHome/ConsignmentsHome.tsx
@@ -1,12 +1,13 @@
 import { tappedConsign, TappedConsignArgs } from "@artsy/cohesion"
 import { ConsignmentsHome_targetSupply } from "__generated__/ConsignmentsHome_targetSupply.graphql"
 import { ConsignmentsHomeQuery } from "__generated__/ConsignmentsHomeQuery.graphql"
+import { FancyModalHeader } from "lib/Components/FancyModal/FancyModalHeader"
 import SwitchBoard from "lib/NativeModules/SwitchBoard"
 import { defaultEnvironment } from "lib/relay/createEnvironment"
 import { renderWithPlaceholder } from "lib/utils/renderWithPlaceholder"
-import { Join, Separator } from "palette"
-import React, { useRef } from "react"
-import { ScrollView } from "react-native"
+import { Flex, Join, Separator } from "palette"
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { Animated, ScrollView, ScrollViewProps } from "react-native"
 import { createFragmentContainer, graphql, QueryRenderer } from "react-relay"
 import { useTracking } from "react-tracking"
 import { RelayModernEnvironment } from "relay-runtime/lib/store/RelayModernEnvironment"
@@ -32,17 +33,30 @@ export const ConsignmentsHome: React.FC<Props> = ({ targetSupply, isLoading }) =
       SwitchBoard.presentModalViewController(navRef.current, route)
     }
   }
+  const { headerElement, scrollProps } = useStickyScrollHeader({
+    header: (
+      <Flex backgroundColor="white">
+        <FancyModalHeader rightButtonText="Submit" onRightButtonPress={() => handleConsignPress({} as any)}>
+          Sell your art
+        </FancyModalHeader>
+      </Flex>
+    ),
+    showAtScrollOffset: 200,
+  })
 
   return (
-    <ScrollView ref={navRef}>
-      <Join separator={<Separator my={3} />}>
-        <Header onConsignPress={handleConsignPress} />
-        <RecentlySold targetSupply={targetSupply} isLoading={isLoading} />
-        <HowItWorks />
-        <ArtistList targetSupply={targetSupply} isLoading={isLoading} />
-        <Footer onConsignPress={handleConsignPress} />
-      </Join>
-    </ScrollView>
+    <>
+      <ScrollView ref={navRef} {...scrollProps}>
+        <Join separator={<Separator my={3} />}>
+          <Header onConsignPress={handleConsignPress} />
+          <RecentlySold targetSupply={targetSupply} isLoading={isLoading} />
+          <HowItWorks />
+          <ArtistList targetSupply={targetSupply} isLoading={isLoading} />
+          <Footer onConsignPress={handleConsignPress} />
+        </Join>
+      </ScrollView>
+      {headerElement}
+    </>
   )
 }
 
@@ -77,4 +91,54 @@ export const ConsignmentsHomeQueryRenderer: React.FC<ConsignmentsHomeQueryRender
       })}
     />
   )
+}
+
+function useStickyScrollHeader({ header, showAtScrollOffset }: { header: JSX.Element; showAtScrollOffset: number }) {
+  const [isVisible, setIsVisible] = useState(false)
+  const entrance = useRef(new Animated.Value(0)).current
+  const headerElement = useMemo(
+    () => (
+      <Animated.View
+        pointerEvents={isVisible ? undefined : "none"}
+        style={{
+          position: "absolute",
+          top: 0,
+          width: "100%",
+          opacity: entrance,
+          transform: [
+            {
+              translateY: entrance.interpolate({
+                inputRange: [0, 1],
+                outputRange: [-20, 0],
+              }),
+            },
+          ],
+        }}
+      >
+        {header}
+      </Animated.View>
+    ),
+    [header, isVisible]
+  )
+
+  const onScroll = useCallback<NonNullable<ScrollViewProps["onScroll"]>>((e) => {
+    setIsVisible(e.nativeEvent.contentOffset.y >= showAtScrollOffset)
+  }, [])
+
+  useEffect(() => {
+    Animated.spring(entrance, {
+      toValue: isVisible ? 1 : 0,
+      useNativeDriver: true,
+      bounciness: -7,
+      speed: 13,
+    }).start()
+  }, [isVisible])
+
+  return {
+    headerElement,
+    scrollProps: {
+      onScroll,
+      scrollEventThreshold: 100,
+    },
+  }
 }
