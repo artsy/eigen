4898fbad6ccadf53c0288f2f6723ff6e753f3da2
Erik <erikdstock@users.noreply.github.com>
2020-09-22 16:40:24 -0500

    My Bids QA items (#3891)
    * simplify sale time structure
    
    * live auctions are never reserve not met
    
    * closed lot message
    
    * improved timeliness checks for sales

---
+++ src/lib/Scenes/MyBids/helpers/timely.ts
@@ -0,0 +1,62 @@
+import moment from "moment-timezone"
+
+/** Whether lot is in one of its pre-closed states */
+export const lotInActiveSale: (lotStanding: {
+  saleArtwork: { sale: { status: string | null } | null } | null
+}) => boolean = (lotStanding) => {
+  const status = lotStanding?.saleArtwork?.sale?.status
+  return !!status && ["open", "preview"].includes(status.toLowerCase())
+}
+
+/** Whether the lot has been completed, regardless of the sale's closed status */
+export const isLotStandingComplete: (lotStanding: { lotState?: { soldStatus?: string } }) => boolean = (lotStanding) =>
+  !!(lotStanding.lotState?.soldStatus && ["sold", "passed"].includes(lotStanding.lotState.soldStatus?.toLowerCase()))
+
+interface SaleWithTimes {
+  status?: string | null
+  liveStartAt?: string | null
+  endAt?: string | null
+}
+
+export class TimelySale {
+  static create(sale: SaleWithTimes): TimelySale {
+    return new TimelySale(sale)
+  }
+  private status: string
+  private endAt: string
+  private liveStartAt: string
+
+  constructor(sale: SaleWithTimes) {
+    this.status = sale.status!
+    this.endAt = sale.endAt!
+    this.liveStartAt = sale.liveStartAt!
+  }
+
+  get isLAI() {
+    return !!this.liveStartAt
+  }
+
+  get isClosed() {
+    return this.status === "closed"
+  }
+
+  get isActive() {
+    return ["open", "preview"].includes(this.status)
+  }
+
+  get relevantEnd(): string {
+    return this.isLAI ? this.liveStartAt : this.endAt
+  }
+
+  isLiveBiddingNow(): boolean {
+    if (this.isClosed || !this.isLAI) {
+      return false
+    }
+
+    const tz = moment.tz.guess(true)
+    const now = moment().tz(tz)
+    const liveStartMoment = moment(this.liveStartAt!).tz(tz)
+
+    return now.isAfter(liveStartMoment)
+  }
+}
