d993f226c83ef46e90eef1b1da1f7c19b31b8a7f
Pavlos Vinieratos <pvinis@gmail.com>
2020-10-02 14:28:10 +0200

    Restore ARTopMenuViewController.m

--- Artsy/View_Controllers/App_Navigation/ARTopMenuViewController.m
+++ Artsy/View_Controllers/App_Navigation/ARTopMenuViewController.m
@@ -1,7 +1,11 @@
+#import "ARTopMenuViewController+DeveloperExtras.h"
 #import "ARContentViewControllers.h"
 #import "ARAppStatus.h"
+#import "ArtsyEcho.h"
 
 #import "UIViewController+FullScreenLoading.h"
+#import "ARTabContentView.h"
+#import "ARTopMenuNavigationDataSource.h"
 #import "ARUserManager.h"
 #import "ArtsyAPI+Private.h"
 #import "ARAppConstants.h"
@@ -30,7 +34,18 @@
 #import <Emission/ARNotificationsManager.h>
 #import <React/RCTScrollView.h>
 
-//remove
+@interface ARTopMenuViewController () <ARTabViewDelegate>
+
+@property (readwrite, nonatomic, strong) ARTopMenuNavigationDataSource *navigationDataSource;
+
+@property (readwrite, nonatomic, assign) NSString * currentTab;
+
+@property (readonly, nonatomic, strong) ArtsyEcho *echo;
+
+// we need to wait for the view to load before we push a deep link VC on startup
+@property (readwrite, nonatomic, strong) NSMutableArray<void (^)(void)> *bootstrapQueue;
+@property (readwrite, nonatomic, assign) BOOL didBootstrap;
+@end
 
 static ARTopMenuViewController *_sharedManager = nil;
 
@@ -44,12 +59,19 @@ static ARTopMenuViewController *_sharedManager = nil;
     return _sharedManager;
 }
 
++ (void)teardownSharedInstance
+{
+    _sharedManager = nil;
+}
 
 - (instancetype)init {
     self = [super init];
     if (!self) {
         return self;
     }
+
+    _didBootstrap = NO;
+    _bootstrapQueue = [[NSMutableArray alloc] init];
 
     return self;
 }
@@ -58,25 +80,41 @@ static ARTopMenuViewController *_sharedManager = nil;
 {
     [super viewDidLoad];
 
+    _echo = [[ArtsyEcho alloc] init];
+
     self.view.backgroundColor = [UIColor whiteColor];
 
-//    self.navigationDataSource = _navigationDataSource ?: [[ARTopMenuNavigationDataSource alloc] init];
-//
-//    ARTabContentView *tabContentView = [[ARTabContentView alloc] initWithFrame:CGRectZero
-//                                                            hostViewController:self
-//                                                                      delegate:self
-//                                                                    dataSource:self.navigationDataSource];
+    self.navigationDataSource = _navigationDataSource ?: [[ARTopMenuNavigationDataSource alloc] init];
 
-//    _tabContentView = tabContentView;
-//    [self.view addSubview:tabContentView];
-//    [tabContentView alignToView:self.view];
+    ARTabContentView *tabContentView = [[ARTabContentView alloc] initWithFrame:CGRectZero
+                                                            hostViewController:self
+                                                                      delegate:self
+                                                                    dataSource:self.navigationDataSource];
 
+    _tabContentView = tabContentView;
+    [self.view addSubview:tabContentView];
+    [tabContentView alignToView:self.view];
+    self.currentTab = [ARTabType home];
+    [self.tabContentView forceSetCurrentTab:[ARTabType home] animated:NO];
 
     // Ensure it's created now and started listening for keyboard changes.
     // TODO Ideally this pod would start listening from launch of the app, so we don't need to rely on this one but can
     // be assured that any VCs guide can be trusted.
     (void)self.keyboardLayoutGuide;
 
+    self.didBootstrap = YES;
+    __weak typeof(self) wself = self;
+    dispatch_async(dispatch_get_main_queue(), ^() {
+        __strong typeof(self) sself = wself;
+        if (!sself) {
+            return;
+        }
+        while (self.bootstrapQueue.count > 0) {
+            void (^completion)(void) = [self.bootstrapQueue firstObject];
+            [self.bootstrapQueue removeObjectAtIndex:0];
+            completion();
+        }
+    });
 }
 
 
@@ -86,6 +124,29 @@ static ARTopMenuViewController *_sharedManager = nil;
     [self.view layoutSubviews];
 }
 
+- (ARNavigationController *)rootNavigationController;
+{
+    return (ARNavigationController *)[self.tabContentView currentNavigationController];
+}
+
+- (ARNavigationController *)rootNavigationControllerAtTab:(NSString *)tab;
+{
+    return (ARNavigationController *)[self.navigationDataSource navigationControllerForTabType:tab];
+}
+
+#pragma mark - Buttons
+
+- (CGFloat)bottomMargin
+{
+    return self.view.safeAreaInsets.bottom * -1;
+}
+
+#pragma mark - ARMenuAwareViewController
+
+- (BOOL)hidesNavigationButtons
+{
+    return YES;
+}
 
 #pragma mark - UIViewController
 
@@ -99,14 +160,126 @@ static ARTopMenuViewController *_sharedManager = nil;
         ARAppNotificationsDelegate *remoteNotificationsDelegate = [[JSDecoupledAppDelegate sharedAppDelegate] remoteNotificationsDelegate];
         [remoteNotificationsDelegate registerForDeviceNotificationsWithContext:ARAppNotificationsRequestContextOnboarding];
     }
+
+#ifdef DEBUG
+    if ([ARAppStatus isRunningTests] == NO) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appHasBeenInjected:) name:@"INJECTION_BUNDLE_NOTIFICATION" object:nil];
+
+            [self runSwiftDeveloperExtras];
+            [self runDeveloperExtras];
+        });
+    }
+#endif
 }
 
+- (UIViewController *)childViewControllerForStatusBarHidden
+{
+    return self.rootNavigationController;
+}
+
+- (UIViewController *)childViewControllerForStatusBarStyle
+{
+    return self.rootNavigationController;
+}
+
+#pragma mark - Pushing VCs
+
+- (void)pushViewController:(UIViewController *)viewController
+{
+    [self pushViewController:viewController animated:ARPerformWorkAsynchronously];
+}
+
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    [self pushViewController:viewController animated:animated completion:nil];
+}
+
+- (void)pushViewController:(__strong UIViewController *)viewController animated:(BOOL)animated completion:(void (^__nullable)(void))completion
+{
+    __weak typeof(self) wself = self;
+    [self afterBootstrap:^{
+        __strong typeof(self) sself = wself;
+        if (!sself) return;
+        [sself pushViewControllerNow:viewController animated:animated completion:completion];
+    }];
+}
+
+- (void)afterBootstrap:(void (^)(void))completion {
+    if (self.didBootstrap) {
+        completion();
+    } else {
+        [self.bootstrapQueue addObject:completion];
+    }
+}
+
+- (void)pushViewControllerNow:(UIViewController *)viewController animated:(BOOL)animated completion:(void (^__nullable)(void))completion
+{
+    NSAssert(viewController != nil, @"Attempt to push a nil view controller.");
+    [self.rootNavigationController pushViewController:viewController animated:animated];
+}
+
+- (void)presentRootViewControllerInTab:(NSString *)tabType animated:(BOOL)animated props:(NSDictionary *)props;
+{
+    __weak typeof(self) wself = self;
+    [self afterBootstrap:^{
+        __strong typeof(self) sself = wself;
+        if (!sself) return;
+        [sself presentRootViewControllerInTabNow:tabType animated:animated props:props];
+    }];
+}
+
+- (void)presentRootViewControllerInTabNow:(NSString *)tabType animated:(BOOL)animated props:(NSDictionary *)props;
+{
+    BOOL alreadySelectedTab = [self.currentTab isEqual:tabType];
+    ARNavigationController *controller = [self rootNavigationControllerAtTab:tabType];
+
+    if (!alreadySelectedTab) {
+        [self.tabContentView setCurrentTab:tabType animated:animated];
+        self.currentTab = tabType;
+    } else if (controller.viewControllers.count > 1) {
+        [controller popToRootViewControllerAnimated:(animated && alreadySelectedTab)];
+    }
+    // Otherwise find the first scrollview and pop to top
+    else {
+        UIViewController *currentRootViewController = [controller.childViewControllers first];
+        UIScrollView *rootScrollView = (id)[self firstScrollToTopScrollViewFromRootView:currentRootViewController.view];
+        [rootScrollView setContentOffset:CGPointMake(rootScrollView.contentOffset.x, -rootScrollView.contentInset.top) animated:YES];
+    }
+    if ([controller.rootViewController isKindOfClass:ARComponentViewController.class]) {
+        ARComponentViewController *vc = (ARComponentViewController *)controller.rootViewController;
+        [props each:^(id key, id value) {
+            [vc setProperty:value forKey:key];
+        }];
+    }
+}
+
+
+#pragma mark - Auto Rotation
+
+// Let the nav decide what rotations to support
+
+- (BOOL)shouldAutorotate
+{
+    return [self.rootNavigationController shouldAutorotate];
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+    return self.rootNavigationController.supportedInterfaceOrientations ?: ([UIDevice isPad] ? UIInterfaceOrientationMaskAll : UIInterfaceOrientationMaskPortrait);
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+{
+    return self.rootNavigationController.preferredInterfaceOrientationForPresentation ?: UIInterfaceOrientationPortrait;
+}
 
 #pragma mark - ARTabViewDelegate
 
 - (void)tabContentView:(ARTabContentView *)tabContentView didChangeToTab:(NSString *)tabType
 {
-//    [[AREmission sharedInstance] updateState:@{[ARStateKey selectedTab]: tabType}];
+    [[AREmission sharedInstance] updateState:@{[ARStateKey selectedTab]: tabType}];
 }
 
 - (NSObject *_Nullable)firstScrollToTopScrollViewFromRootView:(UIView *)initialView
