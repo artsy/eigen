6f55447dcd1af9257f24f3144121ad2665b7163a
Pavlos Vinieratos <pvinis@gmail.com>
2020-10-02 14:15:27 +0200

    Restore Artsy/Views/Utilities/ARTabType.m

---
+++ Artsy/Views/Utilities/ARTabType.m
@@ -0,0 +1,10 @@
+#import "ARTabType.h"
+
+@implementation ARTabType
+// This file must match "BottomTabType.ts"
++(NSString *)home { return @"home"; }
++(NSString *)search { return @"search"; }
++(NSString *)inbox { return @"inbox"; }
++(NSString *)sell { return @"sell"; }
++(NSString *)profile { return @"profile"; }
+@end
