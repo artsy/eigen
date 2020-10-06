ed7e03bd05219b262d5247094d99d6146f8bed25
Ashley Jelks <arjelks@gmail.com>
2020-10-06 15:35:33 -0400

    add ui elements for shipping flow

--- src/lib/Scenes/Artwork/Components/CommercialButtons/InquiryModal.tsx
+++ src/lib/Scenes/Artwork/Components/CommercialButtons/InquiryModal.tsx
@@ -1,10 +1,13 @@
 import { InquiryModal_artwork } from "__generated__/InquiryModal_artwork.graphql"
+import { Checkbox } from "lib/Components/Bidding/Components/Checkbox"
 import { FancyModal } from "lib/Components/FancyModal/FancyModal"
 import { FancyModalHeader } from "lib/Components/FancyModal/FancyModalHeader"
-import { Text } from "palette"
+import ChevronIcon from "lib/Icons/ChevronIcon"
+import { Box, color, Flex, Separator, space, Text } from "palette"
 import React from "react"
 import NavigatorIOS from "react-native-navigator-ios"
 import { createFragmentContainer, graphql } from "react-relay"
+import styled from "styled-components/native"
 import { CollapsibleArtworkDetailsFragmentContainer } from "./CollapsibleArtworkDetails"
 
 interface InquiryModalProps {
@@ -18,6 +21,29 @@ interface InquiryModalProps {
 
 export const InquiryModal: React.FC<InquiryModalProps> = ({ artwork, ...props }) => {
   const { toggleVisibility, modalIsVisible } = props
+  const renderInfoBox = (infoText: string): JSX.Element => {
+    return (
+      <InfoBox key={infoText}>
+        <Flex flexDirection="row">
+          <Checkbox />
+          <Text variant="text">{infoText}</Text>
+        </Flex>
+        {infoText === "Shipping Quote" && (
+          <>
+            <Separator my={2} />
+            <Flex flexDirection="row" justifyContent="space-between">
+              <Text variant="text" color="black60">
+                Add your location
+              </Text>
+              <Box mt={0.5}>
+                <ChevronIcon color="black60" />
+              </Box>
+            </Flex>
+          </>
+        )}
+      </InfoBox>
+    )
+  }
 
   return (
     <FancyModal visible={modalIsVisible} onBackgroundPressed={() => toggleVisibility()}>
@@ -25,12 +51,23 @@ export const InquiryModal: React.FC<InquiryModalProps> = ({ artwork, ...props })
         Contact Gallery
       </FancyModalHeader>
       <CollapsibleArtworkDetailsFragmentContainer artwork={artwork} />
-      <Text m={2} variant="title">
-        More here
-      </Text>
+      <Box m={2}>
+        <Text variant="mediumText">What information are you looking for?</Text>
+        {["Price & Availability", "Shipping Quote", "Condition & Provenance"].map((text) => {
+          return renderInfoBox(text)
+        })}
+      </Box>
     </FancyModal>
   )
 }
+
+const InfoBox = styled(Flex)`
+  border-radius: 5;
+  border: solid 1px ${color("black10")};
+  flex-direction: column;
+  margin-top: ${space(1)}px;
+  padding: ${space(2)}px;
+`
 
 export const InquiryModalFragmentContainer = createFragmentContainer(InquiryModal, {
   artwork: graphql`
