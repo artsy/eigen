9163398669fdd14ffb9f7b2c0bc637356184f7a4
Mounir Dhahri <soussouheros@gmail.com>
2020-10-12 19:42:03 +0200

    fix: center text items withing the sale artwrorks items

--- src/lib/Scenes/Sale/Components/SaleArtworkListItem.tsx
+++ src/lib/Scenes/Sale/Components/SaleArtworkListItem.tsx
@@ -37,7 +37,7 @@ export const SaleArtworkListItem: React.FC<Props> = ({ artwork }) => {
           </Flex>
         )}
 
-        <Flex ml={2} height={100} flex={1}>
+        <Flex ml={2} height={100} flex={1} justifyContent="center">
           {!!artwork.saleArtwork?.lotLabel && (
             <Sans size="3t" color="black60" numberOfLines={1}>
               Lot {artwork.saleArtwork.lotLabel}
