532c6f7d9957997ae7e1d551e3c0371cfd70754d
Will Doenlen <wdoenlen@gmail.com>
2020-10-15 16:52:00 -0400

    Update Fair2 tests to use new testing pattern

--- src/lib/Scenes/Fair2/__tests__/Fair2Editorial-tests.tsx
+++ src/lib/Scenes/Fair2/__tests__/Fair2Editorial-tests.tsx
@@ -1,7 +1,4 @@
-import {
-  Fair2EditorialTestsQuery,
-  Fair2EditorialTestsQueryRawResponse,
-} from "__generated__/Fair2EditorialTestsQuery.graphql"
+import { Fair2EditorialTestsQuery } from "__generated__/Fair2EditorialTestsQuery.graphql"
 import { Fair2EditorialFragmentContainer } from "lib/Scenes/Fair2/Components/Fair2Editorial"
 import { renderWithWrappers } from "lib/tests/renderWithWrappers"
 import { Text, Touchable } from "palette"
@@ -9,58 +6,20 @@ import React from "react"
 import { graphql, QueryRenderer } from "react-relay"
 import { act } from "react-test-renderer"
 import { useTracking } from "react-tracking"
-import { createMockEnvironment } from "relay-test-utils"
+import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
 
 jest.unmock("react-relay")
 
-const FAIR_2_EDITORIAL_FIXTURE: Fair2EditorialTestsQueryRawResponse = {
-  fair: {
-    internalID: "def123",
-    slug: "art-basel-hong-kong-2020",
-    id: "art-basel-hong-kong-2020",
-    articles: {
-      edges: [
-        {
-          node: {
-            id: "QXJ0aWNsZTo1ZTdiZDEzM2ZmNTc3NjAwMWYyOTg2YTI=",
-            internalID: "xyz123",
-            slug: "artsy-editorial-sold-art-basel-hong-kongs-online-viewing-rooms",
-            title: "What Sold at Art Basel in Hong Kong’s Online Viewing Rooms",
-            href: "/article/artsy-editorial-sold-art-basel-hong-kongs-online-viewing-rooms",
-            publishedAt: "Mar 26th, 20",
-            thumbnailImage: {
-              src: "https://example.com?example.jpg",
-            },
-          },
-        },
-        {
-          node: {
-            id: "QXJ0aWNsZTo1ZTZmYTNmMWI3N2Y0NTAwMjA3MzdmNTg=",
-            internalID: "abc123",
-            slug: "artsy-editorial-midst-covid-19-chinese-galleries-adapt-persevere",
-            title: "In the Midst of COVID-19, Chinese Galleries Adapt and Persevere",
-            href: "/article/artsy-editorial-midst-covid-19-chinese-galleries-adapt-persevere",
-            publishedAt: "Mar 17th, 20",
-            thumbnailImage: {
-              src: "https://example.com?example.jpg",
-            },
-          },
-        },
-      ],
-    },
-  },
-}
-
 describe("Fair2Editorial", () => {
   const trackEvent = useTracking().trackEvent
-  const getWrapper = (fixture = FAIR_2_EDITORIAL_FIXTURE) => {
+  const getWrapper = (mockResolvers = {}) => {
     const env = createMockEnvironment()
 
     const tree = renderWithWrappers(
       <QueryRenderer<Fair2EditorialTestsQuery>
         environment={env}
         query={graphql`
-          query Fair2EditorialTestsQuery($fairID: String!) @raw_response_type {
+          query Fair2EditorialTestsQuery($fairID: String!) @relay_test_operation {
             fair(id: $fairID) {
               ...Fair2Editorial_fair
             }
@@ -82,13 +41,30 @@ describe("Fair2Editorial", () => {
       />
     )
 
-    env.mock.resolveMostRecentOperation({ errors: [], data: fixture })
+    env.mock.resolveMostRecentOperation((operation) => MockPayloadGenerator.generate(operation, mockResolvers))
 
     return tree
   }
 
   it("renders the 2 articles", () => {
-    const wrapper = getWrapper()
+    const wrapper = getWrapper({
+      Fair: () => ({
+        articles: {
+          edges: [
+            {
+              node: {
+                title: "What Sold at Art Basel in Hong Kong’s Online Viewing Rooms",
+              },
+            },
+            {
+              node: {
+                title: "In the Midst of COVID-19, Chinese Galleries Adapt and Persevere",
+              },
+            },
+          ],
+        },
+      }),
+    })
 
     const links = wrapper.root.findAllByType(Touchable)
     expect(links).toHaveLength(2)
@@ -103,22 +79,34 @@ describe("Fair2Editorial", () => {
   })
 
   it("renders null if there are no articles", () => {
-    const FAIR_2_EDITORIAL_FIXTURE_NO_ARTICLES = {
-      fair: {
-        ...FAIR_2_EDITORIAL_FIXTURE.fair,
-        id: "art-basel-hong-kong-2020",
+    const wrapper = getWrapper({
+      Fair: () => ({
         articles: {
           edges: [],
         },
-      },
-    } as Fair2EditorialTestsQueryRawResponse
-    const wrapper = getWrapper(FAIR_2_EDITORIAL_FIXTURE_NO_ARTICLES)
+      }),
+    })
 
     expect(wrapper.toJSON()).toBe(null)
   })
 
   it("tracks article taps", () => {
-    const wrapper = getWrapper()
+    const wrapper = getWrapper({
+      Fair: () => ({
+        internalID: "def123",
+        slug: "art-basel-hong-kong-2020",
+        articles: {
+          edges: [
+            {
+              node: {
+                internalID: "xyz123",
+                slug: "artsy-editorial-sold-art-basel-hong-kongs-online-viewing-rooms",
+              },
+            },
+          ],
+        },
+      }),
+    })
     const article = wrapper.root.findAllByType(Touchable)[0]
     act(() => article.props.onPress())
 
