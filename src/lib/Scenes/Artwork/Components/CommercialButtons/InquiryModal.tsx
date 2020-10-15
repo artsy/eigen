40b063faa6dc29cb55569036640ad678a2dd5389
Joel Rosenblatt <7117670+jo-rs@users.noreply.github.com>
2020-10-15 17:47:51 +0200

    Removed unused wrapper

--- src/lib/Scenes/Artwork/Components/CommercialButtons/InquiryModal.tsx
+++ src/lib/Scenes/Artwork/Components/CommercialButtons/InquiryModal.tsx
@@ -41,15 +41,6 @@ export const InquiryModal: React.FC<InquiryModalProps> = ({ artwork, ...props })
   const renderInquiryQuestion = (inquiry: string): JSX.Element => {
     // Shipping requires special logic to accomodate dropdown and shipping modal
     const isShipping = inquiry === InquiryOptions.Shipping
-    const Wrapper: React.ComponentType<{ key: string }> = isShipping
-      ? ({ children }) => (
-          <TouchableOpacity
-            data-test-id="expand-shipping"
-            children={children}
-            onPress={() => toggleLocationExpanded()}
-          />
-        )
-      : React.Fragment
 
     return (
       <React.Fragment key={inquiry}>
