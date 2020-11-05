a40a14799d7680aee0b16ddf31fd1a91e8a648f1
David Sheldrick <d.j.sheldrick@gmail.com>
2020-11-05 09:35:42 +0000

    extract ARModalWithBottomSafeAreaViewController into own files

--- Artsy/App/ARScreenPresenterModule.m
+++ Artsy/App/ARScreenPresenterModule.m
@@ -23,37 +23,7 @@
 #import "ARAugmentedRealityConfig.h"
 #import "ARAugmentedFloorBasedVIRViewController.h"
 #import "ARSerifNavigationViewController.h"
-
-@interface ARModalWithBottomSafeAreaViewController : UIViewController
--(instancetype)initWithStack:(UINavigationController *)stack;
-@property (nonatomic, assign) UINavigationController *stack;
-@end
-@implementation ARModalWithBottomSafeAreaViewController
-
-- (instancetype)initWithStack:(UINavigationController *)stack
-{
-    self = [super init];
-    _stack = stack;
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    [self addChildViewController:self.stack];
-    [self.view addSubview:self.stack.view];
-}
-- (void)viewWillLayoutSubviews
-{
-    [super viewWillLayoutSubviews];
-    [self.stack.view alignTopEdgeWithView:self.flk_topLayoutGuide predicate:@"0"];
-    [self.stack.view alignLeading:@"0" trailing:@"0" toView:self.view];
-    CGFloat bottomInset = [UIApplication sharedApplication].keyWindow.safeAreaInsets.bottom;
-    [self.stack.view alignBottomEdgeWithView:self.view predicate:[[NSNumber numberWithFloat:-bottomInset] stringValue]];
-    self.view.backgroundColor = [UIColor whiteColor];
-}
-
-@end
+#import "ARModalWithBottomSafeAreaViewController.h"
 
 @interface ARScreenPresenterModule () <MFMailComposeViewControllerDelegate>
 @end
