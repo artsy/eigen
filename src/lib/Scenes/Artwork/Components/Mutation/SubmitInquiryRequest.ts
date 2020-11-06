f2b8b04bb253dac3574128e0c8e377bc7a68bc10
Erik Stockmeier <erik.d.stock@gmail.com>
2020-11-06 16:06:49 -0500

    wip

--- src/lib/Scenes/Artwork/Components/Mutation/SubmitInquiryRequest.ts
+++ src/lib/Scenes/Artwork/Components/Mutation/SubmitInquiryRequest.ts
@@ -2,38 +2,43 @@ import { SubmitInquiryRequestMutation } from "__generated__/SubmitInquiryRequest
 import { commitMutation, Environment, graphql } from "relay-runtime"
 
 export const SubmitInquiryRequest = (environment: Environment, inquireable: any, payload: any) => {
-  return commitMutation<SubmitInquiryRequestMutation>(environment, {
-    onError: () => {
-      // Show error state
-    },
-    onCompleted: () => {
-      // Show delayed comfirmation notification
-    },
-    variables: {
-      input: {
-        inquireableID: inquireable.internalID,
-        inquireableType: "Artwork",
-        message: payload.message,
-        questions: payload.inquiryQuestions,
-      },
-    },
-    mutation: graphql`
-      mutation SubmitInquiryRequestMutation($input: SubmitInquiryRequestMutationInput!) {
-        submitInquiryRequestMutation(input: $input) {
-          inquiryRequest {
-            inquireable {
-              __typename
-              ... on Artwork {
-                title
-                artists {
-                  name
-                  birthday
-                }
-              }
-            }
-          }
-        }
-      }
-    `,
+  return new Promise<any>((resolve, reject) => {
+    setTimeout(() => {
+      reject("uh oh:(")
+    }, 500)
   })
+  // return commitMutation<SubmitInquiryRequestMutation>(environment, {
+  //   onError: () => {
+  //     // Show error state
+  //   },
+  //   onCompleted: () => {
+  //     // Show delayed comfirmation notification
+  //   },
+  //   variables: {
+  //     input: {
+  //       inquireableID: inquireable.internalID,
+  //       inquireableType: "Artwork",
+  //       message: payload.message,
+  //       questions: payload.inquiryQuestions,
+  //     },
+  //   },
+  //   mutation: graphql`
+  //     mutation SubmitInquiryRequestMutation($input: SubmitInquiryRequestMutationInput!) {
+  //       submitInquiryRequestMutation(input: $input) {
+  //         inquiryRequest {
+  //           inquireable {
+  //             __typename
+  //             ... on Artwork {
+  //               title
+  //               artists {
+  //                 name
+  //                 birthday
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `,
+  // })
 }
