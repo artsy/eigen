9050e2813f9b22bdd3669ee80b1ad4bfc66c466b
Mounir Dhahri <mounir.dhahri@ensi-uma.tn>
2020-11-10 21:02:33 +0100

    fix: add feedback to more info about this auction button (#4133)
    Co-authored-by: Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>

--- src/lib/Components/Buttons/__tests__/CaretButton-tests.tsx
+++ src/lib/Components/Buttons/__tests__/CaretButton-tests.tsx
@@ -4,12 +4,12 @@ import React from "react"
 import { CaretButton } from "../CaretButton"
 
 import { extractText } from "lib/tests/extractText"
-import { TouchableWithoutFeedback } from "react-native"
+import { TouchableOpacity } from "react-native"
 
 describe("CaretButton", () => {
   it("renders properly", () => {
     const tree = renderWithWrappers(<CaretButton text="I am a caret button" />)
-    expect(tree.root.findAllByType(TouchableWithoutFeedback)).toBeTruthy()
+    expect(tree.root.findAllByType(TouchableOpacity)).toBeTruthy()
     expect(extractText(tree.root)).toContain("I am a caret button")
   })
 })
