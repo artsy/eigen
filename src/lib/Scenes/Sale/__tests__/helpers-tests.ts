a80012839045c662f0744aca527e8148cc2332e8
Pavlos Vinieratos <pvinis@gmail.com>
2020-11-10 13:34:48 +0000

    Update src/lib/Scenes/Sale/__tests__/helpers-tests.ts

--- src/lib/Scenes/Sale/__tests__/helpers-tests.ts
+++ src/lib/Scenes/Sale/__tests__/helpers-tests.ts
@@ -12,7 +12,7 @@ describe(saleStatus, () => {
     expect(saleStatus(dAdd(now, 1, "day"), dAdd(now, 2, "days"), null)).toBe("notYetOpen")
     expect(saleStatus(dSub(now, 1, "day"), dAdd(now, 2, "days"), dAdd(now, 1, "day"))).toBe("active")
     expect(saleStatus(dSub(now, 1, "day"), dAdd(now, 2, "days"), null)).toBe("active")
-    expect(saleStatus(dSub(now, 3, "days"), dSub(now, 2, "days"), null)).toBe("closed")
     expect(saleStatus(dSub(now, 3, "days"), dAdd(now, 2, "days"), dSub(now, 1, "day"))).toBe("closed")
+    expect(saleStatus(dSub(now, 3, "days"), dSub(now, 2, "days"), null)).toBe("closed")
   })
 })
