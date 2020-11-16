2d764c08241e320bc46044d3e2bcb711288d230a
brainbicycle <brian.beckerle@artsymail.com>
2020-11-16 16:11:05 -0500

    Update addPhotos tests

--- src/lib/Scenes/MyCollection/Screens/ArtworkFormModal/Screens/__tests__/MyCollectionArtworkFormAddPhotos-tests.tsx
+++ src/lib/Scenes/MyCollection/Screens/ArtworkFormModal/Screens/__tests__/MyCollectionArtworkFormAddPhotos-tests.tsx
@@ -1,13 +1,16 @@
+import { Route } from "@react-navigation/native"
 import { FancyModalHeader } from "lib/Components/FancyModal/FancyModalHeader"
 import { __appStoreTestUtils__, AppStore } from "lib/store/AppStore"
 import { renderWithWrappers } from "lib/tests/renderWithWrappers"
-import React from "react"
+import React, { ReactElement } from "react"
 import { Image, TouchableOpacity } from "react-native"
-import { AddPhotos, tests } from "../AddPhotos"
+import { MyCollectionAddPhotos, tests } from "../MyCollectionArtworkFormAddPhotos"
 
 jest.mock("formik")
 
-describe("AddPhotos", () => {
+describe("MyCollectionAddPhotos", () => {
+  let mockAddPhotos: ReactElement
+
   beforeEach(() => {
     __appStoreTestUtils__?.injectState({
       myCollection: {
@@ -20,28 +23,36 @@ describe("AddPhotos", () => {
         },
       },
     })
+
+    const mockNav = jest.fn()
+    const mockRoute: Route<"AddPhotos", undefined> = {
+      key: "AddPhotos",
+      name: "AddPhotos",
+      params: undefined,
+    }
+    mockAddPhotos = <MyCollectionAddPhotos navigation={mockNav as any} route={mockRoute} />
   })
 
   it("updates header with correct label based on number of photos selected", () => {
-    const wrapper = renderWithWrappers(<AddPhotos />)
+    const wrapper = renderWithWrappers(mockAddPhotos)
     expect(wrapper.root.findByType(FancyModalHeader).props.children).toStrictEqual(["Photos ", "(2)"])
   })
 
   it("displays the correct number of photos", () => {
-    const wrapper = renderWithWrappers(<AddPhotos />)
+    const wrapper = renderWithWrappers(mockAddPhotos)
     expect(wrapper.root.findAllByType(Image).length).toBe(2)
     expect(wrapper.root.findAllByType(tests.DeletePhotoButton).length).toBe(2)
   })
 
   it("renders add photo button", () => {
-    const wrapper = renderWithWrappers(<AddPhotos />)
+    const wrapper = renderWithWrappers(mockAddPhotos)
     expect(wrapper.root.findByType(tests.AddPhotosButton)).toBeDefined()
   })
 
   it("triggers action on add photo button click", () => {
     const spy = jest.fn()
     AppStore.actions.myCollection.artwork.takeOrPickPhotos = spy as any
-    const wrapper = renderWithWrappers(<AddPhotos />)
+    const wrapper = renderWithWrappers(mockAddPhotos)
     wrapper.root.findByType(tests.AddPhotosButton).findByType(TouchableOpacity).props.onPress()
     expect(spy).toHaveBeenCalled()
   })
@@ -49,7 +60,13 @@ describe("AddPhotos", () => {
   it("triggers action on add photo button click", () => {
     const spy = jest.fn()
     AppStore.actions.myCollection.artwork.takeOrPickPhotos = spy as any
-    const wrapper = renderWithWrappers(<AddPhotos />)
+    const mockNav = jest.fn()
+    const mockRoute: Route<"AddPhotos", undefined> = {
+      key: "AddPhotos",
+      name: "AddPhotos",
+      params: undefined,
+    }
+    const wrapper = renderWithWrappers(<MyCollectionAddPhotos navigation={mockNav as any} route={mockRoute} />)
     wrapper.root.findByType(tests.AddPhotosButton).findByType(TouchableOpacity).props.onPress()
     expect(spy).toHaveBeenCalled()
   })
@@ -57,7 +74,13 @@ describe("AddPhotos", () => {
   it("triggers action on delete photo button click", () => {
     const spy = jest.fn()
     AppStore.actions.myCollection.artwork.removePhoto = spy as any
-    const wrapper = renderWithWrappers(<AddPhotos />)
+    const mockNav = jest.fn()
+    const mockRoute: Route<"AddPhotos", undefined> = {
+      key: "AddPhotos",
+      name: "AddPhotos",
+      params: undefined,
+    }
+    const wrapper = renderWithWrappers(<MyCollectionAddPhotos navigation={mockNav as any} route={mockRoute} />)
     wrapper.root.findAllByType(tests.DeletePhotoButton)[0].findByType(TouchableOpacity).props.onPress()
     expect(spy).toHaveBeenCalledWith({ path: "photo/1" })
   })
