daa38fa451aa343dc83bd2603e74290dada19cd5
Pavlos Vinieratos <pvinis@gmail.com>
2020-11-16 16:37:53 +0000

    update version

--- Artsy_Tests/Supporting_Files/ARTestHelper.m
+++ Artsy_Tests/Supporting_Files/ARTestHelper.m
@@ -16,8 +16,8 @@
 {
     NSOperatingSystemVersion version = [NSProcessInfo processInfo].operatingSystemVersion;
 
-    NSAssert(version.majorVersion == 14 && version.minorVersion == 1,
-             @"The tests should be run on iOS 14.1, not %ld.%ld", version.majorVersion, version.minorVersion);
+    NSAssert(version.majorVersion == 14 && version.minorVersion == 2,
+             @"The tests should be run on iOS 14.2, not %ld.%ld", version.majorVersion, version.minorVersion);
 
     CGSize nativeResolution = [UIScreen mainScreen].nativeBounds.size;
     NSAssert([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPhone && CGSizeEqualToSize(nativeResolution, CGSizeMake(1170, 2532)),
