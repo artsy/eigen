acc0cf5c9a10afdf1ba753370d4116305573d102
Pavlos Vinieratos <pvinis@gmail.com>
2020-11-30 13:02:57 +0000

    add default mock resolvers and an array generator

--- src/lib/tests/mockEnvironmentPayload.ts
+++ src/lib/tests/mockEnvironmentPayload.ts
@@ -1,7 +1,35 @@
 import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
+import { MockResolvers } from "relay-test-utils/lib/RelayMockPayloadGenerator"
 
-export type RelayMockEnvironment = ReturnType<typeof createMockEnvironment>
+const counters: { [path: string]: number } = {}
+export const generateID = (pathComponents: readonly string[] | undefined) => {
+  const path: string = pathComponents?.join(".") ?? "_GLOBAL_"
+  const currentCounter = counters[path]
+  counters[path] = currentCounter === undefined ? 1 : currentCounter + 1
+  return counters[path]
+}
 
-export const mockEnvironmentPayload = (mockEnvironment: RelayMockEnvironment, passedProps = {}) => {
-  mockEnvironment.mock.resolveMostRecentOperation((operation) => MockPayloadGenerator.generate(operation, passedProps))
+/**
+ * Used to generate results in an array.
+ * Usage: ```
+ * Artist: () => ({
+ *   auctionResultsConnection: {
+ *     edges: mockArray(10), // <- this will generate an array with 10 results, all prefilled
+ *   }
+ * })
+ * ```
+ */
+export const mockArray = (length: number) => new Array(length).fill({ node: {} })
+
+export const DefaultMockResolvers: MockResolvers = {
+  String: (ctx) => `${ctx.name}-${generateID(ctx.path)}`,
+}
+
+export const mockEnvironmentPayload = (
+  mockEnvironment: ReturnType<typeof createMockEnvironment>,
+  mockResolvers: MockResolvers = DefaultMockResolvers
+) => {
+  mockEnvironment.mock.resolveMostRecentOperation((operation) =>
+    MockPayloadGenerator.generate(operation, mockResolvers)
+  )
 }
