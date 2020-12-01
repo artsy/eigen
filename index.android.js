c73d0c7c9993dcf6e1b85ff3bdf0087b83e11541
David Sheldrick <d.j.sheldrick@gmail.com>
2020-12-01 10:12:23 +0000

    android project setup

---
+++ index.android.js
@@ -0,0 +1,8 @@
+/**
+ * @format
+ */
+
+import {AppRegistry, View, Text} from 'react-native';
+import React from 'react'
+
+AppRegistry.registerComponent('Artsy', () => () => <View style={{paddingTop: 60, paddingHorizontal: 20}}><Text>Hello</Text></View>);
