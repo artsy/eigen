514c0b3e4a6aa0ef4bc9e8679bd114b85613c2b3
David Sheldrick <d.j.sheldrick@gmail.com>
2020-12-15 11:16:40 +0000

    clean up  ConsignmentsSubmissionForm

--- src/lib/Scenes/Consignments/ConsignmentsHome/ConsignmentsSubmissionForm.tsx
+++ src/lib/Scenes/Consignments/ConsignmentsHome/ConsignmentsSubmissionForm.tsx
@@ -1,14 +1,7 @@
 import Overview from "lib/Scenes/Consignments/Screens/Overview"
-import { Theme } from "palette"
 import React from "react"
 import NavigatorIOS from "react-native-navigator-ios"
 
 export const ConsignmentsSubmissionForm: React.FC = () => {
-  const initialRoute = { component: Overview }
-
-  return (
-    <Theme>
-      <NavigatorIOS initialRoute={initialRoute} navigationBarHidden={true} style={{ flex: 1 }} />
-    </Theme>
-  )
+  return <NavigatorIOS initialRoute={{ component: Overview }} navigationBarHidden={true} style={{ flex: 1 }} />
 }
