613d02de66b369951ace4c6e28131b4b37d4c369
Anna Carey <anna.carey@artsymail.com>
2020-12-18 15:08:28 -0700

    Move send email into util function and handle users with no email client setup

---
+++ src/lib/utils/sendEmail.ts
@@ -0,0 +1,34 @@
+import _ from "lodash"
+import { Alert, Linking } from "react-native"
+
+export const sendEmail = (emailAddress: string, params?: { [key: string]: string }) => {
+  let mailToString = `mailto:${emailAddress}`
+  if (params) {
+    const paramsString = _.toPairs(params)
+      .map(([key, value]) => `${key}=${value}`)
+      .join("&")
+    mailToString += `?${paramsString}`
+  }
+
+  sendEmailWithMailTo(mailToString)
+}
+
+export const sendEmailWithMailTo = (mailToString: string) => {
+  const emailAddress = mailToString.split(":")[1].split("?")[0]
+  Linking.canOpenURL(mailToString)
+    .then((canOpenURL) => {
+      console.log("canOpenURL", canOpenURL)
+      if (canOpenURL) {
+        Linking.openURL(mailToString)
+      } else {
+        showEmailAlert(emailAddress)
+      }
+    })
+    .catch(() => {
+      showEmailAlert(emailAddress)
+    })
+}
+
+const showEmailAlert = (emailAddress: string) => {
+  Alert.alert("No email configured", `You can email ${emailAddress} to get answers to your questions.`)
+}
