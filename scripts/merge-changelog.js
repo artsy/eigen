b23ba10f5d1e5ea5d03b1244eb6f8a05b8fa6580
Pavlos Vinieratos <pvinis@gmail.com>
2020-12-18 11:59:18 +0000

    more

---
+++ scripts/merge-changelog.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+// @ts-check
+
+
+const spawnSync = require("child_process").spawnSync
+
+function sh(command, cwd) {
+ console.log("$ " + command)
+ const task = spawnSync(command, { shell: true, cwd })
+ if (task.status != 0) {
+   throw new Error("[!] " + command)
+ }
+ return task.stdout.toString()
+}
+
+sh("echo wow!")
+sh("touch testfile")
+sh("git add testfile")
