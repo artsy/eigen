e888685f959f7938282f1b90c84c4064af2ad514
dzucconi <mail@damonzucconi.com>
2020-12-21 16:04:03 -0500

    refactor: updates artwork classification screen styles

--- src/lib/Scenes/ArtworkAttributionClassFAQ/ArtworkAttributionClassFAQ.tsx
+++ src/lib/Scenes/ArtworkAttributionClassFAQ/ArtworkAttributionClassFAQ.tsx
@@ -4,7 +4,7 @@ import { goBack } from "lib/navigation/navigate"
 import { defaultEnvironment } from "lib/relay/createEnvironment"
 import renderWithLoadProgress from "lib/utils/renderWithLoadProgress"
 import { useScreenDimensions } from "lib/utils/useScreenDimensions"
-import { Box, Button, Sans, Spacer, Theme } from "palette"
+import { Box, Button, Join, Separator, Spacer, Text, Theme } from "palette"
 import React from "react"
 import { ScrollView } from "react-native"
 import { createFragmentContainer, graphql, QueryRenderer } from "react-relay"
@@ -14,36 +14,38 @@ interface Props {
 }
 
 export const ArtworkAttributionClassFAQ: React.FC<Props> = ({ artworkAttributionClasses }) => {
-  const attributionClasses = artworkAttributionClasses.map((attributionClass, index) => {
-    return (
-      <React.Fragment key={index}>
-        <Sans size="3" weight="medium">
-          {attributionClass.name}
-        </Sans>
-        <Sans size="3">{attributionClass.longDescription}</Sans>
-        <Spacer m={1} />
-      </React.Fragment>
-    )
-  })
-
   const { safeAreaInsets } = useScreenDimensions()
 
   return (
     <Theme>
       <ScrollView>
-        <Box pt={safeAreaInsets.top + 3} pb={safeAreaInsets.top + 4} px={2}>
-          <Spacer mt={3} />
-          <Sans mb={2} size="8">
-            Artwork classifications
-          </Sans>
-          {attributionClasses}
-          <Sans color="black60" size="3" mb={3}>
-            Our partners are responsible for providing accurate classification information for all works.
-          </Sans>
-          <Box height={30}>
-            <Button onPress={goBack} block>
-              OK
-            </Button>
+        <Box pt={safeAreaInsets.top} pb={safeAreaInsets.bottom} px={2}>
+          <Box my={3}>
+            <Join separator={<Spacer my={1.5} />}>
+              <Text variant="largeTitle">Artwork classifications</Text>
+
+              <Join separator={<Spacer my={1} />}>
+                {artworkAttributionClasses.map((attributionClass, index) => {
+                  return (
+                    <React.Fragment key={index}>
+                      <Text variant="mediumText">{attributionClass.name}</Text>
+
+                      <Text>{attributionClass.longDescription}</Text>
+                    </React.Fragment>
+                  )
+                })}
+              </Join>
+
+              <Separator />
+
+              <Text color="black60">
+                Our partners are responsible for providing accurate classification information for all works.
+              </Text>
+
+              <Button onPress={goBack} block>
+                OK
+              </Button>
+            </Join>
           </Box>
         </Box>
       </ScrollView>
@@ -60,17 +62,20 @@ export const ArtworkAttributionClassFAQContainer = createFragmentContainer(Artwo
   `,
 })
 
+export const ARTWORK_ATTRIBUTION_CLASS_FAQ_QUERY = graphql`
+  query ArtworkAttributionClassFAQQuery {
+    artworkAttributionClasses {
+      ...ArtworkAttributionClassFAQ_artworkAttributionClasses
+    }
+  }
+`
+
 export const ArtworkAttributionClassFAQQueryRenderer: React.SFC = (props) => {
   return (
     <QueryRenderer<ArtworkAttributionClassFAQQuery>
       environment={defaultEnvironment}
-      query={graphql`
-        query ArtworkAttributionClassFAQQuery {
-          artworkAttributionClasses {
-            ...ArtworkAttributionClassFAQ_artworkAttributionClasses
-          }
-        }
-      `}
+      // tslint:disable-next-line: relay-operation-generics
+      query={ARTWORK_ATTRIBUTION_CLASS_FAQ_QUERY}
       variables={{}}
       render={renderWithLoadProgress(ArtworkAttributionClassFAQContainer, props)}
     />
