4c3fa729d437dc3d5ccec77f241d4d3b889246cc
Pavlos Vinieratos <pvinis@gmail.com>
2021-01-11 23:58:12 +0200

    feature(share): allow instagram share (#4137)
    * adding instagram share
    
    * add touchable without feedback support
    
    * upgrade
    
    * mock
    
    * fix

--- src/__generated__/ArtworkHeader_artwork.graphql.ts
+++ src/__generated__/ArtworkHeader_artwork.graphql.ts
@@ -6,7 +6,14 @@ import { ReaderFragment } from "relay-runtime";
 import { FragmentRefs } from "relay-runtime";
 export type ArtworkHeader_artwork = {
     readonly images: ReadonlyArray<{
+        readonly url: string | null;
+        readonly imageVersions: ReadonlyArray<string | null> | null;
         readonly " $fragmentRefs": FragmentRefs<"ImageCarousel_images">;
+    } | null> | null;
+    readonly title: string | null;
+    readonly href: string | null;
+    readonly artists: ReadonlyArray<{
+        readonly name: string | null;
     } | null> | null;
     readonly " $fragmentRefs": FragmentRefs<"ArtworkActions_artwork" | "ArtworkTombstone_artwork">;
     readonly " $refType": "ArtworkHeader_artwork";
@@ -34,9 +41,55 @@ const node: ReaderFragment = {
       "plural": true,
       "selections": [
         {
+          "alias": "url",
+          "args": null,
+          "kind": "ScalarField",
+          "name": "imageURL",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "imageVersions",
+          "storageKey": null
+        },
+        {
           "args": null,
           "kind": "FragmentSpread",
           "name": "ImageCarousel_images"
+        }
+      ],
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "title",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "href",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "Artist",
+      "kind": "LinkedField",
+      "name": "artists",
+      "plural": true,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "name",
+          "storageKey": null
         }
       ],
       "storageKey": null
@@ -55,5 +108,5 @@ const node: ReaderFragment = {
   "type": "Artwork",
   "abstractKey": null
 };
-(node as any).hash = 'ab997ece8f9a2923fa7f484708ba9d65';
+(node as any).hash = 'a3f8d6363822b9ae1d416dd45c52c6a4';
 export default node;
