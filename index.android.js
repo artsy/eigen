0796715e8692079266056e6a0ef8b66061c592e1
Pavlos Vinieratos <pvinis@gmail.com>
2021-01-12 17:20:49 +0200

    possibly ok init

---
+++ index.android.js
@@ -0,0 +1,10 @@
+import { AppRegistry } from 'react-native'
+import { Text } from 'react-native'
+
+
+const App = () => (
+  <Text>Android!!</Text>
+)
+
+
+AppRegistry.registerComponent("Artsy", () => App)
