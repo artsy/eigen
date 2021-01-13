2578e13896d3b97ad713bed53fa2be636ca5771d
Pavlos Vinieratos <pvinis@gmail.com>
2021-01-13 13:26:58 +0200

    enable screens

--- index.android.js
+++ index.android.js
@@ -1,5 +1,6 @@
 global.__TEST__ = false
 require("react-native-gesture-handler")
+require("react-native-screens").enableScreens()
 import { AppRegistry } from 'react-native'
 import {App} from './src/lib/AndroidApp'
 AppRegistry.registerComponent("Artsy", () => App)
