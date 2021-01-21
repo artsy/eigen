1d42e77eadc7c150d21b9a1970456d3e0004440c
David Sheldrick <d.j.sheldrick@gmail.com>
2021-01-21 12:35:32 +0000

    add TS query cache fallback for android

--- src/lib/NativeModules/GraphQLQueryCache.ts
+++ src/lib/NativeModules/GraphQLQueryCache.ts
@@ -1,5 +1,54 @@
+import _ from "lodash"
 import { NativeModules } from "react-native"
-const { ARGraphQLQueryCache } = NativeModules
+let { ARGraphQLQueryCache } = NativeModules
+
+function stableSerialize(val: any): string {
+  if (_.isPlainObject(val)) {
+    return `{${Object.keys(val as object)
+      .sort()
+      .map((k) => `${JSON.stringify(k)}:${stableSerialize(val[k])}`)
+      .join(",")}}`
+  } else if (_.isArray(val)) {
+    return `[${val.map(stableSerialize).join(",")}]`
+  } else {
+    return JSON.stringify(val)
+  }
+}
+
+export function requestFingerprint(queryID: string, variables: object) {
+  return stableSerialize({ [queryID]: variables })
+}
+
+if (!ARGraphQLQueryCache) {
+  let cache: {
+    [requestFingerprint: string]: {
+      response: string
+      expires: number
+    }
+  } = {}
+  ARGraphQLQueryCache = {
+    _setResponseForQueryIDWithVariables(response: string, queryID: string, variables: object, ttl: number) {
+      if (ttl === 0) {
+        // default should be one day according to objc code
+        ttl = 60 * 60 * 24
+      }
+      cache[requestFingerprint(queryID, variables)] = { response, expires: Date.now() + ttl * 1000 }
+    },
+    _responseForQueryIDWithVariables(queryID: string, variables: object) {
+      const result = cache[requestFingerprint(queryID, variables)]
+      if (result && result.expires < Date.now()) {
+        return null
+      }
+      return Promise.resolve(result?.response ?? null)
+    },
+    _clearQueryIDWithVariables(queryID: string, variables: object) {
+      delete cache[requestFingerprint(queryID, variables)]
+    },
+    _clearAll() {
+      cache = {}
+    },
+  }
+}
 
 export function set(queryID: string, variables: object, response: string, ttl: number = 0): void {
   ARGraphQLQueryCache._setResponseForQueryIDWithVariables(response, queryID, variables, ttl)
