10be94a0e582e69dc31a5cb65dace62d4867e812
David Sheldrick <d.j.sheldrick@gmail.com>
2021-01-21 14:49:39 +0000

    feat(android): configure metaphysics + relay (#4352)
    * add TS query cache fallback for android
    
    * move new relay cache to separate place
    
    * set up metaphysics url middleware + associated config
    
    * update changelog
    
    * fix react-native-localize mock

--- src/lib/NativeModules/GraphQLQueryCache.ts
+++ src/lib/NativeModules/GraphQLQueryCache.ts
@@ -1,18 +1,33 @@
-import { NativeModules } from "react-native"
+import * as RelayCache from "lib/relay/RelayCache"
+import { NativeModules, Platform } from "react-native"
 const { ARGraphQLQueryCache } = NativeModules
 
 export function set(queryID: string, variables: object, response: string, ttl: number = 0): void {
+  if (Platform.OS !== "ios") {
+    RelayCache.set(queryID, variables, response, ttl)
+    return
+  }
   ARGraphQLQueryCache._setResponseForQueryIDWithVariables(response, queryID, variables, ttl)
 }
 
 export function get(queryID: string, variables: object): Promise<string | null> {
+  if (Platform.OS !== "ios") {
+    return RelayCache.get(queryID, variables)
+  }
   return ARGraphQLQueryCache._responseForQueryIDWithVariables(queryID, variables)
 }
 
 export function clear(queryID: string, variables: object): void {
+  if (Platform.OS !== "ios") {
+    RelayCache.clear(queryID, variables)
+    return
+  }
   ARGraphQLQueryCache._clearQueryIDWithVariables(queryID, variables)
 }
 
 export function clearAll(): void {
+  if (Platform.OS !== "ios") {
+    RelayCache.clearAll()
+  }
   ARGraphQLQueryCache._clearAll()
 }
