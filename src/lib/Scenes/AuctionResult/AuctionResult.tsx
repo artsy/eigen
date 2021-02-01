93398cd9e8f0b4ed9643081ed97d1afb883f8a64
Mounir Dhahri <soussouheros@gmail.com>
2021-02-01 14:50:02 +0100

    wip add mid estimate to auction result screen

--- src/lib/Scenes/AuctionResult/AuctionResult.tsx
+++ src/lib/Scenes/AuctionResult/AuctionResult.tsx
@@ -1,9 +1,7 @@
 import { OwnerType } from "@artsy/cohesion"
 import { AuctionResultQuery, AuctionResultQueryResponse } from "__generated__/AuctionResultQuery.graphql"
-import { AuctionResultsMidEstimate } from "lib/Components/AuctionResults/AuctionResultsMidEstimate"
 import { InfoButton } from "lib/Components/Buttons/InfoButton"
 import { FancyModalHeader } from "lib/Components/FancyModal/FancyModalHeader"
-import { ratioColor } from "lib/Components/Lists/AuctionResult"
 import { navigate } from "lib/navigation/navigate"
 import { defaultEnvironment } from "lib/relay/createEnvironment"
 import { PlaceholderBox } from "lib/utils/placeholders"
@@ -12,7 +10,18 @@ import { ProvideScreenTracking, Schema } from "lib/utils/track"
 import { useStickyScrollHeader } from "lib/utils/useStickyScrollHeader"
 import { capitalize } from "lodash"
 import moment from "moment"
-import { Box, Flex, NoArtworkIcon, Separator, Spacer, Text, TEXT_FONTS } from "palette"
+import {
+  Box,
+  color,
+  DecreaseIcon,
+  Flex,
+  IncreaseIcon,
+  NoArtworkIcon,
+  Separator,
+  Spacer,
+  Text,
+  TEXT_FONTS,
+} from "palette"
 import React, { useCallback, useEffect, useState } from "react"
 import { Animated, Image, TextInput, TouchableWithoutFeedback } from "react-native"
 import { graphql, QueryRenderer } from "react-relay"
@@ -74,7 +83,7 @@ const AuctionResult: React.FC<Props> = ({ artist, auctionResult }) => {
   const ratio = getRatio()
   const difference = getDifference()
 
-  const stats = []
+  const details = []
   const makeRow = (label: string, value: string, options?: { fullWidth?: boolean; testID?: string }) => (
     <Flex key={label} mb={1}>
       <Flex style={{ opacity: 0.5 }}>
@@ -122,31 +131,33 @@ const AuctionResult: React.FC<Props> = ({ artist, auctionResult }) => {
   )
   if (auctionResult.estimate?.display) {
     const { currency, estimate } = auctionResult
-    stats.push(makeRow("Estimate range", `${estimate.display} ${currency}`))
+    details.push(makeRow("Estimate range", `${estimate.display} ${currency}`))
   }
   if (auctionResult.mediumText) {
-    stats.push(makeRow("Medium", capitalize(auctionResult.mediumText)))
+    details.push(makeRow("Medium", capitalize(auctionResult.mediumText)))
   }
   if (auctionResult.dimensionText) {
-    stats.push(makeRow("Dimensions", auctionResult.dimensionText))
+    details.push(makeRow("Dimensions", auctionResult.dimensionText))
   }
   if (auctionResult.dateText) {
-    stats.push(makeRow("Year created", auctionResult.dateText))
+    details.push(makeRow("Year created", auctionResult.dateText))
   }
   if (auctionResult.saleDate) {
-    stats.push(makeRow("Sale date", moment(auctionResult.saleDate).utc().format("MMM D, YYYY"), { testID: "saleDate" }))
+    details.push(
+      makeRow("Sale date", moment(auctionResult.saleDate).utc().format("MMM D, YYYY"), { testID: "saleDate" })
+    )
   }
   if (auctionResult.organization) {
-    stats.push(makeRow("Auction house", auctionResult.organization))
+    details.push(makeRow("Auction house", auctionResult.organization))
   }
   if (auctionResult.saleTitle) {
-    stats.push(makeRow("Sale name", auctionResult.saleTitle))
+    details.push(makeRow("Sale name", auctionResult.saleTitle))
   }
   if (auctionResult.location) {
-    stats.push(makeRow("Sale location", auctionResult.location))
+    details.push(makeRow("Sale location", auctionResult.location))
   }
   if (auctionResult.description) {
-    stats.push(makeRow("Description", auctionResult.description, { fullWidth: true }))
+    details.push(makeRow("Description", auctionResult.description, { fullWidth: true }))
   }
 
   const hasSalePrice = !!auctionResult.priceRealized?.display && !!auctionResult.currency
@@ -217,42 +228,62 @@ const AuctionResult: React.FC<Props> = ({ artist, auctionResult }) => {
               />
             </Flex>
           )}
-          <AuctionResultsMidEstimate percentage="%20" />
-          <Text variant="largeTitle">
-            {hasSalePrice ? `${auctionResult.priceRealized?.display} ${auctionResult.currency}` : salePriceMessage}
-          </Text>
-          {!!ratio && (
-            <Flex flexDirection="row" mt={1}>
-              <Flex borderRadius={2} overflow="hidden">
-                <Flex
-                  position="absolute"
-                  width="100%"
-                  height="100%"
-                  backgroundColor={ratioColor(ratio)}
-                  opacity={0.1}
-                />
-                <Text testID="ratio" variant="mediumText" color={ratioColor(ratio)} px="5px">
-                  {ratio.toFixed(2)}x{" "}
-                  {!!difference &&
-                    `(${difference > 0 ? "+" : ""}${new Intl.NumberFormat().format(difference)} ${
-                      auctionResult.currency
-                    })`}
-                </Text>
-              </Flex>
-              <Text variant="text" color="black60" ml={1}>
-                low estimate
-              </Text>
-            </Flex>
+          {hasSalePrice ? (
+            <>
+              <Text
+                variant="largeTitle"
+                mb={0.5}
+              >{`${auctionResult.priceRealized?.display} ${auctionResult.currency}`}</Text>
+              <AuctionResultsMidEstimate percentage="%20" />
+            </>
+          ) : (
+            <Text variant="largeTitle">{salePriceMessage}</Text>
           )}
+
           <Text variant="title" mt={4} mb={1}>
-            Stats
+            Details
           </Text>
-          {stats}
+          {details}
         </Box>
       </Animated.ScrollView>
       {headerElement}
     </ProvideScreenTracking>
   )
+}
+
+interface AuctionResultsMidEstimateProps {
+  percentage: string
+}
+
+type ArrowDirections = "up" | "down"
+
+const AuctionResultsMidEstimate: React.FC<AuctionResultsMidEstimateProps> = ({ percentage }) => {
+  const arrowDirection: ArrowDirections = percentage[0] !== "-" ? "up" : "down"
+
+  return (
+    <Flex flexDirection="row" alignItems="center">
+      {arrowDirection === "up" ? (
+        <IncreaseIcon bottom={"2px"} height={12} fill={"green100"} />
+      ) : (
+        <DecreaseIcon bottom={"2px"} height={12} fill={"red100"} />
+      )}
+      <Text variant="mediumText" color={ratioColor(percentage)}>
+        {percentage} mid-estimate
+      </Text>
+    </Flex>
+  )
+}
+
+export const ratioColor = (ratioString: string) => {
+  const ratio = Number(ratioString.replace("%", ""))
+  if (ratio >= 5) {
+    return "green100"
+  }
+  if (ratio <= -5) {
+    return "red100"
+  }
+
+  return "black60"
 }
 
 export const AuctionResultQueryRenderer: React.FC<{
@@ -322,9 +353,9 @@ export const AuctionResultQueryRenderer: React.FC<{
 }
 
 const LoadingSkeleton = () => {
-  const stats = []
+  const details = []
   for (let i = 0; i < 8; i++) {
-    stats.push(
+    details.push(
       <Flex flexDirection="row" justifyContent="space-between" mb={2} key={i}>
         <PlaceholderBox width={CONTAINER_HEIGHT + Math.round(Math.random() * CONTAINER_HEIGHT)} height={20} />
         <PlaceholderBox width={CONTAINER_HEIGHT + Math.round(Math.random() * CONTAINER_HEIGHT)} height={20} />
@@ -356,10 +387,10 @@ const LoadingSkeleton = () => {
       {/* Ratio */}
       <PlaceholderBox width={200} height={20} />
       <Spacer mb={4} />
-      {/* "Stats" */}
+      {/* "details" */}
       <PlaceholderBox width={60} height={30} />
       <Spacer mb={2} />
-      {stats}
+      {details}
     </Flex>
   )
 }
--- src/lib/Scenes/AuctionResult/__tests__/AuctionResult-tests.tsx
+++ src/lib/Scenes/AuctionResult/__tests__/AuctionResult-tests.tsx
@@ -42,19 +42,19 @@ describe("AuctionResult", () => {
     expect(tree.root.findAllByType(FancyModalHeader)).toHaveLength(2)
   })
 
-  it("calculates the ratio correctly", () => {
-    const tree = renderAuctionResult({
-      AuctionResult: () => ({
-        estimate: {
-          low: 200,
-        },
-        priceRealized: {
-          cents: 450,
-        },
-      }),
-    })
-    expect(extractText(tree.root.findByProps({ testID: "ratio" }))).toContain("2.25x")
-  })
+  // it("show the mid-estimate", () => {
+  //   const tree = renderAuctionResult({
+  //     AuctionResult: () => ({
+  //       estimate: {
+  //         low: 200,
+  //       },
+  //       priceRealized: {
+  //         cents: 450,
+  //       },
+  //     }),
+  //   })
+  //   expect(extractText(tree.root.findByProps({ testID: "ratio" }))).toContain("2.25x")
+  // })
 
   it("shows if a lot was bought in", () => {
     const tree = renderAuctionResult({
