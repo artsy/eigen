9df779f4525e365bd596e5ac9cbb602ff1c3cb94
Mounir Dhahri <soussouheros@gmail.com>
2021-02-01 14:05:53 +0100

    run yarn relay

--- src/__generated__/CityBMWList_city.graphql.ts
+++ src/__generated__/CityBMWList_city.graphql.ts
@@ -5,8 +5,8 @@
 import { ReaderFragment } from "relay-runtime";
 import { FragmentRefs } from "relay-runtime";
 export type CityBMWList_city = {
-    readonly name: string | null;
-    readonly slug: string | null;
+    readonly name: string;
+    readonly slug: string;
     readonly sponsoredContent: {
         readonly shows: {
             readonly edges: ReadonlyArray<{
