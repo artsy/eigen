fa9eef4ec61d7094f54783bda1a0a98051caee7a
Lily Pacegit <lilyfpace@gmail.com>
2021-02-02 09:39:02 -0500

    fixed keyboard issue

--- src/lib/Scenes/Inbox/Screens/Checkout.tsx
+++ src/lib/Scenes/Inbox/Screens/Checkout.tsx
@@ -4,7 +4,7 @@ import { dismissModal } from "lib/navigation/navigate"
 import { getCurrentEmissionState } from "lib/store/GlobalStore"
 import { useScreenDimensions } from "lib/utils/useScreenDimensions"
 import React, { useState } from "react"
-import { View } from "react-native"
+import { KeyboardAvoidingView, View } from "react-native"
 import WebView from "react-native-webview"
 
 export const Checkout: React.FC<{ orderID: string }> = ({ orderID }) => {
@@ -13,28 +13,34 @@ export const Checkout: React.FC<{ orderID: string }> = ({ orderID }) => {
 
   const [isLoading, setLoading] = useState(false)
   return (
-    <View style={{ flex: 1, flexDirection: "column" }}>
-      <FancyModalHeader
-        onLeftButtonPress={() => {
-          dismissModal()
-        }}
-        leftButtonText="Cancel"
-        rightButtonDisabled
-        rightButtonText=" "
-      >
-        Make Offer
-      </FancyModalHeader>
-      <WebView
-        onLoadStart={() => {
-          setLoading(true)
-        }}
-        onLoadEnd={() => {
-          setLoading(false)
-        }}
-        source={{ uri: webCheckoutUrl }}
-        style={{ marginTop: paddingTop, flex: 1 }}
-      />
-      {!!isLoading && <Spinner style={{ flex: 1, marginBottom: "60%" }} />}
-    </View>
+    <KeyboardAvoidingView
+      behavior="padding"
+      keyboardVerticalOffset={useScreenDimensions().safeAreaInsets.top}
+      style={{ flex: 1 }}
+    >
+      <View style={{ flex: 1, flexDirection: "column" }}>
+        <FancyModalHeader
+          onLeftButtonPress={() => {
+            dismissModal()
+          }}
+          leftButtonText="Cancel"
+          rightButtonDisabled
+          rightButtonText=" "
+        >
+          Make Offer
+        </FancyModalHeader>
+        <WebView
+          onLoadStart={() => {
+            setLoading(true)
+          }}
+          onLoadEnd={() => {
+            setLoading(false)
+          }}
+          source={{ uri: webCheckoutUrl }}
+          style={{ marginTop: paddingTop, flex: 1 }}
+        />
+        {!!isLoading && <Spinner style={{ flex: 1, marginBottom: "60%" }} />}
+      </View>
+    </KeyboardAvoidingView>
   )
 }
