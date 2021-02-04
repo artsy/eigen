3b53e404215bd3b98a03c181dd98bad9526b9ab2
David Sheldrick <d.j.sheldrick@gmail.com>
2021-02-04 12:09:50 +0000

    add feature flag tests
    Co-authored-by: Mounir Dhahri <mounir.dhahri@ensi-uma.tn>

--- src/lib/store/__tests__/ConfigModel-tests.ts
+++ src/lib/store/__tests__/ConfigModel-tests.ts
@@ -2,18 +2,37 @@ import fetchMock from "jest-fetch-mock"
 import { flushPromiseQueue } from "lib/tests/flushPromiseQueue"
 import echoLaunchJSON from "../../../../Artsy/App/EchoNew.json"
 import { ConfigModel } from "../ConfigModel"
+import { FeatureDescriptor, features } from "../features"
 import { __globalStoreTestUtils__, GlobalStore } from "../GlobalStore"
+
+type TestFeatures = "FeatureA" | "FeatureB"
 
 jest.mock("../../../../Artsy/App/EchoNew.json", () => {
   const echo: Partial<ConfigModel["echoState"]> = {
     updated_at: "2021-02-04T11:10:33.768Z",
     features: [
-      { name: "FeatureA", value: true },
-      { name: "FeatureB", value: false },
+      { name: "FeatureAEchoKey", value: true },
+      { name: "FeatureBEchoKey", value: false },
     ],
   }
 
   return echo
+})
+
+jest.mock("lib/store/features", () => {
+  const mockFeatures: { readonly [key: string]: FeatureDescriptor } = {
+    FeatureA: {
+      readyForRelease: true,
+      echoFlagKey: "FeatureAEchoKey",
+    },
+    FeatureB: {
+      readyForRelease: false,
+      echoFlagKey: "FeatureBEchoKey",
+    },
+  }
+  return {
+    features: mockFeatures,
+  }
 })
 
 beforeEach(() => {
@@ -21,8 +40,61 @@ beforeEach(() => {
 })
 
 const getEchoState = () => {
-  return __globalStoreTestUtils__?.getCurrentState().config.echoState
+  return __globalStoreTestUtils__?.getCurrentState().config.echoState!
 }
+
+const getComputedFeatures = () => {
+  return (__globalStoreTestUtils__?.getCurrentState().config.features! as any) as Record<TestFeatures, boolean>
+}
+
+describe("Feature flags", () => {
+  it("are taken from the features definition map and turned into a computed boolean map in the global store", () => {
+    expect(Object.keys(getComputedFeatures()).sort()).toEqual(Object.keys(features).sort())
+    expect(typeof getComputedFeatures().FeatureA).toBe("boolean")
+  })
+  it("are true if the feature is ready for release, and false if not", () => {
+    expect(getComputedFeatures().FeatureA).toBe(true)
+    expect(getComputedFeatures().FeatureB).toBe(false)
+  })
+  it("support admin overrides", () => {
+    expect(getComputedFeatures().FeatureA).toBe(true)
+    GlobalStore.actions.config.setAdminOverride({ key: "FeatureA" as any, value: false })
+    expect(getComputedFeatures().FeatureA).toBe(false)
+    GlobalStore.actions.config.setAdminOverride({ key: "FeatureA" as any, value: null })
+    expect(getComputedFeatures().FeatureA).toBe(true)
+
+    expect(getComputedFeatures().FeatureB).toBe(false)
+    GlobalStore.actions.config.setAdminOverride({ key: "FeatureB" as any, value: true })
+    expect(getComputedFeatures().FeatureB).toBe(true)
+    GlobalStore.actions.config.setAdminOverride({ key: "FeatureB" as any, value: null })
+    expect(getComputedFeatures().FeatureB).toBe(false)
+  })
+  it("use echo if an echo flag is given and the feature is ready for release", () => {
+    expect(getComputedFeatures().FeatureA).toBe(true)
+    // set A's echo flag to false
+    GlobalStore.actions.config.setEchoState({
+      ...echoLaunchJSON,
+      features: [
+        { name: "FeatureAEchoKey", value: false },
+        { name: "FeatureBEchoKey", value: false },
+      ],
+    })
+    expect(getComputedFeatures().FeatureA).toBe(false)
+  })
+
+  it("ignores echo if feature is not ready for release", () => {
+    expect(getComputedFeatures().FeatureB).toBe(false)
+    // set A's echo flag to false
+    GlobalStore.actions.config.setEchoState({
+      ...echoLaunchJSON,
+      features: [
+        { name: "FeatureAEchoKey", value: false },
+        { name: "FeatureBEchoKey", value: true },
+      ],
+    })
+    expect(getComputedFeatures().FeatureB).toBe(false)
+  })
+})
 
 describe("Echo", () => {
   it("uses the bundled version when the app loads", () => {
