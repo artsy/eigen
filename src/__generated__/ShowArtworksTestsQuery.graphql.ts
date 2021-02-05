4896b283100a8aa5223c56c220f3186d09a7d0bc
artsyit <it@artsymail.com>
2021-02-05 22:14:27 +0000

    Update metaphysics schema

--- src/__generated__/ShowArtworksTestsQuery.graphql.ts
+++ src/__generated__/ShowArtworksTestsQuery.graphql.ts
@@ -719,6 +719,7 @@ return {
             "MAJOR_PERIOD",
             "MEDIUM",
             "MERCHANDISABLE_ARTISTS",
+            "PARTNER",
             "PARTNER_CITY",
             "PERIOD",
             "PRICE_RANGE",
