d66f3e4c1940c2647e288bee0db96c6861679792
David Sheldrick <d.j.sheldrick@gmail.com>
2021-02-05 10:38:37 +0000

    feat(android): set up basic deep link handling (#4402)
    * set up basic deep link handling
    
    * update changelog

--- src/lib/AndroidApp.tsx
+++ src/lib/AndroidApp.tsx
@@ -1,10 +1,11 @@
 import { GlobalStore, GlobalStoreProvider } from "lib/store/GlobalStore"
 import { Theme } from "palette"
-import React from "react"
-import { View } from "react-native"
+import React, { useEffect } from "react"
+import { Linking, View } from "react-native"
 import track from "react-tracking"
 import { LogIn } from "./LogIn/LogIn"
 import { ModalStack } from "./navigation/ModalStack"
+import { navigate } from "./navigation/navigate"
 import { BottomTabsNavigator } from "./Scenes/BottomTabs/BottomTabsNavigator"
 import { AdminMenuWrapper } from "./utils/AdminMenuWrapper"
 import { ProvideScreenDimensions } from "./utils/useScreenDimensions"
@@ -12,6 +13,14 @@ import { ProvideScreenDimensions } from "./utils/useScreenDimensions"
 const Main: React.FC<{}> = track()(({}) => {
   const isHydrated = GlobalStore.useAppState((state) => state.sessionState.isHydrated)
   const isLoggedIn = GlobalStore.useAppState((state) => !!state.auth.userAccessToken)
+
+  useEffect(() => {
+    // TODO: handle opening the app from deep link (Linking.getInitialURL)
+    Linking.addEventListener("url", ({ url }) => navigate(url))
+    return () => {
+      Linking.removeAllListeners("url")
+    }
+  }, [])
 
   if (!isHydrated) {
     return <View></View>
