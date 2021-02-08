d7d7f990b4ecc6812ce58117a0a43536fe52e34b
Mounir Dhahri <soussouheros@gmail.com>
2021-02-08 11:35:28 +0100

    chore: use @react-native-community/picker

--- src/lib/Components/Bidding/Components/MaxBidPicker.tsx
+++ src/lib/Components/Bidding/Components/MaxBidPicker.tsx
@@ -1,22 +1,21 @@
+import { Picker } from "@react-native-community/picker"
 import React from "react"
-import { Picker, PickerProperties } from "react-native"
+
 import styled from "styled-components/native"
 
-export interface MaxBidPickerProps extends PickerProperties {
+export interface MaxBidPickerProps {
   bids: ReadonlyArray<{ display: string }>
+  onValueChange: (itemValue: any, itemPosition: number) => void
+  selectedValue: string | number
 }
 
-export class MaxBidPicker extends React.Component<MaxBidPickerProps> {
-  render() {
-    return (
-      <StyledPicker {...this.props} onValueChange={this.props.onValueChange} selectedValue={this.props.selectedValue}>
-        {this.props.bids.map((bid, index) => (
-          <Picker.Item key={index} value={index} label={bid.display} />
-        ))}
-      </StyledPicker>
-    )
-  }
-}
+export const MaxBidPicker: React.FC<MaxBidPickerProps> = ({ bids, onValueChange, selectedValue, ...rest }) => (
+  <StyledPicker {...rest} onValueChange={onValueChange} selectedValue={selectedValue}>
+    {bids.map((bid, index) => (
+      <Picker.Item key={index} value={index} label={bid.display} />
+    ))}
+  </StyledPicker>
+)
 
 const StyledPicker = styled.Picker`
   width: 100%;
