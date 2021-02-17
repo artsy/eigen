a19554027aafb113fbc2a55fa66e6f75797633e5
Adam Butler <ab@adam-butler.com>
2021-02-17 11:45:45 +0100

    fix: encode subject and body

--- src/lib/NativeModules/presentEmailComposer.ts
+++ src/lib/NativeModules/presentEmailComposer.ts
@@ -1,9 +1,10 @@
+import { stringify } from "qs"
 import { Linking, Platform } from "react-native"
 import { LegacyNativeModules } from "./LegacyNativeModules"
 
 export function presentEmailComposer(toAddress: string, subject: string, body?: string) {
   if (Platform.OS !== "ios") {
-    Linking.openURL(`mailto:${toAddress}?subject=${subject}${body ? "&body=" + body : ""}`)
+    Linking.openURL(`mailto:${toAddress}?${stringify({ subject, body })}`)
   } else if (body) {
     LegacyNativeModules.ARScreenPresenterModule.presentEmailComposerWithBody(body, subject, toAddress)
   } else {
