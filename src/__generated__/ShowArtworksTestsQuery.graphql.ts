d7524491caf447614ba70e0f1b79e237f57b8918
David Sheldrick <d.j.sheldrick@gmail.com>
2021-02-23 12:14:46 +0000

    run yarn relay

--- src/__generated__/ShowArtworksTestsQuery.graphql.ts
+++ src/__generated__/ShowArtworksTestsQuery.graphql.ts
@@ -711,6 +711,7 @@ return {
             "FOLLOWED_ARTISTS",
             "GALLERY",
             "INSTITUTION",
+            "LOCATION_CITY",
             "MAJOR_PERIOD",
             "MEDIUM",
             "MERCHANDISABLE_ARTISTS",
--- src/__generated__/ShowTestsQuery.graphql.ts
+++ src/__generated__/ShowTestsQuery.graphql.ts
@@ -1428,6 +1428,7 @@ return {
             "FOLLOWED_ARTISTS",
             "GALLERY",
             "INSTITUTION",
+            "LOCATION_CITY",
             "MAJOR_PERIOD",
             "MEDIUM",
             "MERCHANDISABLE_ARTISTS",
