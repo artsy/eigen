9fca96d52ebb31a0bc4fe927753dee7ba4b1f05a
Adam Butler <ab@adam-butler.com>
2021-03-02 14:28:36 +0100

    fix: back button on last 2 consignments screens

--- src/lib/Scenes/Consignments/Screens/Confirmation.tsx
+++ src/lib/Scenes/Consignments/Screens/Confirmation.tsx
@@ -1,10 +1,11 @@
+import { StackNavigationProp } from "@react-navigation/stack"
 import Spinner from "lib/Components/Spinner"
 import { dismissModal, navigate } from "lib/navigation/navigate"
 import NavigatorIOS from "lib/utils/__legacy_do_not_use__navigator-ios-shim"
 import { Schema, screenTrack } from "lib/utils/track"
 import { Box, Button, color, Flex, Sans, Spacer } from "palette"
 import React from "react"
-import { View, ViewProperties } from "react-native"
+import { Alert, BackHandler, NativeEventSubscription, View, ViewProperties } from "react-native"
 import styled from "styled-components/native"
 
 interface Props extends ViewProperties {
@@ -38,6 +39,7 @@ const Container = styled.View`
   context_screen_owner_type: Schema.OwnerEntityTypes.Consignment,
 })
 export default class Confirmation extends React.Component<Props, State> {
+  backButtonListener?: NativeEventSubscription = undefined
   constructor(props: Props) {
     super(props)
     this.state = {
@@ -45,8 +47,23 @@ export default class Confirmation extends React.Component<Props, State> {
     }
 
     if (this.state.submissionState === SubmissionTypes.Submitting && props.submissionRequestValidationCheck) {
-      setTimeout(this.checkForSubmissionStatus, 1000)
+      setTimeout(this.checkForSubmissionStatus.bind(this), 1000)
     }
+  }
+
+  componentDidMount = () => {
+    this.backButtonListener = BackHandler.addEventListener("hardwareBackPress", this.handleBackButton.bind(this))
+  }
+
+  componentWillUnmount = () => {
+    if (this.backButtonListener) {
+      this.backButtonListener.remove()
+    }
+  }
+
+  handleBackButton = () => {
+    dismissModal()
+    return true
   }
 
   checkForSubmissionStatus = () => {
