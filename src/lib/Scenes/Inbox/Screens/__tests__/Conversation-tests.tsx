2bf9792ae06efcc1974878d634a140510cf65756
Lily Pacegit <lilyfpace@gmail.com>
2021-03-15 12:43:19 -0400

    removed redindant tests

--- src/lib/Scenes/Inbox/Screens/__tests__/Conversation-tests.tsx
+++ src/lib/Scenes/Inbox/Screens/__tests__/Conversation-tests.tsx
@@ -1,3 +1,4 @@
+import { ConversationTestsQuery } from "__generated__/ConversationTestsQuery.graphql"
 import ConnectivityBanner from "lib/Components/ConnectivityBanner"
 import Composer from "lib/Scenes/Inbox/Components/Conversations/Composer"
 import { renderWithWrappers } from "lib/tests/renderWithWrappers"
@@ -7,7 +8,6 @@ import "react-native"
 import { graphql, QueryRenderer } from "react-relay"
 import { act, ReactTestInstance } from "react-test-renderer"
 import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
-import { ConversationTestsQuery } from "__generated__/ConversationTestsQuery.graphql"
 import { Conversation, ConversationFragmentContainer } from "../Conversation"
 import { ConversationDetailsQueryRenderer } from "../ConversationDetails"
 
@@ -94,118 +94,3 @@ it("clicking on detail link opens pushes detail screen into navigator", () => {
     title: "",
   })
 })
-
-xdescribe("isOfferableFromInquiry prop", () => {
-  describe("artwork is offerable from inquiry and there is no submitted order", () => {
-    it("is true", () => {
-      const conversation = renderWithWrappers(
-        <ConversationFragmentContainer me={props as any} navigator={mockNavigator as any} />
-      )
-      expect(conversation.root.findByType(Composer).props.isOfferableFromInquiry).toBe(true)
-    })
-  })
-
-  describe("artwork is not offerable from inquiry and there is no submitted order", () => {
-    it("is false", () => {
-      props.conversation.items[0].item.isOfferableFromInquiry = false
-
-      const conversation = renderWithWrappers(
-        <ConversationFragmentContainer me={props as any} navigator={mockNavigator as any} />
-      )
-      expect(conversation.root.findByType(Composer).props.isOfferableFromInquiry).toBe(false)
-    })
-  })
-
-  describe("artwork is offerable from inquiry and there is a submitted order", () => {
-    it("is false", () => {
-      props.conversation.submittedOrderConnection.edges = [
-        {
-          node: {
-            internalID: "401",
-            state: "SUBMITTED",
-          },
-        },
-      ]
-
-      const conversation = renderWithWrappers(
-        <ConversationFragmentContainer me={props as any} navigator={mockNavigator as any} />
-      )
-      expect(conversation.root.findByType(Composer).props.isOfferableFromInquiry).toBe(false)
-    })
-  })
-
-  describe("artwork is not offerable from inquiry and there is a submitted order", () => {
-    it("is false", () => {
-      props.conversation.items[0].item.isOfferableFromInquiry = false
-      props.conversation.submittedOrderConnection.edges = [
-        {
-          node: {
-            internalID: "401",
-            state: "SUBMITTED",
-          },
-        },
-      ]
-
-      const conversation = renderWithWrappers(
-        <ConversationFragmentContainer me={props as any} navigator={mockNavigator as any} />
-      )
-      expect(conversation.root.findByType(Composer).props.isOfferableFromInquiry).toBe(false)
-    })
-  })
-})
-
-const props = {
-  initials: "JC",
-  conversation: {
-    gravityID: "conversation-420",
-    internalID: "123",
-    id: "420",
-    from: {
-      name: "Anita Garibaldi",
-      email: "anita@garibaldi.br",
-      initials: "AG",
-    },
-    to: { name: "Kimberly Klark", initials: "KK" },
-    unread: false,
-    initial_message: "Adoro! Por favor envie-me mais informações",
-    last_message_id: "222",
-    messages: {
-      edges: [
-        {
-          node: {
-            gravityID: "unique-id",
-            id: 222,
-            body: "Adoro! Por favor envie-me mais informações",
-            from_email_address: "anita@garibaldi.br",
-            attachments: [],
-            from: {
-              name: "Percy",
-              email: "percy@cat.com",
-            },
-          },
-        },
-      ],
-    },
-    items: [
-      {
-        title: "The Mythic Being: Sol’s Drawing #1–5",
-        item: {
-          __typename: "Artwork",
-          id: "adrian-piper-the-mythic-being-sols-drawing-number-1-5",
-          href: "/artwork/adrian-piper-the-mythic-being-sols-drawing-number-1-5",
-          title: "The Mythic Being: Sol’s Drawing #1–5",
-          date: "1974",
-          artist_names: "Adrian Piper",
-          isOfferableFromInquiry: true,
-          image: {
-            url: "https://d32dm0rphc51dk.cloudfront.net/W1FkNoM9IjrND_xv_DTkeg/normalized.jpg",
-            image_url: "https://d32dm0rphc51dk.cloudfront.net/J0uofgV9e8cIxGiZwn12mg/:version.jpg",
-          },
-        },
-      },
-    ],
-    submittedOrderConnection: {
-      edges: [] as any[],
-    },
-  },
-}
