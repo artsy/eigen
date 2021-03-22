fe7759180ca38e787bea791048e5af498c7eedba
Mounir Dhahri <soussouheros@gmail.com>
2021-03-22 19:40:35 +0100

    add onboarding navigation infra

---
+++ src/lib/Scenes/Onboarding/OnboardingCreateAccount.tsx
@@ -0,0 +1,10 @@
+import { Flex, Text } from "palette"
+import React from "react"
+
+export const OnboardingCreateAccount = () => {
+  return (
+    <Flex flex={1} justifyContent="center" alignItems="center">
+      <Text variant="largeTitle">Create account</Text>
+    </Flex>
+  )
+}
