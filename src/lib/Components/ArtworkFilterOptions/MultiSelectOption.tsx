72335d2d8a1a1963c8b7decb937d0e4680a4688b
dzucconi <mail@damonzucconi.com>
2021-04-06 16:30:50 -0400

    feat(filters): updates filter styling behind flag

--- src/lib/Components/ArtworkFilterOptions/MultiSelectOption.tsx
+++ src/lib/Components/ArtworkFilterOptions/MultiSelectOption.tsx
@@ -2,11 +2,12 @@ import { ParamListBase } from "@react-navigation/native"
 import { StackNavigationProp } from "@react-navigation/stack"
 import { useFeatureFlag } from "lib/store/GlobalStore"
 import { FilterData } from "lib/utils/ArtworkFilter/ArtworkFiltersStore"
-import { Box, Check, Flex, Sans, Separator, Text, Touchable } from "palette"
+import { Box, Check, Flex, Sans, Separator, Text } from "palette"
 import React from "react"
-import { FlatList, TouchableWithoutFeedback } from "react-native"
+import { FlatList } from "react-native"
 import styled from "styled-components/native"
 import { FancyModalHeader } from "../FancyModal/FancyModalHeader"
+import { TouchableRow } from "../TouchableRow"
 import { FilterToggleButton } from "./FilterToggleButton"
 
 interface MultiSelectOptionScreenProps {
@@ -56,12 +57,12 @@ export const MultiSelectOptionScreen: React.FC<MultiSelectOptionScreenProps> = (
           style={{ flex: 1 }}
           keyExtractor={(_item, index) => String(index)}
           data={filterOptions}
-          ItemSeparatorComponent={Separator}
+          ItemSeparatorComponent={shouldUseImprovedArtworkFilters ? null : Separator}
           renderItem={({ item }) => {
             return (
               <Box ml={0.5}>
                 {shouldUseImprovedArtworkFilters ? (
-                  <Touchable
+                  <TouchableRow
                     onPress={() => {
                       const currentParamValue = item.paramValue as boolean
                       onSelect(item, !currentParamValue)
@@ -74,7 +75,7 @@ export const MultiSelectOptionScreen: React.FC<MultiSelectOptionScreenProps> = (
 
                       <Check selected={itemIsSelected(item)} disabled={itemIsDisabled(item)} />
                     </OptionListItem>
-                  </Touchable>
+                  </TouchableRow>
                 ) : (
                   <OptionListItem>
                     <Flex mb={0.5}>
@@ -82,16 +83,15 @@ export const MultiSelectOptionScreen: React.FC<MultiSelectOptionScreenProps> = (
                         {item.displayText}
                       </Sans>
                     </Flex>
-                    <TouchableWithoutFeedback>
-                      <FilterToggleButton
-                        onChange={() => {
-                          const currentParamValue = item.paramValue as boolean
-                          onSelect(item, !currentParamValue)
-                        }}
-                        value={itemIsSelected(item)}
-                        disabled={itemIsDisabled(item)}
-                      />
-                    </TouchableWithoutFeedback>
+
+                    <FilterToggleButton
+                      onChange={() => {
+                        const currentParamValue = item.paramValue as boolean
+                        onSelect(item, !currentParamValue)
+                      }}
+                      value={itemIsSelected(item)}
+                      disabled={itemIsDisabled(item)}
+                    />
                   </OptionListItem>
                 )}
               </Box>
