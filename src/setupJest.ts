bedd201a7a5a951f87eb5e648d288c6a37e8b801
Mounir Dhahri <soussouheros@gmail.com>
2021-04-07 11:00:06 +0200

    update changelog and fix test

--- src/setupJest.ts
+++ src/setupJest.ts
@@ -89,8 +89,9 @@ beforeEach(() => {
   require("@react-native-cookies/cookies").clearAll.mockReset()
 })
 
+// prettier-ignore
 // tslint:disable-next-line:no-empty
-jest.mock("@sentry/react-native", () => ({ captureMessage() {}, init() {}, setUser() {}, addBreadcrumb() {} }))
+jest.mock("@sentry/react-native", () => ({ captureMessage() {},  init() {},  setUser() {},  addBreadcrumb() {},  withScope() {} }))
 
 // Needing to mock react-native-scrollable-tab-view due to Flow issue
 jest.mock("react-native-scrollable-tab-view", () => jest.fn(() => null))
