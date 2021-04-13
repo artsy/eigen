6feb972adc8f18a477d210e1eec34f9d1e1e6faf
Pavlos Vinieratos <pvinis@gmail.com>
2021-04-13 14:30:16 +0100

    working slowly

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,12 @@ const { FileStore } = require("metro-cache")
 module.exports = {
   cacheStores: [new FileStore({ root: "./.metro" })],
   maxWorkers: process.env.CI == "true" ? 0 : 4,
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
 }
