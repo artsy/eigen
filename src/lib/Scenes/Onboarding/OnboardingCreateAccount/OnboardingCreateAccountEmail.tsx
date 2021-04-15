842fe599e008be136370952c3a99a65586a6f560
Mounir Dhahri <soussouheros@gmail.com>
2021-04-15 12:17:04 +0200

    wrap create email screen with scrollview

--- src/lib/Scenes/Onboarding/OnboardingCreateAccount/OnboardingCreateAccountEmail.tsx
+++ src/lib/Scenes/Onboarding/OnboardingCreateAccount/OnboardingCreateAccountEmail.tsx
@@ -3,8 +3,9 @@ import { useFormikContext } from "formik"
 import { Input } from "lib/Components/Input/Input"
 import { BackButton } from "lib/navigation/BackButton"
 import { useScreenDimensions } from "lib/utils/useScreenDimensions"
-import { color, Flex, Spacer, Text } from "palette"
+import { Box, color, Flex, Spacer, Text } from "palette"
 import React from "react"
+import { ScrollView } from "react-native"
 import { OnboardingCreateAccountNavigationStack, UserSchema } from "./OnboardingCreateAccount"
 
 export interface OnboardingCreateAccountEmailParams {
@@ -15,20 +16,39 @@ interface OnboardingCreateAccountEmailProps
   extends StackScreenProps<OnboardingCreateAccountNavigationStack, "OnboardingCreateAccountEmail"> {}
 
 export const OnboardingCreateAccountEmail: React.FC<OnboardingCreateAccountEmailProps> = ({ route }) => {
-  const { values, handleSubmit, handleChange, validateForm, errors } = useFormikContext<UserSchema>()
+  const { values, handleSubmit, handleChange, validateForm, errors, setErrors } = useFormikContext<UserSchema>()
 
   return (
-    <Flex alignItems="center" backgroundColor="white" flexGrow={1} pt={useScreenDimensions().safeAreaInsets.top}>
-      <BackButton onPress={route.params.navigateToWelcomeScreen} />
-      <Flex px={1.5} paddingTop={60} justifyContent="flex-start" width="100%">
-        <Text variant="largeTitle">Log in with email</Text>
-        <Spacer mt={120} />
+    <Flex backgroundColor="white" flexGrow={1}>
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+          paddingTop: useScreenDimensions().safeAreaInsets.top,
+          justifyContent: "flex-start",
+        }}
+        showsVerticalScrollIndicator={false}
+        keyboardDismissMode="on-drag"
+        keyboardShouldPersistTaps="always"
+      >
+        <BackButton onPress={route.params.navigateToWelcomeScreen} />
+        <Spacer mt={60} />
+        <Box height={130}>
+          <Text variant="largeTitle">Sign up with email</Text>
+        </Box>
+        <Spacer mt={50} />
         <Input
           autoCapitalize="none"
           autoCompleteType="email"
           enableClearButton
+          autoFocus
           keyboardType="email-address"
           onChangeText={(text) => {
+            // Hide error when the user starts to type again
+            if (errors.email) {
+              setErrors({
+                email: undefined,
+              })
+            }
             handleChange("email")(text.trim())
           }}
           onSubmitEditing={() => {
@@ -37,20 +57,17 @@ export const OnboardingCreateAccountEmail: React.FC<OnboardingCreateAccountEmail
           onBlur={() => {
             validateForm()
           }}
-          blurOnSubmit={false} // This is needed to avoid UI jump when the user submits
+          blurOnSubmit={false}
           placeholder="Email address"
           placeholderTextColor={color("black30")}
-          title="Email"
           value={values.email}
           returnKeyType="next"
           spellCheck={false}
           autoCorrect={false}
-          // We need to to set textContentType to username (instead of emailAddress) here
-          // enable autofill of login details from the device keychain.
           textContentType="username"
           error={errors.email}
         />
-      </Flex>
+      </ScrollView>
     </Flex>
   )
 }
