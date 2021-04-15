7e29127c1489d8f3372afb76c10c2b3adc8e7e23
Mounir Dhahri <soussouheros@gmail.com>
2021-04-15 12:17:50 +0200

    add create account name screen

--- src/lib/Scenes/Onboarding/OnboardingCreateAccount/OnboardingCreateAccountName.tsx
+++ src/lib/Scenes/Onboarding/OnboardingCreateAccount/OnboardingCreateAccountName.tsx
@@ -1,21 +1,58 @@
 import { StackScreenProps } from "@react-navigation/stack"
+import { useFormikContext } from "formik"
+import { Input } from "lib/Components/Input/Input"
 import { BackButton } from "lib/navigation/BackButton"
-import { Flex, Text } from "palette"
+import { useScreenDimensions } from "lib/utils/useScreenDimensions"
+import { Box, color, Flex, Spacer, Text } from "palette"
 import React from "react"
-import { OnboardingCreateAccountNavigationStack } from "./OnboardingCreateAccount"
+import { ScrollView } from "react-native"
+import { OnboardingCreateAccountNavigationStack, UserSchema } from "./OnboardingCreateAccount"
 
 export interface OnboardingCreateAccountNameProps
   extends StackScreenProps<OnboardingCreateAccountNavigationStack, "OnboardingCreateAccountName"> {}
 
 export const OnboardingCreateAccountName: React.FC<OnboardingCreateAccountNameProps> = ({ navigation }) => {
+  const { values, handleSubmit, handleChange, validateForm, errors } = useFormikContext<UserSchema>()
+
   return (
-    <Flex flex={1} justifyContent="center" alignItems="center" backgroundColor="white">
-      <BackButton
-        onPress={() => {
-          navigation.goBack()
+    <Flex backgroundColor="white" flexGrow={1}>
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+          paddingTop: useScreenDimensions().safeAreaInsets.top,
+          justifyContent: "flex-start",
         }}
-      />
-      <Text variant="title">OnboardingCreateAccountName</Text>
+        showsVerticalScrollIndicator={false}
+        keyboardDismissMode="on-drag"
+        keyboardShouldPersistTaps="handled"
+      >
+        <BackButton onPress={navigation.goBack} />
+        <Spacer mt={60} />
+        <Box height={130}>
+          <Text variant="largeTitle">What’s your full name?</Text>
+          <Spacer mt={1.5} />
+          <Text variant="caption" color={color("black60")}>
+            Galleries and auction houses you contact will identity you by your full name.
+          </Text>
+        </Box>
+        <Spacer mt={50} />
+        <Input
+          autoCapitalize="words"
+          autoCompleteType="name"
+          autoCorrect={false}
+          autoFocus
+          onChangeText={(text) => {
+            handleChange("name")(text)
+          }}
+          onSubmitEditing={handleSubmit}
+          onBlur={() => validateForm()}
+          placeholder="First and Last Name"
+          placeholderTextColor={color("black30")}
+          returnKeyType="done"
+          value={values.name}
+          error={errors.name}
+        />
+      </ScrollView>
     </Flex>
   )
 }
