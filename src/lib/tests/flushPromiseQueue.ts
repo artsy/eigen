278e1d8a0e001dd2f0128b89feb36c4a9861d98c
Pavlos Vinieratos <pvinis@gmail.com>
2021-04-21 12:08:47 +0100

    add flush

--- src/lib/tests/flushPromiseQueue.ts
+++ src/lib/tests/flushPromiseQueue.ts
@@ -1,6 +1,19 @@
 /**
- * test utility to wait for all promises to be resolved
+ * Test utility to wait for all promises to be resolved.
+ *
+ * Use this for tests *not* using jest's fake timers.
+ * If you do use fake timers, use `flushPromiseQueueFakeTimers` instead.
  */
-export function flushPromiseQueue() {
+export const flushPromiseQueue = () => {
   return new Promise((r) => setTimeout(() => requestAnimationFrame(r), 0))
+}
+
+/**
+ * Test utility to wait for all promises to be resolved.
+ *
+ * Use this for tests using jest's fake timers.
+ * If you *do not* use fake timers, use `flushPromiseQueue` instead.
+ */
+export const flushPromiseQueueFakeTimers = async () => {
+  return new Promise((res, _) => res(null))
 }
