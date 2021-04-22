62a6ed44a9921357e5eb67f61b0b89f353bd9abb
David Sheldrick <d.j.sheldrick@gmail.com>
2021-04-22 12:07:21 +0100

    fix: keyboard avoidance for iOS13 modals (#4658)
    * fix keyboard avoidance for ios13 modals
    
    * update changelog

--- src/lib/Components/FancyModal/FancyModal.tsx
+++ src/lib/Components/FancyModal/FancyModal.tsx
@@ -42,7 +42,7 @@ export const FancyModal: React.FC<{
           up to the maximum 'top' value, and then add padding if the keyboard comes up any more.
           I'd imagine it would have an API like <FancyKeyboardAvoding height={sheetHeighht} maxHeight={screenHeight - (top + 10)}>
       */
-      <ArtsyKeyboardAvoidingViewContext.Provider value={{ isPresentedModally: true, isVisible: true }}>
+      <ArtsyKeyboardAvoidingViewContext.Provider value={{ isPresentedModally: true, isVisible: true, bottomOffset: 0 }}>
         <ArtsyKeyboardAvoidingView>{children}</ArtsyKeyboardAvoidingView>
       </ArtsyKeyboardAvoidingViewContext.Provider>
     ) : null,
