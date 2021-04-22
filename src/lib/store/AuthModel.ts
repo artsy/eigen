6933e3fe84ba05933062885b507d43c04c2e424f
Mounir Dhahri <mounir.dhahri@ensi-uma.tn>
2021-04-22 11:04:51 +0200

    feat: Onboarding sign up screen (CX-1081) (#4647)
    * add signup screens navigation container
    
    * add the different sign up pages
    
    * update OnboardingCreateAccountStore
    
    * use formik store
    
    * fix keyboard issues
    
    * add frontend validation
    
    * add autofocus to login flow
    
    * wrap create email screen with scrollview
    
    * add create account password screen
    
    * add create account name screen
    
    * fix create account button
    
    * add types to getCurrentRoute
    
    * fix submit issue on account email
    
    * fix back unresponsive button on account login
    
    * navigate to the login screen when email exists
    
    * add terms and conditions notice
    
    * Add linking to privacy policy and terms of use
    
    * do not blur on submit on on create account name screen
    
    * add sign up action
    
    * highlight terms and conditions checkbox
    
    * fix button
    
    * remove wrong file
    
    * Add create account email test
    
    * Add create account name test
    
    * add create account password test
    
    * Add opacity to the different button variants
    
    * add create account test
    
    * fix typescript
    
    * add name validation
    
    * send email when navigating to login screen
    
    * improve navigate to login UX
    
    * add autofocus test
    
    * Update src/palette/elements/Button/Button.tsx
    
    Co-authored-by: David Sheldrick <d.j.sheldrick@gmail.com>
    
    * Update src/lib/Scenes/Onboarding/OnboardingLogin.tsx
    
    Co-authored-by: David Sheldrick <d.j.sheldrick@gmail.com>
    
    * Update src/lib/Scenes/Onboarding/OnboardingCreateAccount/__tests__/OnboardingCreateAccount-tests.tsx
    
    Co-authored-by: David Sheldrick <d.j.sheldrick@gmail.com>
    
    * fix review comments
    
    * wip: add screen wrapper
    
    * create a single shared wrapper
    
    Co-authored-by: David Sheldrick <d.j.sheldrick@gmail.com>

--- src/lib/store/AuthModel.ts
+++ src/lib/store/AuthModel.ts
@@ -23,6 +23,7 @@ export interface AuthModel {
   getXAppToken: Thunk<AuthModel, void, {}, GlobalStoreModel, Promise<string>>
   userExists: Thunk<AuthModel, { email: string }, {}, GlobalStoreModel>
   signIn: Thunk<AuthModel, { email: string; password: string }, {}, GlobalStoreModel, Promise<boolean>>
+  signUp: Thunk<AuthModel, { email: string; password: string; name: string }, {}, GlobalStoreModel, Promise<boolean>>
   forgotPassword: Thunk<AuthModel, { email: string }, {}, GlobalStoreModel, Promise<boolean>>
   gravityUnauthenticatedRequest: Thunk<
     this,
@@ -162,6 +163,29 @@ export const getAuthModel = (): AuthModel => ({
       return true
     }
 
+    return false
+  }),
+  signUp: thunk(async (actions, { email, password, name }) => {
+    const result = await actions.gravityUnauthenticatedRequest({
+      path: `/api/v1/user`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        email,
+        password,
+        name,
+        agreed_to_receive_emails: true,
+        accepted_terms_of_service: true,
+      },
+    })
+
+    // The user account has been successfully created
+    if (result.status === 201) {
+      await actions.signIn({ email, password })
+      return true
+    }
     return false
   }),
 })
