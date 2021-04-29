64fdf0b64f37a0f4f231a8aff41be7dba7a8e918
Mounir Dhahri <soussouheros@gmail.com>
2021-04-29 15:53:13 +0200

    save utm params to the store
    Co-authored-by: Adam Butler <adam.butler@artsymail.com>
    Co-authored-by: David Sheldrick <djsheldrick@gmail.com>
    Co-authored-by: Pavlos Vinieratos <pvinis@gmail.com>
    Co-authored-by: Barry Hoggard <barry@artsymail.com>

--- src/lib/Scenes/Consignments/ConsignmentsHome/ConsignmentsSubmissionForm.tsx
+++ src/lib/Scenes/Consignments/ConsignmentsHome/ConsignmentsSubmissionForm.tsx
@@ -1,9 +1,24 @@
+import { GlobalStore } from "lib/store/GlobalStore"
 import NavigatorIOS from "lib/utils/__legacy_do_not_use__navigator-ios-shim"
-import React from "react"
+import React, { useEffect } from "react"
 import { View } from "react-native"
 import { ConnectedOverview as Overview } from "../Screens/Overview"
 
-export const ConsignmentsSubmissionForm: React.FC = () => {
+interface ConsignmentsSubmissionFormProps {
+  utm_campaign?: string
+  utm_medium?: string
+  utm_source?: string
+}
+
+export const ConsignmentsSubmissionForm: React.FC<ConsignmentsSubmissionFormProps> = (props) => {
+  const { setCampaign } = GlobalStore.actions.consignmentsModel
+
+  useEffect(() => {
+    if (props.utm_campaign && props.utm_medium && props.utm_source) {
+      setCampaign({ utm_campaign: props.utm_campaign, utm_medium: props.utm_medium, utm_source: props.utm_source })
+    }
+  }, [props.utm_campaign, props.utm_medium, props.utm_source])
+
   return (
     <View style={{ flex: 1 }}>
       <NavigatorIOS initialRoute={{ component: Overview }} />
