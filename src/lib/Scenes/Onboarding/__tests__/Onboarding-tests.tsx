7a808def860990ee3b432c8eb73bdebe9e26e69c
Mounir Dhahri <soussouheros@gmail.com>
2021-04-29 15:20:44 +0200

    remove old login from android
    Co-authored-by: Barry Hoggard <barry@artsymail.com>
    Co-authored-by: Adam Butler <adam.butler@artsymail.com>
    
    Co-authored-by: David Sheldrick <djsheldrick@gmail.com>
    Co-authored-by: Pavlos Vinieratos <pvinis@gmail.com>

--- src/lib/Scenes/Onboarding/__tests__/Onboarding-tests.tsx
+++ src/lib/Scenes/Onboarding/__tests__/Onboarding-tests.tsx
@@ -1,23 +1,3 @@
-import React from "react"
-import { __globalStoreTestUtils__ } from "../../../store/GlobalStore"
-import { renderWithWrappers } from "../../../tests/renderWithWrappers"
-import { LogIn } from "../OldLogIn/LogIn"
-import { Onboarding } from "../Onboarding"
-
 describe("Onboarding", () => {
-  it("uses the old flow when ARUseNewOnboarding is set to false", () => {
-    const tree = renderWithWrappers(<Onboarding />)
-
-    __globalStoreTestUtils__?.injectFeatureFlags({ ARUseNewOnboarding: false })
-
-    expect(tree.root.findAllByType(LogIn).length).toEqual(1)
-  })
-
-  it("uses the new flow when ARUseNewOnboarding is set to true", () => {
-    __globalStoreTestUtils__?.injectFeatureFlags({ ARUseNewOnboarding: true })
-
-    const tree = renderWithWrappers(<Onboarding />)
-
-    expect(tree.root.findAllByType(LogIn).length).toEqual(0)
-  })
+  // do nothing
 })
