713acbd1ec60aae013c0a9323781b48b17436530
edez1t <73339470+edez1t@users.noreply.github.com>
2021-05-13 15:36:07 +0300

    fix: incorrect action name (#4773)
    Co-authored-by: Dzmitry Dziamidau <dzmitrydziamidau@wsa-116-21b.local>

--- src/lib/Components/Artist/ArtistHeader.tsx
+++ src/lib/Components/Artist/ArtistHeader.tsx
@@ -85,7 +85,7 @@ export const ArtistHeader: React.FC<Props> = ({ artist, relay }) => {
 
   const successfulFollowChange = () => {
     trackEvent({
-      action_name: artist.isFollowed ? Schema.ActionNames.ArtistFollow : Schema.ActionNames.ArtistUnfollow,
+      action_name: artist.isFollowed ? Schema.ActionNames.ArtistUnfollow : Schema.ActionNames.ArtistFollow,
       action_type: Schema.ActionTypes.Success,
       owner_id: artist.internalID,
       owner_slug: artist.slug,
