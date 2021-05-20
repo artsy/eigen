46e4d293b28f6c348f15fb16f305e167e6e88466
Mounir Dhahri <soussouheros@gmail.com>
2021-05-20 10:35:40 +0200

    run yarn prettier

--- dangerfile.ts
+++ dangerfile.ts
@@ -27,34 +27,34 @@ const testOnlyFilter = (filename: string) => filename.includes("-tests") && type
 const createdAppOnlyFiles = createdFiles.filter(appOnlyFilter)
 const createdTestOnlyFiles = createdFiles.filter(testOnlyFilter)
 
-const newEnzymeImports = createdTestOnlyFiles.filter(filename => {
+const newEnzymeImports = createdTestOnlyFiles.filter((filename) => {
   const content = fs.readFileSync(filename).toString()
   return content.includes('from "enzyme"')
 })
 if (newEnzymeImports.length > 0) {
   warn(`We are trying to migrate away from Enzyme towards \`react-test-renderer\`, but found Enzyme imports in the following new unit test files:
 
-${newEnzymeImports.map(filename => `- \`${filename}\``).join("\n")}
+${newEnzymeImports.map((filename) => `- \`${filename}\``).join("\n")}
 
 See [\`placeholders-tests.tsx\`](https://github.com/artsy/eigen/blob/aebce6e50ece296b5dc63681f7ae0b6ed20b4bcc/src/lib/utils/__tests__/placeholders-tests.tsx) as an example, or [the docs](https://reactjs.org/docs/test-renderer.html).
   `)
 }
 
-const newRenderRelayTreeImports = createdTestOnlyFiles.filter(filename => {
+const newRenderRelayTreeImports = createdTestOnlyFiles.filter((filename) => {
   const content = fs.readFileSync(filename).toString()
   return content.includes('from "lib/tests/renderRelayTree"')
 })
 if (newRenderRelayTreeImports.length > 0) {
   warn(`We are trying to migrate away from \`renderRelayTree\` towards \`relay-test-utils\`, but found Enzyme imports in the following new unit test files:
 
-${newRenderRelayTreeImports.map(filename => `- \`${filename}\``).join("\n")}
+${newRenderRelayTreeImports.map((filename) => `- \`${filename}\``).join("\n")}
 
 See [\`LoggedInUserInfo-tests.tsx\`](https://github.com/artsy/eigen/blob/f33577ebb09800224731365734be411b66ad8126/src/lib/Scenes/MyProfile/__tests__/LoggedInUserInfo-tests.tsx) as an example, or [the docs](https://relay.dev/docs/en/testing-relay-components).
   `)
 }
 
 // Check that every file created has a corresponding test file
-const correspondingTestsForAppFiles = createdAppOnlyFiles.map(f => {
+const correspondingTestsForAppFiles = createdAppOnlyFiles.map((f) => {
   const newPath = path.dirname(f)
   const name = path.basename(f).replace(".ts", "-tests.ts")
   return `${newPath}/__tests__/${name}`
@@ -62,7 +62,7 @@ const correspondingTestsForAppFiles = createdAppOnlyFiles.map(f => {
 
 const modified = danger.git.modified_files
 const editedFiles = modified.concat(danger.git.created_files)
-const testFiles = editedFiles.filter(f => f?.includes("Tests") && f.match(/\.(swift|m)$/))
+const testFiles = editedFiles.filter((f) => f?.includes("Tests") && f.match(/\.(swift|m)$/))
 
 // Validates that we've not accidentally let in a testing
 // shortcut to simplify dev work
@@ -81,14 +81,10 @@ const knownDevTools = danger.github.pr.body?.includes("#known") ?? false
 
 // These files are ones we really don't want changes to, except in really occasional
 // cases, so offer a way out.
-const devOnlyFiles = [
-  "Artsy.xcodeproj/xcshareddata/xcschemes/Artsy.xcscheme",
-]
+const devOnlyFiles = ["Artsy.xcodeproj/xcshareddata/xcschemes/Artsy.xcscheme"]
 for (const file of devOnlyFiles) {
   if (modified.includes(file) && !knownDevTools) {
-    fail(
-      "Developer Specific file shouldn't be changed, you can skip by adding #known to the PR body and re-running CI"
-    )
+    fail("Developer Specific file shouldn't be changed, you can skip by adding #known to the PR body and re-running CI")
   }
 }
 
