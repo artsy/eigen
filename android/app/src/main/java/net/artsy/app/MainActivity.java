a7b5649db8e93230c94b5d85c03211faff4da3c7
Pavlos Vinieratos <pvinis@gmail.com>
2021-05-24 17:55:28 +0100

    Restore MainActivity.java

--- android/app/src/main/java/net/artsy/app/MainActivity.java
+++ android/app/src/main/java/net/artsy/app/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    super.onCreate(savedInstanceState);
     if (!isTablet()) {
       // prevent screen rotation on phones
       setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
