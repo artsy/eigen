37676a9273221bbec59390d0b9a97cb616d6a3a0
Devon Blandin <dblandin@gmail.com>
2021-05-25 14:18:54 +0200

    chore(filters): remove feature flag check for new size options screen

--- src/lib/Components/ArtworkFilter/Filters/SizeOptions.tsx
+++ src/lib/Components/ArtworkFilter/Filters/SizeOptions.tsx
@@ -1,16 +1,10 @@
 import { StackScreenProps } from "@react-navigation/stack"
 import { ArtworkFilterNavigationStack } from "lib/Components/ArtworkFilter"
-import { useFeatureFlag } from "lib/store/GlobalStore"
 import React from "react"
 import { SizeOptionsScreen as NewSizeOptionsScreen } from "./SizeOptionsNew"
-import { SizeOptionsScreen as OldSizeOptionsScreen } from "./SizeOptionsOld"
 
 interface SizeOptionsScreenProps extends StackScreenProps<ArtworkFilterNavigationStack, "SizeOptionsScreen"> {}
 
 export const SizeOptionsScreen: React.FC<SizeOptionsScreenProps> = (props) => {
-  return useFeatureFlag("ARUseImprovedArtworkFilters") ? (
-    <NewSizeOptionsScreen {...props} />
-  ) : (
-    <OldSizeOptionsScreen {...props} />
-  )
+  return <NewSizeOptionsScreen {...props} />
 }
