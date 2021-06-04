554164202a2cee73076820628d778b1432ae5b2a
Dima Tretyak <itretyak@ya.ru>
2021-06-05 00:11:07 +0300

    Make saved search banner in query container

--- src/lib/Components/Artist/ArtistArtworks/SavedSearchBanner.tsx
+++ src/lib/Components/Artist/ArtistArtworks/SavedSearchBanner.tsx
@@ -1,13 +1,65 @@
+import { SavedSearchBanner_me } from '__generated__/SavedSearchBanner_me.graphql'
+import { SavedSearchBannerCreateSavedSearchMutation } from "__generated__/SavedSearchBannerCreateSavedSearchMutation.graphql"
+import { SavedSearchBannerQuery, SearchCriteriaAttributes } from '__generated__/SavedSearchBannerQuery.graphql'
+import { FilterParams, prepareFilterParamsForSaveSearchInput } from 'lib/Components/ArtworkFilter/ArtworkFilterHelpers'
+import { defaultEnvironment } from "lib/relay/createEnvironment"
 import { Button, Flex, Text } from "palette"
-import React from "react"
+import React, { useState } from "react"
+import { commitMutation, createFragmentContainer, graphql, QueryRenderer, RelayProp } from "react-relay"
 
 interface SavedSearchBannerProps {
-  enabled: boolean
+  me?: SavedSearchBanner_me | null
+  artistId: string
+  attributes: SearchCriteriaAttributes
   loading?: boolean
-  onPress: () => void
+  relay: RelayProp
 }
 
-export const SavedSearchBanner: React.FC<SavedSearchBannerProps> = ({ enabled, loading, onPress }) => {
+export const SavedSearchBanner: React.FC<SavedSearchBannerProps> = ({ me, attributes, loading, relay }) => {
+  const [saving, setSaving] = useState(false)
+  const enabled = !!me?.savedSearch?.internalID
+  const inProcess = loading || saving
+
+  const createSavedSearch = () => {
+    setSaving(true)
+    commitMutation<SavedSearchBannerCreateSavedSearchMutation>(relay.environment, {
+      mutation: graphql`
+      mutation SavedSearchBannerCreateSavedSearchMutation($input: CreateSavedSearchInput!) {
+        createSavedSearch(input: $input) {
+          savedSearchOrErrors {
+            ... on SearchCriteria {
+              internalID
+            }
+          }
+        }
+      }
+    `,
+      variables: {
+        input: {
+          attributes,
+        }
+      },
+      onCompleted: () => {
+        setSaving(false)
+      },
+      onError: () => {
+        setSaving(false)
+      }
+    })
+  }
+
+  const handleSaveSearchFiltersPress = () => {
+    if (inProcess) {
+      return
+    }
+
+    if (enabled) {
+      // TODO: deleteSavedSearch
+    } else {
+      createSavedSearch()
+    }
+  }
+
   return (
     <Flex
       backgroundColor="white"
@@ -23,14 +75,55 @@ export const SavedSearchBanner: React.FC<SavedSearchBannerProps> = ({ enabled, l
       </Text>
       <Button
         variant={enabled ? "secondaryOutline" : "primaryBlack"}
-        onPress={onPress}
+        onPress={handleSaveSearchFiltersPress}
         size="small"
-        loading={loading}
+        loading={inProcess}
         longestText="Disable"
         haptic
       >
         {enabled ? "Disable" : "Enable"}
       </Button>
     </Flex>
+  )
+}
+
+export const SavedSearchBannerContainer = createFragmentContainer(
+  SavedSearchBanner,
+  {
+    me: graphql`
+      fragment SavedSearchBanner_me on Me @argumentDefinitions(
+        criteria: { type: "SearchCriteriaAttributes" }
+      ){
+        savedSearch(criteria: $criteria) {
+          internalID
+        }
+      }
+    `,
+  },
+)
+
+export const SavedSearchBannerQueryRender: React.FC<{ filters: FilterParams, artistId: string }> = ({ filters, artistId }) => {
+  const input = prepareFilterParamsForSaveSearchInput(filters)
+  const attributes = {
+    artistID: artistId,
+    ...input,
+  }
+
+  return (
+    <QueryRenderer<SavedSearchBannerQuery>
+      environment={defaultEnvironment}
+      query={graphql`
+        query SavedSearchBannerQuery($criteria: SearchCriteriaAttributes!) {
+          me {
+            ...SavedSearchBanner_me @arguments(criteria: $criteria)
+          }
+        }
+      `}
+      render={({ props }) => <SavedSearchBannerContainer {...props} loading={props === null} attributes={attributes} artistId={artistId} />}
+      variables={{
+        criteria: attributes
+      }}
+      cacheConfig={{ force: true }}
+    />
   )
 }
