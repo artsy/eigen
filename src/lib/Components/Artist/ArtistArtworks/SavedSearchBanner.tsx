37f8992f83568b16af119b3cf00786ad3025df48
Dima Tretyak <itretyak@ya.ru>
2021-06-07 18:52:49 +0300

    Move delete saved search mutation

--- src/lib/Components/Artist/ArtistArtworks/SavedSearchBanner.tsx
+++ src/lib/Components/Artist/ArtistArtworks/SavedSearchBanner.tsx
@@ -1,5 +1,6 @@
 import { SavedSearchBanner_me } from '__generated__/SavedSearchBanner_me.graphql'
 import { SavedSearchBannerCreateSavedSearchMutation } from "__generated__/SavedSearchBannerCreateSavedSearchMutation.graphql"
+import { SavedSearchBannerDeleteSavedSearchMutation } from "__generated__/SavedSearchBannerDeleteSavedSearchMutation.graphql"
 import { SavedSearchBannerQuery, SearchCriteriaAttributes } from '__generated__/SavedSearchBannerQuery.graphql'
 import { FilterParams, prepareFilterParamsForSaveSearchInput } from 'lib/Components/ArtworkFilter/ArtworkFilterHelpers'
 import { defaultEnvironment } from "lib/relay/createEnvironment"
@@ -48,13 +49,41 @@ export const SavedSearchBanner: React.FC<SavedSearchBannerProps> = ({ me, attrib
     })
   }
 
+  const deleteSavedSearch = () => {
+    setSaving(true)
+    commitMutation<SavedSearchBannerDeleteSavedSearchMutation>(relay.environment, {
+      mutation: graphql`
+      mutation SavedSearchBannerDeleteSavedSearchMutation($input: DeleteSavedSearchInput!) {
+        deleteSavedSearch(input: $input) {
+          savedSearchOrErrors {
+            ... on SearchCriteria {
+              internalID
+            }
+          }
+        }
+      }
+    `,
+      variables: {
+        input: {
+          searchCriteriaID: me!.savedSearch!.internalID
+        }
+      },
+      onCompleted: () => {
+        setSaving(false)
+      },
+      onError: () => {
+        setSaving(false)
+      }
+    })
+  }
+
   const handleSaveSearchFiltersPress = () => {
     if (inProcess) {
       return
     }
 
     if (enabled) {
-      // TODO: deleteSavedSearch
+      deleteSavedSearch()
     } else {
       createSavedSearch()
     }
@@ -119,7 +148,7 @@ export const SavedSearchBannerQueryRender: React.FC<{ filters: FilterParams, art
           }
         }
       `}
-      render={({ props }) => <SavedSearchBannerFragmentContainer {...props} loading={props === null} attributes={attributes} artistId={artistId} />}
+      render={({ props }) => <SavedSearchBannerFragmentContainer me={props?.me ?? null} loading={props === null} attributes={attributes} artistId={artistId} />}
       variables={{
         criteria: attributes
       }}
