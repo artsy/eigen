af51b56cc97739a404d9d5aa8f5902b338a5a3a3
Dima Tretyak <itretyak@ya.ru>
2021-06-07 15:01:35 +0300

    Update tests for saved search banner

--- src/lib/Components/Artist/ArtistArtworks/__tests__/SavedSearchBanner-tests.tsx
+++ src/lib/Components/Artist/ArtistArtworks/__tests__/SavedSearchBanner-tests.tsx
@@ -1,41 +1,73 @@
+import { SavedSearchBannerQuery, SearchCriteriaAttributes } from '__generated__/SavedSearchBannerQuery.graphql'
+import { SavedSearchBannerTestsQuery } from "__generated__/SavedSearchBannerTestsQuery.graphql"
+import { mockEnvironmentPayload } from 'lib/tests/mockEnvironmentPayload'
 import { renderWithWrappers } from "lib/tests/renderWithWrappers"
 import { Button } from "palette"
 import React from "react"
-import { SavedSearchBanner } from "../SavedSearchBanner"
+import { graphql, QueryRenderer } from "react-relay"
+import { createMockEnvironment } from "relay-test-utils"
+import { SavedSearchBannerFragmentContainer } from "../SavedSearchBanner"
+
+jest.unmock("react-relay")
 
 describe("SavedSearchBanner", () => {
+  let mockEnvironment: ReturnType<typeof createMockEnvironment>
+  const attributes: SearchCriteriaAttributes = {
+    priceMin: 300,
+    priceMax: 500,
+  }
+
+  beforeEach(() => (mockEnvironment = createMockEnvironment()))
+
+  const TestRenderer = () => {
+    return (
+      <QueryRenderer<SavedSearchBannerTestsQuery>
+          environment={mockEnvironment}
+          query={graphql`
+            query SavedSearchBannerTestsQuery($criteria: SearchCriteriaAttributes!) @relay_test_operation {
+              me {
+                ...SavedSearchBanner_me @arguments(criteria: $criteria)
+              }
+            }
+          `}
+          render={({ props }) => <SavedSearchBannerFragmentContainer {...props} loading={props === null} attributes={attributes} artistId="banksy" />}
+          variables={{
+            criteria: attributes
+          }}
+        />
+    )
+  }
+
   it("renders correctly disabled state", () => {
-    const onPress = jest.fn()
-    const tree = renderWithWrappers(<SavedSearchBanner enabled={false} onPress={onPress} />)
+    const tree = renderWithWrappers(<TestRenderer />)
+    mockEnvironmentPayload(mockEnvironment, {
+      Me: () => ({
+        savedSearch: null
+      })
+    })
+
     const buttonComponent = tree.root.findByType(Button)
 
     expect(buttonComponent.props.children).toEqual("Enable")
     expect(buttonComponent.props.variant).toBe("primaryBlack")
+    expect(buttonComponent.props.loading).toBe(false)
   })
 
   it("renders correctly enabled state", () => {
-    const onPress = jest.fn()
-    const tree = renderWithWrappers(<SavedSearchBanner enabled={true} loading={true} onPress={onPress} />)
+    const tree = renderWithWrappers(<TestRenderer />)
+    mockEnvironmentPayload(mockEnvironment)
+
     const buttonComponent = tree.root.findByType(Button)
 
     expect(buttonComponent.props.children).toEqual("Disable")
     expect(buttonComponent.props.variant).toBe("secondaryOutline")
+    expect(buttonComponent.props.loading).toBe(false)
   })
 
   it("renders correctly loading state", () => {
-    const onPress = jest.fn()
-    const tree = renderWithWrappers(<SavedSearchBanner enabled={false} loading={true} onPress={onPress} />)
+    const tree = renderWithWrappers(<TestRenderer />)
     const buttonComponent = tree.root.findByType(Button)
 
     expect(buttonComponent.props.loading).toBe(true)
-  })
-
-  it("calls onPress when button is pressed", () => {
-    const onPress = jest.fn()
-    const tree = renderWithWrappers(<SavedSearchBanner enabled={false} loading={false} onPress={onPress} />)
-    const buttonComponent = tree.root.findByType(Button)
-
-    buttonComponent.props.onPress()
-    expect(onPress).toHaveBeenCalled()
   })
 })
