1325d615750af8308bf8282b8b4eb1696f922e5d
Dima Tretyak <itretyak@ya.ru>
2021-06-11 14:56:27 +0300

    Remove unused code

--- src/lib/Components/Notification/notificationHooks.tsx
+++ src/lib/Components/Notification/notificationHooks.tsx
@@ -10,7 +10,6 @@ export const useNotification = () => {
     () => ({
       show: contextValue.show,
       hide: contextValue.hide,
-      hideOldest: contextValue.hideOldest,
     }),
     [contextValue]
   )
