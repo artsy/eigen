62fc221b714be2b7800888d8dcb51cc12858e44e
Dima Tretyak <itretyak@ya.ru>
2021-06-11 16:00:58 +0300

    Add NotificationProvider

--- src/lib/AppProviders.tsx
+++ src/lib/AppProviders.tsx
@@ -3,6 +3,7 @@ import { Theme } from "palette"
 import React, { ReactNode } from "react"
 import { RelayEnvironmentProvider } from "relay-hooks"
 import { _FancyModalPageWrapper } from "./Components/FancyModal/FancyModalContext"
+import { NotificationProvider } from "./Components/Notification/NotificationProvider"
 import { ToastProvider } from "./Components/Toast/toastHook"
 import { defaultEnvironment } from "./relay/createEnvironment"
 import { GlobalStoreProvider } from "./store/GlobalStore"
@@ -15,7 +16,11 @@ export const AppProviders = ({ children }: { children: ReactNode }) => (
         <ActionSheetProvider>
           <GlobalStoreProvider>
             <ToastProvider>
-              <_FancyModalPageWrapper>{children}</_FancyModalPageWrapper>
+              <NotificationProvider>
+                <_FancyModalPageWrapper>
+                  {children}
+                </_FancyModalPageWrapper>
+              </NotificationProvider>
             </ToastProvider>
           </GlobalStoreProvider>
         </ActionSheetProvider>
