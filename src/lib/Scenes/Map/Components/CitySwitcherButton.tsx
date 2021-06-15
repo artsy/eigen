56b7d8af30ded99796d4ceab99f3c1f3803f9582
Pavlos Vinieratos <pvinis@gmail.com>
2021-06-15 14:29:28 +0200

    fix props
    Co-authored-by: George Kartalis <g.kartalis7@gmail.com>

--- src/lib/Scenes/Map/Components/CitySwitcherButton.tsx
+++ src/lib/Scenes/Map/Components/CitySwitcherButton.tsx
@@ -62,11 +62,7 @@ export class CitySwitcherButton extends Component<Props> {
             </>
           ) : (
             <Flex alignItems="center" justifyContent="center" flexGrow={1}>
-              <Spinner
-                spinnerColor={color("black60")}
-                style={{ backgroundColor: "transparent" }}
-                size={{ width: 16, height: 16 }}
-              />
+              <Spinner color="black60" style={{ backgroundColor: "transparent" }} width={16} height={16} />
             </Flex>
           )}
         </Background>
