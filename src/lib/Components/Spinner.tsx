ed7967c0c8d426d1a96bd5fbdb5cc0d8c19fc8f3
gkartalis <g.kartalis7@gmail.com>
2021-06-16 11:01:39 +0200

    refactored CenteredSpinner to use palette Spinner

---
+++ src/lib/Components/Spinner.tsx
@@ -0,0 +1,27 @@
+import { Spinner as PaletteSpinner } from "palette"
+import { Color } from "palette/Theme"
+import React from "react"
+import { StyleSheet, View, ViewProps } from "react-native"
+
+interface Props extends ViewProps {
+  spinnerColor?: Color
+  size?: "small" | "medium" | "large"
+}
+
+const Spinner: React.FC<Props> = ({ spinnerColor = "black100", size = "large", ...props }) => {
+  return (
+    <View style={[props.style, styles.container]} testID={props.testID}>
+      <PaletteSpinner color={spinnerColor} size={size} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+})
+
+export default Spinner
