01972fcd9a2633cd9e61667189935df3a0feada5
artsyit <it@artsymail.com>
2021-06-21 10:44:31 +0000

    Update metaphysics schema

--- src/__generated__/SavedSearchBannerCreateSavedSearchMutation.graphql.ts
+++ src/__generated__/SavedSearchBannerCreateSavedSearchMutation.graphql.ts
@@ -5,7 +5,6 @@
 
 import { ConcreteRequest } from "relay-runtime";
 export type CreateSavedSearchInput = {
-    artistID?: string | null;
     attributes?: SearchCriteriaAttributes | null;
     clientMutationId?: string | null;
 };
@@ -14,10 +13,13 @@ export type SearchCriteriaAttributes = {
     additionalGeneIDs?: Array<string> | null;
     artistID?: string | null;
     atAuction?: boolean | null;
+    attributionClass?: Array<string> | null;
     attributionClasses?: Array<string> | null;
     colors?: Array<string> | null;
+    dimensionRange?: string | null;
     dimensionScoreMax?: number | null;
     dimensionScoreMin?: number | null;
+    height?: string | null;
     heightMax?: number | null;
     heightMin?: number | null;
     inquireableOnly?: boolean | null;
@@ -28,6 +30,8 @@ export type SearchCriteriaAttributes = {
     partnerIDs?: Array<string> | null;
     priceMax?: number | null;
     priceMin?: number | null;
+    priceRange?: string | null;
+    width?: number | null;
     widthMax?: number | null;
     widthMin?: number | null;
 };
