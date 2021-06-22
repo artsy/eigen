4ac5a64cf37bd82b9fc43db0597708c7a2cc525a
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2021-06-22 14:20:04 +0300

    feat: convert saved search criteria to filter params (#4988)
    * Add parse price range label helper
    
    * Add parsers for saved search logic
    
    * Improve code readability for parseSizeForFilterParams
    
    * Add parser for colors from search criteria
    
    * Add parser for aggregations names
    
    * Add parser for attributions
    
    * Add parser for ways to buy
    
    * Update tests for price parser
    
    * Add parser for search criteria
    
    * Rename parser to converters
    
    * Use price range label helper for Price filter
    
    * refactor!: improve code readability
    
    * feat: extract color names only from avaialable in aggregation
    
    * feat: extract from agggregation only values
    
    * feat: add converter for filter param for major periods
    
    * docs: add task name in changelog
    
    * fix: return previous shape of Aggregations
    
    * refactor: sync price converts to new format
    
    * refactor: sync size converts to new format
    
    * refactor: sync attribution converts to new format
    
    * refactor: get criterial value by filter param key
    
    * fix: remove unused code
    
    * refactor: check the count of search criteria for arrays
    
    * refactor: remove unused export
    
    * refactor: add return type

---
+++ src/lib/Components/ArtworkFilter/SavedSearch/convertersToFilterParams.ts
@@ -0,0 +1,231 @@
+import { SearchCriteriaAttributes } from "__generated__/SavedSearchBannerQuery.graphql"
+import { Dictionary, isNil, keyBy, mapValues } from "lodash"
+import {
+  Aggregation,
+  Aggregations,
+  FilterData,
+  filterKeyFromAggregation,
+  FilterParamName,
+  getDisplayNameForTimePeriod,
+} from "../ArtworkFilterHelpers"
+import { DEFAULT_FILTERS } from "../ArtworkFilterStore"
+import { ATTRIBUTION_CLASS_OPTIONS } from "../Filters/AttributionClassOptions"
+import { COLORS_INDEXED_BY_VALUE } from "../Filters/ColorsOptions"
+import { localizeDimension, parsePriceRangeLabel, parseRange } from "../Filters/helpers"
+import { SIZE_OPTIONS } from "../Filters/SizeOptions"
+import { WAYS_TO_BUY_FILTER_PARAM_NAMES } from "../Filters/WaysToBuyOptions"
+
+type SearchCriteriaAttributeKeys = keyof SearchCriteriaAttributes
+export type AggregationByFilterParamName = Dictionary<Aggregation[]>
+
+export const convertPriceToFilterParam = (criteria: SearchCriteriaAttributes): FilterData | null => {
+  const priceRangeValue = criteria[FilterParamName.priceRange]
+
+  if (!isNil(priceRangeValue)) {
+    const { min, max } = parseRange(priceRangeValue)
+
+    if (min !== "*" || max !== "*") {
+      return {
+        displayText: parsePriceRangeLabel(min, max),
+        paramValue: `${min}-${max}`,
+        paramName: FilterParamName.priceRange,
+      }
+    }
+  }
+
+  return null
+}
+
+export const convertCustomSizeToFilterParamByName = (paramName: FilterParamName, range: string) => {
+  const { min, max } = parseRange(range)
+  const widthMinLocalized = localizeDimension(min, "in")
+  const widthMaxLocalized = localizeDimension(max, "in")
+
+  return {
+    displayText: `${widthMinLocalized.value}-${widthMaxLocalized.value}`,
+    paramValue: `${min}-${max}`,
+    paramName,
+  }
+}
+
+export const convertSizeToFilterParams = (criteria: SearchCriteriaAttributes): FilterData[] | null => {
+  const filterParams: FilterData[] = []
+  const dimensionRangeValue = criteria[FilterParamName.dimensionRange]
+  const widthValue = criteria[FilterParamName.width]
+  const heightValue = criteria[FilterParamName.height]
+
+  if (!isNil(dimensionRangeValue)) {
+    const sizeOptionItem = SIZE_OPTIONS.find((option) => option.paramValue === dimensionRangeValue)
+
+    if (sizeOptionItem) {
+      filterParams.push(sizeOptionItem)
+    }
+  }
+
+  // Parse custom width size
+  if (!isNil(widthValue)) {
+    filterParams.push(convertCustomSizeToFilterParamByName(FilterParamName.width, widthValue))
+  }
+
+  // Parse custom height size
+  if (!isNil(heightValue)) {
+    filterParams.push(convertCustomSizeToFilterParamByName(FilterParamName.height, heightValue))
+  }
+
+  if (filterParams.length > 0) {
+    return filterParams
+  }
+
+  return null
+}
+
+export const convertColorsToFilterParam = (
+  criteria: SearchCriteriaAttributes,
+  aggregation: AggregationByFilterParamName
+): FilterData | null => {
+  const colorsValue = criteria[FilterParamName.colors]
+
+  if (Array.isArray(colorsValue) && colorsValue.length > 0) {
+    const colorFromAggregationByValue = keyBy(aggregation[FilterParamName.colors], "value")
+    const availableColors = colorsValue.filter((color) => !!colorFromAggregationByValue[color])
+    const colorNames = availableColors.map((color) => COLORS_INDEXED_BY_VALUE[color].name)
+
+    if (availableColors.length > 0) {
+      return {
+        displayText: colorNames.join(", "),
+        paramValue: availableColors,
+        paramName: FilterParamName.colors,
+      }
+    }
+  }
+
+  return null
+}
+
+export const convertAggregationValueNamesToFilterParam = (
+  paramName: FilterParamName,
+  aggregations: Aggregation[],
+  criteriaValues: string[]
+): FilterData | null => {
+  const aggregationByValue = keyBy(aggregations, "value")
+  const availableValues = criteriaValues.filter((criteriaValue) => !!aggregationByValue[criteriaValue])
+  const names = availableValues.map((value) => aggregationByValue[value].name)
+
+  if (availableValues.length > 0) {
+    return {
+      displayText: names.join(", "),
+      paramValue: availableValues,
+      paramName,
+    }
+  }
+
+  return null
+}
+
+export const convertAttributionToFilterParam = (criteria: SearchCriteriaAttributes): FilterData | null => {
+  const attributionValues = criteria[FilterParamName.attributionClass]
+
+  if (Array.isArray(attributionValues) && attributionValues.length > 0) {
+    const attributionItemByValue = keyBy(ATTRIBUTION_CLASS_OPTIONS, "paramValue")
+    const availableAttributions = attributionValues.filter((attribution) => !!attributionItemByValue[attribution])
+    const names = availableAttributions.map((attribution) => attributionItemByValue[attribution].displayText)
+
+    if (availableAttributions.length > 0) {
+      return {
+        displayText: names.join(", "),
+        paramValue: availableAttributions,
+        paramName: FilterParamName.attributionClass,
+      }
+    }
+  }
+
+  return null
+}
+
+export const convertWaysToBuyToFilterParams = (criteria: SearchCriteriaAttributes): FilterData[] | null => {
+  const defautFilterParamByName = keyBy(DEFAULT_FILTERS, "paramName")
+  const availableWaysToBuyFilterParamNames = WAYS_TO_BUY_FILTER_PARAM_NAMES.filter(
+    (filterParamName) => !isNil(criteria[filterParamName as SearchCriteriaAttributeKeys])
+  )
+
+  if (availableWaysToBuyFilterParamNames.length > 0) {
+    return availableWaysToBuyFilterParamNames.map((filterParamName) => ({
+      displayText: defautFilterParamByName[filterParamName].displayText,
+      paramValue: criteria[filterParamName as SearchCriteriaAttributeKeys]!,
+      paramName: filterParamName,
+    }))
+  }
+
+  return null
+}
+
+export const convertMajorPeriodToFilterParam = (criteria: SearchCriteriaAttributes): FilterData | null => {
+  const periods = criteria[FilterParamName.timePeriod]
+
+  if (Array.isArray(periods) && periods.length > 0) {
+    const namedPeriods = periods.map((period) => getDisplayNameForTimePeriod(period))
+
+    return {
+      displayText: namedPeriods.join(", "),
+      paramValue: periods,
+      paramName: FilterParamName.timePeriod,
+    }
+  }
+
+  return null
+}
+
+export const convertSavedSearchCriteriaToFilterParams = (
+  criteria: SearchCriteriaAttributes,
+  aggregations: Aggregations
+): FilterData[] => {
+  let filterParams: FilterData[] = []
+  const aggregationByFilterParamName = keyBy(aggregations, (aggregation) => filterKeyFromAggregation[aggregation.slice])
+  const aggregationValueByFilterParamName = mapValues(
+    aggregationByFilterParamName,
+    "counts"
+  ) as AggregationByFilterParamName
+  const shouldExtractValueNamesFromAggregation = [
+    FilterParamName.locationCities,
+    FilterParamName.materialsTerms,
+    FilterParamName.additionalGeneIDs,
+    FilterParamName.partnerIDs,
+  ]
+
+  const converters = [
+    convertPriceToFilterParam,
+    convertSizeToFilterParams,
+    convertColorsToFilterParam,
+    convertAttributionToFilterParam,
+    convertWaysToBuyToFilterParams,
+    convertMajorPeriodToFilterParam,
+  ]
+
+  converters.forEach((converter) => {
+    const filterParamItem = converter(criteria, aggregationValueByFilterParamName)
+
+    if (filterParamItem) {
+      filterParams = filterParams.concat(filterParamItem)
+    }
+  })
+
+  // Extract value names from aggregation
+  shouldExtractValueNamesFromAggregation.forEach((filterParamName) => {
+    const aggregationValue = aggregationValueByFilterParamName[filterParamName]
+    const criteriaValue = criteria[filterParamName as SearchCriteriaAttributeKeys] as string[]
+
+    if (aggregationValue) {
+      const filterParamItem = convertAggregationValueNamesToFilterParam(
+        filterParamName,
+        aggregationValue,
+        criteriaValue
+      )
+
+      if (filterParamItem) {
+        filterParams = filterParams.concat(filterParamItem)
+      }
+    }
+  })
+
+  return filterParams
+}
