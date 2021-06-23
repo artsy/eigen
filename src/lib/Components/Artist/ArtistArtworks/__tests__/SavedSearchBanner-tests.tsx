d0894c60330e17f95765108b046bd9902ff3cab9
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2021-06-23 13:00:50 +0300

    chore: add specs for deleteSavedSearch and createSavedSearch mutations (#5004)
    * feat: add tests for create save search mutation
    
    * feat: chech the message display for create save saved search mutation
    
    * refactor: return correct response for refetch operation
    
    * refactor: add correct name for tests
    
    * tests: add spec for delete saved search
    
    * docs: add task to changelog
    
    * tests: try to reduce the amount of code used

--- src/lib/Components/Artist/ArtistArtworks/__tests__/SavedSearchBanner-tests.tsx
+++ src/lib/Components/Artist/ArtistArtworks/__tests__/SavedSearchBanner-tests.tsx
@@ -1,14 +1,30 @@
 import { SearchCriteriaAttributes } from "__generated__/SavedSearchBannerQuery.graphql"
 import { SavedSearchBannerTestsQuery } from "__generated__/SavedSearchBannerTestsQuery.graphql"
+import { PopoverMessage } from "lib/Components/PopoverMessage/PopoverMessage"
+import { LegacyNativeModules } from "lib/NativeModules/LegacyNativeModules"
+import { PushAuthorizationStatus } from "lib/Scenes/MyProfile/MyProfilePushNotifications"
 import { mockEnvironmentPayload } from "lib/tests/mockEnvironmentPayload"
 import { renderWithWrappers } from "lib/tests/renderWithWrappers"
-import { Button } from "palette"
+import { Button, Text } from "palette"
 import React from "react"
 import { graphql, QueryRenderer } from "react-relay"
-import { createMockEnvironment } from "relay-test-utils"
+import { act } from "react-test-renderer"
+import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
 import { SavedSearchBannerRefetchContainer } from "../SavedSearchBanner"
 
 jest.unmock("react-relay")
+
+interface Mutation {
+  name: string
+  variables: Record<string, any>
+}
+interface Popover {
+  title: string
+  message: string
+}
+
+const mockFetchNotificationPermissions = LegacyNativeModules.ARTemporaryAPIModule
+  .fetchNotificationPermissions as jest.Mock<any>
 
 describe("SavedSearchBanner", () => {
   let mockEnvironment: ReturnType<typeof createMockEnvironment>
@@ -45,6 +61,36 @@ describe("SavedSearchBanner", () => {
     )
   }
 
+  const checkLogicForMutations = (mockResolvers = {}, mutation: Mutation, popover: Popover) => {
+    mockFetchNotificationPermissions.mockImplementationOnce((cb) => cb(null, PushAuthorizationStatus.Authorized))
+
+    const tree = renderWithWrappers(<TestRenderer />)
+    const buttonComponent = tree.root.findByType(Button)
+
+    mockEnvironmentPayload(mockEnvironment, mockResolvers)
+
+    act(() => buttonComponent.props.onPress())
+
+    const createOperation = mockEnvironment.mock.getMostRecentOperation()
+
+    expect(buttonComponent.props.loading).toBe(true)
+    expect(createOperation.request.node.operation.name).toEqual(mutation.name)
+    expect(createOperation.request.variables).toEqual(mutation.variables)
+
+    act(() => mockEnvironment.mock.resolve(createOperation, MockPayloadGenerator.generate(createOperation)))
+
+    const refetchOperation = mockEnvironment.mock.getMostRecentOperation()
+    expect(refetchOperation.request.node.operation.name).toEqual("SavedSearchBannerRefetchQuery")
+
+    act(() => mockEnvironment.mock.resolveMostRecentOperation(MockPayloadGenerator.generate(refetchOperation)))
+
+    const popoverMessageInstance = tree.root.findByType(PopoverMessage)
+    const textInstances = popoverMessageInstance.findAllByType(Text)
+
+    expect(textInstances[0].props.children).toEqual(popover.title)
+    expect(textInstances[1].props.children).toEqual(popover.message)
+  }
+
   it("renders correctly disabled state", () => {
     const tree = renderWithWrappers(<TestRenderer />)
     mockEnvironmentPayload(mockEnvironment, {
@@ -76,5 +122,52 @@ describe("SavedSearchBanner", () => {
     const buttonComponent = tree.root.findByType(Button)
 
     expect(buttonComponent.props.loading).toBe(true)
+  })
+
+  it("createSavedSearch mutation is handled correctly", async () => {
+    const mockResolvers = {
+      Me: () => ({
+        savedSearch: null,
+      }),
+    }
+    const mutation: Mutation = {
+      name: "SavedSearchBannerCreateSavedSearchMutation",
+      variables: {
+        input: {
+          attributes,
+        },
+      },
+    }
+    const popover: Popover = {
+      title: "Your alert has been set.",
+      message: "We will send you a push notification once new works are added.",
+    }
+
+    checkLogicForMutations(mockResolvers, mutation, popover)
+  })
+
+  it("deleteSavedSearch mutation is handled correctly", async () => {
+    const savedSearchCriteriaId = "some-unique-name"
+    const mockResolvers = {
+      Me: () => ({
+        savedSearch: {
+          internalID: savedSearchCriteriaId,
+        },
+      }),
+    }
+    const mutation: Mutation = {
+      name: "SavedSearchBannerDeleteSavedSearchMutation",
+      variables: {
+        input: {
+          searchCriteriaID: savedSearchCriteriaId,
+        },
+      },
+    }
+    const popover: Popover = {
+      title: "Your alert has been removed.",
+      message: "Don't worry, you can always create a new one.",
+    }
+
+    checkLogicForMutations(mockResolvers, mutation, popover)
   })
 })
