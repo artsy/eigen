25ae0d429c30dd33ac7d47bbf5230ac1d03503da
Mounir Dhahri <soussouheros@gmail.com>
2021-06-23 18:33:01 +0200

    feat: check if no files were changed

--- scripts/changelog/commitChangelog.js
+++ scripts/changelog/commitChangelog.js
@@ -1,10 +1,22 @@
 // @ts-check
 "use strict"
-const { execSync } = require("child_process")
+const { execSync, spawnSync } = require("child_process")
 const updatePlatfromChangeLog = require("./generatePlatformChangelog").updatePlatfromChangeLog
 const ora = require("ora")
 
 const DEFAULT_CHANGELOG_BRANCH = "update-changelog"
+
+
+/**
+ * @param {string} command
+ */
+const exec = (command) => {
+  const task = spawnSync(command, { shell: true })
+  if (task.status != 0) {
+    console.log(task.stderr.toString())
+  }
+  return task.stdout.toString()
+}
 
 /**
  * Checks out the branch, creating it if it doesn't already exist
@@ -21,13 +33,27 @@ const forceCheckout = () => {
   }
 }
 
+/**
+ * Checks if no file is changed
+ */
+const hasNoChanges = () => {
+  // @ts-ignore
+  return exec("git status --porcelain") === ""
+}
+
 const main = async () => {
   // Make sure we are on a clean branch and checkout to it
   forceCheckout()
   // Run the changelog updater
   await Promise.all([updatePlatfromChangeLog("android", "beta"), updatePlatfromChangeLog("ios", "beta")])
+
   // Check if we have any changes in the changelog
-  // If yes, no further action neeeded, quit
+  const hasChanges = hasNoChanges()
+  // If no changes were found, no further action needed, quit
+  if (hasChanges) {
+    ora("no changes were made, no further action needed")
+    return
+  }
   // Commit and push changes to the update changelog branch
   // Check if there is an open PR already
   // If yes, no further action needed, quit
