5361bef5b9743cd256f443ba79261d5e8e34865f
Mounir Dhahri <soussouheros@gmail.com>
2021-06-23 19:14:53 +0200

    chore: update tests

--- scripts/changelog/generatePlatformChangelog-tests.ts
+++ scripts/changelog/generatePlatformChangelog-tests.ts
@@ -45,14 +45,14 @@ describe("getCombinedChangeLog", () => {
 })
 
 describe("getPlaformSpecificChangeLog", () => {
-  it("gets the combined changelog from all prs", () => {
+  it("gets the combined changelog from all prs on app store updates", () => {
     const changelog = {
       androidUserFacingChanges: [],
       crossPlatformUserFacingChanges: ["Added a new button", "Added an other button", "Fixed modal close button"],
       devChanges: ["Improved changelog tooling", "Upgraded lodash"],
       iOSUserFacingChanges: ["fixed button on iOS"],
     }
-    expect(getPlaformSpecificChangeLog("ios", changelog)).toEqual(
+    expect(getPlaformSpecificChangeLog("ios", changelog, "appstore")).toEqual(
       `
 ## Released Changes
 
@@ -74,14 +74,43 @@ describe("getPlaformSpecificChangeLog", () => {
 `
     )
   })
-  it("returns nothing when nothing changed", () => {
+  it("gets the combined changelog from all prs on beta updates", () => {
+    const changelog = {
+      androidUserFacingChanges: [],
+      crossPlatformUserFacingChanges: ["Added a new button", "Added an other button", "Fixed modal close button"],
+      devChanges: ["Improved changelog tooling", "Upgraded lodash"],
+      iOSUserFacingChanges: ["fixed button on iOS"],
+    }
+    expect(getPlaformSpecificChangeLog("ios", changelog, "beta")).toEqual(`
+## Undeployed Changes
+
+### v6.9.4
+
+- Status: **Beta**
+- Changelog update date: **Fri Jun 04 2021 15:48:24 GMT+0200 (Central European Summer Time)**
+- Changelog:
+
+  - User facing changes:
+    - Added a new button
+    - Added an other button
+    - Fixed modal close button
+    - fixed button on iOS
+
+  - Dev changes:
+    - Improved changelog tooling
+    - Upgraded lodash
+
+<!-- DO NOT CHANGE -->
+## Released Changes`)
+  })
+  it("returns nothing when nothing changed on app store updates", () => {
     const emptyChangeLog = {
       androidUserFacingChanges: [],
       crossPlatformUserFacingChanges: [],
       devChanges: [],
       iOSUserFacingChanges: [],
     }
-    expect(getPlaformSpecificChangeLog("ios", emptyChangeLog)).toEqual(
+    expect(getPlaformSpecificChangeLog("ios", emptyChangeLog, "appstore")).toEqual(
       `
 ## Released Changes
 
@@ -91,6 +120,28 @@ describe("getPlaformSpecificChangeLog", () => {
 - App store submission date: **Fri Jun 04 2021 15:48:24 GMT+0200 (Central European Summer Time)**
 - Changelog:
 `
+    )
+  })
+
+  it("returns nothing when nothing changed on beta updates", () => {
+    const emptyChangeLog = {
+      androidUserFacingChanges: [],
+      crossPlatformUserFacingChanges: [],
+      devChanges: [],
+      iOSUserFacingChanges: [],
+    }
+    expect(getPlaformSpecificChangeLog("ios", emptyChangeLog, "beta")).toEqual(
+      `
+## Undeployed Changes
+
+### v6.9.4
+
+- Status: **Beta**
+- Changelog update date: **Fri Jun 04 2021 15:48:24 GMT+0200 (Central European Summer Time)**
+- Changelog:
+
+<!-- DO NOT CHANGE -->
+## Released Changes`
     )
   })
 })
