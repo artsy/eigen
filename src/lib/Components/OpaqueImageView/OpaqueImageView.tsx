dff8fbe58cbd5a6f0a96aab3cbc3ecc746ce71e9
George Kartalis <g.kartalis7@gmail.com>
2021-06-23 15:20:54 +0200

    wip: refactor opaqueImageView to use RNfastImage

--- src/lib/Components/OpaqueImageView/OpaqueImageView.tsx
+++ src/lib/Components/OpaqueImageView/OpaqueImageView.tsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react"
-import FastImage, { FastImageProps } from "react-native-fast-image"
-import { Image, LayoutChangeEvent, PixelRatio, Platform, processColor, StyleSheet, View, ViewProps } from "react-native"
-
 import colors from "lib/data/colors"
+import _ from "lodash"
+import React, { useCallback, useRef, useState } from "react"
+import { Animated, PixelRatio, StyleSheet, View } from "react-native"
+import FastImage, { ImageStyle } from "react-native-fast-image"
 import { createGeminiUrl } from "./createGeminiUrl"
-import { Text } from "palette"
 
-interface Props extends ViewProps {
+interface Props {
   /** The URL from where to fetch the image. */
-  imageURL: string | null
+  imageURL?: string | null
 
   /**
    * By default we fetch a resized version of the image from gemini
@@ -21,8 +20,7 @@ interface Props extends ViewProps {
    * imageURLs that previously failed
    * Use this option to override that behavior and refetch
    */
-  // retryFailedURLs?: boolean
-  // mallon mporei na figei
+  retryFailedURLs?: boolean
 
   /** The background color for the image view */
   placeholderBackgroundColor?: string
@@ -41,31 +39,47 @@ interface Props extends ViewProps {
   aspectRatio?: number
 
   /** A callback that is called once the image is loaded. */
-  // onLoad?: () => void
+  onLoad?: () => void
 
   /**
    * Turn off the fade-in animation
    */
-  // noAnimation?: boolean
-  // test it sto deep zoom
+  noAnimation?: boolean
+  // TODO: need to test it in deep zoom
 
   /**
    * prevents `onLoad` from being called if the image fails to load
    */
-  // failSilently?: boolean
-  // test it sto deep zoom
+  failSilently?: boolean
+  // TODO: need to test it in deep zoom
 
   /**
    * renders the image at a higher threading priority level ('interactive')
    */
   highPriority?: boolean
+
+  style?: ImageStyle[] | ImageStyle
+}
+
+const useComponentSize = () => {
+  const [layoutHeight, setLayoutHeight] = useState(0)
+  const [layoutWidth, setLayoutWidth] = useState(0)
+
+  const onLayout = useCallback((event) => {
+    const { width, height } = event.nativeEvent.layout
+    const scale = PixelRatio.get()
+    setLayoutHeight(height * scale)
+    setLayoutWidth(width * scale)
+  }, [])
+
+  return { layoutHeight, layoutWidth, onLayout }
 }
 
 export const OpaqueImageView: React.FC<Props> = ({ placeholderBackgroundColor = colors["gray-regular"], ...props }) => {
   // Unless `aspectRatio` was not specified at all, default the ratio to 1 to prevent illegal layout calculations.
   const aspectRatio = useState(props.aspectRatio === undefined ? undefined : props.aspectRatio ?? 1)
-  const [width, setWidth] = useState<number | undefined>()
-  const [height, setHeight] = useState<number | undefined>()
+  const { layoutHeight, layoutWidth, onLayout } = useComponentSize()
+  const imageScaleValue = useRef(new Animated.Value(0)).current
 
   if (__DEV__) {
     const style = StyleSheet.flatten(props.style)
@@ -88,7 +102,7 @@ export const OpaqueImageView: React.FC<Props> = ({ placeholderBackgroundColor = 
     return null
   }
 
-  const imageURL = () => {
+  const getImageURL = () => {
     const { imageURL, useRawURL } = props
 
     if (imageURL) {
@@ -97,71 +111,70 @@ export const OpaqueImageView: React.FC<Props> = ({ placeholderBackgroundColor = 
       }
       return createGeminiUrl({
         imageURL,
-        width: width!,
-        height: height!,
+        width: layoutWidth,
+        height: layoutHeight,
         // Either scale or crop, based on if an aspect ratio is available.
         resizeMode: aspectRatio ? "fit" : "fill",
       })
     }
 
-    return null
+    return
   }
-
-  // onLayout = (event: LayoutChangeEvent) => {
-  //   const { width, height } = event.nativeEvent.layout
-  //   console.log("ON LAYOUTTTTT ")
-  //   this.setState({
-  //     width: width,
-  //     height: height,
-  //   })
-  // }
-
-  // const isLaidOut = !!(this.state.width && this.state.height)
-  // const { style, ...props } = this.props
-
-  // Object.assign(props, {
-  //   aspectRatio,
-  //   // imageURL: isLaidOut ? this.imageURL() : null,
-  //   onLayout: this.onLayout,
-  // })
 
   // If no imageURL is given at all, simply set the placeholder background color as a view backgroundColor style so
   // that it shows immediately.
-  let backgroundColorStyle = null
-  // let remainderProps = props
-  // if (Platform.OS === "ios" && this.props.imageURL) {
-  //   const anyProps = props as any
-  // anyProps.placeholderBackgroundColor = processColor(props.placeholderBackgroundColor)
-  // } else {
-  //   const { placeholderBackgroundColor, ...remainder } = props
-  //   remainderProps = remainder
-  //   backgroundColorStyle = { backgroundColor: props.placeholderBackgroundColor }
-  // }
+  const backgroundColorStyle = colors["gray-regular"]
 
-  // if (props.imageURL === null) {
-  return <View style={{ backgroundColor: "red", width: props.width, height: props.height }} />
-  // }
+  const onImageLoadEnd = () => {
+    if (props.onLoad) {
+      props.onLoad()
+    }
+    if (props.noAnimation) {
+      Animated.timing(imageScaleValue, {
+        toValue: 0,
+        duration: 0.25,
+        useNativeDriver: true,
+      }).start()
+    }
+  }
+
+  if (!props.imageURL) {
+    return (
+      <View
+        style={{
+          backgroundColor: backgroundColorStyle,
+          width: props.width ?? layoutWidth,
+          height: props.height ?? layoutHeight,
+        }}
+      />
+    )
+  }
+
   return (
     <FastImage
-      style={{ width: props.width, height: props.height }}
-      // style={[style, backgroundColorStyle] as any}
-      // {...remainderProps}
-      // source={{ uri: remainderProps.imageURL! }}
+      {...props}
+      onLayout={onLayout}
+      style={[
+        { height: props.height ?? layoutHeight, width: props.width ?? layoutWidth },
+        props.style,
+        { backgroundColor: backgroundColorStyle },
+      ]}
       source={{
-        uri: null,
-        //props.imageURL,
+        uri: getImageURL(),
         priority: props.highPriority ? "high" : undefined,
       }}
-    />
+      onLoadEnd={onImageLoadEnd}
+    >
+      <Animated.View
+        style={[
+          { opacity: imageScaleValue },
+          { height: props.height, width: props.width },
+          props.style,
+          { backgroundColor: backgroundColorStyle },
+        ]}
+      />
+    </FastImage>
   )
-
-  // return (
-  //   <Image
-  //     style={[style, backgroundColorStyle] as any}
-  //     {...remainderProps}
-  //     source={{ uri: remainderProps.imageURL! }}
-  //   />
-  // )
 }
 
 export default OpaqueImageView
