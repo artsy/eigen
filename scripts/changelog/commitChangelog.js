2861a5d21f422691a485cbe70aa9912656376f39
Mounir Dhahri <soussouheros@gmail.com>
2021-06-23 18:23:13 +0200

    create work draft

---
+++ scripts/changelog/commitChangelog.js
@@ -0,0 +1,13 @@
+// @ts-check
+"use strict"
+
+const main = async () => {
+  // Make sure we are on a clean branch and checkout to it
+  // Run the changelog updater
+  // Check if we have any changes in the changelog
+  // If yes, no further action neeeded, quit
+  // Commit and push changes to the update changelog branch
+  // Check if there is an open PR already
+  // If yes, no further action needed, quit
+  // Create a pull request and merge it
+}
