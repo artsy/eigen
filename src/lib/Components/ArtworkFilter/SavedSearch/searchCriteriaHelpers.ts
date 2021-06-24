dd70acb526483f83b58413611239c8a706b953fd
Dima Tretyak <itretyak@ya.ru>
2021-06-24 20:57:52 +0300

    feat: add search criteria helpers

---
+++ src/lib/Components/ArtworkFilter/SavedSearch/searchCriteriaHelpers.ts
@@ -0,0 +1,25 @@
+import { isNull } from "lodash"
+import { SearchCriteriaAttributes } from "./types"
+
+export const getOnlyFilledSearchCriteriaValues = (searchCriteria: SearchCriteriaAttributes | null) => {
+  const prepared: Record<string, any> = {}
+
+  if (searchCriteria) {
+    const filledSavedSearchCriteria = Object.entries(searchCriteria).filter((entry) => {
+      const [_, value] = entry
+
+      if (Array.isArray(value)) {
+        return value.length > 0
+      }
+
+      return !isNull(value)
+    })
+
+    filledSavedSearchCriteria.forEach((criteria) => {
+      const [key, value] = criteria
+      prepared[key] = value
+    })
+  }
+
+  return prepared
+}
