7a0ca948b0544ba86ee822a3099a7209da6abe38
Ozzie Vasdi <29984068+ovasdi@users.noreply.github.com>
2021-07-02 13:21:06 -0400

    add no saved addresses content and styles

--- src/lib/Scenes/SavedAddresses/SavedAddresses.tsx
+++ src/lib/Scenes/SavedAddresses/SavedAddresses.tsx
@@ -1,14 +1,20 @@
 import { PageWithSimpleHeader } from "lib/Components/PageWithSimpleHeader"
-import { Flex, Sans } from "palette"
+import { Button, Flex, Text } from "palette"
 import React from "react"
 
 export const SavedAddresses: React.FC = () => {
   return (
     <PageWithSimpleHeader title="Saved Addresses">
-      <Flex py={3} px={2} alignItems="center">
-        <Sans size="4t" color="black100" weight="regular">
+      <Flex py={3} px={2} alignItems="center" height="100%" justifyContent="center">
+        <Text variant="title" mb={2}>
           No Saved Addresses
-        </Sans>
+        </Text>
+        <Text variant="caption" textAlign="center" mb={3}>
+          Please add an address for a faster checkout experience in the future.
+        </Text>
+        <Button block variant="primaryBlack" width={100}>
+          Add New Address
+        </Button>
       </Flex>
     </PageWithSimpleHeader>
   )
