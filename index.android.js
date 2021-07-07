84e1d0f30535e99ec60111957ee4da8a50647ad7
Kizito Egeonu <kizito.egeonu@gmail.com>
2021-07-07 20:56:10 +0200

    Feat(cx-1635): initial android push notification setup (#5084)
    * initial native build setup
    
    * notification icons added
    
    * initial react native push notification config added
    
    * added google-services.json
    
    * added push notification feature flag
    
    * Push setup tests
    
    * changelog entry
    
    * move types to devDependencies
    
    Co-authored-by: Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>

--- index.android.js
+++ index.android.js
@@ -1,6 +1,7 @@
 global.__TEST__ = false
 require("react-native-gesture-handler")
 require("react-native-screens").enableScreens()
+require("./src/lib/utils/PushNotification").configure()
 const { AppRegistry } = require('react-native')
 
 const {App} = require('./src/lib/AndroidApp')
