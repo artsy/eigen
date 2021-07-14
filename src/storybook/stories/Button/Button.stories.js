ec0ee88327a324583a7cb3eef47f838f1c2f81aa
Ole <ole.richter91@gmail.com>
2021-07-14 12:24:50 +0200

    fix test

--- src/storybook/stories/Button/Button.stories.js
+++ src/storybook/stories/Button/Button.stories.js
@@ -3,18 +3,69 @@ import { text } from "@storybook/addon-knobs"
 import { storiesOf } from "@storybook/react-native"
 import React from "react"
 import { Text } from "react-native"
-import { Button } from "palette"
+import { Button, Spacer } from "palette"
 import CenterView from "../CenterView"
 
-storiesOf("Button", module)
+const sizes = ["small", "medium", "large"]
+const variants = [
+  "primaryBlack",
+  "primaryWhite",
+  "secondaryGray",
+  "secondaryOutline",
+  "secondaryOutlineWarning",
+  "noOutline",
+]
+
+const buttonStories = storiesOf("Button", module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add("with text", () => (
-    <Button onPress={action("clicked-text")}>
-      <Text>{text("Button text", "Hello Button")}</Text>
-    </Button>
+  .add("Sizes", () => (
+    <List>
+      {sizes.map((size) => (
+        <Button size={size}>
+          <Text>{text("Button text", "Hello")}</Text>
+        </Button>
+      ))}
+    </List>
   ))
-  .add("with some emoji", () => (
-    <Button onPress={action("clicked-emoji")}>
-      <Text>😀 😎 👍 💯</Text>
-    </Button>
+  .add("Variants", () => (
+    <List>
+      {variants.map((variant) => (
+        <Button variant={variant}>
+          <Text>{text("Button text", "Hello")}</Text>
+        </Button>
+      ))}
+    </List>
   ))
+
+const List = ({ children }) => (
+  <>
+    {children.map((item) => (
+      <>
+        {item}
+        <Spacer mb={2} />
+      </>
+    ))}
+  </>
+)
+
+// sizes.forEach((size) => {
+//   buttonStories.add(size, () => (
+//     <Button size={size}>
+//       <Text>{text("Button text", "Hello")}</Text>
+//     </Button>
+//   ))
+// })
+
+// variants.forEach((variant) => {
+//   buttonStories.add(variant, () => (
+//     <Button variant={variant}>
+//       <Text>{text("Button text", "Hello")}</Text>
+//     </Button>
+//   ))
+// })
+
+// .add("with some emoji", () => (
+//   <Button onPress={action("clicked-emoji")}>
+//     <Text>😀 😎 👍 💯</Text>
+//   </Button>
+// ))
