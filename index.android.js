8bf16519c5c99ec5305bfa8abaaa3b7fea37b89a
Ole <ole.richter91@gmail.com>
2021-07-14 11:39:28 +0200

    fix type errors

---
+++ index.android.js
@@ -0,0 +1,23 @@
+const { Platform } = require("react-native")
+
+global.__TEST__ = false
+
+// start storybook depending on content of storybook.json
+let startStorybook = false
+
+try {
+  const fileContent = require("./storybook.json")
+  startStorybook = fileContent.startStorybook
+} catch {}
+
+if (startStorybook) {
+  console.log("Starting storybook...")
+  require("./src/storybook")
+} else {
+  require("react-native-gesture-handler")
+  require("react-native-screens").enableScreens()
+  require("./src/lib/utils/PushNotification").configure()
+  const { AppRegistry } = require("react-native")
+  const { App } = require("./src/lib/AndroidApp")
+  AppRegistry.registerComponent("Artsy", () => App)
+}
