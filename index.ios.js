4b8ca2020bb8b83729e80c01f9a61fa7d4f7fead
Ole <ole.richter91@gmail.com>
2021-07-14 11:39:28 +0200

    fix type errors

---
+++ index.ios.js
@@ -0,0 +1,21 @@
+const { Platform } = require("react-native")
+
+global.__TEST__ = false
+
+// start storybook depending on content of storybook.json
+let startStorybook = false
+
+try {
+  const fileContent = require("./storybook.json")
+  startStorybook = fileContent.startStorybook
+} catch {}
+
+if (startStorybook) {
+  console.log("Starting storybook...")
+  require("./src/storybook")
+} else {
+  require("react-native-gesture-handler")
+  require("react-native-screens").enableScreens()
+  require("./src/lib/ErrorReporting")
+  require("./src/lib/AppRegistry")
+}
