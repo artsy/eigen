047cb22fd89f39812bdc5570eef4960631914410
George Kartalis <g.kartalis7@gmail.com>
2021-07-15 16:19:57 +0200

    spec: delete mutation test attempt

--- src/lib/Scenes/SavedAddresses/__tests__/SavedAddresses-tests.tsx
+++ src/lib/Scenes/SavedAddresses/__tests__/SavedAddresses-tests.tsx
@@ -1,13 +1,19 @@
 import { SavedAddressesTestsQuery } from "__generated__/SavedAddressesTestsQuery.graphql"
 import { extractText } from "lib/tests/extractText"
 import { renderWithWrappers } from "lib/tests/renderWithWrappers"
-import { Flex } from "palette"
+import { Flex, Touchable } from "palette"
 import React from "react"
 import { graphql, QueryRenderer } from "react-relay"
+import { act } from "react-test-renderer"
 import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
 import { SavedAddressesContainer, SavedAddressesQueryRenderer } from "../SavedAddresses"
 
 jest.unmock("react-relay")
+
+interface Mutation {
+  name: string
+  variables: Record<string, any>
+}
 
 describe(SavedAddressesQueryRenderer, () => {
   let mockEnvironment: ReturnType<typeof createMockEnvironment>
@@ -52,46 +58,48 @@ describe(SavedAddressesQueryRenderer, () => {
     expect(tree.findAllByType(Flex)[4].props.children[0].props.children).toEqual("No Saved Addresses")
   })
 
+  const mockedAddressesResponse = {
+    Me: () => ({
+      name: "saver",
+      addressConnection: {
+        edges: [
+          {
+            node: {
+              id: "VXNlckFkZHJlc3M6NTg0MA==",
+              internalID: "5840",
+              name: "George Tester",
+              addressLine1: "Stallschreiberstr 21",
+              addressLine2: "apt 61, 1st Floor",
+              addressLine3: null,
+              city: "Berlin",
+              region: "Mitte",
+              postalCode: "10179",
+              phoneNumber: "015904832846",
+            },
+          },
+          {
+            node: {
+              id: "VXNlckFkZHJlc3M6NTg2MQ==",
+              internalID: "5861",
+              name: "George Testing",
+              addressLine1: "401 Brodway",
+              addressLine2: "26th Floor",
+              addressLine3: null,
+              city: "New York",
+              region: "New York",
+              postalCode: "NY 10013",
+              phoneNumber: "1293581028945",
+            },
+          },
+        ],
+      },
+    }),
+  }
+
   it("should render the saved addresses on the screen", () => {
     const tree = renderWithWrappers(<TestRenderer />)
     mockEnvironment.mock.resolveMostRecentOperation((operation) => {
-      const result = MockPayloadGenerator.generate(operation, {
-        Me: () => ({
-          name: "saver",
-          addressConnection: {
-            edges: [
-              {
-                node: {
-                  id: "VXNlckFkZHJlc3M6NTg0MA==",
-                  internalID: "5840",
-                  name: "George Tester",
-                  addressLine1: "Stallschreiberstr 21",
-                  addressLine2: "apt 61, 1st Floor",
-                  addressLine3: null,
-                  city: "Berlin",
-                  region: "Mitte",
-                  postalCode: "10179",
-                  phoneNumber: "015904832846",
-                },
-              },
-              {
-                node: {
-                  id: "VXNlckFkZHJlc3M6NTg2MQ==",
-                  internalID: "5861",
-                  name: "George Testing",
-                  addressLine1: "401 Brodway",
-                  addressLine2: "26th Floor",
-                  addressLine3: null,
-                  city: "New York",
-                  region: "New York",
-                  postalCode: "NY 10013",
-                  phoneNumber: "1293581028945",
-                },
-              },
-            ],
-          },
-        }),
-      })
+      const result = MockPayloadGenerator.generate(operation, mockedAddressesResponse)
       return result
     })
     const text = extractText(tree.root)
@@ -108,5 +116,81 @@ describe(SavedAddressesQueryRenderer, () => {
 
     expect(text).toContain("Edit")
     expect(text).toContain("Delete")
+  })
+
+  it.only("should successfully delete address on delete button press", () => {
+    // set the mutation
+    const mutation: Mutation = {
+      name: "deleteSavedAddressDeleteUserAddressMutation",
+      variables: {
+        input: {
+          userAddressID: "5861",
+        },
+      },
+    }
+    // Do we actually need this? (took the idea from savedSearchBanner-tests)
+    // const mockResolvers = {
+    //   Me: () => ({
+    //     addressConnection: {
+    //       edges: [
+    //         {
+    //           node: {
+    //             id: "VXNlckFkZHJlc3M6NTg0MA==",
+    //             internalID: "5840",
+    //             name: "George Tester",
+    //             addressLine1: "Stallschreiberstr 21",
+    //             addressLine2: "apt 61, 1st Floor",
+    //             addressLine3: null,
+    //             city: "Berlin",
+    //             region: "Mitte",
+    //             postalCode: "10179",
+    //             phoneNumber: "015904832846",
+    //           },
+    //         },
+    //         {
+    //           node: {
+    //             id: "VXNlckFkZHJlc3M6NTg2MQ==",
+    //             internalID: "5861",
+    //             name: "George Testing",
+    //             addressLine1: "401 Brodway",
+    //             addressLine2: "26th Floor",
+    //             addressLine3: null,
+    //             city: "New York",
+    //             region: "New York",
+    //             postalCode: "NY 10013",
+    //             phoneNumber: "1293581028945",
+    //           },
+    //         },
+    //       ],
+    //     },
+    //   }),
+    // }
+
+    // do the initial setup
+    const tree = renderWithWrappers(<TestRenderer />)
+    mockEnvironment.mock.resolveMostRecentOperation((operation) => {
+      const result = MockPayloadGenerator.generate(operation, mockedAddressesResponse)
+      return result
+    })
+
+    // track the button
+    const buttons = tree.root.findAllByType(Touchable)
+    const firstDeleteButton = buttons[1]
+
+    act(() => firstDeleteButton.props.onPress())
+    // this breaks
+    const createOperation = mockEnvironment.mock.getMostRecentOperation()
+
+    expect(createOperation.request.node.operation.name).toEqual(mutation.name)
+    expect(createOperation.request.variables).toEqual(mutation.variables)
+
+    act(() => mockEnvironment.mock.resolve(createOperation, MockPayloadGenerator.generate(createOperation)))
+    // // lsls
+
+    // const refetchOperation = mockEnvironment.mock.getMostRecentOperation()
+    // expect(refetchOperation.request.node.operation.name).toEqual("SavedSearchBannerRefetchQuery")
+    // act(() => mockEnvironment.mock.resolveMostRecentOperation(MockPayloadGenerator.generate(refetchOperation)))
+
+    // assert that the address card is unmounted
   })
 })
