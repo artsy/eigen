5f183f20952cd74265e7069fb8ab75b5219f7360
George Kartalis <g.kartalis7@gmail.com>
2021-07-21 15:15:18 +0200

    feat: add new RouteMatcher for SavedAddressForm

--- src/lib/navigation/routes.tsx
+++ src/lib/navigation/routes.tsx
@@ -170,7 +170,8 @@ function getDomainMap(): Record<string, RouteMatcher[] | null> {
     new RouteMatcher("/my-profile/payment/new-card", "MyProfilePaymentNewCreditCard"),
     new RouteMatcher("/my-profile/push-notifications", "MyProfilePushNotifications"),
     new RouteMatcher("/my-profile/saved-addresses", "SavedAddresses"),
-    new RouteMatcher("/my-profile/saved-addresses/new-address", "SavedAddressesNewForm"),
+    new RouteMatcher("/my-profile/saved-addresses/new-address", "SavedAddressesForm"),
+    new RouteMatcher("/my-profile/saved-addresses/edit-address", "SavedAddressesForm"),
     new RouteMatcher("/local-discovery", "LocalDiscovery"),
     new RouteMatcher("/privacy-request", "PrivacyRequest"),
 
