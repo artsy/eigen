2223633ec6df7d32e9ac0c5848349d8f71baa324
Pavlos Vinieratos <pvinis@gmail.com>
2021-07-23 16:47:30 +0100

    more

---
+++ src/storybook/storyLoader.ts
@@ -0,0 +1,15 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require("./stories/Button.stories")
+}
+
+const stories = ["./stories/Button.stories"]
+
+module.exports = {
+  loadStories,
+  stories,
+}
--- src/storybook/storybook-ui.ts
+++ src/storybook/storybook-ui.ts
@@ -1,18 +1,20 @@
-import { getStorybookUI, configure, addDecorator } from "@storybook/react-native"
 import { withKnobs } from "@storybook/addon-knobs"
-
+import { addDecorator, configure, getStorybookUI } from "@storybook/react-native"
 import "./rn-addons"
+import { loadStories } from "./storyLoader"
 
 // enables knobs for all stories
 addDecorator(withKnobs)
 
 // import stories
 configure(() => {
-  require("./stories")
+  loadStories()
 }, module)
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({})
+const StorybookUIRoot = getStorybookUI({
+  onDeviceUI: false,
+})
 
 export default StorybookUIRoot
