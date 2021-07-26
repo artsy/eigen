3f8c531547487248a07b55dd04a919bdb457eb4f
Dima Tretyak <itretyak@ya.ru>
2021-07-26 14:59:17 +0300

    feat: add saved search alerts list scene

---
+++ src/lib/Scenes/SavedSearchAlert/SavedSearchAlertsList.tsx
@@ -0,0 +1,10 @@
+import { Box, Sans } from 'palette'
+import React from "react"
+
+export const SavedSearchAlertsList: React.FC<{}> = () => {
+  return (
+    <Box>
+      <Sans size="8">Create an Alert</Sans>
+    </Box>
+  )
+}
