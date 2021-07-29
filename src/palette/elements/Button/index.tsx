2a179bb3d534eaadc09d49bda5d6556939c76e52
Ole <ole.richter91@gmail.com>
2021-07-29 15:15:29 +0200

    fix another test

--- src/palette/elements/Button/index.tsx
+++ src/palette/elements/Button/index.tsx
@@ -5,12 +5,12 @@ import { Button as ButtonV3, ButtonProps as ButtonPropsV3 } from "./Button"
 import { ButtonProps as ButtonPropsV2, ButtonV2 } from "./ButtonV2"
 export * from "./Button"
 
-export const Button: React.FC<ButtonPropsV2 & ButtonPropsV3> = (props) => {
+export const Button: React.FC<ButtonPropsV3> = (props) => {
   const allowV3 = usePaletteFlagStore((state) => state.allowV3)
 
   if (allowV3) {
     return <ButtonV3 {...props} />
   }
 
-  return <ButtonV2 {...props} />
+  return <ButtonV2 {...(props as ButtonPropsV2)} />
 }
