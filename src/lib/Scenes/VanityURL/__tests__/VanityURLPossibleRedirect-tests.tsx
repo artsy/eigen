ca5e9440cc2305df51371a3c0dd702efd7e9dbe6
George Kartalis <g.kartalis7@gmail.com>
2021-07-30 15:09:27 +0200

    fix: refactored fetchMock in tests and add helper
    Co-authored-by: Devon Blandin <dblandin@gmail.com>
    Co-authored-by: Pavlos Vinieratos <pvinis@gmail.com>

--- src/lib/Scenes/VanityURL/__tests__/VanityURLPossibleRedirect-tests.tsx
+++ src/lib/Scenes/VanityURL/__tests__/VanityURLPossibleRedirect-tests.tsx
@@ -1,15 +1,15 @@
+import fetchMock from "jest-fetch-mock"
 import { ArtsyWebView } from "lib/Components/ArtsyWebView"
 import { navigate } from "lib/navigation/navigate"
 import { __globalStoreTestUtils__ } from "lib/store/GlobalStore"
 import { extractText } from "lib/tests/extractText"
+import { fetchMockResponseOnce } from "lib/tests/fetchMockHelpers"
 import { flushPromiseQueue } from "lib/tests/flushPromiseQueue"
 import { renderWithWrappers } from "lib/tests/renderWithWrappers"
 import { Button, Spinner } from "palette"
 import React from "react"
 import { Linking } from "react-native"
 import { VanityURLPossibleRedirect } from "../VanityURLPossibleRedirect"
-
-const fetchMock = ((global as any).fetch = jest.fn())
 
 beforeEach(() => {
   __globalStoreTestUtils__?.setProductionMode()
@@ -18,17 +18,18 @@ beforeEach(() => {
 
 describe(VanityURLPossibleRedirect, () => {
   it("shows a loading spinner before anything has happened", () => {
-    fetchMock.mockResolvedValueOnce({
-      ok: true,
+    fetchMockResponseOnce({
+      status: 200,
       url: "https://artsy.net/test",
     })
+
     const tree = renderWithWrappers(<VanityURLPossibleRedirect slug="test" />)
     expect(tree.root.findAllByType(Spinner)).toHaveLength(1)
   })
 
   it("sends a fetch request", () => {
-    fetchMock.mockResolvedValueOnce({
-      ok: true,
+    fetchMockResponseOnce({
+      status: 200,
       url: "https://artsy.net/test",
     })
     renderWithWrappers(<VanityURLPossibleRedirect slug="test" />)
@@ -47,18 +48,19 @@ describe(VanityURLPossibleRedirect, () => {
   })
 
   it("calls `navigate` when the redirect points to an external url", async () => {
-    fetchMock.mockResolvedValueOnce({
-      ok: true,
+    fetchMockResponseOnce({
+      status: 200,
       url: "https://google.com/blah",
     })
+
     renderWithWrappers(<VanityURLPossibleRedirect slug="test" />)
     await flushPromiseQueue()
     expect(navigate).toHaveBeenCalledWith("https://google.com/blah")
   })
 
   it("calls `navigate` when the redirect points to a native view", async () => {
-    fetchMock.mockResolvedValueOnce({
-      ok: true,
+    fetchMockResponseOnce({
+      status: 200,
       url: "https://artsy.net/artist/banksy",
     })
     renderWithWrappers(<VanityURLPossibleRedirect slug="test" />)
@@ -80,7 +82,7 @@ describe(VanityURLPossibleRedirect, () => {
     })
 
     it("shows the error page if the fetch fails", async () => {
-      fetchMock.mockRejectedValueOnce({})
+      fetchMock.mockRejectOnce()
       const tree = renderWithWrappers(<VanityURLPossibleRedirect slug="test-fail" />)
       await flushPromiseQueue()
       expect(extractText(tree.root)).toContain("We can't find that page")
@@ -90,7 +92,10 @@ describe(VanityURLPossibleRedirect, () => {
     })
 
     it("shows the error page if the response is not `ok`", async () => {
-      fetchMock.mockResolvedValueOnce({ ok: false, url: "https://whatever" })
+      fetchMockResponseOnce({
+        status: 404,
+        url: "https://whatever",
+      })
       const tree = renderWithWrappers(<VanityURLPossibleRedirect slug="test-not-ok" />)
       await flushPromiseQueue()
       expect(extractText(tree.root)).toContain("We can't find that page")
@@ -101,14 +106,20 @@ describe(VanityURLPossibleRedirect, () => {
   })
 
   it("shows an internal web view when there is no redirect", async () => {
-    fetchMock.mockResolvedValueOnce({ ok: true, url: "https://artsy.net/no-redirect" })
+    fetchMockResponseOnce({
+      status: 200,
+      url: "https://artsy.net/no-redirect",
+    })
     const tree = renderWithWrappers(<VanityURLPossibleRedirect slug="no-redirect" />)
     await flushPromiseQueue()
     expect(tree.root.findAllByType(ArtsyWebView)).toHaveLength(1)
   })
 
   it("shows an internal web view when there is a redirect to a page that is supposed to be shown in a web view", async () => {
-    fetchMock.mockResolvedValueOnce({ ok: true, url: "https://artsy.net/categories" })
+    fetchMockResponseOnce({
+      status: 200,
+      url: "https://artsy.net/categories",
+    })
     const tree = renderWithWrappers(<VanityURLPossibleRedirect slug="genes" />)
     await flushPromiseQueue()
     expect(tree.root.findAllByType(ArtsyWebView)).toHaveLength(1)
