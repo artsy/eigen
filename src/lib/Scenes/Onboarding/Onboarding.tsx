fa9f82e7ec2b38d837e5325c91a949a508d87aaf
Mounir Dhahri <soussouheros@gmail.com>
2021-08-03 14:27:57 +0200

    add routes

--- src/lib/Scenes/Onboarding/Onboarding.tsx
+++ src/lib/Scenes/Onboarding/Onboarding.tsx
@@ -16,41 +16,59 @@ import { OnboardingWelcome } from "./OnboardingWelcome"
 // tslint:disable-next-line:interface-over-type-literal
 export type OnboardingNavigationStack = {
   OnboardingWelcome: undefined
-  OnboardingLogin: undefined
+  OnboardingLogin: { withFadeAnimation: boolean } | undefined
   OnboardingLoginWithEmail: { withFadeAnimation: boolean; email: string } | undefined
-  OnboardingCreateAccount: undefined
+  OnboardingCreateAccount: { withFadeAnimation: boolean } | undefined
   OnboardingCreateAccountWithEmail: undefined
   ForgotPassword: undefined
 }
 
 const StackNavigator = createStackNavigator<OnboardingNavigationStack>()
 
-export const OnboardingWelcomeScreens = () => (
-  <NavigationContainer independent>
-    <StackNavigator.Navigator
-      headerMode="screen"
-      screenOptions={{
-        ...TransitionPresets.SlideFromRightIOS,
-        headerShown: false,
-      }}
-    >
-      <StackNavigator.Screen name="OnboardingWelcome" component={OnboardingWelcome} />
-      <StackNavigator.Screen name="OnboardingLogin" component={OnboardingLogin} />
-      <StackNavigator.Screen
-        name="OnboardingLoginWithEmail"
-        component={OnboardingLoginWithEmail}
-        options={({ route: { params } }) => ({
-          cardStyleInterpolator: params?.withFadeAnimation
-            ? CardStyleInterpolators.forFadeFromBottomAndroid
-            : CardStyleInterpolators.forHorizontalIOS,
-        })}
-      />
-      <StackNavigator.Screen name="OnboardingCreateAccount" component={OnboardingCreateAccount} />
-      <StackNavigator.Screen name="OnboardingCreateAccountWithEmail" component={OnboardingCreateAccountWithEmail} />
-      <StackNavigator.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} />
-    </StackNavigator.Navigator>
-  </NavigationContainer>
-)
+export const OnboardingWelcomeScreens = () => {
+  return (
+    <NavigationContainer independent>
+      <StackNavigator.Navigator
+        headerMode="screen"
+        screenOptions={{
+          ...TransitionPresets.SlideFromRightIOS,
+          headerShown: false,
+        }}
+      >
+        <StackNavigator.Screen name="OnboardingWelcome" component={OnboardingWelcome} />
+        <StackNavigator.Screen
+          name="OnboardingLogin"
+          component={OnboardingLogin}
+          options={({ route: { params } }) => ({
+            cardStyleInterpolator: params?.withFadeAnimation
+              ? CardStyleInterpolators.forFadeFromBottomAndroid
+              : CardStyleInterpolators.forHorizontalIOS,
+          })}
+        />
+        <StackNavigator.Screen
+          name="OnboardingLoginWithEmail"
+          component={OnboardingLoginWithEmail}
+          options={({ route: { params } }) => ({
+            cardStyleInterpolator: params?.withFadeAnimation
+              ? CardStyleInterpolators.forFadeFromBottomAndroid
+              : CardStyleInterpolators.forHorizontalIOS,
+          })}
+        />
+        <StackNavigator.Screen
+          name="OnboardingCreateAccount"
+          component={OnboardingCreateAccount}
+          options={({ route: { params } }) => ({
+            cardStyleInterpolator: params?.withFadeAnimation
+              ? CardStyleInterpolators.forFadeFromBottomAndroid
+              : CardStyleInterpolators.forHorizontalIOS,
+          })}
+        />
+        <StackNavigator.Screen name="OnboardingCreateAccountWithEmail" component={OnboardingCreateAccountWithEmail} />
+        <StackNavigator.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} />
+      </StackNavigator.Navigator>
+    </NavigationContainer>
+  )
+}
 export const Onboarding = () => {
   const onboardingState = GlobalStore.useAppState((state) => state.auth.onboardingState)
 
