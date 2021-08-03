0304b0e5f8c0097028f0ed6481783c22ce53c47b
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2021-08-03 17:58:09 +0300

    feat(FX-3100): create saved search alert form (#5159)
    * feat: add Pill component to palette
    
    * feat: saved search alert form
    
    * chore: fix ESlint warnings
    
    * chore: remove extra text

--- src/lib/Scenes/SavedSearchAlert/CreateSavedSearchAlert.tsx
+++ src/lib/Scenes/SavedSearchAlert/CreateSavedSearchAlert.tsx
@@ -1,7 +1,9 @@
 import { FancyModal } from "lib/Components/FancyModal/FancyModal"
 import { FancyModalHeader } from "lib/Components/FancyModal/FancyModalHeader"
-import { Box, Text } from "palette"
+import { Sans, Text, useTheme } from "palette"
 import React from "react"
+import { ScrollView } from "react-native"
+import { SavedSearchAlertForm } from "./SavedSearchAlertForm"
 
 interface CreateSavedSearchAlertProps {
   visible: boolean
@@ -10,13 +12,18 @@ interface CreateSavedSearchAlertProps {
 
 export const CreateSavedSearchAlert: React.FC<CreateSavedSearchAlertProps> = (props) => {
   const { visible, onClosePress } = props
+  const { space } = useTheme()
 
   return (
     <FancyModal visible={visible} fullScreen>
       <FancyModalHeader useXButton hideBottomDivider onLeftButtonPress={onClosePress} />
-      <Box mx={2}>
-        <Text>Create alert screen</Text>
-      </Box>
+      <ScrollView contentContainerStyle={{ paddingHorizontal: space(2) }}>
+        <Sans size="8" mb={4}>Create an Alert</Sans>
+        <SavedSearchAlertForm onSaved={onClosePress} />
+        <Text variant="text" color="black60" textAlign="center" mt={2}>
+          You will be able to access all your Artist Alerts in your Profile.
+        </Text>
+      </ScrollView>
     </FancyModal>
   )
 }
