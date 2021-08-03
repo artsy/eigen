372e8a34f61090d5490b153a5ddaed21c6e45927
Pavlos Vinieratos <pvinis@gmail.com>
2021-08-03 16:49:11 +0100

    done

--- src/palette/elements/Button/__tests__/Button-tests.tsx
+++ src/palette/elements/Button/__tests__/Button-tests.tsx
@@ -2,19 +2,17 @@ import { fireEvent } from "@testing-library/react-native"
 import { renderWithWrappersTL } from "lib/tests/renderWithWrappers"
 import React from "react"
 import { TouchableWithoutFeedback } from "react-native"
-import { Button, ButtonProps } from "../Button"
+import { Button } from "../Button"
 
-describe("Button", () => {
-  const TestComponent = (props: Partial<ButtonProps>) => <Button {...props}>Hi</Button>
-
+describe(Button, () => {
   it("spinner doesn't show up when not loading", () => {
-    const { queryAllByTestId } = renderWithWrappersTL(<TestComponent />)
+    const { queryAllByTestId } = renderWithWrappersTL(<Button>Hi</Button>)
 
     expect(queryAllByTestId("spinner")).toHaveLength(0)
   })
 
   it("shows spinner if loading is true", () => {
-    const { queryAllByTestId } = renderWithWrappersTL(<TestComponent loading />)
+    const { queryAllByTestId } = renderWithWrappersTL(<Button loading>Hi</Button>)
 
     expect(queryAllByTestId("spinner")).toHaveLength(1)
   })
@@ -22,9 +20,8 @@ describe("Button", () => {
   it("invokes the onClick callback", () => {
     const onPress = jest.fn()
 
-    const { UNSAFE_getByType } = renderWithWrappersTL(<TestComponent onPress={onPress} />)
+    const { container: button } = renderWithWrappersTL(<Button onPress={onPress}>Hi</Button>)
 
-    const button = UNSAFE_getByType(TouchableWithoutFeedback)
     fireEvent.press(button)
 
     expect(onPress).toHaveBeenCalledTimes(1)
@@ -33,9 +30,12 @@ describe("Button", () => {
   it("does not invoke the onClick callback if loading is true", () => {
     const onPress = jest.fn()
 
-    const { UNSAFE_getByType } = renderWithWrappersTL(<TestComponent onPress={onPress} loading />)
+    const { container: button } = renderWithWrappersTL(
+      <Button onPress={onPress} loading>
+        Hi
+      </Button>
+    )
 
-    const button = UNSAFE_getByType(TouchableWithoutFeedback)
     fireEvent.press(button)
 
     expect(onPress).toHaveBeenCalledTimes(0)
@@ -44,7 +44,11 @@ describe("Button", () => {
   it("does not invoke the onClick callback if disabled is true", () => {
     const onPress = jest.fn()
 
-    const { UNSAFE_getByType } = renderWithWrappersTL(<TestComponent onPress={onPress} disabled />)
+    const { UNSAFE_getByType } = renderWithWrappersTL(
+      <Button onPress={onPress} disabled>
+        Hi
+      </Button>
+    )
 
     const button = UNSAFE_getByType(TouchableWithoutFeedback)
     fireEvent.press(button)
