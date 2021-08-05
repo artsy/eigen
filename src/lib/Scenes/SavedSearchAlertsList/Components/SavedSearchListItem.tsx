026cd1573387b8fc0723002f46a299322b4f1b07
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2021-08-05 13:49:30 +0300

    feat(FX-3110): list of saved search alerts (#5220)
    * feat: add saved search alerts list scene
    
    * feat: add route
    
    * feat: saved search alert list
    
    * feat: add pagination
    
    * feat: add placeholder
    
    * feat: remove filter labels
    
    * feat: remove separator
    
    * test: saved searches
    
    * feat: show saved search alerts menu item only when flag enabled
    
    * chore: rename
    
    * feat: request real saved search data
    
    * feat: empty message
    
    * feat: query page info data

---
+++ src/lib/Scenes/SavedSearchAlertsList/Components/SavedSearchListItem.tsx
@@ -0,0 +1,29 @@
+import { Box, ChevronIcon, Flex, Text, Touchable, useColor } from "palette"
+import React from "react"
+
+interface SavedSearchListItemProps {
+  title: string
+  onPress?: () => void
+}
+
+export const SavedSearchListItem: React.FC<SavedSearchListItemProps> = (props) => {
+  const { title, onPress } = props
+  const color = useColor()
+
+  return (
+    <Touchable onPress={onPress} underlayColor={color("black5")}>
+      <Box px={2} py={1.5}>
+        <Flex
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Flex flex={1} flexDirection="row" mr="2">
+            <Text variant="text">{title}</Text>
+          </Flex>
+          <ChevronIcon direction="right" fill="black60" />
+        </Flex>
+      </Box>
+    </Touchable>
+  )
+}
