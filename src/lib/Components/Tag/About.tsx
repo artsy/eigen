1ebfa4d735b2709fc60db08059a36985cd70f76d
Matt Dole <mdole7@gmail.com>
2021-08-06 15:01:32 +0200

    feat(FX-3109): tag page (#5207)
    * feature(FX-3109): tag page
    
    * feat: update pagination query
    
    * feat(FX-3109): updated filters for tag page
    
    * feat(FX-3109): updated zero state for tag artworks grid
    
    * fix(FX-3109): added tag sort options to filter store
    
    * fix(FX-3109): used getDeviceType to define device type
    
    * feature(FX-3109): updated queries
    
    * fix(FX-3109): changed query name for header
    
    * feat(FX-3109): added tabs for tag page
    
    * feat(FX-3109): added test for about tab
    
    Co-authored-by: Hanna Trapachka <trepachko.ann@gmail.com>
    Co-authored-by: Dima Tretyak <itretyak@ya.ru>
    Co-authored-by: Hanna Trapachka <56556580+tr-ann@users.noreply.github.com>

---
+++ src/lib/Components/Tag/About.tsx
@@ -0,0 +1,38 @@
+import { About_tag } from "__generated__/About_tag.graphql"
+import { Text } from "palette"
+import React from "react"
+import { View } from "react-native"
+import DeviceInfo from "react-native-device-info"
+import { createFragmentContainer, graphql } from "react-relay"
+import removeMarkdown from "remove-markdown"
+import { StickyTabPageScrollView } from "../StickyTabPage/StickyTabPageScrollView"
+
+interface AboutProps {
+  tag: About_tag
+}
+
+const sideMargin = DeviceInfo.getDeviceType() === "Handset" ? 0 : 50
+
+const About: React.FC<AboutProps> = ({ tag }) => {
+  if (!tag.description) {
+    return null
+  }
+
+  return (
+    <StickyTabPageScrollView contentContainerStyle={{ paddingTop: 15 }}>
+      <View style={{ marginLeft: sideMargin, marginRight: sideMargin }}>
+        <Text variant="text" mb={2}>
+          {removeMarkdown(tag.description)}
+        </Text>
+      </View>
+    </StickyTabPageScrollView>
+  )
+}
+
+export default createFragmentContainer(About, {
+  tag: graphql`
+    fragment About_tag on Tag {
+      description
+    }
+  `,
+})
