6bb38a96f6f70690d2e05561da8103b647aaec8b
Pavlos Vinieratos <pvinis@gmail.com>
2021-08-09 17:41:15 +0100

    ok

--- src/lib/Components/Bidding/Components/BiddingThemeProvider.tsx
+++ src/lib/Components/Bidding/Components/BiddingThemeProvider.tsx
@@ -3,4 +3,4 @@ import React from "react"
 import { ThemeProvider } from "styled-components/native"
 import { theme } from "../Elements/Theme"
 
-export const BiddingThemeProvider: React.FC = (props) => <ThemeProvider theme={theme} {...props} />
+export const BiddingThemeProvider: React.FC = ({ children }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>
