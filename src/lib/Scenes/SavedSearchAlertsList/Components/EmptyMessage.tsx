8c60879bd2e10397d4c364f2e025c19c25f54498
Dima Tretyak <itretyak@ya.ru>
2021-08-11 19:32:56 +0300

    chore: center text for empty message

--- src/lib/Scenes/SavedSearchAlertsList/Components/EmptyMessage.tsx
+++ src/lib/Scenes/SavedSearchAlertsList/Components/EmptyMessage.tsx
@@ -8,7 +8,9 @@ export const EmptyMessage: React.FC = () => {
     <ScrollView contentContainerStyle={{ flex: 1 }}>
       <Flex px={2} py={4} justifyContent="center" alignItems="center" height="100%">
         <Box maxWidth="80%" alignItems="center">
-          <Text variant="title">You haven’t created any Alerts yet.</Text>
+          <Text variant="title" textAlign="center">
+            You haven’t created any Alerts yet.
+          </Text>
           <Spacer mb={1} />
           <Text textAlign="center" variant="caption" color="black60">
             Filter for the artworks you love on an Artist Page and tap ‘Create Alert’ to be notified when new works are
