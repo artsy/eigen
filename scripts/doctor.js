7b3bd38bfcf4557094171b95c53d745ca88d1732
Pavlos Vinieratos <pvinis@gmail.com>
2021-08-11 15:53:05 +0100

    some preparation

--- scripts/doctor.js
+++ scripts/doctor.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /**
  * This script runs some trivial checks to identify possible
  * issues related to the project configuration and suggest ways to solve them.
@@ -19,39 +21,49 @@ const exec = (command, cwd) => {
   return task.stdout.toString()
 }
 
-const checkEnvVariables = () => {
+const NO = (message, suggestion) => {
+  console.log(`🔴 ${message}`)
+  if (suggestion) console.log(`└──> ${suggestion}`)
+}
+const YES = (message) => console.log(`🟢 ${message}`)
+
+const checkEnvVariablesAreUpToDate = () => {
   exec("touch .env.temp")
   exec("aws s3 cp s3://artsy-citadel/dev/.env.eigen .env.temp")
 
   const updatedEnv = fs.readFileSync("./.env.temp", "utf8").toString()
-  const localEnv = fs.readFileSync("./.env.shared", "utf8").toString()
+  let localEnv = ""
+  try {
+    localEnv = fs.readFileSync("./.env.shared", "utf8").toString()
+  } catch (e) {
+    // in that case, we don't even have a `.env.shared`.
+    localEnv = "nope"
+  }
 
   if (updatedEnv !== localEnv) {
-    console.log(`❌ Your .env.shared file does not match the one in AWS please run ${chalk.green("make artsy")}`)
+    NO("Your .env.shared file does not match the one in AWS.", `Please run ${chalk.green("make artsy")}.`)
   } else {
-    console.log(`✅ Your env file seems to be valid`)
+    YES("Your env file seems to be valid.")
   }
   exec("rm .env.temp")
 }
 
 const checkNodeDependencies = async () => {
   const res = await checkDependencies()
-  // Some dependencies do not match the specified version
-  if (res.error.length) {
-    // The last item is a suggestion on how to solve the error that is not useful for us
-    res.error.pop()
-    res.error.push(`Invoke ${chalk.green("yarn install")} to install missing packages`)
-    res.error.forEach((error) => console.log(error))
+
+  if (res.error.length > 0) {
+    // Some dependencies do not match the specified version
+    NO("Your dependencies are out of sync.", `Invoke ${chalk.green("make install")} to install missing dependencies.`)
   } else {
-    console.log(`✅ Your dependencies match the ones specifed in package.json`)
+    YES("Your dependencies match the ones specifed in package.json.")
   }
 }
 
 const main = async () => {
-  // Check if the environment variables are up to date
-  checkEnvVariables()
+  checkEnvVariablesAreUpToDate()
   // Check if any of the installed npm packages does not match the version in package.json
   await checkNodeDependencies()
+  /// add here
 }
 
 main()
