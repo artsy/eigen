ad7c3299a14242f761d81250c888962b2ce47975
Rachel Quartararo <50849237+rquartararo@users.noreply.github.com>
2021-08-12 14:49:59 +0200

    feat: reorder artwork for sale filters on artist screen (#5252)
    * feat: artist artwork filter sorted
    
    * test: update jest tests to reflect order change

--- src/lib/Components/ArtworkFilter/ArtworkFilterOptionsScreen.tsx
+++ src/lib/Components/ArtworkFilter/ArtworkFilterOptionsScreen.tsx
@@ -496,13 +496,13 @@ const CollectionFiltersSorted: FilterScreen[] = [
 ]
 const ArtistArtworksFiltersSorted: FilterScreen[] = [
   "sort",
+  "attributionClass",
   "medium",
   "additionalGeneIDs",
-  "materialsTerms",
   "priceRange",
-  "attributionClass",
   "dimensionRange",
   "waysToBuy",
+  "materialsTerms",
   "locationCities",
   "majorPeriods",
   "colors",
