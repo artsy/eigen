e54d5e3fabe78609c3d02af9fbe3982b11f6072b
Pavlos Vinieratos <pvinis@gmail.com>
2021-08-13 18:37:44 +0100

    fix storybook on android

--- src/storybook/index.js
+++ src/storybook/index.js
@@ -1,5 +1,7 @@
 import { AppRegistry } from "react-native"
+import RNBootSplash from "react-native-bootsplash"
 
 import StorybookUIRoot from "./storybook-ui"
 
+RNBootSplash.hide()
 AppRegistry.registerComponent("Artsy", () => StorybookUIRoot)
