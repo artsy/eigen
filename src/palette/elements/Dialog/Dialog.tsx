8c66a45ce100fb75a524f8a3144b3f98b015dcfb
Dima Tretyak <itretyak@ya.ru>
2021-08-16 19:40:00 +0300

    feat: add Dialong content

--- src/palette/elements/Dialog/Dialog.tsx
+++ src/palette/elements/Dialog/Dialog.tsx
@@ -2,41 +2,55 @@ import { useTheme } from "palette/Theme"
 import React, { useEffect, useRef, useState } from "react"
 import { Animated, Modal, ModalProps, StyleSheet, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { Button } from "../Button/Button"
+import { Flex } from "../Flex"
+import { Text } from "../Text"
+
+interface DialogAction {
+  text: string
+  action: () => void
+}
 
 interface DialogProps extends Omit<ModalProps, "animationType" | "transparent"> {
+  title: string
+  detail?: string
+  primaryCta: DialogAction
+  secondaryCta?: DialogAction
   onBackgroundPressed?: () => void
 }
 
-export const Dialog: React.FC<DialogProps> = (props) => {
-  const { children, visible, onBackgroundPressed, ...other } = props
+export const Dialog = (props: DialogProps) => {
+  const { visible, title, detail, primaryCta, secondaryCta, onBackgroundPressed, ...other } = props
   const [showModal, setShowModal] = useState(visible)
-  const { space } = useTheme()
+  const { space, color } = useTheme()
   const value = useRef(new Animated.Value(Number(visible))).current
 
-  const runAnimation = (mode: "show" | "hide") => {
+  const fadeIn = () => {
     return new Promise((resolve) => {
-      if (mode === "show") {
-        Animated.timing(value, {
-          toValue: 1,
-          useNativeDriver: true,
-          duration: 170,
-        }).start(resolve)
-      } else {
-        Animated.timing(value, {
-          toValue: 0,
-          useNativeDriver: true,
-          duration: 140,
-        }).start(resolve)
-      }
+      Animated.timing(value, {
+        toValue: 1,
+        useNativeDriver: true,
+        duration: 180,
+      }).start(resolve)
+    })
+  }
+
+  const fadeOut = () => {
+    return new Promise((resolve) => {
+      Animated.timing(value, {
+        toValue: 0,
+        useNativeDriver: true,
+        duration: 150,
+      }).start(resolve)
     })
   }
 
   useEffect(() => {
     if (visible) {
       setShowModal(true)
-      runAnimation("show")
+      fadeIn()
     } else {
-      runAnimation("hide").then(() => {
+      fadeOut().then(() => {
         setShowModal(false)
       })
     }
@@ -46,7 +60,7 @@ export const Dialog: React.FC<DialogProps> = (props) => {
     <Modal {...other} visible={showModal} transparent={true} animationType="none">
       <SafeAreaView style={{ flex: 1, backgroundColor: "transparent" }}>
         <Animated.View
-          style={[StyleSheet.absoluteFillObject, { opacity: value, backgroundColor: "rgba(0, 0, 0, 0.5)" }]}
+          style={[StyleSheet.absoluteFillObject, { opacity: value, backgroundColor: "rgba(194,194,194,0.5)" }]}
         />
         <View style={[StyleSheet.absoluteFillObject, { justifyContent: "center", alignItems: "center", padding: 10 }]}>
           <Animated.View
@@ -60,7 +74,41 @@ export const Dialog: React.FC<DialogProps> = (props) => {
               alignItems: "center",
             }}
           >
-            <View style={{ backgroundColor: "#fff", width: "100%", padding: space(2) }}>{children}</View>
+            <View
+              style={{
+                backgroundColor: color("white100"),
+                borderWidth: 1,
+                borderColor: color("black5"),
+                width: "100%",
+                padding: space(2),
+                shadowColor: "rgba(0, 0, 0, 0.06)",
+                shadowOffset: {
+                  height: 2,
+                  width: 0,
+                },
+                shadowOpacity: 1,
+                shadowRadius: 10,
+              }}
+            >
+              <Text variant="title" mb={0.5}>
+                {title}
+              </Text>
+              {!!detail && (
+                <Text variant="caption" color="black60">
+                  {detail}
+                </Text>
+              )}
+              <Flex mt={0.5} flexDirection="row" justifyContent="flex-end">
+                {!!secondaryCta && (
+                  <Button size="small" variant="text" onPress={secondaryCta.action}>
+                    {secondaryCta.text}
+                  </Button>
+                )}
+                <Button size="small" ml={2} onPress={primaryCta.action}>
+                  {primaryCta.text}
+                </Button>
+              </Flex>
+            </View>
           </Animated.View>
         </View>
       </SafeAreaView>
