52f241ed29b6f8f0da7d191e040ead265c674ab3
George Kartalis <g.kartalis7@gmail.com>
2021-08-16 10:54:17 +0200

    feat: fetch algolia credentials from metaphysics

--- src/lib/Scenes/Search2/Search2.tsx
+++ src/lib/Scenes/Search2/Search2.tsx
@@ -1,21 +1,86 @@
+import { captureMessage } from "@sentry/react-native"
+import { Search2Query, Search2QueryResponse } from "__generated__/Search2Query.graphql"
 import { ArtsyKeyboardAvoidingView } from "lib/Components/ArtsyKeyboardAvoidingView"
 import { SearchInput } from "lib/Components/SearchInput"
+import { defaultEnvironment } from "lib/relay/createEnvironment"
+import { useAlgoliaClient } from "lib/utils/useAlgoliaClient"
 import { Flex, useColor } from "palette"
 import React, { useState } from "react"
+import { connectSearchBox, InstantSearch } from "react-instantsearch-native"
+import { graphql, QueryRenderer } from "react-relay"
 
-export const Search2: React.FC = () => {
+interface ImprovedSearchInputProps {
+  refine: (value: string) => any
+}
+
+const ImprovedSearchInput: React.FC<ImprovedSearchInputProps> = ({ refine }) => {
+  return (
+    <SearchInput
+      placeholder="Search artists"
+      onChangeText={(queryText) => {
+        refine(queryText)
+      }}
+    />
+  )
+}
+
+const ImprovedSearchInputContainer = connectSearchBox(ImprovedSearchInput)
+
+export const Search2: React.FC<Search2QueryResponse> = (props) => {
   const color = useColor()
-  const [_query, setQuery] = useState("")
+  const [searchState, setSearchState] = useState({})
+
+  const { system } = props
+
+  const { searchClient } = useAlgoliaClient(system?.algolia?.appID!, system?.algolia?.apiKey!)
+
+  if (!searchClient) {
+    // error handling in case appID or apiKey is not set ??
+    return null
+  }
+
   return (
     <ArtsyKeyboardAvoidingView>
-      <Flex p={2} pb={1} style={{ borderBottomWidth: 1, borderColor: color("black10") }}>
-        <SearchInput
-          placeholder="Search artists"
-          onChangeText={(queryText) => {
-            setQuery(queryText)
-          }}
-        />
-      </Flex>
+      <InstantSearch
+        searchClient={searchClient}
+        indexName="POC_Artists"
+        searchState={searchState}
+        onSearchStateChange={setSearchState}
+      >
+        <Flex p={2} pb={1} style={{ borderBottomWidth: 1, borderColor: color("black10") }}>
+          <ImprovedSearchInputContainer />
+        </Flex>
+      </InstantSearch>
     </ArtsyKeyboardAvoidingView>
+  )
+}
+
+export const Search2QueryRenderer: React.FC<{}> = ({}) => {
+  return (
+    <QueryRenderer<Search2Query>
+      environment={defaultEnvironment}
+      query={graphql`
+        query Search2Query {
+          system {
+            algolia {
+              appID
+              apiKey
+            }
+          }
+        }
+      `}
+      render={({ props, error }) => {
+        if (error) {
+          if (__DEV__) {
+            console.error(error)
+          } else {
+            captureMessage(error.stack!)
+          }
+        }
+
+        return <Search2 system={props?.system ?? null} />
+      }}
+      variables={{}}
+    />
   )
 }
