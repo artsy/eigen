0acb68f1c7235001f49f70ec842f53b9a80887b6
Dima Tretyak <itretyak@ya.ru>
2021-08-16 20:33:06 +0300

    feat: support notch

--- src/palette/elements/Dialog/Dialog.tsx
+++ src/palette/elements/Dialog/Dialog.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from "palette/Theme"
 import React, { useEffect, useRef, useState } from "react"
-import { Animated, Modal, ModalProps, StyleSheet, View } from "react-native"
+import { Animated, Modal, ModalProps, StyleSheet } from "react-native"
+import { ScrollView } from "react-native-gesture-handler"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Button } from "../Button/Button"
 import { Flex } from "../Flex"
@@ -11,7 +12,7 @@ interface DialogAction {
   action: () => void
 }
 
-interface DialogProps extends Omit<ModalProps, "animationType" | "transparent"> {
+interface DialogProps extends Omit<ModalProps, "animationType" | "transparent" | "statusBarTranslucent"> {
   title: string
   detail?: string
   primaryCta: DialogAction
@@ -57,60 +58,57 @@ export const Dialog = (props: DialogProps) => {
   }, [visible])
 
   return (
-    <Modal {...other} visible={showModal} transparent={true} animationType="none">
-      <SafeAreaView style={{ flex: 1, backgroundColor: "transparent" }}>
+    <Modal {...other} visible={showModal} statusBarTranslucent transparent animationType="none">
+      <Animated.View
+        style={[StyleSheet.absoluteFillObject, { opacity: value, backgroundColor: "rgba(194,194,194,0.5)" }]}
+      />
+      <SafeAreaView
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          padding: space(2),
+          backgroundColor: "transparent",
+        }}
+      >
         <Animated.View
-          style={[StyleSheet.absoluteFillObject, { opacity: value, backgroundColor: "rgba(194,194,194,0.5)" }]}
-        />
-        <View style={[StyleSheet.absoluteFillObject, { justifyContent: "center", alignItems: "center", padding: 10 }]}>
-          <Animated.View
-            style={{
-              opacity: value,
-              width: "100%",
-              height: "95%",
-              marginHorizontal: space(2),
-              backgroundColor: "transparent",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <View
-              style={{
-                backgroundColor: color("white100"),
-                borderWidth: 1,
-                borderColor: color("black5"),
-                width: "100%",
-                padding: space(2),
-                shadowColor: "rgba(0, 0, 0, 0.06)",
-                shadowOffset: {
-                  height: 2,
-                  width: 0,
-                },
-                shadowOpacity: 1,
-                shadowRadius: 10,
-              }}
-            >
-              <Text variant="title" mb={0.5}>
-                {title}
+          style={{
+            width: "100%",
+            maxHeight: "100%",
+            opacity: value,
+            backgroundColor: color("white100"),
+            borderWidth: 1,
+            borderColor: color("black5"),
+            shadowColor: "rgba(0, 0, 0, 0.06)",
+            shadowOffset: {
+              height: 2,
+              width: 0,
+            },
+            shadowOpacity: 1,
+            shadowRadius: 10,
+          }}
+        >
+          <Text variant="title" mb={0.5} mt={2} mx={2}>
+            {title}
+          </Text>
+          {!!detail && (
+            <ScrollView alwaysBounceVertical={false} contentContainerStyle={{ paddingHorizontal: space(2) }}>
+              <Text variant="caption" color="black60">
+                {detail}
               </Text>
-              {!!detail && (
-                <Text variant="caption" color="black60">
-                  {detail}
-                </Text>
-              )}
-              <Flex mt={0.5} flexDirection="row" justifyContent="flex-end">
-                {!!secondaryCta && (
-                  <Button size="small" variant="text" onPress={secondaryCta.action}>
-                    {secondaryCta.text}
-                  </Button>
-                )}
-                <Button size="small" ml={2} onPress={primaryCta.action}>
-                  {primaryCta.text}
-                </Button>
-              </Flex>
-            </View>
-          </Animated.View>
-        </View>
+            </ScrollView>
+          )}
+          <Flex mt={0.5} mb={2} mx={2} flexDirection="row" justifyContent="flex-end">
+            {!!secondaryCta && (
+              <Button size="small" variant="text" onPress={secondaryCta.action}>
+                {secondaryCta.text}
+              </Button>
+            )}
+            <Button size="small" ml={2} onPress={primaryCta.action}>
+              {primaryCta.text}
+            </Button>
+          </Flex>
+        </Animated.View>
       </SafeAreaView>
     </Modal>
   )
