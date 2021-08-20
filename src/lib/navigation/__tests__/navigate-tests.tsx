64bc8d26e4f8fa96584557bfae2aafb940572f49
Dima Tretyak <itretyak@ya.ru>
2021-08-20 16:38:57 +0300

    feat: delay navigate

--- src/lib/navigation/__tests__/navigate-tests.tsx
+++ src/lib/navigation/__tests__/navigate-tests.tsx
@@ -1,5 +1,6 @@
 import { LegacyNativeModules } from "lib/NativeModules/LegacyNativeModules"
 import { GlobalStore } from "lib/store/GlobalStore"
+import { flushPromiseQueue } from "lib/tests/flushPromiseQueue"
 import { Linking } from "react-native"
 import { navigate } from "../navigate"
 
@@ -144,6 +145,7 @@ describe(navigate, () => {
   it("switches tab before pushing in cases where that's required", async () => {
     await navigate("/conversation/234")
     expect(GlobalStore.actions.bottomTabs.switchTab).toHaveBeenCalledWith("inbox")
+    await flushPromiseQueue()
     expect(args(LegacyNativeModules.ARScreenPresenterModule.pushView as any)).toMatchInlineSnapshot(`
       Array [
         "inbox",
@@ -165,6 +167,7 @@ describe(navigate, () => {
     await navigate("my-profile/saved-search-alerts", {
       popToRootTabView: true,
     })
+    await flushPromiseQueue()
     expect(GlobalStore.actions.bottomTabs.switchTab).toHaveBeenCalledWith("profile")
     expect(args(LegacyNativeModules.ARScreenPresenterModule.pushView as any)).toMatchInlineSnapshot(`
       Array [
