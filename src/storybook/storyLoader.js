b03c470eed008a7c5fbc6db56679a64fee03dbd5
Pavlos Vinieratos <pvinis@gmail.com>
2021-08-26 13:13:26 +0100

    feature: Avatar V3 (#5242)
    * Avatar V3
    
    * fix storybook
    
    Co-authored-by: Ole <ole.richter91@gmail.com>

--- src/storybook/storyLoader.js
+++ src/storybook/storyLoader.js
@@ -6,6 +6,10 @@
 function loadStories() {
   require("../palette/colors.stories")
   require("../palette/colorsV2.stories")
+  require("../palette/space.stories")
+  require("../palette/spaceV2.stories")
+  require("../palette/elements/Select/Select.stories")
+  require("../palette/elements/Select/SelectV2.stories")
   require("../palette/elements/Button/Button.stories")
   require("../palette/elements/Button/ButtonV2.stories")
   require("../palette/elements/Checkbox/Checkbox.stories")
@@ -16,13 +20,16 @@ function loadStories() {
   require("../palette/elements/Text/Text.stories")
   require("../palette/elements/Text/TextV1.stories")
   require("../palette/elements/Text/TextV2.stories")
-  require("../palette/space.stories")
-  require("../palette/spaceV2.stories")
+  require("../palette/elements/Avatar/Avatar.stories")
 }
 
 const stories = [
   "../palette/colors.stories",
   "../palette/colorsV2.stories",
+  "../palette/space.stories",
+  "../palette/spaceV2.stories",
+  "../palette/elements/Select/Select.stories",
+  "../palette/elements/Select/SelectV2.stories",
   "../palette/elements/Button/Button.stories",
   "../palette/elements/Button/ButtonV2.stories",
   "../palette/elements/Checkbox/Checkbox.stories",
@@ -33,8 +40,7 @@ const stories = [
   "../palette/elements/Text/Text.stories",
   "../palette/elements/Text/TextV1.stories",
   "../palette/elements/Text/TextV2.stories",
-  "../palette/space.stories",
-  "../palette/spaceV2.stories",
+  "../palette/elements/Avatar/Avatar.stories",
 ]
 
 module.exports = {
