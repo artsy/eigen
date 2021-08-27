e7445e7d95b789609f65e3ece5044e74310d5933
George Kartalis <g.kartalis7@gmail.com>
2021-08-27 12:34:40 +0200

    fix: add spacing above android createsavedsearch modal header

--- src/lib/Scenes/SavedSearchAlert/CreateSavedSearchAlert.tsx
+++ src/lib/Scenes/SavedSearchAlert/CreateSavedSearchAlert.tsx
@@ -1,10 +1,9 @@
-import { ArtsyKeyboardAvoidingView } from "lib/Components/ArtsyKeyboardAvoidingView"
 import { Aggregations, FilterData } from "lib/Components/ArtworkFilter/ArtworkFilterHelpers"
 import { FancyModal } from "lib/Components/FancyModal/FancyModal"
 import { FancyModalHeader } from "lib/Components/FancyModal/FancyModalHeader"
-import { Sans, Text, useTheme } from "palette"
+import { Sans, Spacer, Text, useTheme } from "palette"
 import React from "react"
-import { ScrollView } from "react-native"
+import { Platform, ScrollView } from "react-native"
 import { SavedSearchAlertForm } from "./SavedSearchAlertForm"
 import { SavedSearchAlertFormPropsBase } from "./SavedSearchAlertModel"
 
@@ -25,32 +24,31 @@ export const CreateSavedSearchAlert: React.FC<CreateSavedSearchAlertProps> = (pr
   }
 
   return (
-    <ArtsyKeyboardAvoidingView>
-      <FancyModal visible={visible} fullScreen>
-        <FancyModalHeader useXButton hideBottomDivider onLeftButtonPress={onClosePress} />
-        <ScrollView
-          keyboardDismissMode="on-drag"
-          keyboardShouldPersistTaps="handled"
-          contentContainerStyle={{ paddingHorizontal: space(2) }}
-        >
-          <Sans size="8" mb={1}>
-            Create an Alert
-          </Sans>
-          <Sans size="3t" mb={4}>
-            Receive alerts as Push Notifications directly to your device.
-          </Sans>
-          <SavedSearchAlertForm
-            initialValues={{ name: "" }}
-            aggregations={aggregations}
-            filters={filters}
-            onComplete={handleComplete}
-            {...other}
-          />
-          <Text variant="text" color="black60" textAlign="center" my={2}>
-            You will be able to access all your Saved Alerts in your Profile.
-          </Text>
-        </ScrollView>
-      </FancyModal>
-    </ArtsyKeyboardAvoidingView>
+    <FancyModal visible={visible} fullScreen>
+      {Platform.OS === "android" && <Spacer mt={5} />}
+      <FancyModalHeader useXButton hideBottomDivider onLeftButtonPress={onClosePress} />
+      <ScrollView
+        keyboardDismissMode="on-drag"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ paddingHorizontal: space(2) }}
+      >
+        <Sans size="8" mb={1}>
+          Create an Alert
+        </Sans>
+        <Sans size="3t" mb={4}>
+          Receive alerts as Push Notifications directly to your device.
+        </Sans>
+        <SavedSearchAlertForm
+          initialValues={{ name: "" }}
+          aggregations={aggregations}
+          filters={filters}
+          onComplete={handleComplete}
+          {...other}
+        />
+        <Text variant="text" color="black60" textAlign="center" my={2}>
+          You will be able to access all your Saved Alerts in your Profile.
+        </Text>
+      </ScrollView>
+    </FancyModal>
   )
 }
