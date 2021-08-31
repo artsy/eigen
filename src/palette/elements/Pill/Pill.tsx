e4f1b8b98724eb3717de3aab0c23fab9a2949105
George Kartalis <g.kartalis7@gmail.com>
2021-08-31 12:08:38 +0200

    feat: add variant and active props to Pill

--- src/palette/elements/Pill/Pill.tsx
+++ src/palette/elements/Pill/Pill.tsx
@@ -6,20 +6,25 @@ import { Text } from "../Text"
 
 interface PillProps extends FlexProps {
   onPress?: (event: GestureResponderEvent) => void
+  // if no variant is passed defaulting to textSquare
+  variant?: "textRound"
+  active?: boolean
 }
 
-export const Pill: React.FC<PillProps> = ({ children, style, onPress, ...other }) => {
-  const { color, space } = useTheme()
-
+export const Pill: React.FC<PillProps> = ({ children, style, variant, active = true, onPress, ...other }) => {
+  const { colorV3, space } = useTheme()
+  const borderColor = active ? colorV3("black60") : colorV3("black15")
+  const borderRadius = variant === "textRound" ? 50 : 0
   const content = (
     <Flex
       style={[
         {
           borderWidth: 1,
-          borderColor: color("black60"),
+          borderColor,
           paddingHorizontal: space(1),
           justifyContent: "center",
           height: 30,
+          borderRadius,
         },
         style,
       ]}
