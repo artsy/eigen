f19be203f1a2cb125ed3f47317eef4e3e64caef0
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2021-09-01 12:29:55 +0300

    feat: add popover message to storybook (#5349)

---
+++ src/lib/Components/PopoverMessage/PopoverMessage.stories.tsx
@@ -0,0 +1,81 @@
+import { action } from "@storybook/addon-actions"
+import { storiesOf } from "@storybook/react-native"
+import { Button } from "palette/elements/Button"
+import React from "react"
+import { withThemeV3 } from "storybook/decorators"
+import { DList, List } from "storybook/helpers"
+import { PopoverMessageItem, PopoverMessageType } from "./PopoverMessage"
+import { usePopoverMessage } from "./popoverMessageHooks"
+
+const variants: PopoverMessageType[] = ["default", "error", "info", "success"]
+
+const PopoverMessage: React.FC<PopoverMessageItem & { label: string }> = (props) => {
+  const { label, onPress, ...popoverMessageProps } = props
+  const popoverMessage = usePopoverMessage()
+
+  return (
+    <Button
+      onPress={() => {
+        popoverMessage.show({
+          ...popoverMessageProps,
+          onPress: () => {
+            onPress?.()
+          },
+        })
+      }}
+    >
+      {label}
+    </Button>
+  )
+}
+
+storiesOf("Popover message", module)
+  .addDecorator(withThemeV3)
+  .add("Variants", () => (
+    <DList
+      data={variants}
+      renderItem={({ item: variant }) => (
+        <PopoverMessage label={variant} title="Some title" message="Some message" type={variant} />
+      )}
+    />
+  ))
+  .add("Placements", () => (
+    <List>
+      <PopoverMessage label="Top" title="Some title" />
+      <PopoverMessage label="bottom" title="Some title" placement="bottom" />
+    </List>
+  ))
+  .add("Text", () => (
+    <List>
+      <PopoverMessage label="Only title" title="Some title" />
+      <PopoverMessage label="With title and message" title="Some title" message="Some message" />
+      <PopoverMessage
+        label="With long message"
+        title="Some title"
+        message="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      />
+    </List>
+  ))
+  .add("Miscellaneous", () => (
+    <List>
+      <PopoverMessage label="Auto close in 5 seconds" title="Some title" hideTimeout={5000} />
+      <PopoverMessage
+        label="Without auto close"
+        title="Some title"
+        message="You can tap on the message to hide it"
+        autoHide={false}
+      />
+      <PopoverMessage label="With press hander" title="Tappable" onPress={() => action("item tapped")} />
+      <PopoverMessage
+        label="With undo action (only title)"
+        title="Some title"
+        onUndoPress={() => action("undo tapped")}
+      />
+      <PopoverMessage
+        label="With undo action"
+        title="Some title"
+        message="Some message"
+        onUndoPress={() => action("undo tapped")}
+      />
+    </List>
+  ))
