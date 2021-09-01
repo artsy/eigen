4c40b4d85bb359614403c8a882c69b16b85e2b89
Mounir Dhahri <soussouheros@gmail.com>
2021-09-01 14:14:36 +0200

    add injections support for the context store

--- src/lib/Scenes/Artwork/Components/NewImagesCarousel/NewImagesCarouselContext.tsx
+++ src/lib/Scenes/Artwork/Components/NewImagesCarousel/NewImagesCarouselContext.tsx
@@ -1,15 +1,24 @@
-import { action, Action, createContextStore } from "easy-peasy"
+import { NewImagesCarousel_images } from "__generated__/NewImagesCarousel_images.graphql"
+import { action, Action, createContextStore, State } from "easy-peasy"
 
 interface NewImagesCarouselStoreModel {
   imageIndex: number
+  images: NewImagesCarousel_images
   setImageIndex: Action<NewImagesCarouselStoreModel, number>
+  setImages: Action<NewImagesCarouselStoreModel, NewImagesCarousel_images>
 }
 
 const NewImagesCarouselStoreModel: NewImagesCarouselStoreModel = {
   imageIndex: 0,
+  images: [],
   setImageIndex: action((state, payload) => {
     state.imageIndex = payload
   }),
+  setImages: action((state, payload) => {
+    state.images = payload
+  }),
 }
 
-export const NewImagesCarouselStore = createContextStore(NewImagesCarouselStoreModel)
+export const NewImagesCarouselStore = createContextStore<NewImagesCarouselStoreModel>(
+  (initialData: State<NewImagesCarouselStoreModel>) => ({ ...NewImagesCarouselStoreModel, ...initialData })
+)
