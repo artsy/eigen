33070f2b9ea6e3417bccd40e280df130e7405279
Kizito Egeonu <kizito.egeonu@gmail.com>
2021-09-01 15:23:01 +0200

    done contenttabs

--- src/lib/Scenes/Search/Search.tsx
+++ src/lib/Scenes/Search/Search.tsx
@@ -3,7 +3,6 @@ import { SearchInput } from "lib/Components/SearchInput"
 import { isPad } from "lib/utils/hardware"
 import { Schema } from "lib/utils/track"
 import { Flex, Spacer, useColor } from "palette"
-import { ContentTabs } from "palette/elements/Tabs/ContentTabs"
 import React, { useState } from "react"
 import { Platform, ScrollView } from "react-native"
 import { useTracking } from "react-tracking"
@@ -25,7 +24,7 @@ export const Search: React.FC = () => {
   const [query, setQuery] = useState("")
   const { trackEvent } = useTracking()
   const searchProviderValues = useSearchProviderValues(query)
-  const [active, setActive] = useState("")
+  const [active, setActive] = useState("block1")
   return (
     <SearchContext.Provider value={searchProviderValues}>
       <ArtsyKeyboardAvoidingView>
@@ -55,25 +54,6 @@ export const Search: React.FC = () => {
             }}
           />
         </Flex>
-        {/* <ContentTabs
-          setActiveTab={(id) => {
-            setActive(id)
-          }}
-          tabs={[
-            { label: "block1", id: "block1" },
-            { label: "another", id: "idd" },
-            { label: "block2", id: "block2" },
-            { label: "another2", id: "idd2" },
-            { label: "block3", id: "block3" },
-            { label: "another3", id: "idd3" },
-            { label: "block4", id: "block4" },
-            { label: "another4", id: "idd4" },
-            { label: "block5", id: "block5" },
-            { label: "block6", id: "block6" },
-            { label: "another7", id: "idd7" },
-          ]}
-          activeTab={active}
-        /> */}
         {query.length >= 2 ? (
           <AutosuggestResults query={query} />
         ) : (
