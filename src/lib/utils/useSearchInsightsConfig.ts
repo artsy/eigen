6e8803d3d66ddfee3a7ded93c1f112684a09af00
Dima Tretyak <itretyak@ya.ru>
2021-09-09 20:32:46 +0300

    feat: custom request function

--- src/lib/utils/useSearchInsightsConfig.ts
+++ src/lib/utils/useSearchInsightsConfig.ts
@@ -1,7 +1,26 @@
 import { GlobalStore, useFeatureFlag } from "lib/store/GlobalStore"
 import { useEffect, useState } from "react"
 import { Platform } from "react-native"
-import searchInsights from "search-insights"
+import { createInsightsClient } from "search-insights"
+import { RequestFnType } from "search-insights/dist/utils/request"
+
+// Сustom implementation of the event sending function
+// Otherwise events on android will not be tracked
+const requestFn: RequestFnType = async (url, data) => {
+  try {
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const searchInsights = createInsightsClient(requestFn)
 
 export const useSearchInsightsConfig = (appId?: string, apiKey?: string) => {
   const [configured, setConfigured] = useState(false)
