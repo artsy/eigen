2da14613b18ce08fc4c3833216d8834b968f88a0
Ole <ole.richter91@gmail.com>
2021-09-13 19:02:23 +0200

    fix: Stop onboarding checkboxes fromm overflowing (#5435)

--- src/palette/elements/Checkbox/Checkbox.tsx
+++ src/palette/elements/Checkbox/Checkbox.tsx
@@ -93,7 +93,7 @@ export const Checkbox: React.FC<CheckboxProps> = ({
             {!!isChecked && (!!disabled ? <DisabledMark size={checkboxSize} /> : <CheckMark size={checkboxSize} />)}
           </CssTransition>
         </Flex>
-        <Flex justifyContent="center">
+        <Flex justifyContent="center" flex={1}>
           {!!text && <Text color={textColor}>{text}</Text>}
           {!!subtitle && (
             <Text variant="small" color={subtitleColor}>
