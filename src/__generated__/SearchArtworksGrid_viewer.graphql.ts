b7a7f40dadae464e3fc95759aa0d440bf124fc6b
George Kartalis <g.kartalis7@gmail.com>
2021-09-14 18:41:45 +0200

    feat: add artwork filtering with keyword functionallity (#5436)
    * feat: add artwork filtering with keyword functionallity
    
    * feat: add grid placeholder
    
    * fix: infinite load
    
    * chore: add keyExtractor to Flatlist
    
    * chore: remove contentContainerStyle in favor of shouldAddPadding

---
+++ src/__generated__/SearchArtworksGrid_viewer.graphql.ts
@@ -0,0 +1,154 @@
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ReaderFragment } from "relay-runtime";
+import { FragmentRefs } from "relay-runtime";
+export type SearchArtworksGrid_viewer = {
+    readonly artworksConnection: {
+        readonly edges: ReadonlyArray<{
+            readonly node: {
+                readonly id: string;
+            } | null;
+        } | null> | null;
+        readonly " $fragmentRefs": FragmentRefs<"InfiniteScrollArtworksGrid_connection">;
+    } | null;
+    readonly " $refType": "SearchArtworksGrid_viewer";
+};
+export type SearchArtworksGrid_viewer$data = SearchArtworksGrid_viewer;
+export type SearchArtworksGrid_viewer$key = {
+    readonly " $data"?: SearchArtworksGrid_viewer$data;
+    readonly " $fragmentRefs": FragmentRefs<"SearchArtworksGrid_viewer">;
+};
+
+
+
+const node: ReaderFragment = {
+  "argumentDefinitions": [
+    {
+      "defaultValue": 20,
+      "kind": "LocalArgument",
+      "name": "count"
+    },
+    {
+      "defaultValue": null,
+      "kind": "LocalArgument",
+      "name": "cursor"
+    },
+    {
+      "defaultValue": null,
+      "kind": "LocalArgument",
+      "name": "keyword"
+    }
+  ],
+  "kind": "Fragment",
+  "metadata": {
+    "connection": [
+      {
+        "count": "count",
+        "cursor": "cursor",
+        "direction": "forward",
+        "path": [
+          "artworksConnection"
+        ]
+      }
+    ]
+  },
+  "name": "SearchArtworksGrid_viewer",
+  "selections": [
+    {
+      "alias": "artworksConnection",
+      "args": [
+        {
+          "kind": "Variable",
+          "name": "keyword",
+          "variableName": "keyword"
+        }
+      ],
+      "concreteType": "FilterArtworksConnection",
+      "kind": "LinkedField",
+      "name": "__SearchArtworksGrid_artworksConnection_connection",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "FilterArtworksEdge",
+          "kind": "LinkedField",
+          "name": "edges",
+          "plural": true,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "concreteType": "Artwork",
+              "kind": "LinkedField",
+              "name": "node",
+              "plural": false,
+              "selections": [
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "id",
+                  "storageKey": null
+                },
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "__typename",
+                  "storageKey": null
+                }
+              ],
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "cursor",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "PageInfo",
+          "kind": "LinkedField",
+          "name": "pageInfo",
+          "plural": false,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "endCursor",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "hasNextPage",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        },
+        {
+          "args": null,
+          "kind": "FragmentSpread",
+          "name": "InfiniteScrollArtworksGrid_connection"
+        }
+      ],
+      "storageKey": null
+    }
+  ],
+  "type": "Viewer",
+  "abstractKey": null
+};
+(node as any).hash = 'dd483988e5effca5722d97c1ca01645c';
+export default node;
