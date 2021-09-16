4b8f06b6cce709202508d5655dfc7a23dab858fa
George Kartalis <g.kartalis7@gmail.com>
2021-09-16 19:02:30 +0200

    feat(FX-3292): add filters to searchable artwork grid (#5457)
    * feat: add filters to searchable artwork grid
    
    * chore: refactor SearchArtwork components
    
    Co-authored-by: Carlos Barreto <carlos@artsymail.com>
    
    * chore: fixed skeleton of artworksQueryRenderer
    
    * chore: add todo comment for id/slug analytics
    
    * chore: removed stray comma
    
    * chore: moved container to containers
    
    * chore: moved skeleton to component
    
    * chore: address feedback from review
    
    * feat: add empty message
    
    * chore: removed input from queryrenderer
    
    Co-authored-by: Carlos Barreto <carlos@artsymail.com>

---
+++ src/lib/Components/ArtworkGrids/FilterHeader2.tsx
@@ -0,0 +1,37 @@
+import { bullet, FilterIcon, Flex, Text, TouchableHighlightColor } from "palette"
+import React from "react"
+
+interface FilterHeaderProps {
+  children?: React.ReactNode
+  onFilterPress: () => void
+  selectedFiltersCount: number
+}
+
+export const ArtworksFilterHeader: React.FC<FilterHeaderProps> = ({
+  children,
+  onFilterPress,
+  selectedFiltersCount,
+}) => {
+  return (
+    <Flex flexDirection="row" height={28} my={1} px={2} justifyContent="space-between" alignItems="center">
+      <TouchableHighlightColor
+        haptic
+        onPress={onFilterPress}
+        render={({ color }) => (
+          <Flex flex={1} flexDirection="row" alignItems="center">
+            <FilterIcon fill={color} width="20px" height="20px" />
+            <Text variant="small" numberOfLines={1} color={color} ml={0.5}>
+              Sort & Filter
+            </Text>
+            {selectedFiltersCount > 0 && (
+              <Text variant="small" color="blue100">
+                {` ${bullet} ${selectedFiltersCount}`}
+              </Text>
+            )}
+          </Flex>
+        )}
+      />
+      {children}
+    </Flex>
+  )
+}
