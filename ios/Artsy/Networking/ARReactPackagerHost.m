ed6762e9f93607c093019ae4dc344b9bebe478a8
brainbicycle <brian.beckerle@artsymail.com>
2021-09-16 14:36:24 -0400

    first pass move all ios files to ios folder

---
+++ ios/Artsy/Networking/ARReactPackagerHost.m
@@ -0,0 +1,16 @@
+// this file is generated automatically in a build phase
+#import "ARReactPackagerHost.h"
+#import <Foundation/Foundation.h>
+
+@implementation ARReactPackagerHost
+
++ (NSString *)hostname
+{
+#if defined(DEBUG)
+    return @"Brians-MacBook-Pro.local";
+#else
+    return @"localhost";
+#endif
+}
+
+@end
