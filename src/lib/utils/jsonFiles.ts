ce1ab6951642bab603d89fc65113c9ba7ba74c43
brainbicycle <brian.beckerle@artsymail.com>
2021-09-16 15:48:43 -0400

    update echo file location

--- src/lib/utils/jsonFiles.ts
+++ src/lib/utils/jsonFiles.ts
@@ -1,6 +1,6 @@
 import { Echo } from "lib/store/config/EchoModel"
 import appJsonFile from "../../../app.json"
-import echoLaunchJsonFile from "../../../Artsy/App/EchoNew.json"
+import echoLaunchJsonFile from "../../../data/EchoNew.json"
 
 export const appJson = () => appJsonFile
 export const echoLaunchJson = (): Echo => echoLaunchJsonFile
