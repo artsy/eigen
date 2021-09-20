3a110bd388d0ec6400ab6d8f25a64ddeaa28de7e
Pavlos Vinieratos <pvinis@gmail.com>
2021-09-20 11:35:43 +0300

    remove uses of the flag

--- src/palette/elements/Select/index.tsx
+++ src/palette/elements/Select/index.tsx
@@ -1,5 +1,4 @@
-import { ThemeV2, ThemeV3 } from "palette"
-import { usePaletteFlagStore } from "palette/PaletteFlag"
+import { ThemeV3 } from "palette"
 import React from "react"
 
 // v3
@@ -17,23 +16,11 @@ export type SelectProps<ValueType> = (SelectV2Props<ValueType> | SelectV3Props<V
 export type SelectComponentI<ValueType = any> = React.FC<SelectProps<ValueType>>
 // export type SelectComponentI = React.FC<React.ComponentPropsWithRef<typeof SelectV2>>
 
-export const Select = <ValueType,>(props: SelectProps<ValueType>) => {
-  const allowV3 = usePaletteFlagStore((state) => state.allowV3)
-
-  if (allowV3) {
-    return (
-      <ThemeV3>
-        <SelectV3 {...transformV3Props(props)} />
-      </ThemeV3>
-    )
-  } else {
-    return (
-      <ThemeV2>
-        <SelectV2 {...props} />
-      </ThemeV2>
-    )
-  }
-}
+export const Select = <ValueType,>(props: SelectProps<ValueType>) => (
+  <ThemeV3>
+    <SelectV3 {...transformV3Props(props)} />
+  </ThemeV3>
+)
 
 const transformV3Props = <T,>(props: SelectV2Props<T>): SelectV3Props<T> => {
   return { ...props } as SelectV3Props<T>
