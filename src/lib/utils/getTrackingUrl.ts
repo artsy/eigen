0f342d1077fd2d92494d6bdf0a0943499f9c4560
Serge0n <siarhei.krauchonak@artsymail.com>
2021-09-21 10:02:26 +0300

    getTrackingUrl updates

--- src/lib/utils/getTrackingUrl.ts
+++ src/lib/utils/getTrackingUrl.ts
@@ -1,10 +1,17 @@
 import { OrderHistoryRow_order } from "__generated__/OrderHistoryRow_order.graphql"
+import { TrackOrderSection_section } from "__generated__/TrackOrderSection_section.graphql"
 
 export type OrderHistoryRowLineItem = NonNullable<
   NonNullable<NonNullable<OrderHistoryRow_order["lineItems"]>["edges"]>[0]
 >["node"]
 
-export function getTrackingUrl(orderLineItem: OrderHistoryRowLineItem): string | undefined {
+export type OrderDetailsTrackOrderSectionLineItem = NonNullable<
+  NonNullable<NonNullable<TrackOrderSection_section["lineItems"]>["edges"]>[0]
+>["node"]
+
+export function getTrackingUrl(
+  orderLineItem: OrderHistoryRowLineItem | OrderDetailsTrackOrderSectionLineItem
+): string | undefined {
   const trackingId = orderLineItem?.fulfillments?.edges?.[0]?.node?.trackingId
   const trackingNumber = orderLineItem?.shipment?.trackingNumber
   const trackingUrl = orderLineItem?.shipment?.trackingUrl
