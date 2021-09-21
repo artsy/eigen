d98c7da949b2f18ad8844086956023a751865891
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2021-09-21 12:41:11 -0400

    chore: remove price estimate section + demand index subtitle (#5482)
    * remove price estimate section + demand index subtitle
    
    * remove now unused fragments

--- src/lib/Scenes/MyCollection/Screens/Artwork/Components/ArtworkInsights/__tests__/MyCollectionArtworkInsights-tests.tsx
+++ src/lib/Scenes/MyCollection/Screens/Artwork/Components/ArtworkInsights/__tests__/MyCollectionArtworkInsights-tests.tsx
@@ -9,7 +9,6 @@ import { MyCollectionArtworkArtistAuctionResultsFragmentContainer } from "../MyC
 import { MyCollectionArtworkArtistMarketFragmentContainer } from "../MyCollectionArtworkArtistMarket"
 import { MyCollectionArtworkDemandIndexFragmentContainer } from "../MyCollectionArtworkDemandIndex"
 import { MyCollectionArtworkInsightsFragmentContainer } from "../MyCollectionArtworkInsights"
-import { MyCollectionArtworkPriceEstimateFragmentContainer } from "../MyCollectionArtworkPriceEstimate"
 
 jest.unmock("react-relay")
 
@@ -61,7 +60,6 @@ describe("MyCollectionArtworkInsights", () => {
     expect(text).toContain("Price and market insights")
     expect(text).toContain('<mock-value-for-field-"sizebucket"> other by <mock-value-for-field-"name">')
     expect(wrapper.root.findByType(MyCollectionArtworkDemandIndexFragmentContainer)).toBeDefined()
-    expect(wrapper.root.findByType(MyCollectionArtworkPriceEstimateFragmentContainer)).toBeDefined()
     expect(wrapper.root.findByType(MyCollectionArtworkArtistMarketFragmentContainer)).toBeDefined()
     expect(wrapper.root.findByType(MyCollectionArtworkArtistAuctionResultsFragmentContainer)).toBeDefined()
     expect(wrapper.root.findByType(MyCollectionArtworkArtistArticlesFragmentContainer)).toBeDefined()
