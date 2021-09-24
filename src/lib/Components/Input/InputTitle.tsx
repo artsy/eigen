4a874c1d02c2f873d6fe471a25d911f23dceda19
Pavlos Vinieratos <pvinis@gmail.com>
2021-09-24 12:21:42 +0300

    cleanup(palette v3): remove palette flag (#5511)
    * remove palette flag
    
    * refresh
    
    * default here too
    
    * yep

--- src/lib/Components/Input/InputTitle.tsx
+++ src/lib/Components/Input/InputTitle.tsx
@@ -1,5 +1,4 @@
-import { Text, ThemeV2, ThemeV3, useColor } from "palette"
-import { usePaletteFlagStore } from "palette/PaletteFlag"
+import { Text, useColor } from "palette"
 import React from "react"
 
 export const InputTitle: React.FC<{ required?: boolean }> = ({ children: title, required }) => {
@@ -8,33 +7,14 @@ export const InputTitle: React.FC<{ required?: boolean }> = ({ children: title, 
     return null
   }
 
-  const allowV3 = usePaletteFlagStore((state) => state.allowV3)
-
-  if (allowV3) {
-    return (
-      <ThemeV3>
-        <Text variant="xs" mb={0.5}>
-          {title}
-          {!!required && (
-            <Text variant="mediumText" color={color("blue100")}>
-              *
-            </Text>
-          )}
+  return (
+    <Text variant="xs" mb={0.5}>
+      {title}
+      {!!required && (
+        <Text variant="mediumText" color={color("blue100")}>
+          *
         </Text>
-      </ThemeV3>
-    )
-  } else {
-    return (
-      <ThemeV2>
-        <Text variant="text" mb={0.5}>
-          {title}
-          {!!required && (
-            <Text variant="mediumText" color={color("purple100" /* TODO-PALETTE-V3 "blue100" */)}>
-              *
-            </Text>
-          )}
-        </Text>
-      </ThemeV2>
-    )
-  }
+      )}
+    </Text>
+  )
 }
