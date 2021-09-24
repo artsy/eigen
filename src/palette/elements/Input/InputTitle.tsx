d739d14413cdaab1a09c9ebc2cbf05b0fa1a56f3
Alican Akyuz <alicakyz@gmail.com>
2021-09-24 12:51:30 +0200

    remove unused vars

--- src/palette/elements/Input/InputTitle.tsx
+++ src/palette/elements/Input/InputTitle.tsx
@@ -1,20 +1,16 @@
 import { Text, ThemeV3, useColor } from "palette"
-import { usePaletteFlagStore } from "palette/PaletteFlag"
 import React from "react"
 
 export const InputTitle: React.FC<{ required?: boolean }> = ({ children: title, required }) => {
   const color = useColor()
-  const { allowV3 } = usePaletteFlagStore()
 
   if (!title) {
     return null
   }
 
-  const textTransform = allowV3 ? "uppercase" : "none"
-
   return (
     <ThemeV3>
-      <Text variant="text" mb={0.5} style={{ fontSize: 13, textTransform }}>
+      <Text variant="text" mb={0.5} style={{ fontSize: 13, textTransform: "uppercase" }}>
         {title}
         {!!required && (
           <Text variant="mediumText" color={color("blue100")}>
