2be9507af2db19b6cd57bc305fa632a63c935712
Alican Akyuz <alicakyz@gmail.com>
2021-09-27 15:04:14 +0200

    return null from country code util

--- src/lib/Components/Bidding/Utils/findCountryNameByCountryCode.ts
+++ src/lib/Components/Bidding/Utils/findCountryNameByCountryCode.ts
@@ -1,14 +1,14 @@
 import { COUNTRY_SELECT_OPTIONS } from "lib/Components/CountrySelect"
 
 // finds a country's long name by the passed country code
-const findCountryNameByCountryCode = (countryCode: string): string => {
+const findCountryNameByCountryCode = (countryCode: string): string | null => {
   const countryLongName = COUNTRY_SELECT_OPTIONS.find((opt) => opt.value === countryCode)?.label
 
   if (countryLongName && typeof countryLongName === "string") {
     return countryLongName
   }
 
-  return ""
+  return null
 }
 
 export default findCountryNameByCountryCode
