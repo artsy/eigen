0c4826ed7da76b87bb6c29e17ac3044355d68cb5
Pavlos Vinieratos <pvinis@gmail.com>
2021-10-05 17:36:09 +0300

    use measuredview for inputs too

--- src/lib/utils/useOffscreenStyle.ts
+++ src/lib/utils/useOffscreenStyle.ts
@@ -1,11 +1,10 @@
 import { Dimensions, ViewStyle } from "react-native"
 
-export const useOffscreenStyle = (): ViewStyle => {
-  const { width, height } = Dimensions.get("window")
-
-  return {
-    position: "absolute",
-    top: width + height,
-    left: width + height,
+export const useOffscreenStyle = (notOffscreen?: boolean): ViewStyle => {
+  if (notOffscreen) {
+    return { position: "absolute", top: 0, left: 0 }
   }
+
+  const { width, height } = Dimensions.get("window")
+  return { position: "absolute", top: width + height, left: width + height }
 }
