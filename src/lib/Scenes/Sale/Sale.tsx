7c8a63e875d36637c0f79a3cd33175e9d2f926ca
brainbicycle <brian.beckerle@artsymail.com>
2021-10-07 14:32:00 -0400

    mark sale queries me field as optional

--- src/lib/Scenes/Sale/Sale.tsx
+++ src/lib/Scenes/Sale/Sale.tsx
@@ -320,7 +320,7 @@ export const SaleContainer = createRefetchContainer(
   },
   graphql`
     query SaleRefetchQuery($saleID: String!, $saleSlug: ID!) {
-      me {
+      me @optionalField {
         ...Sale_me
       }
       sale(id: $saleID) {
@@ -352,7 +352,7 @@ export const SaleQueryRenderer: React.FC<{ saleID: string; environment?: RelayMo
                   sale(id: $saleID) {
                     ...Sale_sale
                   }
-                  me {
+                  me @optionalField {
                     ...Sale_me
                   }
                 }
