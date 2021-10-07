cf020d84d8715d3d3d2da7883051abdbf277a8cb
Pavlos Vinieratos <pvinis@gmail.com>
2021-10-07 14:32:23 +0300

    bump uifont

--- src/palette/elements/Text/Text.stories.tsx
+++ src/palette/elements/Text/Text.stories.tsx
@@ -1,7 +1,7 @@
 import { storiesOf } from "@storybook/react-native"
-import { Box } from "palette"
+import { Box, Flex } from "palette"
 import React from "react"
-import { View } from "react-native"
+import { Text as RNText, TextStyle, View } from "react-native"
 import { withTheme } from "storybook/decorators"
 import { DList, List } from "storybook/helpers"
 import { Text, TextProps } from "."
@@ -47,3 +47,28 @@ storiesOf("Theme/Text", module)
       <View />
     </List>
   ))
+  // this is useful for making sure our custom fonts are rendering at the same height for ios and android
+  .add("Font centering", () => {
+    const style: TextStyle = { borderWidth: 1, borderColor: "black", fontSize: 16, lineHeight: 16 }
+    return (
+      <Flex flexDirection="row" flex={1}>
+        <Flex flex={1}>
+          <RNText>System font</RNText>
+          <List>
+            <RNText style={style}>regular TEXT.</RNText>
+            <RNText style={style}>ALL CAPS text.</RNText>
+          </List>
+        </Flex>
+
+        <Flex width="1px" height="100%" borderWidth={1} borderColor="black" />
+
+        <Flex flex={1}>
+          <RNText>Unica custom font</RNText>
+          <List>
+            <RNText style={[style, { fontFamily: "Unica77LL-Regular" }]}>regular TEXT.</RNText>
+            <RNText style={[style, { fontFamily: "Unica77LL-Regular" }]}>ALL CAPS text.</RNText>
+          </List>
+        </Flex>
+      </Flex>
+    )
+  })
