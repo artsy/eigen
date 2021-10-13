219627fbf609d0ec355079ec31c7adbb1252427e
George Kartalis <g.kartalis7@gmail.com>
2021-10-13 14:41:38 +0200

    fix: Pill component

--- src/palette/elements/Pill/Pill.tsx
+++ src/palette/elements/Pill/Pill.tsx
@@ -1,101 +1,173 @@
 import OpaqueImageView from "lib/Components/OpaqueImageView/OpaqueImageView"
-import { Spacer } from "palette"
-import React, { ReactNode } from "react"
-import { GestureResponderEvent, TouchableOpacity } from "react-native"
+import { Color, Flex, IconProps, Spacer, Text, useColor, useTextStyleForPalette } from "palette"
+import React, { ReactNode, useState } from "react"
+import { GestureResponderEvent, Pressable, PressableProps } from "react-native"
+import { config } from "react-spring"
+// @ts-ignore
+import { animated, Spring } from "react-spring/renderprops-native"
 import styled from "styled-components/native"
-import { Color, useTheme } from "../../Theme"
-import { Flex, FlexProps } from "../Flex"
-import { Text, useTextStyleForPalette } from "../Text"
+type PillSize = "xxs" | "xs" | "sm"
 
-const SIZES = {
-  xxs: {
-    height: 28,
-    paddingRight: 10,
-    paddingLeft: 10,
-  },
-  xs: {
-    height: 40,
-    paddingRight: 20,
-    paddingLeft: 20,
-  },
-  sm: {
-    height: 50,
-    paddingRight: 20,
-    paddingLeft: 10,
-  },
-}
-
-interface PillProps extends FlexProps {
-  selected?: boolean
+export interface PillProps {
+  children: ReactNode
+  size?: PillSize
+  onPress?: PressableProps["onPress"]
   rounded?: boolean
-  size?: "xxs" | "xs" | "sm"
-  imageUrl?: string
-  icon?: ReactNode
+  Icon?: React.FC<IconProps>
   iconPosition?: "left" | "right"
   disabled?: boolean
-  onPress?: (event: GestureResponderEvent) => void
-  textColor?: Color
+  selected?: boolean
+  imageUrl?: string
+}
+
+enum DisplayState {
+  Enabled = "enabled",
+  Pressed = "pressed",
+  Selected = "selected",
+}
+
+const getSize = (size: PillSize): { height: number; paddingRight: number; paddingLeft: number } => {
+  switch (size) {
+    case "xxs":
+      return {
+        height: 30,
+        paddingRight: 10,
+        paddingLeft: 10,
+      }
+    case "xs":
+      return {
+        height: 40,
+        paddingRight: 20,
+        paddingLeft: 20,
+      }
+    case "sm":
+      return {
+        height: 50,
+        paddingRight: 20,
+        paddingLeft: 10,
+      }
+  }
 }
 
 export const Pill: React.FC<PillProps> = ({
-  children,
   size = "xxs",
-  selected = true,
-  rounded,
-  icon,
+  selected = false,
+  Icon,
   iconPosition = "left",
   imageUrl,
-  textColor,
-  disabled,
   onPress,
-  ...other
+  children,
+  disabled,
+  rounded,
+  ...rest
 }) => {
-  const { color } = useTheme()
-  const { height, paddingLeft, paddingRight } = SIZES[size]
   const textStyle = useTextStyleForPalette(size === "xxs" ? "xs" : "sm")
+  const initialDisplayState = selected ? DisplayState.Selected : DisplayState.Enabled
+  const [innerDisplayState, setInnerDisplayState] = useState(initialDisplayState)
+  const { height, paddingLeft, paddingRight } = getSize(size)
 
-  const content = (
-    <Flex
-      flexDirection="row"
-      justifyContent="center"
-      alignItems="center"
-      borderWidth={1}
-      borderRadius={icon || rounded ? 50 : 0}
-      borderColor={icon || selected ? color("black60") : color("black15")}
-      paddingLeft={paddingLeft}
-      paddingRight={paddingRight}
-      height={height}
-      {...other}
-    >
-      {iconPosition === "left" && !!icon && (
-        <>
-          {icon}
-          <Spacer ml={1} />
-        </>
-      )}
-      {!!imageUrl && <OpaqueImageViewContainer imageURL={imageUrl} />}
-      <Text numberOfLines={1} style={[textStyle, { color: color(textColor) }]}>
-        {children}
-      </Text>
-      {iconPosition === "right" && !!icon && (
-        <>
-          <Spacer mr={1} />
-          {icon}
-        </>
-      )}
-    </Flex>
-  )
+  const handlePress = (event: GestureResponderEvent) => {
+    if (onPress === undefined || onPress === null) {
+      return
+    }
 
-  if (onPress) {
-    return (
-      <TouchableOpacity disabled={disabled} onPress={onPress}>
-        {content}
-      </TouchableOpacity>
-    )
+    // Did someone tap really fast? Flick the highlighted state
+    if (displayState === DisplayState.Enabled) {
+      setInnerDisplayState(DisplayState.Pressed)
+      setTimeout(() => {
+        setInnerDisplayState(initialDisplayState)
+      }, 0.3)
+    } else {
+      // Was already selected
+      setInnerDisplayState(initialDisplayState)
+    }
+
+    onPress(event)
   }
 
-  return content
+  const iconSpacerMargin = size === "xxs" ? 0.5 : 1
+
+  const displayState = innerDisplayState
+  const to = useStyleForState(displayState)
+
+  return (
+    <Spring native to={to} config={config.stiff}>
+      {(springProps: typeof to) => (
+        <Pressable
+          disabled={!onPress ?? disabled}
+          onPress={handlePress}
+          onPressIn={() => {
+            setInnerDisplayState(DisplayState.Pressed)
+          }}
+          onPressOut={() => {
+            setInnerDisplayState(initialDisplayState)
+          }}
+        >
+          <AnimatedContainer
+            borderRadius={!!Icon || rounded ? 50 : 0}
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+            height={height}
+            paddingLeft={paddingLeft}
+            paddingRight={paddingRight}
+            style={{
+              borderColor: springProps.borderColor,
+            }}
+            icon={Icon}
+            {...rest}
+          >
+            {iconPosition === "left" && !!Icon && (
+              <>
+                {<Icon />}
+                <Spacer mr={iconSpacerMargin} />
+              </>
+            )}
+            {!!imageUrl && <OpaqueImageViewContainer imageURL={imageUrl} />}
+            <AnimatedText numberOfLines={1} style={[textStyle, { color: springProps.textColor }]}>
+              {children}
+            </AnimatedText>
+            {iconPosition === "right" && !!Icon && (
+              <>
+                <Spacer mr={iconSpacerMargin} />
+                {<Icon />}
+              </>
+            )}
+          </AnimatedContainer>
+        </Pressable>
+      )}
+    </Spring>
+  )
 }
+
+const useStyleForState = (state: DisplayState): { textColor: Color; borderColor: string } => {
+  const color = useColor()
+
+  const retval = {} as ReturnType<typeof useStyleForState>
+
+  switch (state) {
+    case DisplayState.Pressed:
+      retval.textColor = "blue100"
+      retval.borderColor = color("blue100")
+      break
+    case DisplayState.Selected:
+      retval.textColor = "black100"
+      retval.borderColor = color("black60")
+      break
+    case DisplayState.Enabled:
+      retval.textColor = "black100"
+      retval.borderColor = color("black15")
+      break
+    default:
+      assertNever(state)
+  }
+
+  return retval
+}
+
+const Container = styled(Flex)<PillProps>`
+  border-width: 1;
+`
 
 export const OpaqueImageViewContainer = styled(OpaqueImageView)`
   width: 30;
@@ -104,3 +176,6 @@ export const OpaqueImageViewContainer = styled(OpaqueImageView)`
   overflow: hidden;
   margin-right: 10;
 `
+
+const AnimatedContainer = animated(Container)
+const AnimatedText = animated(Text)
