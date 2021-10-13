d14bfb5460458c19bbd66871c3753a169932dfce
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2021-10-13 13:22:46 +0300

    feat(FX-3392): tapping back from saved alerts listview should take user to settings (#5597)
    * chore: remove onlyShowInTabName for SavedSearchAlertsList
    
    * feat: add showInTabName to navigation options
    
    * feat: call promise resolve when screen was pushed
    
    * feat: add settings screen when my collection is enabled
    
    * chore: update navigate tests
    
    * chore: add comments, rename varible
    
    * feat: use setTimeout for delay navigate

--- src/lib/Components/Artist/ArtistArtworks/__tests__/SavedSearchButton-tests.tsx
+++ src/lib/Components/Artist/ArtistArtworks/__tests__/SavedSearchButton-tests.tsx
@@ -5,6 +5,7 @@ import { PopoverMessage } from "lib/Components/PopoverMessage/PopoverMessage"
 import { navigate } from "lib/navigation/navigate"
 import { CreateSavedSearchAlert } from "lib/Scenes/SavedSearchAlert/CreateSavedSearchAlert"
 import { SavedSearchAlertMutationResult } from "lib/Scenes/SavedSearchAlert/SavedSearchAlertModel"
+import { __globalStoreTestUtils__ } from "lib/store/GlobalStore"
 import { mockEnvironmentPayload } from "lib/tests/mockEnvironmentPayload"
 import { renderWithWrappers } from "lib/tests/renderWithWrappers"
 import { Button } from "palette"
@@ -146,7 +147,26 @@ describe("SavedSearchButton", () => {
     act(() => tree.root.findByType(CreateSavedSearchAlert).props.onComplete(mockedMutationResult))
     act(() => tree.root.findByType(PopoverMessage).props.onPress())
 
-    expect(navigate).toHaveBeenCalled()
+    expect(navigate).toHaveBeenCalledWith("/my-profile/settings", {
+      popToRootTabView: true,
+      showInTabName: "profile",
+    })
+  })
+
+  it('should call navigate twice when "My Collection" is enabled', async () => {
+    __globalStoreTestUtils__?.injectFeatureFlags({ AREnableMyCollectionIOS: true })
+    const tree = renderWithWrappers(<TestRenderer />)
+
+    act(() => tree.root.findByType(CreateSavedSearchAlert).props.onComplete(mockedMutationResult))
+    act(() => tree.root.findByType(PopoverMessage).props.onPress())
+
+    await new Promise((r) => setTimeout(r, 100))
+
+    expect(navigate).toHaveBeenCalledWith("/my-profile/settings", {
+      popToRootTabView: true,
+      showInTabName: "profile",
+    })
+    expect(navigate).toHaveBeenCalledWith("/my-profile/saved-search-alerts")
   })
 
   it("tracks clicks when the create alert button is pressed", async () => {
