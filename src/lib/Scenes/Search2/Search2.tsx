0564fdaf2e74068664d3e983b4454f1faf9d7497
Dima Tretyak <itretyak@ya.ru>
2021-10-15 15:40:53 +0300

    feat: add showOnRetryErrorMessage prop for autosuggest results

--- src/lib/Scenes/Search2/Search2.tsx
+++ src/lib/Scenes/Search2/Search2.tsx
@@ -161,7 +161,14 @@ export const Search2: React.FC<Search2Props> = (props) => {
       return <SearchResultsContainer indexName={selectedPill.name} categoryDisplayName={selectedPill.displayName} />
     }
     if (selectedPill.name === TOP_PILL.name) {
-      return <AutosuggestResults query={searchState.query!} showResultType showQuickNavigationButtons />
+      return (
+        <AutosuggestResults
+          query={searchState.query!}
+          showResultType
+          showQuickNavigationButtons
+          showOnRetryErrorMessage
+        />
+      )
     }
     return <SearchArtworksQueryRenderer keyword={searchState.query!} />
   }
