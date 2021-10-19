8ff6b7a82d4f5acf3df53509079244cd6e74664a
Sam Raj <sam.raj@artsymail.com>
2021-10-19 13:08:36 +0200

    Added mock picture and bio

--- src/lib/Scenes/MyProfile/MyCollectionAndSavedWorks.tsx
+++ src/lib/Scenes/MyProfile/MyCollectionAndSavedWorks.tsx
@@ -5,7 +5,7 @@ import { StickyTabPage } from "lib/Components/StickyTabPage/StickyTabPage"
 import { navigate } from "lib/navigation/navigate"
 import { defaultEnvironment } from "lib/relay/createEnvironment"
 import renderWithLoadProgress from "lib/utils/renderWithLoadProgress"
-import { Box, Sans, useColor, useSpace } from "palette"
+import { Avatar, Box, Flex, Sans, useColor, useSpace } from "palette"
 import React from "react"
 import { createFragmentContainer, QueryRenderer } from "react-relay"
 import { graphql } from "relay-runtime"
@@ -50,14 +50,24 @@ export const MyProfileHeader: React.FC<{ me?: MyCollectionAndSavedWorks_me }> = 
           navigate("/my-profile/settings")
         }}
       />
-      <Box px={space(2)} pb={space(6)}>
-        <Sans size="8" color={color("black100")}>
-          {me?.name}
-        </Sans>
-        {!!me?.createdAt && (
-          <Sans size="2" color={color("black60")}>{`Member since ${new Date(me?.createdAt).getFullYear()}`}</Sans>
-        )}
-      </Box>
+      <Flex flexDirection="row" alignItems="center" pl={20} pr={20}>
+        <Avatar
+          src="https://d7hftxdivxxvm.cloudfront.net/?resize_to=fill&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2F2qiqRkUQYxTnUaib3yIgpA%2Funtouched-jpg.jpg&width=1820&height=1214&quality=75"
+          size="md"
+        />
+        <Box px={space(2)}>
+          <Sans size="10" color={color("black100")}>
+            {me?.name}
+          </Sans>
+          {!!me?.createdAt && (
+            <Sans size="2" color={color("black60")}>{`Member since ${new Date(me?.createdAt).getFullYear()}`}</Sans>
+          )}
+        </Box>
+      </Flex>
+      <Sans size="2" color={color("black100")} p={20}>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
+        sociis natoque penatibus et magnis dis p
+      </Sans>
     </>
   )
 }
