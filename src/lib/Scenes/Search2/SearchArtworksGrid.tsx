5cb30ba7851f26c41ec5d259bc83631e297e38fd
Anastasia <chizhikpyzhik2015@gmail.com>
2021-10-19 15:09:43 +0300

    FX-3470 Updated copies

--- src/lib/Scenes/Search2/SearchArtworksGrid.tsx
+++ src/lib/Scenes/Search2/SearchArtworksGrid.tsx
@@ -82,7 +82,7 @@ const SearchArtworksGrid: React.FC<SearchArtworksGridProps> = ({ viewer, relay, 
       {artworksCount === 0 ? (
         <Box mb="80px" pt={1}>
           <Box px={2}>
-            <Text variant="md">Sorry, we couldn’t find Artworks for ”{keyword}.”</Text>
+            <Text variant="md">Sorry, we couldn’t find any Artworks for ”{keyword}.”</Text>
             <Text variant="md" color="black60">
               Please try searching again with a different spelling.
             </Text>
