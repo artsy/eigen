1f2fc87e2be3073cb7bd16b4632002ce40341b1e
araujobarret <araujobarret@gmail.com>
2021-10-21 17:21:52 +0200

    feat(CX-2017): support multiple paramater on requestPhotos module

--- src/lib/Scenes/MyProfile/MyProfileEditFormModal.tsx
+++ src/lib/Scenes/MyProfile/MyProfileEditFormModal.tsx
@@ -4,22 +4,78 @@ import { FancyModal } from "lib/Components/FancyModal/FancyModal"
 import { FancyModalHeader } from "lib/Components/FancyModal/FancyModalHeader"
 import { TextArea } from "lib/Components/TextArea"
 import { showPhotoActionSheet } from "lib/utils/requestPhotos"
-import { Box, Button, Flex, Input, Join, Spacer, Text, Touchable, useColor } from "palette"
-import React from "react"
-import { ScrollView } from "react-native"
+import { isArray } from "lodash"
+import { Avatar, Box, Button, Flex, Input, Join, Spacer, Text, Touchable, useColor } from "palette"
+import React, { useRef } from "react"
+import { ScrollView, TextInput } from "react-native"
+import { createFragmentContainer, graphql } from "react-relay"
+import * as Yup from "yup"
+import {
+  getConvectionGeminiKey,
+  getGeminiCredentialsForEnvironment,
+  uploadFileToS3,
+} from "../Consignments/Submission/geminiUploadToS3"
+import { myProfileUpdateProfile } from "./myProfileUpdateProfile"
 
 interface MyProfileEditFormModalProps {
   visible: boolean
   onDismiss(): void
 }
 
+export interface EditMyProfileValuesSchema {
+  photo: string
+  name: string
+  bio: string
+}
+
+export const editMyProfileSchema = Yup.object().shape({
+  photo: Yup.string(),
+  name: Yup.string().required("Name is required").min(1, "Name is required"),
+  bio: Yup.string(),
+})
+
+export async function uploadPhoto(photo: EditMyProfileValuesSchema["photo"]) {
+  const convectionKey = await getConvectionGeminiKey()
+  const acl = "private"
+  const assetCredentials = await getGeminiCredentialsForEnvironment({ acl, name: convectionKey })
+  const bucket = assetCredentials.policyDocument.conditions.bucket
+  const s3 = await uploadFileToS3(photo, acl, assetCredentials)
+  return `https://${bucket}.s3.amazonaws.com/${s3.key}`
+}
+
 export const MyProfileEditFormModal: React.FC<MyProfileEditFormModalProps> = (props) => {
   const { visible, onDismiss } = props
   const color = useColor()
   const { showActionSheetWithOptions } = useActionSheet()
+  const nameInputRef = useRef<Input>(null)
+  const bioInputRef = useRef<TextInput>(null)
+
+  const { handleSubmit, handleChange, dirty, values, errors, validateForm } = useFormik<EditMyProfileValuesSchema>({
+    enableReinitialize: true,
+    validateOnChange: true,
+    validateOnBlur: true,
+    initialValues: {
+      name: me?.name ?? "",
+      bio: "",
+      photo: "",
+    },
+    initialErrors: {},
+    onSubmit: async ({ name, bio, photo }) => {
+      console.log("Submitting....", name, bio, photo)
+      try {
+        const externalPhotoUrl = await uploadPhoto(photo)
+        console.log("Check :: ", externalPhotoUrl)
+        await myProfileUpdateProfile({ name, bio, photo })
+      } catch (e) {
+        console.log("Catch error ", e)
+      }
+      onDismiss()
+    },
+    validationSchema: editMyProfileSchema,
+  })
 
   const chooseImageHandler = () => {
-    showPhotoActionSheet(showActionSheetWithOptions).then((images) => {
+    showPhotoActionSheet(showActionSheetWithOptions, false).then((images) => {
       console.log("Images :: ", images)
     })
   }
@@ -52,7 +108,7 @@ export const MyProfileEditFormModal: React.FC<MyProfileEditFormModalProps> = (pr
           <Flex mx={2}>
             <Input title="Name" required />
             <Spacer py={2} />
-            <TextArea title="ABOUT" />
+            <TextArea ref={bioInputRef} title="ABOUT" onChangeText={handleChange("bio") as (value: string) => void} />
             <Spacer py={2} />
             <Button
               flex={1}
@@ -68,3 +124,12 @@ export const MyProfileEditFormModal: React.FC<MyProfileEditFormModalProps> = (pr
     </FancyModal>
   )
 }
+
+export const MyProfileEditFormModalFragmentContainer = createFragmentContainer(MyProfileEditFormModal, {
+  me: graphql`
+    fragment MyProfileEditFormModal_me on Me {
+      name
+      bio
+    }
+  `,
+})
--- src/lib/utils/requestPhotos.ts
+++ src/lib/utils/requestPhotos.ts
@@ -1,16 +1,40 @@
 import { ActionSheetOptions } from "@expo/react-native-action-sheet"
+import { LegacyNativeModules } from "lib/NativeModules/LegacyNativeModules"
+import { Platform } from "react-native"
 import ImagePicker, { Image } from "react-native-image-crop-picker"
+import { osMajorVersion } from "./platformUtil"
 
-export async function requestPhotos(): Promise<Image[]> {
-  return ImagePicker.openPicker({
-    mediaType: "photo",
-    multiple: true,
-  })
+export async function requestPhotos<T extends boolean>(multiple: T): Promise<T extends true ? Image[] : Image>
+
+export async function requestPhotos(multiple: undefined): Promise<Image[]>
+
+export async function requestPhotos(multiple?: boolean | undefined): Promise<Image | Image[]> {
+  if (Platform.OS === "ios" && osMajorVersion() >= 14) {
+    return LegacyNativeModules.ARPHPhotoPickerModule.requestPhotos()
+  } else {
+    return ImagePicker.openPicker({
+      mediaType: "photo",
+      multiple: multiple ?? true,
+    })
+  }
 }
+
+type ShowActionSheet = (options: ActionSheetOptions, callback: (i: number) => void) => void
+
+export async function showPhotoActionSheet<T extends boolean>(
+  showActionSheet: ShowActionSheet,
+  allowMultipleSelection?: T
+): Promise<T extends true ? Image[] : Image>
+
+export async function showPhotoActionSheet(
+  showActionSheet: ShowActionSheet,
+  allowMultipleSelection: undefined
+): Promise<Image[]>
 
 export async function showPhotoActionSheet(
   showActionSheet: (options: ActionSheetOptions, callback: (i: number) => void) => void,
-  useModal: boolean = false
+  useModal: boolean = false,
+  allowMultipleSelection?: boolean
 ): Promise<Image[]> {
   return new Promise<Image[]>((resolve, reject) => {
     showActionSheet(
@@ -23,7 +47,7 @@ export async function showPhotoActionSheet(
         let photos = null
         try {
           if (buttonIndex === 0) {
-            photos = await requestPhotos()
+            photos = await requestPhotos(allowMultipleSelection !== undefined ? allowMultipleSelection : true)
             resolve(photos)
           }
           if (buttonIndex === 1) {
