6aa228cfd0acfa134e5ab719f3d44270f392107c
Ole <ole.richter91@gmail.com>
2021-10-21 12:11:16 +0200

    fix: Align my collection form input text (#5663)

--- src/lib/Scenes/MyCollection/Screens/ArtworkFormModal/Components/Dimensions.tsx
+++ src/lib/Scenes/MyCollection/Screens/ArtworkFormModal/Components/Dimensions.tsx
@@ -1,9 +1,8 @@
 import { useArtworkForm } from "lib/Scenes/MyCollection/Screens/ArtworkFormModal/Form/useArtworkForm"
-import { Flex, Input, RadioButton, Spacer, Text, useSpace } from "palette"
+import { Flex, Input, RadioButton, Spacer, Text } from "palette"
 import React from "react"
 
 export const Dimensions: React.FC = () => {
-  const space = useSpace()
   const { formik } = useArtworkForm()
 
   return (
@@ -11,7 +10,7 @@ export const Dimensions: React.FC = () => {
       <Flex flexDirection="row">
         <Text variant="xs">DIMENSIONS</Text>
       </Flex>
-      <Spacer my={1} />
+      <Spacer mt={1} mb={0.3} />
       <Flex flexDirection="row">
         <RadioButton selected={formik.values.metric === "cm"} onPress={() => formik.handleChange("metric")("cm")} />
         <Text marginRight="3">cm</Text>
@@ -20,29 +19,33 @@ export const Dimensions: React.FC = () => {
       </Flex>
       <Spacer my={1} />
       <Flex flexDirection="row">
-        <Input
-          title="HEIGHT"
-          keyboardType="decimal-pad"
-          onChangeText={formik.handleChange("height")}
-          onBlur={formik.handleBlur("height")}
-          defaultValue={formik.values.height}
-          style={{ marginRight: space(1) }}
-        />
-        <Input
-          title="WIDTH"
-          keyboardType="decimal-pad"
-          onChangeText={formik.handleChange("width")}
-          onBlur={formik.handleBlur("width")}
-          defaultValue={formik.values.width}
-          style={{ marginRight: space(1) }}
-        />
-        <Input
-          title="DEPTH"
-          keyboardType="decimal-pad"
-          onChangeText={formik.handleChange("depth")}
-          onBlur={formik.handleBlur("depth")}
-          defaultValue={formik.values.depth}
-        />
+        <Flex mr={1} flex={1}>
+          <Input
+            title="HEIGHT"
+            keyboardType="decimal-pad"
+            onChangeText={formik.handleChange("height")}
+            onBlur={formik.handleBlur("height")}
+            defaultValue={formik.values.height}
+          />
+        </Flex>
+        <Flex mr={1} flex={1}>
+          <Input
+            title="WIDTH"
+            keyboardType="decimal-pad"
+            onChangeText={formik.handleChange("width")}
+            onBlur={formik.handleBlur("width")}
+            defaultValue={formik.values.width}
+          />
+        </Flex>
+        <Flex flex={1}>
+          <Input
+            title="DEPTH"
+            keyboardType="decimal-pad"
+            onChangeText={formik.handleChange("depth")}
+            onBlur={formik.handleBlur("depth")}
+            defaultValue={formik.values.depth}
+          />
+        </Flex>
       </Flex>
     </>
   )
