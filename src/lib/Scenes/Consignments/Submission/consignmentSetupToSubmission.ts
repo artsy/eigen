ef1bdfacdfd6a4cea5a7708e3000fac3ba444fa5
Gon4ar5 <stanislau.hanchar@artsymail.com>
2021-10-25 01:09:21 +0300

    remove unused directive

--- src/lib/Scenes/Consignments/Submission/consignmentSetupToSubmission.ts
+++ src/lib/Scenes/Consignments/Submission/consignmentSetupToSubmission.ts
@@ -21,7 +21,6 @@ export const consignmentSetupToMutationInput = (submission: ConsignmentSetup) =>
       dimensionsMetric: submission.metadata && submission.metadata.unit,
       edition: !!submission.editionInfo,
       editionNumber: submission.editionInfo && submission.editionInfo.number,
-      // @ts-expect-error STRICTNESS_MIGRATION --- 🚨 Unsafe legacy code 🚨 Please delete this and fix any type errors if you have time 🙏
       editionSize: submission.editionInfo && submission.editionInfo.size,
       height: submission.metadata && submission.metadata.height,
       locationCity: submission.location && submission.location.city,
