195c3633e7d2d571a9e8c75e377ac8c623f56c44
Dima Tretyak <itretyak@ya.ru>
2021-10-25 20:04:25 +0300

    feat: allow multiple select for size filter

---
+++ src/lib/Components/ArtworkFilter/Filters/NewSizesOptions.tsx
@@ -0,0 +1,43 @@
+import { StackScreenProps } from "@react-navigation/stack"
+import React from "react"
+import { ArtworkFilterNavigationStack } from ".."
+import { FilterData, FilterDisplayName, FilterParamName } from "../ArtworkFilterHelpers"
+import { MultiSelectOptionScreen } from "./MultiSelectOption"
+import { useMultiSelect } from "./useMultiSelect"
+
+interface SizeOptionsScreenProps extends StackScreenProps<ArtworkFilterNavigationStack, "SizeOptionsScreen"> {}
+
+export const SIZES_OPTIONS: FilterData[] = [
+  {
+    displayText: "Small (under 40cm)",
+    paramName: FilterParamName.sizes,
+    paramValue: "SMALL",
+  },
+  {
+    displayText: `Medium (40cm – 100cm)`,
+    paramName: FilterParamName.sizes,
+    paramValue: "MEDIUM",
+  },
+  {
+    displayText: "Large (over 100cm)",
+    paramName: FilterParamName.sizes,
+    paramValue: "LARGE",
+  },
+]
+
+export const NewSizesOptionsScreen: React.FC<SizeOptionsScreenProps> = ({ navigation }) => {
+  const { handleSelect, isSelected } = useMultiSelect({
+    options: SIZES_OPTIONS,
+    paramName: FilterParamName.sizes,
+  })
+  const filterOptions = SIZES_OPTIONS.map((option) => ({ ...option, paramValue: isSelected(option) }))
+
+  return (
+    <MultiSelectOptionScreen
+      onSelect={handleSelect}
+      filterHeaderText={FilterDisplayName.sizes}
+      filterOptions={filterOptions}
+      navigation={navigation}
+    />
+  )
+}
