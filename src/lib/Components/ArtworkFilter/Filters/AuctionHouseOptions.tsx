5f7141af75fc3462f1a029787d0b2c25b7b85270
araujobarret <araujobarret@gmail.com>
2021-10-28 16:11:38 +0200

    refactor(CX-1937): refactor to use the right multi option component

--- src/lib/Components/ArtworkFilter/Filters/AuctionHouseOptions.tsx
+++ src/lib/Components/ArtworkFilter/Filters/AuctionHouseOptions.tsx
@@ -2,7 +2,7 @@ import { StackScreenProps } from "@react-navigation/stack"
 import { ArtworkFilterNavigationStack } from "lib/Components/ArtworkFilter"
 import { FilterData, FilterDisplayName, FilterParamName } from "lib/Components/ArtworkFilter/ArtworkFilterHelpers"
 import React from "react"
-import { MultiSelectCheckOptionScreen } from "./MultiSelectCheckOption"
+import { MultiSelectOptionScreen } from "./MultiSelectOption"
 import { useMultiSelect } from "./useMultiSelect"
 
 export const AUCTION_HOUSE_OPTIONS: FilterData[] = [
@@ -27,7 +27,7 @@ interface AuctionHouseOptionsScreenProps
   extends StackScreenProps<ArtworkFilterNavigationStack, "AuctionHouseOptionsScreen"> {}
 
 export const AuctionHouseOptionsScreen: React.FC<AuctionHouseOptionsScreenProps> = ({ navigation }) => {
-  const { handleSelect, isSelected } = useMultiSelect({
+  const { handleSelect, isSelected, handleClear, isActive } = useMultiSelect({
     options: AUCTION_HOUSE_OPTIONS,
     paramName: FilterParamName.organizations,
   })
@@ -35,11 +35,12 @@ export const AuctionHouseOptionsScreen: React.FC<AuctionHouseOptionsScreenProps>
   const filterOptions = AUCTION_HOUSE_OPTIONS.map((option) => ({ ...option, paramValue: isSelected(option) }))
 
   return (
-    <MultiSelectCheckOptionScreen
+    <MultiSelectOptionScreen
       onSelect={handleSelect}
       filterHeaderText={FilterDisplayName.organizations}
       filterOptions={filterOptions}
       navigation={navigation}
+      {...(isActive ? { rightButtonText: "Clear", onRightButtonPress: handleClear } : {})}
     />
   )
 }
