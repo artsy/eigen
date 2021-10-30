c99d50591514176a612802a9725e3ab71765d7ed
brainbicycle <brian.beckerle@artsymail.com>
2021-10-30 18:22:15 -0400

    add some silly convention checks

--- scripts/check-flags.js
+++ scripts/check-flags.js
@@ -4,19 +4,12 @@
 "use strict"
 
 const fs = require('fs');
-// TODO: This is pretty fragile,
-// depends on prefix, file location and name of readyForRelease key
-// maybe we could do some fancy typescript parsing
-// but for now should at least add some tests to the features file
-// to make sure it doesn't break the script
-// gets slightly more complicated when we have to get old versions of the file
 const checkFlags = () => {
   try {
     const fileContent = fs.readFileSync('./src/lib/store/config/features.ts', 'utf8');
     const releasedFlagsRE = /(?<flag>AR\w+):\s*{\s*|.*readyForRelease:\s+(?<readyForRelease>true|false)/g
     const results = fileContent.matchAll(releasedFlagsRE);
     var lastFlag
-    var flags = {}
     var releasedFlags = []
     for (const result of results) {
       if (result.groups['flag']) {
