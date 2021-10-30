d1b6ef77c64dbda2b7e6b3b282dab330bba08021
brainbicycle <brian.beckerle@artsymail.com>
2021-10-30 17:42:42 -0400

    account for flags missing 'readyForRelease' key

--- scripts/check-flags.js
+++ scripts/check-flags.js
@@ -13,32 +13,37 @@ const fs = require('fs');
 const checkFlags = () => {
   try {
     const fileContent = fs.readFileSync('./src/lib/store/config/features.ts', 'utf8');
-    const flagRE = /(?<flag>AR\w+):\s*{\s*|.*readyForRelease:\s(?<readyForRelease>true|false)/g
-    const results = fileContent.matchAll(flagRE);
+    const releasedFlagsRE = /(?<flag>AR\w+):\s*{\s*|.*readyForRelease:\s+(?<readyForRelease>true|false)/g
+    const results = fileContent.matchAll(releasedFlagsRE);
     var lastFlag
     var flags = {}
+    var releasedFlags = []
     for (const result of results) {
       if (result.groups['flag']) {
         const currentFlag = result.groups['flag']
-        flags[currentFlag] = {}
         lastFlag = currentFlag
       }
       if (result.groups['readyForRelease']) {
         const readyForRelease = result.groups['readyForRelease']
-        flags[lastFlag]['readyForRelease'] = (readyForRelease === 'true')
+        if (readyForRelease === 'true') {
+          releasedFlags.push(lastFlag)
+        }
       }
     }
-    var releasedFlags = []
-    var hiddenFlags = []
-    // TODO: we need to also check for any flags that have not specified readyForRelease
-    for (const key in flags) {
-      const flag = flags[key]
-      if (flag['readyForRelease']) {
-        releasedFlags.push(key)
-      } else {
-        hiddenFlags.push(key)
+
+    const allFlagsRE = /(?<flag>AR\w+):/g
+    const allFlagResults = fileContent.matchAll(allFlagsRE);
+
+    var allFlags = []
+    for (const result of allFlagResults) {
+      if (result.groups['flag']) {
+        const currentFlag = result.groups['flag']
+        allFlags.push(currentFlag)
       }
     }
+
+    const hiddenFlags = allFlags.filter(flag => !releasedFlags.includes(flag));
+
     return { hiddenFlags, releasedFlags }
   } catch (err) {
     return err
