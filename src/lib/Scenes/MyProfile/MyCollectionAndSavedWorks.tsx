7c5a5d88a7ab8c4cc9370311c287e1b8a962fdeb
Sam Raj <sam.raj@artsymail.com>
2021-11-04 12:23:57 +0100

    removing Join component

--- src/lib/Scenes/MyProfile/MyCollectionAndSavedWorks.tsx
+++ src/lib/Scenes/MyProfile/MyCollectionAndSavedWorks.tsx
@@ -6,7 +6,7 @@ import { StickyTabPage } from "lib/Components/StickyTabPage/StickyTabPage"
 import { navigate } from "lib/navigation/navigate"
 import { defaultEnvironment } from "lib/relay/createEnvironment"
 import renderWithLoadProgress from "lib/utils/renderWithLoadProgress"
-import { Avatar, Box, Button, Flex, Join, Sans, Spacer, useColor, useSpace } from "palette"
+import { Avatar, Box, Button, Flex, Sans, useColor } from "palette"
 import React, { useEffect, useState } from "react"
 import { createFragmentContainer, QueryRenderer } from "react-relay"
 import { graphql } from "relay-runtime"
@@ -41,7 +41,6 @@ export const MyCollectionAndSavedWorks: React.FC<{ me: NonNullable<MyCollectionA
 }
 
 export const MyProfileHeader: React.FC<{ me: NonNullable<MyCollectionAndSavedWorks_me> }> = ({ me }) => {
-  const space = useSpace()
   const color = useColor()
 
   const [showModal, setShowModal] = useState(false)
@@ -71,47 +70,47 @@ export const MyProfileHeader: React.FC<{ me: NonNullable<MyCollectionAndSavedWor
           navigate("/my-profile/settings")
         }}
       />
-      <Join separator={<Spacer py={space(1)} />}>
-        <Flex flexDirection="row" alignItems="center" px={2}>
-          <Box
-            height="99"
-            width="99"
-            borderRadius="50"
-            backgroundColor={color("black10")}
-            justifyContent="center"
-            alignItems="center"
-          >
-            {!!profileIcon ? (
-              <Avatar src={profileIcon} size="md" />
-            ) : (
-              <Image source={require("../../../../images/profile_placeholder_avatar.webp")} />
-            )}
-          </Box>
-          <Box px={2} flexShrink={1}>
-            <Sans size="10" color={color("black100")}>
-              {me?.name}
-            </Sans>
-            {!!me?.createdAt && (
-              <Sans size="2" color={color("black60")}>{`Member since ${new Date(me?.createdAt).getFullYear()}`}</Sans>
-            )}
-          </Box>
-        </Flex>
-        <Sans size="2" color={color("black100")} px={2}>
+      <Flex flexDirection="row" alignItems="center" px={2}>
+        <Box
+          height="99"
+          width="99"
+          borderRadius="50"
+          backgroundColor={color("black10")}
+          justifyContent="center"
+          alignItems="center"
+        >
+          {!!profileIcon ? (
+            <Avatar src={profileIcon} size="md" />
+          ) : (
+            <Image source={require("../../../../images/profile_placeholder_avatar.webp")} />
+          )}
+        </Box>
+        <Box px={2} flexShrink={1}>
+          <Sans size="10" color={color("black100")}>
+            {me?.name}
+          </Sans>
+          {!!me?.createdAt && (
+            <Sans size="2" color={color("black60")}>{`Member since ${new Date(me?.createdAt).getFullYear()}`}</Sans>
+          )}
+        </Box>
+      </Flex>
+      {!!me?.bio && (
+        <Sans size="2" color={color("black100")} px={2} pt={2}>
           {me?.bio}
         </Sans>
-        <Flex px={2} pb={2}>
-          <Button
-            variant="outline"
-            size="small"
-            flex={1}
-            onPress={() => {
-              setShowModal(true)
-            }}
-          >
-            Edit Profile
-          </Button>
-        </Flex>
-      </Join>
+      )}
+      <Flex p={2}>
+        <Button
+          variant="outline"
+          size="small"
+          flex={1}
+          onPress={() => {
+            setShowModal(true)
+          }}
+        >
+          Edit Profile
+        </Button>
+      </Flex>
     </>
   )
 }
