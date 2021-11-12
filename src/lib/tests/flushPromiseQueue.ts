895a07bac01ef7e849a23f52499a196b18b9fc38
Pavlos Vinieratos <pvinis@gmail.com>
2021-11-12 07:19:05 +0000

    boom

--- src/lib/tests/flushPromiseQueue.ts
+++ src/lib/tests/flushPromiseQueue.ts
@@ -1,6 +1,4 @@
 /**
  * test utility to wait for all promises to be resolved
  */
-export function flushPromiseQueue() {
-  return new Promise((r) => setTimeout(() => requestAnimationFrame(r), 0))
-}
+export const flushPromiseQueue = () => new Promise((r) => setTimeout(() => requestAnimationFrame(r), 0))
