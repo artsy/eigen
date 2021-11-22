2d141d230e406eae3f0b5b741500914044a54930
Pavlos Vinieratos <pvinis@gmail.com>
2021-11-22 11:33:10 +0000

    fix(tooling): make fonts work for everyone at artsy (#5811)
    * fixed?
    
    * in case its missing
    
    * conflict

---
+++ scripts/post-pod-install.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+
+def remove_mapbox_creds
+  $netrc_path = File.expand_path('~/.netrc')
+  if File.exists?(".i-had-a-netrc-file")
+    contents = File.read($netrc_path)
+    cleaned = contents.gsub(/machine api\.mapbox\.com\nlogin mapbox\npassword .*$/, "")
+    File.open($netrc_path, 'w') { |f|
+      f.write(cleaned)
+    }
+  else
+    File.delete($netrc_path) if File.exist?($netrc_path)
+  end
+end
+
+remove_mapbox_creds
+
+
+def fix_fonts_in_uifonts_pod
+  # inspired by https://github.com/artsy/Artsy-OSSUIFonts/blob/master/Pod/Scripts/ArtsySetup.rb while we still use that pod
+
+  system("./scripts/download-fonts")
+
+  font_file = "Pods/Artsy+UIFonts/Pod/Classes/UIFont+ArtsyFonts.m"
+  system("chmod +w #{font_file}")
+  contents = File.read(font_file)
+  changed = contents.gsub(/static BOOL useClosedFonts = false;/, "static BOOL useClosedFonts = true;")
+  File.write(font_file, changed)
+end
+
+fix_fonts_in_uifonts_pod
