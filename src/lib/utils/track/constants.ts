5fd308730eef901553861622a93a8e84f6bbc00a
George Kartalis <g.kartalis7@gmail.com>
2021-12-01 16:50:14 +0100

    chore: remove userinterfacestyle from analytics constants

--- src/lib/utils/track/constants.ts
+++ src/lib/utils/track/constants.ts
@@ -1,8 +1,3 @@
 export const AnalyticsConstants = {
   FreshInstall: "first user install",
-
-  UserInterfaceStyle: {
-    key: "user interface style",
-    value: { Dark: "dark", Light: "light", Unspecified: "unspecified" },
-  },
 }
