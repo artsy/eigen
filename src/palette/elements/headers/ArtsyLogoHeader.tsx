0b785d26a1e201f917857778f4d9f79bd44a1c3c
Angela <patrinoua@gmail.com>
2021-12-06 16:39:40 +0100

    shadow

--- src/palette/elements/headers/ArtsyLogoHeader.tsx
+++ src/palette/elements/headers/ArtsyLogoHeader.tsx
@@ -1,13 +1,25 @@
-import { ArtsyLogoIcon, Box, Flex, Separator, Spacer } from "palette"
+import { ArtsyLogoIcon, Box, Flex, Spacer } from "palette"
 import React from "react"
 
 export const ArtsyLogoHeader: React.FC = ({}) => (
-  <Box mb={1} mt={2}>
-    <Flex alignItems="center">
-      <ArtsyLogoIcon scale={0.75} />
-    </Flex>
-    <Spacer mb="1" />
-    <Separator />
+  <>
+    <Box
+      mt={2}
+      mb={1}
+      style={{
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.04,
+        shadowRadius: 2.0,
+        backgroundColor: "white",
+        height: 40,
+      }}
+    >
+      <Flex alignItems="center">
+        <ArtsyLogoIcon scale={0.75} />
+      </Flex>
+      <Spacer mb="1" />
+    </Box>
     <Spacer mb="2" />
-  </Box>
+  </>
 )
