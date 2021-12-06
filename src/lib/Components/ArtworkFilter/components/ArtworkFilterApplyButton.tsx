76c81ccbca0eb479cb4c9bd877ccc7d22aca90a9
Dima Tretyak <itretyak@ya.ru>
2021-12-06 15:04:55 +0300

    chore: add underline line when button is pressed

--- src/lib/Components/ArtworkFilter/components/ArtworkFilterApplyButton.tsx
+++ src/lib/Components/ArtworkFilter/components/ArtworkFilterApplyButton.tsx
@@ -1,12 +1,46 @@
 import { useFeatureFlag } from "lib/store/GlobalStore"
 import { BellIcon, Box, Button, Separator, Text, useColor } from "palette"
-import React from "react"
-import { TouchableOpacity, ViewStyle } from "react-native"
+import React, { ReactNode, useState } from "react"
+import { Pressable } from "react-native"
 
 export interface ArtworkFilterApplyButtonProps {
   disabled: boolean
   onCreateAlertPress?: () => void
   onPress: () => void
+}
+
+interface Button {
+  label: string
+  disabled?: boolean
+  icon?: ReactNode
+  onPress: () => void
+}
+
+const InnerButton: React.FC<Button> = (props) => {
+  const { label, disabled, icon, onPress } = props
+  const [isPressed, setIsPressed] = useState(false)
+
+  return (
+    <Pressable
+      onPressIn={() => setIsPressed(true)}
+      onPressOut={() => setIsPressed(false)}
+      onPress={onPress}
+      disabled={disabled}
+      style={{ flex: 1, opacity: disabled ? 0.4 : 1 }}
+    >
+      <Box flex={1} flexDirection="row" alignItems="center" justifyContent="center">
+        {icon}
+        <Text
+          variant="xs"
+          color="white100"
+          lineHeight={14}
+          style={{ textDecorationLine: isPressed ? "underline" : "none" }}
+        >
+          {label}
+        </Text>
+      </Box>
+    </Pressable>
+  )
 }
 
 export const ArtworkFilterApplyButton: React.FC<ArtworkFilterApplyButtonProps> = (props) => {
@@ -15,16 +49,6 @@ export const ArtworkFilterApplyButton: React.FC<ArtworkFilterApplyButtonProps> =
   const isEnabledImprovedAlertsFlow = useFeatureFlag("AREnableImprovedAlertsFlow")
 
   if (isEnabledImprovedAlertsFlow && onCreateAlertPress) {
-    const buttonContainerStyle: ViewStyle = {
-      flex: 1,
-    }
-    const buttonStyle: ViewStyle = {
-      flex: 1,
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "center",
-    }
-
     return (
       <Box
         p={2}
@@ -41,28 +65,13 @@ export const ArtworkFilterApplyButton: React.FC<ArtworkFilterApplyButtonProps> =
         }}
       >
         <Box height={50} borderRadius={50} px={1} backgroundColor="black100" flexDirection="row" alignItems="center">
-          <TouchableOpacity onPress={onCreateAlertPress} style={buttonContainerStyle}>
-            <Box style={buttonStyle}>
-              <BellIcon fill="white100" width="15px" height="15px" mr={1} />
-              <Text variant="xs" color="white100" lineHeight={14}>
-                Create Alert
-              </Text>
-            </Box>
-          </TouchableOpacity>
-
+          <InnerButton
+            label="Create Alert"
+            icon={<BellIcon fill="white100" width="15px" height="15px" mr={1} />}
+            onPress={onCreateAlertPress}
+          />
           <Box width="1" height={20} backgroundColor="white100" mx={1} />
-
-          <TouchableOpacity
-            onPress={onPress}
-            style={[buttonContainerStyle, disabled && { opacity: 0.4 }]}
-            disabled={disabled}
-          >
-            <Box style={buttonStyle}>
-              <Text variant="xs" color="white100" lineHeight={14}>
-                Apply Filters
-              </Text>
-            </Box>
-          </TouchableOpacity>
+          <InnerButton disabled={disabled} label="Apply Filters" onPress={onPress} />
         </Box>
       </Box>
     )
