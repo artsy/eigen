2f237196aa78ebd4b8d1b4b0b92ed91cd7cdede4
Sam Raj <sam.raj@artsymail.com>
2021-12-10 14:19:36 +0100

    location in test

--- src/lib/Scenes/MyCollection/utils/cleanArtworkPayload.tests.ts
+++ src/lib/Scenes/MyCollection/utils/cleanArtworkPayload.tests.ts
@@ -63,6 +63,7 @@ describe("explicitlyClearedFields", () => {
         date: "some-date",
         editionSize: "some-edition-size",
         metric: "in",
+        location: "some-location",
         photos: [],
         provenance: "some-provenance",
       }
