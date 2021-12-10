22192c264af7f075a0f9f370b419c08dc63f8200
George Kartalis <g.kartalis7@gmail.com>
2021-12-10 10:33:39 +0100

    feat: added screenREaderStatus to analytics constants

--- src/lib/utils/track/constants.ts
+++ src/lib/utils/track/constants.ts
@@ -1,3 +1,11 @@
 export const AnalyticsConstants = {
   FreshInstall: "first user install",
+  UserInterfaceStyle: {
+    key: "user interface style",
+    value: { Dark: "dark", Light: "light", Unspecified: "unspecified" },
+  },
+  ScreenReaderStatus: {
+    key: "screen reader status",
+    value: { Enabled: "enabled", Disabled: "disabled" },
+  },
 }
