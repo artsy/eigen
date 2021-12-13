e5dca7ca9a0aa92fb85eb24fb11d5f273ac915b1
Jon Allured <jon.allured@gmail.com>
2021-12-13 09:26:04 -0600

    Use better layout for small editorial widget

--- ArtsyWidget/LatestArticles/LatestArticles+SmallView.swift
+++ ArtsyWidget/LatestArticles/LatestArticles+SmallView.swift
@@ -25,23 +25,23 @@ extension LatestArticles {
                         .scaledToFill()
                         .frame(width: geo.size.width, height: geo.size.height, alignment: .top)
                     VStack() {
-                        HStack() {
+                        Spacer()
+                        HStack(alignment: .top) {
+                            VStack() {
+                                PrimaryText(name: articleTitle, color: .white)
+                                    .lineLimit(2)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                SecondaryText(title: publishedAt, color: .white)
+                                    .lineLimit(1)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                            Spacer()
                             Image(uiImage: artsyLogo)
                                 .resizable()
                                 .frame(width: 20, height: 20)
-                                .padding([.leading, .top], 10)
-                            Spacer()
                         }
-                        Spacer()
-                        VStack() {
-                            PrimaryText(name: articleTitle, color: .white)
-                                .lineLimit(2)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            SecondaryText(title: publishedAt, color: .white)
-                                .lineLimit(1)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        .padding([.leading, .trailing, .bottom], 10)
+                        .padding(10)
+                        .background(Color.black)
                     }
                     .widgetURL(articleUrl)
                 }
