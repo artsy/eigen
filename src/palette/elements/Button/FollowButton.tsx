a03dd0b529e9dff457fa1264e87ac2468de6c260
Fladson Gomes <fladsonthiago@gmail.com>
2021-12-15 18:37:41 +0100

    feat: add more props to FollowButton component
    Some places are using other props that we did not cover on the component first iteration.

--- src/palette/elements/Button/FollowButton.tsx
+++ src/palette/elements/Button/FollowButton.tsx
@@ -1,20 +1,28 @@
 import { Button, CheckIcon } from "palette"
 import React from "react"
 import { PressableProps } from "react-native"
+import { HapticFeedbackTypes } from "react-native-haptic-feedback"
 
 export interface FollowButtonProps {
   isFollowed: boolean
   onPress: PressableProps["onPress"]
+  haptic?: HapticFeedbackTypes | true
+  /** Makes button full width */
+  block?: boolean
+  /** Displays a loader in the button */
+  loading?: boolean
 }
-export const FollowButton: React.FC<FollowButtonProps> = ({ isFollowed, onPress }) => {
+export const FollowButton: React.FC<FollowButtonProps> = ({ isFollowed, onPress, haptic, loading }) => {
   return (
     <Button
+      onPress={onPress}
+      haptic={haptic}
+      loading={loading}
       variant="outline"
       size="small"
       longestText="Following"
-      onPress={onPress}
-      // TODO: add borderColor to control the Follow state
       icon={isFollowed && <CheckIcon fill="black60" width="16px" height="16px" />}
+      // TODO: add borderColor to control the Follow state (it should be black30)
     >
       {isFollowed ? "Following" : "Follow"}
     </Button>
