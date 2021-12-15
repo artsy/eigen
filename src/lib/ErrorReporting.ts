e84dae8ee58ef400c1e2b2f3cc2da07d716e1766
Pavlos Vinieratos <pvinis@gmail.com>
2021-12-15 19:08:58 +0200

    some small things

--- src/lib/ErrorReporting.ts
+++ src/lib/ErrorReporting.ts
@@ -1,11 +1,11 @@
-import * as Sentry from "@sentry/react-native"
+import Sentry from "@sentry/react-native"
 import { useEffect } from "react"
 import { Platform } from "react-native"
 import Config from "react-native-config"
 import { sentryReleaseName } from "../../app.json"
 import { GlobalStore, useFeatureFlag } from "./store/GlobalStore"
 
-function setupSentry(props: Partial<Sentry.ReactNativeOptions> = {}) {
+export const setupSentry = (props: Partial<Sentry.ReactNativeOptions> = {}) => {
   if (!__DEV__ && Config.SENTRY_DSN) {
     Sentry.init({
       dsn: Config.SENTRY_DSN,
@@ -19,8 +19,6 @@ function setupSentry(props: Partial<Sentry.ReactNativeOptions> = {}) {
     })
   }
 }
-
-setupSentry()
 
 export function useSentryConfig() {
   const environment = GlobalStore.useAppState((store) => store.config.environment.env)
--- src/lib/utils/useScreenReaderTracking.ts
+++ src/lib/utils/useScreenReaderTracking.ts
@@ -1,24 +1,21 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { AccessibilityInfo } from "react-native"
 import { AnalyticsConstants } from "./track/constants"
 import { SegmentTrackingProvider } from "./track/SegmentTrackingProvider"
 
 // the purpose of this hook is to track the screen reader status of the user's device
 export const useScreenReaderTracking = () => {
-  const [isScreenReaderEnabled, setIsScreenReaderEnabled] = useState(false)
   useEffect(() => {
-    const getScreenReaderStatus = async () => {
+    const doIt = async () => {
       const isEnabled = await AccessibilityInfo.isScreenReaderEnabled()
-      setIsScreenReaderEnabled(isEnabled)
+      SegmentTrackingProvider.identify?.(null, {
+        [AnalyticsConstants.ScreenReaderStatus.key]: (() => {
+          return isEnabled
+            ? AnalyticsConstants.ScreenReaderStatus.value.Enabled
+            : AnalyticsConstants.ScreenReaderStatus.value.Disabled
+        })(),
+      })
     }
-
-    getScreenReaderStatus()
-    SegmentTrackingProvider.identify?.(null, {
-      [AnalyticsConstants.ScreenReaderStatus.key]: (() => {
-        return isScreenReaderEnabled
-          ? AnalyticsConstants.ScreenReaderStatus.value.Enabled
-          : AnalyticsConstants.ScreenReaderStatus.value.Disabled
-      })(),
-    })
+    doIt()
   }, [])
 }
