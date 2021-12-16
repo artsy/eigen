49fd6a2950121c6af443536b79a6f692452a2935
Pavlos Vinieratos <pvinis@gmail.com>
2021-12-16 05:39:41 -0500

    Update src/lib/ErrorReporting.ts

--- src/lib/ErrorReporting.ts
+++ src/lib/ErrorReporting.ts
@@ -1,4 +1,4 @@
-import Sentry from "@sentry/react-native"
+import * as Sentry from "@sentry/react-native"
 import { useEffect } from "react"
 import { Platform } from "react-native"
 import Config from "react-native-config"
