3467219abb186ca5555cb4d72ee6345813e76d76
George Kartalis <g.kartalis7@gmail.com>
2021-12-16 22:25:52 +0100

    spec: fixed async failing test

--- src/lib/utils/useScreenReaderTracking.tests.ts
+++ src/lib/utils/useScreenReaderTracking.tests.ts
@@ -1,88 +1,22 @@
-import { renderHook } from "@testing-library/react-hooks"
+import { act, renderHook } from "@testing-library/react-hooks"
 import { SegmentTrackingProvider } from "./track/SegmentTrackingProvider"
 import { useScreenReaderTracking } from "./useScreenReaderTracking"
+
+import { waitFor } from "@testing-library/react-native"
 
 jest.mock("react-native", () => ({
   AccessibilityInfo: { isScreenReaderEnabled: jest.fn().mockResolvedValue(true) },
 }))
 
-import { AccessibilityInfo } from "react-native"
-import { useEffect, useRef, useState } from "react"
-
-const isEnabled = (v) => new Promise((res, rej) => setTimeout(() => res(v), 100))
-
-const useTest = () => {
-  const [a, setA] = useState(1)
-  useEffect(() => {
-    const doIt = async () => {
-      const b = await isEnabled(3)
-      setA(b)
-    }
-    doIt()
-  }, [])
-  return a
-}
-
-const useSequence = (values: string[], intervalMs = 50) => {
-  const [first, ...otherValues] = values
-  const [value, setValue] = useState(() => first)
-  const index = useRef(0)
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setValue(otherValues[index.current++])
-      if (index.current >= otherValues.length) {
-        clearInterval(interval)
-      }
-    }, intervalMs)
-    return () => {
-      clearInterval(interval)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, otherValues)
-
-  return value
-}
-
 describe("useScreenReaderTracking", () => {
-  xit("works", async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useTest())
-    expect(result.current).toBe(1)
-    await waitForNextUpdate()
-    expect(result.current).toBe(2)
-  })
-  test("should wait for next update", async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useSequence(["first", "second"]))
+  it("Should track the status of the screen reader of a user", async () => {
+    renderHook(() => useScreenReaderTracking())
 
-    expect(result.current).toBe("first")
-
-    await waitForNextUpdate()
-
-    expect(result.current).toBe("second")
-  })
-
-  test("should wait for multiple updates", async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useSequence(["first", "second", "third"]))
-
-    expect(result.current).toBe("first")
-
-    await waitForNextUpdate()
-
-    expect(result.current).toBe("second")
-
-    await waitForNextUpdate()
-
-    expect(result.current).toBe("third")
-  })
-
-  xit("Should track the status of the screen reader of a user", async () => {
-    expect(await AccessibilityInfo.isScreenReaderEnabled()).toBe(true)
-
-    const { waitForNextUpdate, waitFor } = renderHook(() => useScreenReaderTracking())
-
-    // await waitForNextUpdate({ timeout: 4000 })
-
-    await waitFor(() => expect(SegmentTrackingProvider.identify).toHaveBeenCalledTimes(1))
-    // expect(SegmentTrackingProvider.identify).toHaveBeenCalledWith(null, { "screen reader status": "disabled" })
+    await act(() =>
+      waitFor(() => {
+        expect(SegmentTrackingProvider.identify).toHaveBeenCalledTimes(1)
+        expect(SegmentTrackingProvider.identify).toHaveBeenCalledWith(null, { "screen reader status": "enabled" })
+      })
+    )
   })
 })
