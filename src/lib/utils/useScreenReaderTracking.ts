8328700dbd18903535a2816c8df2a4e440c30c3b
Pavlos Vinieratos <pvinis@gmail.com>
2021-12-16 12:41:22 +0200

    back

--- src/lib/utils/useScreenReaderTracking.ts
+++ src/lib/utils/useScreenReaderTracking.ts
@@ -1,21 +1,24 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { AccessibilityInfo } from "react-native"
 import { AnalyticsConstants } from "./track/constants"
 import { SegmentTrackingProvider } from "./track/SegmentTrackingProvider"
 
 // the purpose of this hook is to track the screen reader status of the user's device
 export const useScreenReaderTracking = () => {
+  const [isScreenReaderEnabled, setIsScreenReaderEnabled] = useState(false)
   useEffect(() => {
-    const doIt = async () => {
+    const getScreenReaderStatus = async () => {
       const isEnabled = await AccessibilityInfo.isScreenReaderEnabled()
-      SegmentTrackingProvider.identify?.(null, {
-        [AnalyticsConstants.ScreenReaderStatus.key]: (() => {
-          return isEnabled
-            ? AnalyticsConstants.ScreenReaderStatus.value.Enabled
-            : AnalyticsConstants.ScreenReaderStatus.value.Disabled
-        })(),
-      })
+      setIsScreenReaderEnabled(isEnabled)
     }
-    doIt()
+
+    getScreenReaderStatus()
+    SegmentTrackingProvider.identify?.(null, {
+      [AnalyticsConstants.ScreenReaderStatus.key]: (() => {
+        return isScreenReaderEnabled
+          ? AnalyticsConstants.ScreenReaderStatus.value.Enabled
+          : AnalyticsConstants.ScreenReaderStatus.value.Disabled
+      })(),
+    })
   }, [])
 }
