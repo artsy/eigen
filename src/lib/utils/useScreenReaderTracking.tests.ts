5b2d58e8b1709530bb431f36acfaab60bca22cc2
Pavlos Vinieratos <pvinis@gmail.com>
2021-12-17 13:10:19 +0200

    refactor: screenreaderTracking hook to not use state (#5909)
    * wip
    
    * spec: fixed async failing test
    
    Co-authored-by: George Kartalis <g.kartalis7@gmail.com>

--- src/lib/utils/useScreenReaderTracking.tests.ts
+++ src/lib/utils/useScreenReaderTracking.tests.ts
@@ -1,12 +1,22 @@
-import { renderHook } from "@testing-library/react-hooks"
+import { act, renderHook } from "@testing-library/react-hooks"
 import { SegmentTrackingProvider } from "./track/SegmentTrackingProvider"
 import { useScreenReaderTracking } from "./useScreenReaderTracking"
 
+import { waitFor } from "@testing-library/react-native"
+
+jest.mock("react-native", () => ({
+  AccessibilityInfo: { isScreenReaderEnabled: jest.fn().mockResolvedValue(true) },
+}))
+
 describe("useScreenReaderTracking", () => {
-  it("Should track the status of the screen reader of a user", () => {
+  it("Should track the status of the screen reader of a user", async () => {
     renderHook(() => useScreenReaderTracking())
 
-    expect(SegmentTrackingProvider.identify).toHaveBeenCalledTimes(1)
-    expect(SegmentTrackingProvider.identify).toHaveBeenCalledWith(null, { "screen reader status": "disabled" })
+    await act(() =>
+      waitFor(() => {
+        expect(SegmentTrackingProvider.identify).toHaveBeenCalledTimes(1)
+        expect(SegmentTrackingProvider.identify).toHaveBeenCalledWith(null, { "screen reader status": "enabled" })
+      })
+    )
   })
 })
