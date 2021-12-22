558f62a6f06351abdeb03f66a6e8fa0ab0c17626
Pavlos Vinieratos <pvinis@gmail.com>
2021-12-22 14:38:19 +0400

    sweet sweet types

--- src/palette/elements/Button/FollowButton.tsx
+++ src/palette/elements/Button/FollowButton.tsx
@@ -1,27 +1,19 @@
 import { Button, CheckIcon } from "palette"
 import React from "react"
-import { PressableProps } from "react-native"
-import { HapticFeedbackTypes } from "react-native-haptic-feedback"
+import { ButtonProps } from "."
 
-export interface FollowButtonProps {
+type FollowButtonProps = Omit<ButtonProps, "variant" | "size" | "longestText" | "icon" | "children"> & {
   isFollowed: boolean
-  onPress: PressableProps["onPress"]
-  haptic?: HapticFeedbackTypes | true
-  /** Makes button full width */
-  block?: boolean
-  /** Displays a loader in the button */
-  loading?: boolean
 }
-export const FollowButton: React.FC<FollowButtonProps> = ({ isFollowed, onPress, haptic, loading }) => {
+
+export const FollowButton: React.FC<FollowButtonProps> = ({ isFollowed, ...rest }) => {
   return (
     <Button
-      onPress={onPress}
-      haptic={haptic}
-      loading={loading}
       variant={isFollowed ? "outline" : "outlineGray"}
       size="small"
       longestText="Following"
       icon={isFollowed && <CheckIcon fill="black60" width="16px" height="16px" />}
+      {...rest}
     >
       {isFollowed ? "Following" : "Follow"}
     </Button>
