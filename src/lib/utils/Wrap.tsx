2701fc612068271a68957829608b7443446ef288
Pavlos Vinieratos <pvinis@gmail.com>
2021-12-22 14:32:27 +0400

    better types

--- src/lib/utils/Wrap.tsx
+++ src/lib/utils/Wrap.tsx
@@ -2,9 +2,9 @@ import React, { FC } from "react"
 
 interface WrapProps {
   if: boolean
-  with: (children: React.ReactElement) => JSX.Element
+  with: (children: React.ReactNode) => JSX.Element
 }
 
 export const Wrap: FC<WrapProps> = ({ if: condition, with: wrapper, children }) => {
-  return condition ? wrapper(children) : children
+  return condition ? wrapper(children) : <>children</>
 }
