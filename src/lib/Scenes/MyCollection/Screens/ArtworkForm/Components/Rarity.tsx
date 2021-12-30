6aaa2feed9b3ce5722602f457edc95d7a601f837
Sam Raj <sam.raj@artsymail.com>
2021-12-30 11:04:12 +0100

    Rarity classification types modal

--- src/lib/Scenes/MyCollection/Screens/ArtworkForm/Components/Rarity.tsx
+++ src/lib/Scenes/MyCollection/Screens/ArtworkForm/Components/Rarity.tsx
@@ -1,13 +1,16 @@
+import { FancyModal } from "lib/Components/FancyModal/FancyModal"
 import { useArtworkForm } from "lib/Scenes/MyCollection/Screens/ArtworkForm/Form/useArtworkForm"
 import { artworkRarityClassifications } from "lib/utils/artworkRarityClassifications"
-import { Flex, Input, INPUT_HEIGHT, Text } from "palette"
+import { CloseIcon, Flex, Input, INPUT_HEIGHT, Separator, Text } from "palette"
 import { Select } from "palette/elements/Select"
-import React from "react"
+import React, { useState } from "react"
+import { TouchableOpacity } from "react-native"
 
 export type AttributionClassType = "LIMITED_EDITION" | "OPEN_EDITION" | "UNIQUE" | "UNKNOWN_EDITION"
 
 export const Rarity: React.FC = () => {
   const { formik } = useArtworkForm()
+  const [isRarityInfoModalVisible, setRarityInfoModalVisible] = useState(false)
 
   const handleValueChange = (value: AttributionClassType) => {
     formik.handleChange("attributionClass")(value)
@@ -21,6 +24,8 @@ export const Rarity: React.FC = () => {
         enableSearch={false}
         title="Rarity"
         placeholder="Select"
+        tooltipText="What is this?"
+        onTooltipPress={() => setRarityInfoModalVisible(true)}
         options={artworkRarityClassifications}
         testID="rarity-select"
       />
@@ -53,6 +58,46 @@ export const Rarity: React.FC = () => {
           </Flex>
         </Flex>
       )}
+      <RarityFieldInformationModal
+        title="Classifications"
+        visible={isRarityInfoModalVisible}
+        onDismiss={() => setRarityInfoModalVisible(false)}
+      />
     </>
+  )
+}
+
+const RarityFieldInformationModal: React.FC<{
+  title: string
+  visible: boolean
+  onDismiss(): any
+}> = ({ title, visible, onDismiss }) => {
+  return (
+    <FancyModal visible={visible} onBackgroundPressed={onDismiss}>
+      <Flex p="2" pb={15} flexDirection="row" alignItems="center" flexGrow={0}>
+        <Flex flex={1} />
+        <Flex flex={2} alignItems="center">
+          <Text>{title}</Text>
+        </Flex>
+        <TouchableOpacity
+          onPress={onDismiss}
+          hitSlop={{ top: 20, right: 20, bottom: 20, left: 20 }}
+          style={{ flex: 1, alignItems: "flex-end" }}
+        >
+          <CloseIcon fill="black60" />
+        </TouchableOpacity>
+      </Flex>
+      <Separator />
+      <Flex m={2}>
+        {artworkRarityClassifications.map((classification) => (
+          <Flex mb={2} key={classification.label}>
+            <Text variant="md" style={{ fontSize: 13, marginBottom: 2, textTransform: "uppercase" }}>
+              {classification.label}
+            </Text>
+            <Text style={{ fontSize: 16, marginBottom: 2 }}>{classification.description}</Text>
+          </Flex>
+        ))}
+      </Flex>
+    </FancyModal>
   )
 }
