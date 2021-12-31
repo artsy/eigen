d477252cccf33db3f102d8adc40b9821f3f78c9b
Ole <ole.richter91@gmail.com>
2021-12-31 14:36:37 +0100

    feat: Allow My Collection artworks without image (#5965)

--- src/lib/Scenes/MyCollection/Screens/ArtworkForm/Form/artworkSchema.tsx
+++ src/lib/Scenes/MyCollection/Screens/ArtworkForm/Form/artworkSchema.tsx
@@ -7,18 +7,6 @@ export const artworkSchema = Yup.object().shape({
     .nullable()
     .test("artistSearchResult", "Artist search result required", (value) => value !== null),
   medium: Yup.string().test("medium", "Medium required", (value) => value !== ""),
-  photos: Yup.array().test("photos", "Photos required", (value) => {
-    // Allow us to bypass adding photos in DEV
-    if (__DEV__) {
-      return true
-    }
-
-    if (value === null || value === undefined) {
-      return false
-    }
-
-    return value.length > 0
-  }),
   title: Yup.string().test("title", "Title is required", (value) => value !== ""),
 })
 
