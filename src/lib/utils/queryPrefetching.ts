ad3df14b747e19f5e3df750de87e5fbf97ce42ba
Ole <ole.richter91@gmail.com>
2022-01-07 16:36:33 +0100

    use networkCacheConfig

--- src/lib/utils/queryPrefetching.ts
+++ src/lib/utils/queryPrefetching.ts
@@ -43,9 +43,7 @@ const isRateLimited = async () => {
 const prefetchQuery = async (environment: Environment, query: GraphQLTaggedNode, variables: Variables = {}) => {
   const operation = createOperationDescriptor(getRequest(query), variables)
 
-  await fetchQuery(environment, query, variables, {
-    fetchPolicy: "store-or-network",
-  })
+  await fetchQuery(environment, query, variables, { networkCacheConfig: { force: false } })
 
   // this will retain the result in the relay store so it's not garbage collected.
   environment.retain(operation)
