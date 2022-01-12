aa5dd2cc7c987cd7065b8237d7735853993bd125
Angela Patrinou <36475005+patrinoua@users.noreply.github.com>
2022-01-12 20:11:44 +0200

    remove things from other PR

--- src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ArtworkDetails/ArtworkDetails.tsx
+++ src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ArtworkDetails/ArtworkDetails.tsx
@@ -1,6 +1,5 @@
-import { Flex, Input, RadioButton, Spacer, Text } from "palette"
+import { CTAButton, Flex, Spacer, Text } from "palette"
 import React from "react"
-import { ArtworkDetailsForm } from "./ArtworkDetailsForm"
 
 interface ArtworkDetailsProps {
   // TODO
@@ -9,30 +8,10 @@ interface ArtworkDetailsProps {
 
 export const ArtworkDetails: React.FC<ArtworkDetailsProps> = ({ handlePress }) => {
   return (
-    <Flex p={1} mt={1}>
-      <Text variant="sm" color="black60">
-        · All fields are required to submit an artwork.
-      </Text>
-      <Text variant="sm" color="black60">
-        · Unfortunately, we do not allow&nbsp;
-        <Text style={{ textDecorationLine: "underline" }}>artists to sell their own work</Text> on Artsy.
-      </Text>
-
-      <Spacer mt={2} />
-
-      <ArtworkDetailsForm handlePress={handlePress} />
-
-      <Input title="Rarity" placeholder="Select a Classification" />
-      <Input title="Height" placeholder="in" />
-      <Input title="Width" placeholder="in" />
-      <Input title="Depth" placeholder="in" />
-      <RadioButton text="in" />
-      <RadioButton text="cm" />
-
-      <Input title="Provenance" placeholder="Describe How You Acquired the Work" />
-      <Input title="Location" placeholder="Enter City Where Artwork is Located" />
-
+    <Flex backgroundColor="peachpuff" p={1} mt={1}>
+      <Text>Upload Photos content</Text>
       <Spacer mt={1} />
+      <CTAButton onPress={handlePress}>Save & Continue</CTAButton>
     </Flex>
   )
 }
