6529806656221f07d7f43f3fedf6c8e064baf49c
Angela Patrinou <36475005+patrinoua@users.noreply.github.com>
2022-01-14 12:45:38 +0100

    add simple tests

--- src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/UploadPhotos/UploadPhotos.tests.tsx
+++ src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/UploadPhotos/UploadPhotos.tests.tsx
@@ -1,1 +1,11 @@
-// this is a test file
+import { renderWithWrappersTL } from "lib/tests/renderWithWrappers"
+import React from "react"
+import "react-native"
+
+import { UploadPhotos } from "./UploadPhotos"
+
+it("renders without throwing an error", () => {
+  renderWithWrappersTL(<UploadPhotos handlePress={() => console.log("do nothing")} />)
+})
+
+// add more tests
