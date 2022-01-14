cb991e55987659477a97ec1b5507465f97cf6503
Angela Patrinou <36475005+patrinoua@users.noreply.github.com>
2022-01-14 12:48:32 +0100

    add more tests

--- src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ContactInformation/ContactInformation.tests.tsx
+++ src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ContactInformation/ContactInformation.tests.tsx
@@ -1,1 +1,11 @@
-// this is a test file
+import { renderWithWrappersTL } from "lib/tests/renderWithWrappers"
+import React from "react"
+import "react-native"
+
+import { ContactInformation } from "./ContactInformation"
+
+it("renders without throwing an error", () => {
+  renderWithWrappersTL(<ContactInformation handlePress={() => console.log("do nothing")} />)
+})
+
+// add more tests
