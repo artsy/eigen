892ff5edc59a03612b1584ba5a6cd3e887b8bb2b
Angela Patrinou <patrinoua@gmail.com>
2022-01-17 16:03:44 +0100

    contact information

--- src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ContactInformation/ContactInformation.tsx
+++ src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ContactInformation/ContactInformation.tsx
@@ -1,14 +1,20 @@
-import { Button, Flex, Spacer, Text } from "palette"
+import { CTAButton, Flex, Input, Spacer, Text } from "palette"
 import React from "react"
 
 export const ContactInformation = ({ handlePress }: { handlePress: () => void }) => {
   return (
-    <Flex backgroundColor="peachpuff" p={1} mt={1}>
-      <Text>ContactInformation content</Text>
+    <Flex p={1} mt={1}>
+      <Text variant="sm" color="black60">
+        We will only use these details to contact you regarding your submission.{" "}
+      </Text>
       <Spacer mt={1} />
-      <Button block haptic maxWidth={540} onPress={handlePress}>
-        Submit Artwork
-      </Button>
+      <Input title="Name" placeholder="in" />
+      <Spacer mt={2} />
+      <Input title="Email" placeholder="in" />
+      <Spacer mt={2} />
+      <Input title="Phone number" placeholder="in" />
+      <Spacer mt={2} />
+      <CTAButton onPress={handlePress}>Submit Artwork</CTAButton>
     </Flex>
   )
 }
