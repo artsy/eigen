403cad038b64c945787a9aec33f64911f2202884
Angela Patrinou <patrinoua@gmail.com>
2022-01-18 14:32:40 +0100

    use Formik

--- src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ContactInformation/ContactInformation.tsx
+++ src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ContactInformation/ContactInformation.tsx
@@ -1,20 +1,53 @@
+import { Formik } from "formik"
 import { CTAButton, Flex, Input, Spacer, Text } from "palette"
 import React from "react"
-
-export const ContactInformation = ({ handlePress }: { handlePress: () => void }) => {
+export interface FormikSchema {
+  name: string
+  email: string
+  phoneNumber: string
+}
+export const ContactInformation = ({}: // handlePress,
+{
+  handlePress: () => void
+}) => {
   return (
-    <Flex p={1} mt={1}>
-      <Text variant="sm" color="black60">
-        We will only use these details to contact you regarding your submission.{" "}
-      </Text>
-      <Spacer mt={1} />
-      <Input title="Name" placeholder="in" />
-      <Spacer mt={2} />
-      <Input title="Email" placeholder="in" />
-      <Spacer mt={2} />
-      <Input title="Phone number" placeholder="in" />
-      <Spacer mt={2} />
-      <CTAButton onPress={handlePress}>Submit Artwork</CTAButton>
-    </Flex>
+    <Formik
+      initialValues={{ name: "", email: "", phoneNumber: "" }}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ values, handleSubmit, setFieldValue, isValid }) => (
+        <Flex p={1} mt={1}>
+          <Text variant="sm" color="black60">
+            We will only use these details to contact you regarding your
+            submission.
+          </Text>
+          <Spacer mt={1} />
+          <Input
+            title="Name"
+            placeholder="Name"
+            onChange={(e) => setFieldValue("name", e)}
+            value={values.name}
+          />
+          <Spacer mt={2} />
+          <Input
+            title="Email"
+            placeholder="in"
+            onChange={(e) => setFieldValue("email", e)}
+            value={values.email}
+          />
+          <Spacer mt={2} />
+          <Input
+            title="Phone number"
+            placeholder="in"
+            onChange={(e) => setFieldValue("phoneNumber", e)}
+            value={values.phoneNumber}
+          />
+          <Spacer mt={2} />
+          <CTAButton onPress={handleSubmit} disabled={!isValid}>
+            Submit Artwork
+          </CTAButton>
+        </Flex>
+      )}
+    </Formik>
   )
 }
