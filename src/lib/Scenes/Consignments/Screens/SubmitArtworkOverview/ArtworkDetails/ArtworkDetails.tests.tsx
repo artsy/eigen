30b5ead777dabef496e65d7c9035d3973d895efc
Alican Akyuz <alicakyz@gmail.com>
2022-01-18 13:13:13 +0100

    test create/update submission logic

--- src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ArtworkDetails/ArtworkDetails.tests.tsx
+++ src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ArtworkDetails/ArtworkDetails.tests.tsx
@@ -1,4 +1,9 @@
 import { fireEvent, waitFor } from "@testing-library/react-native"
+import {
+  ConsignmentAttributionClass,
+  CreateSubmissionMutationInput,
+} from "__generated__/createConsignmentSubmissionMutation.graphql"
+import { UpdateSubmissionMutationInput } from "__generated__/updateConsignSubmissionMutation.graphql"
 import { defaultEnvironment } from "lib/relay/createEnvironment"
 import { __globalStoreTestUtils__ } from "lib/store/GlobalStore"
 import { renderWithWrappersTL } from "lib/tests/renderWithWrappers"
@@ -6,17 +11,15 @@ import React from "react"
 import { act } from "react-test-renderer"
 import { createMockEnvironment } from "relay-test-utils"
 import { createConsignSubmission, updateConsignSubmission } from "../Mutations"
+import { createOrUpdateConsignSubmission } from "../utils/createOrUpdateConsignSubmission"
 import { ArtworkDetails } from "./ArtworkDetails"
-// import { flushPromiseQueue } from "lib/tests/flushPromiseQueue"
-// import { createConsignmentSubmissionMutationResponse } from "__generated__/createConsignmentSubmissionMutation.graphql"
-// import { updateConsignSubmissionMutationResponse } from "__generated__/updateConsignSubmissionMutation.graphql"
 
 jest.mock("lib/Scenes/Consignments/Screens/SubmitArtworkOverview/Mutations/createConsignSubmissionMutation", () => ({
-  createConsignSubmission: jest.fn(),
+  createConsignSubmission: jest.fn().mockResolvedValue("12345"),
 }))
 
 jest.mock("lib/Scenes/Consignments/Screens/SubmitArtworkOverview/Mutations/updateConsignSubmissionMutation", () => ({
-  updateConsignSubmission: jest.fn(),
+  updateConsignSubmission: jest.fn().mockResolvedValue("54321"),
 }))
 
 jest.mock("lib/relay/createEnvironment", () => {
@@ -27,8 +30,8 @@ jest.mock("lib/relay/createEnvironment", () => {
 
 jest.unmock("react-relay")
 
-const createConsignSubmissionMock = createConsignSubmission as jest.Mock<any>
-const updateConsignSubmissionMock = updateConsignSubmission as jest.Mock<any>
+const createConsignSubmissionMock = createConsignSubmission as jest.Mock
+const updateConsignSubmissionMock = updateConsignSubmission as jest.Mock
 const mockEnvironment = defaultEnvironment as ReturnType<typeof createMockEnvironment>
 
 describe("ArtworkDetailsForm", () => {
@@ -39,97 +42,130 @@ describe("ArtworkDetailsForm", () => {
   })
 
   describe("ArtworkDetails", () => {
-    it("renders explanation for form fields", () => {
+    it("renders correct explanation for form fields", () => {
       const { getByText } = renderWithWrappersTL(<ArtworkDetails handlePress={jest.fn()} />)
       expect(getByText("• All fields are required to submit an artwork.")).toBeTruthy()
     })
 
-    it("keeps Save & Continue button disabled until required fields validated", async () => {
-      const { getByTestId, UNSAFE_getByProps } = renderWithWrappersTL(<ArtworkDetails handlePress={jest.fn()} />)
+    describe("createOrUpdateConsignSubmission", () => {
+      it("creates new submission", async () => {
+        const submissionResult = await createOrUpdateConsignSubmission(mockSubmissionForm)
 
-      const SaveButton = UNSAFE_getByProps({
-        testID: "Consignment_ArtworkDetails_Button",
+        expect(createConsignSubmissionMock).toHaveBeenCalled()
+        expect(createConsignSubmissionMock).toHaveBeenCalledWith(mockSubmissionForm)
+        expect(submissionResult).toEqual("12345")
       })
 
-      const inputs = {
-        title: getByTestId("Consignment_TitleInput"),
-        year: getByTestId("Consignment_YearInput"),
-        material: getByTestId("Consignment_MaterialsInput"),
-        height: getByTestId("Consignment_HeightInput"),
-        width: getByTestId("Consignment_WidthInput"),
-        depth: getByTestId("Consignment_DepthInput"),
-        provenance: getByTestId("Consignment_ProvenanceInput"),
-      }
+      it("updates existing submission when ID passed", async () => {
+        const mockUpdateSubmissionForm: UpdateSubmissionMutationInput = {
+          id: "someId",
+          ...mockSubmissionForm,
+        }
 
-      // title missing
-      await waitFor(() => act(() => fireEvent.changeText(inputs.title, "")))
-      expect(SaveButton.props.disabled).toBe(true)
+        const submissionResult = await createOrUpdateConsignSubmission(mockUpdateSubmissionForm)
 
-      // year missing
-      await waitFor(() => {
-        act(() => {
-          fireEvent.changeText(inputs.year, "")
-          fireEvent.changeText(inputs.title, "someTitle")
-        })
+        expect(updateConsignSubmissionMock).toHaveBeenCalled()
+        expect(updateConsignSubmissionMock).toHaveBeenCalledWith(mockUpdateSubmissionForm)
+        expect(submissionResult).toEqual("54321")
       })
-      expect(SaveButton.props.disabled).toBe(true)
-
-      // material missing
-      await waitFor(() => {
-        act(() => {
-          fireEvent.changeText(inputs.material, "")
-          fireEvent.changeText(inputs.year, "1999")
-        })
-      })
-      expect(SaveButton.props.disabled).toBe(true)
-
-      // height missing
-      await waitFor(() => {
-        act(() => {
-          fireEvent.changeText(inputs.height, "")
-          fireEvent.changeText(inputs.material, "oil on c")
-        })
-      })
-      expect(SaveButton.props.disabled).toBe(true)
-
-      // width missing
-      await waitFor(() => {
-        act(() => {
-          fireEvent.changeText(inputs.width, "")
-          fireEvent.changeText(inputs.height, "123")
-        })
-      })
-      expect(SaveButton.props.disabled).toBe(true)
-
-      // depth missing
-      await waitFor(() => {
-        act(() => {
-          fireEvent.changeText(inputs.depth, "")
-          fireEvent.changeText(inputs.width, "123")
-        })
-      })
-      expect(SaveButton.props.disabled).toBe(true)
-
-      // provenance missing
-      await waitFor(() => {
-        act(() => {
-          fireEvent.changeText(inputs.provenance, "")
-          fireEvent.changeText(inputs.depth, "123")
-        })
-      })
-      expect(SaveButton.props.disabled).toBe(true)
     })
 
-    it("successfully creates a submission upon Save & Continue click", () => {
-      console.log("TODO...")
-    })
+    describe("validation", () => {
+      it("keeps Save & Continue button disabled until required fields validated", async () => {
+        const { getByTestId, UNSAFE_getByProps } = renderWithWrappersTL(<ArtworkDetails handlePress={jest.fn()} />)
 
-    it("saves submissionId to AsyncStore", () => {
-      console.log("TODO...")
-    })
+        const SaveButton = UNSAFE_getByProps({
+          testID: "Consignment_ArtworkDetails_Button",
+        })
 
-    it("when form is revisited, updates the submission instead of creating another one", () => {
-      console.log("TODO...")
+        const inputs = {
+          title: getByTestId("Consignment_TitleInput"),
+          year: getByTestId("Consignment_YearInput"),
+          material: getByTestId("Consignment_MaterialsInput"),
+          height: getByTestId("Consignment_HeightInput"),
+          width: getByTestId("Consignment_WidthInput"),
+          depth: getByTestId("Consignment_DepthInput"),
+          provenance: getByTestId("Consignment_ProvenanceInput"),
+        }
+
+        // title missing
+        await waitFor(() => act(() => fireEvent.changeText(inputs.title, "")))
+        expect(SaveButton.props.disabled).toBe(true)
+
+        // year missing
+        await waitFor(() => {
+          act(() => {
+            fireEvent.changeText(inputs.year, "")
+            fireEvent.changeText(inputs.title, "someTitle")
+          })
+        })
+        expect(SaveButton.props.disabled).toBe(true)
+
+        // material missing
+        await waitFor(() => {
+          act(() => {
+            fireEvent.changeText(inputs.material, "")
+            fireEvent.changeText(inputs.year, "1999")
+          })
+        })
+        expect(SaveButton.props.disabled).toBe(true)
+
+        // height missing
+        await waitFor(() => {
+          act(() => {
+            fireEvent.changeText(inputs.height, "")
+            fireEvent.changeText(inputs.material, "oil on c")
+          })
+        })
+        expect(SaveButton.props.disabled).toBe(true)
+
+        // width missing
+        await waitFor(() => {
+          act(() => {
+            fireEvent.changeText(inputs.width, "")
+            fireEvent.changeText(inputs.height, "123")
+          })
+        })
+        expect(SaveButton.props.disabled).toBe(true)
+
+        // depth missing
+        await waitFor(() => {
+          act(() => {
+            fireEvent.changeText(inputs.depth, "")
+            fireEvent.changeText(inputs.width, "123")
+          })
+        })
+        expect(SaveButton.props.disabled).toBe(true)
+
+        // provenance missing
+        await waitFor(() => {
+          act(() => {
+            fireEvent.changeText(inputs.provenance, "")
+            fireEvent.changeText(inputs.depth, "123")
+          })
+        })
+        expect(SaveButton.props.disabled).toBe(true)
+      })
     })
   })
 })
+
+const mockSubmissionForm: CreateSubmissionMutationInput = {
+  artistID: "123",
+  year: "2000",
+  title: "artworkTitle",
+  medium: "oil on canvas",
+  attributionClass: "attributionClass" as ConsignmentAttributionClass,
+  editionNumber: "",
+  editionSizeFormatted: "",
+  height: "100",
+  width: "200",
+  depth: "3",
+  dimensionsMetric: "in",
+  provenance: "acquired",
+  locationCity: "London, England. UK",
+  state: "DRAFT",
+  utmMedium: "",
+  utmSource: "",
+  utmTerm: "",
+}
