e1013437d86802e27a1fc4cb5115cb6f2196a01f
brainbicycle <brian.beckerle@artsymail.com>
2022-01-20 16:42:33 -0500

    only initialize google sign in on android

--- src/lib/AndroidApp.tsx
+++ src/lib/AndroidApp.tsx
@@ -6,7 +6,7 @@ import { SEGMENT_TRACKING_PROVIDER, SegmentTrackingProvider } from "lib/utils/tr
 import { useDeepLinks } from "lib/utils/useDeepLinks"
 import { useStripeConfig } from "lib/utils/useStripeConfig"
 import React, { useEffect } from "react"
-import { UIManager, View } from "react-native"
+import { Platform, UIManager, View } from "react-native"
 import RNBootSplash from "react-native-bootsplash"
 import { AppProviders } from "./AppProviders"
 import { useWebViewCookies } from "./Components/ArtsyReactWebView"
@@ -36,9 +36,11 @@ if (UIManager.setLayoutAnimationEnabledExperimental) {
 const Main: React.FC<{}> = track()(({}) => {
   useDebugging()
   useEffect(() => {
-    GoogleSignin.configure({
-      webClientId: "673710093763-hbj813nj4h3h183c4ildmu8vvqc0ek4h.apps.googleusercontent.com",
-    })
+    if (Platform.OS === "android") {
+      GoogleSignin.configure({
+        webClientId: "673710093763-hbj813nj4h3h183c4ildmu8vvqc0ek4h.apps.googleusercontent.com",
+      })
+    }
   }, [])
   const isHydrated = GlobalStore.useAppState((state) => state.sessionState.isHydrated)
   const isLoggedIn = GlobalStore.useAppState((state) => !!state.auth.userAccessToken)
