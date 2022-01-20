7c208d71fee2ed8d06c90e0969885668be919616
Pavlos Vinieratos <pvinis@gmail.com>
2022-01-20 18:27:11 +0200

    SWEEEEEEET

--- src/storybook/storyLoader.js
+++ src/storybook/storyLoader.js
@@ -12,15 +12,15 @@ function loadStories() {
   require("../palette/elements/Banner/Banner.stories")
   require("../palette/elements/Button/Button.stories")
   require("../palette/elements/Checkbox/Checkbox.stories")
+  require("../palette/elements/CollapsibleMenuItem/CollapsibleMenuItem.stories")
   require("../palette/elements/Dialog/Dialog.stories")
+  require("../palette/elements/Header/Header.stories")
   require("../palette/elements/Input/Input.stories")
   require("../palette/elements/Pill/Pill.stories")
   require("../palette/elements/Radio/RadioButton.stories")
   require("../palette/elements/Select/Select.stories")
   require("../palette/elements/Tabs/Tabs.stories")
-  require("../palette/elements/Header/Header.stories")
   require("../palette/elements/Text/Text.stories")
-  require("../palette/elements/CollapsibleMenuItem/CollapsibleMenuItem.stories")
   require("../palette/icons.stories")
   require("../palette/space.stories")
 }
@@ -34,15 +34,15 @@ const stories = [
   "../palette/elements/Banner/Banner.stories",
   "../palette/elements/Button/Button.stories",
   "../palette/elements/Checkbox/Checkbox.stories",
+  "../palette/elements/CollapsibleMenuItem/CollapsibleMenuItem.stories",
   "../palette/elements/Dialog/Dialog.stories",
+  "../palette/elements/Header/Header.stories",
   "../palette/elements/Input/Input.stories",
   "../palette/elements/Pill/Pill.stories",
   "../palette/elements/Radio/RadioButton.stories",
   "../palette/elements/Select/Select.stories",
   "../palette/elements/Tabs/Tabs.stories",
-  "../palette/elements/Header/Header.stories",
   "../palette/elements/Text/Text.stories",
-  "../palette/elements/CollapsibleMenuItem/CollapsibleMenuItem.stories",
   "../palette/icons.stories",
   "../palette/space.stories",
 ]
