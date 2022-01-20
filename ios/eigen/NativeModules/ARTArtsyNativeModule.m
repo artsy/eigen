022388e61d9850f3996209066aef6397be18c623
brainbicycle <brian.beckerle@artsymail.com>
2022-01-20 16:42:08 -0500

    placeholder constants from native module

--- ios/eigen/NativeModules/ARTArtsyNativeModule.m
+++ ios/eigen/NativeModules/ARTArtsyNativeModule.m
@@ -5,5 +5,16 @@
 RCT_EXPORT_MODULE(ArtsyNativeModule);
 
 
+// TODO: Return proper AppStatus/isBetaOrDev
+// TODO: Return proper GITCommitShortHash
+// See ARTArtsyNativeModule in old project for how
+
+- (NSDictionary *)constantsToExport
+{
+    return @{
+        @"gitCommitShortHash": @"FAKEHASH",
+        @"isBetaOrDev": @(YES),
+    };
+}
 
 @end
