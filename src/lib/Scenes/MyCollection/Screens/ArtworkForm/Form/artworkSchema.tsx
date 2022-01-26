084efd9e8a1a8c90aff3df96829df22a30d62c42
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-01-26 12:32:27 +0100

    prevent adding empty title for artworks in my collection
    Co-authored-by: Ole <ole.richter91@gmail.com>

--- src/lib/Scenes/MyCollection/Screens/ArtworkForm/Form/artworkSchema.tsx
+++ src/lib/Scenes/MyCollection/Screens/ArtworkForm/Form/artworkSchema.tsx
@@ -1,12 +1,15 @@
 import { yupToFormErrors } from "formik"
 import { ArtworkFormValues } from "lib/Scenes/MyCollection/State/MyCollectionArtworkModel"
+import { trim } from "lodash"
 import * as Yup from "yup"
 
 export const artworkSchema = Yup.object().shape({
   artistSearchResult: Yup.object()
     .nullable()
     .test("artistSearchResult", "Artist search result required", (value) => value !== null),
-  title: Yup.string().required("Title is required"),
+  title: Yup.string()
+    .required("Title is required")
+    .test("title", "Title should not be empty", (value) => trim(value || "") !== ""),
 })
 
 export function validateArtworkSchema(values: ArtworkFormValues) {
