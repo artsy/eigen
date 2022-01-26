c3583a6c47b14eee01565024f6bc90b21624f432
brainbicycle <brian.beckerle@artsymail.com>
2022-01-26 14:31:51 -0500

    don't use plist buddy which may not be installed

--- scripts/update-sentry-release-name.js
+++ scripts/update-sentry-release-name.js
@@ -1,13 +1,13 @@
 // @ts-check
 
-const path = require("path")
 const fs = require("fs")
-const { execSync } = require("child_process")
+const plist = require("simple-plist");
 
 const appJSONPath = "./app.json"
 const appJSON = JSON.parse(fs.readFileSync(appJSONPath).toString())
 const appVersion = appJSON['version']
-const bundleVersion = execSync("/usr/libexec/PlistBuddy -c 'print CFBundleVersion' ./Artsy/App_Resources/Artsy-Info.plist").toString().trim()
+const data = plist.readFileSync("./Artsy/App_Resources/Artsy-Info.plist")
+const bundleVersion = data['CFBundleVersion'].toString().trim()
 const releaseName = "ios-" + appVersion + "+" + bundleVersion
 appJSON['sentryReleaseName'] = releaseName
 fs.writeFileSync(appJSONPath, JSON.stringify(appJSON, null, 2))
