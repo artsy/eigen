77868b0631021686090c3a4c287a57de15d514bf
Alican Akyuz <alicakyz@gmail.com>
2022-01-27 10:04:22 +0100

    test: location

--- src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ArtworkDetails/Components/LocationAutocomplete.tests.tsx
+++ src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ArtworkDetails/Components/LocationAutocomplete.tests.tsx
@@ -6,7 +6,7 @@ import React from "react"
 import { RelayEnvironmentProvider } from "react-relay"
 import { act } from "react-test-renderer"
 import { createMockEnvironment } from "relay-test-utils"
-import { ArtworkDetails } from "../ArtworkDetails"
+import { LocationAutocomplete } from "./LocationAutocomplete"
 
 jest.mock("lib/relay/createEnvironment", () => {
   return {
@@ -17,33 +17,41 @@ jest.mock("lib/relay/createEnvironment", () => {
 jest.unmock("react-relay")
 const mockEnvironment = defaultEnvironment as ReturnType<typeof createMockEnvironment>
 
+const mockOnChange = jest.fn()
+const emptyInitialLocation = {
+  city: "",
+  state: "",
+  country: "",
+}
+
 describe("ArtworkDetailsForm", () => {
   const TestRenderer = () => (
     <RelayEnvironmentProvider environment={mockEnvironment}>
-      <ArtworkDetails handlePress={jest.fn()} />
+      <LocationAutocomplete initialLocation={emptyInitialLocation} onChange={mockOnChange} />
     </RelayEnvironmentProvider>
   )
 
   beforeEach(() => mockEnvironment.mockClear())
 
-  describe("ArtistAutoSuggest", () => {
-    it("renders input correctly", () => {
-      const { getByTestId } = renderWithWrappersTL(<TestRenderer />)
-      expect(getByTestId("Submission_ArtistInput")).toBeTruthy()
+  describe("LocationAutocomplete", () => {
+    it("renders input correctly with correct placeholder", () => {
+      const { getByTestId, getByPlaceholderText } = renderWithWrappersTL(<TestRenderer />)
+      expect(getByTestId("Submission_LocationInput")).toBeTruthy()
+      expect(getByPlaceholderText("Enter City Where Artwork Is Located")).toBeTruthy()
     })
 
     it("has input's value empty initially", () => {
       const { getByTestId } = renderWithWrappersTL(<TestRenderer />)
-      const artistInput = getByTestId("Submission_ArtistInput")
-      expect(artistInput.props.value).toBe("")
+      const locationInput = getByTestId("Submission_LocationInput")
+      expect(locationInput.props.value).toBe("")
     })
 
     it("mutates the typed value", () => {
       const { getByTestId } = renderWithWrappersTL(<TestRenderer />)
-      const artistInput = getByTestId("Submission_ArtistInput")
+      const locationInput = getByTestId("Submission_LocationInput")
 
-      act(() => fireEvent.changeText(artistInput, "max"))
-      expect(artistInput.props.value).toBe("max")
+      act(() => fireEvent.changeText(locationInput, "berlin"))
+      expect(locationInput.props.value).toBe("berlin")
     })
   })
 })
