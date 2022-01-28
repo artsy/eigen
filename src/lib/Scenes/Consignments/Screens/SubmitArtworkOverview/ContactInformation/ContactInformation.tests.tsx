711962133e8807096722d663c543d574f519ead1
Angela Patrinou <patrinoua@gmail.com>
2022-01-28 18:36:39 +0100

    boiler plate for testing

--- src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ContactInformation/ContactInformation.tests.tsx
+++ src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ContactInformation/ContactInformation.tests.tsx
@@ -1,11 +1,46 @@
+import { defaultEnvironment } from "lib/relay/createEnvironment"
 import { renderWithWrappersTL } from "lib/tests/renderWithWrappers"
 import React from "react"
 import "react-native"
+import { RelayEnvironmentProvider } from "react-relay"
+import { createMockEnvironment } from "relay-test-utils/"
+import { createConsignSubmission, updateConsignSubmission } from "../Mutations"
 
 import { ContactInformation } from "./ContactInformation"
 
-it("renders without throwing an error", () => {
-  renderWithWrappersTL(<ContactInformation handlePress={() => console.log("do nothing")} />)
+jest.mock(
+  "lib/Scenes/Consignments/Screens/SubmitArtworkOverview/Mutations/updateConsignSubmissionMutation",
+  () => ({
+    updateConsignSubmission: jest.fn().mockResolvedValue("54321"),
+  })
+)
+
+jest.mock("lib/relay/createEnvironment", () => {
+  return {
+    defaultEnvironment: require("relay-test-utils").createMockEnvironment(),
+  }
 })
 
-// add more tests
+jest.unmock("react-relay")
+
+const createConsignSubmissionMock = createConsignSubmission as jest.Mock
+const updateConsignSubmissionMock = updateConsignSubmission as jest.Mock
+const mockEnvironment = defaultEnvironment as ReturnType<typeof createMockEnvironment>
+
+describe("ArtworkDetailsForm", () => {
+  const TestRenderer = () => (
+    <RelayEnvironmentProvider environment={mockEnvironment}>
+      <ContactInformation handlePress={jest.fn()} />
+    </RelayEnvironmentProvider>
+  )
+
+  it("renders without throwing an error", () => {
+    renderWithWrappersTL(<ContactInformation handlePress={() => console.log("do nothing")} />)
+  })
+  it("sends information correctly", () => {
+    renderWithWrappersTL(<ContactInformation handlePress={() => console.log("do nothing")} />)
+  })
+  it(" navigate to the next page correctly", () => {
+    renderWithWrappersTL(<ContactInformation handlePress={() => console.log("do nothing")} />)
+  })
+})
