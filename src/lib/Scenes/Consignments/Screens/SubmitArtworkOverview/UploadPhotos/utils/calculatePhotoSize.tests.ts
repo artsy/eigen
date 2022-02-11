befcb4d032cf71f597d9ef35c0ffde32a8402857
Alican Akyuz <alicakyz@gmail.com>
2022-02-11 15:02:34 +0100

    test: isSizeLimitExceeded

--- src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/UploadPhotos/utils/calculatePhotoSize.tests.ts
+++ src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/UploadPhotos/utils/calculatePhotoSize.tests.ts
@@ -1,6 +1,6 @@
-import { calculateSinglePhotoSize } from "./calculatePhotoSize"
+import { calculateSinglePhotoSize, isSizeLimitExceeded } from "./calculatePhotoSize"
 
-describe("calculatePhotoSize", () => {
+describe("calculateSinglePhotoSize", () => {
   it("returns photo with size correctly calculated with units", () => {
     const photo = calculateSinglePhotoSize({
       size: 3425899,
@@ -13,3 +13,32 @@ describe("calculatePhotoSize", () => {
     expect(photo.error).toBeTrue()
   })
 })
+
+describe("isSizeLimitExceeded", () => {
+  it("calculates all photos size and returns false when limit not exceeded", () => {
+    const isExceeded = isSizeLimitExceeded(mockPhotos)
+    expect(isExceeded).toBe(false)
+  })
+  it("calculates all photos size and returns true when limit not exceeded", () => {
+    const isExceeded = isSizeLimitExceeded(mockPhotosWithSizeExcess)
+    expect(isExceeded).toBe(true)
+  })
+})
+
+const mockPhotos = [
+  {
+    size: 3425899,
+  },
+  {
+    size: 3425899,
+  },
+]
+
+const mockPhotosWithSizeExcess = [
+  {
+    size: 93425899,
+  },
+  {
+    size: 83425899,
+  },
+]
