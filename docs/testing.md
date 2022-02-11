309042ffbc4d1f12a942935fb1a8c987773890a1
Angela Patrinou <patrinoua@gmail.com>
2022-02-11 15:11:34 +0100

    test good case scenario

---
+++ docs/testing.md
@@ -0,0 +1,49 @@
+# Testing with Relay
+
+We have 3 ways of testing relay components:
+
+1. Components using RelayQueryRenderer
+
+1. Components using relay hooks
+
+1. Components using both RelayQueryRenderer & relay hooks !
+
+## Case 1.
+
+renderWithWrappersTL : renderWithWrappers(TestingLibrary)
+All our wrappeprs
+
+setupTestWrapper : abstract some of the boiler of Relay
+Component,
+Query
+Variables
+
+_example file: ItemArtwork.tests.tsx_
+
+Creates query Renderer
+
+Give component
+give test query
+
+pass input variables
+
+has an act that resolves the first request that's being made.
+
+! When we have consecutive requests setupTestWrapperTL doesnot work
+because it creates the environment inside.
+While we need to create our own environment in this case.
+
+So in this case we'd use renderWithWrappersTL and create our own environments.
+
+Example of using multiple environments: ContactInformation.tests.tsx
+
+setupJest:
+we have initialisations of mocks such as
+
+```
+ jest.mock("lib/relay/createEnvironment", () => {
+   return {
+     defaultEnvironment: require("relay-test-utils").createMockEnvironment(),
+   }
+ })
+```
--- src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ContactInformation/ContactInformation.tests.tsx
+++ src/lib/Scenes/Consignments/Screens/SubmitArtworkOverview/ContactInformation/ContactInformation.tests.tsx
@@ -1,12 +1,13 @@
+import { fireEvent } from "@testing-library/react-native"
 import { defaultEnvironment } from "lib/relay/createEnvironment"
+import { flushPromiseQueue } from "lib/tests/flushPromiseQueue"
 import { renderWithWrappersTL } from "lib/tests/renderWithWrappers"
 import React from "react"
 import "react-native"
-import { RelayEnvironmentProvider } from "react-relay"
-import { createMockEnvironment } from "relay-test-utils/"
+import { act } from "react-test-renderer"
+import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils/"
 import { updateConsignSubmission } from "../Mutations"
 import { ContactInformationQueryRenderer } from "./ContactInformation"
-import { ContactInformationFormModel } from "./validation"
 
 jest.mock(
   "lib/Scenes/Consignments/Screens/SubmitArtworkOverview/Mutations/updateConsignSubmissionMutation",
@@ -15,28 +16,14 @@ jest.mock(
   })
 )
 
-jest.mock("lib/relay/createEnvironment", () => {
-  return {
-    defaultEnvironment: require("relay-test-utils").createMockEnvironment(),
-  }
-})
+const updateConsignSubmissionMock = updateConsignSubmission as jest.Mock
 
 jest.unmock("react-relay")
 
-const updateConsignSubmissionMock = updateConsignSubmission as jest.Mock
-const mockEnvironment = defaultEnvironment as ReturnType<typeof createMockEnvironment>
-
 describe("ContactInformationForm", () => {
-  const TestRenderer = () => (
-    <RelayEnvironmentProvider environment={mockEnvironment}>
-      <ContactInformationQueryRenderer handlePress={jest.fn()} />
-    </RelayEnvironmentProvider>
-  )
-
-  beforeEach(() => {
-    ;(updateConsignSubmissionMock as jest.Mock).mockClear()
-    mockEnvironment.mockClear()
-  })
+  const mockEnvironment = defaultEnvironment as ReturnType<typeof createMockEnvironment>
+  const handlePressTest = jest.fn()
+  const TestRenderer = () => <ContactInformationQueryRenderer handlePress={handlePressTest} />
 
   it("renders without throwing an error", () => {
     renderWithWrappersTL(
@@ -51,48 +38,76 @@ describe("ContactInformationForm", () => {
     ).toBeTruthy()
   })
 
-  // test phoneNumber:
-  // isValid  returns phone number
-  // !isValid returns empty string
+  // it.only("DEBUG", () => {
+  //   const { debug } = renderWithWrappersTL(<TestRenderer />)
+  //   debug()
+  // })
+  it.only("Happy path: User can submit information", async () => {
+    const { getAllByText, getByPlaceholderText } = renderWithWrappersTL(<TestRenderer />)
+    updateConsignSubmissionMock.mockResolvedValue("adsfasd")
+    mockEnvironment.mock.resolveMostRecentOperation((operation) =>
+      MockPayloadGenerator.generate(operation, {
+        Me: () => ({
+          ...mockQueryData,
+        }),
+      })
+    )
+
+    await flushPromiseQueue()
+
+    const nameInput = getByPlaceholderText("Your Full Name")
+    expect(nameInput).toBeTruthy()
+    expect(nameInput).toHaveProp("value", "Angela")
+
+    const emailInput = getByPlaceholderText("Your Email Address")
+    expect(emailInput).toBeTruthy()
+    expect(emailInput).toHaveProp("value", "a@a.aaa")
+
+    const phoneInput = getByPlaceholderText("(000) 000-0000")
+    expect(phoneInput).toBeTruthy()
+    expect(phoneInput).toHaveProp("value", "(202) 555-0174")
+
+    expect(getAllByText("Submit Artwork")).toBeTruthy()
+
+    act(() => {
+      fireEvent(getAllByText("Submit Artwork")[0], "press")
+    })
+
+    expect(updateConsignSubmissionMock).toHaveBeenCalled()
+    expect(updateConsignSubmissionMock).toHaveBeenCalledWith({ ...mockFormDataForSubmission })
+    await flushPromiseQueue()
+    expect(handlePressTest).toHaveBeenCalled()
+  })
+
+  // test edge cases - something went wrong
 
   // TODO: (related Jira ticket: https://artsyproduct.atlassian.net/browse/SWA-223 )
   //
-  // it("renders with an error when something is missing/not properly filled out", async () => null)
-  // it("renders empty form when user isn't logged in or does not have an account", () => {
-  //   const { getByText } = renderWithWrappersTL(<TestRenderer />)
-  // })
-
-  // it("renders User Information when that exists (user already has an account)", () => {
-  //   const { getByText } = renderWithWrappersTL(<TestRenderer />)
-  // })
+  // it("keeps Submit button deactivated when something is missing/not properly filled out", async () => null)
 
   // it("updates existing submission when ID passed", async () => {
   //   // check what is in submission // check all fields are there
   //   // if everything is ok => updateSubmission()
   //   // make sure "userName" exists in submission
-  //   // await updateSubmission(mockSubmissionForm, "12345")
+  //   // await updateSubmission(mockQueryData, "12345")
   //   // expect(updateConsignSubmissionMock).toHaveBeenCalled()
-  // })
-
-  // it("navigate to the next page correctly", () => {
-  //   renderWithWrappersTL(<ContactInformation handlePress={() => console.log("do nothing")} />)
   // })
 })
 
-export const mockSubmissionForm: ContactInformationFormModel = {
-  userName: "Angela",
-  userEmail: "a@a.aaa",
-  userPhone: "202-555-0174",
+// test phoneNumber:
+// isValid returns phone number
+// !isValid returns empty string
+
+export const mockQueryData: any = {
+  name: "Angela",
+  email: "a@a.aaa",
+  phoneNumber: { isValid: true, originalNumber: "(202) 555-0174" },
 }
 
-// TODO:
-// Pre-populate phone number from a user's `phoneNumber` field
-// https://artsyproduct.atlassian.net/browse/SWA-224
-// phoneNumber: {
-//   countryCode: "",
-//   display: "",
-//   error: "",
-//   isValid: true,
-//   originalNumber: "",
-//   regionCode: "",
-// },
+export const mockFormDataForSubmission: any = {
+  id: "",
+  state: "SUBMITTED",
+  userEmail: "a@a.aaa",
+  userName: "Angela",
+  userPhone: "+1 (202) 555-0174",
+}
