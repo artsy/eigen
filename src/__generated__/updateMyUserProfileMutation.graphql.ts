e19a34985f801e876a6615da56c8a915f3559fcf
Kizito Egeonu <kizito.egeonu@gmail.com>
2022-02-16 13:38:13 +0100

    yarn relay

--- src/__generated__/updateMyUserProfileMutation.graphql.ts
+++ src/__generated__/updateMyUserProfileMutation.graphql.ts
@@ -4,18 +4,22 @@
 /* @relayHash f8df47c27da02f669c002aa85e039487 */
 
 import { ConcreteRequest } from "relay-runtime";
+export type CurrencyPreference = "EUR" | "GBP" | "USD" | "%future added value";
+export type LengthUnitPreference = "CM" | "IN" | "%future added value";
 export type UpdateMyProfileInput = {
     artworksPerYear?: string | null | undefined;
     bio?: string | null | undefined;
     clientMutationId?: string | null | undefined;
     collectorLevel?: number | null | undefined;
     completedOnboarding?: boolean | null | undefined;
+    currencyPreference?: CurrencyPreference | null | undefined;
     email?: string | null | undefined;
     emailFrequency?: string | null | undefined;
     gender?: string | null | undefined;
     iconUrl?: string | null | undefined;
     industry?: string | null | undefined;
     isCollector?: boolean | null | undefined;
+    lengthUnitPreference?: LengthUnitPreference | null | undefined;
     location?: EditableLocation | null | undefined;
     name?: string | null | undefined;
     notes?: string | null | undefined;
