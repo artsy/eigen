09a854b0948fa98aeef5b5c67f77de11c351a882
Pavlos Vinieratos <pvinis@gmail.com>
2022-02-21 11:00:16 +0000

    remove log

--- src/lib/utils/useIdentifyUser.ts
+++ src/lib/utils/useIdentifyUser.ts
@@ -11,8 +11,6 @@ export function useIdentifyUser() {
   const userId = GlobalStore.useAppState((store) => store.auth.userID)
 
   useEffect(() => {
-    console.log("userId", userId)
-
     Sentry.setUser({ id: userId ?? "none" })
     SegmentTrackingProvider.identify?.(userId, { is_temporary_user: userId === null ? 1 : 0 })
   }, [userId])
