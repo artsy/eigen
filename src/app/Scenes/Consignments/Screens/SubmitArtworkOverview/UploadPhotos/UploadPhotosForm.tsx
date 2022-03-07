9019c14c11644d17969bca84b5acf5f805697ec8
Alican Akyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2022-03-07 14:45:18 +0100

    fix: preserve artwork details in the flow (#6380)
    * fix: preserve artwork details in the flow
    
    * fix: remove state refresh from handleConsignPress
    
    * feat: preserve photos for myCollection, remove them for submission
    
    * chore: rename action
    
    * feat: clean up photosForMyCollection when unmounted
    
    * test: update migration content

--- src/app/Scenes/Consignments/Screens/SubmitArtworkOverview/UploadPhotos/UploadPhotosForm.tsx
+++ src/app/Scenes/Consignments/Screens/SubmitArtworkOverview/UploadPhotos/UploadPhotosForm.tsx
@@ -59,11 +59,16 @@ export const UploadPhotosForm: React.FC<{ isAnyPhotoLoading?: boolean }> = ({
       }
     }
 
-    // let GlobalStore and Formik know about processed photos
-    GlobalStore.actions.artworkSubmission.submission.setPhotos({
-      photos: [...values.photos, ...processedPhotos],
+    const allPhotos = [...values.photos, ...processedPhotos]
+
+    // set photos for my collection, and submission flow state and Formik
+    GlobalStore.actions.artworkSubmission.submission.setPhotosForMyCollection({
+      photos: allPhotos,
     })
-    setFieldValue("photos", [...values.photos, ...processedPhotos])
+    GlobalStore.actions.artworkSubmission.submission.setPhotos({
+      photos: allPhotos,
+    })
+    setFieldValue("photos", allPhotos)
   }
 
   // show Native action sheet and get photos from user's phone
@@ -88,6 +93,10 @@ export const UploadPhotosForm: React.FC<{ isAnyPhotoLoading?: boolean }> = ({
         })
       }
 
+      // set photos for my collection, and submission flow state and Formik
+      GlobalStore.actions.artworkSubmission.submission.setPhotosForMyCollection({
+        photos: filteredPhotos,
+      })
       GlobalStore.actions.artworkSubmission.submission.setPhotos({
         photos: filteredPhotos,
       })
