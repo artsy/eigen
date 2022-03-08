f8f6928f8bfab00623e46d310cc0fc0b50f16825
brainbicycle <brian.beckerle@artsymail.com>
2022-03-08 16:00:06 -0500

    remove native webview code

--- Artsy/View_Controllers/Web_Browsing/ARInternalWebViewManager.h
+++
@@ -1,6 +0,0 @@
-#import <React/RCTViewManager.h>
-
-@interface ARInternalWebViewManager : RCTViewManager
-@property (nonatomic, strong, readwrite) NSString *route;
-@property (nonatomic, assign) BOOL showFullScreen;
-@end
