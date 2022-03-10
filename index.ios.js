9db1e59cad7722446f0dc52b82dcf6c572008248
Pavlos Vinieratos <pvinis@gmail.com>
2022-03-10 13:00:12 +0000

    enable hermes

--- index.ios.js
+++ index.ios.js
@@ -18,6 +18,19 @@ if (metaflags.startStorybook) {
   global.__STORYBOOK__ = true
   require("./src/storybook")
 } else {
+  if (global.HermesInternal) {
+    // Polyfills required to use Intl with Hermes engine
+    require("@formatjs/intl-getcanonicallocales/polyfill")
+    require("@formatjs/intl-locale/polyfill")
+    require("@formatjs/intl-pluralrules/polyfill")
+    require("@formatjs/intl-pluralrules/locale-data/en")
+    require("@formatjs/intl-numberformat/polyfill")
+    require("@formatjs/intl-numberformat/locale-data/en")
+    require("@formatjs/intl-datetimeformat/polyfill")
+    require("@formatjs/intl-datetimeformat/locale-data/en")
+    require("@formatjs/intl-datetimeformat/add-golden-tz")
+  }
+
   require("react-native-gesture-handler")
   require("react-native-screens").enableScreens()
   require("./src/app/AppRegistry")
