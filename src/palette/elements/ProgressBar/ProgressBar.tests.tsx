04edf614a7444c49e7e113af7e9895f9468523a0
Angela Patrinou <patrinoua@gmail.com>
2022-03-15 15:33:05 +0100

    add a test

--- src/palette/elements/ProgressBar/ProgressBar.tests.tsx
+++ src/palette/elements/ProgressBar/ProgressBar.tests.tsx
@@ -1,1 +1,11 @@
-// add a test
+import { renderWithWrappersTL } from "app/tests/renderWithWrappers"
+import React from "react"
+import { ProgressBar } from "./ProgressBar"
+
+describe("ProgressBar", () => {
+  it("renders a ProgressBar", () => {
+    const component = renderWithWrappersTL(<ProgressBar progress={0.3} />)
+
+    expect(component).not.toBeNull()
+  })
+})
