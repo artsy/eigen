c18d59c6f33b5e505527fbb671cfae9fc3300026
George Kartalis <g.kartalis7@gmail.com>
2022-03-22 16:57:05 +0100

    chore: remove scrollview from price filter (#6473)

--- src/app/Components/ArtworkFilter/Filters/PriceRangeOptions.tsx
+++ src/app/Components/ArtworkFilter/Filters/PriceRangeOptions.tsx
@@ -12,7 +12,7 @@ import {
 import { debounce } from "lodash"
 import { Flex, Input, Spacer, Text, useColor } from "palette"
 import React, { useMemo, useState } from "react"
-import { ScrollView, useWindowDimensions } from "react-native"
+import { useWindowDimensions } from "react-native"
 import { ArtworkFilterBackHeader } from "../components/ArtworkFilterBackHeader"
 import { Numeric, parsePriceRangeLabel } from "./helpers"
 
@@ -137,79 +137,77 @@ export const PriceRangeOptionsScreen: React.FC<PriceRangeOptionsScreenProps> = (
         {...(isActive ? { rightButtonText: "Clear", onRightButtonPress: handleClear } : {})}
       />
       <Flex flexGrow={1}>
-        <ScrollView style={{ flex: 1 }}>
-          <Flex m={2}>
-            <Text variant="md">Choose Your Price Range</Text>
-          </Flex>
-          <Flex flexDirection="row" alignItems="center" mb={1} mx={2}>
-            <Input
-              description="Min"
-              placeholder="$USD"
-              enableClearButton
-              keyboardType="number-pad"
-              value={getInputValue(minValue)}
-              onChangeText={handleTextChange(0)}
-              testID="price-min-input"
-              descriptionColor="black100"
+        <Flex m={2}>
+          <Text variant="md">Choose Your Price Range</Text>
+        </Flex>
+        <Flex flexDirection="row" alignItems="center" mb={1} mx={2}>
+          <Input
+            description="Min"
+            placeholder="$USD"
+            enableClearButton
+            keyboardType="number-pad"
+            value={getInputValue(minValue)}
+            onChangeText={handleTextChange(0)}
+            testID="price-min-input"
+            descriptionColor="black100"
+          />
+          <Spacer mx={2} />
+          <Input
+            description="Max"
+            placeholder="$USD"
+            enableClearButton
+            keyboardType="number-pad"
+            value={getInputValue(maxValue)}
+            onChangeText={handleTextChange(1)}
+            testID="price-max-input"
+            descriptionColor="black100"
+          />
+        </Flex>
+        <Spacer mx={2} my={2} />
+        <Flex mx={2}>
+          <Flex alignItems="center" testID="slider">
+            <MultiSlider
+              min={defaultMinValue}
+              max={defaultMaxValue}
+              step={5}
+              snapped
+              // 40 here is the horizontal padding of the slider container
+              sliderLength={width - 40}
+              onValuesChange={handleSliderValueChange}
+              allowOverlap={false}
+              values={sliderRange}
+              trackStyle={{
+                backgroundColor: color("black30"),
+              }}
+              selectedStyle={{
+                backgroundColor: color("blue100"),
+              }}
+              markerStyle={{
+                height: 32,
+                width: 32,
+                borderRadius: 16,
+                backgroundColor: color("white100"),
+                borderColor: color("black10"),
+                borderWidth: 1,
+                shadowRadius: 2,
+                elevation: 5,
+              }}
+              pressedMarkerStyle={{
+                height: 32,
+                width: 32,
+                borderRadius: 16,
+              }}
             />
-            <Spacer mx={2} />
-            <Input
-              description="Max"
-              placeholder="$USD"
-              enableClearButton
-              keyboardType="number-pad"
-              value={getInputValue(maxValue)}
-              onChangeText={handleTextChange(1)}
-              testID="price-max-input"
-              descriptionColor="black100"
-            />
           </Flex>
-          <Spacer mx={2} my={2} />
-          <Flex mx={2}>
-            <Flex alignItems="center" testID="slider">
-              <MultiSlider
-                min={defaultMinValue}
-                max={defaultMaxValue}
-                step={5}
-                snapped
-                // 40 here is the horizontal padding of the slider container
-                sliderLength={width - 40}
-                onValuesChange={handleSliderValueChange}
-                allowOverlap={false}
-                values={sliderRange}
-                trackStyle={{
-                  backgroundColor: color("black30"),
-                }}
-                selectedStyle={{
-                  backgroundColor: color("blue100"),
-                }}
-                markerStyle={{
-                  height: 32,
-                  width: 32,
-                  borderRadius: 16,
-                  backgroundColor: color("white100"),
-                  borderColor: color("black10"),
-                  borderWidth: 1,
-                  shadowRadius: 2,
-                  elevation: 5,
-                }}
-                pressedMarkerStyle={{
-                  height: 32,
-                  width: 32,
-                  borderRadius: 16,
-                }}
-              />
-            </Flex>
-            <Flex flexDirection="row" justifyContent="space-between">
-              <Text variant="xs" color="black60">
-                ${defaultMinValue}
-              </Text>
-              <Text variant="xs" color="black60">
-                ${defaultMaxValue}+
-              </Text>
-            </Flex>
+          <Flex flexDirection="row" justifyContent="space-between">
+            <Text variant="xs" color="black60">
+              ${defaultMinValue}
+            </Text>
+            <Text variant="xs" color="black60">
+              ${defaultMaxValue}+
+            </Text>
           </Flex>
-        </ScrollView>
+        </Flex>
       </Flex>
     </Flex>
   )
