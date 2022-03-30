04dde23bb65d44126e65e953d992a8771aa5efbb
George Kartalis <g.kartalis7@gmail.com>
2022-03-30 18:50:50 +0200

    feat: add crosshair icon and initial location

--- src/app/Scenes/Map/NewMap.tsx
+++ src/app/Scenes/Map/NewMap.tsx
@@ -1,7 +1,11 @@
 import MapboxGL from "@react-native-mapbox-gl/maps"
 import { themeGet } from "@styled-system/theme-get"
+import { Box } from "palette"
+import React, { useRef, useState } from "react"
 import { Dimensions, StyleSheet, View } from "react-native"
 import Config from "react-native-config"
+import styled from "styled-components"
+import { UserPositionButton } from "./Components/UserPositionButton"
 import { ArtsyMapStyleURL } from "./GlobalMap"
 
 MapboxGL.setAccessToken(Config.MAPBOX_API_CLIENT_KEY)
@@ -22,11 +26,66 @@ const styles = StyleSheet.create({
   },
 })
 
+const TopButtonsContainer = styled(Box)`
+  position: absolute;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  width: 100%;
+  height: 100;
+`
+
+const DEFAULT_ZOOM_LEVEL = 11
+const MINIMUM__METERS_BEFORE_UPDATING_POSITION = 300
+const MIN_ZOOM_LVL = 9
+const MAX_ZOOM_LVL = 50
+
+// this is the center of Berlin for now until we decide otherwise
+const INITIAL_LOCATION = [13.413141, 52.523509]
+
 export const NewMapScreen = () => {
+  const cameraRef = useRef<MapboxGL.Camera>(null)
+  const [userLocation, setUserLocation] = useState<GeoJSON.Position>()
+  const [showUserLocation, setShowUserLocation] = useState(false)
+
+  // fly user to their location in the map
+  const onPressUserPositionButton = () => {
+    setShowUserLocation(true)
+    cameraRef.current?.setCamera({
+      centerCoordinate: userLocation,
+      zoomLevel: DEFAULT_ZOOM_LEVEL,
+      animationDuration: 500,
+    })
+  }
+
+  const onUpdateUserLocation = (location: MapboxGL.Location) => {
+    setUserLocation([location.coords.longitude, location.coords.latitude])
+  }
+
   return (
     <View style={styles.page}>
       <View style={styles.container}>
-        <MapboxGL.MapView styleURL={ArtsyMapStyleURL} style={styles.map} compassEnabled={false} />
+        <MapboxGL.MapView styleURL={ArtsyMapStyleURL} style={styles.map} compassEnabled={false}>
+          <TopButtonsContainer style={{ bottom: 10, left: -25 }}>
+            <UserPositionButton onPress={onPressUserPositionButton} />
+          </TopButtonsContainer>
+          <MapboxGL.UserLocation
+            // dynamically pass this onUpdate when user presses the CrossHair Icon
+            // to avoid starting the location manager before that
+            {...(!!showUserLocation && { onUpdate: onUpdateUserLocation })}
+            visible={showUserLocation}
+            androidRenderMode="normal"
+            minDisplacement={MINIMUM__METERS_BEFORE_UPDATING_POSITION}
+          />
+          <MapboxGL.Camera
+            ref={cameraRef}
+            animationMode="flyTo"
+            centerCoordinate={INITIAL_LOCATION}
+            zoomLevel={DEFAULT_ZOOM_LEVEL}
+            minZoomLevel={MIN_ZOOM_LVL}
+            maxZoomLevel={MAX_ZOOM_LVL}
+          />
+        </MapboxGL.MapView>
       </View>
     </View>
   )
