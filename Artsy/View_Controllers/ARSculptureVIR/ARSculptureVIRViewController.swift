0ba7137eb81e9e57535532d744766fe4dba2f3f2
brainbicycle <brian.beckerle@artsymail.com>
2022-04-01 08:49:45 -0400

    change deployment target to 13, add RealityKit

---
+++ Artsy/View_Controllers/ARSculptureVIR/ARSculptureVIRViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ARSculptureVIRViewController.swift
+//  Artsy
+//
+//  Created by Brian Beckerle on 4/1/22.
+//  Copyright © 2022 Artsy. All rights reserved.
+//
+
+import UIKit
+
+class ARSculptureVIRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
