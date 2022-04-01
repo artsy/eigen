36ad011ae0829bdc0f2395ae7fc067f8644fe697
brainbicycle <brian.beckerle@artsymail.com>
2022-04-01 10:13:03 -0400

    add example model and display on tap

--- Artsy/View_Controllers/ARSculptureVIR/ARSculptureVIRViewController.swift
+++ Artsy/View_Controllers/ARSculptureVIR/ARSculptureVIRViewController.swift
@@ -6,11 +6,9 @@ import FocusEntity
 class ARSculptureVIRViewController: UIViewController, ARSessionDelegate {
 
     weak var arView : ARView?
-    var focusEntity: FocusEntity?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    var focusEntity : FocusEntity?
+    var displayedModel : ModelEntity?
+    var previousAnchor : AnchorEntity?
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -30,27 +28,30 @@ class ARSculptureVIRViewController: UIViewController, ARSessionDelegate {
     }
 
     func startARSession(arView: ARView) {
-      // Start AR session
-      let session = arView.session
-      let config = ARWorldTrackingConfiguration()
-      config.planeDetection = [.horizontal]
-      session.run(config)
+        // Start AR session
+        let session = arView.session
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal]
+        session.run(config)
 
-      // Handle ARSession events via delegate
-      session.delegate = self
+        // Handle ARSession events via delegate
+        session.delegate = self
 
-      // Add coaching overlay
-      let coachingOverlay = ARCoachingOverlayView()
-      coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-      coachingOverlay.session = session
-      coachingOverlay.goal = .horizontalPlane
-      coachingOverlay.frame = arView.bounds
-      arView.addSubview(coachingOverlay)
+        // Add coaching overlay
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        coachingOverlay.session = session
+        coachingOverlay.goal = .horizontalPlane
+        coachingOverlay.frame = arView.bounds
+        arView.addSubview(coachingOverlay)
 
-      // Set debug options
-      #if DEBUG
-      arView.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
-      #endif
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ARSculptureVIRViewController.handleTap))
+        arView.addGestureRecognizer(tapGestureRecognizer)
+
+        // Set debug options
+        #if DEBUG
+        arView.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
+        #endif
     }
 
 
@@ -62,4 +63,30 @@ class ARSculptureVIRViewController: UIViewController, ARSessionDelegate {
          self.focusEntity = FocusEntity(on: arView, style: .classic(color: .purple))
      }
 
+    // MARK: UserInteractions
+
+    @objc func handleTap() {
+        guard let arView = self.arView, let focusEntity = self.focusEntity else { return }
+
+        // remove previous model if it exists
+        if
+            let displayedModel = self.displayedModel,
+            let previousAnchor = self.previousAnchor
+        {
+            previousAnchor.removeChild(displayedModel)
+        }
+
+        // Create a new anchor to add content to
+        let anchor = AnchorEntity()
+        self.previousAnchor = anchor
+        arView.scene.anchors.append(anchor)
+
+        // Add a model entity
+        let modelEntity = try! ModelEntity.loadModel(named: "candle")
+        modelEntity.scale = [1.0, 1.0, 1.0]
+        modelEntity.position = focusEntity.position
+        self.displayedModel = modelEntity
+
+        anchor.addChild(modelEntity)
+    }
 }
