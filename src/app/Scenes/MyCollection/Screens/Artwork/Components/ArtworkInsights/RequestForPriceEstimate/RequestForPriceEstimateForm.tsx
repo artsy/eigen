836c3c9c4e20cb954e41f70e9c38a1774493e468
Mounir Dhahri <soussouheros@gmail.com>
2022-04-10 19:11:00 +0200

    fix: padding on request price estimate

--- src/app/Scenes/MyCollection/Screens/Artwork/Components/ArtworkInsights/RequestForPriceEstimate/RequestForPriceEstimateForm.tsx
+++ src/app/Scenes/MyCollection/Screens/Artwork/Components/ArtworkInsights/RequestForPriceEstimate/RequestForPriceEstimateForm.tsx
@@ -1,7 +1,7 @@
 import { ArtsyKeyboardAvoidingView } from "app/Components/ArtsyKeyboardAvoidingView"
 import { useScreenDimensions } from "app/utils/useScreenDimensions"
 import { useFormikContext } from "formik"
-import { Box, Button, Input, PhoneInput, Spacer, Text } from "palette"
+import { Box, Button, Input, Join, PhoneInput, Spacer, Text } from "palette"
 import React from "react"
 import { Platform, ScrollView } from "react-native"
 import { RequestForPriceEstimateFormikSchema } from "./RequestForPriceEstimateScreen"
@@ -16,70 +16,69 @@ export const RequestForPriceEstimateForm = () => {
       <ScrollView keyboardDismissMode="on-drag" keyboardShouldPersistTaps="handled">
         <Box pt={safeAreaInsets.top} pb={safeAreaInsets.bottom} px={2}>
           <Box my={3}>
-            <Text variant="xl">Contact Information</Text>
-            <Spacer p={1} />
-            <Text variant="md" color="black60">
-              We will only use this to contact you about this price estimate
-            </Text>
-            <Spacer p={1} />
-            <Input
-              testID="request-price-estimate-name-input"
-              title="Name"
-              autoCapitalize="words"
-              autoCorrect={false}
-              onChangeText={(text) => {
-                if (errors.requesterName) {
-                  setErrors({
-                    requesterName: undefined,
-                  })
-                }
-                handleChange("requesterName")(text)
-              }}
-              blurOnSubmit={false}
-              placeholder="First and Last Name"
-              returnKeyType="done"
-              maxLength={128}
-              value={values.requesterName}
-              error={errors.requesterName}
-            />
-            <Spacer p={1} />
-            <Input
-              testID="request-price-estimate-email-input"
-              title="Email"
-              autoCapitalize="none"
-              enableClearButton
-              keyboardType="email-address"
-              onChangeText={(text) => {
-                if (errors.requesterEmail) {
-                  setErrors({
-                    requesterEmail: undefined,
-                  })
-                }
-                handleChange("requesterEmail")(text.trim())
-              }}
-              blurOnSubmit={false}
-              placeholder="Email address"
-              value={values.requesterEmail}
-              returnKeyType="next"
-              spellCheck={false}
-              autoCorrect={false}
-              textContentType={Platform.OS === "ios" ? "username" : "emailAddress"}
-              error={errors.requesterEmail}
-            />
-            <Spacer p={1} />
-            <PhoneInput
-              testID="request-price-estimate-phone-input"
-              style={{ flex: 1 }}
-              title="Phone number"
-              autoFocus={!values.requesterPhoneNumber}
-              placeholder="(000) 000-0000"
-              onChangeText={handleChange("requesterPhoneNumber")}
-              value={values.requesterPhoneNumber}
-              setValidation={() => null}
-              accessibilityLabel="Phone number"
-              shouldDisplayLocalError={false}
-            />
-            <Spacer p={2} />
+            <Join separator={<Spacer p={1} />}>
+              <Text variant="xl">Contact Information</Text>
+              <Text variant="md" color="black60">
+                We will only use this to contact you about this price estimate
+              </Text>
+              <Input
+                testID="request-price-estimate-name-input"
+                title="Name"
+                autoCapitalize="words"
+                autoCorrect={false}
+                onChangeText={(text) => {
+                  if (errors.requesterName) {
+                    setErrors({
+                      requesterName: undefined,
+                    })
+                  }
+                  handleChange("requesterName")(text)
+                }}
+                blurOnSubmit={false}
+                placeholder="First and Last Name"
+                returnKeyType="done"
+                maxLength={128}
+                value={values.requesterName}
+                error={errors.requesterName}
+              />
+
+              <Input
+                testID="request-price-estimate-email-input"
+                title="Email"
+                autoCapitalize="none"
+                enableClearButton
+                keyboardType="email-address"
+                onChangeText={(text) => {
+                  if (errors.requesterEmail) {
+                    setErrors({
+                      requesterEmail: undefined,
+                    })
+                  }
+                  handleChange("requesterEmail")(text.trim())
+                }}
+                blurOnSubmit={false}
+                placeholder="Email address"
+                value={values.requesterEmail}
+                returnKeyType="next"
+                spellCheck={false}
+                autoCorrect={false}
+                textContentType={Platform.OS === "ios" ? "username" : "emailAddress"}
+                error={errors.requesterEmail}
+              />
+              <PhoneInput
+                testID="request-price-estimate-phone-input"
+                style={{ flex: 1 }}
+                title="Phone number"
+                autoFocus={!values.requesterPhoneNumber}
+                placeholder="(000) 000-0000"
+                onChangeText={handleChange("requesterPhoneNumber")}
+                value={values.requesterPhoneNumber}
+                setValidation={() => null}
+                accessibilityLabel="Phone number"
+                shouldDisplayLocalError={false}
+              />
+            </Join>
+            <Spacer mt={3} />
             <Button
               block
               onPress={handleSubmit}
