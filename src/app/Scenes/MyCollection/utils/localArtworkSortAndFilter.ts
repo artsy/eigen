bdec9bf94fcd9ffe91d1f9b2ba18ea480c825ce4
Ole <ole.richter91@gmail.com>
2022-04-20 10:23:25 +0200

    chore: Remove AREnableShowOnlySubmittedMyCollectionArtworkFilter feature flag

--- src/app/Scenes/MyCollection/utils/localArtworkSortAndFilter.ts
+++ src/app/Scenes/MyCollection/utils/localArtworkSortAndFilter.ts
@@ -7,7 +7,6 @@ import {
 } from "app/Components/ArtworkFilter/ArtworkFilterHelpers"
 import { ArtworksFiltersStore } from "app/Components/ArtworkFilter/ArtworkFilterStore"
 import { FilterDisplayConfig } from "app/Components/ArtworkFilter/types"
-import { useFeatureFlag } from "app/store/GlobalStore"
 import { normalizeText } from "app/utils/normalizeText"
 import { compact, filter, orderBy, uniqBy } from "lodash"
 import { DateTime } from "luxon"
@@ -18,9 +17,6 @@ export const useLocalArtworkFilter = (artworksList?: any[] | null) => {
   const setFilterType = ArtworksFiltersStore.useStoreActions((s) => s.setFilterTypeAction)
   const setSortOptions = ArtworksFiltersStore.useStoreActions((s) => s.setSortOptions)
   const setFilterOptions = ArtworksFiltersStore.useStoreActions((s) => s.setFilterOptions)
-  const allowOnlySubmittedArtworks = useFeatureFlag(
-    "AREnableShowOnlySubmittedMyCollectionArtworkFilter"
-  )
 
   const initLocalArtworkFilter = (artworks: any[]) => {
     setFilterType("local")
@@ -100,7 +96,7 @@ export const useLocalArtworkFilter = (artworksList?: any[] | null) => {
     ])
     setFilterOptions(
       compact([
-        allowOnlySubmittedArtworks && {
+        {
           configType: FilterConfigTypes.FilterScreenCheckboxItem,
           displayText: "Show Only Submitted Artworks",
           filterType: "showOnlySubmittedArtworks",
