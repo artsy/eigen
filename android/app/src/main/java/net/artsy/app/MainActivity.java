66a59d54d9466fc097408ca070808b708a0a825e
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2022-04-22 08:37:34 -0400

    feat: android braze push (#6562)
    * update braze sdk to latest
    
    * add missing line in android mainActivity for braze setup
    
    * add missing import and update constructor for new version
    
    * enable automatic braze push registration
    
    * register braze messaging service
    
    * wip: manual braze token registration
    
    * Revert "update braze sdk to latest"
    
    This reverts commit c2c4f39870063aec3a8d6d085b11c594b3eb48d1.
    
    * correct changeUser call, patch existing push service
    
    * subclass instead of patch the messaging service
    
    * fix: send the decoded key to analytics (#6608)
    
    Co-authored-by: Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
    Co-authored-by: Kizito Egeonu <kizito.egeonu@gmail.com>
    
    Co-authored-by: Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
    Co-authored-by: Kizito Egeonu <kizito.egeonu@gmail.com>
    
    * wip: attempt to use custom broadcast receiver and message service subclass
    
    * fix broadcast receiver intent handling and registration
    
    * enable verbose logging, revert me
    
    * handle push with no deeplink, add deeplink check
    
    * wip: poc deeplink for foreground and background working, needs cleanup
    
    * ignore debouncing when navigating from push
    
    * cleanup code
    
    * cleanup PushNotification.ts
    
    * cleanup loggers and useDeeplinks
    
    * parse deeplink, cleanup code
    
    * update braze dependencies, remove segment plugin for now
    
    * add icon prefs for braze
    
    * remove mocking for segment appboy plugin
    
    * revert changes, try basic integration with updated library
    
    * add artsy:// schema to manifest
    
    * rewrite braze intent to deeplink properly
    
    * handle artsy schema urls in navigate
    
    * use a custom listener, avoid interfering with existing pushes
    
    * add back segment braze package
    
    * remove unnecessary manual token registration
    
    * use better custom intent handler approach, fixes deeplink from killed state
    
    * remove unnecessary import
    
    * fix failing test
    
    * mock segment appbody dependency
    
    * mock other appboy dependency
    
    * remove verbose logging
    
    Co-authored-by: Mounir Dhahri <soussouheros@gmail.com>
    Co-authored-by: Kizito Egeonu <kizito.egeonu@gmail.com>

--- android/app/src/main/java/net/artsy/app/MainActivity.java
+++ android/app/src/main/java/net/artsy/app/MainActivity.java
@@ -8,18 +8,25 @@ import android.content.res.Configuration;
 import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowManager;
+import android.content.Intent;
+import android.net.Uri;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import com.zoontek.rnbootsplash.RNBootSplash;
+import com.dieam.reactnativepushnotification.modules.RNPushNotification;
 
+import android.util.Log;
 
 public class MainActivity extends ReactActivity {
+  private static final String TAG = MainActivity.class.getName();
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -32,15 +39,14 @@ public class MainActivity extends ReactActivity {
     return new ReactActivityDelegate(this, getMainComponentName()) {
       @Override
       protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
       }
     };
   }
 
   private boolean isTablet() {
     return (this.getResources().getConfiguration().screenLayout
-      & Configuration.SCREENLAYOUT_SIZE_MASK)
-      >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+        & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE;
   }
 
   @Override
@@ -55,5 +61,52 @@ public class MainActivity extends ReactActivity {
       setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
     RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
+
+    RNPushNotification.IntentHandlers.add(new RNPushNotification.RNIntentHandler() {
+      @Override
+      public void onNewIntent(Intent intent) {
+        // If your provider requires some parsing on the intent before the data can be
+        // used, add that code here. Otherwise leave empty.
+      }
+
+      @Nullable
+      @Override
+      public Bundle getBundleFromIntent(Intent intent) {
+        // See here:
+        // https://github.com/zo0r/react-native-push-notification#handling-custom-payloads
+
+        // This should return the bundle data that will be serialized to the
+        // `notification.data`
+        // property sent to the `onNotification()` handler. Return `null` if there is no
+        // data
+        // or this is not an intent from your provider.
+
+        // Parse braze notifications and
+        // send the url in the data bundle so
+        // it can be read in our notifciation callback
+        // on ts side, we may want to pass other stuff for tracking
+        String source = intent.getStringExtra("source");
+        if (source != null && source.equalsIgnoreCase("appboy")) {
+          if (intent.hasExtra("uri")) {
+            Bundle bundle = new Bundle();
+            Bundle uriBundle = new Bundle();
+            String uri = intent.getStringExtra("uri");
+            uriBundle.putString("url", uri);
+            bundle.putBundle("data", uriBundle);
+            return bundle;
+          }
+        }
+
+        return null;
+      }
+    });
+  }
+
+  // Basic overriding this class required for braze integration:
+  // https://www.braze.com/docs/developer_guide/platform_integration_guides/react_native/react_sdk_setup/#step-2-complete-native-setup
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    setIntent(intent);
   }
 }
