2bfbf187c2248ac1555f7f3a79b0e80dbe4a056d
Dima Tretyak <itretyak@ya.ru>
2022-05-03 14:29:43 +0300

    chore: support only ReactNativeFile in upload middleware

--- src/app/relay/middlewares/uploadMiddleware.ts
+++ src/app/relay/middlewares/uploadMiddleware.ts
@@ -1,16 +1,17 @@
 import { isReactNativeFile, ReactNativeFile } from "app/utils/ReactNativeFile"
-import extractFiles, { ExtractableFile } from "extract-files/extractFiles.mjs"
-import isExtractableFile from "extract-files/isExtractableFile.mjs"
+import extractFiles from "extract-files/extractFiles.mjs"
 import { Middleware, RelayNetworkLayerRequestBatch } from "react-relay-network-modern/node8"
 
-const isExtractable = (value: any) => {
-  return isExtractableFile(value) || isReactNativeFile(value)
-}
-
 /**
- * Improved version of `upload` middleware from `react-relay-network-modern` with support for `ReactNativeFile'
+ * Note: this request will fail if you are using React Native debugger with "Network Inspect" feature enabled
+ * Note: we must definitely specify `name` for file, otherwise Metaphysics will assume that the file is NOT being passed
  *
+ * Upload middleware from `react-relay-network-modern` with ReactNativeFile support
  * Original: https://github.com/relay-tools/react-relay-network-modern/blob/master/src/middlewares/upload.js
+ *
+ * If we try to pass Blob/File with specified `name` field to `fetch()`, we will get an error on Android.
+ * For this reason, support for these data formats is extracted from original `upload` middleware
+ * TODO: Add support for Blob/File when problem will be fixed for Android
  */
 export const uploadMiddleware = (): Middleware => {
   return (next) => async (req) => {
@@ -23,9 +24,9 @@ export const uploadMiddleware = (): Middleware => {
       variables: req.variables,
     }
 
-    const { clone: extractedOperations, files } = extractFiles<ExtractableFile | ReactNativeFile>(
+    const { clone: extractedOperations, files } = extractFiles<ReactNativeFile>(
       operations,
-      isExtractable
+      isReactNativeFile
     )
 
     if (files.size) {
@@ -42,9 +43,7 @@ export const uploadMiddleware = (): Middleware => {
 
       index = 0
       files.forEach((_, file) => {
-        // We must explicitly specify filename, otherwise Metaphysics will assume that file is NOT being passed
-        // @ts-ignore
-        formData.append(index++, file, file.name)
+        formData.append(String(index++), file)
       })
 
       req.fetchOpts.method = "POST"
