9f58949266fc5aa583ae012f88477296a237ce82
dariakoko <nargle30@gmail.com>
2022-05-10 14:27:57 +0200

    added PaginationFragment

--- src/app/Scenes/MyCollection/Screens/Insights/AuctionResultsBasedOnArtistsYouCollectList.tsx
+++ src/app/Scenes/MyCollection/Screens/Insights/AuctionResultsBasedOnArtistsYouCollectList.tsx
@@ -2,31 +2,63 @@ import { AuctionResultsBasedOnArtistsYouCollectList_me$key } from "__generated__
 import { AuctionResultsBasedOnArtistsYouCollectListQuery } from "__generated__/AuctionResultsBasedOnArtistsYouCollectListQuery.graphql"
 import { extractNodes } from "app/utils/extractNodes"
 import { Text } from "palette"
-import React from "react"
-import { graphql, useFragment, useLazyLoadQuery } from "react-relay"
+import React, { useState } from "react"
+import { graphql, useFragment, useLazyLoadQuery, usePaginationFragment } from "react-relay"
+
+const articlesQueryVariables = {
+  count: 10,
+}
 
 export const AuctionResultsBasedOnArtistsYouCollectList: React.FC<{}> = () => {
-  const data = useLazyLoadQuery<AuctionResultsBasedOnArtistsYouCollectListQuery>(
+  const queryData = useLazyLoadQuery<AuctionResultsBasedOnArtistsYouCollectListQuery>(
     AuctionResultsBasedOnArtistsYouCollectListScreenQuery,
-    {}
+    articlesQueryVariables
   )
-  if (!data) {
+
+  const { data, loadNext, hasNext, isLoadingNext, refetch } = usePaginationFragment<
+    AuctionResultsBasedOnArtistsYouCollectListQuery,
+    AuctionResultsBasedOnArtistsYouCollectList_me$key
+  >(auctionResultsBasedOnArtistsYouCollectListFragment, queryData.me!)
+
+  const [refreshing, setRefreshing] = useState(false)
+
+  const handleLoadMore = () => {
+    if (!hasNext || isLoadingNext) {
+      return
+    }
+
+    loadNext(articlesQueryVariables.count)
+  }
+
+  const handleRefresh = () => {
+    setRefreshing(true)
+    refetch({})
+    setRefreshing(false)
+  }
+
+  const results = extractNodes(data.myCollectionAuctionResults)
+
+  console.log("LOGD RESULTS = ", results.length, results)
+
+  console.log("data = ", data)
+  if (!queryData) {
     return null
   }
 
-  const fragmentData = useFragment<AuctionResultsBasedOnArtistsYouCollectList_me$key>(
+  /*   const fragmentData = useFragment<AuctionResultsBasedOnArtistsYouCollectList_me$key>(
     auctionResultsBasedOnArtistsYouCollectListFragment,
     data.me!
-  )
-  const results = extractNodes(fragmentData.myCollectionAuctionResults)
+  ) */
 
-  console.log("LOGD RESULTS = ", results.length, results)
   return <Text> HELLO </Text>
 }
 
 const auctionResultsBasedOnArtistsYouCollectListFragment = graphql`
-  fragment AuctionResultsBasedOnArtistsYouCollectList_me on Me {
-    myCollectionAuctionResults(first: 1) {
+  fragment AuctionResultsBasedOnArtistsYouCollectList_me on Me
+  @refetchable(queryName: "AuctionResultsBasedOnArtistsYouCollectList_meRefetch")
+  @argumentDefinitions(count: { type: "Int", defaultValue: 10 }, after: { type: "String" }) {
+    myCollectionAuctionResults(first: $count, after: $after)
+      @connection(key: "AuctionResults_myCollectionAuctionResults") {
       totalCount
       edges {
         node {
@@ -41,9 +73,23 @@ const auctionResultsBasedOnArtistsYouCollectListFragment = graphql`
 `
 
 const AuctionResultsBasedOnArtistsYouCollectListScreenQuery = graphql`
-  query AuctionResultsBasedOnArtistsYouCollectListQuery {
+  query AuctionResultsBasedOnArtistsYouCollectListQuery($count: Int, $after: String) {
     me {
-      ...AuctionResultsBasedOnArtistsYouCollectList_me
+      ...AuctionResultsBasedOnArtistsYouCollectList_me @arguments(count: $count, after: $after)
     }
   }
 `
+
+/*
+{
+          totalCount
+          edges {
+            node {
+              saleDate
+              internalID
+              artistID
+              ...AuctionResultListItem_auctionResult
+            }
+          }
+        }
+*/
