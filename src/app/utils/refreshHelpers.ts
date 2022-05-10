da97044c3ff0bc0346f1c601d3f033fb9fe267c1
Ole <ole.richter91@gmail.com>
2022-05-10 12:11:23 +0200

    Increase max event listeners

--- src/app/utils/refreshHelpers.ts
+++ src/app/utils/refreshHelpers.ts
@@ -1,6 +1,7 @@
 import EventEmitter from "events"
 
 export const RefreshEvents = new EventEmitter()
+RefreshEvents.setMaxListeners(20)
 
 export const FAVORITE_ARTWORKS_REFRESH_KEY = "refreshFavoriteArtworks"
 export const MY_COLLECTION_REFRESH_KEY = "refreshMyCollection"
