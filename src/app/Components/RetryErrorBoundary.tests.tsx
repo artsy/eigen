2e1f7713687f727900915dc9cac159f82edd9b29
Ole <ole.richter91@gmail.com>
2022-05-10 10:35:40 +0200

    feat: Introduce NotFoundFailureView (#6700)
    Co-authored-by: Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>

--- src/app/Components/RetryErrorBoundary.tests.tsx
+++ src/app/Components/RetryErrorBoundary.tests.tsx
@@ -1,9 +1,10 @@
-import { renderWithWrappers } from "app/tests/renderWithWrappers"
+import { __globalStoreTestUtils__ } from "app/store/GlobalStore"
+import { renderWithWrappers, renderWithWrappersTL } from "app/tests/renderWithWrappers"
 import React from "react"
 import "react-native"
 
 import { LoadFailureView } from "./LoadFailureView"
-import { RetryErrorBoundaryLegacy } from "./RetryErrorBoundary"
+import { RetryErrorBoundary, RetryErrorBoundaryLegacy } from "./RetryErrorBoundary"
 
 const consoleError = console.error
 beforeEach(() => {
@@ -13,43 +14,101 @@ afterEach(() => {
   console.error = consoleError
 })
 
-it("Renders the fallback view when the rendered component crashes", () => {
-  const tree = renderWithWrappers(
-    <RetryErrorBoundaryLegacy render={() => <CrashingComponent shouldCrash />} />
-  )
-  expect(tree.root.findAllByType(LoadFailureView)).toHaveLength(1)
+describe("RetryErrorBoundary", () => {
+  describe("when the rendered component crashes", () => {
+    beforeEach(() => {
+      __globalStoreTestUtils__?.injectFeatureFlags({
+        AREnableCascadingEndTimerSalePageGrid: true,
+      })
+    })
+
+    it("renders Unable to load", () => {
+      const { getByText } = renderWithWrappersTL(
+        <RetryErrorBoundary>
+          <CrashingComponent shouldCrash />
+        </RetryErrorBoundary>
+      )
+
+      expect(getByText("Unable to load")).toBeDefined()
+    })
+  })
+
+  describe("when the rendered component crashes with a 404 status code", () => {
+    beforeEach(() => {
+      __globalStoreTestUtils__?.injectFeatureFlags({ AREnableCascadingEndTimerSalePageGrid: true })
+    })
+
+    const error = {
+      res: {
+        json: {
+          errors: [
+            {
+              extensions: {
+                httpStatusCodes: [404],
+              },
+            },
+          ],
+        },
+      },
+    }
+
+    it("renders NotFoundFailureView", () => {
+      const { getByText } = renderWithWrappersTL(
+        <RetryErrorBoundary>
+          <CrashingComponent shouldCrash error={error} />
+        </RetryErrorBoundary>
+      )
+
+      expect(getByText("Not Found")).toBeDefined()
+    })
+  })
+})
+describe("RetryErrorBoundaryLegacy", () => {
+  it("Renders the fallback view when the rendered component crashes", () => {
+    const tree = renderWithWrappers(
+      <RetryErrorBoundaryLegacy render={() => <CrashingComponent shouldCrash />} />
+    )
+    expect(tree.root.findAllByType(LoadFailureView)).toHaveLength(1)
+  })
+
+  it("passes false for isRetry to render prop on first pass", () => {
+    let receivedIsRetry = true
+    renderWithWrappers(
+      <RetryErrorBoundaryLegacy
+        render={({ isRetry }) => {
+          receivedIsRetry = isRetry
+          return <CrashingComponent shouldCrash />
+        }}
+      />
+    )
+    expect(receivedIsRetry).toBeFalsy()
+  })
+
+  it("passes true for isRetry to render prop on retry", () => {
+    let receivedIsRetry = false
+    const tree = renderWithWrappers(
+      <RetryErrorBoundaryLegacy
+        render={({ isRetry }) => {
+          receivedIsRetry = isRetry
+          // Only crash on the first attempt, succeed on the retry.
+          return <CrashingComponent shouldCrash={isRetry ? false : true} />
+        }}
+      />
+    )
+    // Simulate user pressing retry button
+    tree.root.findAllByType(LoadFailureView)[0].props.onRetry()
+    expect(receivedIsRetry).toBeTruthy()
+  })
 })
 
-it("passes false for isRetry to render prop on first pass", () => {
-  let receivedIsRetry = true
-  renderWithWrappers(
-    <RetryErrorBoundaryLegacy
-      render={({ isRetry }) => {
-        receivedIsRetry = isRetry
-        return <CrashingComponent shouldCrash />
-      }}
-    />
-  )
-  expect(receivedIsRetry).toBeFalsy()
-})
+const CrashingComponent: React.FC<{ shouldCrash: boolean; error?: any }> = ({
+  shouldCrash,
+  error,
+}) => {
+  if (shouldCrash && error) {
+    throw error
+  }
 
-it("passes true for isRetry to render prop on retry", () => {
-  let receivedIsRetry = false
-  const tree = renderWithWrappers(
-    <RetryErrorBoundaryLegacy
-      render={({ isRetry }) => {
-        receivedIsRetry = isRetry
-        // Only crash on the first attempt, succeed on the retry.
-        return <CrashingComponent shouldCrash={isRetry ? false : true} />
-      }}
-    />
-  )
-  // Simulate user pressing retry button
-  tree.root.findAllByType(LoadFailureView)[0].props.onRetry()
-  expect(receivedIsRetry).toBeTruthy()
-})
-
-const CrashingComponent: React.FC<{ shouldCrash: boolean }> = ({ shouldCrash }) => {
   const thing: any = null
   if (shouldCrash && thing.thisshouldcrash) {
     return null
