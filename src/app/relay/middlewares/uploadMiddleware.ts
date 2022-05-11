a3383c784329dcc1349414846734e70649bd3e57
Dima Tretyak <itretyak@ya.ru>
2022-05-11 22:41:23 +0300

    refactor: some optimization for upload middleware

--- src/app/relay/middlewares/uploadMiddleware.ts
+++ src/app/relay/middlewares/uploadMiddleware.ts
@@ -5,6 +5,8 @@ const isReactNativeFile = <T>(value: any): value is T => {
   return value instanceof ReactNativeFile
 }
 
+let queryMap: any
+
 /**
  * Note: this request will fail if you are using React Native debugger with "Network Inspect" feature enabled
  * Note: we must definitely specify `name` for file, otherwise Metaphysics will assume that the file is NOT being passed
@@ -22,33 +24,31 @@ export const uploadMiddleware = (): Middleware => {
       throw new Error("RelayRequestBatch is not supported")
     }
 
-    const operations = {
-      query: require("../../../../data/complete.queryMap.json")[req.id],
-      variables: req.variables,
-    }
-
-    const { clone: extractedOperations, files } = extractFiles<ReactNativeFile>(
-      operations,
-      "",
-      isReactNativeFile
-    )
+    const { variables, id } = req
+    const result = extractFiles<ReactNativeFile>({ variables }, "", isReactNativeFile)
+    const { clone: extractedVariables, files } = result
 
     if (files.size) {
+      if (!queryMap) {
+        queryMap = require("../../../../data/complete.queryMap.json")
+      }
+
+      const query = queryMap[id]
       const formData = new FormData()
       const pathMap: Record<string, any> = {}
       let index = 0
 
-      files.forEach((path) => {
+      for (const path of files.values()) {
         pathMap[index++] = path
-      })
+      }
 
-      formData.append("operations", JSON.stringify(extractedOperations))
+      formData.append("operations", JSON.stringify({ query, variables: extractedVariables }))
       formData.append("map", JSON.stringify(pathMap))
 
       index = 0
-      files.forEach((_, file) => {
+      for (const file of files.keys()) {
         formData.append(String(index++), file)
-      })
+      }
 
       req.fetchOpts.method = "POST"
       req.fetchOpts.body = formData
