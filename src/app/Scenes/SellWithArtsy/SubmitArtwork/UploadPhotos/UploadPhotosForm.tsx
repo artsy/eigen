99e19309a12020b6daa5a17612d671ddddb30527
Alican Akyuz <alicakyz@gmail.com>
2022-05-11 11:00:57 +0200

    fix: set allPhotos for myCol as well

--- src/app/Scenes/SellWithArtsy/SubmitArtwork/UploadPhotos/UploadPhotosForm.tsx
+++ src/app/Scenes/SellWithArtsy/SubmitArtwork/UploadPhotos/UploadPhotosForm.tsx
@@ -88,7 +88,7 @@ export const UploadPhotosForm: React.FC<{ isAnyPhotoLoading?: boolean }> = ({
     })
 
     // store photos in asynstorage to be retrieved later when the user goes to My Collection
-    storeLocalPhotos(submission.submissionId, photos)
+    storeLocalPhotos(submission.submissionId, allPhotos)
 
     setFieldValue("photos", allPhotos)
   }
