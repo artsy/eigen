1b38e8e45e41c5d4075adf954f8f49ac4d099281
Mounir Dhahri <soussouheros@gmail.com>
2022-05-19 14:35:01 +0200

    chore: extract MarketSignalsSectionHeader

--- src/app/Scenes/MyCollection/Screens/Insights/MyCollectionInsights.tsx
+++ src/app/Scenes/MyCollection/Screens/Insights/MyCollectionInsights.tsx
@@ -1,6 +1,5 @@
 import { MyCollectionInsightsQuery } from "__generated__/MyCollectionInsightsQuery.graphql"
 import { StickyTabPageScrollView } from "app/Components/StickyTabPage/StickyTabPageScrollView"
-import { InfoModal } from "app/Scenes/SellWithArtsy/SubmitArtwork/ArtworkDetails/InfoModal/InfoModal"
 import { extractNodes } from "app/utils/extractNodes"
 import { Flex, Spinner, Text, Touchable, useSpace } from "palette"
 import React, { Suspense, useState } from "react"
@@ -8,6 +7,7 @@ import { useLazyLoadQuery } from "react-relay"
 import { graphql } from "relay-runtime"
 import { ActivateMoreMarketInsightsBanner } from "./ActivateMoreMarketInsightsBanner"
 import { AuctionResultsForArtistsYouCollectRail } from "./AuctionResultsForArtistsYouCollectRail"
+import { MarketSignalsSectionHeader } from "./MarketSignalsSectionHeader"
 import { MyCollectionInsightsEmptyState } from "./MyCollectionInsightsEmptyState"
 import { MyCollectionInsightsOverview } from "./MyCollectionInsightsOverview"
 
@@ -45,48 +45,6 @@ export const MyCollectionInsights: React.FC<{}> = ({}) => {
     >
       {myCollectionArtworksCount > 0 ? renderContent() : <MyCollectionInsightsEmptyState />}
     </StickyTabPageScrollView>
-  )
-}
-
-const MarketSignalsSectionHeader = () => {
-  const [shouldShowarketSignalsModal, setShouldShowarketSignalsModal] = useState(false)
-
-  return (
-    <Flex justifyContent="space-between" flexDirection="row" mb={2} alignItems="center" px={2}>
-      <Text variant="lg">Market Signals</Text>
-      <Touchable onPress={() => setShouldShowarketSignalsModal(true)} haptic>
-        <Text style={{ textDecorationLine: "underline" }} variant="sm" color="black60">
-          What is this?
-        </Text>
-      </Touchable>
-      <InfoModal
-        title="Market Signals"
-        visible={shouldShowarketSignalsModal}
-        onDismiss={() => setShouldShowarketSignalsModal(false)}
-      >
-        <Flex mt={1}>
-          <Text caps>what are artsy insights</Text>
-          <Text>
-            Artsy insights are free, at-a glance insights into the market and career of artists in
-            your collection.
-          </Text>
-          <Text caps mt={2}>
-            where do insights come from?
-          </Text>
-          <Text>
-            Our market data comes from the Artsy price database, which includes millions of results
-            from leading auction houses across the globe.
-          </Text>
-          <Text caps mt={2}>
-            will I see insights on my entire collection?
-          </Text>
-          <Text>
-            Our database covers 300,000 artists — and counting. Not all artists in your collection
-            will have insights right now, but we're adding more all the time.
-          </Text>
-        </Flex>
-      </InfoModal>
-    </Flex>
   )
 }
 
