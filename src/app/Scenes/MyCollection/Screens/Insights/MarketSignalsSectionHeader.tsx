8faaa1c55da3e15a5ae0ff9e113b522c36d4cad2
Mounir Dhahri <soussouheros@gmail.com>
2022-05-19 14:42:10 +0200

    chore: rename state to isVisible

--- src/app/Scenes/MyCollection/Screens/Insights/MarketSignalsSectionHeader.tsx
+++ src/app/Scenes/MyCollection/Screens/Insights/MarketSignalsSectionHeader.tsx
@@ -3,21 +3,17 @@ import { Flex, Text, Touchable } from "palette"
 import React, { useState } from "react"
 
 export const MarketSignalsSectionHeader: React.FC<{}> = () => {
-  const [shouldShowarketSignalsModal, setShouldShowarketSignalsModal] = useState(false)
+  const [isVisible, setIsVisible] = useState(false)
 
   return (
     <Flex justifyContent="space-between" flexDirection="row" mb={2} alignItems="center" px={2}>
       <Text variant="lg">Market Signals</Text>
-      <Touchable onPress={() => setShouldShowarketSignalsModal(true)} haptic>
+      <Touchable onPress={() => setIsVisible(true)} haptic>
         <Text style={{ textDecorationLine: "underline" }} variant="sm" color="black60">
           What is this?
         </Text>
       </Touchable>
-      <InfoModal
-        title="Market Signals"
-        visible={shouldShowarketSignalsModal}
-        onDismiss={() => setShouldShowarketSignalsModal(false)}
-      >
+      <InfoModal title="Market Signals" visible={isVisible} onDismiss={() => setIsVisible(false)}>
         <Flex mt={1}>
           <Text caps>what are artsy insights</Text>
           <Text>
