7a1179ad89e6c54c41584682e542a60b9c372655
Dima Tretyak <itretyak@ya.ru>
2022-05-20 17:32:23 +0300

    chore: updates tests for Artists screen

--- src/app/Scenes/Artist/Artist.tests.tsx
+++ src/app/Scenes/Artist/Artist.tests.tsx
@@ -1,14 +1,7 @@
-import { ArtistAboutContainer } from "app/Components/Artist/ArtistAbout/ArtistAbout"
-import ArtistArtworks from "app/Components/Artist/ArtistArtworks/ArtistArtworks"
-import { ArtistHeaderFragmentContainer } from "app/Components/Artist/ArtistHeader"
-import { ArtistInsights } from "app/Components/Artist/ArtistInsights/ArtistInsights"
-import { __globalStoreTestUtils__ } from "app/store/GlobalStore"
-import { extractText } from "app/tests/extractText"
-import { renderWithWrappers } from "app/tests/renderWithWrappers"
+import { renderWithWrappersTL } from "app/tests/renderWithWrappers"
 import { postEventToProviders } from "app/utils/track/providers"
-import _ from "lodash"
+import { isEqual } from "lodash"
 import React from "react"
-import "react-native"
 import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
 import { MockResolvers } from "relay-test-utils/lib/RelayMockPayloadGenerator"
 import { ArtistQueryRenderer } from "./Artist"
@@ -18,30 +11,24 @@ jest.unmock("react-tracking")
 
 type ArtistQueries = "ArtistAboveTheFoldQuery" | "ArtistBelowTheFoldQuery"
 
-describe("availableTabs", () => {
-  const originalError = console.error
-  const originalWarn = console.warn
-  let environment = createMockEnvironment()
+describe("Artist", () => {
+  let mockEnvironment: ReturnType<typeof createMockEnvironment>
 
   beforeEach(() => {
-    environment = createMockEnvironment()
-    console.error = jest.fn()
-    console.warn = jest.fn()
+    mockEnvironment = createMockEnvironment()
   })
 
   afterEach(() => {
-    environment = createMockEnvironment()
-    console.error = originalError
-    console.warn = originalWarn
+    jest.clearAllMocks()
   })
 
   function mockMostRecentOperation(name: ArtistQueries, mockResolvers: MockResolvers = {}) {
-    expect(environment.mock.getMostRecentOperation().request.node.operation.name).toBe(name)
-    environment.mock.resolveMostRecentOperation((operation) => {
+    expect(mockEnvironment.mock.getMostRecentOperation().request.node.operation.name).toBe(name)
+    mockEnvironment.mock.resolveMostRecentOperation((operation) => {
       const result = MockPayloadGenerator.generate(operation, {
         ID({ path }) {
           // need to make sure artist id is stable between above-and-below-the-fold queries to avoid cache weirdness
-          if (_.isEqual(path, ["artist", "id"])) {
+          if (isEqual(path, ["artist", "id"])) {
             return "artist-id"
           }
         },
@@ -52,11 +39,14 @@ describe("availableTabs", () => {
   }
 
   const TestWrapper = (props: Record<string, any>) => (
-    <ArtistQueryRenderer artistID="ignored" environment={environment} {...props} />
+    <ArtistQueryRenderer artistID="ignored" environment={mockEnvironment} {...props} />
   )
 
   it("returns an empty state if artist has no metadata, shows, insights, or works", async () => {
-    const tree = renderWithWrappers(<TestWrapper />)
+    const { getByText } = renderWithWrappersTL(<TestWrapper />)
+    const message =
+      "There aren’t any works available by the artist at this time. Follow to receive notifications when new works are added."
+
     mockMostRecentOperation("ArtistAboveTheFoldQuery", {
       Artist() {
         return {
@@ -70,14 +60,13 @@ describe("availableTabs", () => {
         }
       },
     })
-    expect(tree.root.findAllByType(ArtistHeaderFragmentContainer)).toHaveLength(1)
-    expect(extractText(tree.root)).toContain(
-      "There aren’t any works available by the artist at this time. Follow to receive notifications when new works are added"
-    )
+
+    expect(getByText(message)).toBeTruthy()
   })
 
-  it("returns only About tab if artist has only metadata", async () => {
-    const tree = renderWithWrappers(<TestWrapper />)
+  it("returns only Overview tab if artist has only metadata", async () => {
+    const { queryByText } = renderWithWrappersTL(<TestWrapper />)
+
     mockMostRecentOperation("ArtistAboveTheFoldQuery", {
       Artist() {
         return {
@@ -88,21 +77,17 @@ describe("availableTabs", () => {
             auctionLots: false,
             articles: false,
           },
-          auctionResultsConnection: {
-            totalCount: 0,
-          },
         }
       },
     })
-    expect(tree.root.findAllByType(ArtistHeaderFragmentContainer)).toHaveLength(1)
-    expect(tree.root.findAllByType(ArtistAboutContainer)).toHaveLength(0)
-    // it only shows below the fold
-    mockMostRecentOperation("ArtistBelowTheFoldQuery")
-    expect(tree.root.findAllByType(ArtistAboutContainer)).toHaveLength(1)
+
+    expect(queryByText("Overview")).toBeTruthy()
+    expect(queryByText("Artworks")).toBeFalsy()
   })
 
-  it("returns About tab if artist has only articles", async () => {
-    const tree = renderWithWrappers(<TestWrapper />)
+  it("returns Overview tab if artist has only articles", async () => {
+    const { getByText } = renderWithWrappersTL(<TestWrapper />)
+
     mockMostRecentOperation("ArtistAboveTheFoldQuery", {
       Artist() {
         return {
@@ -113,37 +98,37 @@ describe("availableTabs", () => {
             auctionLots: false,
             articles: true,
           },
-          auctionResultsConnection: {
-            totalCount: 0,
-          },
         }
       },
     })
-    mockMostRecentOperation("ArtistBelowTheFoldQuery")
-    expect(tree.root.findAllByType(ArtistAboutContainer)).toHaveLength(1)
+
+    expect(getByText("Overview")).toBeTruthy()
   })
 
   it("returns three tabs if artist has metadata, works, and auction results", async () => {
-    const tree = renderWithWrappers(<TestWrapper />)
+    const { getByText } = renderWithWrappersTL(<TestWrapper />)
+
     mockMostRecentOperation("ArtistAboveTheFoldQuery", {
       Artist() {
         return {
           has_metadata: true,
           counts: { articles: 1, related_artists: 0, artworks: 1, partner_shows: 1 },
-          auctionResultsConnection: {
-            totalCount: 10,
+          statuses: {
+            auctionLots: true,
+            artworks: true,
           },
         }
       },
     })
-    expect(tree.root.findAllByType(ArtistArtworks)).toHaveLength(1)
-    mockMostRecentOperation("ArtistBelowTheFoldQuery")
-    expect(tree.root.findAllByType(ArtistAboutContainer)).toHaveLength(1)
-    expect(tree.root.findAllByType(ArtistInsights)).toHaveLength(1)
+
+    expect(getByText("Overview")).toBeTruthy()
+    expect(getByText("Artworks")).toBeTruthy()
+    expect(getByText("Insights")).toBeTruthy()
   })
 
   it("hides Artist insights tab when there are no auction results", async () => {
-    const tree = renderWithWrappers(<TestWrapper />)
+    const { queryByText } = renderWithWrappersTL(<TestWrapper />)
+
     mockMostRecentOperation("ArtistAboveTheFoldQuery", {
       Artist() {
         return {
@@ -152,21 +137,20 @@ describe("availableTabs", () => {
             artworks: true,
             auctionLots: false,
           },
-          auctionResultsConnection: {
-            totalCount: 0,
-          },
         }
       },
     })
-    expect(tree.root.findAllByType(ArtistArtworks)).toHaveLength(1)
-    mockMostRecentOperation("ArtistBelowTheFoldQuery")
-    expect(tree.root.findAllByType(ArtistAboutContainer)).toHaveLength(1)
-    expect(tree.root.findAllByType(ArtistInsights)).toHaveLength(0)
+
+    expect(queryByText("Overview")).toBeTruthy()
+    expect(queryByText("Artworks")).toBeTruthy()
+    expect(queryByText("Insights")).toBeFalsy()
   })
 
   it("tracks a page view", () => {
-    renderWithWrappers(<TestWrapper />)
+    renderWithWrappersTL(<TestWrapper />)
+
     mockMostRecentOperation("ArtistAboveTheFoldQuery")
+
     expect(postEventToProviders).toHaveBeenCalledTimes(1)
     expect(postEventToProviders).toHaveBeenNthCalledWith(1, {
       context_screen: "Artist",
