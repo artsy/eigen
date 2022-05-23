927f941e06280768cda087c8de5fb13d7131c42e
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-05-23 19:23:23 +0200

    fix height issue in Android

--- src/app/Scenes/MyCollection/Screens/ArtworkForm/Components/SavingArtworkModal.tsx
+++ src/app/Scenes/MyCollection/Screens/ArtworkForm/Components/SavingArtworkModal.tsx
@@ -1,7 +1,6 @@
 import { CircularSpinner, Flex, Spacer, Text } from "palette"
 import React from "react"
 import { ImageBackground, Modal, ModalProps, StyleSheet } from "react-native"
-import { useScreenDimensions } from "shared/hooks"
 
 interface SavingArtworkModalProps {
   isVisible: boolean
@@ -12,26 +11,22 @@ export const SavingArtworkModal: React.FC<SavingArtworkModalProps & ModalProps> 
   isVisible,
   loadingText = "",
   ...rest
-}) => {
-  const { height: screenHeight } = useScreenDimensions()
+}) => (
+  <Modal animationType="fade" visible={isVisible} {...rest} statusBarTranslucent>
+    <ImageBackground
+      style={{ height: "100%", width: "100%", ...StyleSheet.absoluteFillObject }}
+      source={require("images/InsightsLoadingImage.webp")}
+      resizeMode="cover"
+    >
+      <Flex flex={1} alignItems="center" justifyContent="center">
+        <CircularSpinner color="white100" size="large" />
 
-  return (
-    <Modal animationType="fade" visible={isVisible} {...rest} statusBarTranslucent>
-      <ImageBackground
-        style={{ height: screenHeight, width: "100%", ...StyleSheet.absoluteFillObject }}
-        source={require("images/InsightsLoadingImage.webp")}
-        resizeMode="cover"
-      >
-        <Flex flex={1} alignItems="center" justifyContent="center">
-          <CircularSpinner color="white100" size="large" />
+        <Spacer my={5} />
 
-          <Spacer my={5} />
-
-          <Text variant="md" color="white100">
-            {loadingText}
-          </Text>
-        </Flex>
-      </ImageBackground>
-    </Modal>
-  )
-}
+        <Text variant="md" color="white100">
+          {loadingText}
+        </Text>
+      </Flex>
+    </ImageBackground>
+  </Modal>
+)
