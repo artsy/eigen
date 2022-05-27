1a326f28f70d9f5f1c9554dd4963371ab994a751
Dima Tretyak <itretyak@ya.ru>
2022-05-27 15:30:53 +0300

    chore: add "Sort By" for saved search list

--- src/app/Scenes/SavedSearchAlertsList/SavedSearchAlertsList.tsx
+++ src/app/Scenes/SavedSearchAlertsList/SavedSearchAlertsList.tsx
@@ -1,23 +1,68 @@
 import { SavedSearchAlertsList_me$data } from "__generated__/SavedSearchAlertsList_me.graphql"
 import { SavedSearchAlertsListQuery } from "__generated__/SavedSearchAlertsListQuery.graphql"
+import { FancyModal } from "app/Components/FancyModal/FancyModal"
+import { FancyModalHeader } from "app/Components/FancyModal/FancyModalHeader"
 import { PageWithSimpleHeader } from "app/Components/PageWithSimpleHeader"
+import { TouchableRow } from "app/Components/TouchableRow"
 import { defaultEnvironment } from "app/relay/createEnvironment"
 import { renderWithPlaceholder } from "app/utils/renderWithPlaceholder"
-import React from "react"
+import { Flex, RadioDot, Text } from "palette"
+import React, { useState } from "react"
 import { createFragmentContainer, graphql, QueryRenderer } from "react-relay"
 import { SavedSearchAlertsListPlaceholder } from "./Components/SavedSearchAlertsListPlaceholder"
 import { SavedSearchesListContainer as SavedSearchesList } from "./Components/SavedSearchesList"
+import { SortButton } from "./Components/SortButton"
 
 interface SavedSearchAlertsListProps {
   me: SavedSearchAlertsList_me$data
 }
 
+interface SortOption {
+  value: "CREATED_AT_DESC" | "NAME_ASC"
+  text: string
+}
+
+const SORT_OPTIONS: SortOption[] = [
+  { value: "CREATED_AT_DESC", text: "Recently Added" },
+  { value: "NAME_ASC", text: "Name (A-Z)" },
+]
+
 export const SavedSearchAlertsList: React.FC<SavedSearchAlertsListProps> = (props) => {
   const { me } = props
+  const [modalVisible, setModalVisible] = useState(false)
+  const [selectedSortValue, setSelectedSortValue] = useState("CREATED_AT_DESC")
+
+  const closeModal = () => {
+    setModalVisible(false)
+  }
+
+  const handleSelectOption = (option: SortOption) => {
+    setSelectedSortValue(option.value)
+    closeModal()
+  }
 
   return (
-    <PageWithSimpleHeader title="Saved Alerts">
+    <PageWithSimpleHeader
+      title="Saved Alerts"
+      right={<SortButton onPress={() => setModalVisible(true)} />}
+    >
       <SavedSearchesList me={me} />
+
+      <FancyModal visible={modalVisible} maxHeight={250} onBackgroundPressed={closeModal}>
+        <FancyModalHeader useXButton onLeftButtonPress={closeModal}>
+          Sort By
+        </FancyModalHeader>
+        {SORT_OPTIONS.map((option) => (
+          <TouchableRow key={option.value} onPress={() => handleSelectOption(option)}>
+            <Flex flexDirection="row" p={2} alignItems="center" justifyContent="space-between">
+              <Flex flex={1} mr={1}>
+                <Text numberOfLines={2}>{option.text}</Text>
+              </Flex>
+              <RadioDot selected={selectedSortValue === option.value} />
+            </Flex>
+          </TouchableRow>
+        ))}
+      </FancyModal>
     </PageWithSimpleHeader>
   )
 }
@@ -49,7 +94,7 @@ export const SavedSearchAlertsListQueryRenderer: React.FC = () => {
       render={renderWithPlaceholder({
         Container: SavedSearchAlertsListFragmentContainer,
         renderPlaceholder: () => (
-          <PageWithSimpleHeader title="Saved Alerts">
+          <PageWithSimpleHeader title="Saved Alerts" right={<SortButton disabled />}>
             <SavedSearchAlertsListPlaceholder />
           </PageWithSimpleHeader>
         ),
