6fb35c256137f01cfbec8b170e027e7ac77c5c7d
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-06-01 17:01:03 -0400

    fix: handle unknown color filter options without crashing

--- src/app/Components/ArtworkFilter/Filters/ColorsOptions.tests.tsx
+++ src/app/Components/ArtworkFilter/Filters/ColorsOptions.tests.tsx
@@ -94,6 +94,36 @@ describe("Colors options screen", () => {
     expect(tree.root.findAllByType(ColorsSwatch)).toHaveLength(aggregation!.counts.length)
   })
 
+  describe("with unrecognized colors", () => {
+    const updatedMockAggregations: Aggregations = [
+      {
+        ...mockAggregations[0],
+        counts: [
+          ...mockAggregations[0].counts,
+          {
+            name: "Stanky Bean",
+            count: 197,
+            value: "stanky-bean",
+          },
+        ],
+      },
+    ]
+    const updatedAggregation = aggregationForFilter(FilterParamName.colors, updatedMockAggregations)
+
+    it("it does not try to render swatches for them", () => {
+      const tree = renderWithWrappers(
+        <MockColorScreen
+          aggregations={updatedMockAggregations}
+          {...getEssentialProps()}
+          initialData={initialState}
+        />
+      )
+
+      const expectedSwatchCount = updatedAggregation!.counts.length
+      expect(tree.root.findAllByType(ColorsSwatch)).toHaveLength(expectedSwatchCount - 1)
+    })
+  })
+
   describe("selecting a color filter", () => {
     it("displays a color filter option when selected", () => {
       const injectedState: ArtworkFiltersState = {
