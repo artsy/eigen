2cbcaa678a23123f36d3379fd9cc6381f59bb0fb
brainbicycle <brian.beckerle@artsymail.com>
2022-06-15 13:39:36 -0300

    fix header bg color

--- src/app/Scenes/Onboarding/OnboardingWebView.tsx
+++ src/app/Scenes/Onboarding/OnboardingWebView.tsx
@@ -1,10 +1,15 @@
 import { StackScreenProps } from "@react-navigation/stack"
 import { ArtsyWebViewPage } from "app/Components/ArtsyWebView"
+import { Flex } from "palette"
 import { OnboardingNavigationStack } from "./Onboarding"
 
 export interface OnboardingWebViewProps
   extends StackScreenProps<OnboardingNavigationStack, "OnboardingWebView"> {}
 
 export const OnboardingWebView: React.FC<OnboardingWebViewProps> = ({ route, navigation }) => {
-  return <ArtsyWebViewPage url={route.params.url} backAction={() => navigation.goBack()} />
+  return (
+    <Flex flex={1} style={{ backgroundColor: "white" }}>
+      <ArtsyWebViewPage url={route.params.url} backAction={() => navigation.goBack()} />
+    </Flex>
+  )
 }
