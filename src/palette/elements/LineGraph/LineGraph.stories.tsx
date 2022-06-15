3339f44a2b3531f90b76642c5bfcfda03c7eeaa8
Mounir Dhahri <soussouheros@gmail.com>
2022-06-15 13:50:07 +0200

    fix types on the story

--- src/palette/elements/LineGraph/LineGraph.stories.tsx
+++ src/palette/elements/LineGraph/LineGraph.stories.tsx
@@ -5,6 +5,6 @@ import { Flex } from ".."
 
 storiesOf("LineGraph", module).add("LineGraph", () => (
   <Flex mx={2} my={2}>
-    <LineGraph />
+    <LineGraph totalLots={50} averagePrice="USD $12,586" />
   </Flex>
 ))
