e2bfe3307584beb98d47d78a9a65b97b0cdaf42a
George Kartalis <g.kartalis7@gmail.com>
2022-06-29 09:53:42 +0200

    refactor(tests): added more

--- src/app/Scenes/Partner/Components/PartnerLocationSection.tests.tsx
+++ src/app/Scenes/Partner/Components/PartnerLocationSection.tests.tsx
@@ -1,7 +1,5 @@
-import { PartnerLocationSection_partner$data } from "__generated__/PartnerLocationSection_partner.graphql"
 import { GlobalStoreProvider } from "app/store/GlobalStore"
-import { renderRelayTree } from "app/tests/renderRelayTree"
-import { CleanRelayFragment } from "app/utils/relayHelpers"
+import { setupTestWrapperTL } from "app/tests/setupTestWrapper"
 import { Theme } from "palette"
 import { graphql } from "react-relay"
 import { PartnerLocationSectionContainer as PartnerLocationSection } from "./PartnerLocationSection"
@@ -20,42 +18,47 @@ const PartnerLocationSectionFixture = {
 }
 
 describe("PartnerLoationSection", () => {
-  const getWrapper = async (partner: CleanRelayFragment<PartnerLocationSection_partner$data>) =>
-    await renderRelayTree({
-      Component: (props: any) => {
-        return (
-          <GlobalStoreProvider>
-            <Theme>
-              <PartnerLocationSection partner={{ ...partner }} {...props} />
-            </Theme>
-          </GlobalStoreProvider>
-        )
-      },
-      query: graphql`
-        query PartnerLocationSectionTestsQuery @raw_response_type {
-          partner(id: "gagosian") {
-            name
-            cities
-            locations: locationsConnection(first: 0) {
-              totalCount
-            }
+  const { renderWithRelay } = setupTestWrapperTL({
+    Component: (props) => (
+      <GlobalStoreProvider>
+        <Theme>
+          <PartnerLocationSection partner={{ ...props.partner }} {...props} />
+        </Theme>
+      </GlobalStoreProvider>
+    ),
+    query: graphql`
+      query PartnerLocationSectionTestsQuery @raw_response_type {
+        partner(id: "gagosian") {
+          name
+          cities
+          locations: locationsConnection(first: 0) {
+            totalCount
           }
         }
-      `,
-      mockData: {
-        partner,
-      },
-    })
+      }
+    `,
+  })
 
   it("renders the locations text correctly", async () => {
     const partnerWithLocations = {
       ...PartnerLocationSectionFixture,
       cities,
     }
-    const wrapper = await getWrapper(partnerWithLocations as any)
-    expect(wrapper.text()).toContain(
-      "Gagosian has 14 locations in New York, Beverly Hills, San Francisco, London, Paris, Le Bourget, Geneva, Basel, Rome, Athens and Central, Hong Kong"
-    )
+
+    const { queryByText } = renderWithRelay({
+      Partner: () => partnerWithLocations,
+    })
+
+    expect(queryByText(/Gagosian has 14 locations in/)).toBeTruthy()
+    expect(
+      queryByText(
+        /New York, Beverly Hills, San Francisco, London, Paris, Le Bourget, Geneva, Basel, Rome, Athens/
+      )
+    ).toBeTruthy()
+    expect(queryByText(/and/)).toBeTruthy()
+    expect(queryByText(/Central, Hong Kong/)).toBeTruthy()
+
+    expect(queryByText("See all location details")).toBeTruthy()
   })
 })
 
