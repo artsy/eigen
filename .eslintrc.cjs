33c9178685442074c0e33bc0c4ddf2551c726d25
Pavlos Vinieratos <pvinis@gmail.com>
2022-06-30 12:09:22 +0300

    more

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,25 @@ module.exports = {
     "@typescript-eslint/no-extra-semi": OFF,
     "@typescript-eslint/ban-types": OFF,
     "@typescript-eslint/no-non-null-assertion": OFF, // for sure a warning, but later
+    "@typescript-eslint/no-inferrable-types": OFF,
+    "no-var": OFF,
+    "@typescript-eslint/prefer-as-const": OFF,
     "no-useless-escape": OFF,
-    // "arrow-parens": [ERR, "always"],
+    "no-prototype-builtins": OFF,
+    "no-case-declarations": OFF,
+    "@typescript-eslint/no-empty-interface": OFF,
+    "@typescript-eslint/no-empty-function": OFF,
+    "no-async-promise-executor": OFF,
+    "@typescript-eslint/ban-ts-comment": OFF,
+    "no-inner-declarations": OFF,
+    "@typescript-eslint/no-explicit-any": OFF, // could be a warning
+    "@typescript-eslint/no-unused-vars": OFF, // could be a warning
+    "@typescript-eslint/no-var-requires": OFF, // could be a warning
+    "no-extra-boolean-cast": OFF, // debatable
+    "no-control-regex": OFF,
+    "no-constant-condition": OFF,
+    "@typescript-eslint/no-non-null-asserted-optional-chain": OFF, // should be an error?
+    "promise/always-return": OFF,
+    "no-empty": OFF,
   },
 }
