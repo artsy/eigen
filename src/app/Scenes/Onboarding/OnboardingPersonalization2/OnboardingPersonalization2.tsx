d4fdcf50b8fdaea874a0883c38d2a43890d2cfe4
George Kartalis <g.kartalis7@gmail.com>
2022-07-20 21:43:40 +0200

    feat: onboarding preparation pr (#7070)
    * feat: add AREnableNewOnboarding feature flag
    
    * feat: OnboardingPersonalizationWelcome screen
    
    * test: add welcome screen test
    
    * feat: add navigation structure
    
    * feat: copy force workflowengine
    
    * feat: copy over config tests and onboarding context from force
    
    * feat: wrap nav container with onboarding provider
    
    * feat: add simple navigation
    
    * feat: give a way to go to home screen for debugging
    
    * chore: removed unused prettier-ignores

---
+++ src/app/Scenes/Onboarding/OnboardingPersonalization2/OnboardingPersonalization2.tsx
@@ -0,0 +1,45 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator, TransitionPresets } from "@react-navigation/stack"
+import { GlobalStore } from "app/store/GlobalStore"
+import { Text } from "palette"
+import { OnboardingProvider } from "./Hooks/useOnboardingContext"
+import { OnboardingPersonalizationWelcome } from "./OnboardingPersonalizationWelcome"
+
+// tslint:disable-next-line:interface-over-type-literal
+export type OnboardingPersonalization2NavigationStack = {
+  OnboardingPersonalizationWelcome: undefined
+  // This is where we want to add the next screen
+  NextScreen: undefined
+}
+
+const StackNavigator = createStackNavigator<OnboardingPersonalization2NavigationStack>()
+
+export const OnboardingPersonalization2 = () => {
+  // this function marks onBoardingState as complete when the flow is done will be changed
+  // when onboarding is fully out to just navigate to home screen
+  const handleDone = () => GlobalStore.actions.auth.setState({ onboardingState: "complete" })
+
+  return (
+    <OnboardingProvider onDone={handleDone}>
+      <NavigationContainer independent>
+        <StackNavigator.Navigator
+          headerMode="screen"
+          screenOptions={{
+            ...TransitionPresets.ModalTransition,
+            headerShown: false,
+          }}
+        >
+          <StackNavigator.Screen
+            name="OnboardingPersonalizationWelcome"
+            component={OnboardingPersonalizationWelcome}
+          />
+          <StackNavigator.Screen
+            // This is where we want to add the next screen
+            name="NextScreen"
+            component={() => <Text>Next Screen</Text>}
+          />
+        </StackNavigator.Navigator>
+      </NavigationContainer>
+    </OnboardingProvider>
+  )
+}
