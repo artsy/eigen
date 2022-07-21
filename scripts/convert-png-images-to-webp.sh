49bab500ea07ef1882f41d52a62b3e03898d2a5c
George Kartalis <g.kartalis7@gmail.com>
2022-07-21 18:06:33 +0200

    feat: add convert-png-images-to-webp to scripts

---
+++ scripts/convert-png-images-to-webp.sh
@@ -0,0 +1,13 @@
+# Converts images from pngs to webps for bundle size reduction
+# it will be run for all png images in the `/images` directory
+
+for f in "images/"*.png; do
+  echo "Converting $f"
+  ff=${f%????}
+  echo "no ext ${ff}"
+
+  cwebp -q 75 -m 6 "$(pwd)/${f}" -o "${ff}.webp"
+
+  echo "Deleting the png: ${f}"
+  rm -rf "$(pwd)/${f}"
+done
