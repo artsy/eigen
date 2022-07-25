f9dcfaf98f2d53f898141615cdd6dadedb60bcdd
araujobarret <araujobarret@gmail.com>
2022-07-25 11:48:26 +0200

    fix: widget test missing prop

--- ios/ArtsyTests/ArtsyWidget/ArtworkTests.swift
+++ ios/ArtsyTests/ArtsyWidget/ArtworkTests.swift
@@ -3,18 +3,18 @@ import XCTest
 class ArtworkTests: XCTestCase {
     // it returns a cleaned up URL from the first image
     func testFirstImageUrlWithArtworkImage() throws {
-        let artworkImage = ArtworkImage(geminiToken: "abc123")
+        let artworkImage = ArtworkImage(geminiToken: "abc123", position: 1)
         let artist = Artist(name: "Sally Something")
         let artwork = Artwork(artist: artist, artworkImages: [artworkImage], id: "abc123", title: "Super Sculpture")
-                
+
         XCTAssertEqual(artwork.firstImageToken, artworkImage.geminiToken)
     }
-    
+
     // it returns a cleaned up URL from the fallback image
     func testFallsBackWithoutArtworkImage() throws {
         let artist = Artist(name: "Sally Something")
         let artwork = Artwork(artist: artist, artworkImages: [], id: "abc123", title: "Super Sculpture")
-                
+
         XCTAssertEqual(artwork.firstImageToken, ArtworkImage.fallback().geminiToken)
     }
 }
