ec200249b5d50048b9dd7604cebfc3fa6cd6428c
Ole <ole.richter91@gmail.com>
2022-07-26 14:22:44 +0200

    fix: Send only valid vortex mediums when fetching price insights

---
+++ src/app/utils/marketPriceInsightHelpers.tsx
@@ -0,0 +1,26 @@
+const VALID_VORTEX_MEDIUMS = [
+  "Painting",
+  "Sculpture",
+  "Photography",
+  "Print",
+  "Drawing, Collage or other Work on Paper",
+  "Mixed Media",
+  "Performance Art",
+  "Installation",
+  "Video/Film/Animation",
+  "Architecture",
+  "Fashion Design and Wearable Art",
+  "Jewelry",
+  "Design/Decorative Art",
+  "Textile Arts",
+  "Posters",
+  "Books and Portfolios",
+  "Other",
+  "Ephemera or Merchandise",
+  "Reproduction",
+  "NFT",
+]
+
+export const getVortexMedium = (medium: string, category: string) => {
+  return VALID_VORTEX_MEDIUMS.includes(medium) ? medium : category
+}
