875f69f6e5576ebc8c52fad8e4b27a1af34d0a0b
Dima Tretyak <itretyak@ya.ru>
2022-07-27 14:02:21 +0300

    feat: add NewBuyNowArtworksRail

---
+++ src/app/Scenes/Sale/Components/NewBuyNowArtworksRail.tsx
@@ -0,0 +1,55 @@
+import { NewBuyNowArtworksRail_sale$data } from "__generated__/NewBuyNowArtworksRail_sale.graphql"
+import { SmallArtworkRail } from "app/Components/ArtworkRail/SmallArtworkRail"
+import { SectionTitle } from "app/Components/SectionTitle"
+import { navigate } from "app/navigation/navigate"
+import { extractNodes } from "app/utils/extractNodes"
+import { compact } from "lodash"
+import { Flex } from "palette"
+import { createFragmentContainer } from "react-relay"
+import { graphql } from "react-relay"
+
+interface NewBuyNowArtworksRailProps {
+  sale: NewBuyNowArtworksRail_sale$data
+}
+
+export const NewBuyNowArtworksRail: React.FC<NewBuyNowArtworksRailProps> = ({ sale }) => {
+  const artworks = extractNodes(sale.promotedSale?.artworksConnection)
+
+  if (!artworks?.length) {
+    return null
+  }
+
+  return (
+    <Flex mt={3} testID="bnmo-rail-wrapper">
+      <Flex mx={2}>
+        <SectionTitle title="Artworks Available to Buy Now" />
+      </Flex>
+      <SmallArtworkRail
+        artworks={compact(artworks)}
+        onPress={(artwork) => {
+          navigate(artwork?.href!)
+        }}
+      />
+    </Flex>
+  )
+}
+
+// TODO: Replace NewBuyNowArtworksRail with BuyNowArtworksRail when AREnableArtworksConnectionForAuction is released
+export const NewBuyNowArtworksRailContainer = createFragmentContainer(NewBuyNowArtworksRail, {
+  sale: graphql`
+    fragment NewBuyNowArtworksRail_sale on Sale
+    @argumentDefinitions(count: { type: "Int", defaultValue: 20 }, cursor: { type: "String" }) {
+      internalID
+      promotedSale {
+        artworksConnection(first: $count, after: $cursor)
+          @connection(key: "NewBuyNowArtworksRail_artworksConnection") {
+          edges {
+            node {
+              ...SmallArtworkRail_artworks
+            }
+          }
+        }
+      }
+    }
+  `,
+})
