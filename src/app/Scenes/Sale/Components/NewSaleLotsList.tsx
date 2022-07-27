f274d71122150a18707c53763fd4b6628d716d2a
Dima Tretyak <itretyak@ya.ru>
2022-07-27 19:58:01 +0300

    feat: fetch fresh data

--- src/app/Scenes/Sale/Components/NewSaleLotsList.tsx
+++ src/app/Scenes/Sale/Components/NewSaleLotsList.tsx
@@ -1,6 +1,6 @@
 import { OwnerType } from "@artsy/cohesion"
-import { NewSaleLotsList_unfilteredArtworks$data } from "__generated__/NewSaleLotsList_unfilteredArtworks.graphql"
-import { NewSaleLotsList_viewer$data } from "__generated__/NewSaleLotsList_viewer.graphql"
+import { NewSaleLotsList_unfilteredArtworks$key } from "__generated__/NewSaleLotsList_unfilteredArtworks.graphql"
+import { NewSaleLotsList_viewer$key } from "__generated__/NewSaleLotsList_viewer.graphql"
 import {
   filterArtworksParams,
   prepareFilterArtworksParamsForInput,
@@ -11,26 +11,29 @@ import { FilteredArtworkGridZeroState } from "app/Components/ArtworkGrids/Filter
 import { InfiniteScrollArtworksGridContainer } from "app/Components/ArtworkGrids/InfiniteScrollArtworksGrid"
 import { PAGE_SIZE } from "app/Components/constants"
 import { extractNodes } from "app/utils/extractNodes"
+import { isFunction } from "lodash"
 import { Box, Flex, Text } from "palette"
 import { useEffect, useRef } from "react"
-import { createPaginationContainer, graphql, RelayPaginationProp } from "react-relay"
+import { graphql, useFragment, usePaginationFragment } from "react-relay"
 import useInterval from "react-use/lib/useInterval"
 
 interface NewSaleLotsListProps {
-  unfilteredArtworks: NewSaleLotsList_unfilteredArtworks$data | null
-  viewer: NewSaleLotsList_viewer$data | null
+  unfilteredArtworks: NewSaleLotsList_unfilteredArtworks$key
+  viewer: NewSaleLotsList_viewer$key
   saleID: string
   saleSlug: string
-  relay: RelayPaginationProp
 }
 
-export const NewSaleLotsList: React.FC<NewSaleLotsListProps> = ({
-  unfilteredArtworks,
-  viewer,
-  saleID,
-  saleSlug,
-  relay,
-}) => {
+export const NewSaleLotsList: React.FC<NewSaleLotsListProps> = ({ saleID, saleSlug, ...rest }) => {
+  const unfilteredArtworks = useFragment(unfilteredArtworksFragment, rest.unfilteredArtworks)
+  const {
+    data: viewer,
+    isLoadingNext,
+    hasNext,
+    loadNext,
+    refetch,
+  } = usePaginationFragment(viewerFragment, rest.viewer)
+
   const appliedFiltersState = ArtworksFiltersStore.useStoreState((state) => state.appliedFilters)
   const filterTypeState = ArtworksFiltersStore.useStoreState((state) => state.filterType)
   const setFiltersCountAction = ArtworksFiltersStore.useStoreActions(
@@ -51,12 +54,6 @@ export const NewSaleLotsList: React.FC<NewSaleLotsListProps> = ({
     (sort) => sort.paramValue === filterParams?.sort
   )
 
-  const nodes = extractNodes(viewer?.artworksConnection)
-  const nodesCountRef = useRef(PAGE_SIZE)
-  nodesCountRef.current = nodes.length
-
-  console.log("[debug] nodes", nodes.length)
-
   const trackClear = () => {
     console.log("[debug] track clear")
   }
@@ -72,24 +69,27 @@ export const NewSaleLotsList: React.FC<NewSaleLotsListProps> = ({
   const refetchVariablesRef = useRef(refetchVariables)
   refetchVariablesRef.current = refetchVariables
 
-  const refetch = () => {
+  const handleRefetch = () => {
     console.log("[debug] refetch")
 
-    if (relay !== undefined) {
-      relay.refetchConnection(
-        PAGE_SIZE,
-        (error) => {
-          if (error) {
-            throw new Error(error.message)
-          }
-        },
-        refetchVariables
-      )
-    }
+    refetch(refetchVariables, {
+      fetchPolicy: "store-and-network",
+      onComplete: (error) => {
+        if (error) {
+          throw new Error(error.message)
+        }
+      },
+    })
   }
 
-  const refetchRef = useRef(refetch)
-  refetchRef.current = refetch
+  const refetchRef = useRef(handleRefetch)
+  refetchRef.current = handleRefetch
+
+  const nodes = extractNodes(viewer?.artworksConnection)
+  const nodesCountRef = useRef(PAGE_SIZE)
+  nodesCountRef.current = nodes.length
+
+  console.log("[debug] nodes", nodes.length)
 
   useEffect(() => {
     setFilterTypeAction("newSaleArtwork")
@@ -114,16 +114,19 @@ export const NewSaleLotsList: React.FC<NewSaleLotsListProps> = ({
 
   useInterval(() => {
     console.log("[debug] refetch by interval", nodesCountRef.current, refetchVariablesRef.current)
+    const variables = {
+      ...refetchVariablesRef.current,
+      count: nodesCountRef.current,
+    }
 
-    relay.refetchConnection(
-      nodesCountRef.current,
-      (error) => {
+    refetch(variables, {
+      fetchPolicy: "store-and-network",
+      onComplete: (error) => {
         if (error) {
           throw new Error(error.message)
         }
       },
-      refetchVariablesRef.current
-    )
+    })
   }, 60 * 1000)
 
   if (totalCount === 0) {
@@ -156,9 +159,17 @@ export const NewSaleLotsList: React.FC<NewSaleLotsListProps> = ({
           contextScreenOwnerType={OwnerType.sale}
           contextScreenOwnerId={saleID}
           contextScreenOwnerSlug={saleSlug}
-          hasMore={relay.hasMore}
-          loadMore={relay.loadMore}
-          isLoading={relay.isLoading}
+          hasMore={() => hasNext}
+          loadMore={(count, callback) =>
+            loadNext(count, {
+              onComplete: (error) => {
+                if (isFunction(callback)) {
+                  callback(error)
+                }
+              },
+            })
+          }
+          isLoading={() => isLoadingNext}
           showLotLabel
           hidePartner
           hideUrgencyTags
@@ -168,78 +179,50 @@ export const NewSaleLotsList: React.FC<NewSaleLotsListProps> = ({
   )
 }
 
-export const NewSaleLotsListContainer = createPaginationContainer(
-  NewSaleLotsList,
-  {
-    unfilteredArtworks: graphql`
-      fragment NewSaleLotsList_unfilteredArtworks on FilterArtworksConnection {
-        counts {
-          followedArtists
-          total
-        }
+const unfilteredArtworksFragment = graphql`
+  fragment NewSaleLotsList_unfilteredArtworks on FilterArtworksConnection {
+    counts {
+      followedArtists
+      total
+    }
 
-        aggregations {
-          slice
-          counts {
-            count
-            name
-            value
-          }
-        }
+    aggregations {
+      slice
+      counts {
+        count
+        name
+        value
       }
-    `,
-    viewer: graphql`
-      fragment NewSaleLotsList_viewer on Viewer
-      @argumentDefinitions(
-        saleID: { type: "ID" }
-        count: { type: "Int", defaultValue: 10 }
-        cursor: { type: "String" }
-        input: { type: "FilterArtworksInput" }
-      ) {
-        artworksConnection(
-          saleID: $saleID
-          first: $count
-          after: $cursor
-          input: $input
-          aggregations: [TOTAL]
-        ) @connection(key: "NewSaleLotsList__artworksConnection") {
-          counts {
-            total
-          }
-          edges {
-            node {
-              id
-            }
-          }
-          ...SaleArtworkList_connection
-          ...InfiniteScrollArtworksGrid_connection
-        }
-      }
-    `,
-  },
-  {
-    getConnectionFromProps(props) {
-      return props.viewer?.artworksConnection
-    },
-    getVariables(_props, { count, cursor }, fragmentVariables) {
-      return {
-        ...fragmentVariables,
-        cursor,
-        count,
-      }
-    },
-    query: graphql`
-      query NewSaleLotsListQuery(
-        $saleID: ID!
-        $count: Int
-        $cursor: String
-        $input: FilterArtworksInput
-      ) {
-        viewer {
-          ...NewSaleLotsList_viewer
-            @arguments(saleID: $saleID, count: $count, cursor: $cursor, input: $input)
-        }
-      }
-    `,
+    }
   }
-)
+`
+
+const viewerFragment = graphql`
+  fragment NewSaleLotsList_viewer on Viewer
+  @argumentDefinitions(
+    saleID: { type: "ID" }
+    count: { type: "Int", defaultValue: 10 }
+    cursor: { type: "String" }
+    input: { type: "FilterArtworksInput" }
+  )
+  @refetchable(queryName: "NewSaleLotsListPaginationQuery") {
+    artworksConnection(
+      saleID: $saleID
+      first: $count
+      after: $cursor
+      input: $input
+      aggregations: [TOTAL, FOLLOWED_ARTISTS]
+    ) @connection(key: "NewSaleLotsList__artworksConnection") {
+      counts {
+        total
+      }
+      edges {
+        node {
+          id
+        }
+      }
+      ...SaleArtworkList_connection
+      ...InfiniteScrollArtworksGrid_connection
+    }
+  }
+`
--- src/app/Scenes/Sale/Sale.tsx
+++ src/app/Scenes/Sale/Sale.tsx
@@ -1,5 +1,5 @@
-import { Sale_sale$data } from "__generated__/Sale_sale.graphql"
-import { Sale_viewer$data } from "__generated__/Sale_viewer.graphql"
+import { Sale_sale$key } from "__generated__/Sale_sale.graphql"
+import { Sale_viewer$key } from "__generated__/Sale_viewer.graphql"
 import { SaleBelowTheFoldNewQuery } from "__generated__/SaleBelowTheFoldNewQuery.graphql"
 import {
   AnimatedArtworkFilterButton,
@@ -8,24 +8,44 @@ import {
 } from "app/Components/ArtworkFilter"
 import { ArtworkFiltersStoreProvider } from "app/Components/ArtworkFilter/ArtworkFilterStore"
 import { DEFAULT_NEW_SALE_ARTWORK_SORT } from "app/Components/ArtworkFilter/Filters/SortOptions"
-import { defaultEnvironment } from "app/relay/createEnvironment"
-import { PlaceholderBox, PlaceholderText, ProvidePlaceholderContext } from "app/utils/placeholders"
 import { ProvideScreenTracking, Schema } from "app/utils/track"
 import { AuctionWebsocketContextProvider } from "app/Websockets/auctions/AuctionSocketContext"
-import _, { times } from "lodash"
-import { Box, Flex, Join, Spacer } from "palette"
 import React, { useState } from "react"
-import { createFragmentContainer, graphql, QueryRenderer } from "react-relay"
+import { graphql, useFragment, useLazyLoadQuery } from "react-relay"
 import RelayModernEnvironment from "relay-runtime/lib/store/RelayModernEnvironment"
-import { NewSaleLotsListContainer } from "./Components/NewSaleLotsList"
-import { COVER_IMAGE_HEIGHT } from "./Components/SaleHeader"
+import { NewSaleLotsList } from "./Components/NewSaleLotsList"
 
-interface Props {
-  viewer: Sale_viewer$data
-  sale: Sale_sale$data
+export const tracks = {
+  screen: (id: string, slug: string) => {
+    return {
+      context_screen: Schema.PageNames.Auction,
+      context_screen_owner_type: Schema.OwnerEntityTypes.Auction,
+      context_screen_owner_id: id,
+      context_screen_owner_slug: slug,
+    }
+  },
 }
 
-export const Sale: React.FC<Props> = ({ viewer, sale }) => {
+export const SaleQueryRenderer: React.FC<{
+  saleID: string
+  environment?: RelayModernEnvironment
+}> = ({ saleID }) => {
+  const data = useLazyLoadQuery<SaleBelowTheFoldNewQuery>(
+    SaleScreenBelowNewQuery,
+    {
+      saleID,
+      saleIDString: saleID,
+      input: {
+        sort: DEFAULT_NEW_SALE_ARTWORK_SORT.paramValue,
+      },
+    },
+    {
+      fetchPolicy: "store-and-network",
+    }
+  )
+  const sale = useFragment<Sale_sale$key>(saleFragment, data.sale)
+  const viewer = useFragment<Sale_viewer$key>(viewerFragment, data.viewer)
+
   const [isFilterArtworksModalVisible, setFilterArtworkModalVisible] = useState(false)
 
   const openFilterArtworksModal = () => {
@@ -34,6 +54,11 @@ export const Sale: React.FC<Props> = ({ viewer, sale }) => {
 
   const closeFilterArtworksModal = () => {
     setFilterArtworkModalVisible(false)
+  }
+
+  if (!sale || !viewer) {
+    // Display loading placeholder
+    return null
   }
 
   const websocketEnabled = !!sale.extendedBiddingIntervalMinutes
@@ -48,8 +73,8 @@ export const Sale: React.FC<Props> = ({ viewer, sale }) => {
         enabled={websocketEnabled}
       >
         <ProvideScreenTracking info={tracks.screen(sale.internalID, sale.slug)}>
-          <NewSaleLotsListContainer
-            unfilteredArtworks={viewer.unfilteredArtworks}
+          <NewSaleLotsList
+            unfilteredArtworks={viewer.unfilteredArtworks!}
             viewer={viewer}
             saleID={sale.internalID}
             saleSlug={sale.slug}
@@ -69,112 +94,35 @@ export const Sale: React.FC<Props> = ({ viewer, sale }) => {
   )
 }
 
-export const tracks = {
-  screen: (id: string, slug: string) => {
-    return {
-      context_screen: Schema.PageNames.Auction,
-      context_screen_owner_type: Schema.OwnerEntityTypes.Auction,
-      context_screen_owner_id: id,
-      context_screen_owner_slug: slug,
-    }
-  },
-}
-
-const SaleContainer = createFragmentContainer(Sale, {
-  sale: graphql`
-    fragment Sale_sale on Sale {
-      internalID
-      slug
-      extendedBiddingIntervalMinutes
-    }
-  `,
-  viewer: graphql`
-    fragment Sale_viewer on Viewer
-    @argumentDefinitions(saleID: { type: "ID" }, input: { type: "FilterArtworksInput" }) {
-      unfilteredArtworks: artworksConnection(
-        saleID: $saleID
-        aggregations: [FOLLOWED_ARTISTS, ARTIST, MEDIUM, TOTAL]
-        first: 0
-      ) {
-        ...NewSaleLotsList_unfilteredArtworks
-      }
-      ...NewSaleLotsList_viewer @arguments(saleID: $saleID, input: $input)
-    }
-  `,
-})
-
-export const SalePlaceholder: React.FC<{}> = () => (
-  <ProvidePlaceholderContext>
-    <PlaceholderBox height={COVER_IMAGE_HEIGHT} width="100%" />
-    <Flex px={2}>
-      <Join separator={<Spacer my={2} />}>
-        <Box>
-          <PlaceholderText width={200 + Math.random() * 100} marginTop={20} />
-          <PlaceholderText width={200 + Math.random() * 100} marginTop={20} />
-          <PlaceholderText width={100 + Math.random() * 100} marginTop={5} />
-        </Box>
-        <Box>
-          <PlaceholderText height={20} width={100 + Math.random() * 100} marginBottom={20} />
-          <PlaceholderBox height={50} width="100%" />
-        </Box>
-        <Box>
-          <PlaceholderText height={20} width={100 + Math.random() * 100} marginBottom={5} />
-          <Flex flexDirection="row" py={2}>
-            {times(3).map((index: number) => (
-              <Flex key={index} marginRight={1}>
-                <PlaceholderBox height={120} width={120} />
-                <PlaceholderText marginTop={20} key={index} width={40 + Math.random() * 80} />
-              </Flex>
-            ))}
-          </Flex>
-        </Box>
-      </Join>
-    </Flex>
-  </ProvidePlaceholderContext>
-)
-
 const SaleScreenBelowNewQuery = graphql`
   query SaleBelowTheFoldNewQuery($saleID: ID, $saleIDString: String!, $input: FilterArtworksInput) {
     sale(id: $saleIDString) {
       ...Sale_sale
     }
-
     viewer {
       ...Sale_viewer @arguments(saleID: $saleID, input: $input)
     }
   }
 `
 
-export const SaleQueryRenderer: React.FC<{
-  saleID: string
-  environment?: RelayModernEnvironment
-}> = ({ saleID, environment }) => {
-  return (
-    <QueryRenderer<SaleBelowTheFoldNewQuery>
-      environment={environment || defaultEnvironment}
-      query={SaleScreenBelowNewQuery}
-      variables={{
-        saleID,
-        saleIDString: saleID,
-        input: {
-          sort: DEFAULT_NEW_SALE_ARTWORK_SORT.paramValue,
-        },
-      }}
-      render={({ props }) => {
-        if (props?.sale && props.viewer) {
-          return <SaleContainer viewer={props.viewer} sale={props.sale} />
-        }
+const saleFragment = graphql`
+  fragment Sale_sale on Sale {
+    internalID
+    slug
+    extendedBiddingIntervalMinutes
+  }
+`
 
-        return (
-          <ProvidePlaceholderContext>
-            <SalePlaceholder />
-          </ProvidePlaceholderContext>
-        )
-      }}
-      cacheConfig={{
-        force: true,
-      }}
-      fetchPolicy="store-and-network"
-    />
-  )
-}
+const viewerFragment = graphql`
+  fragment Sale_viewer on Viewer
+  @argumentDefinitions(saleID: { type: "ID" }, input: { type: "FilterArtworksInput" }) {
+    unfilteredArtworks: artworksConnection(
+      saleID: $saleID
+      aggregations: [FOLLOWED_ARTISTS, ARTIST, MEDIUM, TOTAL]
+      first: 0
+    ) {
+      ...NewSaleLotsList_unfilteredArtworks
+    }
+    ...NewSaleLotsList_viewer @arguments(saleID: $saleID, input: $input)
+  }
+`
