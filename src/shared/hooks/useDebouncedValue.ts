669e263324d11392b1263a3061e21c17cd7b762e
George Kartalis <g.kartalis7@gmail.com>
2022-07-27 22:45:37 +0200

    feat: add useDebouncedValue hook

---
+++ src/shared/hooks/useDebouncedValue.ts
@@ -0,0 +1,28 @@
+import { debounce } from "lodash"
+import { useCallback, useEffect, useState } from "react"
+
+interface UseDebounce {
+  delay?: number
+  callback: (...args: any[]) => void
+}
+
+export const useDebounce = ({ callback, delay = 200 }: UseDebounce) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useCallback(debounce(callback, delay), [callback, delay])
+}
+
+interface UseDebouncedValue<T> {
+  value: T
+  delay?: number
+}
+
+export const useDebouncedValue = <T>({ value, delay = 200 }: UseDebouncedValue<T>) => {
+  const [debouncedValue, setValue] = useState(value)
+  const debouncedSetValue = useDebounce({ callback: setValue, delay })
+
+  useEffect(() => {
+    debouncedSetValue(value)
+  }, [debouncedSetValue, value])
+
+  return { debouncedValue }
+}
