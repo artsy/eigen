938ad157eb3180ef7fdd1b8d4e7a56cbc7a20e6e
Pavlos Vinieratos <pvinis@gmail.com>
2022-08-01 13:21:38 +0300

    ok

--- scripts/circleci-config.ts
+++ scripts/circleci-config.ts
@@ -1,9 +1,8 @@
-#!/usr/bin/env node
+#!/usr/bin/env deno run --allow-write
 
-// @ts-check
-
-import yaml from "js-yaml"
-import fs from "fs"
+// import yaml from "js-yaml"
+import * as yaml from "https://deno.land/x/js_yaml_port/js-yaml.js"
+// import fs from "fs"
 
 const config = {
   version: 2.1,
@@ -26,7 +25,7 @@ const config = {
 
 const output =
   "# This file is auto-generated!\n" +
-  "# Please edit `scripts/circleci-config.mjs`.\n\n" +
+  "# Please edit `scripts/circleci-config.ts`.\n\n" +
   yaml.dump(config, { indent: 2, lineWidth: 200 })
 
-fs.writeFileSync("main.yml", output)
+Deno.writeTextFileSync("main.yml", output)
