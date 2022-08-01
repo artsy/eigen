c4cc6749b598f8c5b4f6d097197c965d575fcbf6
Ole <ole.richter91@gmail.com>
2022-08-01 14:21:09 +0200

    fix: Move career higlights card padding out of the component

--- src/app/Scenes/MyCollection/Screens/Insights/CareerHighlightCard.tsx
+++ src/app/Scenes/MyCollection/Screens/Insights/CareerHighlightCard.tsx
@@ -97,7 +97,7 @@ export const CareerHighlightPromotionalCard: React.FC = () => {
         // TODO: Navigate to detail card
       }}
     >
-      <Flex ml={2} width={200} height={135} backgroundColor="white100" flexDirection="row">
+      <Flex width={200} height={135} backgroundColor="white100" flexDirection="row">
         <Flex p={1} flex={1}>
           <Flex flex={1} justifyContent="center">
             <Text variant="xs">Discover career highlights for your artists.</Text>
