46ae826db264befe5125896fe231ab498b393c03
Dima Tretyak <itretyak@ya.ru>
2022-08-03 14:13:47 +0300

    chore: make HeaderTitle non-clickable

--- src/app/Scenes/ReverseImage/Components/HeaderTitle.tsx
+++ src/app/Scenes/ReverseImage/Components/HeaderTitle.tsx
@@ -9,7 +9,12 @@ export const HeaderTitle: React.FC<HeaderTitleProps> = (props) => {
   const { title } = props
 
   return (
-    <Flex {...StyleSheet.absoluteFillObject} justifyContent="center" alignItems="center">
+    <Flex
+      {...StyleSheet.absoluteFillObject}
+      justifyContent="center"
+      alignItems="center"
+      pointerEvents="none"
+    >
       <Text variant="md" color="white100">
         {title}
       </Text>
