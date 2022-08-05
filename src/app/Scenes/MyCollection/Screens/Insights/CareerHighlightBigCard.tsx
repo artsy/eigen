d7d7b8d605ec9ac588909c88617e5b780138e15e
dariakoko <nargle30@gmail.com>
2022-08-05 18:17:21 +0200

    add scroll

--- src/app/Scenes/MyCollection/Screens/Insights/CareerHighlightBigCard.tsx
+++ src/app/Scenes/MyCollection/Screens/Insights/CareerHighlightBigCard.tsx
@@ -4,7 +4,9 @@ import { CareerHighlightBigCardGroupShow_myCollectionInfo$key } from "__generate
 import { CareerHighlightBigCardReviewed_myCollectionInfo$key } from "__generated__/CareerHighlightBigCardReviewed_myCollectionInfo.graphql"
 import { CareerHighlightBigCardSoloShow_myCollectionInfo$key } from "__generated__/CareerHighlightBigCardSoloShow_myCollectionInfo.graphql"
 import { formatTombstoneText } from "app/Components/ArtistListItem"
-import { EntityHeader, Flex, IconProps, Text } from "palette"
+import { EntityHeader, Flex, IconProps, Text, useSpace } from "palette"
+import { useState } from "react"
+import { FlatList, LayoutChangeEvent, View } from "react-native"
 import { useFragment } from "react-relay"
 import { graphql } from "relay-runtime"
 import { useScreenDimensions } from "shared/hooks"
@@ -14,9 +16,10 @@ const CardHeader: React.FC<{
   count: number
   label: string
   icon: React.ReactElement<IconProps>
-}> = ({ count, label, icon }) => {
+  onLayout?: ((event: LayoutChangeEvent) => void) | undefined
+}> = ({ count, label, icon, onLayout }) => {
   return (
-    <>
+    <Flex onLayout={onLayout}>
       <Flex justifyContent="space-between" flexDirection="row" alignItems="center">
         <Text variant="xxl" color="blue100">
           {count}
@@ -36,7 +39,7 @@ const CardHeader: React.FC<{
       <Text mt={1} mb={4} variant="lg">
         {label}
       </Text>
-    </>
+    </Flex>
   )
 }
 
@@ -50,35 +53,51 @@ export const CareerHighlightBigCardBiennial: React.FC<CareerHighlightBigCardBien
   highlightData,
 }) => {
   const { width } = useScreenDimensions()
+  const space = useSpace()
   const highlightDataBiennial = useFragment(fragmentBiennial, highlightData).biennialInsights
   const count = highlightDataBiennial.length
   const { label, Icon } = getCareerHiglight(type, count)
+  const [headerHeight, setHeaderHeight] = useState<number>(0)
 
   return (
-    <Flex px={2} width={width} onMoveShouldSetResponderCapture={() => true} testID="biennial-card">
+    <View style={{ paddingHorizontal: space(2), width }} testID="biennial-card">
       <CardHeader
         count={count}
         label={label}
         icon={<Icon fill="black100" width={21} height={21} />}
+        onLayout={(event: LayoutChangeEvent) => {
+          const layout = event.nativeEvent.layout
+          setHeaderHeight(layout.height)
+        }}
       />
-      {highlightDataBiennial.map((i: any) => {
-        return (
-          !!i.artist && (
+      <FlatList
+        key={type}
+        scrollEnabled={count > 2}
+        showsVerticalScrollIndicator={false}
+        data={highlightDataBiennial}
+        keyExtractor={(_item, index) => `${type}_${index}`}
+        contentContainerStyle={{ paddingBottom: headerHeight }}
+        renderItem={({ item }: any) => {
+          return (
             <EntityHeader
               mb={2}
-              name={i.artist.name}
+              name={item.artist.name}
               meta={
-                formatTombstoneText(i.artist.nationality, i.artist.birthday, i.artist.deathday) ??
-                undefined
+                formatTombstoneText(
+                  item.artist.nationality,
+                  item.artist.birthday,
+                  item.artist.deathday
+                ) ?? undefined
               }
-              imageUrl={i.artist.image?.url ?? undefined}
-              initials={i.artist.initials ?? undefined}
-              key={i.artist.id}
+              imageUrl={item.artist.image?.url ?? undefined}
+              initials={item.artist.initials ?? undefined}
+              key={`${item.artist.id}_${type}`}
             />
           )
-        )
-      })}
-    </Flex>
+        }}
+        onEndReachedThreshold={1}
+      />
+    </View>
   )
 }
 
@@ -114,35 +133,51 @@ export const CareerHighlightBigCardCollected: React.FC<CareerHighlightBigCardCol
   highlightData,
 }) => {
   const { width } = useScreenDimensions()
+  const space = useSpace()
   const highlightDataCollected = useFragment(fragmentCollected, highlightData).collectedInsights
   const count = highlightDataCollected.length
   const { label, Icon } = getCareerHiglight(type, count)
+  const [headerHeight, setHeaderHeight] = useState<number>(0)
 
   return (
-    <Flex px={2} width={width} onMoveShouldSetResponderCapture={() => true} testID="collected-card">
+    <View style={{ paddingHorizontal: space(2), width }} testID="collected-card">
       <CardHeader
         count={count}
         label={label}
         icon={<Icon fill="black100" width={21} height={21} />}
+        onLayout={(event: LayoutChangeEvent) => {
+          const layout = event.nativeEvent.layout
+          setHeaderHeight(layout.height)
+        }}
       />
-      {highlightDataCollected.map((i: any) => {
-        return (
-          !!i.artist && (
+      <FlatList
+        key={type}
+        scrollEnabled={count > 5}
+        showsVerticalScrollIndicator={false}
+        data={highlightDataCollected}
+        keyExtractor={(_item, index) => `${type}_${index}`}
+        contentContainerStyle={{ paddingBottom: headerHeight }}
+        renderItem={({ item }: any) => {
+          return (
             <EntityHeader
               mb={2}
-              name={i.artist.name}
+              name={item.artist.name}
               meta={
-                formatTombstoneText(i.artist.nationality, i.artist.birthday, i.artist.deathday) ??
-                undefined
+                formatTombstoneText(
+                  item.artist.nationality,
+                  item.artist.birthday,
+                  item.artist.deathday
+                ) ?? undefined
               }
-              imageUrl={i.artist.image?.url ?? undefined}
-              initials={i.artist.initials ?? undefined}
-              key={i.artist.id}
+              imageUrl={item.artist.image?.url ?? undefined}
+              initials={item.artist.initials ?? undefined}
+              key={`${item.artist.id}_${type}`}
             />
           )
-        )
-      })}
-    </Flex>
+        }}
+        onEndReachedThreshold={1}
+      />
+    </View>
   )
 }
 const fragmentCollected = graphql`
@@ -177,40 +212,51 @@ export const CareerHighlightBigCardGroupShow: React.FC<CareerHighlightBigCardGro
   highlightData,
 }) => {
   const { width } = useScreenDimensions()
+  const space = useSpace()
   const highlightDataGroupShow = useFragment(fragmentGroupShow, highlightData).groupShowInsights
   const count = highlightDataGroupShow.length
   const { label, Icon } = getCareerHiglight(type, count)
+  const [headerHeight, setHeaderHeight] = useState<number>(0)
 
   return (
-    <Flex
-      px={2}
-      width={width}
-      onMoveShouldSetResponderCapture={() => true}
-      testID="group-show-card"
-    >
+    <View style={{ paddingHorizontal: space(2), width }} testID="group-show-card">
       <CardHeader
         count={count}
         label={label}
         icon={<Icon fill="black100" width={21} height={21} />}
+        onLayout={(event: LayoutChangeEvent) => {
+          const layout = event.nativeEvent.layout
+          setHeaderHeight(layout.height)
+        }}
       />
-      {highlightDataGroupShow.map((i: any) => {
-        return (
-          !!i.artist && (
+      <FlatList
+        key={type}
+        scrollEnabled={count > 5}
+        showsVerticalScrollIndicator={false}
+        data={highlightDataGroupShow}
+        keyExtractor={(_item, index) => `${type}_${index}`}
+        contentContainerStyle={{ paddingBottom: headerHeight }}
+        renderItem={({ item }: any) => {
+          return (
             <EntityHeader
               mb={2}
-              name={i.artist.name}
+              name={item.artist.name}
               meta={
-                formatTombstoneText(i.artist.nationality, i.artist.birthday, i.artist.deathday) ??
-                undefined
+                formatTombstoneText(
+                  item.artist.nationality,
+                  item.artist.birthday,
+                  item.artist.deathday
+                ) ?? undefined
               }
-              imageUrl={i.artist.image?.url ?? undefined}
-              initials={i.artist.initials ?? undefined}
-              key={i.artist.id}
+              imageUrl={item.artist.image?.url ?? undefined}
+              initials={item.artist.initials ?? undefined}
+              key={`${item.artist.id}_${type}`}
             />
           )
-        )
-      })}
-    </Flex>
+        }}
+        onEndReachedThreshold={1}
+      />
+    </View>
   )
 }
 const fragmentGroupShow = graphql`
@@ -245,35 +291,51 @@ export const CareerHighlightBigCardSoloShow: React.FC<CareerHighlightBigCardSolo
   highlightData,
 }) => {
   const { width } = useScreenDimensions()
+  const space = useSpace()
   const highlightDataSoloShow = useFragment(fragmentSoloShow, highlightData).soloShowInsights
   const count = highlightDataSoloShow.length
   const { label, Icon } = getCareerHiglight(type, count)
+  const [headerHeight, setHeaderHeight] = useState<number>(0)
 
   return (
-    <Flex px={2} width={width} onMoveShouldSetResponderCapture={() => true} testID="solo-show-card">
+    <View style={{ paddingHorizontal: space(2), width }} testID="solo-show-card">
       <CardHeader
         count={count}
         label={label}
         icon={<Icon fill="black100" width={21} height={21} />}
+        onLayout={(event: LayoutChangeEvent) => {
+          const layout = event.nativeEvent.layout
+          setHeaderHeight(layout.height)
+        }}
       />
-      {highlightDataSoloShow.map((i: any) => {
-        return (
-          !!i.artist && (
+      <FlatList
+        key={type}
+        scrollEnabled={count > 5}
+        showsVerticalScrollIndicator={false}
+        data={highlightDataSoloShow}
+        keyExtractor={(_item, index) => `${type}_${index}`}
+        contentContainerStyle={{ paddingBottom: headerHeight }}
+        renderItem={({ item }: any) => {
+          return (
             <EntityHeader
               mb={2}
-              name={i.artist.name}
+              name={item.artist.name}
               meta={
-                formatTombstoneText(i.artist.nationality, i.artist.birthday, i.artist.deathday) ??
-                undefined
+                formatTombstoneText(
+                  item.artist.nationality,
+                  item.artist.birthday,
+                  item.artist.deathday
+                ) ?? undefined
               }
-              imageUrl={i.artist.image?.url ?? undefined}
-              initials={i.artist.initials ?? undefined}
-              key={i.artist.id}
+              imageUrl={item.artist.image?.url ?? undefined}
+              initials={item.artist.initials ?? undefined}
+              key={`${item.artist.id}_${type}`}
             />
           )
-        )
-      })}
-    </Flex>
+        }}
+        onEndReachedThreshold={1}
+      />
+    </View>
   )
 }
 
@@ -309,35 +371,51 @@ export const CareerHighlightBigCardReviewed: React.FC<CareerHighlightBigCardRevi
   highlightData,
 }) => {
   const { width } = useScreenDimensions()
+  const space = useSpace()
   const highlightDataReviewed = useFragment(fragmentReviewed, highlightData).reviewedInsights
   const count = highlightDataReviewed.length
   const { label, Icon } = getCareerHiglight(type, count)
+  const [headerHeight, setHeaderHeight] = useState<number>(0)
 
   return (
-    <Flex px={2} width={width} onMoveShouldSetResponderCapture={() => true} testID="reviewed-card">
+    <View style={{ paddingHorizontal: space(2), width }} testID="reviewed-card">
       <CardHeader
         count={count}
         label={label}
         icon={<Icon fill="black100" width={21} height={21} />}
+        onLayout={(event: LayoutChangeEvent) => {
+          const layout = event.nativeEvent.layout
+          setHeaderHeight(layout.height)
+        }}
       />
-      {highlightDataReviewed.map((i: any) => {
-        return (
-          !!i.artist && (
+      <FlatList
+        key={type}
+        scrollEnabled={count > 5}
+        showsVerticalScrollIndicator={false}
+        data={highlightDataReviewed}
+        keyExtractor={(_item, index) => `${type}_${index}`}
+        contentContainerStyle={{ paddingBottom: headerHeight }}
+        renderItem={({ item }: any) => {
+          return (
             <EntityHeader
               mb={2}
-              name={i.artist.name}
+              name={item.artist.name}
               meta={
-                formatTombstoneText(i.artist.nationality, i.artist.birthday, i.artist.deathday) ??
-                undefined
+                formatTombstoneText(
+                  item.artist.nationality,
+                  item.artist.birthday,
+                  item.artist.deathday
+                ) ?? undefined
               }
-              imageUrl={i.artist.image?.url ?? undefined}
-              initials={i.artist.initials ?? undefined}
-              key={i.artist.id}
+              imageUrl={item.artist.image?.url ?? undefined}
+              initials={item.artist.initials ?? undefined}
+              key={`${item.artist.id}_${type}`}
             />
           )
-        )
-      })}
-    </Flex>
+        }}
+        onEndReachedThreshold={1}
+      />
+    </View>
   )
 }
 
