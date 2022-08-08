2c594031a8bac113e02c1d98e981804f88dc55b3
Pavlos Vinieratos <pvinis@gmail.com>
2022-08-08 17:16:09 +0300

    aha

--- src/app/Components/AnimatableHeader/AnimatableHeaderFlatList.tsx
+++ src/app/Components/AnimatableHeader/AnimatableHeaderFlatList.tsx
@@ -1,17 +1,18 @@
-import { FlatListProps } from "react-native"
+import { FlatList, FlatListProps } from "react-native"
 import Animated from "react-native-reanimated"
 import { useAnimatableHeaderContext } from "./AnimatableHeaderContext"
 import { AnimatableHeaderLargeTitle } from "./AnimatableHeaderLargeTitle"
 import { AnimatableHeaderShadow } from "./AnimatableHeaderShadow"
 
 export const AnimatableHeaderFlatList = <T extends any>(props: FlatListProps<T>) => {
-  const { ListHeaderComponent, ...other } = props
+  const { ListHeaderComponent, ...rest } = props
   const { onScrollForAnimation } = useAnimatableHeaderContext()
 
   return (
     <>
-      <Animated.FlatList
-        {...other}
+      <FlatList<T>
+        {...rest}
+        renderScrollComponent={(p) => <Animated.ScrollView {...p} />}
         scrollEventThrottle={0.0000000001}
         ListHeaderComponent={
           <>
