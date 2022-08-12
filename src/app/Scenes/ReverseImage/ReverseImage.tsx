3a773f8d280445b2bc78b9c90c43ec253f4c92d1
Dima Tretyak <itretyak@ya.ru>
2022-08-12 12:37:17 +0300

    fix: warning message

--- src/app/Scenes/ReverseImage/ReverseImage.tsx
+++ src/app/Scenes/ReverseImage/ReverseImage.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
 import { useEffect } from "react"
-import { StatusBar } from "react-native"
+import { Platform, StatusBar } from "react-native"
 import { ReverseImageArtworkNotFoundScreen } from "./Screens/ArtworkNotFound/ReverseImageArtworkNotFoundScreen"
 import { ReverseImageCameraScreen } from "./Screens/Camera/ReverseImageCamera"
 import { ReverseImageMultipleResultsScreen } from "./Screens/MultipleResults/ReverseImageMultipleResults"
@@ -13,12 +13,18 @@ const Stack = createStackNavigator<ReverseImageNavigationStack>()
 export const ReverseImage = () => {
   useEffect(() => {
     StatusBar.setBarStyle("light-content")
-    StatusBar.setBackgroundColor("transparent")
+
+    if (Platform.OS === "android") {
+      StatusBar.setBackgroundColor("transparent")
+    }
 
     return () => {
       // restore the previous color for the status bar, as on all other screens
       StatusBar.setBarStyle("dark-content")
-      StatusBar.setBackgroundColor("transparent")
+
+      if (Platform.OS === "android") {
+        StatusBar.setBackgroundColor("transparent")
+      }
     }
   }, [])
 
