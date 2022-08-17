62264ad71762f35fad9db8f9cf50a2f8bdfaecba
Laura Bhayani <laura.bhayani@artsymail.com>
2022-08-17 14:11:51 -0400

    replace button animations and smooth out page transitions

--- src/app/Scenes/Onboarding/OnboardingV2/Components/AnimatedFadingPill.tsx
+++ src/app/Scenes/Onboarding/OnboardingV2/Components/AnimatedFadingPill.tsx
@@ -1,39 +1,16 @@
-import { Box, Pill, PillProps } from "palette"
-import { useEffect, useRef, useState } from "react"
-import { Animated } from "react-native"
+import { Pill, PillProps } from "palette"
+import Animated, { FadeInUp, FadeOutDown, Layout } from "react-native-reanimated"
 
 interface FadingPillProps extends PillProps {
   isVisible: boolean
 }
 
-const AnimatedBox = Animated.createAnimatedComponent(Box)
-
 export const FADE_OUT_PILL_ANIMATION_DURATION = 500
 
 export const AnimatedFadingPill: React.FC<FadingPillProps> = ({ isVisible, ...rest }) => {
-  const [visible, setIsVisible] = useState(isVisible)
-  const visibility = useRef(new Animated.Value(1)).current
-
-  useEffect(() => {
-    if (!isVisible) {
-      Animated.timing(visibility, {
-        toValue: 0,
-        duration: FADE_OUT_PILL_ANIMATION_DURATION,
-        useNativeDriver: true,
-      }).start(() => setIsVisible(false))
-    }
-  }, [isVisible])
-
-  const containerStyle = {
-    opacity: visibility.interpolate({
-      inputRange: [0, 1],
-      outputRange: [0, 1],
-    }),
-  }
-
   return (
-    <AnimatedBox style={containerStyle}>
-      {!!visible && <Pill {...rest}>{rest.children}</Pill>}
-    </AnimatedBox>
+    <Animated.View entering={FadeInUp} exiting={FadeOutDown} layout={Layout}>
+      {!!isVisible && <Pill {...rest}>{rest.children}</Pill>}
+    </Animated.View>
   )
 }
