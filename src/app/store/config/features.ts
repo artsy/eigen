f51766425f1688a640ab75b380d5b0805de2da6a
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2022-08-24 13:36:59 +0300

    chore: display debug info for reverse image screen (#7260)
    * chore: display debug info
    
    * refactor: use DevToggle

--- src/app/store/config/features.ts
+++ src/app/store/config/features.ts
@@ -338,6 +338,9 @@ export const devToggles = defineDevToggles({
   DTShowErrorInLoadFailureView: {
     description: "Show error in load failure view",
   },
+  DTShowDebugReverseImageView: {
+    description: "Show debug view for reverse image",
+  },
 })
 
 export const isDevToggle = (name: FeatureName | DevToggleName): name is DevToggleName => {
