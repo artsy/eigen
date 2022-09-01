c7bab12120a3a96272d5a5b544d01b90d9eea583
Ole <ole.richter91@gmail.com>
2022-09-01 16:41:39 +0200

    fix: Adjust MyC insights sell-through rate formatting

--- src/app/utils/marketPriceInsightHelpers.tsx
+++ src/app/utils/marketPriceInsightHelpers.tsx
@@ -89,3 +89,13 @@ export const computeCategoriesForChart = (
 
   return catForChart.map((cat) => ({ name: cat.name, color: cat.color }))
 }
+
+export const formatSellThroughRate = (sellThroughRate: number | null) => {
+  if (sellThroughRate === null) {
+    return ""
+  }
+
+  const sellThroughRatePercentage = (sellThroughRate as number) * 100
+  // show up to 2 decimal places
+  return `${Math.round(sellThroughRatePercentage * 100)}%`
+}
