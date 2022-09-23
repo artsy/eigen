b551a504e2d273e7587c98280824f2282f195231
Dima Tretyak <itretyak@ya.ru>
2022-09-23 12:50:10 +0300

    feat: ability to pass diameter for VisualClueDot

--- src/palette/elements/VisualClue/VisualClueDot.tsx
+++ src/palette/elements/VisualClue/VisualClueDot.tsx
@@ -3,15 +3,20 @@ import { StyleProp, ViewStyle } from "react-native"
 
 const DOT_DIAMETER = 6
 
-export const VisualClueDot: React.FC<{ style?: StyleProp<ViewStyle> }> = ({ style }) => {
+interface VisualClueDotProps {
+  diameter?: number
+  style?: StyleProp<ViewStyle>
+}
+
+export const VisualClueDot: React.FC<VisualClueDotProps> = ({ diameter = DOT_DIAMETER, style }) => {
   const color = useColor()
 
   return (
     <Flex
       style={{
-        height: DOT_DIAMETER,
-        minWidth: DOT_DIAMETER,
-        borderRadius: DOT_DIAMETER / 2,
+        height: diameter,
+        minWidth: diameter,
+        borderRadius: diameter / 2,
         backgroundColor: color("blue100"),
         ...(style as object),
       }}
