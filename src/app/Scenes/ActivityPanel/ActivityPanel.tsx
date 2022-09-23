cd38b7969584dd29f0586c8e488ace861c6426d6
Dima Tretyak <itretyak@ya.ru>
2022-09-23 14:10:47 +0300

    feat: fetch notifications

--- src/app/Scenes/ActivityPanel/ActivityPanel.tsx
+++ src/app/Scenes/ActivityPanel/ActivityPanel.tsx
@@ -1,3 +1,33 @@
-export const ActivityPanel = () => {
-  return null
+import { ActivityPanelQuery } from "__generated__/ActivityPanelQuery.graphql"
+import { Flex, Text } from "palette"
+import { Suspense } from "react"
+import { graphql, useLazyLoadQuery } from "react-relay"
+import { ActivityPanelList } from "./ActivityPanelList"
+
+export const ActivityPanelContent = () => {
+  const queryData = useLazyLoadQuery<ActivityPanelQuery>(ActivityPanelScreenQuery, {})
+
+  return <ActivityPanelList viewer={queryData.viewer} />
 }
+
+export const ActivityPanel = () => {
+  return (
+    <Suspense fallback={<Placeholder />}>
+      <ActivityPanelContent />
+    </Suspense>
+  )
+}
+
+const Placeholder = () => (
+  <Flex flex={1} justifyContent="center" alignItems="center">
+    <Text>Loading</Text>
+  </Flex>
+)
+
+const ActivityPanelScreenQuery = graphql`
+  query ActivityPanelQuery {
+    viewer {
+      ...ActivityPanelList_viewer
+    }
+  }
+`
---
+++ src/app/Scenes/ActivityPanel/ActivityPanelList.tsx
@@ -0,0 +1,73 @@
+import { ActivityPanelList_viewer$key } from "__generated__/ActivityPanelList_viewer.graphql"
+import { ActivityPanelQuery } from "__generated__/ActivityPanelQuery.graphql"
+import { extractNodes } from "app/utils/extractNodes"
+import { Flex, Text } from "palette"
+import { useState } from "react"
+import { FlatList } from "react-native"
+import { graphql, usePaginationFragment } from "react-relay"
+
+interface ActivityPanelListProps {
+  viewer: ActivityPanelList_viewer$key | null
+}
+
+export const ActivityPanelList: React.FC<ActivityPanelListProps> = ({ viewer }) => {
+  const [refreshing, setRefreshing] = useState(false)
+  const { data, hasNext, isLoadingNext, loadNext, refetch } = usePaginationFragment<
+    ActivityPanelQuery,
+    ActivityPanelList_viewer$key
+  >(notificationsConnectionFragment, viewer)
+  const notifications = extractNodes(data?.notificationsConnection)
+
+  const handleLoadMore = () => {
+    if (!hasNext || isLoadingNext) {
+      return
+    }
+
+    loadNext(10)
+  }
+
+  const handleRefresh = () => {
+    setRefreshing(true)
+    refetch(
+      {},
+      {
+        onComplete: () => {
+          setRefreshing(false)
+        },
+      }
+    )
+  }
+
+  return (
+    <FlatList
+      data={notifications}
+      refreshing={refreshing}
+      renderItem={({ item }) => {
+        return (
+          <Flex>
+            <Text>{item.title}</Text>
+            <Text>{item.message}</Text>
+          </Flex>
+        )
+      }}
+      onEndReached={handleLoadMore}
+      onRefresh={handleRefresh}
+    />
+  )
+}
+
+const notificationsConnectionFragment = graphql`
+  fragment ActivityPanelList_viewer on Viewer
+  @refetchable(queryName: "ActivityPanelList_viewerRefetch")
+  @argumentDefinitions(count: { type: "Int", defaultValue: 10 }, after: { type: "String" }) {
+    notificationsConnection(first: $count, after: $after)
+      @connection(key: "ActivityPanelList_notificationsConnection") {
+      edges {
+        node {
+          title
+          message
+        }
+      }
+    }
+  }
+`
