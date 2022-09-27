430270827d8e2e0124d9f68805a1b42db095664b
Dima Tretyak <itretyak@ya.ru>
2022-09-27 20:13:47 +0300

    refactor: small imrpovement

--- src/app/Scenes/Activity/util/getDateLabel.tests.ts
+++ src/app/Scenes/Activity/util/getDateLabel.tests.ts
@@ -1,29 +1,31 @@
 import { DateTime } from "luxon"
-import { getDateLabel } from "../util"
+import { getDateLabel } from "./getDateLabel"
 
 describe("getDateLabel", () => {
   it("returns 'Today' label", () => {
-    const result1 = getDateLabel(DateTime.utc().minus({ hours: 1 }))
+    const result1 = getDateLabel(DateTime.utc().minus({ hours: 1 }).toString())
     expect(result1).toEqual("Today")
 
-    const result2 = getDateLabel(DateTime.utc().minus({ hours: 5 }))
+    const result2 = getDateLabel(DateTime.utc().minus({ hours: 5 }).toString())
     expect(result2).toEqual("Today")
 
-    const result3 = getDateLabel(DateTime.utc().minus({ hours: 23, minutes: 59, seconds: 59 }))
+    const result3 = getDateLabel(
+      DateTime.utc().minus({ hours: 23, minutes: 59, seconds: 59 }).toString()
+    )
     expect(result3).toEqual("Today")
   })
 
   it("returns 'x days ago' label", () => {
-    const result1 = getDateLabel(DateTime.utc().minus({ days: 1 }))
+    const result1 = getDateLabel(DateTime.utc().minus({ days: 1 }).toString())
     expect(result1).toEqual("1 days ago")
 
-    const result2 = getDateLabel(DateTime.utc().minus({ days: 2 }))
+    const result2 = getDateLabel(DateTime.utc().minus({ days: 2 }).toString())
     expect(result2).toEqual("2 days ago")
 
-    const result3 = getDateLabel(DateTime.utc().minus({ days: 31 }))
+    const result3 = getDateLabel(DateTime.utc().minus({ days: 31 }).toString())
     expect(result3).toEqual("31 days ago")
 
-    const result4 = getDateLabel(DateTime.utc().minus({ days: 365 }))
+    const result4 = getDateLabel(DateTime.utc().minus({ days: 365 }).toString())
     expect(result4).toEqual("365 days ago")
   })
 })
