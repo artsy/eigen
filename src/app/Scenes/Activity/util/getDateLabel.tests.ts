a7a7b5920a99660c639fd61c563b90ec518df8a0
Dima Tretyak <itretyak@ya.ru>
2022-09-30 18:36:00 +0300

    fix: display "Yesterday" label for activity items (#7423)
    * fix: display Yesterday label
    
    * fix: failed test
    
    * refactor: small improvement

--- src/app/Scenes/Activity/util/getDateLabel.tests.ts
+++ src/app/Scenes/Activity/util/getDateLabel.tests.ts
@@ -1,4 +1,4 @@
-import { DateTime } from "luxon"
+import { DateTime, Settings } from "luxon"
 import { getDateLabel } from "./getDateLabel"
 
 describe("getDateLabel", () => {
@@ -14,9 +14,6 @@ describe("getDateLabel", () => {
   })
 
   it("returns 'x days ago' label", () => {
-    const result1 = getDateLabel(DateTime.now().minus({ days: 1 }).toString())
-    expect(result1).toEqual("1 day ago")
-
     const result2 = getDateLabel(DateTime.now().minus({ days: 2 }).toString())
     expect(result2).toEqual("2 days ago")
 
@@ -25,5 +22,19 @@ describe("getDateLabel", () => {
 
     const result4 = getDateLabel(DateTime.now().minus({ days: 365 }).toString())
     expect(result4).toEqual("365 days ago")
+  })
+
+  it("returns 'Yesterday' label", () => {
+    const result1 = getDateLabel(DateTime.now().minus({ days: 1 }).toString())
+    expect(result1).toEqual("Yesterday")
+
+    // 00:00:00 today
+    const startOfToday = DateTime.now().startOf("day").toMillis()
+    Settings.now = () => startOfToday
+
+    // 23:59:59 yesterday
+    const endOfPrevDay = DateTime.now().endOf("day").minus({ days: 1 })
+    const result2 = getDateLabel(endOfPrevDay.toString())
+    expect(result2).toEqual("Yesterday")
   })
 })
