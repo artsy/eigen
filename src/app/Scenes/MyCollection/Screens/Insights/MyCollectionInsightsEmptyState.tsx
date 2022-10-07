4fc6df17deab62f185a8ace9c942e3272a755379
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-10-07 19:12:45 +0200

    fix: update Insights empty state
    # Conflicts:
    #	src/app/Scenes/MyCollection/Screens/Insights/MyCollectionInsightsEmptyState.tsx

--- src/app/Scenes/MyCollection/Screens/Insights/MyCollectionInsightsEmptyState.tsx
+++ src/app/Scenes/MyCollection/Screens/Insights/MyCollectionInsightsEmptyState.tsx
@@ -1,7 +1,8 @@
+import { ZeroState } from "app/Components/States/ZeroState"
 import { navigate, popToRoot } from "app/navigation/navigate"
 import { Tab } from "app/Scenes/MyProfile/MyProfileHeaderMyCollectionAndSavedWorks"
 import { useFeatureFlag } from "app/store/GlobalStore"
-import { Button, Flex, Text, useSpace } from "palette"
+import { Button, useSpace } from "palette"
 import { Image } from "react-native"
 
 export const MyCollectionInsightsEmptyState = () => {
@@ -13,37 +14,30 @@ export const MyCollectionInsightsEmptyState = () => {
     : require("images/my-collection-insights-empty-state.webp")
 
   return (
-    <Flex px={2} testID="my-collection-insights-empty-state">
-      <Text variant="md" textAlign="center">
-        Gain deeper knowledge of your collection
-      </Text>
-      <Text variant="xs" color="black60" textAlign="center">
-        Get free market insights about the artists you collect.
-      </Text>
-      <Image
-        source={image}
-        resizeMode="contain"
-        style={{
-          alignSelf: "center",
-          height: 120,
-          width: 327,
-          marginVertical: space(2),
-        }}
-      />
-      <Button
-        block
-        onPress={() =>
-          navigate("my-collection/artworks/new", {
-            passProps: {
-              mode: "add",
-              source: Tab.insights,
-              onSuccess: popToRoot,
-            },
-          })
-        }
-      >
-        Upload Artwork
-      </Button>
-    </Flex>
+    <ZeroState
+      bigTitle="Gain deeper knowledge of your collection"
+      subtitle="Get free market insights about the artists you collect."
+      image={
+        <Image
+          source={image}
+          resizeMode="contain"
+          style={{ alignSelf: "center", marginVertical: space(2) }}
+        />
+      }
+      callToAction={
+        <>
+          <Button
+            block
+            onPress={() => {
+              navigate("my-collection/artworks/new", {
+                passProps: { mode: "add", source: Tab.insights, onSuccess: popToRoot },
+              })
+            }}
+          >
+            Upload Artwork
+          </Button>
+        </>
+      }
+    />
   )
 }
