3dd63425ce9dfdc361a172bda6d7d5d78ea6bf12
brainbicycle <brian.beckerle@artsymail.com>
2022-10-17 15:37:32 -0400

    add functions for writing and reading actions

--- src/app/utils/AuthLog/logAuthActions.ts
+++ src/app/utils/AuthLog/logAuthActions.ts
@@ -1,3 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+const authActions: Array<{ label: string; data: string }> = []
+const authStorageKey = "AUTH_LOG_KEY"
+
 export function logAuthAction(label: string, data: string) {
   console.log("Logging auth action", { label, data })
+  authActions.concat([{ label, data }])
+}
+
+export function storeAuthActions() {
+  const authJSON = JSON.stringify(authActions)
+  AsyncStorage.setItem(authStorageKey, authJSON)
+}
+
+export async function readAuthActions() {
+  const authJSON = await AsyncStorage.getItem(authStorageKey)
+  return authJSON
 }
