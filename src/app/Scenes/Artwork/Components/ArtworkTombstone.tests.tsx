4cb293f420abfac4258c1e0d593e75dbe5331d74
George Kartalis <g.kartalis7@gmail.com>
2022-10-17 18:47:44 +0200

    refactor: redesign artworkTombstone

--- src/app/Scenes/Artwork/Components/ArtworkTombstone.tests.tsx
+++ src/app/Scenes/Artwork/Components/ArtworkTombstone.tests.tsx
@@ -1,11 +1,9 @@
 import { fireEvent } from "@testing-library/react-native"
 import { ArtworkTombstone_artwork$data } from "__generated__/ArtworkTombstone_artwork.graphql"
 import { ArtworkFixture } from "app/__fixtures__/ArtworkFixture"
-import { LegacyNativeModules } from "app/NativeModules/LegacyNativeModules"
 import { navigate } from "app/navigation/navigate"
 import { renderWithWrappers } from "app/tests/renderWithWrappers"
 import { ArtworkTombstone } from "./ArtworkTombstone"
-import { CertificateAuthenticityModal } from "./CertificateAuthenticityModal"
 
 describe("ArtworkTombstone", () => {
   it("renders fields correctly", () => {
@@ -14,16 +12,6 @@ describe("ArtworkTombstone", () => {
     )
 
     expect(queryByText("Hello im a title, 1992")).toBeTruthy()
-    expect(queryByText("Painting")).toBeTruthy()
-    expect(queryByText("Edition 100/200")).toBeTruthy()
-
-    expect(queryByText(/This work is part of/)).toHaveTextContent(
-      "This work is part of a limited edition set."
-    )
-
-    expect(queryByText(/This work includes a/)).toHaveTextContent(
-      "This work includes a Certificate of Authenticity."
-    )
 
     expect(queryByText("Lot 8")).toBeNull()
     expect(queryByText("Cool Auction")).toBeNull()
@@ -49,63 +37,6 @@ describe("ArtworkTombstone", () => {
     fireEvent.press(getByText(/Andy Warhol/))
 
     expect(navigate).toHaveBeenCalledWith("/artist/andy-warhol")
-  })
-
-  it("redirects to attribution class faq page when attribution class is clicked", () => {
-    const { getByText } = renderWithWrappers(<ArtworkTombstone artwork={artworkTombstoneArtwork} />)
-
-    fireEvent.press(getByText("a limited edition set"))
-
-    expect(navigate).toHaveBeenCalledWith("/artwork-classifications")
-  })
-
-  it("shows the authenticity modal when Certificate of Authenticity is tapped", () => {
-    const { getByText, UNSAFE_getByType } = renderWithWrappers(
-      <ArtworkTombstone artwork={artworkTombstoneArtwork} />
-    )
-
-    expect(UNSAFE_getByType(CertificateAuthenticityModal)).toHaveProp("visible", false)
-
-    fireEvent.press(getByText("Certificate of Authenticity"))
-
-    expect(UNSAFE_getByType(CertificateAuthenticityModal)).toHaveProp("visible", true)
-  })
-
-  it("closes the authenticity modal when Certificate of Authenticity", () => {
-    const { getByText, UNSAFE_getByType, getByTestId } = renderWithWrappers(
-      <ArtworkTombstone artwork={artworkTombstoneArtwork} />
-    )
-
-    fireEvent.press(getByText("Certificate of Authenticity"))
-
-    expect(UNSAFE_getByType(CertificateAuthenticityModal)).toHaveProp("visible", true)
-
-    fireEvent.press(getByTestId("fancy-modal-header-right-button"))
-
-    expect(UNSAFE_getByType(CertificateAuthenticityModal)).toHaveProp("visible", false)
-  })
-
-  describe("for a user not in the US", () => {
-    it("renders dimensions in centimeters", () => {
-      LegacyNativeModules.ARCocoaConstantsModule.CurrentLocale = "fr_FR"
-
-      const { queryByText } = renderWithWrappers(
-        <ArtworkTombstone artwork={artworkTombstoneArtwork} />
-      )
-
-      expect(queryByText("38.1 × 50.8 cm")).toBeTruthy()
-    })
-  })
-
-  describe("for a US based user", () => {
-    it("renders dimensions in inches", () => {
-      LegacyNativeModules.ARCocoaConstantsModule.CurrentLocale = "en_US"
-      const { queryByText } = renderWithWrappers(
-        <ArtworkTombstone artwork={artworkTombstoneArtwork} />
-      )
-
-      expect(queryByText("15 × 20 in")).toBeTruthy()
-    })
   })
 
   describe("for an artwork with more than 3 artists", () => {
@@ -208,25 +139,12 @@ describe("ArtworkTombstone", () => {
       expect(queryByText(/Alex Katz/)).toBeNull()
       expect(queryByText(/Pablo Picasso/)).toBeNull()
     })
-
-    it("shows follow button", () => {
-      const { queryByText } = renderWithWrappers(
-        <ArtworkTombstone artwork={oneArtistArtworkData} />
-      )
-      expect(queryByText("Follow")).toBeTruthy()
-    })
   })
 
   describe("for an artwork with no artists but a cultural maker", () => {
     it("renders artist name", () => {
       const { queryByText } = renderWithWrappers(<ArtworkTombstone artwork={noArtistArtworkData} />)
       expect(queryByText("18th century American")).toBeTruthy()
-    })
-
-    it("doesn't show follow button", () => {
-      const { queryByText } = renderWithWrappers(<ArtworkTombstone artwork={noArtistArtworkData} />)
-
-      expect(queryByText("Follow")).toBeNull()
     })
   })
 })
@@ -263,19 +181,7 @@ const artworkTombstoneArtwork: ArtworkTombstone_artwork$data = {
       " $fragmentSpreads": null as any,
     },
   ],
-  cultural_maker: null,
-  dimensions: {
-    in: "15 × 20 in",
-    cm: "38.1 × 50.8 cm",
-  },
-  edition_of: "Edition 100/200",
-  attributionClass: {
-    shortArrayDescription: ["This work is part of", "a limited edition set"],
-  },
-  certificateOfAuthenticity: {
-    label: "A Certificate",
-    __typename: "ArtworkInfoRow",
-  },
+  culturalMaker: null,
   " $fragmentType": "ArtworkTombstone_artwork",
 }
 
@@ -326,5 +232,5 @@ const oneArtistArtworkData = {
 const noArtistArtworkData = {
   ...artworkTombstoneArtwork,
   artists: [],
-  cultural_maker: "18th century American",
+  culturalMaker: "18th century American",
 }
