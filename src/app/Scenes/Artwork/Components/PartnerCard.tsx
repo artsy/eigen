7b7284b3854c1ea3ac1b2d301499be5744b2771c
George Kartalis <g.kartalis7@gmail.com>
2022-10-20 14:12:31 +0200

    refactor: convert partnercard to a funct component

--- src/app/Scenes/Artwork/Components/PartnerCard.tsx
+++ src/app/Scenes/Artwork/Components/PartnerCard.tsx
@@ -8,57 +8,59 @@ import { TouchableWithoutFeedback } from "react-native"
 import { createFragmentContainer, graphql, RelayProp } from "react-relay"
 import { Questions } from "./Questions"
 
-interface Props {
+interface PartnerCardProps {
   artwork: PartnerCard_artwork$data
   relay: RelayProp
   shouldShowQuestions?: boolean
 }
 
-export class PartnerCard extends React.Component<Props> {
-  handleTap(href: string) {
-    navigateToPartner(href)
+export const PartnerCard: React.FC<PartnerCardProps> = ({ artwork, shouldShowQuestions }) => {
+  const handleTap = (href: string) => navigateToPartner(href)
+
+  const partner = artwork.partner!
+  const galleryOrBenefitAuction =
+    artwork.sale && (artwork.sale.isBenefit || artwork.sale.isGalleryAuction)
+
+  if (partner.type === "Auction House" || galleryOrBenefitAuction) {
+    return null
   }
 
-  render() {
-    const { artwork, shouldShowQuestions } = this.props
-    const partner = artwork.partner!
-    const galleryOrBenefitAuction =
-      artwork.sale && (artwork.sale.isBenefit || artwork.sale.isGalleryAuction)
-    if (partner.type === "Auction House" || galleryOrBenefitAuction) {
-      return null
-    }
-    let locationNames = null
-    const imageUrl = partner.profile && partner.profile.icon ? partner.profile.icon.url : null
-    if (partner.cities) {
-      locationNames = get(partner, (p) => limitWithCount(p.cities as any, 2), [])!.join(", ")
-    }
-    const showPartnerType =
-      partner.type === "Institution" ||
-      partner.type === "Gallery" ||
-      partner.type === "Institutional Seller"
-    const partnerTypeDisplayText = partner.type === "Gallery" ? partner.type : "Institution"
-    return (
-      <Flex>
-        {!!showPartnerType && (
-          <>
-            <Text variant="md">{partnerTypeDisplayText}</Text>
-            <Spacer my={1} />
-          </>
-        )}
-        <TouchableWithoutFeedback onPress={this.handleTap.bind(this, partner.href!)}>
-          <EntityHeader
-            name={partner.name!}
-            href={(partner.isDefaultProfilePublic && partner.href) || undefined}
-            meta={locationNames || undefined}
-            imageUrl={imageUrl || undefined}
-            initials={partner.initials || undefined}
-          />
-        </TouchableWithoutFeedback>
-        <Spacer mt={2} />
-        {!!shouldShowQuestions && <Questions artwork={artwork} />}
-      </Flex>
-    )
+  let locationNames = null
+
+  const imageUrl = partner.profile && partner.profile.icon ? partner.profile.icon.url : null
+
+  if (partner.cities) {
+    locationNames = get(partner, (p) => limitWithCount(p.cities as any, 2), [])!.join(", ")
   }
+
+  const showPartnerType =
+    partner.type === "Institution" ||
+    partner.type === "Gallery" ||
+    partner.type === "Institutional Seller"
+
+  const partnerTypeDisplayText = partner.type === "Gallery" ? partner.type : "Institution"
+
+  return (
+    <Flex>
+      {!!showPartnerType && (
+        <>
+          <Text variant="md">{partnerTypeDisplayText}</Text>
+          <Spacer my={1} />
+        </>
+      )}
+      <TouchableWithoutFeedback onPress={() => handleTap(partner.href!)}>
+        <EntityHeader
+          name={partner.name!}
+          href={(partner.isDefaultProfilePublic && partner.href) || undefined}
+          meta={locationNames || undefined}
+          imageUrl={imageUrl || undefined}
+          initials={partner.initials || undefined}
+        />
+      </TouchableWithoutFeedback>
+      <Spacer mt={2} />
+      {!!shouldShowQuestions && <Questions artwork={artwork} />}
+    </Flex>
+  )
 }
 
 export const PartnerCardFragmentContainer = createFragmentContainer(PartnerCard, {
