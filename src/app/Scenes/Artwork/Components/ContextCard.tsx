a64a0369ec03a8563cc0672ce1a6413473afae7d
George Kartalis <g.kartalis7@gmail.com>
2022-10-26 18:43:08 +0200

    refactor: contextCard

--- src/app/Scenes/Artwork/Components/ContextCard.tsx
+++ src/app/Scenes/Artwork/Components/ContextCard.tsx
@@ -1,6 +1,5 @@
 import { ContextCard_artwork$data } from "__generated__/ContextCard_artwork.graphql"
 import { navigate } from "app/navigation/navigate"
-import { track as _track } from "app/utils/track"
 import { Avatar, Box, Flex, Text } from "palette"
 import { TouchableWithoutFeedback } from "react-native"
 import { createFragmentContainer, graphql, RelayProp } from "react-relay"
@@ -10,40 +9,18 @@ interface ContextCardProps {
   relay: RelayProp
 }
 
-export const ContextCard: React.FC<ContextCardProps> = (props) => {
-  let meta = null
-  let header = ""
-  let imageUrl = ""
-
-  const {
-    artwork: { context },
-  } = props
-
-  // Don't display a context card ever if the work is in a non-auction sale as the existing
-  // sale page is not built for this purpose.
-  if (context && context.__typename === "Sale" && context.isAuction === false) {
+export const ContextCard: React.FC<ContextCardProps> = ({ artwork: { context } }) => {
+  if (context?.__typename !== "Sale" || context?.isAuction === false) {
     return null
   }
 
-  if (context) {
-    switch (context.__typename) {
-      case "Sale":
-        header = "Auction"
-        meta = context.formattedStartDateTime
-        imageUrl = context.coverImage && context.coverImage.url ? context.coverImage.url : ""
-        break
-      default:
-        return null
-    }
-  } else {
-    return null
-  }
+  const imageUrl = context?.coverImage?.url
 
   return (
     <>
       <Box mb={2}>
         <Text variant="md" color="black100">
-          {header}
+          Auction
         </Text>
       </Box>
       <Flex>
@@ -56,7 +33,7 @@ export const ContextCard: React.FC<ContextCardProps> = (props) => {
             )}
             <Flex justifyContent="space-between" width={0} flexGrow={1}>
               <Text variant="sm-display">{context.name!}</Text>
-              <Text variant="sm">{meta}</Text>
+              <Text variant="sm">{context.formattedStartDateTime}</Text>
             </Flex>
           </Flex>
         </TouchableWithoutFeedback>
@@ -68,13 +45,11 @@ export const ContextCard: React.FC<ContextCardProps> = (props) => {
 export const ContextCardFragmentContainer = createFragmentContainer(ContextCard, {
   artwork: graphql`
     fragment ContextCard_artwork on Artwork {
-      id
+      internalID
       context {
         __typename
         ... on Sale {
-          id
           name
-          isLiveOpen
           href
           formattedStartDateTime
           isAuction
