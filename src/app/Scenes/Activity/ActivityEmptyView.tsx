5737c9a20d7fdea7966465e2c0058cf16d1e2a80
Dima Tretyak <itretyak@ya.ru>
2022-11-03 20:19:37 +0300

    feat: add ability to refetch activities when empty state

--- src/app/Scenes/Activity/ActivityEmptyView.tsx
+++ src/app/Scenes/Activity/ActivityEmptyView.tsx
@@ -1,8 +1,4 @@
-import { useStickyTabPageContext } from "app/Components/StickyTabPage/StickyTabPageContext"
-import { StickyTabPageFlatListContext } from "app/Components/StickyTabPage/StickyTabPageFlatList"
 import { apostrophe, Flex, quoteLeft, quoteRight, Spacer, Text } from "palette"
-import { useContext } from "react"
-import Animated from "react-native-reanimated"
 import { NotificationType } from "./types"
 
 interface ActivityEmptyViewProps {
@@ -22,25 +18,15 @@ const entityByType: Record<NotificationType, { title: string; message: string }>
 }
 
 export const ActivityEmptyView: React.FC<ActivityEmptyViewProps> = ({ type }) => {
-  const { staticHeaderHeight } = useStickyTabPageContext()
-  const { tabSpecificContentHeight } = useContext(StickyTabPageFlatListContext)
-  const headerHeight = Animated.add(staticHeaderHeight ?? 0, tabSpecificContentHeight ?? 0)
   const entity = entityByType[type]
 
   return (
-    <Flex flex={1} accessibilityLabel="Activities are empty">
-      <Animated.View
-        style={{
-          height: headerHeight ?? 0,
-        }}
-      />
-      <Flex flex={1} justifyContent="center" mx={2}>
-        <Text textAlign="center">{entity.title}</Text>
-        <Spacer mt={2} />
-        <Text variant="xs" color="black60" textAlign="center">
-          {entity.message}
-        </Text>
-      </Flex>
+    <Flex mx={2} accessibilityLabel="Activities are empty">
+      <Text textAlign="center">{entity.title}</Text>
+      <Spacer mt={2} />
+      <Text variant="xs" color="black60" textAlign="center">
+        {entity.message}
+      </Text>
     </Flex>
   )
 }
