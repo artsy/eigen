83ab4d9baa3bdbe00c9f3478e7996c4f4d439a28
brainbicycle <brian.beckerle@artsymail.com>
2022-11-08 16:40:34 -0500

    instrument facebook auth, needs cleanup

--- src/app/store/AuthModel.ts
+++ src/app/store/AuthModel.ts
@@ -2,6 +2,7 @@ import { ActionType, AuthService, CreatedAccount } from "@artsy/cohesion"
 import { appleAuth } from "@invertase/react-native-apple-authentication"
 import CookieManager from "@react-native-cookies/cookies"
 import { GoogleSignin } from "@react-native-google-signin/google-signin"
+import { logAuthAction } from "app/auth/logAuthActions"
 import { OAuthProvider } from "app/auth/types"
 import * as RelayCache from "app/relay/RelayCache"
 import { isArtsyEmail } from "app/utils/general"
@@ -485,14 +486,21 @@ export const getAuthModel = (): AuthModel => ({
     }
   }),
   authFacebook: thunk(async (actions, options) => {
+    logAuthAction("AUTH FACEBOOK 1", "authFacebook")
+
     return await new Promise<AuthPromiseResolveType>(async (resolve, reject) => {
       try {
+        logAuthAction("AUTH FACEBOOK 2 - before loginWithPermissions", "")
         const { declinedPermissions, isCancelled } = await LoginManager.logInWithPermissions([
           "public_profile",
           "email",
         ])
 
+        const data1 = JSON.stringify({ declinedPermissions, isCancelled })
+        logAuthAction("AUTH FACEBOOK 2 - after loginWithPermissions", data1)
+
         if (declinedPermissions?.includes("email")) {
+          logAuthAction("AUTH FACEBOOK 2 - error", "no email permission")
           reject(
             new AuthError("Please allow the use of email to continue.", "Email Permission Declined")
           )
@@ -500,16 +508,23 @@ export const getAuthModel = (): AuthModel => ({
         }
         const accessToken = !isCancelled && (await AccessToken.getCurrentAccessToken())
         if (!accessToken) {
+          logAuthAction("AUTH FACEBOOK 2 - error", "no access token")
           return
         }
 
         const responseFacebookInfoCallback = async (error: any | null, result: any | null) => {
+          logAuthAction("AUTH FACEBOOK callback 1", JSON.stringify({ error, result }))
           if (error) {
+            logAuthAction("AUTH FACEBOOK callback 1 - error", JSON.stringify({ error }))
             reject(new AuthError("Error fetching facebook data", error))
             return
           }
 
           if (!result || !result.email) {
+            logAuthAction(
+              "AUTH FACEBOOK callback 1 - error - no result or email ",
+              JSON.stringify({ result })
+            )
             reject(
               new AuthError(
                 "There is no email associated with your Facebook account. Please log in using your email and password instead."
@@ -519,6 +534,7 @@ export const getAuthModel = (): AuthModel => ({
           }
 
           if (options.signInOrUp === "signUp") {
+            logAuthAction("AUTH FACEBOOK callback - signup option 1", JSON.stringify({ options }))
             const resultGravitySignUp = await actions.signUp({
               email: result.email as string,
               name: result.name as string,
@@ -526,6 +542,11 @@ export const getAuthModel = (): AuthModel => ({
               oauthProvider: "facebook",
               agreedToReceiveEmails: options.agreedToReceiveEmails,
             })
+
+            logAuthAction(
+              "AUTH FACEBOOK callback - signup option 2",
+              JSON.stringify({ resultGravitySignUp })
+            )
 
             if (resultGravitySignUp.success) {
               resolve({ success: true })
@@ -543,6 +564,8 @@ export const getAuthModel = (): AuthModel => ({
           }
 
           if (options.signInOrUp === "signIn") {
+            logAuthAction("AUTH FACEBOOK callback - signin option 1", JSON.stringify({ options }))
+
             // we need to get X-ACCESS-TOKEN before actual sign in
             const resultGravityAccessToken = await actions.gravityUnauthenticatedRequest({
               path: `/oauth2/access_token`,
@@ -560,7 +583,16 @@ export const getAuthModel = (): AuthModel => ({
               },
             })
 
+            logAuthAction(
+              "AUTH FACEBOOK callback - signin option 2",
+              JSON.stringify({ resultGravityAccessToken })
+            )
+
             if (resultGravityAccessToken.status === 201) {
+              logAuthAction(
+                "AUTH FACEBOOK callback - signin option 201 3",
+                JSON.stringify({ resultGravityAccessToken })
+              )
               const { access_token: userAccessToken } = await resultGravityAccessToken.json() // here's the X-ACCESS-TOKEN we needed now we can get user's email and sign in
               const { email } = await actions.getUser({ accessToken: userAccessToken })
               const resultGravitySignIn = await actions.signIn({
@@ -569,6 +601,11 @@ export const getAuthModel = (): AuthModel => ({
                 accessToken: accessToken.accessToken,
                 onSignIn: options.onSignIn,
               })
+
+              logAuthAction(
+                "AUTH FACEBOOK callback - signin option 201 4",
+                JSON.stringify({ resultGravitySignIn })
+              )
 
               if (resultGravitySignIn) {
                 resolve({ success: true })
@@ -579,10 +616,13 @@ export const getAuthModel = (): AuthModel => ({
               }
             } else {
               const res = await resultGravityAccessToken.json()
+              logAuthAction("AUTH FACEBOOK callback - signin option !201", JSON.stringify({ res }))
               showError(res, reject, "facebook")
             }
           }
         }
+
+        logAuthAction("AUTH FACEBOOK callback - before info request", "")
 
         // get info from facebook
         const infoRequest = new GraphRequest(
@@ -597,6 +637,7 @@ export const getAuthModel = (): AuthModel => ({
           },
           responseFacebookInfoCallback
         )
+        logAuthAction("AUTH FACEBOOK callback - making info request", JSON.stringify(infoRequest))
         new GraphRequestManager().addRequest(infoRequest).start()
       } catch (e) {
         if (e instanceof Error) {
