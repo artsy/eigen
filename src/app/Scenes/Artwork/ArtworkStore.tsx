402709b9f4b2b9131ab0480a239ca5b1fe85543a
Dima Tretyak <itretyak@ya.ru>
2022-11-09 20:25:54 +0300

    feat: add ArtworkStore

---
+++ src/app/Scenes/Artwork/ArtworkStore.tsx
@@ -0,0 +1,31 @@
+import { action, Action, createContextStore, State } from "easy-peasy"
+
+interface ArtworkModel {
+  auctionState: string | null
+  selectedEditionId: string | null
+  setAuctionState: Action<this, string | null>
+  setSelectedEditionId: Action<this, string | null>
+}
+
+export type ArtworkState = State<ArtworkModel>
+
+const artworkModel: ArtworkModel = {
+  auctionState: null,
+  selectedEditionId: null,
+  setAuctionState: action((state, payload) => {
+    state.auctionState = payload
+  }),
+  setSelectedEditionId: action((state, payload) => {
+    state.selectedEditionId = payload
+  }),
+}
+
+export const ArtworkStore = createContextStore<ArtworkModel>(
+  (initialData: ArtworkModel) => ({
+    ...artworkModel,
+    ...initialData,
+  }),
+  { name: "SavedSearchStore" }
+)
+
+export const ArtworkStoreProvider = ArtworkStore.Provider
