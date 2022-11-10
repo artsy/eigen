a2701d370fca23fa54afb1bdb9e8c95f3d1a1661
dariakoko <nargle30@gmail.com>
2022-11-10 10:48:03 +0100

    tests

--- src/app/Scenes/Home/Components/HomeFeedOnboardingCard.tests.tsx
+++ src/app/Scenes/Home/Components/HomeFeedOnboardingCard.tests.tsx
@@ -1,5 +1,5 @@
 import { fireEvent } from "@testing-library/react-native"
-import { switchTab } from "app/navigation/navigate"
+import { navigate, switchTab } from "app/navigation/navigate"
 import { renderWithWrappers } from "app/tests/renderWithWrappers"
 import "react-native"
 import { HomeFeedOnboardingCard } from "./HomeFeedOnboardingCard"
@@ -7,23 +7,38 @@ import { HomeFeedOnboardingRailItemProps } from "./HomeFeedOnboardingRail"
 
 describe("HomeFeedOnboardingCard", () => {
   it("renders without throwing an error", () => {
-    const { getByText } = renderWithWrappers(<HomeFeedOnboardingCard item={mockItem} />)
+    const { getByText } = renderWithWrappers(<HomeFeedOnboardingCard item={mockItemSell} />)
 
-    expect(getByText(mockItem.title)).toBeTruthy()
+    expect(getByText(mockItemSell.title)).toBeTruthy()
   })
 
-  it("button works", async () => {
-    const { getByText } = renderWithWrappers(<HomeFeedOnboardingCard item={mockItem} />)
-    fireEvent.press(getByText(mockItem.button))
+  it("sell card's button works", async () => {
+    const { getByText } = renderWithWrappers(<HomeFeedOnboardingCard item={mockItemSell} />)
+    fireEvent.press(getByText(mockItemSell.button))
     expect(switchTab).toHaveBeenCalledWith("sell")
+  })
+
+  it("MyC card's button works", async () => {
+    const { getByText } = renderWithWrappers(<HomeFeedOnboardingCard item={mockItemMyC} />)
+    fireEvent.press(getByText(mockItemMyC.button))
+    expect(navigate).toHaveBeenCalledWith("/")
   })
 })
 
-const mockItem: HomeFeedOnboardingRailItemProps = {
+const mockItemSell: HomeFeedOnboardingRailItemProps = {
   shouldShow: true,
   type: "SWA",
   title: "Sell with Artsy ",
   subtitle: "Get the best sales options for artworks from your collection.",
   image: require("images/homefeed-my-collection-inboarding-1.webp"),
   button: "Learn more",
+}
+
+const mockItemMyC: HomeFeedOnboardingRailItemProps = {
+  shouldShow: true,
+  type: "MyC",
+  title: "Manage your collection",
+  subtitle: "Get powerful market insights about artworks you own.",
+  image: require("images/homefeed-my-collection-inboarding-0.webp"),
+  button: "Explore My Collection",
 }
