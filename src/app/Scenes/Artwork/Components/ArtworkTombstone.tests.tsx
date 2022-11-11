faad53d7816b7efe9203646f6a76baf6d84aa072
Dima Tretyak <itretyak@ya.ru>
2022-11-11 15:20:09 +0300

    fix: failed tests

--- src/app/Scenes/Artwork/Components/ArtworkTombstone.tests.tsx
+++ src/app/Scenes/Artwork/Components/ArtworkTombstone.tests.tsx
@@ -1,7 +1,7 @@
 import { screen } from "@testing-library/react-native"
 import { ArtworkTombstone_artwork$data } from "__generated__/ArtworkTombstone_artwork.graphql"
 import { ArtworkFixture } from "app/__fixtures__/ArtworkFixture"
-import { __globalStoreTestUtils__ } from "app/store/GlobalStore"
+import { __globalStoreTestUtils__, GlobalStoreProvider } from "app/store/GlobalStore"
 import { flushPromiseQueue } from "app/tests/flushPromiseQueue"
 import { setupTestWrapperTL } from "app/tests/setupTestWrapper"
 import { Theme } from "palette"
@@ -14,7 +14,9 @@ describe("ArtworkTombstone", () => {
   const { renderWithRelay } = setupTestWrapperTL({
     Component: (props) => (
       <Theme>
-        <ArtworkTombstoneFragmentContainer {...props} />
+        <GlobalStoreProvider>
+          <ArtworkTombstoneFragmentContainer {...props} />
+        </GlobalStoreProvider>
       </Theme>
     ),
     query: graphql`
