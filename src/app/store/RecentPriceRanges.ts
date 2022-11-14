87fa3f9adbfa3583fc2b71df48c009b805b5737a
Dima Tretyak <itretyak@ya.ru>
2022-11-14 14:36:44 +0300

    feat: don't save the same price range again

--- src/app/store/RecentPriceRanges.ts
+++ src/app/store/RecentPriceRanges.ts
@@ -10,9 +10,17 @@ export interface RecentPriceRangesModel {
 
 export const getRecentPriceRanges = (): RecentPriceRangesModel => ({
   recentPriceRanges: [],
-  addNewPriceRange: action((state, priceRange) => {
+  addNewPriceRange: action((state, newPriceRange) => {
+    const hasSamePriceRange = state.recentPriceRanges.find(
+      (priceRange) => priceRange === newPriceRange
+    )
+
+    if (hasSamePriceRange) {
+      return
+    }
+
     const prevPriceRanges = state.recentPriceRanges.slice(0, MAX_SHOWN_RECENT_PRICE_RANGES - 1)
-    state.recentPriceRanges = [priceRange, ...prevPriceRanges]
+    state.recentPriceRanges = [newPriceRange, ...prevPriceRanges]
   }),
 })
 
