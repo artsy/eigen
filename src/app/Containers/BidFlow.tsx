8ba97ae97c207c630390a04f9b5bb90194187212
Dima Tretyak <itretyak@ya.ru>
2022-11-18 20:05:31 +0300

    chore: remove top safe area insets

--- src/app/Containers/BidFlow.tsx
+++ src/app/Containers/BidFlow.tsx
@@ -1,6 +1,5 @@
 import NavigatorIOS from "app/utils/__legacy_do_not_use__navigator-ios-shim"
 import { View } from "react-native"
-import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { TimeOffsetProvider } from "../Components/Bidding/Context/TimeOffsetProvider"
 import { SelectMaxBidQueryRenderer } from "../Components/Bidding/Screens/SelectMaxBid"
 
@@ -8,7 +7,7 @@ export const BidFlow: React.FC<
   Omit<React.ComponentProps<typeof SelectMaxBidQueryRenderer>, "navigator">
 > = (props) => {
   return (
-    <View style={{ flex: 1, paddingTop: useSafeAreaInsets().top }}>
+    <View style={{ flex: 1 }}>
       <TimeOffsetProvider>
         <NavigatorIOS initialRoute={{ component: SelectMaxBidQueryRenderer, passProps: props }} />
       </TimeOffsetProvider>
