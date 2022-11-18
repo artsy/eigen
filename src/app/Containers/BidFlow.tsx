9dfb4a7696c5fb8d49558fff89b18e940d341274
Dima Tretyak <itretyak@ya.ru>
2022-11-18 20:34:49 +0300

    refactor: use top insets only for android

--- src/app/Containers/BidFlow.tsx
+++ src/app/Containers/BidFlow.tsx
@@ -1,13 +1,16 @@
 import NavigatorIOS from "app/utils/__legacy_do_not_use__navigator-ios-shim"
-import { View } from "react-native"
+import { Platform, View } from "react-native"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { TimeOffsetProvider } from "../Components/Bidding/Context/TimeOffsetProvider"
 import { SelectMaxBidQueryRenderer } from "../Components/Bidding/Screens/SelectMaxBid"
 
 export const BidFlow: React.FC<
   Omit<React.ComponentProps<typeof SelectMaxBidQueryRenderer>, "navigator">
 > = (props) => {
+  const insets = useSafeAreaInsets()
+
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, paddingTop: Platform.OS === "android" ? insets.top : 0 }}>
       <TimeOffsetProvider>
         <NavigatorIOS initialRoute={{ component: SelectMaxBidQueryRenderer, passProps: props }} />
       </TimeOffsetProvider>
