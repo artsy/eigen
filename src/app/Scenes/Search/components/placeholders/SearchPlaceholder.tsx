576de80813a1d4cac5fe8cc3070a7136e02f9c3e
Dima Tretyak <itretyak@ya.ru>
2022-11-23 17:47:01 +0300

    chore: update placeholder

--- src/app/Scenes/Search/components/placeholders/SearchPlaceholder.tsx
+++ src/app/Scenes/Search/components/placeholders/SearchPlaceholder.tsx
@@ -3,19 +3,23 @@ import {
   PlaceholderRaggedText,
   PlaceholderText,
   ProvidePlaceholderContext,
+  RandomWidthPlaceholderText,
+  useMemoizedRandom,
 } from "app/utils/placeholders"
 import { times } from "lodash"
-import { Box, Flex, Spacer } from "palette"
+import { Box, Flex, Join, Spacer } from "palette"
+import { useDisplayCuratedCollections } from "../../useDisplayCuratedCollections"
 
 export const SearchPlaceholder: React.FC = () => {
+  const displayCuratedCollections = useDisplayCuratedCollections()
+
   return (
     <ProvidePlaceholderContext>
-      <Box p={2}>
+      <Box p={2} pb={0}>
         <PlaceholderBox height={46} />
-        <Spacer mt={2} />
-        <Spacer mt={1} />
+        <Spacer mt={30} />
         <PlaceholderText width="50%" height={20} />
-        <Spacer mt={2} />
+        <Spacer mt={1} />
         {times(4).map((index) => (
           <Flex key={`search-placeholder-${index}`} flexDirection="row" mb={2}>
             <PlaceholderBox width={40} height={40} />
@@ -25,6 +29,24 @@ export const SearchPlaceholder: React.FC = () => {
           </Flex>
         ))}
       </Box>
+
+      {!!displayCuratedCollections && (
+        <Box mx={2}>
+          <PlaceholderText width="50%" height={20} />
+          <Flex flexDirection="row" mt={2}>
+            <Join separator={<Spacer ml={1} />}>
+              {times(3 + useMemoizedRandom() * 10).map((index) => (
+                <Flex key={index}>
+                  <PlaceholderBox key={index} height={180} width={295} />
+                  <Spacer mt={1} />
+                  <PlaceholderText width={120} height={20} />
+                  <RandomWidthPlaceholderText minWidth={30} maxWidth={90} height={20} />
+                </Flex>
+              ))}
+            </Join>
+          </Flex>
+        </Box>
+      )}
     </ProvidePlaceholderContext>
   )
 }
