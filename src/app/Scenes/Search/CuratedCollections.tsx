e20e0965729751b6b4b12f384aa4127022b1bfa5
Dima Tretyak <itretyak@ya.ru>
2022-11-25 16:40:27 +0300

    refactor: convert to rail

--- src/app/Scenes/Search/CuratedCollections.tsx
+++ src/app/Scenes/Search/CuratedCollections.tsx
@@ -1,6 +1,8 @@
 import { CuratedCollections_collections$key } from "__generated__/CuratedCollections_collections.graphql"
+import { CardRailFlatList } from "app/Components/Home/CardRailFlatList"
 import { SectionTitle } from "app/Components/SectionTitle"
-import { Join, Spacer } from "palette"
+import { compact } from "lodash"
+import { Box, Spacer } from "palette"
 import { graphql, useFragment } from "react-relay"
 import { CuratedCollectionItem } from "./CuratedCollectionItem"
 
@@ -10,20 +12,26 @@ interface CuratedCollectionsProps {
 
 export const CuratedCollections: React.FC<CuratedCollectionsProps> = ({ collections }) => {
   const data = useFragment(CuratedCollectionsFragment, collections)
+  const filledCollections = compact(data.collections)
+
+  if (filledCollections.length === 0) {
+    return null
+  }
 
   return (
     <>
-      <SectionTitle title="Artsy Curated Collections" />
+      <Box mx={2}>
+        <SectionTitle title="Artsy Curated Collections" />
+      </Box>
 
-      <Join separator={<Spacer mb={2} />}>
-        {data.collections?.map((collection, position) => (
-          <CuratedCollectionItem
-            key={collection!.internalID}
-            collection={collection!}
-            position={position}
-          />
-        ))}
-      </Join>
+      <CardRailFlatList
+        data={filledCollections}
+        keyExtractor={(node) => node.internalID}
+        renderItem={({ item, index }) => {
+          return <CuratedCollectionItem collection={item} position={index} />
+        }}
+        ItemSeparatorComponent={() => <Spacer ml={1} />}
+      />
     </>
   )
 }
