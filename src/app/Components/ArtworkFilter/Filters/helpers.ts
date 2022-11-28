800a232f00aaeb26f108fc1a453ae8a57f2507c1
Dima Tretyak <itretyak@ya.ru>
2022-11-28 21:30:18 +0300

    feat(FX-4479): empty state for recent price ranges (#7739)
    * refactor: add parsePriceRange helper
    
    * refactore: recent price ranges
    
    * refactor: add RecentPriceRangesList component
    
    * feat: add empty state
    
    * chore: add/update test cases
    
    * chore: small improvement
    
    * chore: add a11y things
    
    Co-authored-by: George Kartalis <g.kartalis7@gmail.com>
    
    Co-authored-by: George Kartalis <g.kartalis7@gmail.com>

--- src/app/Components/ArtworkFilter/Filters/helpers.ts
+++ src/app/Components/ArtworkFilter/Filters/helpers.ts
@@ -9,6 +9,8 @@ export interface Range {
   max: Numeric
 }
 
+export type PriceRange = Numeric[]
+
 const ONE_IN_TO_CM = 2.54
 // to be removed in favor of userPrefs.metric
 export const IS_USA = LegacyNativeModules.ARCocoaConstantsModule.CurrentLocale === "en_US"
@@ -109,4 +111,15 @@ export const parsePriceRangeLabel = (min: Numeric, max: Numeric) => {
   }
 
   return `${parsedMin}${parsedMax}`
+}
+
+// TODO: Refactor and combine it with parseRange
+export const parsePriceRange = (range: string = "*-*"): PriceRange => {
+  return range.split("-").map((s) => {
+    if (s === "*") {
+      return s
+    }
+
+    return parseInt(s, 10)
+  })
 }
