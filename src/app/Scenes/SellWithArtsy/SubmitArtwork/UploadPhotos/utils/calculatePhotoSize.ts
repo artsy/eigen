a4b03b147f4c671817cba89556ff63a99d1f1d60
Mounir Dhahri <soussouheros@gmail.com>
2022-11-30 15:25:23 +0100

    fix: address review comment

--- src/app/Scenes/SellWithArtsy/SubmitArtwork/UploadPhotos/utils/calculatePhotoSize.ts
+++ src/app/Scenes/SellWithArtsy/SubmitArtwork/UploadPhotos/utils/calculatePhotoSize.ts
@@ -1,7 +1,7 @@
 import { Photo } from "app/Scenes/SellWithArtsy/SubmitArtwork/UploadPhotos/validation"
 import { transformBytesToSize } from "app/utils/transformBytesToSize"
 
-export const totalSizeLimitInBytes = 30000000
+const TOTAL_SIZE_LIMIT_IN_BYTES = 30000000
 
 // calculates a photos size from Bytes to unit and return updated photo
 export const calculateSinglePhotoSize = (photo: Photo): Photo => {
@@ -32,5 +32,5 @@ export const getPhotosSize = (photos: Photo[]) => {
 export const isSizeLimitExceeded = (photos: Photo[]): boolean => {
   const allPhotosSize = getPhotosSize(photos)
 
-  return allPhotosSize > totalSizeLimitInBytes
+  return allPhotosSize > TOTAL_SIZE_LIMIT_IN_BYTES
 }
