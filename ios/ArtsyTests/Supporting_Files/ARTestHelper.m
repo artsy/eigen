31b72b4797fa4fc1876dd6f12e81426406e232c8
Pavlos Vinieratos <pvinis@gmail.com>
2022-12-01 16:14:52 +0000

    feat: staging indicator (and major dep update) (#7751)
    * add
    
    * Update src/app/App.tsx
    
    * Update src/app/utils/DynamicIslandStagingIndicator.tsx
    
    * Update src/app/utils/DynamicIslandStagingIndicator.tsx
    
    * whatever
    
    * aha
    
    * try
    
    * not needed

--- ios/ArtsyTests/Supporting_Files/ARTestHelper.m
+++ ios/ArtsyTests/Supporting_Files/ARTestHelper.m
@@ -16,8 +16,8 @@
 {
     NSOperatingSystemVersion version = [NSProcessInfo processInfo].operatingSystemVersion;
 
-    NSAssert(version.majorVersion == 16 && version.minorVersion == 0,
-             @"The tests should be run on iOS 16.0, not %ld.%ld", version.majorVersion, version.minorVersion);
+    NSAssert(version.majorVersion == 16 && version.minorVersion == 1,
+             @"The tests should be run on iOS 16.1, not %ld.%ld", version.majorVersion, version.minorVersion);
 
     CGSize nativeResolution = [UIScreen mainScreen].nativeBounds.size;
     NSAssert([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPhone && CGSizeEqualToSize(nativeResolution, CGSizeMake(1179, 2556)),
