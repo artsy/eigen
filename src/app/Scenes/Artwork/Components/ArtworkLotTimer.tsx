9c127f61e633b0b45a5a2ddc7f4aeed27f8fe31b
George Kartalis <g.kartalis7@gmail.com>
2022-12-02 17:57:47 +0100

    fix: fix fix

--- src/app/Scenes/Artwork/Components/ArtworkLotTimer.tsx
+++ src/app/Scenes/Artwork/Components/ArtworkLotTimer.tsx
@@ -179,7 +179,7 @@ const RenderCountdown: React.FC<AuctionWebsocketWrapperProps> = ({
   // display the timer and progress bar only when duration is positive and lot is biddable
   const shouldShowTimer = !!isBiddableInAuction && moment.duration(duration).milliseconds() > 0
 
-  const isBiddingClosed = !artwork.saleArtwork?.endedAt || timerState === AuctionTimerState.CLOSED
+  const isBiddingClosed = !!artwork.saleArtwork?.endedAt || timerState === AuctionTimerState.CLOSED
 
   return (
     <>
