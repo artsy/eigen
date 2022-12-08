338ba11363caabb0743a2b8726ada4f7a7e446cd
Ole <ole.richter91@gmail.com>
2022-12-08 18:55:18 +0100

    fix: Always use local image for My Collection images when available (#7820)
    * fix: Always use local image for My Collection images when available
    
    * add comment

--- src/app/Scenes/MyCollection/Components/MyCollectionImageView.tsx
+++ src/app/Scenes/MyCollection/Components/MyCollectionImageView.tsx
@@ -54,20 +54,8 @@ export const MyCollectionImageView: React.FC<MyCollectionImageViewProps> = ({
   }, [])
 
   const renderImage = () => {
-    // prioritise imageURL
-    if (imageURL) {
-      const targetURL = imageURL.replace(":version", "square")
-      return (
-        <OpaqueImageView
-          testID="Image-Remote"
-          imageURL={targetURL}
-          retryFailedURLs
-          height={imageHeight}
-          width={imageWidth}
-          aspectRatio={aspectRatio}
-        />
-      )
-    } else if (localImage) {
+    // Use local image if it exists to avoid images being displayed with a wrong aspect ratio while they are being processed by Gemini
+    if (localImage) {
       return (
         <RNImage
           testID="Image-Local"
@@ -93,6 +81,18 @@ export const MyCollectionImageView: React.FC<MyCollectionImageViewProps> = ({
           source={{
             uri: localImageConsignments.path,
           }}
+        />
+      )
+    } else if (imageURL) {
+      const targetURL = imageURL.replace(":version", "square")
+      return (
+        <OpaqueImageView
+          testID="Image-Remote"
+          imageURL={targetURL}
+          retryFailedURLs
+          height={imageHeight}
+          width={imageWidth}
+          aspectRatio={aspectRatio}
         />
       )
     } else {
