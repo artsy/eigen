1d56c6cd48acea4ec488e3cbd9388d8bfc795373
Ole <ole.richter91@gmail.com>
2023-01-03 19:02:00 +0100

    chore: Clean up AREnableTrove feature flag (#7917)

--- src/app/store/config/features.ts
+++ src/app/store/config/features.ts
@@ -67,11 +67,6 @@ export const features = defineFeatures({
     readyForRelease: false,
     description: "Enable Saved Addresses",
   },
-  AREnableTrove: {
-    readyForRelease: true,
-    description: "Enable Trove in homepage",
-    echoFlagKey: "AREnableTrove",
-  },
   AREnableShowsRail: {
     readyForRelease: true,
     description: "Enable Shows in homepage",
