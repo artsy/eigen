9f2c587633a07b614d51753edc237c95653ebb0a
Sam Raj <rajsam003@gmail.com>
2023-01-17 18:02:46 +0100

    return

---
+++ src/app/Scenes/ArtQuiz/ArtQuizArtworks.tsx
@@ -0,0 +1,12 @@
+import { Flex, Text } from "palette"
+import { Modal } from "react-native"
+
+export const ArtQuizArtworks = () => {
+  return (
+    <Flex>
+      <Modal>
+        <Text>ArtQuizArtworks Modal</Text>
+      </Modal>
+    </Flex>
+  )
+}
