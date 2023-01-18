1bd611ce30743505c0c868f4fecd438b4e9e367b
Mounir Dhahri <soussouheros@gmail.com>
2023-01-18 17:12:37 +0100

    chore: address review comment

--- src/app/navigation/NavStack.tsx
+++ src/app/navigation/NavStack.tsx
@@ -116,7 +116,7 @@ export const NavStack: React.FC<{
         options={(props) => {
           const focusedRoute = findFocusedRoute(props.navigation.getState())
           const params = focusedRoute?.params as any
-          const screenOptions = modules[params.moduleName as AppModule].options.screenOptions
+          const screenOptions = modules[params.moduleName as AppModule]?.options?.screenOptions
 
           return { ...screenOptions }
         }}
