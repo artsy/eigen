5ef61986fece6959a16e23c704cc3825ab31b5fc
Christopher Pappas <damassi.pappas@gmail.com>
2023-01-18 11:28:29 -0800

    address feedback

--- src/app/utils/DynamicIslandStagingIndicator.tsx
+++ src/app/utils/DynamicIslandStagingIndicator.tsx
@@ -1,5 +1,4 @@
 import { useIsStaging } from "app/store/GlobalStore"
-import { SystemDeviceInfo } from "app/system/SystemDeviceInfo"
 import { Flex } from "palette"
 import DeviceInfo from "react-native-device-info"
 
@@ -12,7 +11,7 @@ export const DynamicIslandStagingIndicator = () => {
 
   let rect: { top: number; left: number } | undefined
 
-  switch (SystemDeviceInfo.getDeviceNameSync()) {
+  switch (DeviceInfo.getDeviceNameSync()) {
     case "iPhone 14 Pro":
       rect = { top: 9.5, left: 132 }
       break
