08b313c36199a9de8cbcfe02c833e733d9ca8126
Dima Tretyak <itretyak@ya.ru>
2023-01-23 17:13:57 +0300

    chore: check displayUnreadActivityPanelIndicator

--- src/app/Scenes/Home/Components/HomeHeader.tsx
+++ src/app/Scenes/Home/Components/HomeHeader.tsx
@@ -3,15 +3,15 @@ import { ArtsyLogoIcon, Box, Flex, Spacer } from "palette"
 import { ActivityIndicator } from "./ActivityIndicator"
 
 export const HomeHeader: React.FC = () => {
-  const hasNotifications = GlobalStore.useAppState(
-    (state) => state.bottomTabs.sessionState.unreadCounts.unreadActivityPanelNotifications > 0
+  const shouldUnreadActivityPanelIndicator = GlobalStore.useAppState(
+    (state) => state.bottomTabs.sessionState.displayUnreadActivityPanelIndicator
   )
 
   return (
     <Box mb={1} mt={2}>
       <Flex alignItems="center">
         <ArtsyLogoIcon scale={0.75} />
-        <ActivityIndicator hasNotifications={hasNotifications} />
+        <ActivityIndicator hasNotifications={shouldUnreadActivityPanelIndicator} />
       </Flex>
       <Spacer mb="1" />
     </Box>
