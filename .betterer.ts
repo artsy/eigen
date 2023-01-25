bbf03aa8b1820d4bc2ec9b7bc85443c8e4ccecf4
Pavlos Vinieratos <pvinis@gmail.com>
2023-01-25 13:24:56 +0200

    feat: add betterer (#8045)
    * this is great!
    
    * try ci
    
    * class components
    
    * unmock
    
    * works
    
    * hm
    
    * this
    
    * move stuff around
    
    * Delete betterer-check.yml
    
    * strictness
    
    * cache and sort
    
    * ci
    
    * not now
    
    * fixed
    
    * Update .circleci/config.yml

---
+++ .betterer.ts
@@ -0,0 +1,48 @@
+import { regexp } from "@betterer/regexp"
+import { BettererFileTest } from "@betterer/betterer"
+
+const typescriptFiles = ["./src/**/*.ts", "./src/**/*.tsx"]
+const typescriptTestFiles = ["./src/**/*.tests.ts", "./src/**/*.tests.tsx"]
+const imageExtensionsToAvoid = ["png", "jpg", "jpeg"]
+
+export default {
+  "Stop using useAnimatedValue, use useSharedValue instead!": () =>
+    regexp(/useAnimatedValue\(/).include(typescriptFiles),
+
+  "Stop using moment, use luxon instead!": () =>
+    regexp(/from "(moment|moment-timezone)"/).include(typescriptFiles),
+
+  "Finish our strictnes migration!": () =>
+    regexp(/Unsafe legacy code 🚨 Please delete this/).include(typescriptFiles),
+
+  // "Avoid non-webp images!": () =>
+  //   countNonWebpImages().include([`./images/**/*.{${imageExtensionsToAvoid.join(",")}}`]),
+
+  "Avoid using test-renderer!": () =>
+    regexp(/renderWithWrappersLEGACY.* from ".*renderWithWrappers"/).include(typescriptTestFiles),
+
+  "Remove all relay unmocks!": () => regexp(/unmock\("react-relay"\)/).include(typescriptTestFiles),
+
+  "Fix all STRICTNESS_MIGRATION!": () => regexp(/STRICTNESS_MIGRATION/).include(typescriptFiles),
+
+  "Avoid having skipped tests!": () =>
+    regexp(/(fdescribe\(|describe.only\(|fit\(|xit\(|it.only\(|it.skip\()/).include(
+      typescriptTestFiles
+    ),
+
+  "Avoid using class components!": () =>
+    regexp(/extends (React\.)?Component/).include(typescriptFiles),
+}
+
+// const countNonWebpImages = () =>
+//   new BettererFileTest(async (filePaths, fileTestResult) => {
+//     filePaths.forEach((filePath) => {
+//       // the file contents don't matter
+//       const file = fileTestResult.addFile(filePath, "")
+//       file.addIssue(
+//         0,
+//         0,
+//         "don't use non-webp images. you can use the script to convert other formats to webp."
+//       )
+//     })
+//   })
