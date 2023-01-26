d5e165464209185a9190df9ffebc32edf1e1d521
Pavlos Vinieratos <pvinis@gmail.com>
2023-01-26 23:21:57 +0000

    fix: betterer commit flow (#8062)

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
+  "*": ["yarn secrets:check:staged"],
   "*.@(ts|tsx)": ["yarn lint", "yarn prettier-write"],
   "*.@(json|md)": ["yarn prettier-write"],
-  "*": ["yarn secrets:check:staged", "yarn betterer:precommit"],
 }
