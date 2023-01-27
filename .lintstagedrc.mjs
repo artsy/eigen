3e94d2b2495277f510c745f8c58ef3bf3f6e5736
Christopher Pappas <damassi.pappas@gmail.com>
2023-01-26 19:14:56 -0800

    temp(betterer): Fixme: remove from precommit

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
-  "*": ["yarn secrets:check:staged"],
+  "*": ["yarn secrets:check:staged", "yarn betterer:all && git add .betterer.results"],
   "*.@(ts|tsx)": ["yarn lint", "yarn prettier-write"],
   "*.@(json|md)": ["yarn prettier-write"],
 }
