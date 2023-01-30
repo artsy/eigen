15bc4b299db6b14e7c3e1419f678d36d01c214f2
Kizito Egeonu <kizito.egeonu@gmail.com>
2023-01-30 10:00:57 +0100

    Remove after seen

--- src/app/Scenes/MyCollection/Screens/Insights/MedianAuctionPriceRail.tsx
+++ src/app/Scenes/MyCollection/Screens/Insights/MedianAuctionPriceRail.tsx
@@ -4,6 +4,8 @@ import {
   OwnerType,
   TappedMyCollectionInsightsMedianAuctionRailItem,
 } from "@artsy/cohesion"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { useNavigation } from "@react-navigation/native"
 import { MedianAuctionPriceRail_me$key } from "__generated__/MedianAuctionPriceRail_me.graphql"
 import { SectionTitle } from "app/Components/SectionTitle"
 import { navigate } from "app/system/navigation/navigate"
@@ -11,6 +13,7 @@ import { extractNodes } from "app/utils/extractNodes"
 import { getVortexMedium } from "app/utils/marketPriceInsightHelpers"
 import { groupBy } from "lodash"
 import { Flex } from "palette"
+import { useEffect, useState } from "react"
 import { FlatList } from "react-native"
 import { useFragment } from "react-relay"
 import { useTracking } from "react-tracking"
@@ -21,11 +24,36 @@ interface MedianAuctionPriceRailProps {
   me: MedianAuctionPriceRail_me$key
 }
 
+const HAS_SEEN_MEDIAN_AUCTION_PRICE_TOOLTIP = "HAS_SEEN_MEDIAN_AUCTION_PRICE_TOOLTIP"
+
 export const MedianAuctionPriceRail: React.FC<MedianAuctionPriceRailProps> = (props) => {
   const me = useFragment(fragment, props.me)
   const artworks = extractNodes(me.priceInsightUpdates)
 
   const tracking = useTracking()
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    const unsubscribe = navigation?.addListener("blur", () => {
+      setHasSeenToolTip(true)
+      AsyncStorage.setItem(HAS_SEEN_MEDIAN_AUCTION_PRICE_TOOLTIP, "true")
+    })
+    return unsubscribe
+  }, [navigation])
+
+  const [hasSeenTooltip, setHasSeenToolTip] = useState(false)
+
+  useEffect(() => {
+    AsyncStorage.getItem(HAS_SEEN_MEDIAN_AUCTION_PRICE_TOOLTIP).then((seen) => {
+      if (seen === "true") {
+        setHasSeenToolTip(true)
+      }
+    })
+    return () => {
+      AsyncStorage.setItem(HAS_SEEN_MEDIAN_AUCTION_PRICE_TOOLTIP, "true")
+    }
+  }, [])
 
   if (artworks.length === 0) {
     return <></>
@@ -65,8 +93,10 @@ export const MedianAuctionPriceRail: React.FC<MedianAuctionPriceRailProps> = (pr
         renderItem={({ item, index }) => (
           <MedianAuctionPriceListItem
             artworks={item}
-            enableToolTip={index === 0}
+            enableToolTip={index === 0 && !hasSeenTooltip}
             onPress={(medium) => {
+              setHasSeenToolTip(true)
+              AsyncStorage.setItem(HAS_SEEN_MEDIAN_AUCTION_PRICE_TOOLTIP, "true")
               const artistID = item[0].artist?.internalID
               const category =
                 medium || getVortexMedium(item[0].medium ?? "", item[0].mediumType?.name ?? "")
