a13b2f2b1416c1e9753dcbf6c1e7762f45e87386
George Kartalis <g.kartalis7@gmail.com>
2023-01-30 15:03:02 +0100

    fix: viewingroomartworks.tests

--- src/app/Scenes/ViewingRoom/ViewingRoomArtworks.tests.tsx
+++ src/app/Scenes/ViewingRoom/ViewingRoomArtworks.tests.tsx
@@ -1,7 +1,7 @@
+import { screen } from "@testing-library/react-native"
 import { ViewingRoomArtworksTestsQuery } from "__generated__/ViewingRoomArtworksTestsQuery.graphql"
 import { navigate } from "app/system/navigation/navigate"
 import { extractText } from "app/utils/tests/extractText"
-import { mockEdges } from "app/utils/tests/resolveMostRecentRelayOperation"
 import { setupTestWrapper } from "app/utils/tests/setupTestWrapper"
 import { Touchable } from "palette"
 import { FlatList, TouchableHighlight } from "react-native"
@@ -11,14 +11,12 @@ import { tracks, ViewingRoomArtworksContainer } from "./ViewingRoomArtworks"
 
 describe("ViewingRoom", () => {
   const { renderWithRelay } = setupTestWrapper<ViewingRoomArtworksTestsQuery>({
-    Component: (props) => {
-      if (props?.viewingRoom) {
-        return <ViewingRoomArtworksContainer viewingRoom={props.viewingRoom} />
-      }
-      return null
-    },
+    Component: (props) => <ViewingRoomArtworksContainer viewingRoom={props.viewingRoom!} />,
     query: graphql`
       query ViewingRoomArtworksTestsQuery {
+        artwork(id: "selected-artwork") {
+          ...ViewingRoomArtwork_selectedArtwork
+        }
         viewingRoom(id: "unused") {
           ...ViewingRoomArtworks_viewingRoom
         }
@@ -27,46 +25,47 @@ describe("ViewingRoom", () => {
   })
 
   it("renders a flatlist with one artwork", () => {
-    const tree = renderWithRelay({
-      ViewingRoom: () => ({ artworksConnection: { edges: mockEdges(1) } }),
-    })
+    renderWithRelay()
 
-    expect(tree.UNSAFE_getAllByType(FlatList)).toHaveLength(1)
-    expect(tree.UNSAFE_getAllByType(TouchableHighlight)).toHaveLength(1)
+    expect(screen.UNSAFE_getAllByType(FlatList)).toHaveLength(1)
+    expect(screen.UNSAFE_getAllByType(TouchableHighlight)).toHaveLength(1)
   })
 
   it("renders additional information if it exists", () => {
-    const tree = renderWithRelay({
-      ViewingRoom: () => ({
-        artworksConnection: {
-          edges: mockEdges(1),
-        },
+    renderWithRelay({
+      Artwork: () => ({
+        additionalInformation: "additionalInformation-1",
       }),
     })
 
     expect(
-      extractText(tree.UNSAFE_getByProps({ testID: "artwork-additional-information" }))
+      extractText(screen.UNSAFE_getByProps({ testID: "artwork-additional-information" }))
     ).toEqual("additionalInformation-1")
   })
 
   it("navigates to artwork screen + calls tracking on press", () => {
-    const tree = renderWithRelay({
+    renderWithRelay({
       ViewingRoom: () => ({
-        artworksConnection: { edges: mockEdges(1) },
+        internalID: "viewing-room-internalID-1",
+        slug: "slug-1",
+      }),
+      Artwork: () => ({
+        internalID: "artwork-internalID-1",
+        slug: "artwork-slug-1",
       }),
     })
 
-    tree.UNSAFE_getByType(Touchable).props.onPress()
+    screen.UNSAFE_getByType(Touchable).props.onPress()
 
-    expect(navigate).toHaveBeenCalledWith("/viewing-room/slug-1/artworksConnection.slug-1")
+    expect(navigate).toHaveBeenCalledWith("/viewing-room/slug-1/artwork-slug-1")
 
     expect(useTracking().trackEvent).toHaveBeenCalledWith({
-      ...tracks.context("internalID-1", "slug-1"),
+      ...tracks.context("viewing-room-internalID-1", "slug-1"),
       ...tracks.tappedArtworkGroup(
-        "internalID-1",
+        "viewing-room-internalID-1",
         "slug-1",
-        "artworksConnection.internalID-1",
-        "artworksConnection.slug-1"
+        "artwork-internalID-1",
+        "artwork-slug-1"
       ),
     })
   })
