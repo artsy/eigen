2617482d687d64c8a6979ccc4b27ed52ba049013
George Kartalis <g.kartalis7@gmail.com>
2023-02-03 17:53:31 +0100

    revert: reverts process color because the color doesn't work (#8154)

--- src/app/Components/OpaqueImageView/OpaqueImageView.tsx
+++ src/app/Components/OpaqueImageView/OpaqueImageView.tsx
@@ -1,7 +1,6 @@
 import { isALocalImage } from "app/Scenes/Artwork/Components/ImageCarousel/ImageCarousel"
 import React from "react"
 import {
-  ColorValue,
   Image,
   ImageResizeMode,
   LayoutChangeEvent,
@@ -33,7 +32,7 @@ interface Props extends ViewProps {
   retryFailedURLs?: boolean
 
   /** The background color for the image view */
-  placeholderBackgroundColor?: ColorValue
+  placeholderBackgroundColor?: string
 
   width?: number
   height?: number
@@ -80,7 +79,7 @@ interface State {
  */
 export default class OpaqueImageView extends React.Component<Props, State> {
   static defaultProps: Props = {
-    placeholderBackgroundColor: processColor("#E7E7E7") as ColorValue, // this is black10. Change it to that when this component becomes a function component.
+    placeholderBackgroundColor: "#E7E7E7", // this is black10. Change it to that when this component becomes a function component.
   }
 
   constructor(props: Props) {
