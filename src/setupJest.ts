5aca4ffc13b693f19815d254314e8c949e888216
Pavlos Vinieratos <pvinis@gmail.com>
2023-02-03 11:04:49 +0000

    mock this quickly for now

--- src/setupJest.ts
+++ src/setupJest.ts
@@ -92,6 +92,8 @@ jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage)
 
 jest.mock("@react-native-community/netinfo", () => mockRNCNetInfo)
 
+jest.mock("react-native-vision-camera", () => {})
+
 jest.mock("react-native-safe-area-context", () => mockSafeAreaContext)
 
 require("jest-fetch-mock").enableMocks()
