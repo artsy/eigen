6eb180c601020a3f511854665fa143a961e33de7
Nikita Skalkin <nikskalkin@gmail.com>
2023-02-08 16:26:24 +0100

    feat(FX-4607): Add "Artsy Editorial" prefix in activity panel

---
+++ src/app/Scenes/Activity/ActivityItemTypeLabel.tsx
@@ -0,0 +1,30 @@
+import { Text } from "palette"
+
+interface Props {
+  notificationType: string
+}
+
+export const ActivityItemTypeLabel: React.FC<Props> = ({ notificationType }) => {
+  const getNotificationType = () => {
+    if (notificationType === "ARTWORK_ALERT") {
+      return "Alert"
+    }
+    if (notificationType === "ARTICLE_FEATURED_ARTIST") {
+      return "Artsy Editorial"
+    }
+
+    return null
+  }
+  const notificationTypeLabel = getNotificationType()
+  const notificationTypeColor = notificationType == "ARTWORK_ALERT" ? "blue100" : "black60"
+
+  return (
+    <Text
+      variant="xs"
+      accessibilityLabel={`Notification type: ${notificationTypeLabel}`}
+      color={notificationTypeColor}
+    >
+      {notificationTypeLabel} •{" "}
+    </Text>
+  )
+}
