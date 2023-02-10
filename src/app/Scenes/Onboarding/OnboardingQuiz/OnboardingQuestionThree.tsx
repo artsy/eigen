33c13115ea9b62f2e4cc835793e5f00bb7ffcc30
Sam Raj <rajsam003@gmail.com>
2023-02-10 13:49:34 +0100

    feat: add art quiz in question 3 options

--- src/app/Scenes/Onboarding/OnboardingQuiz/OnboardingQuestionThree.tsx
+++ src/app/Scenes/Onboarding/OnboardingQuiz/OnboardingQuestionThree.tsx
@@ -10,6 +10,7 @@ import {
   OPTION_FOLLOW_GALLERIES_IM_INTERESTED_IN,
   OPTION_KEEP_TRACK_OF_ART,
   OPTION_TOP_AUCTION_LOTS,
+  OPTION_THE_ART_TASTE_QUIZ,
 } from "app/Scenes/Onboarding/OnboardingQuiz/config"
 import { useCallback, useMemo } from "react"
 import { OnboardingQuestionTemplate } from "./Components/OnboardingQuestionTemplate"
@@ -34,6 +35,26 @@ export const OnboardingQuestionThree = () => {
 
   const options = useMemo(() => {
     switch (true) {
+      case questionTwo.length > 2 &&
+        questionTwo.includes(OPTION_DEVELOPING_MY_ART_TASTES) &&
+        questionTwo.includes(OPTION_COLLECTING_ART_THAT_MOVES_ME) &&
+        questionTwo.includes(OPTION_FINDING_GREAT_INVESTMENTS):
+      case questionTwo.length > 1 &&
+        questionTwo.includes(OPTION_DEVELOPING_MY_ART_TASTES) &&
+        questionTwo.includes(OPTION_FINDING_GREAT_INVESTMENTS):
+      case questionTwo.length > 1 &&
+        questionTwo.includes(OPTION_COLLECTING_ART_THAT_MOVES_ME) &&
+        questionTwo.includes(OPTION_FINDING_GREAT_INVESTMENTS):
+      case questionTwo.length > 1 &&
+        questionTwo.includes(OPTION_DEVELOPING_MY_ART_TASTES) &&
+        questionTwo.includes(OPTION_COLLECTING_ART_THAT_MOVES_ME):
+        return [
+          OPTION_THE_ART_TASTE_QUIZ,
+          OPTION_TOP_AUCTION_LOTS,
+          OPTION_ARTISTS_ON_THE_RISE,
+          OPTION_A_CURATED_SELECTION_OF_ARTWORKS,
+        ]
+
       case questionTwo.length > 1:
         return [
           OPTION_FOLLOW_ARTISTS_I_WANT_TO_COLLECT,
@@ -44,6 +65,7 @@ export const OnboardingQuestionThree = () => {
 
       case questionTwo[0] === OPTION_DEVELOPING_MY_ART_TASTES:
         return [
+          OPTION_THE_ART_TASTE_QUIZ,
           OPTION_TOP_AUCTION_LOTS,
           OPTION_A_CURATED_SELECTION_OF_ARTWORKS,
           OPTION_ARTISTS_ON_THE_RISE,
@@ -55,6 +77,7 @@ export const OnboardingQuestionThree = () => {
       case questionTwo[0] === OPTION_FINDING_GREAT_INVESTMENTS:
       case questionTwo[0] === OPTION_COLLECTING_ART_THAT_MOVES_ME:
         return [
+          OPTION_THE_ART_TASTE_QUIZ,
           OPTION_FOLLOW_ARTISTS_I_WANT_TO_COLLECT,
           OPTION_TOP_AUCTION_LOTS,
           OPTION_ARTISTS_ON_THE_RISE,
