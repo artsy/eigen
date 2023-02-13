fdfbc506f20df8c4390d2cffd3f543fd45dcd421
Sam Raj <rajsam003@gmail.com>
2023-02-13 14:26:19 +0100

    is staging

--- src/app/Scenes/ArtQuiz/ArtQuizResults/ArtQuizResultsTabs/ArtQuizExploreArtworks.tsx
+++ src/app/Scenes/ArtQuiz/ArtQuizResults/ArtQuizResultsTabs/ArtQuizExploreArtworks.tsx
@@ -1,10 +1,12 @@
-import { Flex, Spinner, useSpace } from "@artsy/palette-mobile"
+import { Flex, Spinner } from "@artsy/palette-mobile"
 import { ArtQuizExploreArtworksQuery } from "__generated__/ArtQuizExploreArtworksQuery.graphql"
 import { ArtQuizResultsTabs_me$data } from "__generated__/ArtQuizResultsTabs_me.graphql"
 import GenericGrid from "app/Components/ArtworkGrids/GenericGrid"
 import { StickyTabPageScrollView } from "app/Components/StickyTabPage/StickyTabPageScrollView"
+import { useIsStaging } from "app/store/GlobalStore"
 import { extractNodes } from "app/utils/extractNodes"
 import { uniqBy } from "lodash"
+import { Text } from "palette"
 import { Suspense, useMemo } from "react"
 import { graphql, useLazyLoadQuery } from "react-relay"
 import { useScreenDimensions } from "shared/hooks"
@@ -14,7 +16,6 @@ const ArtQuizExploreArtworksContent = ({
 }: {
   savedArtworks: ArtQuizResultsTabs_me$data["quiz"]["savedArtworks"]
 }) => {
-  const space = useSpace()
   const dimensions = useScreenDimensions()
 
   const limit = useMemo(() => {
@@ -38,12 +39,12 @@ const ArtQuizExploreArtworksContent = ({
   return (
     <StickyTabPageScrollView
       contentContainerStyle={{
-        paddingVertical: space(2),
+        paddingVertical: 20,
       }}
     >
       <GenericGrid
         artworks={artworks}
-        width={dimensions.width - space(2)}
+        width={dimensions.width - 20}
         hidePartner
         artistNamesTextStyle={{ weight: "regular" }}
         saleInfoTextStyle={{ weight: "medium", color: "black100" }}
@@ -53,11 +54,10 @@ const ArtQuizExploreArtworksContent = ({
 }
 
 const ArtQuizExploreArtworksFallback = () => {
-  const space = useSpace()
   return (
     <StickyTabPageScrollView
       contentContainerStyle={{
-        paddingVertical: space(2),
+        paddingVertical: 20,
       }}
     >
       <Flex p={2} justifyContent="center" alignItems="center">
@@ -72,6 +72,22 @@ export const ArtQuizExploreArtworks = ({
 }: {
   savedArtworks: ArtQuizResultsTabs_me$data["quiz"]["savedArtworks"]
 }) => {
+  const isStaging = useIsStaging()
+
+  if (isStaging) {
+    return (
+      <StickyTabPageScrollView
+        contentContainerStyle={{
+          paddingVertical: 20,
+        }}
+      >
+        <Text variant="xs" color="black60">
+          We don't have any recommendations for you at this time.
+        </Text>
+      </StickyTabPageScrollView>
+    )
+  }
+
   return (
     <Suspense fallback={<ArtQuizExploreArtworksFallback />}>
       <ArtQuizExploreArtworksContent savedArtworks={savedArtworks} />
