249e115f52a1b2483e8c83f94d9d3c778887b101
egdbear <egdfer@gmail.com>
2023-02-16 09:36:12 +0100

    fix: use capital K for price values

--- src/app/utils/formatLargeNumber.ts
+++ src/app/utils/formatLargeNumber.ts
@@ -2,7 +2,7 @@ export function formatLargeNumber(number: number, decimalPlaces = 0) {
   if (number < 1000) {
     return number.toString()
   } else if (number < 1000000) {
-    return `${(number / 1000).toFixed(decimalPlaces)}k`
+    return `${(number / 1000).toFixed(decimalPlaces)}K`
   } else if (number < 1000000000) {
     return `${(number / 1000000).toFixed(decimalPlaces)}M`
   } else {
