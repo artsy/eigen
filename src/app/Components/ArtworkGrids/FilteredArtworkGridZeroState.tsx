47164673d69c5175366a54728d477cd600863449
Dima Tretyak <itretyak@ya.ru>
2023-02-21 14:35:26 +0300

    fix: centering content

--- src/app/Components/ArtworkGrids/FilteredArtworkGridZeroState.tsx
+++ src/app/Components/ArtworkGrids/FilteredArtworkGridZeroState.tsx
@@ -16,7 +16,7 @@ export const FilteredArtworkGridZeroState: React.FC<ZeroStateProps> = (props) =>
   )
 
   return (
-    <Flex flexDirection="column" px={4}>
+    <Flex flexDirection="column" alignItems="center" px={4}>
       <Text textAlign="center" color="black100" variant="sm">
         No results found{"\n"}Please try another search.
       </Text>
