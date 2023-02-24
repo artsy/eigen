45f0dcaa34cbc283a8203670929f9bab50439bdf
Sam Raj <rajsam003@gmail.com>
2023-02-24 11:16:49 +0100

    relove conflict

---
+++ src/app/Scenes/ArtQuiz/ArtQuizButton.tsx
@@ -0,0 +1,18 @@
+import { CloseIcon, HeartIcon, Touchable } from "palette"
+
+interface ArtQuizButtonProps {
+  variant: "Like" | "Dislike"
+  onPress: () => void
+}
+
+export const ArtQuizButton = ({ variant, onPress }: ArtQuizButtonProps) => {
+  return (
+    <Touchable onPress={onPress}>
+      {variant === "Like" ? (
+        <HeartIcon height={40} width={50} />
+      ) : (
+        <CloseIcon height={40} width={50} />
+      )}
+    </Touchable>
+  )
+}
