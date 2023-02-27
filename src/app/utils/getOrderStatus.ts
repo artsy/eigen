c63b36007cc06bd44593138050933e2dc810b27d
Matt Dole <mdole7@gmail.com>
2023-02-27 13:35:15 +0100

    feat: replace state map with displayState
    Co-authored-by: Adam Iskounen <adam.iskounen@artsymail.com>

--- src/app/utils/getOrderStatus.ts
+++ src/app/utils/getOrderStatus.ts
@@ -1,69 +1,28 @@
-import {
-  CommerceOrderStateEnum,
-  OrderDetailsHeader_info$data,
-} from "__generated__/OrderDetailsHeader_info.graphql"
-import { OrderHistoryRowLineItem } from "./getTrackingUrl"
-
-export type OrderDetailsHeaderLineItem = NonNullable<
-  NonNullable<NonNullable<OrderDetailsHeader_info$data["lineItems"]>["edges"]>[0]
->["node"]
-
-export type OrderState = Exclude<
-  CommerceOrderStateEnum,
-  "ABANDONED" | "PENDING" | "%future added value"
->
-export type ShipmentStatus =
-  | "DRAFT"
-  | "PENDING"
-  | "CONFIRMED"
-  | "COLLECTED"
-  | "IN_TRANSIT"
-  | "COMPLETED"
-  | "CANCELED"
-
-enum ORDER_STATUSES {
-  Pending = "Pending",
-  Confirmed = "Confirmed",
-  Delivered = "Delivered",
-  Canceled = "Canceled",
-  Refunded = "Refunded",
-  PaymentProcessing = "Payment processing",
-}
-
-enum SHIPMENT_STATUSES {
-  Processing = "Processing",
-  InTransit = "In transit",
-  Delivered = "Delivered",
-  Canceled = "Canceled",
-}
+import { CommerceOrderDisplayStateEnum } from "__generated__/OrderDetailsHeader_info.graphql"
 
 const orderStatusesMap = {
-  SUBMITTED: ORDER_STATUSES.Pending,
-  APPROVED: ORDER_STATUSES.Confirmed,
-  FULFILLED: ORDER_STATUSES.Delivered,
-  CANCELLED: ORDER_STATUSES.Canceled,
-  REFUNDED: ORDER_STATUSES.Refunded,
-  PENDING: SHIPMENT_STATUSES.Processing,
-  CONFIRMED: SHIPMENT_STATUSES.Processing,
-  DRAFT: ORDER_STATUSES.Pending,
-  PROCESSING_APPROVAL: ORDER_STATUSES.PaymentProcessing,
-  COLLECTED: SHIPMENT_STATUSES.InTransit,
-  IN_TRANSIT: SHIPMENT_STATUSES.InTransit,
-  COMPLETED: SHIPMENT_STATUSES.Delivered,
-  CANCELED: SHIPMENT_STATUSES.Canceled,
+  SUBMITTED: "Pending",
+  APPROVED: "Confirmed",
+  FULFILLED: "Delivered",
+  REFUNDED: "Refunded",
+  PROCESSING: "Processing",
+  PROCESSING_APPROVAL: "Payment processing",
+  IN_TRANSIT: "In transit",
+  CANCELED: "Canceled",
 }
 
-type OrderStatus = keyof typeof orderStatusesMap
-
-export function getOrderStatus(
-  orderState: OrderState,
-  orderLineItem?: OrderHistoryRowLineItem | OrderDetailsHeaderLineItem
-): string {
-  if (orderState === "CANCELED" || orderState === "REFUNDED" || !orderLineItem?.shipment?.status) {
-    return orderStatusesMap[orderState as OrderStatus].toLowerCase()
+export function getOrderStatus(displayState: CommerceOrderDisplayStateEnum) {
+  switch (displayState) {
+    case "SUBMITTED":
+    case "APPROVED":
+    case "FULFILLED":
+    case "REFUNDED":
+    case "PROCESSING":
+    case "PROCESSING_APPROVAL":
+    case "IN_TRANSIT":
+    case "CANCELED":
+      return orderStatusesMap[displayState].toLowerCase()
+    default:
+      return "unknown"
   }
-
-  const shipment: ShipmentStatus = orderLineItem.shipment.status.toUpperCase() as ShipmentStatus
-
-  return orderStatusesMap[shipment]?.toLowerCase()
 }
