37632e93128929d7098934c2a00f4377e7edf6e7
George Kartalis <g.kartalis7@gmail.com>
2023-03-02 17:47:20 +0100

    fix: add back the skip navigate

--- src/app/Scenes/ArtQuiz/ArtQuizWelcome.tsx
+++ src/app/Scenes/ArtQuiz/ArtQuizWelcome.tsx
@@ -32,6 +32,7 @@ export const ArtQuizWelcome = () => {
             variant="text"
             onPress={() => {
               GlobalStore.actions.auth.setArtQuizState("complete")
+              navigate("/")
             }}
           >
             Skip
