6c39e50f7e80716997506b82f1353263e150ddbe
George Kartalis <g.kartalis7@gmail.com>
2023-03-02 17:53:42 +0100

    chore: hide artquiz when ff is false

--- src/app/Scenes/Home/HomeContainer.tsx
+++ src/app/Scenes/Home/HomeContainer.tsx
@@ -1,15 +1,17 @@
 import { HomeQueryRenderer } from "app/Scenes/Home/Home"
-import { GlobalStore } from "app/store/GlobalStore"
+import { GlobalStore, useFeatureFlag } from "app/store/GlobalStore"
 import { navigate } from "app/system/navigation/navigate"
 
 export const HomeContainer = () => {
   const artQuizState = GlobalStore.useAppState((state) => state.auth.onboardingArtQuizState)
 
+  const shouldShowArtQuiz = useFeatureFlag("ARShowArtQuizApp")
+
   const navigateToArtQuiz = async () => {
     await navigate("/art-quiz")
   }
 
-  if (artQuizState === "incomplete") {
+  if (artQuizState === "incomplete" && shouldShowArtQuiz) {
     navigateToArtQuiz()
     return null
   }
