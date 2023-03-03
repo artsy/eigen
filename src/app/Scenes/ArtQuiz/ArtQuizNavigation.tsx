1c1077cf070fa3ee3d16241ab28984877e90c2e1
George Kartalis <g.kartalis7@gmail.com>
2023-03-03 11:30:55 +0100

    chore: revert artquiznavigation changes

--- src/app/Scenes/ArtQuiz/ArtQuizNavigation.tsx
+++ src/app/Scenes/ArtQuiz/ArtQuizNavigation.tsx
@@ -3,7 +3,6 @@ import { createStackNavigator, TransitionPresets } from "@react-navigation/stack
 import { ArtQuizNavigationQuery } from "__generated__/ArtQuizNavigationQuery.graphql"
 import { ArtQuizArtworks } from "app/Scenes/ArtQuiz/ArtQuizArtworks"
 import { ArtQuizLoader } from "app/Scenes/ArtQuiz/ArtQuizLoader"
-import { ArtQuizResults } from "app/Scenes/ArtQuiz/ArtQuizResults/ArtQuizResults"
 import { ArtQuizWelcome } from "app/Scenes/ArtQuiz/ArtQuizWelcome"
 import { navigate } from "app/system/navigation/navigate"
 import { Suspense, useEffect } from "react"
@@ -12,20 +11,28 @@ import { graphql, useLazyLoadQuery } from "react-relay"
 export type ArtQuizNavigationStack = {
   ArtQuizWelcome: undefined
   ArtQuizArtworks: undefined
-  ArtQuizResults: undefined
 }
 
 export const StackNavigator = createStackNavigator<ArtQuizNavigationStack>()
 
 const ArtQuiz: React.FC = () => {
   const queryResult = useLazyLoadQuery<ArtQuizNavigationQuery>(artQuizNavigationQuery, {}).me?.quiz
+
   const isQuizCompleted = !!queryResult?.completedAt
+  const lastInteractedArtworkIndex = queryResult?.quizArtworkConnection?.edges?.findIndex(
+    (edge) => edge?.interactedAt === null
+  )
+  const isQuizStartedButIncomplete = !!lastInteractedArtworkIndex
 
   useEffect(() => {
     if (isQuizCompleted) {
       navigate("/art-quiz/results")
     }
   }, [isQuizCompleted])
+
+  if (isQuizStartedButIncomplete) {
+    return <ArtQuizArtworks />
+  }
 
   return (
     <NavigationContainer independent>
@@ -39,7 +46,6 @@ const ArtQuiz: React.FC = () => {
       >
         <StackNavigator.Screen name="ArtQuizWelcome" component={ArtQuizWelcome} />
         <StackNavigator.Screen name="ArtQuizArtworks" component={ArtQuizArtworks} />
-        <StackNavigator.Screen name="ArtQuizResults" component={ArtQuizResults} />
       </StackNavigator.Navigator>
     </NavigationContainer>
   )
@@ -58,6 +64,11 @@ const artQuizNavigationQuery = graphql`
     me {
       quiz {
         completedAt
+        quizArtworkConnection(first: 16) {
+          edges {
+            interactedAt
+          }
+        }
       }
     }
   }
