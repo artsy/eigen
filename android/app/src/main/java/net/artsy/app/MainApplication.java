0eb7854479623858b75c4603503da648b36f0c8c
brainbicycle <brian.beckerle@artsymail.com>
2023-03-06 13:30:38 -0500

    remove adjust on android side

--- android/app/src/main/java/net/artsy/app/MainApplication.java
+++ android/app/src/main/java/net/artsy/app/MainApplication.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import com.braze.Braze;
 import com.braze.support.BrazeLogger;
 import com.appboy.AppboyLifecycleCallbackListener;
-import com.segment.analytics.android.integrations.adjust.AdjustIntegration;
 import com.segment.analytics.Analytics;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -68,7 +67,6 @@ public class MainApplication extends Application implements ReactApplication {
 
     Analytics analytics = new Analytics.Builder(this,
         ReactNativeConfigUtils.decode(segmentWriteKey, BuildConfig.XOR_KEY))
-        .use(AdjustIntegration.FACTORY)
         .build();
 
     Analytics.setSingletonInstance(analytics);
