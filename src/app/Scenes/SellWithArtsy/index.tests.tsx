3369c1c4049c6dc67095eabd1f36428c648c2b51
Kizito Egeonu <kizito.egeonu@gmail.com>
2023-03-06 14:43:03 +0100

    Feat(CX-3494): Implement Tracking in new SWA modules (#8311)
    * Feat(CX-3494): Implement Tracking in new SWA modules
    
    * tsc
    
    * address review comments
    
    ---------
    
    Co-authored-by: dariakoko <nargle30@gmail.com>

--- src/app/Scenes/SellWithArtsy/index.tests.tsx
+++ src/app/Scenes/SellWithArtsy/index.tests.tsx
@@ -1,6 +1,8 @@
 import { ContextModule, OwnerType } from "@artsy/cohesion"
+import { fireEvent } from "@testing-library/react-native"
+import { __globalStoreTestUtils__ } from "app/store/GlobalStore"
 import { mockTrackEvent } from "app/utils/tests/globallyMockedStuff"
-import { renderWithWrappersLEGACY } from "app/utils/tests/renderWithWrappers"
+import { renderWithWrappers, renderWithWrappersLEGACY } from "app/utils/tests/renderWithWrappers"
 import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
 import { SellWithArtsyRecentlySold } from "./Components/SellWithArtsyRecentlySold"
 import { SellWithArtsyHomeQueryRenderer } from "./SellWithArtsyHome"
@@ -41,7 +43,7 @@ describe("ConsignmentsHome index", () => {
       expect.objectContaining({
         context_module: ContextModule.sellHeader,
         context_screen_owner_type: OwnerType.sell,
-        subject: "Submit a work",
+        subject: "Submit an Artwork",
       })
     )
   })
@@ -57,8 +59,120 @@ describe("ConsignmentsHome index", () => {
       expect.objectContaining({
         context_module: ContextModule.sellFooter,
         context_screen_owner_type: OwnerType.sell,
-        subject: "Submit a work",
+        subject: "Submit an Artwork",
       })
     )
+  })
+})
+
+describe("New SellWithArtsyLandingPage", () => {
+  describe("Tracking", () => {
+    let mockEnvironment: ReturnType<typeof createMockEnvironment>
+
+    beforeEach(() => {
+      mockEnvironment = createMockEnvironment()
+      __globalStoreTestUtils__?.injectFeatureFlags({ AREnableNewSWALandingPage: true })
+    })
+
+    const TestWrapper = () => {
+      return <SellWithArtsyHomeQueryRenderer environment={mockEnvironment} />
+    }
+
+    // HEADER
+    describe("Header Events", () => {
+      it("tracks Consign Events", () => {
+        const { getByTestId } = renderWithWrappers(<TestWrapper />)
+        const headerConsignButton = getByTestId("header-consign-CTA")
+
+        fireEvent(headerConsignButton, "onPress")
+        expect(mockTrackEvent).toHaveBeenCalledTimes(1)
+        expect(mockTrackEvent).toHaveBeenLastCalledWith(
+          expect.objectContaining({
+            action: "tappedConsign",
+            context_module: "sellHeader",
+            context_screen_owner_type: "sell",
+            destination_screen_owner_type: "consignmentSubmission",
+            subject: "Start Selling",
+          })
+        )
+      })
+
+      it("tracks Inquiry Events", () => {
+        const { getByTestId } = renderWithWrappers(<TestWrapper />)
+        const headerInquiryButton = getByTestId("Header-inquiry-CTA")
+
+        fireEvent(headerInquiryButton, "onPress")
+        expect(mockTrackEvent).toHaveBeenCalledTimes(1)
+        expect(mockTrackEvent).toHaveBeenLastCalledWith(
+          expect.objectContaining({
+            action: "tappedConsignmentInquiry",
+            context_module: "sellHeader",
+            context_screen: "sell",
+            context_screen_owner_type: "sell",
+            subject: "Get in Touch",
+          })
+        )
+      })
+    })
+
+    // HOWITWORKS
+    describe("HowItWorks Events", () => {
+      it("tracks Consign Events", () => {
+        const { getByTestId } = renderWithWrappers(<TestWrapper />)
+        const headerConsignButton = getByTestId("HowItWorks-consign-CTA")
+
+        fireEvent(headerConsignButton, "onPress")
+        expect(mockTrackEvent).toHaveBeenCalledTimes(1)
+        expect(mockTrackEvent).toHaveBeenLastCalledWith(
+          expect.objectContaining({
+            action: "tappedConsign",
+            context_module: "sellHowItWorks",
+            context_screen_owner_type: "sell",
+            destination_screen_owner_type: "consignmentSubmission",
+            subject: "Start Selling",
+          })
+        )
+      })
+    })
+
+    // MEETTHESPECIALISTS
+    describe("MeetTheSpecialists Events", () => {
+      it("tracks Inquiry Events", () => {
+        const { getByTestId } = renderWithWrappers(<TestWrapper />)
+        const headerConsignButton = getByTestId("MeetTheSpecialists-inquiry-CTA")
+
+        fireEvent(headerConsignButton, "onPress")
+        expect(mockTrackEvent).toHaveBeenCalledTimes(1)
+        expect(mockTrackEvent).toHaveBeenLastCalledWith(
+          expect.objectContaining({
+            action: "tappedConsignmentInquiry",
+            context_module: "sellMeetTheSpecialists",
+            context_screen: "sell",
+            context_screen_owner_type: "sell",
+            subject: "Get in Touch",
+          })
+        )
+      })
+    })
+
+    // SPEAKTOTHETEAM
+    describe("SpeakToTheTeam Events", () => {
+      it("tracks Inquiry Events", () => {
+        const { getByTestId } = renderWithWrappers(<TestWrapper />)
+        const headerConsignButton = getByTestId("SpeakToTheTeam-inquiry-CTA")
+
+        fireEvent(headerConsignButton, "onPress")
+        expect(mockTrackEvent).toHaveBeenCalledTimes(1)
+        expect(mockTrackEvent).toHaveBeenLastCalledWith(
+          expect.objectContaining({
+            action: "tappedConsignmentInquiry",
+            context_module: "sellSpeakToTheTeam",
+            context_screen: "sell",
+            context_screen_owner_type: "sell",
+            subject: "Get in Touch",
+          })
+        )
+      })
+    })
   })
 })
