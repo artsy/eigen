5165a7c15e28081311cc2be96ee524ae37e0f017
George Kartalis <g.kartalis7@gmail.com>
2023-03-15 15:49:41 +0100

    refactor: ArtworkActions to a react FC:

--- src/app/Scenes/Artwork/Components/ArtworkActions.tsx
+++ src/app/Scenes/Artwork/Components/ArtworkActions.tsx
@@ -3,17 +3,17 @@ import { ArtworkActions_artwork$data } from "__generated__/ArtworkActions_artwor
 import { LegacyNativeModules } from "app/NativeModules/LegacyNativeModules"
 import { unsafe__getEnvironment } from "app/store/GlobalStore"
 import { cm2in } from "app/utils/conversions"
-import { Schema, track } from "app/utils/track"
+import { Schema } from "app/utils/track"
 import { take } from "lodash"
 import { Touchable } from "palette"
 import React from "react"
 import { TouchableWithoutFeedback } from "react-native"
-import { createFragmentContainer, graphql, RelayProp } from "react-relay"
+import { createFragmentContainer, graphql } from "react-relay"
+import { useTracking } from "react-tracking"
 import styled from "styled-components/native"
 
 interface ArtworkActionsProps {
   artwork: ArtworkActions_artwork$data
-  relay?: RelayProp
   shareOnPress: () => void
 }
 
@@ -38,19 +38,21 @@ export const shareContent = (
   }
 }
 
-@track()
-export class ArtworkActions extends React.Component<ArtworkActionsProps> {
-  @track(() => ({
-    action_name: Schema.ActionNames.ViewInRoom,
-    action_type: Schema.ActionTypes.Tap,
-    context_module: Schema.ContextModules.ArtworkActions,
-  }))
-  openViewInRoom() {
-    const {
-      artwork: { image, id, slug, heightCm, widthCm },
-    } = this.props
+export const ArtworkActions: React.FC<ArtworkActionsProps> = ({
+  artwork: { image, id, slug, heightCm, widthCm, isHangable },
+  shareOnPress,
+}) => {
+  const { trackEvent } = useTracking()
+
+  const openViewInRoom = () => {
     const heightIn = cm2in(heightCm!)
     const widthIn = cm2in(widthCm!)
+
+    trackEvent({
+      action_name: Schema.ActionNames.ViewInRoom,
+      action_type: Schema.ActionTypes.Tap,
+      context_module: Schema.ContextModules.ArtworkActions,
+    })
 
     LegacyNativeModules.ARTNativeScreenPresenterModule.presentAugmentedRealityVIR(
       image?.url!,
@@ -61,30 +63,24 @@ export class ArtworkActions extends React.Component<ArtworkActionsProps> {
     )
   }
 
-  render() {
-    const {
-      artwork: { isHangable },
-    } = this.props
-
-    return (
-      <Flex justifyContent="center" flexDirection="row" width="100%">
-        {!!(LegacyNativeModules.ARCocoaConstantsModule.AREnabled && isHangable) && (
-          <TouchableWithoutFeedback onPress={() => this.openViewInRoom()}>
-            <UtilButton pr={2}>
-              <EyeOpenedIcon mr={0.5} />
-              <Text variant="sm">View in Room</Text>
-            </UtilButton>
-          </TouchableWithoutFeedback>
-        )}
-        <Touchable haptic onPress={() => this.props.shareOnPress()}>
-          <UtilButton>
-            <ShareIcon mr={0.5} />
-            <Text variant="sm">Share</Text>
+  return (
+    <Flex justifyContent="center" flexDirection="row" width="100%">
+      {!!(LegacyNativeModules.ARCocoaConstantsModule.AREnabled && isHangable) && (
+        <TouchableWithoutFeedback onPress={() => openViewInRoom()}>
+          <UtilButton pr={2}>
+            <EyeOpenedIcon mr={0.5} />
+            <Text variant="sm">View in Room</Text>
           </UtilButton>
-        </Touchable>
-      </Flex>
-    )
-  }
+        </TouchableWithoutFeedback>
+      )}
+      <Touchable haptic onPress={() => shareOnPress()}>
+        <UtilButton>
+          <ShareIcon mr={0.5} />
+          <Text variant="sm">Share</Text>
+        </UtilButton>
+      </Touchable>
+    </Flex>
+  )
 }
 
 const UtilButton = styled(Flex)`
