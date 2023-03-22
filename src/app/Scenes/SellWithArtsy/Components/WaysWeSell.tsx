58e18752d95f70ea6730956a1258a4d9a205e8d8
Mounir Dhahri <soussouheros@gmail.com>
2023-03-22 11:55:55 +0100

    chore: change SWA images (#8402)
    chore: change swa images

--- src/app/Scenes/SellWithArtsy/Components/WaysWeSell.tsx
+++ src/app/Scenes/SellWithArtsy/Components/WaysWeSell.tsx
@@ -1,27 +1,44 @@
 import { Flex, Spacer, Text, useColor, useSpace } from "@artsy/palette-mobile"
 import { useExtraLargeWidth } from "app/Components/ArtworkRail/useExtraLargeWidth"
-import { Image } from "react-native"
+import { Image, ImageSourcePropType } from "react-native"
 import { FlatList } from "react-native-gesture-handler"
 
-const data = [
+const data: Array<{
+  title: string
+  text: string
+  imageSource: ImageSourcePropType
+  height: number
+  width: number
+}> = [
   {
     title: "Auctions",
     text: "Our curated auctions provide you with multiple opportunities to reach the widest audience and successfully achieve your artwork’s full potential.",
+    imageSource: require("images/ways-we-sell-auction.png"),
+    width: 600,
+    height: 392,
   },
   {
     title: "Private Sales",
     text: "We offer tailored and discreet sales arrangements for our collectors’ highest value and most sensitive artworks.",
+    imageSource: require("images/ways-we-sell-private-sales.png"),
+    width: 600,
+    height: 317,
   },
   {
     title: "Online storefront",
     text: "When your work is listed directly on Artsy.net—the world’s largest online art marketplace—it reaches over 3 million art lovers.",
+    imageSource: require("images/ways-we-sell-online-storefront.png"),
+    width: 600,
+    height: 491,
   },
 ]
 
 export const WaysWeSell: React.FC = () => {
   const space = useSpace()
   const color = useColor()
+
   const maxImageWidth = useExtraLargeWidth()
+
   return (
     <Flex bg="black100" py={4}>
       <Flex mx={2}>
@@ -37,31 +54,29 @@ export const WaysWeSell: React.FC = () => {
         horizontal
         showsHorizontalScrollIndicator={false}
         data={data}
-        renderItem={({ item, index }) => (
-          <Flex maxWidth={maxImageWidth} mr={2} alignSelf="flex-end">
-            <Image
-              source={
-                // because these images are suffixed @1x, @2x etc we cannot `require` outside Image
-                // as this would break tests since in test env the module would not be found without
-                // the suffixes included literally
-                index === 0
-                  ? require("images/ways-we-sell-auction.png")
-                  : index === 1
-                  ? require("images/ways-we-sell-private-sales.png")
-                  : require("images/ways-we-sell-online-storefront.png")
-              }
-              style={{ maxWidth: maxImageWidth }}
-              resizeMode="cover"
-            />
+        renderItem={({ item }) => {
+          const imageAspectRatio = item.height / item.width
 
-            <Text variant="md" color={color("white100")} mt={1}>
-              {item.title}
-            </Text>
-            <Text variant="xs" color={color("white100")} mt={1} maxWidth>
-              {item.text}
-            </Text>
-          </Flex>
-        )}
+          const imageHeight = maxImageWidth * imageAspectRatio
+          return (
+            <Flex maxWidth={maxImageWidth} mr={2} alignSelf="flex-end">
+              <Flex height={imageHeight} justifyContent="flex-end">
+                <Image
+                  source={item.imageSource}
+                  style={{ width: maxImageWidth, height: imageHeight }}
+                  resizeMode="cover"
+                />
+              </Flex>
+
+              <Text variant="md" color={color("white100")} mt={1}>
+                {item.title}
+              </Text>
+              <Text variant="xs" color={color("white100")} mt={1} maxWidth>
+                {item.text}
+              </Text>
+            </Flex>
+          )
+        }}
         keyExtractor={(item) => item.title}
         ListFooterComponent={() => <Spacer x={4} />}
       />
