866f96636b62524781258c13c6bfeb4a2be2cebb
brainbicycle <brian.beckerle@artsymail.com>
2023-03-24 15:23:00 -0400

    add a sceneview

--- ios/Artsy/View_Controllers/ARVIR/VCs/ARAugmentedWallBasedVIRViewController.swift
+++ ios/Artsy/View_Controllers/ARVIR/VCs/ARAugmentedWallBasedVIRViewController.swift
@@ -1,12 +1,22 @@
 import UIKit
+import ARKit
 
 class ARAugmentedWallBasedVIRViewController: UIViewController {
 
-    @objc var config : ARAugmentedRealityConfig!
+    var config : ARAugmentedRealityConfig!
+    var sceneView : ARSCNView!
+
+    @objc func initWithConfig(_ config: ARAugmentedRealityConfig) {
+        self.config = config
+        self.sceneView = ARSCNView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black
+        self.sceneView.backgroundColor = UIColor.black
+        self.view.addSubview(self.sceneView)
+        self.sceneView.align(toView: self.view!)
         print("Got config with artwork \(config.artworkSlug)")
-        // Do any additional setup after loading the view.
     }
 }
