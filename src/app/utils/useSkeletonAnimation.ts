53bb3b049031d8378b5c2593f2e622f06a4634f1
Mounir Dhahri <soussouheros@gmail.com>
2023-03-30 11:59:04 +0200

    chore: animate loading skeletons

---
+++ src/app/utils/useSkeletonAnimation.ts
@@ -0,0 +1,30 @@
+import { useEffect } from "react"
+import {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated"
+
+type IUseSkeletonAnimationProps = {
+  speed?: number
+  targetOpacityValue?: number
+}
+
+export function useSkeletonAnimation({
+  speed = 1000,
+  targetOpacityValue = 0.2,
+}: IUseSkeletonAnimationProps) {
+  const shared = useSharedValue(0)
+
+  useEffect(() => {
+    shared.value = withRepeat(withTiming(1, { duration: speed }), Infinity, true)
+  }, [])
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(shared.value, [0, 1], [targetOpacityValue, 1]),
+  }))
+
+  return animatedStyle
+}
