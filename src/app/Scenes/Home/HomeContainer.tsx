d1a4020e83023c878532670bb1093918343d784a
brainbicycle <brian.beckerle@artsymail.com>
2023-04-03 14:30:34 -0400

    move push prompt to home container

--- src/app/Scenes/Home/HomeContainer.tsx
+++ src/app/Scenes/Home/HomeContainer.tsx
@@ -1,9 +1,11 @@
 import { HomeQueryRenderer } from "app/Scenes/Home/Home"
 import { GlobalStore, useFeatureFlag } from "app/store/GlobalStore"
 import { navigate } from "app/system/navigation/navigate"
+import { requestPushNotificationsPermission } from "app/utils/PushNotification"
 
 export const HomeContainer = () => {
   const artQuizState = GlobalStore.useAppState((state) => state.auth.onboardingArtQuizState)
+  const onboardingState = GlobalStore.useAppState((state) => state.auth.onboardingState)
 
   const shouldShowArtQuiz = useFeatureFlag("ARShowArtQuizApp")
 
@@ -14,6 +16,10 @@ export const HomeContainer = () => {
   if (shouldShowArtQuiz && artQuizState === "incomplete") {
     navigateToArtQuiz()
     return null
+  }
+
+  if (!onboardingState || onboardingState === "complete" || onboardingState === "none") {
+    requestPushNotificationsPermission()
   }
 
   return <HomeQueryRenderer />
