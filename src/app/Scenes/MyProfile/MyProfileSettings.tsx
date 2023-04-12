6920c0780f6fc11fc30586f28bf57434b3c0755e
Daria Kozlova <daria.kozlova@artsymail.com>
2023-04-12 11:14:37 +0200

    fix: adjust the settings screen (#8491)

--- src/app/Scenes/MyProfile/MyProfileSettings.tsx
+++ src/app/Scenes/MyProfile/MyProfileSettings.tsx
@@ -1,10 +1,11 @@
 import { Spacer, Flex, useColor, Text, Separator } from "@artsy/palette-mobile"
+import { Button } from "app/Components/Button"
+import { FancyModalHeader } from "app/Components/FancyModal/FancyModalHeader"
 import { MenuItem } from "app/Components/MenuItem"
 import { presentEmailComposer } from "app/NativeModules/presentEmailComposer"
 import { Tab } from "app/Scenes/Favorites/Favorites"
 import { GlobalStore, useFeatureFlag } from "app/store/GlobalStore"
 import { navigate } from "app/system/navigation/navigate"
-import { Button } from "app/Components/Button"
 import { Alert, ScrollView } from "react-native"
 
 interface MyProfileSettingsProps {
@@ -20,87 +21,88 @@ export const MyProfileSettings: React.FC<MyProfileSettingsProps> = ({ onSuccess 
   const separatorColor = color("black5")
 
   return (
-    <ScrollView>
-      <Text variant="lg-display" mx={2} mt={6}>
-        Account
-      </Text>
-      <Spacer y={1} />
-      <MenuItem
-        title="Edit Profile"
-        onPress={() =>
-          navigate("my-profile/edit", {
-            passProps: {
-              onSuccess,
-            },
-          })
-        }
-      />
-      <Separator my={1} borderColor={separatorColor} />
-      <MenuItem title="Account" onPress={() => navigate("my-account")} />
-      <Separator my={1} borderColor={separatorColor} />
-      <MenuItem title="Saved Alerts" onPress={() => navigate("my-profile/saved-search-alerts")} />
-      <Separator my={1} borderColor={separatorColor} />
-      <MenuItem
-        title="Follows"
-        onPress={() =>
-          navigate("favorites", {
-            passProps: {
-              initialTab: Tab.artists,
-            },
-          })
-        }
-      />
-      <Separator my={1} borderColor={separatorColor} />
-      {!!showOrderHistory && (
-        <>
-          <MenuItem title="Order History" onPress={() => navigate("/orders")} />
-          <Separator my={1} borderColor={separatorColor} />
-        </>
-      )}
-      <MenuItem title="Payment" onPress={() => navigate("my-profile/payment")} />
-      <Separator my={1} borderColor={separatorColor} />
-      {!!darkModeSupport && (
-        <>
-          <MenuItem title="Dark Mode" onPress={() => navigate("settings/dark-mode")} />
-          <Separator my={1} borderColor={separatorColor} />
-        </>
-      )}
+    <>
+      <FancyModalHeader hideBottomDivider>Account</FancyModalHeader>
 
-      {!!showSavedAddresses && (
-        <>
-          <MenuItem
-            title="Saved Addresses"
-            onPress={() => navigate("my-profile/saved-addresses")}
-          />
-          <Separator my={1} borderColor={separatorColor} />
-        </>
-      )}
+      <ScrollView>
+        <Spacer y={2} />
+        <MenuItem
+          title="Edit Profile"
+          onPress={() =>
+            navigate("my-profile/edit", {
+              passProps: {
+                onSuccess,
+              },
+            })
+          }
+        />
+        <Separator my={1} borderColor={separatorColor} />
+        <MenuItem title="Account Settings" onPress={() => navigate("my-account")} />
+        <Separator my={1} borderColor={separatorColor} />
+        <MenuItem title="Saved Alerts" onPress={() => navigate("my-profile/saved-search-alerts")} />
+        <Separator my={1} borderColor={separatorColor} />
+        <MenuItem
+          title="Follows"
+          onPress={() =>
+            navigate("favorites", {
+              passProps: {
+                initialTab: Tab.artists,
+              },
+            })
+          }
+        />
+        <Separator my={1} borderColor={separatorColor} />
+        {!!showOrderHistory && (
+          <>
+            <MenuItem title="Order History" onPress={() => navigate("/orders")} />
+            <Separator my={1} borderColor={separatorColor} />
+          </>
+        )}
+        <MenuItem title="Payment" onPress={() => navigate("my-profile/payment")} />
+        <Separator my={1} borderColor={separatorColor} />
+        {!!darkModeSupport && (
+          <>
+            <MenuItem title="Dark Mode" onPress={() => navigate("settings/dark-mode")} />
+            <Separator my={1} borderColor={separatorColor} />
+          </>
+        )}
 
-      <MenuItem
-        title="Push Notifications"
-        onPress={() => navigate("my-profile/push-notifications")}
-      />
-      <Separator my={1} borderColor={separatorColor} />
+        {!!showSavedAddresses && (
+          <>
+            <MenuItem
+              title="Saved Addresses"
+              onPress={() => navigate("my-profile/saved-addresses")}
+            />
+            <Separator my={1} borderColor={separatorColor} />
+          </>
+        )}
 
-      <MenuItem
-        title="Send Feedback"
-        onPress={() => presentEmailComposer("support@artsy.net", "Feedback from the Artsy app")}
-      />
-      <Separator my={1} borderColor={separatorColor} />
+        <MenuItem
+          title="Push Notifications"
+          onPress={() => navigate("my-profile/push-notifications")}
+        />
+        <Separator my={1} borderColor={separatorColor} />
 
-      <MenuItem title="Personal Data Request" onPress={() => navigate("privacy-request")} />
-      <Separator my={1} borderColor={separatorColor} />
+        <MenuItem
+          title="Send Feedback"
+          onPress={() => presentEmailComposer("support@artsy.net", "Feedback from the Artsy app")}
+        />
+        <Separator my={1} borderColor={separatorColor} />
 
-      <MenuItem title="About" onPress={() => navigate("about")} />
-      <Separator my={1} borderColor={separatorColor} />
+        <MenuItem title="Personal Data Request" onPress={() => navigate("privacy-request")} />
+        <Separator my={1} borderColor={separatorColor} />
 
-      <Flex flexDirection="row" alignItems="center" justifyContent="center" py="7.5px" px={2}>
-        <Button variant="fillDark" haptic onPress={confirmLogout} block>
-          Log Out
-        </Button>
-      </Flex>
-      <Spacer y={1} />
-    </ScrollView>
+        <MenuItem title="About" onPress={() => navigate("about")} />
+        <Separator my={1} borderColor={separatorColor} />
+
+        <Flex flexDirection="row" alignItems="center" justifyContent="center" py="7.5px" px={2}>
+          <Button variant="fillDark" haptic onPress={confirmLogout} block>
+            Log Out
+          </Button>
+        </Flex>
+        <Spacer y={1} />
+      </ScrollView>
+    </>
   )
 }
 
