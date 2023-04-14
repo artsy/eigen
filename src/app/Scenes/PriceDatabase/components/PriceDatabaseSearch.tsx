da02f64ed348fec4308fba899d6f15ca3289d44d
Ole <ole.richter91@gmail.com>
2023-04-14 11:38:08 +0200

    fix: ADd keyboardShouldPersistTaps to price database scroll view

--- src/app/Scenes/PriceDatabase/components/PriceDatabaseSearch.tsx
+++ src/app/Scenes/PriceDatabase/components/PriceDatabaseSearch.tsx
@@ -51,7 +51,7 @@ export const PriceDatabaseSearch: React.FC<StackScreenProps<ArtworkFilterNavigat
 
   return (
     <ArtsyKeyboardAvoidingView>
-      <ScrollView>
+      <ScrollView keyboardShouldPersistTaps={true}>
         <Flex my={2}>
           <Flex mx={2} mb={2}>
             <Text variant="xl">Artsy Price</Text>
