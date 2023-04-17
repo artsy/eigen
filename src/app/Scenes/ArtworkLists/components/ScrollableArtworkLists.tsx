c9132047c59a77207cb67063d4eaf0cb8944d0be
Dima Tretyak <itretyak@ya.ru>
2023-04-17 20:22:33 +0300

    refactor: add `ArtworkListsContent` component

--- src/app/Scenes/ArtworkLists/components/ScrollableArtworkLists.tsx
+++ src/app/Scenes/ArtworkLists/components/ScrollableArtworkLists.tsx
@@ -7,23 +7,29 @@ const artworkLists = times(20).map((index) => ({
   artworksCount: index * 10,
 }))
 
+export const ArtworkListsContent = () => {
+  return (
+    <Join separator={<Spacer y={1} />}>
+      {artworkLists.map((artworkList, index) => {
+        return (
+          <Flex key={`artwork-list-${index}`}>
+            <Text variant="xs">{artworkList.name}</Text>
+            <Text variant="xs" color="black60">
+              {artworkList.artworksCount} Artworks
+            </Text>
+          </Flex>
+        )
+      })}
+    </Join>
+  )
+}
+
 export const ScrollableArtworkLists = () => {
   const space = useSpace()
 
   return (
     <ScrollView contentContainerStyle={{ paddingHorizontal: space(2), paddingBottom: space(2) }}>
-      <Join separator={<Spacer y={1} />}>
-        {artworkLists.map((artworkList, index) => {
-          return (
-            <Flex key={`artwork-list-${index}`}>
-              <Text variant="xs">{artworkList.name}</Text>
-              <Text variant="xs" color="black60">
-                {artworkList.artworksCount} Artworks
-              </Text>
-            </Flex>
-          )
-        })}
-      </Join>
+      <ArtworkListsContent />
     </ScrollView>
   )
 }
