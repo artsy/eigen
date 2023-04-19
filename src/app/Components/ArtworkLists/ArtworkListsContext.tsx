076bfe072a7c631b47db4e84039dea9898075e53
Dima Tretyak <itretyak@ya.ru>
2023-04-19 14:59:06 +0300

    feat: add artwork lists context

---
+++ src/app/Components/ArtworkLists/ArtworkListsContext.tsx
@@ -0,0 +1,187 @@
+import { createContext, Dispatch, FC, useContext, useReducer, useState } from "react"
+
+export enum SceneKey {
+  SelectListsForArtwork,
+  CreateNewList,
+}
+
+export interface RecentlyAddedArtworkList {
+  internalID: string
+  name: string
+}
+
+type State = {
+  currentSceneKey: SceneKey
+  artwork: ArtworkEntity | null
+  addingArtworkListIDs: string[]
+  removingArtworkListIDs: string[]
+  recentlyAddedArtworkList: RecentlyAddedArtworkList | null
+}
+
+export enum Mode {
+  AddingArtworkListIDs = "addingArtworkListIDs",
+  RemovingArtworkListIDs = "removingArtworkListIDs",
+}
+
+type Action =
+  | { type: "SET_SCENE_KEY"; payload: SceneKey }
+  | { type: "SET_RECENTLY_ADDED_ARTWORK_LIST"; payload: RecentlyAddedArtworkList | null }
+  | { type: "SET_ARTWORK"; payload: ArtworkEntity | null }
+  | { type: "RESET" }
+  | {
+      type: "ADD_OR_REMOVE_ARTWORK_LIST_ID"
+      payload: { mode: Mode; artworkListID: string }
+    }
+
+export interface ArtworkEntity {
+  id: string
+  internalID: string
+  title: string
+  year: string | null
+  artistNames: string | null
+  imageURL: string | null
+}
+
+export interface ResultArtworkListEntity {
+  id: string
+  name: string
+}
+
+export interface OnSaveResultData {
+  selectedArtworkLists: ResultArtworkListEntity[]
+  removedArtworkLists: ResultArtworkListEntity[]
+  addedArtworkLists: ResultArtworkListEntity[]
+}
+
+export interface ArtworkListsContextState {
+  state: State
+  artworkListId?: string
+  isSavedToArtworkList: boolean
+  dispatch: Dispatch<Action>
+  reset: () => void
+  onSave: (result: OnSaveResultData) => void
+}
+
+interface ArtworkListsProviderProps {
+  artworkListId?: string
+  // Needed for testing purposes
+  artwork?: ArtworkEntity
+}
+
+export const INITIAL_STATE: State = {
+  currentSceneKey: SceneKey.SelectListsForArtwork,
+  artwork: null,
+  addingArtworkListIDs: [],
+  removingArtworkListIDs: [],
+  recentlyAddedArtworkList: null,
+}
+
+export const ArtworkListsContext = createContext<ArtworkListsContextState>(
+  null as unknown as ArtworkListsContextState
+)
+
+export const useArtworkListsContext = () => {
+  return useContext(ArtworkListsContext)
+}
+
+/**
+ *
+ * If `artworkListId` was passed, it means the user is on the artwork lists page
+ * In this case, whether the artwork is saved or not will depend on the local state (not on the status received from backend)
+ */
+export const ArtworkListsProvider: FC<ArtworkListsProviderProps> = ({
+  children,
+  artworkListId,
+  artwork,
+}) => {
+  const [isSavedToArtworkList, setIsSavedToArtworkList] = useState(!!artworkListId)
+  const [state, dispatch] = useReducer(reducer, {
+    ...INITIAL_STATE,
+    artwork: artwork ?? null,
+  })
+
+  const onSave = (result: OnSaveResultData) => {
+    console.log("[debug] save result", result)
+
+    if (artworkListId) {
+      const isSaved = result.selectedArtworkLists.find((list) => list.id === artworkListId)
+
+      setIsSavedToArtworkList(!!isSaved)
+
+      return
+    }
+  }
+
+  const reset = () => {
+    dispatch({
+      type: "RESET",
+    })
+  }
+
+  const value: ArtworkListsContextState = {
+    state,
+    artworkListId,
+    isSavedToArtworkList,
+    dispatch,
+    reset,
+    onSave,
+  }
+
+  const renderSceneByKey = () => {
+    if (state.currentSceneKey === SceneKey.CreateNewList) {
+      console.log("[debug] render CreateNewList scene")
+
+      return
+    }
+
+    console.log("[debug] render SelectListsForArtwork scene")
+  }
+
+  return (
+    <ArtworkListsContext.Provider value={value}>
+      {children}
+      {!!state.artwork && renderSceneByKey()}
+    </ArtworkListsContext.Provider>
+  )
+}
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "SET_SCENE_KEY":
+      return {
+        ...state,
+        currentSceneKey: action.payload,
+      }
+    case "ADD_OR_REMOVE_ARTWORK_LIST_ID":
+      // eslint-disable-next-line no-case-declarations
+      const { artworkListID, mode } = action.payload
+      // eslint-disable-next-line no-case-declarations
+      const ids = state[mode]
+
+      if (ids.includes(artworkListID)) {
+        return {
+          ...state,
+          [mode]: ids.filter((id) => id !== artworkListID),
+        }
+      }
+
+      return {
+        ...state,
+        [mode]: [...ids, artworkListID],
+      }
+    case "SET_RECENTLY_ADDED_ARTWORK_LIST":
+      return {
+        ...state,
+        recentlyAddedArtworkList: action.payload,
+      }
+    case "SET_ARTWORK":
+      return {
+        ...state,
+        artwork: action.payload,
+      }
+    case "RESET":
+      return INITIAL_STATE
+    default:
+      return state
+  }
+}
