d65b16b4c30c5a5e2b1ad8218eb3c42c244cc058
Dima Tretyak <itretyak@ya.ru>
2023-04-19 20:46:49 +0300

    feat: display recently created artwork list

--- src/app/Components/ArtworkLists/views/CreateNewArtworkListView.tsx
+++ src/app/Components/ArtworkLists/views/CreateNewArtworkListView.tsx
@@ -1,11 +1,17 @@
-import { Button } from "@artsy/palette-mobile"
+import { Button, Spacer } from "@artsy/palette-mobile"
+import { BottomSheetView, useBottomSheetDynamicSnapPoints } from "@gorhom/bottom-sheet"
 import { useArtworkListsContext } from "app/Components/ArtworkLists/ArtworkListsContext"
 import { AutomountedBottomSheetModal } from "app/Components/ArtworkLists/components/AutomountedBottomSheetModal"
-
-const SNAP_POINTS = ["50%", "95%"]
+import { BottomSheetTextInput } from "app/Components/ArtworkLists/components/BottomSheetInput"
+import { useMemo, useState } from "react"
 
 export const CreateNewArtworkListView = () => {
+  const [name, setName] = useState("")
   const { dispatch } = useArtworkListsContext()
+  const initialSnapPoints = useMemo(() => ["CONTENT_HEIGHT"], [])
+
+  const { animatedHandleHeight, animatedSnapPoints, animatedContentHeight, handleContentLayout } =
+    useBottomSheetDynamicSnapPoints(initialSnapPoints)
 
   const closeCurrenView = () => {
     dispatch({
@@ -14,11 +20,45 @@ export const CreateNewArtworkListView = () => {
     })
   }
 
+  const setRecentlyAddedArtworkList = () => {
+    dispatch({
+      type: "SET_RECENTLY_ADDED_ARTWORK_LIST",
+      payload: {
+        internalID: "recently-created-artwork-list-id",
+        name,
+      },
+    })
+  }
+
+  const handleSave = () => {
+    // Run save mutation
+    setRecentlyAddedArtworkList()
+    closeCurrenView()
+  }
+
   return (
-    <AutomountedBottomSheetModal visible snapPoints={SNAP_POINTS} onDismiss={closeCurrenView}>
-      <Button width="100%" block variant="outline" onPress={closeCurrenView}>
-        Back
-      </Button>
+    <AutomountedBottomSheetModal
+      visible
+      snapPoints={animatedSnapPoints}
+      handleHeight={animatedHandleHeight}
+      contentHeight={animatedContentHeight}
+      onDismiss={closeCurrenView}
+    >
+      <BottomSheetView onLayout={handleContentLayout}>
+        <BottomSheetTextInput placeholder="Placeholder" value={name} onChangeText={setName} />
+
+        <Spacer y={2} />
+
+        <Button width="100%" block onPress={handleSave}>
+          Save
+        </Button>
+
+        <Spacer y={2} />
+
+        <Button width="100%" block variant="outline" onPress={closeCurrenView}>
+          Back
+        </Button>
+      </BottomSheetView>
     </AutomountedBottomSheetModal>
   )
 }
