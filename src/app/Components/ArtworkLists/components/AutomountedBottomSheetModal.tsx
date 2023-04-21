e344fbc2bb1feff141be2a33eccb186acd874354
Dima Tretyak <itretyak@ya.ru>
2023-04-21 15:07:14 +0300

    feat: add bottom sheets

---
+++ src/app/Components/ArtworkLists/components/AutomountedBottomSheetModal.tsx
@@ -0,0 +1,30 @@
+import { BottomSheetModal, BottomSheetModalProps } from "@gorhom/bottom-sheet"
+import { ArtworkListsBottomSheetBackdrop } from "app/Components/ArtworkLists/components/ArtworkListsBottomSheetBackdrop"
+import { FC, useEffect, useRef } from "react"
+
+interface AutomountedBottomSheetModalProps extends BottomSheetModalProps {
+  visible: boolean
+}
+
+export const AutomountedBottomSheetModal: FC<AutomountedBottomSheetModalProps> = ({
+  visible,
+  ...rest
+}) => {
+  const ref = useRef<BottomSheetModal>(null)
+
+  useEffect(() => {
+    if (visible) {
+      ref.current?.present()
+    }
+  }, [visible])
+
+  return (
+    <BottomSheetModal
+      ref={ref}
+      enablePanDownToClose
+      keyboardBlurBehavior="restore"
+      backdropComponent={ArtworkListsBottomSheetBackdrop}
+      {...rest}
+    />
+  )
+}
