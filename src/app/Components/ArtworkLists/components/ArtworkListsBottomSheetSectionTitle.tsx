c6c28936be3517f8916ddf595c7650bef1a2562e
Dima Tretyak <itretyak@ya.ru>
2023-04-25 15:43:05 +0300

    refactor: Add only top offset

--- src/app/Components/ArtworkLists/components/ArtworkListsBottomSheetSectionTitle.tsx
+++ src/app/Components/ArtworkLists/components/ArtworkListsBottomSheetSectionTitle.tsx
@@ -3,7 +3,7 @@ import { FC } from "react"
 
 export const ArtworkListsBottomSheetSectionTitle: FC = ({ children }) => {
   return (
-    <Text variant="sm-display" textAlign="center" my={1}>
+    <Text variant="sm-display" textAlign="center" mt={1}>
       {children}
     </Text>
   )
