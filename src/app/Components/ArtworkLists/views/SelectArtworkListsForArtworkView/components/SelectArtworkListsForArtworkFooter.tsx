5c5decb1ca9e57002cab2fa42eaa32abc5a46f9d
Dima Tretyak <itretyak@ya.ru>
2023-04-26 21:59:53 +0300

    feat: save mutation

--- src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/SelectArtworkListsForArtworkFooter.tsx
+++ src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/SelectArtworkListsForArtworkFooter.tsx
@@ -1,17 +1,38 @@
 import { Box, Button, Spacer, Text } from "@artsy/palette-mobile"
 import { useBottomSheetModal } from "@gorhom/bottom-sheet"
+import { SelectArtworkListsForArtworkFooterMutation } from "__generated__/SelectArtworkListsForArtworkFooterMutation.graphql"
 import { useArtworkListsContext } from "app/Components/ArtworkLists/ArtworkListsContext"
 import { ArtworkListsViewName } from "app/Components/ArtworkLists/views/constants"
+import { graphql, useMutation } from "react-relay"
 
 export const SelectArtworkListsForArtworkFooter = () => {
   const { state } = useArtworkListsContext()
   const { dismiss } = useBottomSheetModal()
   const { addingArtworkListIDs, removingArtworkListIDs, selectedArtworkListIDs } = state
   const hasChanges = addingArtworkListIDs.length !== 0 || removingArtworkListIDs.length !== 0
+  const artwork = state.artwork!
+
+  const [commit, mutationInProgress] =
+    useMutation<SelectArtworkListsForArtworkFooterMutation>(SaveArtworkListsMutation)
 
   const handleSave = () => {
-    // TODO: Save mutation
-    dismiss(ArtworkListsViewName.SelectArtworkListsForArtwork)
+    commit({
+      variables: {
+        artworkID: artwork.internalID,
+        input: {
+          artworkIDs: [artwork.internalID],
+          addToCollectionIDs: state.addingArtworkListIDs,
+          removeFromCollectionIDs: state.removingArtworkListIDs,
+        },
+      },
+      onCompleted: () => {
+        dismiss(ArtworkListsViewName.SelectArtworkListsForArtwork)
+      },
+      onError: (error) => {
+        // TODO: Log error
+        console.log("[debug] error", error)
+      },
+    })
   }
 
   return (
@@ -22,7 +43,13 @@ export const SelectArtworkListsForArtworkFooter = () => {
 
       <Spacer y={1} />
 
-      <Button width="100%" block disabled={!hasChanges} onPress={handleSave}>
+      <Button
+        width="100%"
+        block
+        disabled={!hasChanges}
+        loading={mutationInProgress}
+        onPress={handleSave}
+      >
         Save
       </Button>
     </Box>
@@ -36,3 +63,32 @@ const getSelectedListsCountText = (count: number) => {
 
   return `${count} lists selected`
 }
+
+const SaveArtworkListsMutation = graphql`
+  mutation SelectArtworkListsForArtworkFooterMutation(
+    $artworkID: String!
+    $input: ArtworksCollectionsBatchUpdateInput!
+  ) {
+    artworksCollectionsBatchUpdate(input: $input) {
+      responseOrError {
+        ... on ArtworksCollectionsBatchUpdateSuccess {
+          addedToArtworkLists: addedToCollections {
+            internalID
+            default
+            ...ArtworkListItem_item @arguments(artworkID: $artworkID)
+          }
+          removedFromArtworkLists: removedFromCollections {
+            internalID
+            default
+            ...ArtworkListItem_item @arguments(artworkID: $artworkID)
+          }
+        }
+        ... on ArtworksCollectionsBatchUpdateFailure {
+          mutationError {
+            statusCode
+          }
+        }
+      }
+    }
+  }
+`
