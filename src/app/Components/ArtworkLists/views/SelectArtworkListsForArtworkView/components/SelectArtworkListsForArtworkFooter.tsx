a4971dd8699dee34bfc4a5d5afe4205a7a14b612
Dima Tretyak <itretyak@ya.ru>
2023-04-27 15:30:26 +0300

    feat(FX-4756): Save updated artwork lists for artwork (#8585)
    * feat: save mutation
    
    * refactor: create custom hook
    
    * chore: ignore cached data
    
    * feat: add updater
    
    * refactor: use updater inside custom hook
    
    * refactor: rename helper
    
    * tests: add test cases for helper
    
    * chore: capture error

--- src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/SelectArtworkListsForArtworkFooter.tsx
+++ src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/SelectArtworkListsForArtworkFooter.tsx
@@ -1,6 +1,8 @@
 import { Box, Button, Spacer, Text } from "@artsy/palette-mobile"
 import { useBottomSheetModal } from "@gorhom/bottom-sheet"
+import { captureMessage } from "@sentry/react-native"
 import { useArtworkListsContext } from "app/Components/ArtworkLists/ArtworkListsContext"
+import { useUpdateArtworkListsForArtwork } from "app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/useUpdateArtworkListsForArtwork"
 import { ArtworkListsViewName } from "app/Components/ArtworkLists/views/constants"
 
 export const SelectArtworkListsForArtworkFooter = () => {
@@ -8,10 +10,31 @@ export const SelectArtworkListsForArtworkFooter = () => {
   const { dismiss } = useBottomSheetModal()
   const { addingArtworkListIDs, removingArtworkListIDs, selectedArtworkListIDs } = state
   const hasChanges = addingArtworkListIDs.length !== 0 || removingArtworkListIDs.length !== 0
+  const artwork = state.artwork!
+
+  const [commit, mutationInProgress] = useUpdateArtworkListsForArtwork(artwork.id)
 
   const handleSave = () => {
-    // TODO: Save mutation
-    dismiss(ArtworkListsViewName.SelectArtworkListsForArtwork)
+    commit({
+      variables: {
+        artworkID: artwork.internalID,
+        input: {
+          artworkIDs: [artwork.internalID],
+          addToCollectionIDs: state.addingArtworkListIDs,
+          removeFromCollectionIDs: state.removingArtworkListIDs,
+        },
+      },
+      onCompleted: () => {
+        dismiss(ArtworkListsViewName.SelectArtworkListsForArtwork)
+      },
+      onError: (error) => {
+        if (__DEV__) {
+          console.error(error)
+        } else {
+          captureMessage(error?.stack!)
+        }
+      },
+    })
   }
 
   return (
@@ -22,7 +45,13 @@ export const SelectArtworkListsForArtworkFooter = () => {
 
       <Spacer y={1} />
 
-      <Button width="100%" block disabled={!hasChanges} onPress={handleSave}>
+      <Button
+        width="100%"
+        block
+        disabled={!hasChanges}
+        loading={mutationInProgress}
+        onPress={handleSave}
+      >
         Save
       </Button>
     </Box>
---
+++ src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/useUpdateArtworkListsForArtwork.ts
@@ -0,0 +1,136 @@
+import {
+  useUpdateArtworkListsForArtworkMutation,
+  useUpdateArtworkListsForArtworkMutation$data,
+} from "__generated__/useUpdateArtworkListsForArtworkMutation.graphql"
+import { UseMutationConfig, graphql, useMutation } from "react-relay"
+import { Disposable, RecordSourceSelectorProxy } from "relay-runtime"
+
+interface Counts {
+  custom: number
+  default: number
+}
+
+type Response = NonNullable<
+  NonNullable<
+    NonNullable<useUpdateArtworkListsForArtworkMutation$data>["artworksCollectionsBatchUpdate"]
+  >["responseOrError"]
+>
+export type ArtworkListEntity =
+  | Response["addedToArtworkLists"]
+  | Response["removedFromArtworkLists"]
+type MutationResult = [
+  (config: UseMutationConfig<useUpdateArtworkListsForArtworkMutation>) => Disposable,
+  boolean
+]
+
+export const useUpdateArtworkListsForArtwork = (artworkID: string): MutationResult => {
+  const [initialCommit, isInProgress] =
+    useMutation<useUpdateArtworkListsForArtworkMutation>(SaveArtworkListsMutation)
+
+  const commit = (config: UseMutationConfig<useUpdateArtworkListsForArtworkMutation>) => {
+    return initialCommit({
+      ...config,
+      updater: updater(artworkID),
+    })
+  }
+
+  return [commit, isInProgress]
+}
+
+const updater =
+  (artworkID: string) =>
+  (
+    store: RecordSourceSelectorProxy<useUpdateArtworkListsForArtworkMutation$data>,
+    data: useUpdateArtworkListsForArtworkMutation$data
+  ) => {
+    const artwork = store.get(artworkID)
+
+    if (!artwork) {
+      return
+    }
+
+    const response = data.artworksCollectionsBatchUpdate?.responseOrError
+    const addedCounts = getArtworkListsCountByType(response?.addedToArtworkLists)
+    const removedCounts = getArtworkListsCountByType(response?.removedFromArtworkLists)
+
+    // Set `isSaved` field to `true` if artwork was saved in "Saved Artworks"
+    if (addedCounts.default > 0) {
+      artwork.setValue(true, "isSaved")
+    }
+
+    // Set `isSaved` field to `false` if artwork was unsaved from "Saved Artworks"
+    if (removedCounts.default > 0) {
+      artwork.setValue(false, "isSaved")
+    }
+
+    const entity = artwork.getLinkedRecord("collectionsConnection", {
+      first: 0,
+      default: false,
+      saves: true,
+    })
+
+    if (!entity) {
+      return
+    }
+
+    /**
+     * Update `totalCount` field, based on which we decide
+     * whether to display the manage lists for artwork modal or
+     * immediately remove artwork from "Saved Artworks"
+     */
+    const prevValue = (entity.getValue("totalCount") ?? 0) as number
+    const newValue = prevValue + addedCounts.custom - removedCounts.custom
+
+    entity.setValue(newValue, "totalCount")
+  }
+
+export const getArtworkListsCountByType = (artworkLists: ArtworkListEntity) => {
+  const artworkListsEntities = artworkLists ?? []
+  const defaultCounts: Counts = {
+    custom: 0,
+    default: 0,
+  }
+
+  return artworkListsEntities.reduce((acc, artworkList) => {
+    if (!artworkList) {
+      return acc
+    }
+
+    const key: keyof Counts = artworkList.default ? "default" : "custom"
+    const prevCount = acc[key]
+
+    return {
+      ...acc,
+      [key]: prevCount + 1,
+    }
+  }, defaultCounts)
+}
+
+const SaveArtworkListsMutation = graphql`
+  mutation useUpdateArtworkListsForArtworkMutation(
+    $artworkID: String!
+    $input: ArtworksCollectionsBatchUpdateInput!
+  ) {
+    artworksCollectionsBatchUpdate(input: $input) {
+      responseOrError {
+        ... on ArtworksCollectionsBatchUpdateSuccess {
+          addedToArtworkLists: addedToCollections {
+            internalID
+            default
+            ...ArtworkListItem_item @arguments(artworkID: $artworkID)
+          }
+          removedFromArtworkLists: removedFromCollections {
+            internalID
+            default
+            ...ArtworkListItem_item @arguments(artworkID: $artworkID)
+          }
+        }
+        ... on ArtworksCollectionsBatchUpdateFailure {
+          mutationError {
+            statusCode
+          }
+        }
+      }
+    }
+  }
+`
