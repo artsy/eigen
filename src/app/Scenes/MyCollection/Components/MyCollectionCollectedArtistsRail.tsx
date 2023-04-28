0582f8ed2520ad988af0ebf2b86dcd69526c5e26
Mounir Dhahri <soussouheros@gmail.com>
2023-04-28 17:02:44 +0200

    feat: add collected artists rail

---
+++ src/app/Scenes/MyCollection/Components/MyCollectionCollectedArtistsRail.tsx
@@ -0,0 +1,32 @@
+import { Flex, Spacer, Text } from "@artsy/palette-mobile"
+import { ScrollView } from "react-native"
+
+export const MyCollectionCollectedArtistsRail = () => {
+  return (
+    <ScrollView horizontal>
+      <Artist />
+      <Spacer x={2} />
+      <Artist />
+      <Spacer x={2} />
+      <Artist />
+    </ScrollView>
+  )
+}
+
+export const ARTIST_CIRCLE_DIAMETER = 80
+
+const Artist: React.FC = () => {
+  return (
+    <Flex width={ARTIST_CIRCLE_DIAMETER}>
+      <Flex
+        height={ARTIST_CIRCLE_DIAMETER}
+        width={ARTIST_CIRCLE_DIAMETER}
+        borderRadius={ARTIST_CIRCLE_DIAMETER / 2}
+        backgroundColor="black10"
+      ></Flex>
+      <Text variant="xs" numberOfLines={2} textAlign="center">
+        Artist Name
+      </Text>
+    </Flex>
+  )
+}
