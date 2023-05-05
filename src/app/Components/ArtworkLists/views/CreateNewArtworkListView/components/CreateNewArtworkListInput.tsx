e1807fa1e4c9e9836d0c1bbe896c3d65a87d12d0
Dima Tretyak <itretyak@ya.ru>
2023-05-05 15:01:36 +0300

    fix: always use fixed height for error/remaining label in create artwork list flow (#8626)
    fix: always use fixed height for error/remaining label

--- src/app/Components/ArtworkLists/views/CreateNewArtworkListView/components/CreateNewArtworkListInput.tsx
+++ src/app/Components/ArtworkLists/views/CreateNewArtworkListView/components/CreateNewArtworkListInput.tsx
@@ -1,4 +1,4 @@
-import { InputProps } from "@artsy/palette-mobile"
+import { Box, Flex, InputProps, Text, computeBorderColor, useColor } from "@artsy/palette-mobile"
 import { BottomSheetInput } from "app/Components/BottomSheetInput"
 import { RemainingCharactersLabel } from "./RemainingCharactersLabel"
 
@@ -7,11 +7,23 @@ interface CreateNewArtworkListInputProps extends InputProps {
   maxLength: number
 }
 
-export const CreateNewArtworkListInput = (props: CreateNewArtworkListInputProps) => {
+export const CreateNewArtworkListInput = ({ error, ...rest }: CreateNewArtworkListInputProps) => {
+  const color = useColor()
+  const borderColor = color(computeBorderColor({ error: !!error }))
+
   return (
-    <>
-      <BottomSheetInput {...props} />
-      <RemainingCharactersLabel currentLength={props.value.length} maxLength={props.maxLength} />
-    </>
+    <Box>
+      <BottomSheetInput {...rest} style={{ borderColor: borderColor }} />
+
+      <Flex height={25} justifyContent="center" mt={1}>
+        {!!error ? (
+          <Text variant="xs" color="red100">
+            {error}
+          </Text>
+        ) : (
+          <RemainingCharactersLabel currentLength={rest.value.length} maxLength={rest.maxLength} />
+        )}
+      </Flex>
+    </Box>
   )
 }
