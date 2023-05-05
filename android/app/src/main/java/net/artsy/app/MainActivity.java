e5f21b16ccad181c11a217c687e1439218c4bd03
George Kartalis <g.kartalis7@gmail.com>
2023-05-05 17:13:35 +0200

    build(deps): bump react-native from 0.67.5 to 0.68.7

--- android/app/src/main/java/net/artsy/app/MainActivity.java
+++ android/app/src/main/java/net/artsy/app/MainActivity.java
@@ -15,6 +15,8 @@ import androidx.annotation.Nullable;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
 import com.zoontek.rnbootsplash.RNBootSplash;
 import com.dieam.reactnativepushnotification.modules.RNPushNotification;
 
@@ -31,6 +33,27 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "eigen";
+  }
+
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * you can specify the rendered you wish to use (Fabric or the older renderer).
+  */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new MainActivityDelegate(this, getMainComponentName());
+  }
+  public static class MainActivityDelegate extends ReactActivityDelegate {
+    public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
+      super(activity, mainComponentName);
+    }
+    @Override
+    protected ReactRootView createRootView() {
+      ReactRootView reactRootView = new ReactRootView(getContext());
+      // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+      reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+      return reactRootView;
+    }
   }
 
   private boolean isTablet() {
