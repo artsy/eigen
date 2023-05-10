cd1c22571f0af44ad9526e30139bbf94519255ae
Dima Tretyak <itretyak@ya.ru>
2023-05-10 21:25:18 +0300

    chore: place bottom sheet provider before toast

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -64,8 +64,8 @@ export const TestProviders: React.FC<{ skipRelay?: boolean }> = ({
       Theme,
       PopoverMessageProvider,
       ShareSheetProvider,
-      ToastProvider,
       ArtworkListsProvider,
+      ToastProvider,
     ],
     children
   )
