2471d2f98ac85532eca6bb1bb4b570eeadcbd448
George Kartalis <g.kartalis7@gmail.com>
2023-05-11 12:19:58 +0200

    fix(sourcemap): attempt

--- scripts/hermes-android-bundle-bytecode.sh
+++ scripts/hermes-android-bundle-bytecode.sh
@@ -3,10 +3,10 @@
 NODE_OPTIONS=--max_old_space_size=8192
 
 platform=$(node -e "const platform=require('os').platform();console.log(platform === 'darwin' ? 'osx-bin' : (platform === 'linux' ? 'linux64-bin' : (platform === 'win32' ? 'win64-bin' : 'unsupported-os')));")
+BUNDLE_DIR="android/app/src/main/assets"
 
 ./node_modules/react-native/sdks/hermesc/$platform/hermesc \
 -O -emit-binary \
 -output-source-map \
--out=android/app/src/main/assets/index.android.bundle.hbc android/app/src/main/assets/index.android.bundle \
-&& rm -f android/app/src/main/assets/index.android.bundle \
-&& mv android/app/src/main/assets/index.android.bundle.hbc android/app/src/main/assets/index.android.bundle
+-out="$BUNDLE_DIR/index.android.bundle.hbc" "$BUNDLE_DIR/index.android-js.bundle" \
+&& mv "$BUNDLE_DIR/index.android.bundle.hbc" "$BUNDLE_DIR/index.android.bundle"
--- scripts/hermes-android-bundle.sh
+++ scripts/hermes-android-bundle.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NODE_OPTIONS=--max_old_space_size=8192
+BUNDLE_DIR="android/app/src/main/assets"
 
 react-native bundle \
 --dev false \
@@ -8,6 +9,6 @@ react-native bundle \
 --entry-file index.android.js \
 --reset-cache \
 --assets-dest android/app/src/main/res \
---bundle-output android/app/src/main/assets/index.android.bundle \
---sourcemap-output android/app/src/main/assets/index.android.bundle.packager.map \
+--bundle-output $BUNDLE_DIR/index.android-js.bundle \
+--sourcemap-output $BUNDLE_DIR/index.android.bundle.packager.map \
 --minify false
