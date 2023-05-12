6a8db0cd6f44527a7c167775d2232c04da62fd26
George Kartalis <g.kartalis7@gmail.com>
2023-05-12 18:04:04 +0200

    chore: replace pods hermes with rn hermes

--- scripts/hermes-ios-bundle-bytecode.sh
+++ scripts/hermes-ios-bundle-bytecode.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+platform=$(node -e "const platform=require('os').platform();console.log(platform === 'darwin' ? 'osx-bin' : (platform === 'linux' ? 'linux64-bin' : (platform === 'win32' ? 'win64-bin' : 'unsupported-os')));")
 BUNDLE_DIR="dist"
 
-ios/Pods/hermes-engine/destroot/bin/hermesc \
+./node_modules/react-native/sdks/hermesc/$platform/hermesc \
 -O -emit-binary \
 -output-source-map \
 -out="$BUNDLE_DIR/main.jsbundle.hbc" "$BUNDLE_DIR/main.jsbundle" \
