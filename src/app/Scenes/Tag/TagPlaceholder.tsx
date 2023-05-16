46d4a3a992191c0874e23a64ef25719453574525
George Kartalis <g.kartalis7@gmail.com>
2023-05-16 13:05:05 +0200

    refactor: tag placeholder

--- src/app/Scenes/Tag/TagPlaceholder.tsx
+++ src/app/Scenes/Tag/TagPlaceholder.tsx
@@ -1,23 +1,38 @@
-import { Spacer, Flex, Separator } from "@artsy/palette-mobile"
-import { PlaceholderGrid, PlaceholderText } from "app/utils/placeholders"
+import { Flex, Separator, Screen, Box, Spacer } from "@artsy/palette-mobile"
+import {
+  PlaceholderButton,
+  PlaceholderGrid,
+  PlaceholderText,
+  ProvidePlaceholderContext,
+} from "app/utils/placeholders"
 
-// TODO: redo
-export const TagPlaceholder: React.FC = () => (
-  <Flex>
-    <Flex px={2}>
-      {/* Entity name */}
-      <PlaceholderText />
-    </Flex>
-    <Spacer y={1} />
-    <Separator />
-    <Spacer y={2} />
-    {/* Filters */}
-    <Flex justifyContent="space-between" flexDirection="row" px={2}>
-      <PlaceholderText width={180} />
-      <PlaceholderText width={50} />
-    </Flex>
-    <Spacer y={0.5} />
-    {/* Grid */}
-    <PlaceholderGrid />
-  </Flex>
-)
+export const TagPlaceholder: React.FC = () => {
+  return (
+    <ProvidePlaceholderContext>
+      <Screen>
+        <Box flexDirection="row" alignItems="center">
+          <Spacer x={2} />
+          <PlaceholderButton width={20} height={20} />
+          <Spacer x={2} />
+          <PlaceholderText marginTop={10} width={100} height={20} />
+        </Box>
+        <Spacer y={1} />
+        <Flex flexDirection="row" justifyContent="space-around" pb={1}>
+          <PlaceholderText marginTop={10} width={100} height={20} />
+          <PlaceholderText marginTop={10} width={100} height={20} />
+        </Flex>
+        <Separator />
+        <Flex accessibilityLabel="Tag results are loading">
+          <Flex height={28} my={1} px={2} justifyContent="space-between">
+            <Flex flex={1} pt={0.5} flexDirection="row">
+              <PlaceholderButton width={20} height={20} />
+              <PlaceholderButton marginLeft={5} width={70} height={20} />
+            </Flex>
+          </Flex>
+          <Separator mb={2} />
+          <PlaceholderGrid />
+        </Flex>
+      </Screen>
+    </ProvidePlaceholderContext>
+  )
+}
