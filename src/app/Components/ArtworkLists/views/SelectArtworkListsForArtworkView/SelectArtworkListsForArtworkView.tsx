001e19119a001d0600774626c0b83f6512bb6b16
Dima Tretyak <itretyak@ya.ru>
2023-05-16 20:56:29 +0300

    chore: add bottom offsets for artwork lists views (#8696)
    * chore: add bottom offset
    
    * refactor: pass offsets via props
    
    * feat: add `useArtworkListsBottomOffset` hook
    
    * feat: add `defaultSpace` arg

--- src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/SelectArtworkListsForArtworkView.tsx
+++ src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/SelectArtworkListsForArtworkView.tsx
@@ -2,6 +2,7 @@ import { Flex } from "@artsy/palette-mobile"
 import { useArtworkListsContext } from "app/Components/ArtworkLists/ArtworkListsContext"
 import { ArtworkListsBottomSheetSectionTitle } from "app/Components/ArtworkLists/components/ArtworkListsBottomSheetSectionTitle"
 import { AutomountedBottomSheetModal } from "app/Components/ArtworkLists/components/AutomountedBottomSheetModal"
+import { useArtworkListsBottomOffset } from "app/Components/ArtworkLists/useArtworkListsBottomOffset"
 import { SelectArtworkListsForArtwork } from "app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/SelectArtworkListsForArtwork"
 import { SelectArtworkListsForArtworkFooter } from "app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/SelectArtworkListsForArtworkFooter"
 import { SelectArtworkListsForArtworkHeader } from "app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/SelectArtworkListsForArtworkHeader"
@@ -11,6 +12,7 @@ const SNAP_POINTS = ["50%", "95%"]
 
 export const SelectArtworkListsForArtworkView = () => {
   const { reset } = useArtworkListsContext()
+  const bottomOffset = useArtworkListsBottomOffset(2)
 
   return (
     <AutomountedBottomSheetModal
@@ -29,7 +31,7 @@ export const SelectArtworkListsForArtworkView = () => {
         <SelectArtworkListsForArtwork />
       </Flex>
 
-      <SelectArtworkListsForArtworkFooter />
+      <SelectArtworkListsForArtworkFooter p={2} pb={`${bottomOffset}px`} />
     </AutomountedBottomSheetModal>
   )
 }
