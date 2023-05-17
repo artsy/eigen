fd833daa66115873a8dcfe9106e2023b4080fffb
Mounir Dhahri <soussouheros@gmail.com>
2023-05-17 14:47:29 +0200

    Revert "fix: hot reload when app is run using yarn start (#8697)"
    This reverts commit 2863a041b2a56cc2ac22f90d235b717258399cbf.

--- src/app/system/relay/middlewares/metaphysicsMiddleware.ts
+++ src/app/system/relay/middlewares/metaphysicsMiddleware.ts
@@ -1,6 +1,7 @@
 import { captureMessage } from "@sentry/react-native"
 import { LegacyNativeModules } from "app/NativeModules/LegacyNativeModules"
-import { getCurrentEmissionState, unsafe__getEnvironment } from "app/store/GlobalStore"
+import { getCurrentEmissionState } from "app/store/GlobalStore"
+import { unsafe__getEnvironment } from "app/store/GlobalStore"
 import { logQueryPath } from "app/utils/loggers"
 import _ from "lodash"
 import { Middleware, urlMiddleware } from "react-relay-network-modern/node8"
@@ -109,8 +110,12 @@ export function persistedQueryMiddleware(): Middleware {
     let body: { variables?: object; query?: string; documentID?: string } = {}
     const queryID = req.getID()
     const variables = req.getVariables()
-
-    body = { documentID: queryID, variables }
+    if (__DEV__) {
+      body = { query: require("../../../../../data/complete.queryMap.json")[queryID], variables }
+      ;(req as any).operation.text = body.query ?? null
+    } else {
+      body = { documentID: queryID, variables }
+    }
 
     if (body && (body.query || body.documentID)) {
       req.fetchOpts.body = JSON.stringify(body)
