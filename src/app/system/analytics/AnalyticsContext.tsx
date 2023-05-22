d64bc40cdc4dd72441cdb29ffcf84442ff1ca3c2
Dima Tretyak <itretyak@ya.ru>
2023-05-22 20:28:00 +0300

    feat: add `AnalyticsContext`

---
+++ src/app/system/analytics/AnalyticsContext.tsx
@@ -0,0 +1,19 @@
+import { OwnerType } from "@artsy/cohesion"
+import { FC, createContext, useContext } from "react"
+
+export interface AnalyticsContextProps {
+  contextOwnerId?: string
+  contextOwnerSlug?: string
+  // TODO: Or better to use ScreenOwnerType
+  contextOwnerType?: OwnerType
+}
+
+const AnalyticsContext = createContext<AnalyticsContextProps>({})
+
+export const AnalyticsContextProvider: FC<AnalyticsContextProps> = ({ children, ...rest }) => {
+  return <AnalyticsContext.Provider value={rest}>{children}</AnalyticsContext.Provider>
+}
+
+export const useAnalyticsContext = () => {
+  return useContext(AnalyticsContext)
+}
