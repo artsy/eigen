69b96a15fc85b9afc46262bbb5502a56ec3e2c37
Dima Tretyak <itretyak@ya.ru>
2023-05-22 20:05:30 +0300

    feat: track `addedArtworkToArtworkList`

--- src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/SelectArtworkListsForArtworkFooter.tsx
+++ src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/SelectArtworkListsForArtworkFooter.tsx
@@ -1,3 +1,4 @@
+import { ActionType, AddedArtworkToArtworkList, OwnerType } from "@artsy/cohesion"
 import { Box, BoxProps, Button, Spacer, Text } from "@artsy/palette-mobile"
 import { useBottomSheetModal } from "@gorhom/bottom-sheet"
 import { captureMessage } from "@sentry/react-native"
@@ -6,10 +7,12 @@ import { ResultAction } from "app/Components/ArtworkLists/types"
 import { useUpdateArtworkListsForArtwork } from "app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/useUpdateArtworkListsForArtwork"
 import { ArtworkListsViewName } from "app/Components/ArtworkLists/views/constants"
 import { FC } from "react"
+import { useTracking } from "react-tracking"
 
 export const SelectArtworkListsForArtworkFooter: FC<BoxProps> = (props) => {
   const { state, addingArtworkListIDs, removingArtworkListIDs, onSave } = useArtworkListsContext()
   const { dismiss } = useBottomSheetModal()
+  const { trackEvent } = useTracking()
   const { selectedTotalCount } = state
   const hasChanges = addingArtworkListIDs.length !== 0 || removingArtworkListIDs.length !== 0
   const artwork = state.artwork!
@@ -17,6 +20,20 @@ export const SelectArtworkListsForArtworkFooter: FC<BoxProps> = (props) => {
     selectedTotalCount + addingArtworkListIDs.length - removingArtworkListIDs.length
 
   const [commit, mutationInProgress] = useUpdateArtworkListsForArtwork(artwork.id)
+
+  const trackAddedArtworkToArtworkLists = () => {
+    const event: AddedArtworkToArtworkList = {
+      action: ActionType.addedArtworkToArtworkList,
+      context_owner_id: artwork.internalID,
+      // TODO: Keep artwork slug in ArtworkEntity
+      // context_owner_slug: artwork.slug,
+      context_owner_type: OwnerType.artwork,
+      artwork_ids: [artwork.internalID],
+      owner_ids: addingArtworkListIDs,
+    }
+
+    trackEvent(event)
+  }
 
   const handleSave = () => {
     commit({
@@ -29,6 +46,10 @@ export const SelectArtworkListsForArtworkFooter: FC<BoxProps> = (props) => {
         },
       },
       onCompleted: () => {
+        if (addingArtworkListIDs.length > 0) {
+          trackAddedArtworkToArtworkLists()
+        }
+
         dismiss(ArtworkListsViewName.SelectArtworkListsForArtwork)
         onSave({
           action: ResultAction.ModifiedArtworkLists,
