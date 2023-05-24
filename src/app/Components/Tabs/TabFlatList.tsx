5d0526efe27ef8af6c3a004bef7055b9b2400f5d
George Kartalis <g.kartalis7@gmail.com>
2023-05-24 13:43:11 +0200

    feat: add listener to tab.scrollview/flatlist

--- src/app/Components/Tabs/TabFlatList.tsx
+++ src/app/Components/Tabs/TabFlatList.tsx
@@ -1,8 +1,10 @@
 import { useSpace } from "@artsy/palette-mobile"
+import { useListenForTabContentScroll } from "app/Components/Tabs/useListenForTabContentScroll"
 import { FlatListProps } from "react-native"
 import { Tabs } from "react-native-collapsible-tab-view"
 
 export function TabFlatList<T>(props: FlatListProps<T>) {
+  useListenForTabContentScroll()
   const space = useSpace()
 
   const contentContainerStyle = (props.contentContainerStyle ?? {}) as object
--- src/app/Components/Tabs/TabScrollView.tsx
+++ src/app/Components/Tabs/TabScrollView.tsx
@@ -1,8 +1,10 @@
 import { useSpace } from "@artsy/palette-mobile"
+import { useListenForTabContentScroll } from "app/Components/Tabs/useListenForTabContentScroll"
 import { ScrollViewProps } from "react-native"
 import { Tabs } from "react-native-collapsible-tab-view"
 
 export const TabScrollView: React.FC<ScrollViewProps> = (props) => {
+  useListenForTabContentScroll()
   const space = useSpace()
 
   const contentContainerStyle = (props.contentContainerStyle ?? {}) as object
