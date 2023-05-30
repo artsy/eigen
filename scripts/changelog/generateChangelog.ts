fdb0196eafcf26de07064a703f7661ca011d76bd
brainbicycle <brian.beckerle@artsymail.com>
2023-05-30 13:13:26 -0400

    pr review cleanup
    Co-authored-by: Dima Tretyak <itretyak@ya.ru>

--- scripts/changelog/generateChangelog.ts
+++ scripts/changelog/generateChangelog.ts
@@ -9,6 +9,13 @@ import yargs from "yargs/yargs"
 
 const octokit = new Octokit({ auth: process.env.CHANGELOG_GITHUB_TOKEN_KEY })
 
+const SECTIONS = [
+  "Cross-platform user-facing changes",
+  "iOS user-facing changes",
+  "Android user-facing changes",
+  "Dev changes",
+]
+
 async function getPrsBetweenTags(tag1: string, tag2: string): Promise<PullsGetResponse[]> {
   const compare = await octokit.repos.compareCommits({
     owner: "artsy",
@@ -33,36 +40,65 @@ async function getPrsBetweenTags(tag1: string, tag2: string): Promise<PullsGetRe
   return prs
 }
 
+const parseSectionChanges = (content: string) => {
+  const entries = content.split("\n")
+
+  // Remove section title (e.g `#### iOS user-facing changes`)
+  entries.splice(0, 1)
+
+  const trimmedEntries = entries.map((entry) => entry.trim())
+  const filledEntries = trimmedEntries.filter((entry) => {
+    const isNotEmpty = entry.length > 0
+    const isNotEntryDelimiter = entry !== "-"
+
+    return isNotEmpty && isNotEntryDelimiter
+  })
+
+  return filledEntries
+}
+
+const parseSectionPositions = (section: string, body: string) => {
+  const startSectionPosition = body.indexOf(`#### ${section}`)
+
+  if (startSectionPosition === -1) {
+    return null
+  }
+
+  let endSectionPosition = body.indexOf("####", startSectionPosition + 1)
+
+  /**
+   * If we didn't get the position of the next section,
+   * it means that the current section is the **last** section.
+   * Trying to get the position of the end of the block with changelog sections
+   */
+  if (endSectionPosition === -1) {
+    endSectionPosition = body.indexOf("<!-- end_changelog_updates -->", startSectionPosition + 1)
+  }
+
+  if (startSectionPosition !== -1 && endSectionPosition !== -1) {
+    return {
+      start: startSectionPosition,
+      end: endSectionPosition,
+    }
+  }
+
+  return null
+}
+
 async function getChangelogFromPrs(prs: PullsGetResponse[]) {
   let changelog = ""
   const prsWithoutChangelog: PullsGetResponse[] = []
   for (const pr of prs) {
     const prBody = pr.body
     let prHasChangelog = false
-    for (const section of [
-      "Cross-platform user-facing changes",
-      "iOS user-facing changes",
-      "Android user-facing changes",
-      "Dev changes",
-    ]) {
-      const sectionStart = prBody.indexOf(`#### ${section}`)
-      if (sectionStart !== -1) {
-        let sectionEnd = prBody.indexOf("####", sectionStart + 1)
-        sectionEnd =
-          sectionEnd !== -1
-            ? sectionEnd
-            : prBody.indexOf("<!-- end_changelog_updates -->", sectionStart + 1)
-        if (sectionEnd !== -1) {
-          const entries = prBody
-            .slice(sectionStart, sectionEnd)
-            .split("\n")
-            .slice(1)
-            .map((x) => x.trim())
-            .filter((x) => x && x !== "-")
-          for (const entry of entries) {
-            changelog += `${entry} : ${section} : PR #${pr.number}\n`
-            prHasChangelog = true
-          }
+    for (const section of SECTIONS) {
+      const positions = parseSectionPositions(section, prBody)
+      if (positions !== null) {
+        const sectionContent = prBody.slice(positions.start, positions.end)
+        const sectionChanges = parseSectionChanges(sectionContent)
+        for (const entry of sectionChanges) {
+          changelog += `${entry} : ${section} : PR #${pr.number}\n`
+          prHasChangelog = true
         }
       }
     }
