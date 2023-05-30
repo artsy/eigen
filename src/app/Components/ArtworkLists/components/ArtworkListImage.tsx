b656700b800554cba5ffa37b057edf2e21f89a72
Dima Tretyak <itretyak@ya.ru>
2023-05-30 16:05:22 +0300

    refactor: use correct image placeholders for artwork lists (#8778)
    * refactor: make `ArtworkListNoImage` reusable
    
    * refactor: use `NoImage`
    
    * refactor: make `ArtworkListImageBorder` reusable
    
    * refactor: make `ArtworkListImage` reusable
    
    * refactor: reuse artwork list image component
    
    * refactor: use the same component for artwork list images
    
    * feat: add `ArtworkListImagePreview`

---
+++ src/app/Components/ArtworkLists/components/ArtworkListImage.tsx
@@ -0,0 +1,32 @@
+import { Flex, FlexProps } from "@artsy/palette-mobile"
+import { ArtworkListNoImage } from "app/Components/ArtworkLists/components/ArtworkListNoImage"
+import { OpaqueImageView } from "app/Components/OpaqueImageView2"
+import { FC } from "react"
+
+export interface ArtworkListImageProps extends FlexProps {
+  size: number
+  imageURL: string | null
+  withoutBorder?: boolean
+}
+
+export const ArtworkListImage: FC<ArtworkListImageProps> = ({
+  imageURL,
+  size,
+  withoutBorder,
+  ...rest
+}) => {
+  return (
+    <Flex bg="black5" {...rest}>
+      {imageURL ? (
+        <OpaqueImageView imageURL={imageURL} width={size} height={size} />
+      ) : (
+        <ArtworkListNoImage
+          width={size}
+          height={size}
+          borderColor="black15"
+          borderWidth={withoutBorder ? 0 : 1}
+        />
+      )}
+    </Flex>
+  )
+}
