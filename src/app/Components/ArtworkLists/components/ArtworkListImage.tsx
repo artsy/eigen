5e08dbc2e234d17a3cd5c5290d047fb298b3c434
Dima Tretyak <itretyak@ya.ru>
2023-05-30 15:43:22 +0300

    feat: add `ArtworkListImagePreview`

--- src/app/Components/ArtworkLists/components/ArtworkListImage.tsx
+++ src/app/Components/ArtworkLists/components/ArtworkListImage.tsx
@@ -3,18 +3,29 @@ import { ArtworkListNoImage } from "app/Components/ArtworkLists/components/Artwo
 import { OpaqueImageView } from "app/Components/OpaqueImageView2"
 import { FC } from "react"
 
-interface ArtworkListImageProps extends FlexProps {
+export interface ArtworkListImageProps extends FlexProps {
   size: number
   imageURL: string | null
+  withoutBorder?: boolean
 }
 
-export const ArtworkListImage: FC<ArtworkListImageProps> = ({ imageURL, size, ...rest }) => {
+export const ArtworkListImage: FC<ArtworkListImageProps> = ({
+  imageURL,
+  size,
+  withoutBorder,
+  ...rest
+}) => {
   return (
     <Flex bg="black5" {...rest}>
       {imageURL ? (
         <OpaqueImageView imageURL={imageURL} width={size} height={size} />
       ) : (
-        <ArtworkListNoImage width={size} height={size} borderColor="black15" borderWidth={1} />
+        <ArtworkListNoImage
+          width={size}
+          height={size}
+          borderColor="black15"
+          borderWidth={withoutBorder ? 0 : 1}
+        />
       )}
     </Flex>
   )
--- src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/ArtworkListItem.tsx
+++ src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/ArtworkListItem.tsx
@@ -1,7 +1,7 @@
 import { Flex, Join, Spacer, Text } from "@artsy/palette-mobile"
 import { ArtworkListItem_item$key } from "__generated__/ArtworkListItem_item.graphql"
 import { useArtworkListsContext } from "app/Components/ArtworkLists/ArtworkListsContext"
-import { EntityPreview } from "app/Components/ArtworkLists/components/EntityPreview"
+import { ArtworkListImagePreview } from "app/Components/ArtworkLists/components/ArtworkListImagePreview"
 import { ArtworkListMode } from "app/Components/ArtworkLists/types"
 import { extractNodes } from "app/utils/extractNodes"
 import { FC } from "react"
@@ -71,7 +71,7 @@ export const ArtworkListItem: FC<ArtworkListItemProps> = (props) => {
     <TouchableOpacity onPress={handleArtworkListPress}>
       <Flex py={1} px={2} flexDirection="row" alignItems="center">
         <Join separator={<Spacer x={1} />}>
-          <EntityPreview imageURL={imageURL} />
+          <ArtworkListImagePreview imageURL={imageURL} />
 
           <Flex flex={1}>
             <Text variant="xs">{artworkList.name}</Text>
