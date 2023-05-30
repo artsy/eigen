99a26b779145b6eed6179508c88ce67022fdd161
Ole <ole.richter91@gmail.com>
2023-05-30 11:42:20 +0200

    fix: Check location permission for Android

--- src/app/utils/hooks/useLocationOrIpAddress.ts
+++ src/app/utils/hooks/useLocationOrIpAddress.ts
@@ -1,5 +1,6 @@
 import Geolocation from "@react-native-community/geolocation"
 import { useEffect, useState } from "react"
+import { PermissionsAndroid, Platform } from "react-native"
 
 export interface Location {
   lat: number
@@ -23,6 +24,8 @@ export const useLocationOrIpAddress = (disabled = false) => {
       const response = await fetch("https://api.ipify.org")
       const ip = await response.text()
 
+      console.log({ ip })
+
       setIpAddress(ip)
     } catch (error) {
       console.log("Failed to get devices IP address for location.", error)
@@ -31,20 +34,33 @@ export const useLocationOrIpAddress = (disabled = false) => {
     }
   }
 
-  const getLocation = () => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        setLocation({ lat: position.coords.latitude, lng: position.coords.longitude })
+  const getLocation = async () => {
+    let granted = false
 
-        setIsLoading(false)
-      },
-      (error) => {
-        console.log("Couldn't get device's location. Falling back to IP address", error)
-        // Get IP address as a fallback
-        getIpAddress()
-      },
-      { timeout: 15000, maximumAge: 10000 }
-    )
+    // Check if we have location permissions for Android to avoid the app crashing
+    if (Platform.OS === "android") {
+      granted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+    } else {
+      granted = true
+    }
+
+    if (granted) {
+      Geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({ lat: position.coords.latitude, lng: position.coords.longitude })
+
+          setIsLoading(false)
+        },
+        (error) => {
+          console.log("Couldn't get device's location. Falling back to IP address", error)
+          // Get IP address as a fallback
+          getIpAddress()
+        },
+        { timeout: 15000, maximumAge: 10000 }
+      )
+    } else {
+      getIpAddress()
+    }
   }
 
   useEffect(() => {
