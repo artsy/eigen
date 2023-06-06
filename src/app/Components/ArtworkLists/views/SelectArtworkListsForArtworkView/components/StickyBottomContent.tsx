ff71533681d4817b9d75ff27a26b091c4f3531c6
Dima Tretyak <itretyak@ya.ru>
2023-06-06 22:58:21 +0300

    feat: add offsets for device with notch

--- src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/StickyBottomContent.tsx
+++ src/app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/components/StickyBottomContent.tsx
@@ -5,18 +5,22 @@ import {
   useBottomSheetModal,
 } from "@gorhom/bottom-sheet"
 import { useArtworkListsContext } from "app/Components/ArtworkLists/ArtworkListsContext"
+import { useArtworkListsBottomOffset } from "app/Components/ArtworkLists/useArtworkListsBottomOffset"
 import { useSavePendingArtworkListsChanges } from "app/Components/ArtworkLists/views/SelectArtworkListsForArtworkView/useSavePendingArtworkListsChanges"
 import { ArtworkListsViewName } from "app/Components/ArtworkLists/views/constants"
 import { FC } from "react"
 
-const STICKY_BOTTOM_CONTENT_HEIGHT = 120
+const STICKY_BOTTOM_CONTENT_HEIGHT = 100
 
 export const StickyBottomContentPlaceholder = () => {
-  return <Box height={STICKY_BOTTOM_CONTENT_HEIGHT} />
+  const bottomOffset = useArtworkListsBottomOffset(2)
+
+  return <Box height={STICKY_BOTTOM_CONTENT_HEIGHT + bottomOffset} />
 }
 
 export const StickyBottomContent: FC<BottomSheetFooterProps> = ({ animatedFooterPosition }) => {
   const { state, addingArtworkListIDs, removingArtworkListIDs } = useArtworkListsContext()
+  const bottomOffset = useArtworkListsBottomOffset(2)
   const { dismiss } = useBottomSheetModal()
   const { selectedTotalCount } = state
   const totalCount =
@@ -30,22 +34,26 @@ export const StickyBottomContent: FC<BottomSheetFooterProps> = ({ animatedFooter
 
   return (
     <BottomSheetFooter animatedFooterPosition={animatedFooterPosition}>
-      <Box bg="white100" height={STICKY_BOTTOM_CONTENT_HEIGHT} px={2} justifyContent="center">
-        <Text variant="xs" textAlign="center">
-          {getSelectedListsCountText(totalCount)}
-        </Text>
+      <Box bg="white100">
+        <Box height={STICKY_BOTTOM_CONTENT_HEIGHT} px={2} pt={2}>
+          <Text variant="xs" textAlign="center">
+            {getSelectedListsCountText(totalCount)}
+          </Text>
 
-        <Spacer y={1} />
+          <Spacer y={1} />
 
-        <Button
-          width="100%"
-          block
-          disabled={!state.hasUnsavedChanges}
-          loading={inProgress}
-          onPress={save}
-        >
-          Save
-        </Button>
+          <Button
+            width="100%"
+            block
+            disabled={!state.hasUnsavedChanges}
+            loading={inProgress}
+            onPress={save}
+          >
+            Save
+          </Button>
+        </Box>
+
+        <Box height={bottomOffset} />
       </Box>
     </BottomSheetFooter>
   )
