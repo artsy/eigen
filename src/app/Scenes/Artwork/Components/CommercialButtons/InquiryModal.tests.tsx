9267cc72ce1a3c1a24b192e9c580253b47e58538
Tam <kis.tamara91@gmail.com>
2023-06-08 10:17:54 +0200

    refactor: inquiry modal test (#8829)
    * fix(NX-3544): refactor inquiry modal test
    
    * fix(NX-3544): fix failed inquiry test
    
    * fix(NX-3544): remove unused location mocks
    
    * fix(NX-3544): remove location mocks

--- src/app/Scenes/Artwork/Components/CommercialButtons/InquiryModal.tests.tsx
+++ src/app/Scenes/Artwork/Components/CommercialButtons/InquiryModal.tests.tsx
@@ -1,31 +1,43 @@
-jest.mock("app/utils/googleMaps", () => ({
-  getLocationPredictions: jest.fn(),
-  getLocationDetails: jest.fn(),
-}))
-
+import { fireEvent, screen } from "@testing-library/react-native"
 import { InquiryModalTestsQuery } from "__generated__/InquiryModalTestsQuery.graphql"
-import { FancyModalHeader } from "app/Components/FancyModal/FancyModalHeader"
-import {
-  ArtworkInquiryContext,
-  ArtworkInquiryStateProvider,
-} from "app/utils/ArtworkInquiry/ArtworkInquiryStore"
-import { extractText } from "app/utils/tests/extractText"
+import { ArtworkInquiryContext } from "app/utils/ArtworkInquiry/ArtworkInquiryStore"
 import { flushPromiseQueue } from "app/utils/tests/flushPromiseQueue"
 import { rejectMostRecentRelayOperation } from "app/utils/tests/rejectMostRecentRelayOperation"
-import { renderWithWrappersLEGACY } from "app/utils/tests/renderWithWrappers"
+import { setupTestWrapper } from "app/utils/tests/setupTestWrapper"
 import React from "react"
-import { TextInput } from "react-native"
-import { graphql, QueryRenderer } from "react-relay"
-import { act } from "react-test-renderer"
-import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
+import { graphql } from "react-relay"
 import { InquiryModalFragmentContainer } from "./InquiryModal"
-import { ShippingModal } from "./ShippingModal"
-import { press } from "./helpers"
-
-let env: ReturnType<typeof createMockEnvironment>
 
 const toggleVisibility = jest.fn()
 const onMutationSuccessful = jest.fn()
+const mockDispatch = jest.fn()
+
+const initialState = {
+  shippingLocation: null,
+  inquiryType: null,
+  message: null,
+  inquiryQuestions: [],
+}
+
+const { renderWithRelay } = setupTestWrapper<InquiryModalTestsQuery>({
+  Component: (props) => {
+    return (
+      <ArtworkInquiryContext.Provider
+        value={{ state: initialState, dispatch: mockDispatch }}
+        {...props}
+      >
+        <FakeApp {...props} />
+      </ArtworkInquiryContext.Provider>
+    )
+  },
+  query: graphql`
+    query InquiryModalTestsQuery @relay_test_operation {
+      artwork(id: "pumpkins") {
+        ...InquiryModal_artwork
+      }
+    }
+  `,
+})
 
 // An app shell that holds modal visibility properties
 const FakeApp = (props: InquiryModalTestsQuery["response"]) => {
@@ -47,178 +59,143 @@ const FakeApp = (props: InquiryModalTestsQuery["response"]) => {
   )
 }
 
-interface RenderComponentProps {
-  props: InquiryModalTestsQuery["response"] | null
-  error: Error | null
-}
-
-const renderComponent = ({ props, error }: RenderComponentProps) => {
-  if (props?.artwork) {
-    return (
-      <ArtworkInquiryStateProvider>
-        <FakeApp {...props} />
-      </ArtworkInquiryStateProvider>
-    )
-  } else if (error) {
-    console.log(error)
-  }
-}
-
-const initialState = {
-  shippingLocation: null,
-  inquiryType: null,
-  message: null,
-  inquiryQuestions: [],
-}
-
-const mockDispatch = jest.fn()
-
-const renderComponentWithMockDispatch = ({ props, error }: RenderComponentProps) => {
-  if (props?.artwork) {
-    return (
-      <ArtworkInquiryContext.Provider value={{ state: initialState, dispatch: mockDispatch }}>
-        <FakeApp {...props} />
-      </ArtworkInquiryContext.Provider>
-    )
-  } else if (error) {
-    console.log(error)
-  }
-}
-
-interface TestRenderProps {
-  renderer: (props: RenderComponentProps) => JSX.Element | undefined
-}
-
-const TestRenderer = ({ renderer }: TestRenderProps) => {
-  return (
-    <QueryRenderer<InquiryModalTestsQuery>
-      environment={env}
-      query={graphql`
-        query InquiryModalTestsQuery @relay_test_operation {
-          artwork(id: "pumpkins") {
-            ...InquiryModal_artwork
-          }
-        }
-      `}
-      variables={{}}
-      render={renderer}
-    />
-  )
-}
-
-const mockResolver = {
-  Artwork: () => ({
-    inquiryQuestions: [
-      { internalID: "price_and_availability", question: "Price & Availability" },
-      { internalID: "shipping_quote", question: "Shipping" },
-      { internalID: "condition_and_provenance", question: "Condition & Provance" },
-    ],
-  }),
-}
-
-const getWrapper = (mockResolvers = mockResolver, renderer = renderComponent) => {
-  const tree = renderWithWrappersLEGACY(<TestRenderer renderer={renderer} />)
-  act(() => {
-    env.mock.resolveMostRecentOperation((operation) => {
-      return MockPayloadGenerator.generate(operation, mockResolvers)
-    })
-  })
-  return tree
-}
-
-beforeEach(() => {
-  env = createMockEnvironment()
-})
-
 describe("<InquiryModal />", () => {
   it("renders the modal", () => {
-    const tree = getWrapper()
-    expect(extractText(tree.root)).toContain("What information are you looking for?")
+    renderWithRelay()
+    expect(screen.queryByText("What information are you looking for?")).toBeOnTheScreen()
   })
 
-  it("state resets when exiting and re-entering the modal", async () => {
-    const wrapper = getWrapper(mockResolver, renderComponent)
-    expect(wrapper.root.findByType(InquiryModalFragmentContainer).props.modalIsVisible).toBeTruthy()
-    const checkBox = wrapper.root.findByProps({ testID: "checkbox-shipping_quote" })
-    const input = wrapper.root.findByType(TextInput)
-    checkBox.props.onPress()
+  it("open and close modal", async () => {
+    renderWithRelay()
+    expect(screen.getByTestId("inquiry-modal")).toHaveProp("visible", true)
+
+    const cancelButton = screen.getByTestId("fancy-modal-header-left-button")
+
+    fireEvent.press(cancelButton)
     await flushPromiseQueue()
-    expect(checkBox.props.checked).toBeTruthy()
-    wrapper.root.findByProps({ testID: "checkbox-shipping_quote" }).props.onPress()
-    const testMessage = "Test Message"
-    input.props.onChangeText(testMessage)
-    expect(input.props.value).toBe(testMessage)
-    await press(wrapper.root, { text: "Cancel" })
 
-    expect(wrapper.root.findByType(InquiryModalFragmentContainer).props.modalIsVisible).toBeFalsy()
+    expect(screen.getByTestId("inquiry-modal")).toHaveProp("visible", false)
   })
+})
 
-  describe("user can select 'Price & Availability'", () => {
-    it.todo("user taps checkbox and option is selected")
-  })
-
-  describe("user can select 'Condition & Provenance'", () => {
-    it("user taps checkbox and option is selected", () => {
-      const wrapper = getWrapper(mockResolver, renderComponentWithMockDispatch)
-      wrapper.root.findByProps({ testID: "checkbox-condition_and_provenance" }).props.onPress()
-
-      expect(mockDispatch).toBeCalledWith({
-        payload: {
-          details: null,
-          isChecked: true,
-          questionID: "condition_and_provenance",
-        },
-        type: "selectInquiryQuestion",
-      })
+describe("user can select checkboxes", () => {
+  it("user can select 'Price & Availability'", () => {
+    renderWithRelay({
+      Artwork: () => ({
+        inquiryQuestions: [
+          { internalID: "price_and_availability", question: "Price & Availability" },
+          { internalID: "shipping_quote", question: "Shipping" },
+          { internalID: "condition_and_provenance", question: "Condition & Provance" },
+        ],
+      }),
+    })
+    const checkbox = screen.getByTestId("checkbox-price_and_availability")
+    fireEvent.press(checkbox)
+    expect(mockDispatch).toBeCalledWith({
+      payload: {
+        details: null,
+        isChecked: true,
+        questionID: "price_and_availability",
+      },
+      type: "selectInquiryQuestion",
     })
   })
 
-  describe("when submiting an inquiry", () => {
-    it("it shows error message on failed inquiry", async () => {
-      const wrapper = getWrapper()
-      wrapper.root.findByProps({ testID: "checkbox-shipping_quote" }).props.onPress()
-      press(wrapper.root, { text: "Send" })
-      rejectMostRecentRelayOperation(env, new Error())
-      await flushPromiseQueue()
-      expect(extractText(wrapper.root)).toContain("Sorry, we were unable to send this message")
+  it("user can select 'Shipping quote'", () => {
+    renderWithRelay({
+      Artwork: () => ({
+        inquiryQuestions: [
+          { internalID: "price_and_availability", question: "Price & Availability" },
+          { internalID: "shipping_quote", question: "Shipping" },
+          { internalID: "condition_and_provenance", question: "Condition & Provance" },
+        ],
+      }),
+    })
+
+    const checkbox = screen.getByTestId("checkbox-shipping_quote")
+    fireEvent.press(checkbox)
+
+    expect(mockDispatch).toBeCalledWith({
+      payload: {
+        isChecked: true,
+        questionID: "shipping_quote",
+      },
+      type: "selectInquiryQuestion",
     })
   })
 
-  describe("user can select Shipping", () => {
-    it("user selecting shipping exposes the 'Add your location' CTA", () => {
-      const wrapper = getWrapper()
-      wrapper.root.findByProps({ testID: "checkbox-shipping_quote" }).props.onPress()
-
-      expect(extractText(wrapper.root)).toContain("Add your location")
+  it("user can select 'Condition and provance'", () => {
+    renderWithRelay({
+      Artwork: () => ({
+        inquiryQuestions: [
+          { internalID: "price_and_availability", question: "Price & Availability" },
+          { internalID: "shipping_quote", question: "Shipping" },
+          { internalID: "condition_and_provenance", question: "Condition & Provance" },
+        ],
+      }),
     })
+    const checkbox = screen.getByTestId("checkbox-condition_and_provenance")
+    fireEvent.press(checkbox)
 
-    it("user can visit shipping modal", async () => {
-      const wrapper = getWrapper()
-      wrapper.root.findByProps({ testID: "checkbox-shipping_quote" }).props.onPress()
-
-      expect(extractText(wrapper.root)).toContain("Add your location")
-      expect(wrapper.root.findByType(ShippingModal).props.modalIsVisible).toBeFalsy()
-
-      await press(wrapper.root, { text: /^Add your location/ })
-
-      expect(wrapper.root.findByType(ShippingModal).props.modalIsVisible).toBeTruthy()
-      const header = wrapper.root.findByType(ShippingModal).findByType(FancyModalHeader)
-      expect(extractText(header)).toContain("Add Location")
-    })
-  })
-
-  describe("user can input a custom message", () => {
-    it("message is typed in", () => {
-      const testString = "Test message"
-      const wrapper = getWrapper(mockResolver, renderComponentWithMockDispatch)
-      wrapper.root.findByType(TextInput).props.onChangeText(testString)
-
-      expect(mockDispatch).toBeCalledWith({
-        payload: testString,
-        type: "setMessage",
-      })
+    expect(mockDispatch).toBeCalledWith({
+      payload: {
+        details: null,
+        isChecked: true,
+        questionID: "condition_and_provenance",
+      },
+      type: "selectInquiryQuestion",
     })
   })
 })
 
-// replace testID with testID
+describe("when submiting an inquiry", () => {
+  it("it shows error message on failed inquiry", async () => {
+    const { env } = renderWithRelay(
+      {
+        Artwork: () => ({
+          inquiryQuestions: [
+            { internalID: "price_and_availability", question: "Price & Availability" },
+            { internalID: "shipping_quote", question: "Shipping" },
+            { internalID: "condition_and_provenance", question: "Condition & Provance" },
+          ],
+        }),
+      },
+      {
+        value: {
+          state: {
+            inquiryQuestions: ["test"],
+          },
+          dispatch: mockDispatch,
+        },
+      }
+    )
+
+    const checkbox = screen.getByTestId("checkbox-shipping_quote")
+
+    fireEvent.press(checkbox)
+    const sendButton = screen.getByTestId("fancy-modal-header-right-button")
+
+    fireEvent.press(sendButton)
+    rejectMostRecentRelayOperation(env, new Error())
+
+    await flushPromiseQueue()
+
+    expect(
+      screen.getByText("Sorry, we were unable to send this message. Please try again.")
+    ).toBeOnTheScreen()
+  })
+})
+
+describe("user can add a custom message", () => {
+  it("add custom message", () => {
+    renderWithRelay()
+    const testString = "Test message"
+    const input = screen.getByPlaceholderText("Add a custom note...")
+    fireEvent.changeText(input, testString)
+
+    expect(mockDispatch).toBeCalledWith({
+      payload: testString,
+      type: "setMessage",
+    })
+  })
+})
