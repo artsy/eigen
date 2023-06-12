09856b414ec1b8021180ae14d6a8376d11a83d87
dariakoko <nargle30@gmail.com>
2023-06-12 14:47:12 +0200

    chore: refactor initials function

--- src/app/Scenes/MyCollection/utils/convertNameToInitials.tsx
+++ src/app/Scenes/MyCollection/utils/convertNameToInitials.tsx
@@ -4,12 +4,8 @@ import { take } from "lodash"
 export const Initials = (string = "", length = 3) => {
   if (!string) return null
 
-  // FIXME: Expected 1 arguments, but got 2.
-  // @ts-ignore
-  const letters = take(string.match(/\b[A-Z]/g, ""), length)
+  const letters = take(string.match(/\b[A-Z]/g), length)
   if (letters.length >= 1) return letters.join("").toUpperCase()
 
-  // FIXME: Expected 1 arguments, but got 2.
-  // @ts-ignore
-  return take(string.match(/\b\w/g, ""), length).join("").toUpperCase()
+  return take(string.match(/\b\w/g), length).join("").toUpperCase()
 }
