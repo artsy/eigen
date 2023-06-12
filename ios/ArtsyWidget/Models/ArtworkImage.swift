cd0d3f680307a2acf13b3c55809e85e4c3b816d8
Jon Allured <jon.allured@gmail.com>
2023-06-12 11:02:44 -0500

    feat: Prefer default images in widget GRO-1756 (#8856)
    Prefer default images in widget

--- ios/ArtsyWidget/Models/ArtworkImage.swift
+++ ios/ArtsyWidget/Models/ArtworkImage.swift
@@ -4,10 +4,11 @@ struct ArtworkImage: Codable {
     static func fallback() -> ArtworkImage {
         let artwork = Fixtures.primaryArtwork
         let token = artwork.artworkImages.first!.geminiToken
-        let artworkImage = ArtworkImage(geminiToken: token, position: 1)
+        let artworkImage = ArtworkImage(geminiToken: token, isDefault: true, position: 1)
         return artworkImage
     }
     
     let geminiToken: String
+    let isDefault: Bool
     let position: Int
 }
