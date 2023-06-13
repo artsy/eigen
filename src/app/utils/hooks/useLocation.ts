828a7fb9a4162bde54368ae64d4ebaac293c752d
Ole <ole.richter91@gmail.com>
2023-06-13 09:56:21 +0200

    feat: Use includeShowsNearIpBasedLocation in Shows rail

--- src/app/utils/hooks/useLocation.ts
+++ src/app/utils/hooks/useLocation.ts
@@ -12,25 +12,11 @@ Geolocation.setRNConfiguration({ skipPermissionRequests: true })
 /**
  * Returns the user's location if available, otherwise returns the user's IP address
  * Usage:
- *   const { isLoading, location, ip } = useLocationOrIpAddress()
+ *   const { isLoading, location, ip } = useLocation()
  */
-export const useLocationOrIpAddress = (disabled = false) => {
+export const useLocation = (disabled = false) => {
   const [isLoading, setIsLoading] = useState(!disabled)
   const [location, setLocation] = useState<Location | null>(null)
-  const [ipAddress, setIpAddress] = useState<string | null>(null)
-
-  const getIpAddress = async () => {
-    try {
-      const response = await fetch("https://api.ipify.org")
-      const ip = await response.text()
-
-      setIpAddress(ip)
-    } catch (error) {
-      console.log("Failed to get devices IP address for location.", error)
-    } finally {
-      setIsLoading(false)
-    }
-  }
 
   const getLocation = async () => {
     let granted = false
@@ -50,14 +36,10 @@ export const useLocationOrIpAddress = (disabled = false) => {
           setIsLoading(false)
         },
         (error) => {
-          console.log("Couldn't get device's location. Falling back to IP address", error)
-          // Get IP address as a fallback
-          getIpAddress()
+          console.log("Couldn't get device's location.", error)
         },
-        { timeout: 15000, maximumAge: 10000 }
+        { timeout: 3000, maximumAge: 10000 }
       )
-    } else {
-      getIpAddress()
     }
   }
 
@@ -69,5 +51,5 @@ export const useLocationOrIpAddress = (disabled = false) => {
     getLocation()
   }, [disabled])
 
-  return { isLoading, location, ipAddress }
+  return { isLoading, location }
 }
