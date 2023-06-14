artsy/eigen

commit c43a073ca88f553e5f42eed35ee96d64d6213e55

fix(FX-4814): fixes wrong create collection validation message (#8868)

src/app/Components/ArtworkLists/views/CreateNewArtworkListView/useCreateNewArtworkList.ts
@@ -20,7 +20,18 @@ export const useCreateNewArtworkList = (): Response => {
       ...config,
       onCompleted: (data, errors) => {
         const response = data.createCollection?.responseOrError
-        const errorMessage = response?.mutationError?.message
+
+        // use generic error message by default
+        let errorMessage = "Something went wrong."
+
+        // if there is a specific error message for the name field, use that instead
+        const nameErrorMessage = response?.mutationError?.fieldErrors?.find(
+          (e) => e?.name === "name"
+        )
+
+        if (nameErrorMessage) {
+          errorMessage = nameErrorMessage.message
+        }
 
         if (errorMessage) {
           const error = new Error(errorMessage)
@@ -83,7 +94,10 @@ const CreateNewArtworkListMutation = graphql`
 
         ... on CreateCollectionFailure {
           mutationError {
-            message
+            fieldErrors {
+              name
+              message
+            }
           }
         }
       }

src/app/Components/ArtworkLists/views/CreateNewArtworkListView/CreateNewArtworkListView.tests.tsx
@@ -117,7 +117,12 @@ describe("CreateNewArtworkListView", () => {
             responseOrError: {
               __typename: "CreateCollectionFailure",
               mutationError: {
-                message: errorMessage,
+                fieldErrors: [
+                  {
+                    name: "name",
+                    message: errorMessage,
+                  },
+                ],
               },
             },
           },

commit eb90f2ea7e0c65aa2301146a94cfeda6e249f984

fix(artwork lists): update handling of create/edit mutation error
Fixes a regression introduced in [#8868][] for which we would default to a
generic error message, even if a successful mutation response came back
from Metaphysics.

[#8868]: https://github.com/artsy/eigen/pull/8868

Co-authored-by: Nikita Skalkin <nikskalkin@gmail.com>
Co-authored-by: devon blandin <dblandin@gmail.com>

src/app/Components/ArtworkLists/views/CreateNewArtworkListView/components/CreateNewArtworkListForm.tsx
@@ -70,18 +70,21 @@ export const CreateNewArtworkListForm: FC<FlexProps> = (props) => {
       },
       onCompleted: (data) => {
         const response = data.createCollection?.responseOrError
-        const artworkList = response?.collection!
-        const result: ArtworkListEntity = {
-          name: artworkList.name,
-          internalID: artworkList.internalID,
+
+        if (response?.__typename === "CreateCollectionSuccess") {
+          const artworkList = response?.collection!
+          const result: ArtworkListEntity = {
+            name: artworkList.name,
+            internalID: artworkList.internalID,
+          }
+
+          setRecentlyAddedArtworkList(result)
+          preselectRecentlyAddedArtworkList(result)
+          closeCurrentView()
+          trackAnalyticEvent(artworkList.internalID)
+
+          helpers.setSubmitting(false)
         }
-
-        setRecentlyAddedArtworkList(result)
-        preselectRecentlyAddedArtworkList(result)
-        closeCurrentView()
-        trackAnalyticEvent(artworkList.internalID)
-
-        helpers.setSubmitting(false)
       },
       onError: (error) => {
         helpers.setFieldError("name", error.message)

src/app/Components/ArtworkLists/views/CreateNewArtworkListView/useCreateNewArtworkList.ts
@@ -21,26 +21,26 @@ export const useCreateNewArtworkList = (): Response => {
       onCompleted: (data, errors) => {
         const response = data.createCollection?.responseOrError
 
-        // use generic error message by default
-        let errorMessage = "Something went wrong."
+        if (response?.__typename === "CreateCollectionFailure") {
+          // use generic error message by default
+          let errorMessage = "Something went wrong."
 
-        // if there is a specific error message for the name field, use that instead
-        const nameErrorMessage = response?.mutationError?.fieldErrors?.find(
-          (e) => e?.name === "name"
-        )
+          const nameErrorMessage = response?.mutationError?.fieldErrors?.find(
+            (e) => e?.name === "name"
+          )
 
-        if (nameErrorMessage) {
-          errorMessage = nameErrorMessage.message
+          // if there is a specific error message for the name field, use that instead
+          if (nameErrorMessage) {
+            errorMessage = nameErrorMessage.message
+          }
+
+          if (errorMessage) {
+            const error = new Error(errorMessage)
+            config.onError?.(error)
+          }
+        } else {
+          config.onCompleted?.(data, errors)
         }
-
-        if (errorMessage) {
-          const error = new Error(errorMessage)
-          config.onError?.(error)
-
-          return
-        }
-
-        return config.onCompleted?.(data, errors)
       },
       updater: (store, data) => {
         config.updater?.(store, data)
@@ -84,6 +84,7 @@ const CreateNewArtworkListMutation = graphql`
   mutation useCreateNewArtworkListMutation($input: createCollectionInput!) {
     createCollection(input: $input) {
       responseOrError {
+        __typename
         ... on CreateCollectionSuccess {
           collection {
             internalID

src/app/Scenes/ArtworkList/views/EditArtworkListView/useEditArtworkList.ts
@@ -14,24 +14,25 @@ export const useEditArtworkList = (): Response => {
       onCompleted: (data, errors) => {
         const response = data.updateCollection?.responseOrError
 
-        if (response?.__typename !== "UpdateCollectionFailure") {
-          return config.onCompleted?.(data, errors)
-        }
+        if (response?.__typename === "UpdateCollectionFailure") {
+          // use generic error message by default
+          let errorMessage = "Something went wrong."
 
-        // use generic error message by default
-        let errorMessage = "Something went wrong."
+          const nameErrorMessage = response?.mutationError?.fieldErrors?.find(
+            (e) => e?.name === "name"
+          )
 
-        // if there is a specific error message for the name field, use that instead
-        const nameErrorMessage = response?.mutationError?.fieldErrors?.find(
-          (e) => e?.name === "name"
-        )
-        if (nameErrorMessage) {
-          errorMessage = nameErrorMessage.message
-        }
+          // if there is a specific error message for the name field, use that instead
+          if (nameErrorMessage) {
+            errorMessage = nameErrorMessage.message
+          }
 
-        if (errorMessage) {
-          const error = new Error(errorMessage)
-          config.onError?.(error)
+          if (errorMessage) {
+            const error = new Error(errorMessage)
+            config.onError?.(error)
+          }
+        } else {
+          config.onCompleted?.(data, errors)
         }
       },
     })