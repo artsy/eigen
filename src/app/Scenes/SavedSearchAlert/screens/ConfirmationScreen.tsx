bbd6a837a41b047bb2bede3163a20229d1498984
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2023-06-15 17:25:09 -0400

    feat: rough in labels and artworks grid

--- src/app/Scenes/SavedSearchAlert/screens/ConfirmationScreen.tsx
+++ src/app/Scenes/SavedSearchAlert/screens/ConfirmationScreen.tsx
@@ -1,14 +1,17 @@
 import { Box, Flex, Join, Spacer, Text } from "@artsy/palette-mobile"
 import { RouteProp, useRoute } from "@react-navigation/native"
 import { StackScreenProps } from "@react-navigation/stack"
+import { ConfirmationScreenMatchingArtworksQuery } from "__generated__/ConfirmationScreenMatchingArtworksQuery.graphql"
 import { ConfirmationScreenQuery } from "__generated__/ConfirmationScreenQuery.graphql"
+import GenericGrid, { GenericGridPlaceholder } from "app/Components/ArtworkGrids/GenericGrid"
 import { FancyModalHeader } from "app/Components/FancyModal/FancyModalHeader"
 import { Pill } from "app/Components/Pill"
 import { CreateSavedSearchAlertNavigationStack } from "app/Scenes/SavedSearchAlert/SavedSearchAlertModel"
 import { getRelayEnvironment } from "app/system/relay/defaultEnvironment"
+import { extractNodes } from "app/utils/extractNodes"
+import { useScreenDimensions } from "app/utils/hooks/useScreenDimensions"
 import renderWithLoadProgress from "app/utils/renderWithLoadProgress"
 import { QueryRenderer, graphql } from "react-relay"
-
 type Props = StackScreenProps<CreateSavedSearchAlertNavigationStack, "ConfirmationScreen">
 
 export const ConfirmationScreen: React.FC<Props> = (props) => {
@@ -29,45 +32,94 @@ export const ConfirmationScreen: React.FC<Props> = (props) => {
           We’ll let you know when matching works are added to Artsy.
         </Text>
         <Spacer y={2} />
-        <QueryRenderer<ConfirmationScreenQuery>
-          environment={getRelayEnvironment()}
-          query={graphql`
-            query ConfirmationScreenQuery($searchCriteriaID: ID!) {
-              me {
-                savedSearch(id: $searchCriteriaID) {
-                  labels {
-                    displayValue
-                  }
-                }
-              }
-            }
-          `}
-          // cacheConfig={{ force: true }} // We want to always fetch latest bid increments.
-          variables={{ searchCriteriaID }}
-          render={renderWithLoadProgress<ConfirmationScreenQuery["response"]>((props) => {
-            if (!props?.me?.savedSearch?.labels) return null
-
-            const { labels } = props.me.savedSearch
-
-            return (
-              <>
-                <Flex flexDirection="row" flexWrap="wrap">
-                  <Join separator={<Spacer x={1} />}>
-                    {labels.map((label, index) => {
-                      return (
-                        <Pill key={`filter-label-${index}`} block>
-                          {label.displayValue}
-                        </Pill>
-                      )
-                    })}
-                  </Join>
-                  {/* <ArtworkGrid first={20} input={filterArgs} /> */}
-                </Flex>
-              </>
-            )
-          })}
-        />
+        <SavedSearchQR searchCriteriaID={searchCriteriaID} />
       </Box>
     </Box>
+  )
+}
+
+const SavedSearchQR: React.FC<{ searchCriteriaID: string }> = ({ searchCriteriaID }) => {
+  return (
+    <QueryRenderer<ConfirmationScreenQuery>
+      environment={getRelayEnvironment()}
+      query={graphql`
+        query ConfirmationScreenQuery($searchCriteriaID: ID!) {
+          me {
+            savedSearch(id: $searchCriteriaID) {
+              labels {
+                displayValue
+                field
+                value
+              }
+            }
+          }
+        }
+      `}
+      variables={{ searchCriteriaID }}
+      render={renderWithLoadProgress<ConfirmationScreenQuery["response"]>((props) => {
+        if (!props?.me?.savedSearch?.labels) return null
+
+        const { labels } = props.me.savedSearch
+
+        return (
+          <>
+            <Flex flexDirection="row" flexWrap="wrap">
+              <Join separator={<Spacer x={1} />}>
+                {labels.map((label, index) => {
+                  return (
+                    <Pill key={`filter-label-${index}`} block>
+                      {label.displayValue}
+                    </Pill>
+                  )
+                })}
+              </Join>
+            </Flex>
+            <Spacer y={2} />
+            <MatchingArtworksQR />
+          </>
+        )
+      })}
+    />
+  )
+}
+
+const MatchingArtworksQR = () => {
+  const screen = useScreenDimensions()
+  return (
+    <QueryRenderer<ConfirmationScreenMatchingArtworksQuery>
+      environment={getRelayEnvironment()}
+      query={graphql`
+        query ConfirmationScreenMatchingArtworksQuery {
+          # TODO: actual filter input props to be parsed from labels' fields & values
+          artworksConnection(first: 20) {
+            edges {
+              node {
+                ...GenericGrid_artworks
+              }
+            }
+          }
+        }
+      `}
+      variables={{}}
+      render={renderWithLoadProgress<ConfirmationScreenMatchingArtworksQuery["response"]>(
+        (props) => {
+          const artworks = extractNodes(props.artworksConnection)
+          if (artworks.length === 0) {
+            return <GenericGridPlaceholder width={screen.width - 40} />
+          } else {
+            return (
+              <Box borderTopWidth={1} borderTopColor="black30" pt={1}>
+                <Text variant="sm" color="black60">
+                  {/* TODO: actual artworks count */}
+                  You might like these 000 works currently on Artsy that match your criteria
+                </Text>
+                <Spacer y={2} />
+                <GenericGrid artworks={artworks} />
+              </Box>
+            )
+          }
+        }
+      )}
+    />
   )
 }
