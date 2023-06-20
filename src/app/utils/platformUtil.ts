c1cee5a25052f90168d1e28191feb567d97fbe32
George Kartalis <g.kartalis7@gmail.com>
2023-06-20 10:27:16 +0200

    refactor(cleanup): remove some dead code

--- src/app/utils/platformUtil.ts
+++ src/app/utils/platformUtil.ts
@@ -1,15 +1,4 @@
 import { Platform } from "react-native"
-import { useSafeAreaInsets } from "react-native-safe-area-context"
-
-// FIXME: iOS modal presentation adds safe area insets
-// When we unify modal presentation we should be able to remove this code
-export const useBackButtonTopPadding = () => {
-  if (Platform.OS === "ios") {
-    return 0
-  } else {
-    return 13 + useSafeAreaInsets().top
-  }
-}
 
 export const osMajorVersion = () => {
   // eslint-disable-next-line no-constant-condition
