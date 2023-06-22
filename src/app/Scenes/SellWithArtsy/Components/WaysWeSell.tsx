aeea32f4913d9f6486d1538519b0d0deb060ff99
Ole <ole.richter91@gmail.com>
2023-06-22 12:21:37 +0200

    fix: SWA WaysWeSell section spacing

--- src/app/Scenes/SellWithArtsy/Components/WaysWeSell.tsx
+++ src/app/Scenes/SellWithArtsy/Components/WaysWeSell.tsx
@@ -1,4 +1,4 @@
-import { Flex, Spacer, Text, useColor, useSpace } from "@artsy/palette-mobile"
+import { Flex, Spacer, Text, useColor } from "@artsy/palette-mobile"
 import { useExtraLargeWidth } from "app/Components/ArtworkRail/useExtraLargeWidth"
 import { Image, ImageSourcePropType } from "react-native"
 import { FlatList } from "react-native-gesture-handler"
@@ -36,7 +36,6 @@ const data: Array<{
 ]
 
 export const WaysWeSell: React.FC = () => {
-  const space = useSpace()
   const color = useColor()
 
   const maxImageWidth = useExtraLargeWidth()
@@ -45,17 +44,16 @@ export const WaysWeSell: React.FC = () => {
     (maxImageWidth * Math.max(...data.map((i) => i.height))) / IMAGE_WIDTH
 
   return (
-    <Flex bg="black100" py={4}>
-      <Flex mx={2}>
-        <Text variant="lg-display" color={color("white100")} mb={1}>
-          A sales strategy tailored to your artwork
-        </Text>
-        <Text variant="xs" color={color("white100")} mb={2}>
-          A dedicated specialist works with you to select the best sales option for your artwork.
-        </Text>
-      </Flex>
+    <Flex bg="black100" py={4} px={2}>
+      <Text variant="lg-display" color={color("white100")} mb={1}>
+        A sales strategy tailored to your artwork
+      </Text>
+
+      <Text variant="xs" color={color("white100")} mb={2}>
+        A dedicated specialist works with you to select the best sales option for your artwork.
+      </Text>
+
       <FlatList
-        contentContainerStyle={{ marginLeft: space(2) }}
         horizontal
         showsHorizontalScrollIndicator={false}
         data={data}
@@ -64,7 +62,7 @@ export const WaysWeSell: React.FC = () => {
 
           const imageHeight = maxImageWidth * imageAspectRatio
           return (
-            <Flex maxWidth={maxImageWidth} mr={2} alignSelf="flex-start">
+            <Flex maxWidth={maxImageWidth} alignSelf="flex-start">
               <Flex height={maxImageContainerHeight} justifyContent="flex-end">
                 <Image
                   source={item.imageSource}
@@ -84,8 +82,8 @@ export const WaysWeSell: React.FC = () => {
             </Flex>
           )
         }}
+        ItemSeparatorComponent={() => <Spacer x={2} />}
         keyExtractor={(item) => item.title}
-        ListFooterComponent={() => <Spacer x={4} />}
       />
     </Flex>
   )
