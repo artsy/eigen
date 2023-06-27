92c10738516e532dd3ed37fc680eb7b90bfea875
dzucconi <mail@damonzucconi.com>
2023-06-27 15:29:07 -0400

    feat(artist): adds about tab empty state

---
+++ src/app/Components/Artist/ArtistAbout/ArtistAboutEmpty.tsx
@@ -0,0 +1,26 @@
+import { Box, BoxProps, Text, Touchable } from "@artsy/palette-mobile"
+import { navigate } from "app/system/navigation/navigate"
+import { FC } from "react"
+
+export const ArtistAboutEmpty: FC<BoxProps> = (props) => {
+  return (
+    <Box {...props}>
+      <Text variant="md" textAlign="center">
+        We'll update this page when more information is available.
+      </Text>
+
+      <Text variant="md" textAlign="center" color="black60">
+        Do you represent this artist?{" "}
+        <Touchable
+          onPress={() => {
+            navigate("https://partners.artsy.net")
+          }}
+        >
+          <Text variant="md" color="black100" style={{ textDecorationLine: "underline" }}>
+            Become a partner.
+          </Text>
+        </Touchable>
+      </Text>
+    </Box>
+  )
+}
