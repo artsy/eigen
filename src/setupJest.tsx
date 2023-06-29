93475ebec747e0fb87f66295048cef14fe1fadc3
brainbicycle <brian.beckerle@artsymail.com>
2023-06-29 15:22:07 -0400

    maybe we need this after all

--- src/setupJest.tsx
+++ src/setupJest.tsx
@@ -470,6 +470,25 @@ jest.mock("app/NativeModules/LegacyNativeModules", () => ({
       CurrentLocale: "en_US",
       LocalTimeZone: "",
     },
+    ARNotificationsManager: {
+      nativeState: {
+        userAgent: "Jest Unit Tests",
+        authenticationToken: "authenticationToken",
+        launchCount: 1,
+        userID: "userID",
+        userEmail: "user@example.com",
+      },
+      postNotificationName: jest.fn(),
+      didFinishBootstrapping: jest.fn(),
+      reactStateUpdated: jest.fn(),
+    },
+    ARTemporaryAPIModule: {
+      fetchNotificationPermissions: jest.fn(),
+      markUserPermissionStatus: jest.fn(),
+      markNotificationsRead: jest.fn(),
+      setApplicationIconBadgeNumber: jest.fn(),
+      getUserEmail: jest.fn(),
+    },
     ARPHPhotoPickerModule: {
       requestPhotos: jest.fn(),
     },
