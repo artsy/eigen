9286870a1935d5c8ecd56c345c12b6002a84adc1
George Kartalis <g.kartalis7@gmail.com>
2023-07-07 15:12:45 +0200

    chore: refresh long press ff (#8966)
    * chore: refresh long press ff
    
    * chore: readyForRelease: false

--- src/app/store/config/features.ts
+++ src/app/store/config/features.ts
@@ -237,11 +237,11 @@ export const features: { [key: string]: FeatureDescriptor } = {
     readyForRelease: false,
     showInDevMenu: true,
   },
-  AREnableLongPressArtworkCards: {
+  AREnableLongPressOnArtworkCards: {
     description: "Enable Context Menu on artwork cards",
-    readyForRelease: true,
+    readyForRelease: false,
     showInDevMenu: true,
-    echoFlagKey: "AREnableLongPressArtworkCards",
+    echoFlagKey: "AREnableLongPressOnArtworkCards",
   },
   AREnableShowsForYouLocation: {
     description: "Enable Shows For You Location",
