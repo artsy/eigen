f8395b1e7d4d0fbb647a4714014ace31c73f01fe
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2023-07-11 17:45:05 -0400

    feat: add confirmation header content

--- src/app/Scenes/SavedSearchAlert/screens/ConfirmationScreen.tsx
+++ src/app/Scenes/SavedSearchAlert/screens/ConfirmationScreen.tsx
@@ -1,13 +1,17 @@
-import { Box, Button, Spacer, Text } from "@artsy/palette-mobile"
+import { Box, Flex, Join, Spacer, Text } from "@artsy/palette-mobile"
 import { StackScreenProps } from "@react-navigation/stack"
 import { FancyModalHeader } from "app/Components/FancyModal/FancyModalHeader"
+import { Pill } from "app/Components/Pill"
 import { CreateSavedSearchAlertNavigationStack } from "app/Scenes/SavedSearchAlert/SavedSearchAlertModel"
+import { useSavedSearchPills } from "app/Scenes/SavedSearchAlert/useSavedSearchPills"
+import { ScrollView } from "react-native"
 
 type Props = StackScreenProps<CreateSavedSearchAlertNavigationStack, "ConfirmationScreen">
 
 export const ConfirmationScreen: React.FC<Props> = (props) => {
   const { route } = props
   const { closeModal } = route.params
+  const pills = useSavedSearchPills()
 
   const handleLeftButtonPress = () => {
     closeModal?.()
@@ -17,9 +21,35 @@ export const ConfirmationScreen: React.FC<Props> = (props) => {
     <Box>
       <FancyModalHeader hideBottomDivider useXButton onLeftButtonPress={handleLeftButtonPress} />
       <Box px={2}>
-        <Text variant="lg">Confirmation Screen TKTK</Text>
-        <Spacer y={2} />
-        <Button onPress={closeModal}>Dismiss</Button>
+        <Text variant="lg">Your alert has been saved</Text>
+
+        <Spacer y={1} />
+
+        <ScrollView>
+          <Text variant="sm" color="black60">
+            We’ll let you know when matching works are added to Artsy.
+          </Text>
+
+          <Spacer y={2} />
+
+          <Flex flexDirection="row" flexWrap="wrap">
+            <Join separator={<Spacer x={1} />}>
+              {pills.map((pill) => {
+                return (
+                  <Pill key={`param-${pill.paramName}-value-${pill.value}`} block>
+                    {pill.label}
+                  </Pill>
+                )
+              })}
+            </Join>
+          </Flex>
+
+          <Spacer y={2} />
+
+          <Text pb={1000} backgroundColor="black10">
+            Artworks TKTK
+          </Text>
+        </ScrollView>
       </Box>
     </Box>
   )
