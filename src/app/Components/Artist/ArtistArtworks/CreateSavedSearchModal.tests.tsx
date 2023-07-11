be1c0a70aab2b41831bdd9291323bab47d58441d
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2023-07-11 17:45:03 -0400

    feat: wire up placeholder confirmation screen for create alert flow

--- src/app/Components/Artist/ArtistArtworks/CreateSavedSearchModal.tests.tsx
+++ src/app/Components/Artist/ArtistArtworks/CreateSavedSearchModal.tests.tsx
@@ -1,13 +1,10 @@
 import { OwnerType } from "@artsy/cohesion"
-import { fireEvent } from "@testing-library/react-native"
 import {
   SavedSearchEntity,
   SearchCriteriaAttributes,
 } from "app/Components/ArtworkFilter/SavedSearch/types"
 import { CreateSavedSearchAlert } from "app/Scenes/SavedSearchAlert/CreateSavedSearchAlert"
 import { SavedSearchAlertMutationResult } from "app/Scenes/SavedSearchAlert/SavedSearchAlertModel"
-import { navigate } from "app/system/navigation/navigate"
-import { delay } from "app/utils/delay"
 import { mockTrackEvent } from "app/utils/tests/globallyMockedStuff"
 import { renderWithWrappers } from "app/utils/tests/renderWithWrappers"
 import {
@@ -49,34 +46,6 @@ describe("CreateSavedSearchModal", () => {
 
   it("renders without throwing an error", () => {
     renderWithWrappers(<TestRenderer />)
-  })
-
-  it("should navigate to the saved search alerts list when popover is pressed", async () => {
-    const { getByText, UNSAFE_root } = renderWithWrappers(<TestRenderer />)
-
-    UNSAFE_root.findByType(CreateSavedSearchAlert).props.params.onComplete(mockedMutationResult)
-
-    fireEvent.press(getByText("Your alert has been created."))
-
-    expect(navigate).toHaveBeenCalledWith("/my-profile/settings", {
-      popToRootTabView: true,
-      showInTabName: "profile",
-    })
-  })
-
-  it("should call navigate twice", async () => {
-    const { UNSAFE_root, getByText } = renderWithWrappers(<TestRenderer />)
-
-    UNSAFE_root.findByType(CreateSavedSearchAlert).props.params.onComplete(mockedMutationResult)
-    fireEvent.press(getByText("Your alert has been created."))
-
-    await delay(200)
-
-    expect(navigate).toHaveBeenCalledWith("/my-profile/settings", {
-      popToRootTabView: true,
-      showInTabName: "profile",
-    })
-    expect(navigate).toHaveBeenCalledWith("/my-profile/saved-search-alerts")
   })
 
   it("tracks clicks when the create alert button is pressed", async () => {
