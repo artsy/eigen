c0af6ce1d22903323b5fa7e80174311d9d7d51bd
Matt Dole <mdole7@gmail.com>
2023-07-13 16:36:48 +0200

    feat: send additional Sift events on Android

--- src/app/system/navigation/ModalStack.tsx
+++ src/app/system/navigation/ModalStack.tsx
@@ -8,8 +8,11 @@ import { Severity, addBreadcrumb } from "@sentry/react-native"
 import { AppModule, modules } from "app/AppRegistry"
 import { __unsafe_mainModalStackRef } from "app/NativeModules/ARScreenPresenterModule"
 import { GlobalStore } from "app/store/GlobalStore"
+import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
 import { logNavigation } from "app/utils/loggers"
+import { useEffect, useRef } from "react"
 import { Platform } from "react-native"
+import SiftReactNative from "sift-react-native"
 import { NavStack } from "./NavStack"
 import { useReloadedDevNavigationState } from "./useReloadedDevNavigationState"
 
@@ -24,6 +27,24 @@ const Stack = createStackNavigator()
 export const ModalStack: React.FC = ({ children }) => {
   const initialState = useReloadedDevNavigationState("main_modal_stack", __unsafe_mainModalStackRef)
   const { setSessionState: setNavigationReady } = GlobalStore.actions
+
+  // Code for Sift tracking; needs to be manually fired on Android
+  // See https://github.com/SiftScience/sift-react-native/pull/23#issuecomment-1630984250
+  const enableAdditionalSiftAndroidTracking = useFeatureFlag(
+    "AREnableAdditionalSiftAndroidTracking"
+  )
+  const trackSiftAndroid = Platform.OS === "android" && enableAdditionalSiftAndroidTracking
+  const routeNameRef = useRef()
+  useEffect(() => {
+    if (trackSiftAndroid) {
+      const initialRouteName = routeNameRef.current
+      // Expect error becuase their typing doesn't include setPageName, but it's there
+      // @ts-expect-error
+      SiftReactNative.setPageName(`screen_${initialRouteName}`)
+      SiftReactNative.upload()
+    }
+  }, [])
+
   return (
     <NavigationContainer
       ref={__unsafe_mainModalStackRef}
@@ -51,6 +72,12 @@ export const ModalStack: React.FC = ({ children }) => {
             data: { ...params },
             level: Severity.Info,
           })
+
+          if (trackSiftAndroid) {
+            // @ts-expect-error
+            SiftReactNative.setPageName(`screen_${currentRoute.name}`)
+            SiftReactNative.upload()
+          }
         }
       }}
     >
