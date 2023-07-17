6355f4e8ddec28588fff24a2e9587e3e2f3d018f
araujobarret <araujobarret@gmail.com>
2023-07-17 15:06:17 +0200

    feat: add related genes to artist about tab

---
+++ src/app/Components/Artist/ArtistAbout/ArtistAboutRelatedGenes.tsx
@@ -0,0 +1,54 @@
+import { Flex, Spacer } from "@artsy/palette-mobile"
+import { ArtistAboutRelatedGenes_genes$key } from "__generated__/ArtistAboutRelatedGenes_genes.graphql"
+import { Pill } from "app/Components/Pill"
+import { navigate } from "app/system/navigation/navigate"
+import { isPad } from "app/utils/hardware"
+import { Fragment } from "react"
+import { ScrollView } from "react-native"
+import { graphql, useFragment } from "react-relay"
+
+interface ArtistAboutRelatedGenesProps {
+  genes: ArtistAboutRelatedGenes_genes$key
+}
+
+const MAX_WIDTH = 800
+
+export const ArtistAboutRelatedGenes: React.FC<ArtistAboutRelatedGenesProps> = ({ genes }) => {
+  const data = useFragment(query, genes)
+  const isTablet = isPad()
+
+  if (!data) {
+    return null
+  }
+
+  const handlePillPress = (href: string) => {
+    navigate(href)
+  }
+
+  return (
+    <Flex flexDirection="row" flexWrap="wrap">
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ maxWidth: isTablet ? "100%" : MAX_WIDTH, flexWrap: "wrap" }}
+      >
+        {data.map(({ name, href, internalID }) => (
+          <Fragment key={`gene-${internalID}`}>
+            <Pill rounded onPress={() => handlePillPress(href ?? "")}>
+              {name}
+            </Pill>
+            <Spacer x={1} y={4} />
+          </Fragment>
+        ))}
+      </ScrollView>
+    </Flex>
+  )
+}
+
+const query = graphql`
+  fragment ArtistAboutRelatedGenes_genes on Gene @relay(plural: true) {
+    internalID
+    href
+    name
+  }
+`
