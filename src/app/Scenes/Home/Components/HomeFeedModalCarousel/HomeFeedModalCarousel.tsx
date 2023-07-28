980d0345b2e0f07a36e3677c834e2f74abb24dbf
dariakoko <nargle30@gmail.com>
2023-07-28 20:20:58 +0200

    feat: update copy | onboarding screens

--- src/app/Scenes/Home/Components/HomeFeedModalCarousel/HomeFeedModalCarousel.tsx
+++ src/app/Scenes/Home/Components/HomeFeedModalCarousel/HomeFeedModalCarousel.tsx
@@ -2,6 +2,7 @@ import { Spacer, GraphIcon, ArtworkIcon, Flex, Join } from "@artsy/palette-mobil
 import { PhoneIcon } from "app/Components/Icons/HomeFeedOnboarding/PhoneIcon"
 import { SellWithEaseIcon } from "app/Components/Icons/HomeFeedOnboarding/SellWithEaseIcon"
 import { StepWithImage } from "app/Components/StepWithImage/StepWithImage"
+import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
 import { HomeFeedModalCarouselContainer } from "./HomeFeedModalCarouselContainer"
 import { HomeFeedModalCarouselScreenWrapper } from "./HomeFeedModalCarouselScreenWrapper"
 
@@ -14,29 +15,61 @@ export const HomeFeedModalCarousel: React.FC<HomeFeedModalCarouselProps> = ({
   isVisible,
   toggleModal,
 }) => {
+  const enableCollectedArtists = useFeatureFlag("AREnableMyCollectionCollectedArtists")
+
+  // TODO: add new images, confirm coopy, change buttons on the last screen + rename components
   return (
     <Flex flex={1}>
-      <HomeFeedModalCarouselContainer isVisible={isVisible} toggleModal={toggleModal}>
-        <HomeFeedModalCarouselScreenWrapper
-          title="Create a private record of your artworks"
-          description="Manage your collection online easily and securely in one place."
-          imgSrc={require("images/my-collection-onboarding-image-1.webp")}
-        />
-        <HomeFeedModalCarouselScreenWrapper
-          title="Get insights on your collection"
-          description="Track market demand and get insights into the market value of artworks in your collection."
-          imgSrc={require("images/my-collection-onboarding-image-2.webp")}
-        />
-        <HomeFeedModalCarouselScreenWrapper
-          title="Keep track of artists you collect"
-          description="Discover more about the artists you collect, with latest career news and auction results."
-          imgSrc={require("images/my-collection-onboarding-image-3.webp")}
-        />
-        <HomeFeedModalCarouselScreenWrapper
-          title="How it works"
-          description={<HowItWorksScreen />}
-        />
-      </HomeFeedModalCarouselContainer>
+      {!!enableCollectedArtists ? (
+        <HomeFeedModalCarouselContainer isVisible={isVisible} toggleModal={toggleModal}>
+          <HomeFeedModalCarouselScreenWrapper
+            title="Create a private record of your artworks"
+            description="Manage your collection online easily and securely in one place."
+            imgSrc={require("images/my-collection-onboarding-image-1.webp")}
+          />
+          <HomeFeedModalCarouselScreenWrapper
+            title="Get insights on your collection"
+            description="Track market demand and get insights into the market value of artworks in your collection."
+            imgSrc={require("images/my-collection-onboarding-image-2.webp")}
+          />
+          <HomeFeedModalCarouselScreenWrapper
+            title="Keep track of artists you collect"
+            description="Discover more about the artists you collect, with latest career news and auction results."
+            imgSrc={require("images/my-collection-onboarding-image-3.webp")}
+          />
+          <HomeFeedModalCarouselScreenWrapper
+            title="Build your reputation with galleries"
+            description="Share artists you collect to help galleries get to know your interests and collector profile."
+            imgSrc={require("images/my-collection-onboarding-image-3.webp")}
+          />
+          <HomeFeedModalCarouselScreenWrapper
+            title="How it works"
+            description={<HowItWorksScreenNew />}
+          />
+        </HomeFeedModalCarouselContainer>
+      ) : (
+        <HomeFeedModalCarouselContainer isVisible={isVisible} toggleModal={toggleModal}>
+          <HomeFeedModalCarouselScreenWrapper
+            title="Create a private record of your artworks"
+            description="Manage your collection online easily and securely in one place."
+            imgSrc={require("images/my-collection-onboarding-image-1.webp")}
+          />
+          <HomeFeedModalCarouselScreenWrapper
+            title="Get insights on your collection"
+            description="Track market demand and get insights into the market value of artworks in your collection."
+            imgSrc={require("images/my-collection-onboarding-image-2.webp")}
+          />
+          <HomeFeedModalCarouselScreenWrapper
+            title="Keep track of artists you collect"
+            description="Discover more about the artists you collect, with latest career news and auction results."
+            imgSrc={require("images/my-collection-onboarding-image-3.webp")}
+          />
+          <HomeFeedModalCarouselScreenWrapper
+            title="How it works"
+            description={<HowItWorksScreen />}
+          />
+        </HomeFeedModalCarouselContainer>
+      )}
     </Flex>
   )
 }
@@ -64,6 +97,35 @@ const HowItWorksScreen = () => {
           title="Access your collection on-the-go"
           text="Carry your collection in your pocket, wherever you go."
           icon={PhoneIcon}
+        />
+      </Join>
+    </Flex>
+  )
+}
+
+const HowItWorksScreenNew = () => {
+  return (
+    <Flex mt={4}>
+      <Join separator={<Spacer y={2} />}>
+        <StepWithImage
+          title="Add artists and artworks you collect"
+          text="Add them directly from Artsy entries or upload your own images and details."
+          icon={ArtworkIcon}
+        />
+        <StepWithImage
+          title="Check for insights"
+          text="Get free insights into the markets and careers of the artists in your collection."
+          icon={GraphIcon}
+        />
+        <StepWithImage
+          title="Request a Price Estimate"
+          text="On eligible works, request an estimate from our specialists."
+          icon={SellWithEaseIcon}
+        />
+        <StepWithImage
+          title="Sell with Ease"
+          text="Access free price estimates and sales options from our experts on eligible artworks."
+          icon={SellWithEaseIcon}
         />
       </Join>
     </Flex>
--- src/app/Scenes/MyCollection/Components/MyCollectionZeroState.tsx
+++ src/app/Scenes/MyCollection/Components/MyCollectionZeroState.tsx
@@ -1,23 +1,30 @@
 import { ActionType, AddCollectedArtwork, ContextModule, OwnerType } from "@artsy/cohesion"
 import { useSpace, Flex, LockIcon, Button, Text, Tabs, Box } from "@artsy/palette-mobile"
 import { ZeroState } from "app/Components/States/ZeroState"
+import { HomeFeedModalCarousel } from "app/Scenes/Home/Components/HomeFeedModalCarousel/HomeFeedModalCarousel"
 import { Tab } from "app/Scenes/MyProfile/MyProfileHeaderMyCollectionAndSavedWorks"
 import { navigate } from "app/system/navigation/navigate"
 import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
+import { useState } from "react"
 import { Image } from "react-native"
 import { useTracking } from "react-tracking"
 
 export const MyCollectionZeroState: React.FC = () => {
   const enableCollectedArtists = useFeatureFlag("AREnableMyCollectionCollectedArtists")
-
   const { trackEvent } = useTracking()
   const space = useSpace()
+
+  const [isMyCollectionModalVisible, setIsMyCollectionModalVisible] = useState(false)
 
   const image = require("images/my-collection-empty-state.webp")
 
   return (
     <Tabs.ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <Box mt={4}>
+        <HomeFeedModalCarousel
+          isVisible={isMyCollectionModalVisible}
+          toggleModal={(isVisible) => setIsMyCollectionModalVisible(isVisible)}
+        />
         <ZeroState
           bigTitle="Know Your Collection Better"
           subtitle="Manage your collection online and get free market insights."
@@ -49,7 +56,13 @@ export const MyCollectionZeroState: React.FC = () => {
                   Add to My Collection
                 </Button>
                 <Flex alignItems="center" pt={2}>
-                  <Text onPress={() => {}}>Learn More</Text>
+                  <Text
+                    onPress={() => {
+                      setIsMyCollectionModalVisible(true)
+                    }}
+                  >
+                    Learn More
+                  </Text>
                 </Flex>
               </>
             ) : (
