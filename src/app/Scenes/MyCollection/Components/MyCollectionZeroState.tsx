0079bbd3c1ed0629f07469de959235a7df2ebd85
dariakoko <nargle30@gmail.com>
2023-07-28 19:37:15 +0200

    feat: adjust zero state component

--- src/app/Scenes/MyCollection/Components/MyCollectionZeroState.tsx
+++ src/app/Scenes/MyCollection/Components/MyCollectionZeroState.tsx
@@ -3,10 +3,13 @@ import { useSpace, Flex, LockIcon, Button, Text, Tabs, Box } from "@artsy/palett
 import { ZeroState } from "app/Components/States/ZeroState"
 import { Tab } from "app/Scenes/MyProfile/MyProfileHeaderMyCollectionAndSavedWorks"
 import { navigate } from "app/system/navigation/navigate"
+import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
 import { Image } from "react-native"
 import { useTracking } from "react-tracking"
 
 export const MyCollectionZeroState: React.FC = () => {
+  const enableCollectedArtists = useFeatureFlag("AREnableMyCollectionCollectedArtists")
+
   const { trackEvent } = useTracking()
   const space = useSpace()
 
@@ -29,28 +32,50 @@ export const MyCollectionZeroState: React.FC = () => {
             />
           }
           callToAction={
-            <>
-              <Button
-                testID="add-artwork-button-zero-state"
-                onPress={() => {
-                  trackEvent(tracks.addCollectedArtwork())
-                  navigate("my-collection/artworks/new", {
-                    passProps: {
-                      source: Tab.collection,
-                    },
-                  })
-                }}
-                block
-              >
-                Upload Artwork
-              </Button>
-              <Flex flexDirection="row" justifyContent="center" alignItems="center" py={1}>
-                <LockIcon fill="black60" />
-                <Text color="black60" pl={0.5} variant="xs">
-                  My Collection is not shared with sellers.
-                </Text>
-              </Flex>
-            </>
+            !!enableCollectedArtists ? (
+              <>
+                <Button
+                  testID="add-artwork-button-zero-state"
+                  onPress={() => {
+                    trackEvent(tracks.addCollectedArtwork())
+                    navigate("my-collection/artworks/new", {
+                      passProps: {
+                        source: Tab.collection,
+                      },
+                    })
+                  }}
+                  block
+                >
+                  Add to My Collection
+                </Button>
+                <Flex alignItems="center" pt={2}>
+                  <Text onPress={() => {}}>Learn More</Text>
+                </Flex>
+              </>
+            ) : (
+              <>
+                <Button
+                  testID="add-artwork-button-zero-state"
+                  onPress={() => {
+                    trackEvent(tracks.addCollectedArtwork())
+                    navigate("my-collection/artworks/new", {
+                      passProps: {
+                        source: Tab.collection,
+                      },
+                    })
+                  }}
+                  block
+                >
+                  Upload Artwork
+                </Button>
+                <Flex flexDirection="row" justifyContent="center" alignItems="center" py={1}>
+                  <LockIcon fill="black60" />
+                  <Text color="black60" pl={0.5} variant="xs">
+                    My Collection is not shared with sellers.
+                  </Text>
+                </Flex>
+              </>
+            )
           }
         />
       </Box>
