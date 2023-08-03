9805e3cd73d70fb60397661ffdda91c658c8eef7
Daria Kozlova <daria.kozlova@artsymail.com>
2023-08-03 19:38:52 +0200

    fix(ONYX-212): remove three dots from artist list item (#9120)
    fix: remove three dots from artist list item

--- src/app/Scenes/MyCollection/Components/MyCollectionCollectedArtistItem.tsx
+++ src/app/Scenes/MyCollection/Components/MyCollectionCollectedArtistItem.tsx
@@ -1,8 +1,7 @@
-import { MoreIcon, Touchable, useSpace } from "@artsy/palette-mobile"
+import { useSpace } from "@artsy/palette-mobile"
 import { MyCollectionCollectedArtistItem_artist$key } from "__generated__/MyCollectionCollectedArtistItem_artist.graphql"
 import { ArtistListItemContainer } from "app/Components/ArtistListItem"
 import { MyCollectionTabsStore } from "app/Scenes/MyCollection/State/MyCollectionTabsStore"
-import { useMemo } from "react"
 import { graphql, useFragment } from "react-relay"
 
 interface ArtistItem {
@@ -30,21 +29,12 @@ export const MyCollectionCollectedArtistItem: React.FC<ArtistItem> = ({
     })
   }
 
-  const RightButton = useMemo(() => {
-    return (
-      <Touchable onPress={showArtistPreview}>
-        <MoreIcon height={18} width={18} />
-      </Touchable>
-    )
-  }, [artistData.internalID])
-
   return (
     <ArtistListItemContainer
       artist={artistData}
       showFollowButton={false}
       withFeedback
       containerStyle={{ paddingHorizontal: space(2) }}
-      RightButton={RightButton}
       onPress={showArtistPreview}
       isPrivate={isPrivate}
     />
