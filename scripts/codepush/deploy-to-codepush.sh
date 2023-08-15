110b34401a2c308a37b2771bc24ebdf265a3f661
brainbicycle <brian.beckerle@artsymail.com>
2023-08-15 15:34:29 -0300

    get codepush description from most recent commit or passed in arg

--- scripts/codepush/deploy-to-codepush.sh
+++ scripts/codepush/deploy-to-codepush.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <deployment>"
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+  echo "Usage: $0 <deployment> [description]"
   echo "Deployment must be either 'Canary' or 'Staging'."
   exit 1
 fi
@@ -17,7 +17,16 @@ if [ "$deployment" != "Canary" ] && [ "$deployment" != "Staging" ]; then
   exit 1
 fi
 
-# Run the appcenter codepush release-react command with the specified deployment option
-appcenter codepush release-react -a mobile-artsy/Eigen -d $deployment --plist-file-prefix ios/Artsy/App_Resources
+# Get the description from the command-line argument if provided, otherwise use the last commit hash and description
+if [ -n "$2" ]; then
+  description="$2"
+else
+  commit_hash=$(git rev-parse --short HEAD)
+  commit_description=$(git log -1 --pretty=%s)
+  description="Commit: $commit_hash, Description: $commit_description"
+fi
+
+# Run the appcenter codepush release-react command with the specified deployment option and description
+appcenter codepush release-react -a mobile-artsy/Eigen -d $deployment --plist-file-prefix ios/Artsy/App_Resources --description "$description"
 
 echo "Release to $deployment deployment successful."
