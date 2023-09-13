5b6ea7cdb0ac410c3d591a0b929cd6073c0244c6
dariakoko <nargle30@gmail.com>
2023-09-13 10:51:33 +0200

    feat: use fragment

--- src/app/AppRegistry.tsx
+++ src/app/AppRegistry.tsx
@@ -11,7 +11,7 @@ import { FadeIn } from "app/Components/FadeIn"
 import { ArtQuiz } from "app/Scenes/ArtQuiz/ArtQuiz"
 import { ArtQuizResults } from "app/Scenes/ArtQuiz/ArtQuizResults/ArtQuizResults"
 import { ArticleScreen } from "app/Scenes/Article/ArticleScreen"
-import { BrowseSimilarWorksModal } from "app/Scenes/Artwork/Components/BrowseSimilarWorksModal/BrowseSimilarWorksModal"
+import { BrowseSimilarWorksModalQueryRenderer } from "app/Scenes/Artwork/Components/BrowseSimilarWorksModal/BrowseSimilarWorksModal"
 import { ArtworkListScreen } from "app/Scenes/ArtworkList/ArtworkList"
 import { ArtworkRecommendationsScreen } from "app/Scenes/ArtworkRecommendations/ArtworkRecommendations"
 import { GalleriesForYouScreen } from "app/Scenes/GalleriesForYou/GalleriesForYouScreen"
@@ -420,7 +420,7 @@ export const modules = defineModules({
     modalPresentationStyle: "fullScreen",
   }),
   BottomTabs: reactModule(BottomTabs, { fullBleed: true }),
-  BrowseSimilarWorks: reactModule(BrowseSimilarWorksModal, {
+  BrowseSimilarWorks: reactModule(BrowseSimilarWorksModalQueryRenderer, {
     hidesBackButton: true,
     hidesBottomTabs: true,
   }), // TODO: change the name
--- src/app/Scenes/Artwork/Components/BrowseSimilarWorksModal/BrowseSimilarWorksModal.tsx
+++ src/app/Scenes/Artwork/Components/BrowseSimilarWorksModal/BrowseSimilarWorksModal.tsx
@@ -1,20 +1,21 @@
 import { Box } from "@artsy/palette-mobile"
 import { BrowseSimilarWorksModalQuery } from "__generated__/BrowseSimilarWorksModalQuery.graphql"
+import { BrowseSimilarWorksModal_artwork$key } from "__generated__/BrowseSimilarWorksModal_artwork.graphql"
 import { computeArtworkAlertProps } from "app/Components/Artist/ArtistArtworks/CreateArtworkAlertModal"
 import { BrowseSimilarWorksModalContentWrapper } from "app/Scenes/Artwork/Components/BrowseSimilarWorksModal/BrowseSimilarWorksModalContentWrapper"
 import { withSuspense } from "app/utils/hooks/withSuspense"
-import { graphql, useLazyLoadQuery } from "react-relay"
+import { graphql, useFragment, useLazyLoadQuery } from "react-relay"
 
 const BrowseSimilarWorksPlaceholder: React.FC<{}> = () => {
   return <Box backgroundColor="red" height={200}></Box>
 }
 
-export const BrowseSimilarWorksModal: React.FC<{ artworkID: string }> = withSuspense((props) => {
-  const data = useLazyLoadQuery<BrowseSimilarWorksModalQuery>(SimilarWorksModalQuery, {
-    artworkID: props.artworkID,
-  })
+const BrowseSimilarWorksModal: React.FC<{ artwork: BrowseSimilarWorksModal_artwork$key }> = (
+  props
+) => {
+  const artwork = useFragment(similarWorksModalFragment, props.artwork)
 
-  const artworkAlert = computeArtworkAlertProps(data.artwork)
+  const artworkAlert = computeArtworkAlertProps(artwork)
 
   if (!artworkAlert.hasArtists) {
     return null
@@ -27,27 +28,44 @@ export const BrowseSimilarWorksModal: React.FC<{ artworkID: string }> = withSusp
       aggregations={artworkAlert.aggregations!}
     />
   )
-}, BrowseSimilarWorksPlaceholder)
+}
+
+export const BrowseSimilarWorksModalQueryRenderer: React.FC<{ artworkID: string }> = withSuspense(
+  (props) => {
+    const data = useLazyLoadQuery<BrowseSimilarWorksModalQuery>(SimilarWorksModalQuery, {
+      artworkID: props.artworkID,
+    })
+
+    return <BrowseSimilarWorksModal artwork={data.artwork!} />
+  },
+  BrowseSimilarWorksPlaceholder
+)
+
+const similarWorksModalFragment = graphql`
+  fragment BrowseSimilarWorksModal_artwork on Artwork {
+    title
+    internalID
+    slug
+    artistsArray: artists {
+      internalID
+      name
+    }
+    attributionClass {
+      internalID
+    }
+    mediumType {
+      filterGene {
+        slug
+        name
+      }
+    }
+  }
+`
 
 const SimilarWorksModalQuery = graphql`
   query BrowseSimilarWorksModalQuery($artworkID: String!) {
     artwork(id: $artworkID) {
-      title
-      internalID
-      slug
-      artistsArray: artists {
-        internalID
-        name
-      }
-      attributionClass {
-        internalID
-      }
-      mediumType {
-        filterGene {
-          slug
-          name
-        }
-      }
+      ...BrowseSimilarWorksModal_artwork
     }
   }
 `
