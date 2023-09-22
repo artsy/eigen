72375d53bfbfe554aa9149fcfda4c43682e599a0
George Kartalis <g.kartalis7@gmail.com>
2023-09-22 13:17:16 +0200

    chore: cleanup diff

--- src/app/store/config/features.ts
+++ src/app/store/config/features.ts
@@ -56,11 +56,6 @@ export const features: { [key: string]: FeatureDescriptor } = {
     description: "Enable My Collection Price Estimate Range",
     showInDevMenu: false,
   },
-  AREnableArtworksFromNonArtsyArtists: {
-    readyForRelease: true,
-    description: "Enable My Collection artworks from non-Artsy artists",
-    echoFlagKey: "AREnableArtworksFromNonArtsyArtists",
-  },
   AREnableArtworksConnectionForAuction: {
     readyForRelease: true,
     description: "Use artworksConnection for Auction screen",
