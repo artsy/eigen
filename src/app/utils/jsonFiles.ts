343536976b866719395c69849d6cb8ebbc199b4f
brainbicycle <brian.beckerle@artsymail.com>
2023-09-26 08:24:38 -0400

    make imported json types less strict

--- src/app/utils/jsonFiles.ts
+++ src/app/utils/jsonFiles.ts
@@ -2,5 +2,14 @@ import { Echo } from "app/store/config/EchoModel"
 import appJsonFile from "../../../app.json"
 import echoLaunchJsonFile from "../../../ios/Artsy/App/EchoNew.json"
 
-export const appJson = () => appJsonFile
+type AppConfig = {
+  appName: string
+  version: string
+  isAndroidBeta: boolean
+  nativeCodeVersion: Record<string, string>
+  codePushReleaseName: string
+  codePushDist: string
+}
+
+export const appJson = () => appJsonFile as AppConfig
 export const echoLaunchJson = (): Echo => echoLaunchJsonFile
