512b6498e539b7a6172582ce5d60604a8ef504b3
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2023-10-02 10:36:35 -0400

    chore: codepush remove native check (#9363)
    * remove nativeCodeVersion from the app.json
    
    * remove check native code step
    
    * store native hash as ci cache
    
    * use my branch to test
    
    * test change to non-native code
    
    * revert test changes
    
    * remove now unnecessary scripts
    
    * i don't think we need jq anymore :)

--- scripts/codepush/determine-deployment-type.sh
+++ scripts/codepush/determine-deployment-type.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Get the native code version before the merge (from the main branch)
-pre_merge_native_code_version=$(git show main:app.json | jq -r '.nativeCodeVersion | to_entries | reduce .[] as $item (0; . as $key | if $item.key | tonumber > $key then $item.key | tonumber else $key end)')
+# Calculate the current native hash
+current_native_hash=$(./scripts/codepush/calculate-native-hash.sh)
 
-# Get the native code version after the merge (from the current commit)
-post_merge_native_code_version=$(jq -r '.nativeCodeVersion | to_entries | reduce .[] as $item (0; . as $key | if $item.key | tonumber > $key then $item.key | tonumber else $key end)' app.json)
+# Read the previous native hash from the cache
+if [ -f ./native-hash.txt ]; then
+  previous_native_hash=$(cat ./native-hash.txt)
+else
+  previous_native_hash=""
+fi
 
-# Compare the native version numbers before and after the merge
-if [ "$pre_merge_native_code_version" != "$post_merge_native_code_version" ]; then
-  # If the native version number has changed, then we need to deploy a new native build
+# Compare the current native hash with the previous one
+if [ "$current_native_hash" != "$previous_native_hash" ]; then
+  # If the native hash has changed, then we need to deploy a new native build
   echo "beta"
 else
-  # If the native version number has not changed, then we can deploy using codepush
+  # If the native hash has not changed, then we can deploy using codepush
   echo "codepush"
 fi
+
+# Save the current native hash for the next build
+echo $current_native_hash > ./native-hash.txt
