35821f87acd074ba96347a5cae5c926a78c1e15e
brainbicycle <brian.beckerle@artsymail.com>
2023-10-05 12:58:14 -0400

    try updating the upload command

--- scripts/codepush/maybe_notify_beta_needed.sh
+++ scripts/codepush/maybe_notify_beta_needed.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 S3_TIMESTAMP_PATH="s3://artsy-citadel/eigen/last-notified.txt"
 
 # Attempt to fetch the last notification timestamp
-aws s3 cp $S3_TIMESTAMP_PATH ./last-notified.txt > /dev/null 2>&1
+aws s3 cp $S3_TIMESTAMP_PATH ./last-notified.txt > /dev/null || echo "No previous timestamp found." >&2
 
 if [ -f ./last-notified.txt ]; then
   last_notified=$(cat ./last-notified.txt)
