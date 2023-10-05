7b67aafe37a94ce817e5a3766025baeaea0ae86c
Mounir Dhahri <soussouheros@gmail.com>
2023-10-05 18:09:14 +0200

    feat: add rarity filter functionality

--- src/app/Components/NewArtworkFilter/helpers.ts
+++ src/app/Components/NewArtworkFilter/helpers.ts
@@ -1,3 +1,6 @@
+import { NewArtworksFiltersStore } from "app/Components/NewArtworkFilter/NewArtworkFilterStore"
+import { isEqual } from "lodash"
+
 export enum NewFilterParamName {
   artistIDs = "artistIDs",
   attributionClass = "rarity",
@@ -30,4 +33,21 @@ export type NewFilterData =
 export const paramNameToDisplayLabelMap = {
   attributionClass: "Rarity",
   categories: "Categories",
+}
+
+// A hook that returns the applied filters for a given paramName
+export const useSelectedFiltersByParamName = (paramName: NewFilterParamName) => {
+  const selectedFilters = NewArtworksFiltersStore.useStoreState((state) => state.selectedFilters)
+
+  return selectedFilters.filter((filter) => filter.paramName === paramName)
+}
+
+export const isFilterSelected = (
+  selectedFilters: NewFilterData[],
+  filterValue: NewFilterData["paramValue"]
+) => {
+  if (selectedFilters.find((selectedFilter) => isEqual(selectedFilter.paramValue, filterValue))) {
+    return true
+  }
+  return false
 }
