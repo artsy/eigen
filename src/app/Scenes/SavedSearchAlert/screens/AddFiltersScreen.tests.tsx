7101256694bb1e957479fe5f7f2a8bd729d037d6
Mounir Dhahri <soussouheros@gmail.com>
2023-10-06 12:33:39 +0200

    feat: add clear all button

--- src/app/Scenes/SavedSearchAlert/screens/AddFiltersScreen.tests.tsx
+++ src/app/Scenes/SavedSearchAlert/screens/AddFiltersScreen.tests.tsx
@@ -1,22 +1,61 @@
-import { fireEvent } from "@testing-library/react-native"
+import { fireEvent, waitFor } from "@testing-library/react-native"
+import {
+  NewArtworkFiltersStoreProvider,
+  getNewArtworkFilterStoreModel,
+} from "app/Components/NewArtworkFilter/NewArtworkFilterStore"
+import { NewFilterParamName } from "app/Components/NewArtworkFilter/helpers"
 import { ClearAllButton } from "app/Scenes/SavedSearchAlert/screens/AddFiltersScreen"
-import { flushPromiseQueue } from "app/utils/tests/flushPromiseQueue"
 import { renderWithWrappers } from "app/utils/tests/renderWithWrappers"
 import { Alert } from "react-native"
 
 jest.spyOn(Alert, "alert")
 
 describe("ClearAllButton", () => {
-  it("Shows an alert when pressed", async () => {
-    const { getByText } = renderWithWrappers(<ClearAllButton />)
+  it("Is enabled when there are active filters", () => {
+    const { getByText } = renderWithWrappers(
+      <NewArtworkFiltersStoreProvider runtimeModel={initialData}>
+        <ClearAllButton />
+      </NewArtworkFiltersStoreProvider>
+    )
 
     fireEvent(getByText("Clear All"), "onPress")
 
-    await flushPromiseQueue()
-
-    expect(Alert.alert).toHaveBeenCalled()
+    waitFor(() => {
+      expect(Alert.alert).toHaveBeenCalled()
+    })
   })
 
-  // TODO: Implement feature and test
-  it("Is disabled when there are no filters", () => {})
+  it("Is disabled when there are no filters", async () => {
+    const { getByText } = renderWithWrappers(
+      <NewArtworkFiltersStoreProvider>
+        <ClearAllButton />
+      </NewArtworkFiltersStoreProvider>
+    )
+
+    fireEvent(getByText("Clear All"), "onPress")
+
+    waitFor(() => {
+      expect(Alert.alert).not.toHaveBeenCalled()
+    })
+  })
 })
+
+const initialData = {
+  ...getNewArtworkFilterStoreModel(),
+  selectedFilters: [
+    {
+      paramName: NewFilterParamName.attributionClass,
+      paramValue: {
+        value: "unique",
+        displayLabel: "Unique",
+      },
+    },
+    {
+      paramName: NewFilterParamName.attributionClass,
+      paramValue: {
+        value: "limited edition",
+        displayLabel: "Limited Edition",
+      },
+    },
+  ],
+}
