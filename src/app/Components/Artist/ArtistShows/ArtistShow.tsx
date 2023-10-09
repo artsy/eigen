8012036f25839a3d79e30212f509aa7d5f26c5a6
Carlos Alberto de Araujo Barreto <araujobarret@gmail.com>
2023-10-09 18:17:19 +0200

    feat(DIA-113): add missing track to some areas of artist screen (#9395)
    * feat: add missing track to some areas of artist screen
    
    * fix: remove legacy wrapper from Shows list component and fix it

--- src/app/Components/Artist/ArtistShows/ArtistShow.tsx
+++ src/app/Components/Artist/ArtistShows/ArtistShow.tsx
@@ -1,27 +1,38 @@
-import { ArtistShow_show$data } from "__generated__/ArtistShow_show.graphql"
+import { ActionType, ContextModule, OwnerType, TappedShowGroup } from "@artsy/cohesion"
+import { Touchable } from "@artsy/palette-mobile"
+import { ArtistShow_show$data, ArtistShow_show$key } from "__generated__/ArtistShow_show.graphql"
 import OpaqueImageView from "app/Components/OpaqueImageView/OpaqueImageView"
 import { navigate } from "app/system/navigation/navigate"
 import { hrefForPartialShow } from "app/utils/router"
-import { Touchable } from "@artsy/palette-mobile"
 import { View, ViewStyle } from "react-native"
-import { createFragmentContainer, graphql } from "react-relay"
+import { graphql, useFragment } from "react-relay"
+import { useTracking } from "react-tracking"
 import Metadata from "./Metadata"
 
 interface Props {
-  show: ArtistShow_show$data
+  show: ArtistShow_show$key
   styles?: {
     container?: ViewStyle
     image?: ViewStyle
     metadata?: ViewStyle
   }
+  index: number
 }
 
-const Show: React.FC<Props> = ({ styles, show }) => {
-  const handleTap = () => {
-    navigate(hrefForPartialShow(show))
+export const ArtistShow: React.FC<Props> = ({ styles, show, index }) => {
+  const tracking = useTracking()
+  const data = useFragment(query, show)
+
+  if (!data) {
+    return null
   }
 
-  const image = show.cover_image
+  const handleTap = () => {
+    tracking.trackEvent(tracks.tappedShowGroup(data, index))
+    navigate(hrefForPartialShow(data))
+  }
+
+  const image = data.coverImage
   const imageURL = image && image.url
 
   return (
@@ -33,23 +44,35 @@ const Show: React.FC<Props> = ({ styles, show }) => {
         />
         {/* this wrapper required to make numberOfLines work when parent is a row */}
         <View style={{ flex: 1 }}>
-          <Metadata show={show} style={styles && styles.metadata} />
+          <Metadata show={data} style={!!styles && styles.metadata} />
         </View>
       </View>
     </Touchable>
   )
 }
 
-export const ArtistShowFragmentContainer = createFragmentContainer(Show, {
-  show: graphql`
-    fragment ArtistShow_show on Show {
-      slug
-      href
-      is_fair_booth: isFairBooth
-      cover_image: coverImage {
-        url(version: "large")
-      }
-      ...Metadata_show
+const query = graphql`
+  fragment ArtistShow_show on Show {
+    internalID
+    slug
+    href
+    is_fair_booth: isFairBooth
+    coverImage {
+      url(version: "large")
     }
-  `,
-})
+    ...Metadata_show
+  }
+`
+
+const tracks = {
+  tappedShowGroup: (show: ArtistShow_show$data, index: number): TappedShowGroup => ({
+    action: ActionType.tappedShowGroup,
+    context_module: ContextModule.currentShowsRail,
+    context_screen_owner_type: OwnerType.artist,
+    destination_screen_owner_type: OwnerType.show,
+    destination_screen_owner_id: show.internalID,
+    destination_screen_owner_slug: show.slug,
+    horizontal_slide_position: index + 1,
+    type: "thumbnail",
+  }),
+}
--- src/app/Components/Artist/RelatedArtistsRailCell.tsx
+++ src/app/Components/Artist/RelatedArtistsRailCell.tsx
@@ -1,41 +1,59 @@
+import { ActionType, ContextModule, OwnerType, TappedArtistGroup } from "@artsy/cohesion"
 import { Flex, FollowButton, Image, Text, Touchable } from "@artsy/palette-mobile"
-import { RelatedArtistsRailCell_artist$key } from "__generated__/RelatedArtistsRailCell_artist.graphql"
+import {
+  RelatedArtistsRailCell_artist$data,
+  RelatedArtistsRailCell_artist$key,
+} from "__generated__/RelatedArtistsRailCell_artist.graphql"
+import {
+  RelatedArtistsRailCell_relatedArtist$data,
+  RelatedArtistsRailCell_relatedArtist$key,
+} from "__generated__/RelatedArtistsRailCell_relatedArtist.graphql"
 
 import { navigate } from "app/system/navigation/navigate"
 import { useFollowArtist } from "app/utils/mutations/useFollowArtist"
 import { useFragment, graphql } from "react-relay"
+import { useTracking } from "react-tracking"
 
 const DEFAULT_CELL_WIDTH = 300
 
 interface RelatedArtistsRailCellProps {
+  relatedArtist: RelatedArtistsRailCell_relatedArtist$key
   artist: RelatedArtistsRailCell_artist$key
+  index: number
 }
 
-export const RelatedArtistsRailCell: React.FC<RelatedArtistsRailCellProps> = ({ artist }) => {
-  const data = useFragment(query, artist)
+export const RelatedArtistsRailCell: React.FC<RelatedArtistsRailCellProps> = ({
+  relatedArtist,
+  artist,
+  index,
+}) => {
+  const tracking = useTracking()
+  const relatedArtistData = useFragment(relatedArtistQuery, relatedArtist)
+  const artistData = useFragment(artistQuery, artist)
   const [commitMutation] = useFollowArtist()
 
-  if (!data) {
+  if (!relatedArtistData || !artistData) {
     return null
   }
 
   const handleOnPress = () => {
-    navigate(data.href)
+    tracking.trackEvent(tracks.tappedArtistGroup(relatedArtistData, artistData, index))
+    navigate(relatedArtistData.href)
   }
 
   const handleOnFollow = () => {
     commitMutation({
       variables: {
         input: {
-          artistID: data.internalID,
-          unfollow: data.isFollowed,
+          artistID: relatedArtistData.internalID,
+          unfollow: relatedArtistData.isFollowed,
         },
       },
       optimisticResponse: {
         followArtist: {
           artist: {
-            ...data,
-            isFollowed: !data.isFollowed,
+            ...relatedArtistData,
+            isFollowed: !relatedArtistData.isFollowed,
           },
         },
       },
@@ -47,27 +65,28 @@ export const RelatedArtistsRailCell: React.FC<RelatedArtistsRailCellProps> = ({ 
     <Touchable onPress={handleOnPress}>
       <Image
         testID="related-artist-cover"
-        src={data.image?.url ?? ""}
+        src={relatedArtistData.image?.url ?? ""}
         aspectRatio={1.3}
         width={DEFAULT_CELL_WIDTH}
       />
 
       <Flex flexDirection="row" justifyContent="space-between" my={1}>
         <Flex>
-          <Text variant="sm-display">{data.name}</Text>
+          <Text variant="sm-display">{relatedArtistData.name}</Text>
           <Text variant="xs" color="black60">
-            {data.formattedNationalityAndBirthday}
+            {relatedArtistData.formattedNationalityAndBirthday}
           </Text>
         </Flex>
-        <FollowButton isFollowed={!!data.isFollowed} onPress={handleOnFollow} />
+        <FollowButton isFollowed={!!relatedArtistData.isFollowed} onPress={handleOnFollow} />
       </Flex>
     </Touchable>
   )
 }
 
-const query = graphql`
-  fragment RelatedArtistsRailCell_artist on Artist {
+const relatedArtistQuery = graphql`
+  fragment RelatedArtistsRailCell_relatedArtist on Artist {
     id
+    slug
     internalID @required(action: NONE)
     name @required(action: NONE)
     href @required(action: NONE)
@@ -78,3 +97,29 @@ const query = graphql`
     }
   }
 `
+
+const artistQuery = graphql`
+  fragment RelatedArtistsRailCell_artist on Artist {
+    slug
+    internalID @required(action: NONE)
+  }
+`
+
+export const tracks = {
+  tappedArtistGroup: (
+    relatedArtist: NonNullable<RelatedArtistsRailCell_relatedArtist$data>,
+    artist: NonNullable<RelatedArtistsRailCell_artist$data>,
+    index: number
+  ): TappedArtistGroup => ({
+    action: ActionType.tappedArtistGroup,
+    context_module: ContextModule.relatedArtistsRail,
+    context_screen_owner_type: OwnerType.artist,
+    context_screen_owner_id: artist.internalID,
+    context_screen_owner_slug: artist.slug,
+    destination_screen_owner_type: OwnerType.artist,
+    destination_screen_owner_slug: relatedArtist.slug,
+    destination_screen_owner_id: relatedArtist.internalID,
+    horizontal_slide_position: index + 1,
+    type: "thumbnail",
+  }),
+}
