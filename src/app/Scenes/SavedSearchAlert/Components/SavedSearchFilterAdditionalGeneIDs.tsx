d07c1292a34d16b8cab6a84e1dae97b0f6b32dd5
Mounir Dhahri <soussouheros@gmail.com>
2023-10-13 15:39:23 +0200

    chore: update list of mediums

--- src/app/Scenes/SavedSearchAlert/Components/SavedSearchFilterAdditionalGeneIDs.tsx
+++ src/app/Scenes/SavedSearchAlert/Components/SavedSearchFilterAdditionalGeneIDs.tsx
@@ -104,29 +104,86 @@ export const SavedSearchFilterAdditionalGeneIDs = () => {
   )
 }
 
-export const gravityArtworkMediumCategories = [
-  { label: "Print", value: "Print" },
-  { label: "Photography", value: "Photography" },
+// List from gravity
+export const gravityArtworkMediumCategories: { label: string; value: string }[] = [
+  {
+    label: "Architecture",
+    value: "architecture-1",
+  },
+  {
+    label: "Books and Portfolios",
+    value: "books-and-portfolios",
+  },
+  {
+    label: "Design",
+    value: "design",
+  },
   {
     label: "Work on Paper",
-    value: "Work on Paper",
+    value: "work-on-paper",
   },
-  { label: "Ephemera or Merchandise", value: "Ephemera or Merchandise" },
-  { label: "Painting", value: "Painting" },
-  { label: "Drawing", value: "Drawing" },
-  { label: "Sculpture", value: "Sculpture" },
-  { label: "Mixed Media", value: "Mixed Media" },
-  { label: "Performance Art", value: "Performance Art" },
-  { label: "Installation", value: "Installation" },
-  { label: "Video/Film/Animation", value: "Video/Film/Animation" },
-  { label: "Architecture", value: "Architecture" },
-  { label: "Fashion Design and Wearable Art", value: "Fashion Design and Wearable Art" },
-  { label: "Jewelry", value: "Jewelry" },
-  { label: "Design/Decorative Art", value: "Design/Decorative Art" },
-  { label: "Textile Arts", value: "Textile Arts" },
-  { label: "Posters", value: "Posters" },
-  { label: "Books and Portfolios", value: "Books and Portfolios" },
-  { label: "Reproduction", value: "Reproduction" },
-  { label: "NFT", value: "NFT" },
-  { label: "Other", value: "Other" },
+  {
+    label: "Ephemera or Merchandise",
+    value: "ephemera-or-merchandise",
+  },
+  {
+    label: "Fashion Design and Wearable Art",
+    value: "fashion-design-and-wearable-art",
+  },
+  {
+    label: "Installation",
+    value: "installation",
+  },
+  {
+    label: "Jewelry",
+    value: "jewelry",
+  },
+  {
+    label: "Mixed-Media",
+    value: "mixed-media",
+  },
+  {
+    label: "NFT",
+    value: "nft",
+  },
+  {
+    label: "Other",
+    value: "other",
+  },
+  {
+    label: "Painting",
+    value: "painting",
+  },
+  {
+    label: "Performance Art",
+    value: "performance-art",
+  },
+  {
+    label: "Photography",
+    value: "photography",
+  },
+  {
+    label: "Poster",
+    value: "poster",
+  },
+  {
+    label: "Prints",
+    value: "prints",
+  },
+  {
+    label: "Reproduction",
+    value: "reproduction",
+  },
+  {
+    label: "Sculpture",
+    value: "sculpture",
+  },
+  {
+    label: "Textile Arts",
+    value: "textile-arts",
+  },
+  {
+    label: "Film/Video",
+    value: "film-slash-video",
+  },
 ]
