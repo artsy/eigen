3741b04c07d9ce3f5f81daf867b3097832d50f51
George Kartalis <g.kartalis7@gmail.com>
2023-10-20 16:56:34 +0200

    chore: cleanup native ff references
    Co-authored-by: brainbicycle <brian.beckerle@artsymail.com>

--- ios/Artsy/View_Controllers/ARVIR/VCs/ARAugmentedVIRSetupViewController.h
+++ ios/Artsy/View_Controllers/ARVIR/VCs/ARAugmentedVIRSetupViewController.h
@@ -8,7 +8,7 @@
  */
 @interface ARAugmentedVIRSetupViewController : UIViewController 
 
-- (instancetype)initWithMovieURL:(NSURL *)movieURL config:(ARAugmentedRealityConfig *)config enableInstantVIR:(BOOL)enableInstantVIR;
+- (instancetype)initWithMovieURL:(NSURL *)movieURL config:(ARAugmentedRealityConfig *)config;
 
 /// Is AR even supported?
 + (BOOL)canOpenARView;
