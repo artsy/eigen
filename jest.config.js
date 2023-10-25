1318be4948b043d3b12313e756c8b55f84fb0f4a
brainbicycle <brian.beckerle@artsymail.com>
2023-10-25 15:43:00 -0400

    Revert "use recommended setting for ts-jest"
    This reverts commit c959fdf26bf9958a4548dca87b862e06e9eb760a.

--- jest.config.js
+++ jest.config.js
@@ -1,36 +1,29 @@
 const moduleNameMap = require("./alias").jestModuleNameMap
 
-module.exports = async () => {
-  return {
-    cacheDirectory: ".cache/jest",
-    moduleFileExtensions: ["ts", "tsx", "js"],
-    moduleNameMapper: moduleNameMap,
-    preset: "react-native",
-    rootDir: "./",
-    setupFilesAfterEnv: [
-      "jest-extended",
-      "@testing-library/jest-native/extend-expect",
-      "./src/setupJest.tsx",
-    ],
-    testMatch: ["<rootDir>/**/*.tests.(ts|tsx|js)"],
-    testEnvironment: "jest-environment-jsdom",
-    testEnvironmentOptions: {
-      url: "http://localhost/",
-    },
-    globals: {
-      "ts-jest": {
-        isolatedModules: true,
-      },
-    },
-    transform: {
-      "^[./a-zA-Z0-9$_-]+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$":
-        "<rootDir>/node_modules/react-native/jest/assetFileTransformer.js",
-      ".*(ts|tsx|js|jsx)$": "babel-jest",
-      "\\.graphql$": "jest-raw-loader",
-    },
-    transformIgnorePatterns: [
-      "node_modules/(?!(react-native(-.*)?/(@react-native-community/.*))?|react-navigation|@react-navigation/.*)",
-    ],
-    watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
-  }
+module.exports = {
+  cacheDirectory: ".cache/jest",
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  moduleNameMapper: moduleNameMap,
+  preset: "react-native",
+  rootDir: "./",
+  setupFilesAfterEnv: [
+    "jest-extended",
+    "@testing-library/jest-native/extend-expect",
+    "./src/setupJest.tsx",
+  ],
+  testMatch: ["<rootDir>/**/*.tests.(ts|tsx|js)"],
+  testEnvironment: "jest-environment-jsdom",
+  testEnvironmentOptions: {
+    url: "http://localhost/",
+  },
+  transform: {
+    "^[./a-zA-Z0-9$_-]+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$":
+      "<rootDir>/node_modules/react-native/jest/assetFileTransformer.js",
+    ".*(ts|tsx|js|jsx)$": "babel-jest",
+    "\\.graphql$": "jest-raw-loader",
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(react-native(-.*)?/(@react-native-community/.*))?|react-navigation|@react-navigation/.*)",
+  ],
+  watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
 }
