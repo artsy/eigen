d1c60d4b80b034fca4f388e4b9aed061a0bfa3f1
George Kartalis <g.kartalis7@gmail.com>
2023-10-25 13:16:20 +0200

    refactor: fair screen and useArtworkFilters to support hooks

--- src/app/Scenes/Fair/FairAllFollowedArtists.tests.tsx
+++ src/app/Scenes/Fair/FairAllFollowedArtists.tests.tsx
@@ -1,76 +1,74 @@
-// import { FairAllFollowedArtistsTestsQuery } from "__generated__/FairAllFollowedArtistsTestsQuery.graphql"
-// import { renderWithWrappersLEGACY } from "app/utils/tests/renderWithWrappers"
-// import { graphql, QueryRenderer } from "react-relay"
-// import { act } from "react-test-renderer"
-// import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
-// import { FairArtworksFragmentContainer } from "./Components/FairArtworks"
-// import {
-//   FairAllFollowedArtists,
-//   FairAllFollowedArtistsFragmentContainer,
-// } from "./FairAllFollowedArtists"
+import { FairAllFollowedArtistsTestsQuery } from "__generated__/FairAllFollowedArtistsTestsQuery.graphql"
+import { renderWithWrappersLEGACY } from "app/utils/tests/renderWithWrappers"
+import { graphql, QueryRenderer } from "react-relay"
+import { act } from "react-test-renderer"
+import { createMockEnvironment, MockPayloadGenerator } from "relay-test-utils"
+import { FairArtworks } from "./Components/FairArtworks"
+import {
+  FairAllFollowedArtists,
+  FairAllFollowedArtistsFragmentContainer,
+} from "./FairAllFollowedArtists"
 
-// describe("FairAllFollowedArtists", () => {
-//   let env: ReturnType<typeof createMockEnvironment>
+describe("FairAllFollowedArtists", () => {
+  let env: ReturnType<typeof createMockEnvironment>
 
-//   beforeEach(() => {
-//     env = createMockEnvironment()
-//   })
+  beforeEach(() => {
+    env = createMockEnvironment()
+  })
 
-//   const TestRenderer = () => (
-//     <QueryRenderer<FairAllFollowedArtistsTestsQuery>
-//       environment={env}
-//       query={graphql`
-//         query FairAllFollowedArtistsTestsQuery($fairID: String!) @relay_test_operation {
-//           fair(id: $fairID) {
-//             ...FairAllFollowedArtists_fair
-//           }
-//           fairForFilters: fair(id: $fairID) {
-//             ...FairAllFollowedArtists_fairForFilters
-//           }
-//         }
-//       `}
-//       variables={{ fairID: "art-basel-hong-kong-2019" }}
-//       render={({ props, error }) => {
-//         if (props?.fair && props?.fairForFilters) {
-//           return (
-//             <FairAllFollowedArtistsFragmentContainer
-//               fair={props.fair}
-//               fairForFilters={props.fairForFilters}
-//             />
-//           )
-//         } else if (error) {
-//           console.log(error)
-//         }
-//       }}
-//     />
-//   )
+  const TestRenderer = () => (
+    <QueryRenderer<FairAllFollowedArtistsTestsQuery>
+      environment={env}
+      query={graphql`
+        query FairAllFollowedArtistsTestsQuery($fairID: String!) @relay_test_operation {
+          fair(id: $fairID) {
+            ...FairAllFollowedArtists_fair
+          }
+          fairForFilters: fair(id: $fairID) {
+            ...FairAllFollowedArtists_fairForFilters
+          }
+        }
+      `}
+      variables={{ fairID: "art-basel-hong-kong-2019" }}
+      render={({ props, error }) => {
+        if (props?.fair && props?.fairForFilters) {
+          return (
+            <FairAllFollowedArtistsFragmentContainer
+              fair={props.fair}
+              fairForFilters={props.fairForFilters}
+            />
+          )
+        } else if (error) {
+          console.log(error)
+        }
+      }}
+    />
+  )
 
-//   const getWrapper = (mockResolvers = {}) => {
-//     const tree = renderWithWrappersLEGACY(<TestRenderer />)
-//     act(() => {
-//       env.mock.resolveMostRecentOperation((operation) =>
-//         MockPayloadGenerator.generate(operation, mockResolvers)
-//       )
-//     })
-//     return tree
-//   }
+  const getWrapper = (mockResolvers = {}) => {
+    const tree = renderWithWrappersLEGACY(<TestRenderer />)
+    act(() => {
+      env.mock.resolveMostRecentOperation((operation) =>
+        MockPayloadGenerator.generate(operation, mockResolvers)
+      )
+    })
+    return tree
+  }
 
-//   it("renders without throwing an error", () => {
-//     const wrapper = getWrapper()
-//     expect(wrapper.root.findAllByType(FairAllFollowedArtists)).toHaveLength(1)
-//   })
+  it("renders without throwing an error", () => {
+    const wrapper = getWrapper()
+    expect(wrapper.root.findAllByType(FairAllFollowedArtists)).toHaveLength(1)
+  })
 
-//   it("renders a grid of artworks in the fair filtered by artists the user follows", () => {
-//     const wrapper = getWrapper()
-//     expect(wrapper.root.findAllByType(FairArtworksFragmentContainer)).toHaveLength(1)
-//     expect(
-//       wrapper.root.findAllByType(FairArtworksFragmentContainer)[0].props.initiallyAppliedFilter
-//     ).toStrictEqual([
-//       {
-//         displayText: "All Artists I Follow",
-//         paramName: "includeArtworksByFollowedArtists",
-//         paramValue: true,
-//       },
-//     ])
-//   })
-// })
+  it("renders a grid of artworks in the fair filtered by artists the user follows", () => {
+    const wrapper = getWrapper()
+    expect(wrapper.root.findAllByType(FairArtworks)).toHaveLength(1)
+    expect(wrapper.root.findAllByType(FairArtworks)[0].props.initiallyAppliedFilter).toStrictEqual([
+      {
+        displayText: "All Artists I Follow",
+        paramName: "includeArtworksByFollowedArtists",
+        paramValue: true,
+      },
+    ])
+  })
+})
