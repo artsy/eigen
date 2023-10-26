d04972de82fcd84dd85016e29f9d38c476aa1aa6
George Kartalis <g.kartalis7@gmail.com>
2023-10-26 10:30:48 +0200

    refactor: cleanup tests

--- src/app/utils/hardware.tests.ts
+++ src/app/utils/hardware.tests.ts
@@ -1,29 +1,22 @@
+import RNDeviceInfo from "react-native-device-info"
+import { truncatedTextLimit } from "./hardware"
+
 jest.mock("react-native", () => ({
-  Platform: {
-    isPad: true,
-    OS: "ios",
-  },
   Dimensions: {
     get: jest.fn(() => ({ width: 300, height: 600 })),
   },
 }))
 
-beforeEach(() => {
-  Platform.OS = "ios"
-  ;(Platform as any).isPad = false
-})
-
-import { Platform } from "react-native"
-import { truncatedTextLimit } from "./hardware"
-
 describe(truncatedTextLimit, () => {
   it("returns 320 if device is an iPad", () => {
-    ;(Platform as any).isPad = true
+    jest.spyOn(RNDeviceInfo, "isTablet").mockReturnValue(true)
+
     expect(truncatedTextLimit()).toBe(320)
   })
 
   it("returns 140 if device is not an iPad", () => {
-    ;(Platform as any).isPad = false
+    jest.spyOn(RNDeviceInfo, "isTablet").mockReturnValue(false)
+
     expect(truncatedTextLimit()).toBe(140)
   })
 })
