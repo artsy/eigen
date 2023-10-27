9e88c31a594707da5b5571a9c431b3e32d6f46d1
brainbicycle <brian.beckerle@artsymail.com>
2023-10-27 15:27:20 -0400

    test the export map in babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 const moduleResolverAlias = require("./alias").babelModuleResolverAlias
+const paletteExportMap = require("./scripts/palette/exportMap.json")
 
 module.exports = (api) => {
   api.cache.forever() // don't call this babel config all the time if it hasn't changed.
@@ -15,12 +16,17 @@ module.exports = (api) => {
         "transform-imports",
         {
           "@artsy/palette-mobile": {
-            transform: function (importName, matches) {
-              if (importName === "Flex") {
-                return `@artsy/palette-mobile/dist/elements/${importName}`
-              } else {
-                return `@artsy/palette-mobile/dist/${importName}`
+            transform: function (importName) {
+              const srcPath = paletteExportMap[importName]
+
+              if (!srcPath) {
+                throw new Error(`Cannot find a mapping for ${importName}. Check your exportMap.`)
               }
+
+              // Convert the src path to the desired dist path
+              const distPath = srcPath.replace("/src/", "/dist/").replace(/\.tsx?$/, "")
+
+              return `@artsy/palette-mobile${distPath}`
             },
             preventFullImport: true,
           },
