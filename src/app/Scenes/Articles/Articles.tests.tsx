91429bc5264dc1e5e981a8740af6cf7a3f76a887
George <g.kartalis7@gmail.com>
2023-10-31 14:34:03 +0100

    Update src/app/Scenes/Articles/Articles.tests.tsx

--- src/app/Scenes/Articles/Articles.tests.tsx
+++ src/app/Scenes/Articles/Articles.tests.tsx
@@ -10,7 +10,7 @@ describe("Articles", () => {
   it("renders articles", async () => {
     renderWithRelay({
       Query: () => ({
-        articlesConnection: mockArticlesConnection!,
+        articlesConnection: mockArticlesConnection,
       }),
     })
 
