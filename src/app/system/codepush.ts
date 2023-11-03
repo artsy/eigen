cee13a9022dbbcd0462accbbf1a21797125f924f
brainbicycle <brian.beckerle@artsymail.com>
2023-11-03 09:21:35 -0400

    only allow manual updates for dev and beta

--- src/app/system/codepush.ts
+++ src/app/system/codepush.ts
@@ -29,13 +29,9 @@ const options = () => {
   if (__TEST__) {
     return {}
   } else if (ArtsyNativeModule.isBetaOrDev) {
-    const checkFrequency = __DEV__
-      ? codePush.CheckFrequency.MANUAL
-      : codePush.CheckFrequency.ON_APP_START
     return {
       deploymentKey: stagingKey,
-      checkFrequency: checkFrequency,
-      updateDialog: true,
+      checkFrequency: codePush.CheckFrequency.MANUAL,
     }
   } else {
     return { deploymentKey: codePushProdKey }
