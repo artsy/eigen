1b82263f9bd8365f66a58087d26a39951d9742e6
dariakoko <nargle30@gmail.com>
2023-11-15 18:28:05 +0100

    address review comments

--- src/app/Scenes/SavedSearchAlert/screens/ConfirmationScreen.tsx
+++ src/app/Scenes/SavedSearchAlert/screens/ConfirmationScreen.tsx
@@ -1,4 +1,4 @@
-import { ContextModule, OwnerType } from "@artsy/cohesion"
+import { ActionType, ContextModule, OwnerType, TappedEntityGroup } from "@artsy/cohesion"
 import { Box, Button, Flex, Spacer, Text, useTheme, Pill } from "@artsy/palette-mobile"
 import { NavigationProp, RouteProp, useNavigation, useRoute } from "@react-navigation/native"
 import {
@@ -229,10 +229,12 @@ const MatchingArtworks: React.FC<MatchingArtworksProps> = ({ artworksConnection,
 }
 
 const tracks = {
-  tappedArtworkGroup: (slug: string) => ({
+  tappedArtworkGroup: (slug: string): TappedEntityGroup => ({
+    action: ActionType.tappedArtworkGroup,
     context_module: ContextModule.alertConfirmation,
     context_screen_owner_type: OwnerType.alerts,
     destination_screen_owner_type: OwnerType.artwork,
     destination_screen_owner_slug: slug,
+    type: "thumbnail",
   }),
 }
