cbca9b8b914452eca126238a99342902b2882cf8
Nikita Skalkin <nikskalkin@gmail.com>
2023-11-15 11:33:47 +0100

    feat(onyx-489): Support Artist Series as an alert criteria (#9558)
    * feat(onyx-489): Support Artist Series as an alert criteria
    
    * fixes tests

--- src/app/Scenes/SavedSearchAlert/SavedSearchAlert.tsx
+++ src/app/Scenes/SavedSearchAlert/SavedSearchAlert.tsx
@@ -24,6 +24,7 @@ export const SavedSearchAlertQueryRenderer: React.FC<SearchCriteriaAlertBaseProp
               acquireable
               additionalGeneIDs
               artistIDs
+              artistSeriesIDs
               atAuction
               attributionClass
               colors
