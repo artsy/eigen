c4a7c6e2343a7d91cfe68e93c0835677d8b35885
Ole <ole.richter91@gmail.com>
2023-11-24 14:49:14 +0100

    feat: Add title to "Clear Filters" alert on Alert Filter screen (#9589)
    feat: Add title to Clear Filters alert on Alert Filter screen

--- src/app/Scenes/SavedSearchAlert/screens/SavedSearchFilterScreen.tsx
+++ src/app/Scenes/SavedSearchAlert/screens/SavedSearchFilterScreen.tsx
@@ -97,7 +97,7 @@ export const ClearAllButton = () => {
       disabled={disabled}
       accessibilityState={{ disabled }}
       onPress={() => {
-        Alert.alert("Are you sure you want to clear all filters?", undefined, [
+        Alert.alert("Clear Filters", "Are you sure you want to clear all filters?", [
           {
             text: "Cancel",
             style: "cancel",
@@ -106,7 +106,6 @@ export const ClearAllButton = () => {
           {
             text: "Clear All",
             onPress() {
-              // Trigger action to clear all filters
               clearAllFiltersAction()
             },
             style: "destructive",
