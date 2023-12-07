e27559ee56530ec2d8f4136171de6ab7892237de
Mounir Dhahri <soussouheros@gmail.com>
2023-12-07 15:35:08 +0100

    chore: retire AREnableLargeArtworkRailSaveIcon feature flag

--- src/app/store/config/features.ts
+++ src/app/store/config/features.ts
@@ -59,11 +59,6 @@ export const features: { [key: string]: FeatureDescriptor } = {
     description: "Use artworksConnection for Auction screen",
     echoFlagKey: "AREnableArtworksConnectionForAuction",
   },
-  AREnableLargeArtworkRailSaveIcon: {
-    description: "Enable save icon for large artwork rails",
-    readyForRelease: true,
-    echoFlagKey: "AREnableLargeArtworkRailSaveIcon",
-  },
   AREnableMoneyFormattingInMyCollectionForm: {
     description: "Enable Money formatting in MyCollection Form",
     readyForRelease: true,
