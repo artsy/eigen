c74874bc26508610e5ed34e51d95b531b4cc6ba3
Mounir Dhahri <soussouheros@gmail.com>
2023-12-07 13:07:14 +0100

    chore: Update placeholders and add suggested filters

--- src/app/Scenes/SavedSearchAlert/Components/EditSavedSearchAlertPlaceholder.tsx
+++ src/app/Scenes/SavedSearchAlert/Components/EditSavedSearchAlertPlaceholder.tsx
@@ -1,7 +1,17 @@
-import { BackButton, Box, Flex, Spacer } from "@artsy/palette-mobile"
+import {
+  BackButton,
+  Box,
+  Flex,
+  Join,
+  Skeleton,
+  SkeletonBox,
+  SkeletonText,
+  Spacer,
+} from "@artsy/palette-mobile"
 import { PageWithSimpleHeader } from "app/Components/PageWithSimpleHeader"
 import { goBack } from "app/system/navigation/navigate"
 import { PlaceholderText } from "app/utils/placeholders"
+import { times } from "lodash"
 
 export const EditSavedSearchFormPlaceholder = () => {
   return (
@@ -11,42 +21,52 @@ export const EditSavedSearchFormPlaceholder = () => {
       noSeparator
       left={<BackButton onPress={goBack} />}
     >
-      <Box p={2}>
-        {/* Input name */}
-        <Box mb={2}>
-          <PlaceholderText width={50} height={18} />
-          <Spacer y={0.5} />
-          <PlaceholderText height={40} />
-        </Box>
+      <Skeleton>
+        <Flex px={2}>
+          <Join separator={<Spacer y={4} />}>
+            {/* Input name */}
+            <Box mt={4}>
+              <SkeletonText variant="sm-display">Alert name</SkeletonText>
+              <Spacer y={1} />
+              <PlaceholderText height={50} />
+            </Box>
 
-        {/* Filter pills */}
-        <Box mb={2}>
-          <PlaceholderText width={50} height={18} />
-          <Spacer y={0.5} />
-          <Flex flexDirection="row" flexWrap="wrap" mt={1} mx={-0.5}>
-            <Box mx={0.5}>
-              <PlaceholderText width={65} height={30} />
+            {/* Filter pills */}
+            <Box>
+              <SkeletonText variant="sm-display">We'll send you alerts for</SkeletonText>
+              <Spacer y={1} />
+              <Flex flexDirection="row" flexWrap="wrap" mx={-0.5}>
+                {times(3).map((i) => (
+                  <Box mx={0.5} key={i}>
+                    <PlaceholderText width={50 + Math.random() * 80} height={34} />
+                  </Box>
+                ))}
+              </Flex>
             </Box>
-            <Box mx={0.5}>
-              <PlaceholderText width={100} height={30} />
-            </Box>
-            <Box mx={0.5}>
-              <PlaceholderText width={70} height={30} />
-            </Box>
-            <Box mx={0.5}>
-              <PlaceholderText width={120} height={30} />
-            </Box>
-            <Box mx={0.5}>
-              <PlaceholderText width={100} height={30} />
-            </Box>
-          </Flex>
-        </Box>
 
-        <Spacer y={4} />
-        <PlaceholderText height={50} />
-        <Spacer y={2} />
-        <PlaceholderText height={50} />
-      </Box>
+            {/* Suggested filters */}
+            <Box>
+              <SkeletonText variant="sm-display">Add Filters</SkeletonText>
+              <Spacer y={1} />
+              <Flex flexDirection="row" flexWrap="wrap">
+                <SkeletonBox mr={1} mb={1} width={80} height={30} borderRadius={17} />
+                <SkeletonBox mr={1} mb={1} width={120} height={30} borderRadius={17} />
+                <SkeletonBox mr={1} mb={1} width={90} height={30} borderRadius={17} />
+                <SkeletonBox mr={1} mb={1} width={140} height={30} borderRadius={17} />
+              </Flex>
+            </Box>
+
+            {/* What you're looking for */}
+            <Box>
+              <SkeletonText variant="sm-display">
+                Tell us more about what you’re looking for
+              </SkeletonText>
+              <Spacer y={1} />
+              <SkeletonBox height={150} />
+            </Box>
+          </Join>
+        </Flex>
+      </Skeleton>
     </PageWithSimpleHeader>
   )
 }
