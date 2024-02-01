0d8e1b1bd38b2d33890cd4151403ab43c2c5fb80
Nikita Skalkin <nikskalkin@gmail.com>
2024-02-01 14:57:19 +0100

    tests

---
+++ src/app/Scenes/Activity/utils/__tests__/formattedTimeLeft.tests.ts
@@ -0,0 +1,34 @@
+import { formattedTimeLeft } from "app/Scenes/Activity/utils/formattedTimeLeft"
+
+describe("formattedTimeLeft", () => {
+  it("should return correct countdown copy and text color", () => {
+    expect(formattedTimeLeft({ days: "2", hours: "2", minutes: "2", seconds: "2" })).toEqual({
+      timerCopy: "2 days 2 hours",
+      textColor: "blue100",
+    })
+    expect(formattedTimeLeft({ days: "1", hours: "1", minutes: "1", seconds: "1" })).toEqual({
+      timerCopy: "1 day 1 hour",
+      textColor: "blue100",
+    })
+    expect(formattedTimeLeft({ days: "0", hours: "23", minutes: "59", seconds: "59" })).toEqual({
+      timerCopy: "23 hours",
+      textColor: "blue100",
+    })
+    expect(formattedTimeLeft({ days: "0", hours: "0", minutes: "59", seconds: "59" })).toEqual({
+      timerCopy: "59 minutes",
+      textColor: "red100",
+    })
+    expect(formattedTimeLeft({ days: "0", hours: "0", minutes: "1", seconds: "59" })).toEqual({
+      timerCopy: "1 minute",
+      textColor: "red100",
+    })
+    expect(formattedTimeLeft({ days: "0", hours: "0", minutes: "0", seconds: "59" })).toEqual({
+      timerCopy: "59 seconds",
+      textColor: "red100",
+    })
+    expect(formattedTimeLeft({ days: "0", hours: "0", minutes: "0", seconds: "1" })).toEqual({
+      timerCopy: "1 second",
+      textColor: "red100",
+    })
+  })
+})
