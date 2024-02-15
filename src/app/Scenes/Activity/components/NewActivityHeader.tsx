865e00b35d6781b901ed1a325adc7176e9c80b7c
Ole <ole.richter91@gmail.com>
2024-02-15 19:32:44 +0100

    feat: Render Activity panel "Offers" filter conditionally (#9842)
    feat: Make Activity panel offers filter conditionally

--- src/app/Scenes/Activity/components/NewActivityHeader.tsx
+++ src/app/Scenes/Activity/components/NewActivityHeader.tsx
@@ -1,9 +1,15 @@
 import { Flex, Pill } from "@artsy/palette-mobile"
+import { NewActivityHeaderQuery } from "__generated__/NewActivityHeaderQuery.graphql"
 import { ActivityScreenStore } from "app/Scenes/Activity/ActivityScreenStore"
+import { graphql, useLazyLoadQuery } from "react-relay"
 
 export const NewActivityHeader: React.FC = () => {
+  const data = useLazyLoadQuery<NewActivityHeaderQuery>(query, {})
+
   const type = ActivityScreenStore.useStoreState((state) => state.type)
   const setType = ActivityScreenStore.useStoreActions((actions) => actions.setType)
+
+  const displayOffersFilter = !!data?.viewer?.partnerOfferNotifications?.edges?.length
 
   return (
     <Flex px={2} pb={2}>
@@ -19,15 +25,34 @@ export const NewActivityHeader: React.FC = () => {
         >
           Alerts
         </Pill>
-        <Pill
+        {!!displayOffersFilter && <Pill
           variant="default"
           mr={0.5}
           selected={type === "offers"}
           onPress={() => setType("offers")}
         >
           Offers
-        </Pill>
+        </Pill>}
       </Flex>
     </Flex>
   )
 }
+
+const query = graphql`
+query NewActivityHeaderQuery {
+  viewer {
+    partnerOfferNotifications: notificationsConnection(
+      first: 1
+      notificationTypes: [PARTNER_OFFER_CREATED]
+    ) {
+      # Total count does not work and returns a value even when there are no notifications
+      # TODO: Use totalCount once the issue is fixed
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+}
+`
