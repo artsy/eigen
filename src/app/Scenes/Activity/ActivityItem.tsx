39d20021bea86da2248a05610b0e4f0568bc03ca
Mounir Dhahri <soussouheros@gmail.com>
2024-02-20 18:08:03 +0100

    feat: Improve Activity screen performance (#9832)
    chore: improve activity screen performance
    (cherry picked from commit 6455957c8e67bcebccb6fc54a61e8afbd208cddd)

--- src/app/Scenes/Activity/ActivityItem.tsx
+++ src/app/Scenes/Activity/ActivityItem.tsx
@@ -12,6 +12,7 @@ import { navigateToActivityItem } from "app/Scenes/Activity/utils/navigateToActi
 import { navigate } from "app/system/navigation/navigate"
 import { extractNodes } from "app/utils/extractNodes"
 import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
+import { memo } from "react"
 import { TouchableOpacity } from "react-native"
 import { graphql, useFragment } from "react-relay"
 import { useTracking } from "react-tracking"
@@ -20,6 +21,7 @@ import { isArtworksBasedNotification } from "./utils/isArtworksBasedNotification
 
 interface ActivityItemProps {
   notification: ActivityItem_notification$key
+  isVisible?: boolean
 }
 
 const UNREAD_INDICATOR_SIZE = 8
@@ -27,103 +29,172 @@ const ARTWORK_IMAGE_SIZE = 55
 const NEW_ARTWORK_IMAGE_SIZE = 60
 const BADGE_BORDER_RADIUS = 3
 
-export const ActivityItem: React.FC<ActivityItemProps> = (props) => {
-  const enableNavigateToASingleNotification = useFeatureFlag("AREnableSingleActivityPanelScreen")
-  const enableNewActivityPanelManagement = useFeatureFlag("AREnableNewActivityPanelManagement")
+export const ActivityItem: React.FC<ActivityItemProps> = memo(
+  (props) => {
+    const enableNavigateToASingleNotification = useFeatureFlag("AREnableSingleActivityPanelScreen")
+    const enableNewActivityPanelManagement = useFeatureFlag("AREnableNewActivityPanelManagement")
 
-  const markAsRead = useMarkNotificationAsRead()
-  const tracking = useTracking()
-  const item = useFragment(activityItemFragment, props.notification)
-  const artworks = extractNodes(item.artworksConnection)
-  const artworksCount = item.objectsCount
-  const remainingArtworksCount = artworksCount - 4
-  const shouldDisplayCounts =
-    isArtworksBasedNotification(item.notificationType) && remainingArtworksCount > 0
-  const isPartnerOffer = item.notificationType === "PARTNER_OFFER_CREATED"
-  const isEditorial = item.notificationType === "ARTICLE_FEATURED_ARTIST"
+    const markAsRead = useMarkNotificationAsRead()
+    const tracking = useTracking()
+    const item = useFragment(activityItemFragment, props.notification)
+    const artworks = extractNodes(item.artworksConnection)
+    const artworksCount = item.objectsCount
+    const remainingArtworksCount = artworksCount - 4
+    const shouldDisplayCounts =
+      isArtworksBasedNotification(item.notificationType) && remainingArtworksCount > 0
+    const isPartnerOffer = item.notificationType === "PARTNER_OFFER_CREATED"
+    const isEditorial = item.notificationType === "ARTICLE_FEATURED_ARTIST"
 
-  const handlePress = () => {
-    tracking.trackEvent(tracks.tappedNotification(item.notificationType))
+    const handlePress = () => {
+      tracking.trackEvent(tracks.tappedNotification(item.notificationType))
 
-    if (item.isUnread) {
-      markAsRead(item)
+      if (item.isUnread) {
+        markAsRead(item)
+      }
+
+      if (enableNavigateToASingleNotification) {
+        navigate(`/notification/${item.internalID}`)
+      } else {
+        navigateToActivityItem(item.targetHref)
+      }
     }
 
-    if (enableNavigateToASingleNotification) {
-      navigate(`/notification/${item.internalID}`)
-    } else {
-      navigateToActivityItem(item.targetHref)
-    }
-  }
-
-  if (enableNewActivityPanelManagement) {
-    return (
-      <TouchableOpacity activeOpacity={0.65} onPress={handlePress}>
-        <Flex flexDirection="row" alignItems="center" px={2}>
-          <Flex flex={1} mr={2}>
-            <Flex flexDirection="column" py={2}>
-              <Flex flexDirection={isPartnerOffer ? "row" : "column"}>
-                <Flex flexDirection="row" alignItems="center">
-                  {artworks.map((artwork) => {
-                    return (
-                      <Flex
-                        key={`${item.internalID}-${artwork.internalID}`}
-                        mr={1}
-                        mb={1}
-                        accessibilityLabel="Activity Artwork Image"
-                      >
-                        <Image
-                          src={artwork.image?.preview?.src ?? ""}
-                          width={NEW_ARTWORK_IMAGE_SIZE}
+    if (enableNewActivityPanelManagement) {
+      return (
+        <TouchableOpacity activeOpacity={0.65} onPress={handlePress}>
+          <Flex flexDirection="row" alignItems="center" px={2}>
+            <Flex flex={1} mr={2}>
+              <Flex flexDirection="column" py={2}>
+                <Flex flexDirection={isPartnerOffer ? "row" : "column"}>
+                  <Flex flexDirection="row" alignItems="center">
+                    {artworks.map((artwork) => {
+                      return (
+                        <Flex
+                          key={`${item.internalID}-${artwork.internalID}`}
+                          mr={1}
+                          mb={1}
+                          accessibilityLabel="Activity Artwork Image"
                           height={NEW_ARTWORK_IMAGE_SIZE}
-                        />
-                      </Flex>
-                    )
-                  })}
+                          width={NEW_ARTWORK_IMAGE_SIZE}
+                        >
+                          <Image
+                            src={artwork.image?.preview?.src ?? ""}
+                            width={NEW_ARTWORK_IMAGE_SIZE}
+                            height={NEW_ARTWORK_IMAGE_SIZE}
+                            showLoadingState={!props.isVisible}
+                          />
+                        </Flex>
+                      )
+                    })}
 
-                  {!!shouldDisplayCounts && (
-                    <Text
-                      variant="xs"
-                      color="black60"
-                      accessibilityLabel="Remaining artworks count"
-                    >
-                      + {remainingArtworksCount}
-                    </Text>
-                  )}
-                </Flex>
-
-                <Flex justifyContent="center">
-                  {!!isPartnerOffer && (
-                    <Flex
-                      borderRadius={BADGE_BORDER_RADIUS}
-                      backgroundColor="blue10"
-                      alignSelf="flex-start"
-                    >
-                      <Text variant="xs" color="blue100" px={0.5}>
-                        {getNotificationTypeBadge(item.notificationType)}
+                    {!!shouldDisplayCounts && (
+                      <Text
+                        variant="xs"
+                        color="black60"
+                        accessibilityLabel="Remaining artworks count"
+                      >
+                        + {remainingArtworksCount}
                       </Text>
-                    </Flex>
-                  )}
-
-                  <Text variant="sm-display" fontWeight="bold">
-                    {item.headline}
-                  </Text>
-
-                  {!!isEditorial && <Text variant="xs">{item.message}</Text>}
-
-                  <Flex flexDirection="row">
-                    <ActivityItemTypeLabel notificationType={item.notificationType} />
-                    <Text variant="xs" mr={0.5}>
-                      {item.publishedAt}
-                    </Text>
-                    {shouldDisplayExpiresInTimer(item.notificationType, item.item) && (
-                      <ExpiresInTimer item={item.item} />
                     )}
+                  </Flex>
+
+                  <Flex justifyContent="center">
+                    {!!isPartnerOffer && (
+                      <Flex
+                        borderRadius={BADGE_BORDER_RADIUS}
+                        backgroundColor="blue10"
+                        alignSelf="flex-start"
+                      >
+                        <Text variant="xs" color="blue100" px={0.5}>
+                          {getNotificationTypeBadge(item.notificationType)}
+                        </Text>
+                      </Flex>
+                    )}
+
+                    <Text variant="sm-display" fontWeight="bold">
+                      {item.headline}
+                    </Text>
+
+                    {!!isEditorial && <Text variant="xs">{item.message}</Text>}
+
+                    <Flex flexDirection="row">
+                      <ActivityItemTypeLabel notificationType={item.notificationType} />
+                      <Text variant="xs" mr={0.5}>
+                        {item.publishedAt}
+                      </Text>
+                      {shouldDisplayExpiresInTimer(item.notificationType, item.item) && (
+                        <ExpiresInTimer item={item.item} />
+                      )}
+                    </Flex>
                   </Flex>
                 </Flex>
               </Flex>
             </Flex>
+            {!!item.isUnread && (
+              <Flex
+                width={UNREAD_INDICATOR_SIZE}
+                height={UNREAD_INDICATOR_SIZE}
+                borderRadius={UNREAD_INDICATOR_SIZE / 2}
+                bg="blue100"
+                accessibilityLabel="Unread notification indicator"
+              />
+            )}
           </Flex>
+        </TouchableOpacity>
+      )
+    }
+    return (
+      <TouchableOpacity activeOpacity={0.65} onPress={handlePress}>
+        <Flex py={2} flexDirection="row" alignItems="center" px={2}>
+          <Flex flex={1}>
+            <Flex flexDirection="row">
+              <ActivityItemTypeLabel notificationType={item.notificationType} />
+
+              <Text variant="xs" color="black60">
+                {item.publishedAt}
+              </Text>
+            </Flex>
+
+            <Text variant="sm-display" fontWeight="bold">
+              {item.title}
+            </Text>
+
+            {!isPartnerOffer && <Text variant="sm-display">{item.message}</Text>}
+
+            {shouldDisplayExpiresInTimer(item.notificationType, item.item) && (
+              <ExpiresInTimer item={item.item} />
+            )}
+
+            <Spacer y={1} />
+
+            <Flex flexDirection="row" alignItems="center">
+              {artworks.map((artwork) => {
+                return (
+                  <Flex
+                    key={`${item.internalID}-${artwork.internalID}`}
+                    mr={1}
+                    accessibilityLabel="Activity Artwork Image"
+                    height={NEW_ARTWORK_IMAGE_SIZE}
+                    width={NEW_ARTWORK_IMAGE_SIZE}
+                  >
+                    <Image
+                      src={artwork.image?.preview?.src ?? ""}
+                      width={ARTWORK_IMAGE_SIZE}
+                      height={ARTWORK_IMAGE_SIZE}
+                      showLoadingState={!props.isVisible}
+                    />
+                  </Flex>
+                )
+              })}
+
+              {!!shouldDisplayCounts && (
+                <Text variant="xs" color="black60" accessibilityLabel="Remaining artworks count">
+                  + {remainingArtworksCount}
+                </Text>
+              )}
+            </Flex>
+          </Flex>
+
           {!!item.isUnread && (
             <Flex
               width={UNREAD_INDICATOR_SIZE}
@@ -136,69 +207,11 @@ export const ActivityItem: React.FC<ActivityItemProps> = (props) => {
         </Flex>
       </TouchableOpacity>
     )
+  },
+  (prevProps, nextProps) => {
+    return prevProps.isVisible === nextProps.isVisible
   }
-  return (
-    <TouchableOpacity activeOpacity={0.65} onPress={handlePress}>
-      <Flex py={2} flexDirection="row" alignItems="center" px={2}>
-        <Flex flex={1}>
-          <Flex flexDirection="row">
-            <ActivityItemTypeLabel notificationType={item.notificationType} />
-
-            <Text variant="xs" color="black60">
-              {item.publishedAt}
-            </Text>
-          </Flex>
-
-          <Text variant="sm-display" fontWeight="bold">
-            {item.title}
-          </Text>
-
-          {!isPartnerOffer && <Text variant="sm-display">{item.message}</Text>}
-
-          {shouldDisplayExpiresInTimer(item.notificationType, item.item) && (
-            <ExpiresInTimer item={item.item} />
-          )}
-
-          <Spacer y={1} />
-
-          <Flex flexDirection="row" alignItems="center">
-            {artworks.map((artwork) => {
-              return (
-                <Flex
-                  key={`${item.internalID}-${artwork.internalID}`}
-                  mr={1}
-                  accessibilityLabel="Activity Artwork Image"
-                >
-                  <Image
-                    src={artwork.image?.preview?.src ?? ""}
-                    width={ARTWORK_IMAGE_SIZE}
-                    height={ARTWORK_IMAGE_SIZE}
-                  />
-                </Flex>
-              )
-            })}
-
-            {!!shouldDisplayCounts && (
-              <Text variant="xs" color="black60" accessibilityLabel="Remaining artworks count">
-                + {remainingArtworksCount}
-              </Text>
-            )}
-          </Flex>
-        </Flex>
-
-        {!!item.isUnread && (
-          <Flex
-            width={UNREAD_INDICATOR_SIZE}
-            height={UNREAD_INDICATOR_SIZE}
-            borderRadius={UNREAD_INDICATOR_SIZE / 2}
-            bg="blue100"
-            accessibilityLabel="Unread notification indicator"
-          />
-        )}
-      </Flex>
-    </TouchableOpacity>
-  )
-}
+)
 
 const activityItemFragment = graphql`
   fragment ActivityItem_notification on Notification {
