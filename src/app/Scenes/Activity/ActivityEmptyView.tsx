cdf9954ed403bb661c6a62a72f82fa45deae59e8
dariakoko <nargle30@gmail.com>
2024-02-21 14:00:35 -0600

    feat: match empty states to the design

--- src/app/Scenes/Activity/ActivityEmptyView.tsx
+++ src/app/Scenes/Activity/ActivityEmptyView.tsx
@@ -1,4 +1,5 @@
-import { Flex, Spacer, Text } from "@artsy/palette-mobile"
+import { Flex, Spacer, Text, Touchable } from "@artsy/palette-mobile"
+import { navigate } from "app/system/navigation/navigate"
 import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
 import { NotificationType } from "./types"
 
@@ -6,24 +7,66 @@ interface ActivityEmptyViewProps {
   type: NotificationType
 }
 
-const entityByType: Record<NotificationType, { title: string; message: string }> = {
+const artistLink = (
+  <Touchable onPress={() => navigate("/artists")}>
+    <Text variant="xs" underline>
+      Artists
+    </Text>
+  </Touchable>
+)
+
+const galleriesLink = (
+  <Touchable onPress={() => navigate("/galleries")}>
+    <Text variant="xs" underline>
+      Galeries
+    </Text>
+  </Touchable>
+)
+
+const entityByType: Record<
+  NotificationType,
+  { title: string; message: string; geStartedMessage?: any; links?: any }
+> = {
   all: {
-    title: `Follow artists and galleries to stay up to date`,
+    title: "Stay up to date with the artists and artworks you love",
     message:
-      "Keep track of the art and events you love, and get recommendations based on who you follow.",
+      "Follow artists and galleries to keep track of their latest updates. Or create an alert and we’ll let you know when there’s a matching work.",
+    geStartedMessage: "Get started with:",
+    links: (
+      <>
+        {artistLink}
+        {galleriesLink}
+      </>
+    ),
   },
   follows: {
     title: "Follow artists and galleries to stay up to date",
-    message:
-      "Keep track of the art and events you love, and get recommendations based on who you follow.",
+    message: "Keep track of the art and events you love, and get updates based on who you follow.",
+    geStartedMessage: "Get started with:",
+    links: (
+      <>
+        {artistLink}
+        {galleriesLink}
+      </>
+    ),
   },
   alerts: {
-    title: `Hunting for a particular artwork?`,
-    message: `Create alerts on an artist or artwork page and get notifications here when there’s a match.`,
+    title: "Hunting for a particular artwork?",
+    message:
+      "Create alerts on an artist or artwork page and get notifications here when there’s a match.",
+    geStartedMessage: "Get started with:",
+    links: artistLink,
   },
   offers: {
-    title: `Your offers will appear here`,
-    message: `When you receive an offer on an artwork, it will appear here.`,
+    title: "Your offers will appear here",
+    message: "When you receive an offer on an artwork, it will appear here.",
+    geStartedMessage: "Get started with:",
+    links: (
+      <>
+        {artistLink}
+        {galleriesLink}
+      </>
+    ),
   },
 }
 
@@ -31,12 +74,29 @@ export const ActivityEmptyView: React.FC<ActivityEmptyViewProps> = ({ type }) =>
   const entity = entityByType[type]
   const enableNewActivityPanelManagement = useFeatureFlag("AREnableNewActivityPanelManagement")
 
+  if (enableNewActivityPanelManagement) {
+    return (
+      <Flex mx={4} accessibilityLabel="Activities are empty" pt={4}>
+        <Text textAlign="start" variant="sm-display">
+          {entity.title}
+        </Text>
+        <Spacer y={2} />
+        <Text variant="xs" color="black60" textAlign="start">
+          {entity.message}
+        </Text>
+        <Spacer y={2} />
+        <Flex flexDirection="row" gap={10}>
+          <Text variant="xs" color="black60" textAlign="start">
+            {entity.geStartedMessage}
+          </Text>
+          {entity.links}
+        </Flex>
+      </Flex>
+    )
+  }
+
   return (
-    <Flex
-      mx={2}
-      accessibilityLabel="Activities are empty"
-      pt={enableNewActivityPanelManagement ? 2 : 0}
-    >
+    <Flex mx={2} accessibilityLabel="Activities are empty" pt={0}>
       <Text textAlign="center">{entity.title}</Text>
       <Spacer y={2} />
       <Text variant="xs" color="black60" textAlign="center">
