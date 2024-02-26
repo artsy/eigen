68deceec01064545aec871023ecdee99d0adec32
Ole <ole.richter91@gmail.com>
2024-02-26 12:00:47 +0100

    fix: Do not navigate to Activity screen when notification type is not yet supported

--- src/app/Scenes/Activity/utils/navigateToActivityItem.ts
+++ src/app/Scenes/Activity/utils/navigateToActivityItem.ts
@@ -1,11 +1,26 @@
+import { ActivityItem_notification$data } from "__generated__/ActivityItem_notification.graphql"
+import { ActivityRailItem_item$data } from "__generated__/ActivityRailItem_item.graphql"
 import { FilterArray } from "app/Components/ArtworkFilter/ArtworkFilterHelpers"
 import { ORDERED_ARTWORK_SORTS } from "app/Components/ArtworkFilter/Filters/SortOptions"
+import { SUPPORTED_NOTIFICATION_TYPES } from "app/Scenes/Activity/ActivityItemScreen"
 import { matchRoute } from "app/routes"
 import { navigate } from "app/system/navigation/navigate"
 import { last } from "lodash"
 import { parse as parseQueryString } from "query-string"
 
-export const navigateToActivityItem = (targetHref: string) => {
+export const navigateToActivityItem = (
+  item: ActivityItem_notification$data | ActivityRailItem_item$data,
+  enableNavigateToASingleNotification: boolean
+) => {
+  const { internalID, targetHref, notificationType } = item
+
+  if (
+    enableNavigateToASingleNotification &&
+    SUPPORTED_NOTIFICATION_TYPES.includes(notificationType)
+  ) {
+    return navigate(`/notification/${internalID}`)
+  }
+
   const splittedQueryParams = targetHref.split("?")
   const queryParams = last(splittedQueryParams) ?? ""
   const parsed = parseQueryString(queryParams)
