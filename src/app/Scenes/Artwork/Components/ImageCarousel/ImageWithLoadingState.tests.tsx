3bec4e7d8e52918b6e88d57301f89cddd45b1c25
Mounir Dhahri <soussouheros@gmail.com>
2024-02-26 11:27:55 +0100

    chore: fix broken tests

--- src/app/Scenes/Artwork/Components/ImageCarousel/ImageWithLoadingState.tests.tsx
+++ src/app/Scenes/Artwork/Components/ImageCarousel/ImageWithLoadingState.tests.tsx
@@ -1,18 +1,25 @@
-import { OpaqueImageView } from "app/Components/OpaqueImageView2"
+import { screen } from "@testing-library/react-native"
 import { renderWithWrappers } from "app/utils/tests/renderWithWrappers"
+import { Image } from "react-native"
 import { ImageWithLoadingState } from "./ImageWithLoadingState"
 
 const imageURL = "https://image.com/image.jpg"
 const style = { width: 100, height: 300 }
 
+// React-test-renderer has issues with memo components, so we need to mock the palette-mobile Image component
+// Until it gets fixed
+// See https://github.com/facebook/react/issues/17301
+jest.mock("@artsy/palette-mobile", () => ({
+  ...jest.requireActual("@artsy/palette-mobile"),
+  Image: require("react-native").Image,
+}))
+
 describe("ImageWithLoadingState", () => {
-  it("renders the image", () => {
-    const { getAllByLabelText } = renderWithWrappers(
-      <ImageWithLoadingState imageURL={imageURL} {...style} />
-    )
-    const images = getAllByLabelText("Image with Loading State")
+  it("renders the image", async () => {
+    renderWithWrappers(<ImageWithLoadingState imageURL={imageURL} {...style} />)
+    const images = screen.getAllByLabelText("Image with Loading State")
 
     expect(images).toHaveLength(1)
-    expect(images[0].findByType(OpaqueImageView)).toHaveProp("imageURL", imageURL)
+    expect(await images[0].findByType(Image)).toHaveProp("src", imageURL)
   })
 })
