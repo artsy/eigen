e791ce5c64a0cc2fb73f29379baa6f1d78fee665
dariakoko <nargle30@gmail.com>
2024-03-01 19:05:31 -0600

    chore: revert Alerts spesific change

--- src/app/Scenes/Activity/ActivityList.tsx
+++ src/app/Scenes/Activity/ActivityList.tsx
@@ -95,7 +95,6 @@ export const ActivityList: React.FC<ActivityListProps> = ({ viewer, type, me }) 
               )
             }
             data={notifications}
-            scrollEnabled={notifications.length > 1}
             onViewableItemsChanged={props.onViewableItemsChanged}
             viewabilityConfig={props.viewabilityConfig}
             keyExtractor={keyExtractor}
