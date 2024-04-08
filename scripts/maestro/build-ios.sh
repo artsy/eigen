d720cded0a7a3226476e621e7570726db11f471c
George Kartalis <g.kartalis7@gmail.com>
2024-04-08 15:28:03 +0200

    feat: maestro iOS build script

---
+++ scripts/maestro/build-ios.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Creates the iOS required build file inside eigen/ios/build/Build/Products/Store-iphonesimulator/Artsy.app
+# Opens the Finder window inside the folder where build file is located.
+
+# CI=1 in order to avoid including flipper in the build
+CI=1 yarn pod-install
+
+# Generate the iOS build file
+yarn maestro:ios:release:build
+
+# Open the folder where the build file is located
+open ios/build/Build/Products/Store-iphonesimulator
+
+# Discard the diff that will be generated afterwards (Podfile.lock)
