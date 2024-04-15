c3771d4b7a370eb63edf6e1c1b8ae45aa2cfae5e
George Kartalis <g.kartalis7@gmail.com>
2024-04-15 17:32:04 +0200

    refactor(PartnerShowRailItem): to fc and fix image crash/issue

--- src/app/Scenes/Partner/Components/PartnerShowRailItem.tsx
+++ src/app/Scenes/Partner/Components/PartnerShowRailItem.tsx
@@ -1,64 +1,54 @@
-import { Spacer, Flex, Text } from "@artsy/palette-mobile"
+import { Spacer, Flex, Text, useScreenDimensions } from "@artsy/palette-mobile"
 import { PartnerShowRailItem_show$data } from "__generated__/PartnerShowRailItem_show.graphql"
 import OpaqueImageView from "app/Components/OpaqueImageView/OpaqueImageView"
 import { exhibitionDates } from "app/Scenes/Map/exhibitionPeriodParser"
 import { navigate } from "app/system/navigation/navigate"
-import { Schema, track } from "app/utils/track"
+import { Schema } from "app/utils/track"
 import { first } from "lodash"
 import React from "react"
-import { Dimensions, TouchableWithoutFeedback } from "react-native"
+import { TouchableWithoutFeedback } from "react-native"
 import { createFragmentContainer, graphql } from "react-relay"
-import styled from "styled-components/native"
-
-const { width: windowWidth } = Dimensions.get("window")
-const ImageView = styled(OpaqueImageView)`
-  height: 200px;
-  width: 100%;
-`
+import { useTracking } from "react-tracking"
 
 interface Props {
   show: PartnerShowRailItem_show$data
 }
 
-@track()
-export class PartnerShowRailItem extends React.Component<Props> {
-  @track((props) => ({
-    action_name: Schema.ActionNames.NearbyShow,
-    action_type: Schema.ActionTypes.Tap,
-    owner_id: props.show.internalID,
-    owner_slug: props.show.id,
-    owner_type: Schema.OwnerEntityTypes.Show,
-  }))
-  onPress() {
-    navigate(`/show/${this.props.show.slug}`)
+export const PartnerShowRailItem: React.FC<Props> = (props) => {
+  const { width: windowWidth } = useScreenDimensions()
+  const tracking = useTracking()
+
+  const onPress = () => {
+    tracking.trackEvent(tracks.tapPartnerShowRailItem(props.show.internalID, props.show.slug))
+    navigate(`/show/${props.show.slug}`)
   }
 
-  render() {
-    const { show } = this.props
-    const { name, exhibitionPeriod, endAt, coverImage, images } = show
-    const imageURL = coverImage?.url || first(images)?.url
+  const { show } = props
+  const { name, exhibitionPeriod, endAt, coverImage, images } = show
+  const imageURL = coverImage?.url || first(images)?.url
 
-    return (
-      <TouchableWithoutFeedback onPress={() => this.onPress()}>
-        <Flex my="15px" mr={2} width={windowWidth - 100}>
-          <ImageView
-            imageURL={imageURL}
-            blurhash={coverImage?.blurhash}
-            style={{ alignItems: "center", justifyContent: "center" }}
-          />
-          <Spacer y={1} />
-          <Text variant="sm" numberOfLines={1}>
-            {name}
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <Flex my="15px" mr={2} width={windowWidth - 100}>
+        <OpaqueImageView
+          imageURL={imageURL}
+          blurhash={coverImage?.blurhash}
+          style={{ alignItems: "center", justifyContent: "center" }}
+          height={200}
+          width={windowWidth - 100}
+        />
+        <Spacer y={1} />
+        <Text variant="sm" numberOfLines={1}>
+          {name}
+        </Text>
+        {!!(exhibitionPeriod && endAt) && (
+          <Text variant="sm" color="black60">
+            {exhibitionDates(exhibitionPeriod, endAt)}
           </Text>
-          {!!(exhibitionPeriod && endAt) && (
-            <Text variant="sm" color="black60">
-              {exhibitionDates(exhibitionPeriod, endAt)}
-            </Text>
-          )}
-        </Flex>
-      </TouchableWithoutFeedback>
-    )
-  }
+        )}
+      </Flex>
+    </TouchableWithoutFeedback>
+  )
 }
 
 export const PartnerShowRailItemContainer = createFragmentContainer(PartnerShowRailItem, {
@@ -79,3 +69,13 @@ export const PartnerShowRailItemContainer = createFragmentContainer(PartnerShowR
     }
   `,
 })
+
+const tracks = {
+  tapPartnerShowRailItem: (internalID: string, id: string) => ({
+    action_name: Schema.ActionNames.NearbyShow,
+    action_type: Schema.ActionTypes.Tap,
+    owner_id: internalID,
+    owner_slug: id,
+    owner_type: Schema.OwnerEntityTypes.Show,
+  }),
+}
