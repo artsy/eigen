dc1457617e80f61370cf525e9548b0c64b1defef
Leandro Motta <leamotta@msn.com>
2024-04-16 11:36:12 +0200

    add note in notification panel

--- src/app/Scenes/Activity/components/NotificationArtworkList.tsx
+++ src/app/Scenes/Activity/components/NotificationArtworkList.tsx
@@ -1,17 +1,19 @@
 import { OwnerType } from "@artsy/cohesion"
-import { Flex, Screen } from "@artsy/palette-mobile"
+import { Flex, Screen, Text, useColor } from "@artsy/palette-mobile"
 import { NotificationArtworkList_artworksConnection$key } from "__generated__/NotificationArtworkList_artworksConnection.graphql"
 import { PriceOfferMessage } from "app/Components/ArtworkGrids/ArtworkGridItem"
 import { MasonryInfiniteScrollArtworkGrid } from "app/Components/ArtworkGrids/MasonryInfiniteScrollArtworkGrid"
 import { CommercialButtonsQueryRenderer } from "app/Scenes/Activity/components/NotificationCommercialButtons"
 import { extractNodes } from "app/utils/extractNodes"
 import { FC } from "react"
+import { ImageBackground, View } from "react-native"
 import { useFragment, graphql } from "react-relay"
 
 export interface PartnerOffer {
   endAt: string
   isAvailable: boolean
   targetHref: string
+  note: string
 }
 
 interface NotificationArtworkListProps {
@@ -29,6 +31,9 @@ export const NotificationArtworkList: FC<NotificationArtworkListProps> = (props)
   const artworksConnectionData = useFragment(notificationArtworkListFragment, artworksConnection)
 
   const artworks = extractNodes(artworksConnectionData)
+  const note = partnerOffer?.note
+  const partnerIcon = artworks[0].partner?.profile?.icon?.resized
+  const color = useColor()
 
   return (
     <Flex minHeight={400}>
@@ -50,6 +55,42 @@ export const NotificationArtworkList: FC<NotificationArtworkListProps> = (props)
           partnerOffer={partnerOffer}
         />
       )}
+      {!!note && (
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            padding: 20,
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: color("black5"),
+              flexDirection: "row",
+              width: "100%",
+              padding: 10,
+            }}
+          >
+            {!!partnerIcon && (
+              <View style={{ marginRight: 10 }}>
+                <ImageBackground
+                  source={{ uri: partnerIcon.src }}
+                  style={{ width: 30, height: 30 }}
+                  imageStyle={{ borderRadius: 15, borderColor: color("black30"), borderWidth: 1 }}
+                />
+              </View>
+            )}
+            <View style={{ flex: 1 }}>
+              <Text variant="sm" color="black100" fontWeight="bold">
+                Note from the gallery
+              </Text>
+              <Text variant="sm" color="black100">
+                "{note}"
+              </Text>
+            </View>
+          </View>
+        </View>
+      )}
     </Flex>
   )
 }
@@ -62,6 +103,16 @@ export const notificationArtworkListFragment = graphql`
         id
         slug
         href
+        partner {
+          profile {
+            icon {
+              resized(width: 30, height: 30) {
+                src
+                srcSet
+              }
+            }
+          }
+        }
         image(includeAll: false) {
           aspectRatio
         }
