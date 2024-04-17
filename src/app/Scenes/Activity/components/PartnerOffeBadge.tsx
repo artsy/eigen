90cd98c9081fa0fe29a66cd6577b8e59d18e5a61
leamotta <leamotta@msn.com>
2024-04-17 11:58:35 +0200

    fix: add border radius to notification badge (#10091)
    add border radius to notification badge

--- src/app/Scenes/Activity/components/PartnerOffeBadge.tsx
+++ src/app/Scenes/Activity/components/PartnerOffeBadge.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@artsy/palette-mobile"
+import { Box, Text } from "@artsy/palette-mobile"
 import { NotificationTypesEnum } from "__generated__/ActivityRail_notificationsConnection.graphql"
 import { getNotificationTypeBadge } from "app/Scenes/Activity/utils/getNotificationTypeLabel"
 
@@ -8,14 +8,10 @@ interface PartnerOfferBadgeProps {
 
 export const PartnerOfferBadge: React.FC<PartnerOfferBadgeProps> = ({ notificationType }) => {
   return (
-    <Text
-      variant="xs"
-      color="blue100"
-      backgroundColor="blue10"
-      style={{ alignSelf: "flex-start" }}
-      px={0.5}
-    >
-      {getNotificationTypeBadge(notificationType)}
-    </Text>
+    <Box borderRadius={3} backgroundColor="blue10" px={0.5} alignSelf="flex-start">
+      <Text variant="xs" color="blue100">
+        {getNotificationTypeBadge(notificationType)}
+      </Text>
+    </Box>
   )
 }
