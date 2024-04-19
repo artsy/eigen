feb917f36607f3ed3c27149831d016640f603a50
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2024-04-19 12:51:49 -0400

    chore(tools): release followups + util scripts #trivial (#10108)
    * script for getting order of merge commits
    
    * update copyright to current year
    
    * add a util script for ordering commits
    
    * add a util script for getting merge hash for a pr
    
    * newlines
    
    * fix date command

---
+++ scripts/utils/commit-hash-from-pr.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+### Script to get merge commit corresponding to a given PR number
+### e.g if you want to see the commit hash for https://github.com/artsy/eigen/pull/10107
+### Run the script as `./scripts/utils/commit-hash-from-pr.sh 10107`
+### The merge commit is usually the commit you want to cherry-pick into release candidates
+### Requires running `yarn setup:releases` to set up the environment variables
+
+source .env.releases
+GITHUB_TOKEN=${CHANGELOG_GITHUB_TOKEN_KEY}
+
+OWNER="artsy"
+REPO="eigen"
+
+PR_NUMBER=$1
+
+API_URL="https://api.github.com/repos/${OWNER}/${REPO}/pulls/${PR_NUMBER}"
+pr_data=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" "${API_URL}")
+
+# Extract the merge commit hash
+merge_commit=$(echo "${pr_data}" | jq -r '.merge_commit_sha')
+
+if [ "${merge_commit}" != "null" ]; then
+    echo "Merge commit for PR #${PR_NUMBER}: ${merge_commit}"
+else
+    echo "No merge commit found for PR #${PR_NUMBER}. It might not be merged yet."
+fi
