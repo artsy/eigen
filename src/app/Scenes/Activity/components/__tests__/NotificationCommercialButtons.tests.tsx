4c89d2cdb4753c8cb4e5009ddb09a37b7f5ba862
Kazunari Okuda <saori.cube@gmail.com>
2024-04-23 12:52:49 +0200

    test: add tests for NotificationCommercialButtons

--- src/app/Scenes/Activity/components/__tests__/NotificationCommercialButtons.tests.tsx
+++ src/app/Scenes/Activity/components/__tests__/NotificationCommercialButtons.tests.tsx
@@ -1,6 +1,7 @@
 import { screen } from "@testing-library/react-native"
 import { NotificationCommercialButtons_Test_Query } from "__generated__/NotificationCommercialButtons_Test_Query.graphql"
 import { CommercialButtons } from "app/Scenes/Activity/components/NotificationCommercialButtons"
+import { __globalStoreTestUtils__ } from "app/store/GlobalStore"
 import { flushPromiseQueue } from "app/utils/tests/flushPromiseQueue"
 import { setupTestWrapper } from "app/utils/tests/setupTestWrapper"
 import { Suspense } from "react"
@@ -56,16 +57,31 @@ describe("CommercialButtons", () => {
     ),
   })
 
-  it("renders buttons correctly ", async () => {
-    renderWithRelay({
-      Artwork: () => ({
-        internalID: "artwork-id",
-      }),
+  describe("when partner offer is expired", () => {
+    it("renders Make an Offer and Contact Gallery when the feature flag is off", async () => {
+      renderWithRelay({
+        Artwork: () => ({
+          internalID: "artwork-id",
+        }),
+      })
+
+      await flushPromiseQueue()
+
+      expect(screen.getByText("Make an Offer")).toBeTruthy()
+      expect(screen.getByText("Contact Gallery")).toBeTruthy()
     })
 
-    await flushPromiseQueue()
+    it("renders View Work when the feature flag is on", async () => {
+      __globalStoreTestUtils__?.injectFeatureFlags({ AREnablePartnerOfferOnArtworkScreen: true })
+      renderWithRelay({
+        Artwork: () => ({
+          internalID: "artwork-id",
+        }),
+      })
 
-    expect(screen.getByText("Make an Offer")).toBeTruthy()
-    expect(screen.getByText("Contact Gallery")).toBeTruthy()
+      await flushPromiseQueue()
+
+      expect(screen.getByText("View Work")).toBeTruthy()
+    })
   })
 })
