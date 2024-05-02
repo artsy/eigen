0e94b853411115d237bcecd7541d4b9c2009caba
Mounir Dhahri <soussouheros@gmail.com>
2024-05-02 11:41:19 +0200

    fix: search input placeholder on android

--- src/app/Scenes/Search/Search.tsx
+++ src/app/Scenes/Search/Search.tsx
@@ -27,7 +27,12 @@ import { SEARCH_PILLS, SEARCH_THROTTLE_INTERVAL, TOP_PILL } from "./constants"
 import { getContextModuleByPillName } from "./helpers"
 import { PillType } from "./types"
 
-const SEARCH_INPUT_PLACEHOLDER = "Search artists, artworks, galleries, etc"
+const SEARCH_INPUT_PLACEHOLDER = [
+  "Search artists, artworks, galleries, etc",
+  "Search artists, artworks, etc",
+  "Search artworks, etc",
+  "Search",
+]
 
 export const searchQueryDefaultVariables: SearchQuery$variables = {
   term: "",
