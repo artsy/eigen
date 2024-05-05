8f8530b2b9022988c47e2119f1e7f15437002252
Jackie Potts <jacqueline.a.potts@gmail.com>
2024-05-05 16:00:45 -0400

    Add tracking event when Learn more on shipping and taxes component

--- src/app/Scenes/Artwork/Components/ShippingAndTaxes.tsx
+++ src/app/Scenes/Artwork/Components/ShippingAndTaxes.tsx
@@ -1,7 +1,9 @@
+import { ActionType } from "@artsy/cohesion"
 import { Spacer, Box, Text, LinkText } from "@artsy/palette-mobile"
 import { ShippingAndTaxes_artwork$data } from "__generated__/ShippingAndTaxes_artwork.graphql"
 import { navigate } from "app/system/navigation/navigate"
 import { createFragmentContainer, graphql } from "react-relay"
+import { useTracking } from "react-tracking"
 
 interface ShippingAndTaxesProps {
   artwork: ShippingAndTaxes_artwork$data
@@ -9,9 +11,19 @@ interface ShippingAndTaxesProps {
 
 const ShippingAndTaxes: React.FC<ShippingAndTaxesProps> = ({ artwork }) => {
   const { shippingInfo, shippingOrigin, priceIncludesTaxDisplay, taxInfo } = artwork
+  const { trackEvent } = useTracking()
 
   const handleLearnMorePress = () => {
     if (!!taxInfo?.moreInfo?.url) {
+      const payload = {
+        action: ActionType.tappedLearnMore,
+        context_module: "artworkDetails",
+        type: "Link",
+        subject: "Learn more",
+        flow: "Shipping",
+      }
+
+      trackEvent(payload)
       navigate(taxInfo.moreInfo.url)
     }
   }
