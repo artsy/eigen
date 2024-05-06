b26e6e190c17cf7a8348370fd1660b5838834dbe
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2024-05-06 09:52:44 -0400

    chore(tracking): Render tax info and link to help center for unlisted works + tracking events (#10194)

--- src/app/Scenes/Artwork/Components/ShippingAndTaxes.tsx
+++ src/app/Scenes/Artwork/Components/ShippingAndTaxes.tsx
@@ -1,7 +1,9 @@
+import { ActionType } from "@artsy/cohesion"
 import { Spacer, Box, Text, LinkText } from "@artsy/palette-mobile"
 import { ShippingAndTaxes_artwork$data } from "__generated__/ShippingAndTaxes_artwork.graphql"
 import { navigate } from "app/system/navigation/navigate"
 import { createFragmentContainer, graphql } from "react-relay"
+import { useTracking } from "react-tracking"
 
 interface ShippingAndTaxesProps {
   artwork: ShippingAndTaxes_artwork$data
@@ -9,9 +11,19 @@ interface ShippingAndTaxesProps {
 
 const ShippingAndTaxes: React.FC<ShippingAndTaxesProps> = ({ artwork }) => {
   const { shippingInfo, shippingOrigin, priceIncludesTaxDisplay, taxInfo } = artwork
+  const { trackEvent } = useTracking()
 
   const handleLearnMorePress = () => {
     if (!!taxInfo?.moreInfo?.url) {
+      const payload = {
+        action: ActionType.tappedLearnMore,
+        context_module: "artworkDetails",
+        type: "Link",
+        subject: "Learn more",
+        flow: "Shipping",
+      }
+
+      trackEvent(payload)
       navigate(taxInfo.moreInfo.url)
     }
   }
@@ -23,13 +35,6 @@ const ShippingAndTaxes: React.FC<ShippingAndTaxesProps> = ({ artwork }) => {
           <Text variant="md">Shipping and taxes</Text>
           <Spacer y={1} />
         </>
-      )}
-
-      {!!taxInfo && !artwork.isUnlisted && (
-        <Text variant="sm" color="black60">
-          {taxInfo.displayText}{" "}
-          <LinkText onPress={handleLearnMorePress}>{taxInfo.moreInfo.displayText}</LinkText>
-        </Text>
       )}
 
       {!!shippingOrigin && (
@@ -47,6 +52,13 @@ const ShippingAndTaxes: React.FC<ShippingAndTaxesProps> = ({ artwork }) => {
       {!!priceIncludesTaxDisplay && (
         <Text variant="sm" color="black60">
           {priceIncludesTaxDisplay}
+        </Text>
+      )}
+
+      {!!taxInfo && (
+        <Text variant="sm" color="black60">
+          {taxInfo.displayText}{" "}
+          <LinkText onPress={handleLearnMorePress}>{taxInfo.moreInfo.displayText}</LinkText>
         </Text>
       )}
     </Box>
