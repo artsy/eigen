3774cc81b025a506319e5d00ade53dec735be6fe
Mounir Dhahri <soussouheros@gmail.com>
2024-05-07 15:19:18 +0200

    feat: add loading logic to upload artwork

--- src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmissionArtworkFormArtist.tsx
+++ src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmissionArtworkFormArtist.tsx
@@ -2,6 +2,7 @@ import { Flex, Spacer, Text } from "@artsy/palette-mobile"
 import { StackScreenProps } from "@react-navigation/stack"
 import { AutosuggestResult } from "app/Components/AutosuggestResults/AutosuggestResults"
 import { AutosuggestResultsPlaceholder } from "app/Components/AutosuggestResults/AutosuggestResultsPlaceholder"
+import LoadingModal from "app/Components/Modals/LoadingModal"
 import { ArtistAutosuggest } from "app/Scenes/MyCollection/Screens/ArtworkForm/Components/ArtistAutosuggest"
 import { ArtworkFormScreen } from "app/Scenes/SellWithArtsy/ArtworkForm/SubmissionArtworkForm"
 import { useSubmissionContext } from "app/Scenes/SellWithArtsy/ArtworkForm/Utils/navigationHelpers"
@@ -9,19 +10,22 @@ import { ArtworkDetailsFormModel } from "app/Scenes/SellWithArtsy/ArtworkForm/Ut
 import { createOrUpdateSubmission } from "app/Scenes/SellWithArtsy/SubmitArtwork/ArtworkDetails/utils/createOrUpdateSubmission"
 import { PlaceholderBox, PlaceholderText, ProvidePlaceholderContext } from "app/utils/placeholders"
 import { useFormikContext } from "formik"
-import { Suspense } from "react"
+import { Suspense, useState } from "react"
 
 export const SubmissionArtworkFormArtist: React.FC<
   StackScreenProps<ArtworkFormScreen, "ArtworkFormArtist">
 > = ({}) => {
   const { navigateToNextStep } = useSubmissionContext()
+  const [isLoading, setIsLoading] = useState(false)
 
   const formik = useFormikContext<ArtworkDetailsFormModel>()
 
   const handleResultPress = async (result: AutosuggestResult) => {
+    setIsLoading(true)
+
+    formik.setFieldValue("artistSearchResult", result)
     formik.setFieldValue("artist", result.displayLabel)
     formik.setFieldValue("artistId", result.internalID)
-    formik.setFieldValue("artistSearchResult", result)
 
     if (!result.internalID) {
       console.error("Artist ID not found")
@@ -34,18 +38,23 @@ export const SubmissionArtworkFormArtist: React.FC<
       artistSearchResult: result,
     }
 
-    // TODO: Not use `createOrUpdateSubmission` from old submission flow
-    // TODO: Does it make sense to create a new submission here?
-    // We might end up with a lot of submissions that are never completed
-    const submissionId = await createOrUpdateSubmission(updatedValues, formik.values.submissionId)
-
-    formik.setFieldValue("submissionId", submissionId)
-
-    navigateToNextStep()
+    try {
+      // TODO: Not use `createOrUpdateSubmission` from old submission flow
+      // TODO: Does it make sense to create a new submission here?
+      // We might end up with a lot of submissions that are never completed
+      const submissionId = await createOrUpdateSubmission(updatedValues, formik.values.submissionId)
+      formik.setFieldValue("submissionId", submissionId)
+      navigateToNextStep()
+    } catch (error) {
+      console.error("Error creating submission", error)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return (
     <Flex>
+      <LoadingModal isVisible={isLoading} dark />
       <Text variant="lg" mb={2}>
         Add artist name
       </Text>
