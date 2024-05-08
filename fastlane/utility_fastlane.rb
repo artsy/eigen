b96e8ffe81dc758f10a8cf89d76977d10ae73988
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2024-05-08 14:36:16 -0400

    build(releases): automate next version increment (#10213)
    * testing lane
    
    chore: prepare for next release --no-verify
    
    chore: prepare for next release --no-verify
    
    chore: prepare for next release --no-verify
    
    chore: prepare for next release --no-verify
    
    * add step in the config, cleanup
    
    * remove no verify
    
    * minor cleanup
    
    * send a slack notification if version creation fails
    
    * chore: prepare for next release
    
    * add a message when pr is created
    
    * remove test changes
    
    * revert me: test changes
    
    * see if install gems is enough
    
    * try mac executor
    
    * try a valid mac executor
    
    * other stuff we may need
    
    * remove redundant awscli install
    
    * fix incorrect UI commands
    
    * don't use vars before they are initialized
    
    * don't think we need node
    
    * use method that isn't made up
    
    * revert me: downgrade version to get past commit step
    
    * try more aggressive git add
    
    * remove test changes, update beta executor
    
    * update messages for pr feedback
    
    * pr review, nochangelog in pr, fix broken link
    
    * better emoji for pr message

--- fastlane/utility_fastlane.rb
+++ fastlane/utility_fastlane.rb
@@ -1,8 +1,8 @@
 # Utility functions
 
 desc "Updates the version string in app.json"
-lane :update_version_string do
-  new_version = prompt(text: "What is the new human-readable release version?")
+lane :update_version_string do |options|
+  new_version = options[:version] || prompt(text: "What is the new human-readable release version?")
   $APP_JSON['version'] = new_version
   write_contents_to_file($APP_JSON_PATH, JSON.pretty_generate($APP_JSON))
 end
@@ -74,8 +74,65 @@ lane :tag_and_push do |options|
   tag = options[:tag]
   `git tag -d "#{tag}"`
   add_git_tag tag: tag
-  `git remote add http https://github.com/artsy/eigen.git`
+  `git remote add http https://github.com/artsy/eigen.git || true`
   `git push http #{tag} -f`
+end
+
+desc 'Create a new version in app store connect'
+lane :create_next_app_version do |options|
+  api_key = generate_app_store_connect_api_key
+
+  next_version = options[:next_version_code]
+  UI.message("Let's create a new app version #{next_version}.")
+
+  deliver(
+    api_key: api_key,
+    app_version: next_version,
+    skip_metadata: true,
+    automatic_release: false,
+    phased_release: false,
+    submit_for_review: false
+  )
+end
+
+desc "Prepares a new branch with version changes, pushes it, and creates a PR"
+lane :prepare_version_update_pr do |options|
+  version_change_branch = "version-update-#{Time.now.strftime('%Y%m%d%H%M%S')}"
+  commit_message = options[:commit_message]
+
+  sh "git add -A"
+  sh "git commit -m '#{commit_message}'"
+
+  sh "git remote add http https://github.com/artsy/eigen.git || true"  # '|| true' to ignore errors if remote already exists
+
+  # Create the new branch in remote and push changes
+  sh "git push origin HEAD:refs/heads/#{version_change_branch}"
+
+  pr_url = create_pull_request(
+    api_token: ENV["CHANGELOG_GITHUB_TOKEN_KEY"],
+    repo: "artsy/eigen",
+    title: commit_message,
+    head: version_change_branch,
+    assignees: ["brainbicycle", "gkartalis"],
+    base: "main",
+    body: "This PR updates the app version to prepare for next release. #nochangelog"
+  )
+
+  pr_url
+end
+
+lane :latest_betas do
+  api_key = generate_app_store_connect_api_key
+  parent_version_name = $APP_JSON['version']
+  ios_build_number = latest_testflight_build_number(
+    api_key: api_key
+  )
+  android_build_number = google_play_track_version_codes(
+      track: 'alpha'
+  ).first
+  puts "Parent latest version: #{parent_version_name}"
+  puts "iOS latest version: #{ios_build_number}"
+  puts "Android latest version: #{android_build_number}"
 end
 
 lane :check_flags do
@@ -121,6 +178,12 @@ def generate_spaceship_token
     is_key_content_base64: true,
     in_house: false
   )
+end
+
+def increment_version_number(current_version)
+  major, minor, patch = current_version.split('.').map(&:to_i)
+  new_minor = minor + 1
+  "#{major}.#{new_minor}.#{patch}"
 end
 
 def format_build_number(build_number)
