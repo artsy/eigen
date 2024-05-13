170bc579a786a70eee3eb98bae2ab893411d4391
George Kartalis <g.kartalis7@gmail.com>
2024-05-13 16:54:43 +0200

    chore: hide settings behind ff

--- src/app/Scenes/MyProfile/MyProfileSettings.tsx
+++ src/app/Scenes/MyProfile/MyProfileSettings.tsx
@@ -131,7 +131,6 @@ export const MyProfileSettings: React.FC<MyProfileSettingsProps> = ({ onSuccess 
             <Separator my={1} borderColor={separatorColor} />
           </>
         )}
-
         <Flex flexDirection="row" alignItems="center" justifyContent="center" py="7.5px" px={2}>
           <Button variant="fillDark" haptic onPress={confirmLogout} block>
             Log Out
