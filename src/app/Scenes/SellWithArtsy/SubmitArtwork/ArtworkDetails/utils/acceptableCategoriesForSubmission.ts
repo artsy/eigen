c9403114345f8b8ba5bfc1883c3a1aa0501b8431
Mounir Dhahri <soussouheros@gmail.com>
2024-05-17 11:04:41 +0200

    chore: update list of submission categories

--- src/app/Scenes/SellWithArtsy/SubmitArtwork/ArtworkDetails/utils/acceptableCategoriesForSubmission.ts
+++ src/app/Scenes/SellWithArtsy/SubmitArtwork/ArtworkDetails/utils/acceptableCategoriesForSubmission.ts
@@ -12,25 +12,15 @@ export type AcceptableCategoryValue = AcceptableValuesMapKey
 
 // By having this ACCEPTABLE_VALUES_MAP structure, we are forced to update this list
 // whenever ConsignmentSubmissionCategoryAggregation changes, because yarn tsc will fail
-export const ACCEPTABLE_CATEGORY_VALUES_MAP: Record<
-  AcceptableValuesMapKey,
-  AcceptableCategoryValue
+export const ACCEPTABLE_CATEGORY_VALUES_MAP: Partial<
+  Record<AcceptableValuesMapKey, AcceptableCategoryValue>
 > = {
-  ARCHITECTURE: "ARCHITECTURE",
-  DESIGN_DECORATIVE_ART: "DESIGN_DECORATIVE_ART",
-  DRAWING_COLLAGE_OR_OTHER_WORK_ON_PAPER: "DRAWING_COLLAGE_OR_OTHER_WORK_ON_PAPER",
-  FASHION_DESIGN_AND_WEARABLE_ART: "FASHION_DESIGN_AND_WEARABLE_ART",
-  INSTALLATION: "INSTALLATION",
-  JEWELRY: "JEWELRY",
-  MIXED_MEDIA: "MIXED_MEDIA",
   OTHER: "OTHER",
   PAINTING: "PAINTING",
-  PERFORMANCE_ART: "PERFORMANCE_ART",
+  DRAWING_COLLAGE_OR_OTHER_WORK_ON_PAPER: "DRAWING_COLLAGE_OR_OTHER_WORK_ON_PAPER",
   PHOTOGRAPHY: "PHOTOGRAPHY",
   PRINT: "PRINT",
   SCULPTURE: "SCULPTURE",
-  TEXTILE_ARTS: "TEXTILE_ARTS",
-  VIDEO_FILM_ANIMATION: "VIDEO_FILM_ANIMATION",
 }
 
 export const formatCategoryValueForSubmission = (categoryValue: string) => {
