1b490ae571018d4dbddbd912f359683f8f25e728
Mounir Dhahri <soussouheros@gmail.com>
2024-05-22 09:43:50 +0200

    feat: update thank you for your submission screen

--- src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkProgressBar.tsx
+++ src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkProgressBar.tsx
@@ -1,10 +1,4 @@
-import {
-  CheckCircleFillIcon,
-  Flex,
-  ProgressBar,
-  useScreenDimensions,
-  useSpace,
-} from "@artsy/palette-mobile"
+import { Flex, ProgressBar } from "@artsy/palette-mobile"
 import { SubmitArtworkFormStore } from "app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkFormStore"
 import { __unsafe__SubmissionArtworkFormNavigationRef } from "app/Scenes/SellWithArtsy/ArtworkForm/SubmitArtworkForm"
 import {
@@ -21,13 +15,10 @@ const COUNTABLE_STEPS = ARTWORK_FORM_STEPS.filter(
   (step) => NON_COUNTABLE_STEPS.indexOf(step) === -1
 )
 
-const ICON_WIDTH = 18
-
 const PROGRESS_BAR_HEIGHT = 22
 
 export const SubmitArtworkProgressBar: React.FC = ({}) => {
   const currentStep = SubmitArtworkFormStore.useStoreState((state) => state.currentStep)
-  const { width: screenWidth } = useScreenDimensions()
 
   const hasStartedFlowFromMyCollection = useMemo(() => {
     const routes = (
@@ -45,8 +36,6 @@ export const SubmitArtworkProgressBar: React.FC = ({}) => {
 
     return false
   }, [currentStep])
-
-  const space = useSpace()
 
   // Returns the total steps based on whether the flow has started from My Collection or not
   // This is required for accurate progress bar calculation
@@ -66,36 +55,16 @@ export const SubmitArtworkProgressBar: React.FC = ({}) => {
   }
 
   const hasCompletedForm = currentStep === "CompleteYourSubmission"
-  const checkIconWidth = ICON_WIDTH + space(0.5)
 
   return (
     <Flex height={PROGRESS_BAR_HEIGHT}>
-      <Flex
-        // Subtracting the width of the check icon and the horizontal padding
-        // to make sure the progress bar doesn't overflow
-        width={
-          hasCompletedForm
-            ? screenWidth - 2 * space(2) - checkIconWidth
-            : screenWidth - 2 * space(2)
-        }
-      >
+      <Flex>
         <ProgressBar
           progress={progress * 100}
           animationDuration={300}
           trackColor={hasCompletedForm ? "green100" : "blue100"}
         />
       </Flex>
-      {!!hasCompletedForm && (
-        <Flex
-          position="absolute"
-          alignSelf="flex-end"
-          alignItems="flex-end"
-          style={{ top: 2 }}
-          width={ICON_WIDTH + space(0.5)}
-        >
-          <CheckCircleFillIcon height={ICON_WIDTH} width={ICON_WIDTH} fill="green100" />
-        </Flex>
-      )}
     </Flex>
   )
 }
