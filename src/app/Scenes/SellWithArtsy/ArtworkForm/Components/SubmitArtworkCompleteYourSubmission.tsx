ba928fc2e75cbd6e8b9f0b0641919adb8d535c7e
Mounir Dhahri <soussouheros@gmail.com>
2024-05-22 14:45:35 +0200

    feat: update thank you for your submission screen (#10272)

--- src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkCompleteYourSubmission.tsx
+++ src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkCompleteYourSubmission.tsx
@@ -1,8 +1,11 @@
-import { Message, Spacer, Text } from "@artsy/palette-mobile"
+import { CheckCircleFillIcon, Flex, Message, Spacer, Text, useSpace } from "@artsy/palette-mobile"
 import { useEffect } from "react"
-import { Alert, BackHandler, ScrollView } from "react-native"
+import { BackHandler, Platform, ScrollView } from "react-native"
+
+const ICON_WIDTH = 28
 
 export const SubmitArtworkCompleteYourSubmission = () => {
+  const space = useSpace()
   // Do not allow the user to go back when pressing the back button on Android
   useEffect(() => {
     const subscription = BackHandler.addEventListener("hardwareBackPress", () => {
@@ -14,36 +17,35 @@ export const SubmitArtworkCompleteYourSubmission = () => {
 
   return (
     <ScrollView>
-      <Text variant="lg" mt={4} mb={2}>
-        Thank you for submitting your artwork
+      <Flex>
+        <Flex mb={2} flexDirection="row">
+          <Flex flex={1} pr={1}>
+            <Text variant="lg-display">Thank you for submitting your artwork</Text>
+          </Flex>
+          <CheckCircleFillIcon
+            height={ICON_WIDTH}
+            width={ICON_WIDTH}
+            fill="green100"
+            // Add top margin for iOS to align with the text
+            top={Platform.OS === "ios" ? space(0.5) : 0}
+          />
+        </Flex>
+      </Flex>
+
+      <Spacer y={2} />
+
+      <Text variant="xs">
+        We will email you within 3-5 days to confirm if your artwork has been accepted or not. In
+        the meantime your submission will appear in the feature, My Collection.
       </Text>
 
       <Spacer y={2} />
 
-      <Text>
-        You have submitted the minimum information we need for an initial assessment. You will be
-        notified within 3-5 days to confirm if your artwork has been accepted or not.
-      </Text>
-
-      <Spacer y={2} />
-
-      <Message title="Next steps">
-        <Text variant="xs" color="black60">
-          If accepted, we will ask for additional details to lorem ipsum dolor. To help speed up
-          your submission, you can{" "}
-          <Text
-            variant="xs"
-            color="black60"
-            underline
-            onPress={() => {
-              Alert.alert("Fill out the additional information")
-            }}
-          >
-            fill out the additional information
-          </Text>{" "}
-          now.
-        </Text>
-      </Message>
+      <Message
+        title="Next steps"
+        variant="success"
+        text="If your submission is accepted, we will ask for additional details to lorem ipsum dolor."
+      ></Message>
     </ScrollView>
   )
 }
