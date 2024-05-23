afdb0c29eb46246684b80d4888f4877143ac8663
George Kartalis <g.kartalis7@gmail.com>
2024-05-23 16:43:12 +0200

    feat: add git tags for codepush submission iOS and Android

--- scripts/codepush/promote-to-prod.sh
+++ scripts/codepush/promote-to-prod.sh
@@ -11,3 +11,19 @@ fi
 rollout_percentage=$1
 
 bundle exec fastlane promote_codepush rollout_percentage:$rollout_percentage
+
+# fetch the latest codepush submission tags
+IOS_TAG=$(git tag -l "codepush-canary-ios-*" | tail -n 1)
+ANDROID_TAG=$(git tag -l "codepush-canary-android-*" | tail -n 1)
+
+# Create the new submission tags
+NEW_IOS_TAG="${IOS_TAG}-submission"
+NEW_ANDROID_TAG="${ANDROID_TAG}-submission"
+
+# Annotate the new submission tags
+git tag -am "$NEW_IOS_TAG" "$NEW_IOS_TAG"
+git tag -am "$NEW_ANDROID_TAG" "$NEW_ANDROID_TAG"
+
+# Step 7: Push the new tags to the remote repository
+git push origin "$NEW_IOS_TAG"
+git push origin "$NEW_ANDROID_TAG"
