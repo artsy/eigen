2c0bf220c8739321cfdc54ea5e1043f5e3f9501c
George <g.kartalis7@gmail.com>
2024-05-29 15:21:48 +0200

    fix: android anrs due to popover (#10306)

--- src/app/Scenes/MyCollection/Screens/ArtworkList/MyCollectionArtworkGridItem.tsx
+++ src/app/Scenes/MyCollection/Screens/ArtworkList/MyCollectionArtworkGridItem.tsx
@@ -1,6 +1,5 @@
 import { tappedCollectedArtwork } from "@artsy/cohesion"
-import { Flex, Box, Text, Popover } from "@artsy/palette-mobile"
-import { themeGet } from "@styled-system/theme-get"
+import { Flex, Box, Text, Popover, useColor } from "@artsy/palette-mobile"
 import { MyCollectionArtworkGridItem_artwork$data } from "__generated__/MyCollectionArtworkGridItem_artwork.graphql"
 import { DEFAULT_SECTION_MARGIN } from "app/Components/ArtworkGrids/InfiniteScrollArtworksGrid"
 import HighDemandIcon from "app/Components/Icons/HighDemandIcon"
@@ -12,11 +11,10 @@ import { navigate } from "app/system/navigation/navigate"
 import { useLocalImage } from "app/utils/LocalImageStore"
 import { useScreenDimensions } from "app/utils/hooks"
 import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
-import { View } from "react-native"
+import { TouchableHighlight, View } from "react-native"
 import { isTablet } from "react-native-device-info"
 import { createFragmentContainer, graphql } from "react-relay"
 import { useTracking } from "react-tracking"
-import styled from "styled-components/native"
 
 interface MyCollectionArtworkGridItemProps {
   artwork: MyCollectionArtworkGridItem_artwork$data
@@ -27,6 +25,7 @@ const MyCollectionArtworkGridItem: React.FC<MyCollectionArtworkGridItemProps> = 
   artwork,
   displayToolTip,
 }) => {
+  const color = useColor()
   const { trackEvent } = useTracking()
   const displayImage = artwork.images?.find((i: any) => i?.isDefault) || artwork.images?.[0]
   const { width } = useScreenDimensions()
@@ -65,40 +64,42 @@ const MyCollectionArtworkGridItem: React.FC<MyCollectionArtworkGridItemProps> = 
   }
 
   return (
-    <TouchElement
-      accessibilityLabel="Go to artwork details"
-      accessibilityRole="link"
-      onPress={() => {
-        if (!!artist) {
-          trackEvent(tracks.tappedCollectedArtwork(internalID, slug))
-          navigate("/my-collection/artwork/" + slug, {
-            passProps: {
-              medium,
-              category: mediumType?.name,
-              artistInternalID: artist.internalID,
-            },
-          })
-        } else {
-          console.warn("MyCollectionArtworkGridItem: Error: Missing artist.artistID")
-        }
-      }}
+    <Popover
+      visible={!!displayToolTip && isActive}
+      onDismiss={handleDismissPopover}
+      onPressOutside={handleDismissPopover}
+      onCloseComplete={clearActivePopover}
+      placement="top"
+      title={
+        <Text variant="xs" color="white100" fontWeight="500">
+          Interested in Selling This Work?
+        </Text>
+      }
+      content={
+        <Text variant="xs" color="white100">
+          Submit for sale and let our experts find the best selling option for you.
+        </Text>
+      }
     >
-      <Popover
-        visible={!!displayToolTip && isActive}
-        onDismiss={handleDismissPopover}
-        onPressOutside={handleDismissPopover}
-        onCloseComplete={clearActivePopover}
-        placement="top"
-        title={
-          <Text variant="xs" color="white100" fontWeight="500">
-            Interested in Selling This Work?
-          </Text>
-        }
-        content={
-          <Text variant="xs" color="white100">
-            Submit for sale and let our experts find the best selling option for you.
-          </Text>
-        }
+      <TouchableHighlight
+        underlayColor={color("white100")}
+        activeOpacity={0.8}
+        accessibilityLabel="Go to artwork details"
+        accessibilityRole="link"
+        onPress={() => {
+          if (!!artist) {
+            trackEvent(tracks.tappedCollectedArtwork(internalID, slug))
+            navigate("/my-collection/artwork/" + slug, {
+              passProps: {
+                medium,
+                category: mediumType?.name,
+                artistInternalID: artist.internalID,
+              },
+            })
+          } else {
+            console.warn("MyCollectionArtworkGridItem: Error: Missing artist.artistID")
+          }
+        }}
       >
         <View>
           <MyCollectionImageView
@@ -135,8 +136,8 @@ const MyCollectionArtworkGridItem: React.FC<MyCollectionArtworkGridItemProps> = 
             ) : null}
           </Box>
         </View>
-      </Popover>
-    </TouchElement>
+      </TouchableHighlight>
+    </Popover>
   )
 }
 
@@ -182,15 +183,6 @@ export const MyCollectionArtworkGridItemFragmentContainer = createFragmentContai
     `,
   }
 )
-
-const TouchElement = styled.TouchableHighlight.attrs(() => ({
-  underlayColor: themeGet("colors.white100"),
-  activeOpacity: 0.8,
-}))``
-
-export const tests = {
-  TouchElement,
-}
 
 const tracks = {
   tappedCollectedArtwork: (targetID: string, targetSlug: string) => {
