a0c156e162d7e6e0a91ba80b076a3a4806165dfd
Mounir Dhahri <soussouheros@gmail.com>
2024-06-06 11:36:49 +0200

    fix: signature issues (#10360)
    * chore: make signature mandatory
    
    * chore: improve tapping on signature performance
    
    * fix: text size
    
    * chore: update palette
    
    * fix: set field
    
    * chore: remove validation

--- src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkPurchaseHistory.tsx
+++ src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkPurchaseHistory.tsx
@@ -2,6 +2,8 @@ import { Flex, Join, RadioButton, Spacer, Text } from "@artsy/palette-mobile"
 import { Select } from "app/Components/Select"
 import { ArtworkDetailsFormModel } from "app/Scenes/SellWithArtsy/ArtworkForm/Utils/validation"
 import { useFormikContext } from "formik"
+import { useEffect, useState } from "react"
+import { ScrollView } from "react-native"
 
 export const PROVENANCE_LIST = [
   "Purchased directly from gallery",
@@ -17,47 +19,64 @@ export const PROVENANCE_LIST = [
 
 export const SubmitArtworkPurchaseHistory = () => {
   const { setFieldValue, values } = useFormikContext<ArtworkDetailsFormModel>()
+  const [isSigned, setIsSigned] = useState(values.signature)
+
+  useEffect(() => {
+    // This is required for performance reasons
+    // We don't want to set the value of the form field on every render
+    if (isSigned !== null && isSigned !== undefined) {
+      setFieldValue("signature", isSigned)
+    }
+  }, [isSigned, setFieldValue])
 
   return (
-    <Flex px={2}>
-      <Text variant="lg-display" mb={2}>
-        Where did you purchase the artwork?
-      </Text>
+    <Flex px={2} flex={1}>
+      <ScrollView>
+        <Text variant="lg-display" mb={2}>
+          Where did you purchase the artwork?
+        </Text>
 
-      <Join separator={<Spacer y={2} />}>
-        <Flex>
-          <Select
-            options={PROVENANCE_LIST}
-            title="Purchase information"
-            testID="PurchaseInformation_Select"
-            onSelectValue={(value) => {
-              setFieldValue("provenance", value)
-            }}
-            value={values.provenance}
-          />
-        </Flex>
-
-        <Flex>
-          <Text>Is the work signed?</Text>
-          <Flex flexDirection="row" mt={2}>
-            <RadioButton
-              mr={2}
-              text="Yes"
-              selected={values.signature === true}
-              onPress={() => {
-                setFieldValue("signature", true)
+        <Join separator={<Spacer y={2} />}>
+          <Flex>
+            <Select
+              options={PROVENANCE_LIST}
+              title="Purchase information"
+              testID="PurchaseInformation_Select"
+              onSelectValue={(value) => {
+                setFieldValue("provenance", value)
               }}
-            />
-            <RadioButton
-              text="No"
-              selected={values.signature === false}
-              onPress={() => {
-                setFieldValue("signature", false)
-              }}
+              value={values.provenance}
             />
           </Flex>
-        </Flex>
-      </Join>
+
+          <Flex>
+            <Text>Is the work signed?</Text>
+            <Flex flexDirection="row" mt={2}>
+              <RadioButton
+                mr={2}
+                text="Yes"
+                textVariant="sm-display"
+                accessibilityState={{ checked: !!isSigned }}
+                accessibilityLabel="Work is signed"
+                selected={isSigned === true}
+                onPress={() => {
+                  setIsSigned(true)
+                }}
+              />
+              <RadioButton
+                text="No"
+                textVariant="sm-display"
+                accessibilityState={{ checked: !!isSigned }}
+                accessibilityLabel="Work is not signed"
+                selected={isSigned === false}
+                onPress={() => {
+                  setIsSigned(false)
+                }}
+              />
+            </Flex>
+          </Flex>
+        </Join>
+      </ScrollView>
     </Flex>
   )
 }
