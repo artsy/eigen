2ff823681e23a471b86d50dde08f4b471df0e024
Mounir Dhahri <soussouheros@gmail.com>
2024-06-21 14:34:07 +0200

    fix: useSelectedTab

--- src/app/utils/hooks/useSelectedTab.ts
+++ src/app/utils/hooks/useSelectedTab.ts
@@ -1,15 +1,20 @@
+import { useNavigationState } from "@react-navigation/native"
 import { __unsafe_mainModalStackRef } from "app/NativeModules/ARScreenPresenterModule"
 import { BottomTabType } from "app/Scenes/BottomTabs/BottomTabType"
 
 export function useSelectedTab(): BottomTabType {
-  const tabState = __unsafe_mainModalStackRef.current
+  const tabState = useNavigationState(
+    (state) => state.routes.find((r) => r.state?.type === "tab")?.state
+  )
+
+  const _unsafe_tabState = __unsafe_mainModalStackRef.current
     ?.getState()
     ?.routes.find((r) => r.state?.type === "tab")?.state
 
-  if (!tabState) {
+  if (!tabState || !_unsafe_tabState) {
     return "home"
   } else {
-    const { index, routes } = tabState
+    const { index, routes } = tabState || _unsafe_tabState
     if (index === undefined) {
       return "home"
     }
