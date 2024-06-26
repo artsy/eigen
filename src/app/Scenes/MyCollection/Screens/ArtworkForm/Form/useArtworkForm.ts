ebd093bd5aaf936dc22c00a864ee142afbc6a815
Mounir Dhahri <soussouheros@gmail.com>
2024-06-26 15:12:49 +0200

    fix: broken edit my collection artwork (#10432)

--- src/app/Scenes/MyCollection/Screens/ArtworkForm/Form/useArtworkForm.ts
+++ src/app/Scenes/MyCollection/Screens/ArtworkForm/Form/useArtworkForm.ts
@@ -1,17 +1,15 @@
 import { ArtworkFormValues } from "app/Scenes/MyCollection/State/MyCollectionArtworkModel"
 import { GlobalStore } from "app/store/GlobalStore"
-import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
 import { FormikProps, useFormikContext } from "formik"
 import { useEffect } from "react"
 
-export function useArtworkForm(): { formik: FormikProps<ArtworkFormValues> } {
+export function useArtworkForm(disabled?: boolean): { formik: FormikProps<ArtworkFormValues> } {
   const formik = useFormikContext<ArtworkFormValues>()
-  const enableNewSubmissionFlow = useFeatureFlag("AREnableNewSubmissionFlow")
 
   useEffect(() => {
     // We don't want to update the form values if the new submission flow is enabled
     // because the form values are managed by the new submission flow
-    if (enableNewSubmissionFlow) {
+    if (disabled) {
       return
     }
 
