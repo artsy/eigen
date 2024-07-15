d286377725911e296778e8e4d84f8fee84ceed1f
Mounir Dhahri <soussouheros@gmail.com>
2024-07-15 13:02:16 +0200

    feat: add UI

--- src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkFrameInformation.tsx
+++ src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkFrameInformation.tsx
@@ -1,5 +1,5 @@
 import { OwnerType } from "@artsy/cohesion"
-import { Flex, Spacer, Text } from "@artsy/palette-mobile"
+import { Box, Flex, Input, RadioButton, Spacer, Text } from "@artsy/palette-mobile"
 import { NavigationProp, useNavigation } from "@react-navigation/native"
 import { useToast } from "app/Components/Toast/toastHook"
 import { SubmitArtworkFormStore } from "app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkFormStore"
@@ -9,12 +9,21 @@ import { ArtworkDetailsFormModel } from "app/Scenes/SellWithArtsy/ArtworkForm/Ut
 import { ProvideScreenTrackingWithCohesionSchema } from "app/utils/track"
 import { screen } from "app/utils/track/helpers"
 import { useFormikContext } from "formik"
-import { ScrollView } from "react-native"
+import { useRef, useState } from "react"
+import { LayoutAnimation, ScrollView } from "react-native"
 
 export const SubmitArtworkFrameInformation = () => {
-  const { values } = useFormikContext<ArtworkDetailsFormModel>()
+  const { values, setFieldValue } = useFormikContext<ArtworkDetailsFormModel>()
 
   const { show: showToast } = useToast()
+
+  const [isFramed, setIsFramed] = useState<Boolean | null>(null)
+  const [height, setHeight] = useState<string | undefined>(undefined)
+  const [width, setWidth] = useState<string | undefined>(undefined)
+  const [depth, setDepth] = useState<string | undefined>(undefined)
+
+  const widthRef = useRef<Input>(null)
+  const depthRef = useRef<Input>(null)
 
   const setIsLoading = SubmitArtworkFormStore.useStoreActions((actions) => actions.setIsLoading)
   const setCurrentStep = SubmitArtworkFormStore.useStoreActions((actions) => actions.setCurrentStep)
@@ -54,7 +63,100 @@ export const SubmitArtworkFrameInformation = () => {
           <Flex>
             <Text variant="lg-display">Frame</Text>
 
-            <Spacer y={2} />
+            <Text variant="sm-display" mt={2}>
+              Is the work framed?
+            </Text>
+
+            <Flex flexDirection="row" mt={2}>
+              <RadioButton
+                mr={4}
+                text="Yes"
+                selected={isFramed === true}
+                onPress={() => {
+                  LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+                  setIsFramed(true)
+                }}
+              />
+              <RadioButton
+                text="No"
+                selected={isFramed === false}
+                onPress={() => {
+                  LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+                  setIsFramed(false)
+                }}
+              />
+            </Flex>
+
+            {isFramed === true && (
+              <Flex>
+                <Flex flexDirection="row" justifyContent="space-between">
+                  <Box flex={1}>
+                    <Input
+                      title="Height"
+                      keyboardType="decimal-pad"
+                      testID="Submission_HeightInput"
+                      value={height}
+                      onChangeText={setHeight}
+                      fixedRightPlaceholder={values.dimensionsMetric}
+                      accessibilityLabel="Height"
+                      onSubmitEditing={() => {
+                        widthRef.current?.focus()
+                      }}
+                      returnKeyLabel="Next"
+                    />
+                  </Box>
+                  <Spacer x={2} />
+                  <Box flex={1}>
+                    <Input
+                      title="Width"
+                      keyboardType="decimal-pad"
+                      testID="Submission_WidthInput"
+                      value={width}
+                      onChangeText={setWidth}
+                      fixedRightPlaceholder={values.dimensionsMetric}
+                      accessibilityLabel="Width"
+                      ref={widthRef}
+                      onSubmitEditing={() => {
+                        depthRef.current?.focus()
+                      }}
+                      returnKeyLabel="Next"
+                    />
+                  </Box>
+                </Flex>
+                <Box width="50%" pr={1}>
+                  <Input
+                    title="Depth"
+                    keyboardType="decimal-pad"
+                    testID="Submission_DepthInput"
+                    value={depth}
+                    onChangeText={setDepth}
+                    fixedRightPlaceholder={values.dimensionsMetric}
+                    accessibilityLabel="Depth"
+                    ref={depthRef}
+                  />
+                </Box>
+
+                <Spacer y={2} />
+
+                <Flex flexDirection="row">
+                  <RadioButton
+                    mr={2}
+                    text="in"
+                    selected={values.dimensionsMetric === "in"}
+                    onPress={() => {
+                      setFieldValue("dimensionsMetric", "in")
+                    }}
+                  />
+                  <RadioButton
+                    text="cm"
+                    selected={values.dimensionsMetric === "cm"}
+                    onPress={() => {
+                      setFieldValue("dimensionsMetric", "in")
+                    }}
+                  />
+                </Flex>
+              </Flex>
+            )}
           </Flex>
         </ScrollView>
       </Flex>
