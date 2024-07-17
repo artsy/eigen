c3dd1096f654ad6d68b968539e7b3ad7be5d7c6f
brainbicycle <brian.beckerle@artsymail.com>
2024-07-17 14:01:26 -0300

    first pass at paginated partner artist list

--- src/app/Scenes/Partner/Components/PartnerArtistsListPaginated.tsx
+++ src/app/Scenes/Partner/Components/PartnerArtistsListPaginated.tsx
@@ -1,6 +1,9 @@
-import { Text } from "@artsy/palette-mobile"
+import { Flex, Screen, Spacer } from "@artsy/palette-mobile"
 import { PartnerArtistsListPaginatedQuery } from "__generated__/PartnerArtistsListPaginatedQuery.graphql"
 import { PartnerArtistsListPaginated_partner$key } from "__generated__/PartnerArtistsListPaginated_partner.graphql"
+import { ArtistListItemContainer as ArtistListItem } from "app/Components/ArtistListItem"
+import { extractNodes } from "app/utils/extractNodes"
+import { ActivityIndicator } from "react-native"
 import { graphql, usePaginationFragment } from "react-relay"
 
 interface PartnerArtistsListPaginatedProps {
@@ -15,10 +18,38 @@ export const PartnerArtistsListPaginated: React.FC<PartnerArtistsListPaginatedPr
     PartnerArtistsListPaginated_partner$key
   >(PartnerArtistsListPaginatedFragment, partner)
 
+  const artists = extractNodes(data.artistsConnection)
+
+  const handleLoadMore = () => {
+    if (!hasNext || isLoadingNext) {
+      return
+    }
+
+    loadNext(20)
+  }
+
   return (
-    <>
-      <Text variant="sm-display">Artists should be displayed here</Text>
-    </>
+    <Screen.FlatList
+      data={artists}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => {
+        return <ArtistListItem artist={item} />
+      }}
+      ItemSeparatorComponent={() => <Spacer y={2} />}
+      onEndReached={handleLoadMore}
+      onEndReachedThreshold={1}
+      ListFooterComponent={() => (
+        <Flex
+          alignItems="center"
+          justifyContent="center"
+          p={4}
+          pb={6}
+          style={{ opacity: isLoadingNext && hasNext ? 1 : 0 }}
+        >
+          <ActivityIndicator />
+        </Flex>
+      )}
+    />
   )
 }
 
