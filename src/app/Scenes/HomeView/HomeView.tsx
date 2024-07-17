e3ffa8241adf648ec487dce1ad3ce32c50d78935
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-07-17 15:25:09 -0400

    feat: add proper Screen components with flat list

--- src/app/Scenes/HomeView/HomeView.tsx
+++ src/app/Scenes/HomeView/HomeView.tsx
@@ -1,14 +1,45 @@
-import { Box, Text } from "@artsy/palette-mobile"
+import { Flex, Screen, Separator, Spacer, Text } from "@artsy/palette-mobile"
+import { goBack } from "app/system/navigation/navigate"
+
+const SCREEN_TITLE = "HomeView WIP"
+
+const dummyData = Array.from({ length: 10 }).map((_, i) => {
+  return {
+    index: i,
+  }
+})
 
 export const HomeView: React.FC = () => {
   return (
-    <Box>
-      <Text>Home View</Text>
-      <Text my={1}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto inventore dolor in porro
-        expedita sint molestias itaque, suscipit nulla ducimus nemo asperiores quisquam officia
-        veniam necessitatibus at rerum atque alias.
+    <Screen>
+      <Screen.AnimatedHeader onBack={goBack} title={SCREEN_TITLE} />
+
+      <Screen.StickySubHeader
+        title={SCREEN_TITLE}
+        separatorComponent={<Separator borderColor="black5" />}
+      />
+
+      <Screen.Body fullwidth>
+        <Screen.FlatList
+          data={dummyData}
+          keyExtractor={(item) => `${item.index}`}
+          renderItem={({ item }) => {
+            return <Placeholder item={item} />
+          }}
+          ItemSeparatorComponent={() => <Spacer y={1} />}
+        />
+      </Screen.Body>
+    </Screen>
+  )
+}
+
+const Placeholder: React.FC<{ item: any }> = (props) => {
+  const { item } = props
+  return (
+    <Flex bg="black10" alignItems="center">
+      <Text color="black60" py={2}>
+        Section {item.index}
       </Text>
-    </Box>
+    </Flex>
   )
 }
