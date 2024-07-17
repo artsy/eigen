461ac8f44ffa10c001000fccfb72ad0a020833e7
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-07-17 12:48:01 -0400

    feat: stub in a HomeView component for experimentation

---
+++ src/app/Scenes/HomeView/HomeView.tsx
@@ -0,0 +1,14 @@
+import { Box, Text } from "@artsy/palette-mobile"
+
+export const HomeView: React.FC = () => {
+  return (
+    <Box>
+      <Text>Home View</Text>
+      <Text my={1}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto inventore dolor in porro
+        expedita sint molestias itaque, suscipit nulla ducimus nemo asperiores quisquam officia
+        veniam necessitatibus at rerum atque alias.
+      </Text>
+    </Box>
+  )
+}
