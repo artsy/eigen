812eec2728d15b6df8f2f547414a139525fc5f64
Mounir Dhahri <soussouheros@gmail.com>
2024-08-01 18:04:37 +0200

    chore: wip

--- src/app/Scenes/HomeView/Sections/ArtworksRailHomeViewSection.tsx
+++ src/app/Scenes/HomeView/Sections/ArtworksRailHomeViewSection.tsx
@@ -1,11 +1,16 @@
 import { Flex } from "@artsy/palette-mobile"
+import { ArtworksRailHomeViewSectionQuery } from "__generated__/ArtworksRailHomeViewSectionQuery.graphql"
 import { ArtworksRailHomeViewSection_section$key } from "__generated__/ArtworksRailHomeViewSection_section.graphql"
-import { LargeArtworkRail } from "app/Components/ArtworkRail/LargeArtworkRail"
+import {
+  LargeArtworkRail,
+  LargeArtworkRailPlaceholder,
+} from "app/Components/ArtworkRail/LargeArtworkRail"
 import { SectionTitle } from "app/Components/SectionTitle"
+import { SectionT } from "app/Scenes/HomeView/Sections/Section"
 import { navigate } from "app/system/navigation/navigate"
 import { extractNodes } from "app/utils/extractNodes"
-import { View } from "react-native"
-import { graphql, useFragment } from "react-relay"
+import { withSuspense } from "app/utils/hooks/withSuspense"
+import { graphql, useFragment, useLazyLoadQuery } from "react-relay"
 
 interface ArtworksRailHomeViewSectionProps {
   section: ArtworksRailHomeViewSection_section$key
@@ -30,23 +35,58 @@ export const ArtworksRailHomeViewSection: React.FC<ArtworksRailHomeViewSectionPr
 
   return (
     <Flex>
-      <View>
-        <Flex pl={2} pr={2}>
-          <SectionTitle title={title} {...(href ? { onPress: () => navigate(href) } : {})} />
-        </Flex>
-        <LargeArtworkRail
-          artworks={artworks}
-          onPress={handleOnArtworkPress}
-          showSaveIcon
-          {...(href ? { onMorePress: () => navigate(href) } : {})}
-        />
-      </View>
+      <Flex pl={2} pr={2}>
+        <SectionTitle title={title} {...(href ? { onPress: () => navigate(href) } : {})} />
+      </Flex>
+      <LargeArtworkRail
+        artworks={artworks}
+        onPress={handleOnArtworkPress}
+        showSaveIcon
+        {...(href ? { onMorePress: () => navigate(href) } : {})}
+      />
     </Flex>
   )
 }
 
+const artworksRailHomeViewSectionQuery = graphql`
+  query ArtworksRailHomeViewSectionQuery($id: String!) {
+    homeView {
+      section(id: $id) {
+        ...ArtworksRailHomeViewSection_section
+      }
+    }
+  }
+`
+
+interface ArtworksRailHomeViewSectionQueryRendererProps {
+  section: SectionT
+}
+
+export const ArtworksRailHomeViewSectionQueryRenderer = withSuspense(
+  (props: ArtworksRailHomeViewSectionQueryRendererProps) => {
+    const data = useLazyLoadQuery<ArtworksRailHomeViewSectionQuery>(
+      artworksRailHomeViewSectionQuery,
+      {
+        id: props.section.internalID as string,
+      }
+    )
+
+    if (!data?.homeView?.section) {
+      return null
+    }
+
+    return <ArtworksRailHomeViewSection section={data.homeView.section} />
+  },
+  () => (
+    <Flex flexDirection="row">
+      <LargeArtworkRailPlaceholder />
+    </Flex>
+  )
+)
+
 const fragment = graphql`
   fragment ArtworksRailHomeViewSection_section on ArtworksRailHomeViewSection {
+    id
     component {
       href
       title
