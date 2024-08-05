070a1c9677f29c7cfee70bacb91db410a3e3758b
George <g.kartalis7@gmail.com>
2024-08-05 17:12:11 +0200

    refactor(Masonry): refactor collections page to use performant tabs.masonry (#10550)
    * refactor(Masonry): collection page to use Masonry
    
    * refactor(Masonry): tests
    
    * refactor(Masonry): tests
    
    * refactor(Masonry): tests
    
    * refactor: boost performance by wrapping renderitem usecallback
    
    * refactor: address pr comments
    
    * refactor: pr comments
    
    * feat: add placeholder

---
+++ src/app/Scenes/Collection/CollectionOverview.tsx
@@ -0,0 +1,60 @@
+import { Box, Tabs, useSpace } from "@artsy/palette-mobile"
+import { CollectionOverview_collection$key } from "__generated__/CollectionOverview_collection.graphql"
+import { ReadMore } from "app/Components/ReadMore"
+import { Schema } from "app/utils/track"
+import { isTablet } from "react-native-device-info"
+import { graphql, useFragment } from "react-relay"
+import { CollectionsHubRailsContainer as CollectionHubsRails } from "./Components/CollectionHubsRails/index"
+import { CollectionFeaturedArtistsContainer as CollectionFeaturedArtists } from "./Components/FeaturedArtists"
+
+interface CollectionProps {
+  collection: CollectionOverview_collection$key
+}
+
+export const CollectionOverview: React.FC<CollectionProps> = ({ collection }) => {
+  const data = useFragment(fragment, collection)
+  const space = useSpace()
+
+  if (!data) {
+    return null
+  }
+
+  return (
+    <Tabs.ScrollView style={{ paddingTop: space(2) }}>
+      {!!data.collectionDescription && (
+        <Box mb={4} mt={0.5} accessibilityLabel="Read more">
+          <ReadMore
+            content={data.collectionDescription}
+            maxChars={isTablet() ? 300 : 250} // truncate at 300 characters on iPads and 250 on all other devices
+            contextModule={Schema.ContextModules.CollectionDescription}
+            trackingFlow={Schema.Flow.AboutTheCollection}
+            textStyle="sans"
+          />
+        </Box>
+      )}
+      {!!data.showFeaturedArtists && (
+        <Box>
+          <CollectionFeaturedArtists collection={data} />
+        </Box>
+      )}
+      {!!data.linkedCollections && (
+        <Box>
+          <CollectionHubsRails linkedCollections={data.linkedCollections} collection={data} />
+        </Box>
+      )}
+    </Tabs.ScrollView>
+  )
+}
+
+const fragment = graphql`
+  fragment CollectionOverview_collection on MarketingCollection {
+    showFeaturedArtists
+    isDepartment
+    collectionDescription: descriptionMarkdown
+    ...FeaturedArtists_collection
+    ...CollectionHubsRails_collection
+    linkedCollections {
+      ...CollectionHubsRails_linkedCollections
+    }
+  }
+`
