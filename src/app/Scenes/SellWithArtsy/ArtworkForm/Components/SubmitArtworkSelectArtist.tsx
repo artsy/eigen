34c43fb61971edb6268d8f3a0a165314b5baaaf2
dariakoko <nargle30@gmail.com>
2024-08-14 15:23:24 +0200

    chore: address the rest of the screens

--- src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkSelectArtist.tsx
+++ src/app/Scenes/SellWithArtsy/ArtworkForm/Components/SubmitArtworkSelectArtist.tsx
@@ -1,4 +1,3 @@
-import { OwnerType } from "@artsy/cohesion"
 import { Flex, Spacer, Text } from "@artsy/palette-mobile"
 import { NavigationProp, useNavigation } from "@react-navigation/native"
 import { AutosuggestResult } from "app/Components/AutosuggestResults/AutosuggestResults"
@@ -11,13 +10,12 @@ import { SubmitArtworkFormStore } from "app/Scenes/SellWithArtsy/ArtworkForm/Com
 import { SubmitArtworkStackNavigation } from "app/Scenes/SellWithArtsy/ArtworkForm/SubmitArtworkForm"
 import { useSubmissionContext } from "app/Scenes/SellWithArtsy/ArtworkForm/Utils/useSubmissionContext"
 import { SubmissionModel } from "app/Scenes/SellWithArtsy/ArtworkForm/Utils/validation"
+import { useSubmitArtworkTracking } from "app/Scenes/SellWithArtsy/Hooks/useSubmitArtworkTracking"
 import { createOrUpdateSubmission } from "app/Scenes/SellWithArtsy/SubmitArtwork/ArtworkDetails/utils/createOrUpdateSubmission"
 import { navigate } from "app/system/navigation/navigate"
 import { PlaceholderBox, PlaceholderText, ProvidePlaceholderContext } from "app/utils/placeholders"
-import { ProvideScreenTrackingWithCohesionSchema } from "app/utils/track"
-import { screen } from "app/utils/track/helpers"
 import { useFormikContext } from "formik"
-import { Suspense } from "react"
+import { Suspense, useEffect } from "react"
 import { TouchableOpacity } from "react-native"
 
 export const SubmitArtworkSelectArtist = () => {
@@ -29,6 +27,14 @@ export const SubmitArtworkSelectArtist = () => {
   const navigation = useNavigation<NavigationProp<SubmitArtworkStackNavigation, "SelectArtist">>()
 
   const formik = useFormikContext<SubmissionModel>()
+
+  const { trackSubmissionStepScreen } = useSubmitArtworkTracking()
+
+  useEffect(() => {
+    if (currentStep === "SelectArtist") {
+      trackSubmissionStepScreen(currentStep, undefined)
+    }
+  }, [currentStep])
 
   const handleResultPress = async (result: AutosuggestResult) => {
     setIsLoading(true)
@@ -80,53 +86,49 @@ export const SubmitArtworkSelectArtist = () => {
   }
 
   return (
-    <ProvideScreenTrackingWithCohesionSchema
-      info={screen({ context_screen_owner_type: OwnerType.submitArtworkStepSelectArtist })}
-    >
-      <Flex px={2}>
-        <Flex pb={6}>
-          <Text variant="lg-display" mb={2}>
-            Add artist name
-          </Text>
+    <Flex px={2}>
+      <Flex pb={6}>
+        <Text variant="lg-display" mb={2}>
+          Add artist name
+        </Text>
 
-          <Suspense fallback={<Placeholder />}>
-            <ArtistAutosuggest
-              onResultPress={handleResultPress}
-              disableCustomArtists
-              onlyP1Artists
-              disableFormik
-              loading={isLoading}
-              hideCollectedArtists
-              autoFocus={currentStep === "SelectArtist"}
-              Hint={
-                <Flex py={1}>
-                  <Text variant="xs" color="black60">
-                    Currently, artists can not sell their own work on Artsy.
+        <Suspense fallback={<Placeholder />}>
+          <ArtistAutosuggest
+            onResultPress={handleResultPress}
+            disableCustomArtists
+            onlyP1Artists
+            disableFormik
+            loading={isLoading}
+            hideCollectedArtists
+            autoFocus={currentStep === "SelectArtist"}
+            Hint={
+              <Flex py={1}>
+                <Text variant="xs" color="black60">
+                  Currently, artists can not sell their own work on Artsy.
+                </Text>
+                <TouchableOpacity>
+                  <Text
+                    underline
+                    variant="xs"
+                    color="black60"
+                    style={{
+                      zIndex: 1000,
+                    }}
+                    onPress={() => {
+                      navigate(
+                        "https://support.artsy.net/s/article/Im-an-artist-Can-I-submit-my-own-work-to-sell"
+                      )
+                    }}
+                  >
+                    Learn more.
                   </Text>
-                  <TouchableOpacity>
-                    <Text
-                      underline
-                      variant="xs"
-                      color="black60"
-                      style={{
-                        zIndex: 1000,
-                      }}
-                      onPress={() => {
-                        navigate(
-                          "https://support.artsy.net/s/article/Im-an-artist-Can-I-submit-my-own-work-to-sell"
-                        )
-                      }}
-                    >
-                      Learn more.
-                    </Text>
-                  </TouchableOpacity>
-                </Flex>
-              }
-            />
-          </Suspense>
-        </Flex>
+                </TouchableOpacity>
+              </Flex>
+            }
+          />
+        </Suspense>
       </Flex>
-    </ProvideScreenTrackingWithCohesionSchema>
+    </Flex>
   )
 }
 
