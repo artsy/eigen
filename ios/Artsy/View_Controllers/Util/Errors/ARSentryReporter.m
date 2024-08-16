707d1b8edd96f005ebc2604089543bc815f5144b
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2024-08-15 21:10:06 -0400

    chore: track failed deeplinks + nav failures (#10584)
    * organize the app folder a bit
    
    * first pass at category to manage deeplink timeouts and errors
    
    * start the timeout on deeplink
    
    * native module for invalidating timeout
    
    * expose module for invalidating timeout and invalidate on navigate
    
    * invalidate in nav events rather than navigate call
    
    * capture url in sentry event
    
    * first pass at tracking webview errors
    
    * placeholder method in the not found view for capturing error
    
    * attempt to pass route to not found view
    
    * fix error for deeplink report
    
    * cleanup live sale error
    
    * fix parsing of not found route
    
    * track 404s in webviews
    
    * more consistent event names + extra names
    
    * add explicit platform check

--- ios/Artsy/View_Controllers/Util/Errors/ARSentryReporter.m
+++ ios/Artsy/View_Controllers/Util/Errors/ARSentryReporter.m
@@ -6,7 +6,7 @@
 + (void)reportProblemForUserID:(NSString *)userID
                         inSale:(NSString *)saleID
                withDescription:(NSString *)description {
-    NSError *auctionError = [NSError errorWithDomain:@"LiveSale" code:418 userInfo:@{ NSLocalizedDescriptionKey: @"User reported issue." }];
+    NSError *auctionError = [NSError errorWithDomain:@"LiveSale: User reported issue." code:418 userInfo:@{ NSLocalizedDescriptionKey: @"User reported issue." }];
     [SentrySDK captureError:auctionError withScopeBlock:^(SentryScope * _Nonnull scope) {
         [scope setContextValue:@{
             @"sale-id" : saleID,
