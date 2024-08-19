67d5bbf323ac16b7eda090822100c571062796c4
Leandro Motta <leamotta@msn.com>
2024-08-19 14:02:36 +0200

    check if partner offer is active for make offer button

--- src/app/Scenes/Artwork/Components/ArtworkCommercialButtons.tsx
+++ src/app/Scenes/Artwork/Components/ArtworkCommercialButtons.tsx
@@ -214,6 +214,7 @@ const partnerOfferFragment = graphql`
   fragment ArtworkCommercialButtons_partnerOffer on PartnerOfferToCollector {
     internalID
     isAvailable
+    isActive
     endAt
   }
 `
