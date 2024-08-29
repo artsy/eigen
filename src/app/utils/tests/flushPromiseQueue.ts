595579f86d39cf631eb3a76a0982be0adde12a88
Carlos <araujobarret@gmail.com>
2024-08-29 12:04:43 +0200

    test(flushPromiseQueue): deprecate flushPromiseQueue and give some examples (#10676)
    * test: deprecate flushPromiseQueue and give some examples
    
    * test: enhance deprecation message

--- src/app/utils/tests/flushPromiseQueue.ts
+++ src/app/utils/tests/flushPromiseQueue.ts
@@ -1,5 +1,10 @@
 /**
- * test utility to wait for all promises to be resolved
+ * @deprecated consider instead:
+ *   - await waitFor(() => { ... })
+ *   - waiting an element to appear with `await screen.findBy`
+ *   - waiting an element to disappear with `await waitForElementToBeRemoved(() => screen.getByTestId("test"))`
+ *   - resolving any pending relay operations with `mockMostRecentOperation`
+ *   - spreading any missing component dependent fragment in the test query
  */
 export const flushPromiseQueue = () =>
   new Promise((r) => setTimeout(() => requestAnimationFrame(r), 0))
