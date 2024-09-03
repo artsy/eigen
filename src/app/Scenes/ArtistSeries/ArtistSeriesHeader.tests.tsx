18c3e41966e223d97bb38cfcdb7341434a1e8558
George Kartalis <g.kartalis7@gmail.com>
2024-09-03 12:18:20 +0200

    fix: artistHeader tests

--- src/app/Scenes/ArtistSeries/ArtistSeriesHeader.tests.tsx
+++ src/app/Scenes/ArtistSeries/ArtistSeriesHeader.tests.tsx
@@ -1,34 +1,34 @@
-// import { screen } from "@testing-library/react-native"
-// import { ArtistSeriesHeaderTestsQuery } from "__generated__/ArtistSeriesHeaderTestsQuery.graphql"
-// import { ArtistSeriesHeaderFragmentContainer } from "app/Scenes/ArtistSeries/ArtistSeriesHeader"
-// import { setupTestWrapper } from "app/utils/tests/setupTestWrapper"
-// import { graphql } from "react-relay"
+import { screen } from "@testing-library/react-native"
+import { ArtistSeriesHeaderTestsQuery } from "__generated__/ArtistSeriesHeaderTestsQuery.graphql"
+import { ArtistSeriesHeaderFragmentContainer } from "app/Scenes/ArtistSeries/ArtistSeriesHeader"
+import { setupTestWrapper } from "app/utils/tests/setupTestWrapper"
+import { graphql } from "react-relay"
 
-// describe("Artist Series Header", () => {
-//   const { renderWithRelay } = setupTestWrapper<ArtistSeriesHeaderTestsQuery>({
-//     Component: ({ artistSeries }) => (
-//       <ArtistSeriesHeaderFragmentContainer artistSeries={artistSeries!} />
-//     ),
-//     query: graphql`
-//       query ArtistSeriesHeaderTestsQuery @raw_response_type {
-//         artistSeries(id: "pumpkins") {
-//           ...ArtistSeriesHeader_artistSeries
-//         }
-//       }
-//     `,
-//   })
+describe("Artist Series Header", () => {
+  const { renderWithRelay } = setupTestWrapper<ArtistSeriesHeaderTestsQuery>({
+    Component: ({ artistSeries }) => (
+      <ArtistSeriesHeaderFragmentContainer artistSeries={artistSeries!} />
+    ),
+    query: graphql`
+      query ArtistSeriesHeaderTestsQuery @raw_response_type {
+        artistSeries(id: "pumpkins") {
+          ...ArtistSeriesHeader_artistSeries
+        }
+      }
+    `,
+  })
 
-//   it("renders the Artist Series header", () => {
-//     renderWithRelay({
-//       ArtistSeries: () => ({
-//         title: "Pumpkins",
-//         image: {
-//           url: "https://www.imagesofpumpkins.cloudfront.net/primary/square.jpg",
-//         },
-//       }),
-//     })
+  it("renders the Artist Series header", () => {
+    renderWithRelay({
+      ArtistSeries: () => ({
+        title: "Pumpkins",
+        image: {
+          url: "https://www.imagesofpumpkins.cloudfront.net/primary/square.jpg",
+        },
+      }),
+    })
 
-//     expect(screen.getByText("Pumpkins")).toBeOnTheScreen()
-//     expect(screen.getByTestId("ArtistSeriesHeaderImage")).toBeOnTheScreen()
-//   })
-// })
+    expect(screen.getByText("Pumpkins")).toBeOnTheScreen()
+    expect(screen.getByTestId("ArtistSeriesHeaderImage")).toBeOnTheScreen()
+  })
+})
