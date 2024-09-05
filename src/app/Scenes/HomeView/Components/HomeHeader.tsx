d9fb96007249fc96245a3593baee85edc0aac1b7
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-09-05 10:30:08 -0400

    fix: ensure logo remains centered horizontally

--- src/app/Scenes/HomeView/Components/HomeHeader.tsx
+++ src/app/Scenes/HomeView/Components/HomeHeader.tsx
@@ -10,32 +10,38 @@ export const HomeHeader: React.FC = () => {
 
   return (
     <Box py={2}>
-      <Flex flexDirection="row" justifyContent="space-between" px={2}>
-        <InfoButton
-          titleElement={
-            <Text color="blue100" weight="medium">
-              Alpha
-            </Text>
-          }
-          modalTitle="Home View"
-          modalContent={
-            <Box py={1}>
-              <Join separator={<Spacer y={0.5} />}>
-                <Text variant="sm">Hello! 👋</Text>
-                <Text variant="sm">
-                  This is an unreleased version of the app home screen. To switch to the current
-                  production version, disable the feature flag for "Use new home view" in admin
-                  settings.
-                </Text>
-                <Text variant="sm">
-                  Please direct any feedback to the #pdde-art-advisor channel in Slack.
-                </Text>
-              </Join>
-            </Box>
-          }
-        />
-        <ArtsyLogoBlackIcon scale={0.75} />
-        <ActivityIndicator hasUnseenNotifications={hasUnseenNotifications} />
+      <Flex flexDirection="row" px={2} justifyContent="space-between" alignItems="center">
+        <Box flex={1}>
+          <InfoButton
+            titleElement={
+              <Text color="blue100" weight="medium">
+                Alpha
+              </Text>
+            }
+            modalTitle="Home View"
+            modalContent={
+              <Box py={1} flex={1}>
+                <Join separator={<Spacer y={0.5} />}>
+                  <Text variant="sm">Hello! 👋</Text>
+                  <Text variant="sm">
+                    This is an unreleased version of the app home screen. To switch to the current
+                    production version, disable the feature flag for "Use new home view" in admin
+                    settings.
+                  </Text>
+                  <Text variant="sm">
+                    Please direct any feedback to the #pdde-art-advisor channel in Slack.
+                  </Text>
+                </Join>
+              </Box>
+            }
+          />
+        </Box>
+        <Box>
+          <ArtsyLogoBlackIcon scale={0.75} />
+        </Box>
+        <Box flex={1} alignItems="flex-end">
+          <ActivityIndicator hasUnseenNotifications={hasUnseenNotifications} />
+        </Box>
       </Flex>
     </Box>
   )
