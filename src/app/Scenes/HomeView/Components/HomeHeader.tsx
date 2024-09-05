b51943694b6a3ff4f20ab3578b7f70cb8fc902b8
devon blandin <dblandin@gmail.com>
2024-09-05 15:56:56 +0200

    feat: hook up an info button/modal to the New/Alpha badge on the homeview (#10718)
    feat: hook up an info button/modal to the New/Alpha badge on the home view

--- src/app/Scenes/HomeView/Components/HomeHeader.tsx
+++ src/app/Scenes/HomeView/Components/HomeHeader.tsx
@@ -1,4 +1,5 @@
-import { Spacer, ArtsyLogoBlackIcon, Flex, Box, Text } from "@artsy/palette-mobile"
+import { Spacer, ArtsyLogoBlackIcon, Flex, Box, Text, Join } from "@artsy/palette-mobile"
+import { InfoButton } from "app/Components/Buttons/InfoButton"
 import { GlobalStore } from "app/store/GlobalStore"
 import { ActivityIndicator } from "./ActivityIndicator"
 
@@ -10,9 +11,29 @@ export const HomeHeader: React.FC = () => {
   return (
     <Box mb={1} mt={2}>
       <Flex flexDirection="row" justifyContent="space-between">
-        <Text ml={2} color="blue100" weight="medium">
-          New
-        </Text>
+        <InfoButton
+          titleElement={
+            <Text ml={2} color="blue100" weight="medium">
+              Alpha
+            </Text>
+          }
+          modalTitle="Home View"
+          modalContent={
+            <Box py={1}>
+              <Join separator={<Spacer y={0.5} />}>
+                <Text variant="sm">Hello! 👋</Text>
+                <Text variant="sm">
+                  This is an unreleased version of the app home screen. To switch to the current
+                  production version, disable the feature flag for "Use new home view" in admin
+                  settings.
+                </Text>
+                <Text variant="sm">
+                  Please direct any feedback to the #pdde-art-advisor channel in Slack.
+                </Text>
+              </Join>
+            </Box>
+          }
+        />
         <ArtsyLogoBlackIcon scale={0.75} />
         <ActivityIndicator hasUnseenNotifications={hasUnseenNotifications} />
       </Flex>
