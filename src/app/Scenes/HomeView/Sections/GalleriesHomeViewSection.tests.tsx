c3ceed593397a3eb3baba2c9c8b8f82998311a7b
Mounir Dhahri <soussouheros@gmail.com>
2024-09-09 10:01:07 +0200

    feat: add GalleriesHomeViewSection tracking

--- src/app/Scenes/HomeView/Sections/GalleriesHomeViewSection.tests.tsx
+++ src/app/Scenes/HomeView/Sections/GalleriesHomeViewSection.tests.tsx
@@ -2,6 +2,7 @@ import { fireEvent, screen } from "@testing-library/react-native"
 import { GalleriesHomeViewSectionTestsQuery } from "__generated__/GalleriesHomeViewSectionTestsQuery.graphql"
 import { GalleriesHomeViewSection } from "app/Scenes/HomeView/Sections/GalleriesHomeViewSection"
 import { navigate } from "app/system/navigation/navigate"
+import { mockTrackEvent } from "app/utils/tests/globallyMockedStuff"
 import { setupTestWrapper } from "app/utils/tests/setupTestWrapper"
 import { graphql } from "react-relay"
 
@@ -25,13 +26,16 @@ describe("GalleriesHomeViewSection", () => {
 
   it("renders the section properly", async () => {
     renderWithRelay({
-      HomeViewComponent: () => ({
-        title: "Galleries Near You",
-        backgroundImageURL: "https://url.com/image.jpg",
-        description: "Follow these local galleries for updates on artists you love.",
-        behaviors: {
-          viewAll: {
-            href: "/galleries-for-you",
+      GalleriesHomeViewSection: () => ({
+        internalID: "home-view-section-galleries-near-you",
+        component: {
+          title: "Galleries Near You",
+          backgroundImageURL: "https://url.com/image.jpg",
+          description: "Follow these local galleries for updates on artists you love.",
+          behaviors: {
+            viewAll: {
+              href: "/galleries-for-you",
+            },
           },
         },
       }),
@@ -40,6 +44,16 @@ describe("GalleriesHomeViewSection", () => {
     expect(screen.getByText("Galleries Near You")).toBeOnTheScreen()
 
     fireEvent.press(screen.getByText("Explore"))
+    expect(mockTrackEvent.mock.calls[0]).toMatchInlineSnapshot(`
+      [
+        {
+          "action": "tappedShowMore",
+          "context_module": "home-view-section-galleries-near-you",
+          "context_screen_owner_type": "home",
+          "destination_screen_owner_type": "galleriesForYou",
+        },
+      ]
+    `)
     expect(navigate).toHaveBeenCalledWith("/galleries-for-you")
   })
 })
