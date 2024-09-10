006ada7cc09840472b33ab7ea683a13e565bece4
George Kartalis <g.kartalis7@gmail.com>
2024-09-10 15:09:46 +0200

    boom ~> GOTCHA android native module link

--- android/app/src/main/java/net/artsy/app/MainApplication.kt
+++ android/app/src/main/java/net/artsy/app/MainApplication.kt
@@ -30,6 +30,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+              add(ArtsyNativePackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
