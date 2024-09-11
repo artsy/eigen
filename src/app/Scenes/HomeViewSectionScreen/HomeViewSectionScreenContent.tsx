1db4ee18e96dfed8fe222955500046caab2aadd7
Mounir Dhahri <soussouheros@gmail.com>
2024-09-11 10:04:59 +0200

    fix: home view artworks screen (#10743)
    * fix: update section type on section screen
    
    * chore: add tests to home view section screen artworks

--- src/app/Scenes/HomeViewSectionScreen/HomeViewSectionScreenContent.tsx
+++ src/app/Scenes/HomeViewSectionScreen/HomeViewSectionScreenContent.tsx
@@ -1,7 +1,6 @@
+import { Flex, Spacer, Text } from "@artsy/palette-mobile"
 import { HomeViewSectionScreenQuery$data } from "__generated__/HomeViewSectionScreenQuery.graphql"
 import { HomeViewSectionScreenArtworks } from "app/Scenes/HomeViewSectionScreen/HomeViewSectionScreenArtworks"
-
-import { Text } from "react-native-svg"
 
 type HomeViewSectionScreenContentT = NonNullable<
   HomeViewSectionScreenQuery$data["homeView"]["section"]
@@ -13,12 +12,31 @@ export const HomeViewSectionScreenContent: React.FC<{ section: HomeViewSectionSc
   const { section } = props
 
   switch (section.__typename) {
-    case "ArtworksRailHomeViewSection":
+    case "HomeViewSectionArtworks":
       return <HomeViewSectionScreenArtworks section={section} />
 
     default:
       if (__DEV__) {
-        return <Text>Non supported screen section: {section.__typename}</Text>
+        return (
+          <Flex p={2}>
+            <Text fontWeight="500">Non supported screen section:</Text>
+
+            <Spacer y={2} />
+
+            <Text>
+              Section Type:{" "}
+              <Text fontWeight={500} color="devpurple">
+                {section.__typename}
+              </Text>
+            </Text>
+            <Text>
+              Section ID:{" "}
+              <Text fontWeight={500} color="devpurple">
+                {section.internalID}
+              </Text>
+            </Text>
+          </Flex>
+        )
       }
       return null
   }
