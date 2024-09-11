368b993f8897f51987aac1cc2f2d1e8a1ce4d446
Mounir Dhahri <soussouheros@gmail.com>
2024-09-11 08:53:46 +0200

    chore: remove unnecessary test

--- src/app/Scenes/Home/HomeContainer.tests.tsx
+++ src/app/Scenes/Home/HomeContainer.tests.tsx
@@ -26,14 +26,9 @@ describe("conditional rendering of old vs new home screen", () => {
         () => __globalStoreTestUtils__?.injectFeatureFlags({ ARPreferLegacyHomeScreen: false })
       )
 
-      xit("renders the NEW screen", () => {
+      it("renders the NEW screen", () => {
         renderWithWrappers(<HomeContainer />)
         expect(screen.getByTestId("new-home-view-skeleton")).toBeOnTheScreen()
-      })
-
-      it("temporarily renders the old screen while we make breaking changes", () => {
-        renderWithWrappers(<HomeContainer />)
-        expect(screen.queryByTestId("new-home-view-skeleton")).not.toBeOnTheScreen()
       })
     })
 
