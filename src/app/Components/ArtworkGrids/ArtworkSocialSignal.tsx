65c2388b6513139fde3bbc8846068c60eede880e
Sultan <sultan.al-maari@artsymail.com>
2024-09-11 15:52:42 +0200

    feat(EMI-2027): Add Curators' Pick and Increased Interest signals

---
+++ src/app/Components/ArtworkGrids/ArtworkSocialSignal.tsx
@@ -0,0 +1,70 @@
+import { Box, Text } from "@artsy/palette-mobile"
+import { ArtworkSocialSignal_collectorSignals$key } from "__generated__/ArtworkSocialSignal_collectorSignals.graphql"
+import { graphql, useFragment } from "react-relay"
+
+interface ArtworkSocialSignalProps {
+  collectorSignals: ArtworkSocialSignal_collectorSignals$key
+  hideIncreasedInterest?: boolean
+  hideCuratorsPick?: boolean
+  dark?: boolean
+}
+export const ArtworkSocialSignal: React.FC<ArtworkSocialSignalProps> = ({
+  collectorSignals,
+  hideCuratorsPick,
+  hideIncreasedInterest,
+  dark = false,
+}) => {
+  const { curatorsPick, increasedInterest } = useFragment(fragment, collectorSignals)
+
+  if ((hideIncreasedInterest && hideCuratorsPick) || (!curatorsPick && !increasedInterest)) {
+    return null
+  }
+
+  const primaryColor = dark ? "white100" : "black100"
+
+  switch (true) {
+    case increasedInterest && !hideIncreasedInterest:
+      return (
+        <Box
+          px={0.5}
+          pb="2px"
+          alignSelf="flex-start"
+          borderRadius={3}
+          borderWidth={1}
+          borderColor={primaryColor}
+          mb={0.5}
+        >
+          <Text color={primaryColor} variant="xxs">
+            Increased Interest
+          </Text>
+        </Box>
+      )
+
+    case curatorsPick && !hideCuratorsPick:
+      return (
+        <Box
+          px={0.5}
+          pb="2px"
+          alignSelf="flex-start"
+          borderRadius={3}
+          borderWidth={1}
+          borderColor={primaryColor}
+          mb={0.5}
+        >
+          <Text color={primaryColor} variant="xxs">
+            Curators’ Pick
+          </Text>
+        </Box>
+      )
+
+    default:
+      return null
+  }
+}
+
+const fragment = graphql`
+  fragment ArtworkSocialSignal_collectorSignals on CollectorSignals {
+    increasedInterest
+    curatorsPick
+  }
+`
