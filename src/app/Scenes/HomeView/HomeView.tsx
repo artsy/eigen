1a95680e7a73b82aa2297588afda33e6c66b72ce
Mounir Dhahri <soussouheros@gmail.com>
2024-09-12 18:16:41 +0200

    feat: migrate HomeViewSectionSales section

--- src/app/Scenes/HomeView/HomeView.tsx
+++ src/app/Scenes/HomeView/HomeView.tsx
@@ -174,7 +174,6 @@ const sectionsFragment = graphql`
             }
             ... on HomeViewSectionSales {
               internalID
-              ...HomeViewSectionSales_section
             }
             ... on HomeViewSectionGalleries {
               internalID
