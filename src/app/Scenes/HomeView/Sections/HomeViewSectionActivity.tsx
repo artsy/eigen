8017f14dff4221ff58f6a4884b85333adaf38461
Mounir Dhahri <soussouheros@gmail.com>
2024-09-13 12:17:56 +0200

    feat: add HomeViewSectionActivity Placeholder

--- src/app/Scenes/HomeView/Sections/HomeViewSectionActivity.tsx
+++ src/app/Scenes/HomeView/Sections/HomeViewSectionActivity.tsx
@@ -1,15 +1,21 @@
-import { Flex, Spacer } from "@artsy/palette-mobile"
+import { Flex, Join, SkeletonBox, SkeletonText, Spacer } from "@artsy/palette-mobile"
 import { HomeViewSectionActivityQuery } from "__generated__/HomeViewSectionActivityQuery.graphql"
 import { HomeViewSectionActivity_section$key } from "__generated__/HomeViewSectionActivity_section.graphql"
 import { SectionTitle } from "app/Components/SectionTitle"
 import { shouldDisplayNotification } from "app/Scenes/Activity/utils/shouldDisplayNotification"
 import { SeeAllCard } from "app/Scenes/Home/Components/ActivityRail"
-import { ActivityRailItem } from "app/Scenes/Home/Components/ActivityRailItem"
+import {
+  ACTIVITY_RAIL_ARTWORK_IMAGE_SIZE,
+  ACTIVITY_RAIL_ITEM_WIDTH,
+  ActivityRailItem,
+} from "app/Scenes/Home/Components/ActivityRailItem"
 import { HOME_VIEW_SECTIONS_SEPARATOR_HEIGHT } from "app/Scenes/HomeView/HomeView"
 import { useHomeViewTracking } from "app/Scenes/HomeView/useHomeViewTracking"
 import { navigate } from "app/system/navigation/navigate"
 import { extractNodes } from "app/utils/extractNodes"
 import { withSuspense } from "app/utils/hooks/withSuspense"
+import { useMemoizedRandom } from "app/utils/placeholders"
+import { times } from "lodash"
 import { FlatList } from "react-native"
 import { graphql, useFragment, useLazyLoadQuery } from "react-relay"
 
@@ -131,6 +137,40 @@ const homeViewSectionActivityQuery = graphql`
   }
 `
 
+export const HomeViewSectionActivityPlaceholder: React.FC = () => {
+  const randomValue = useMemoizedRandom()
+
+  return (
+    <Flex ml={2} mr={2} my={2}>
+      <SkeletonText variant="lg-display">Latest Activity</SkeletonText>
+      <Spacer y={2} />
+      <Flex flexDirection="row">
+        <Join separator={<Spacer x="15px" />}>
+          {times(3 + randomValue * 10).map((index) => (
+            <Flex key={index} flexDirection="row">
+              <SkeletonBox
+                height={ACTIVITY_RAIL_ARTWORK_IMAGE_SIZE}
+                width={ACTIVITY_RAIL_ARTWORK_IMAGE_SIZE}
+              />
+              <Flex ml={1} maxWidth={ACTIVITY_RAIL_ITEM_WIDTH}>
+                <SkeletonText variant="xs" numberOfLines={1}>
+                  6 new works by Andy Warhol
+                </SkeletonText>
+                <SkeletonText variant="xs" numberOfLines={1}>
+                  2021-01-01
+                </SkeletonText>
+                <SkeletonText variant="xs" numberOfLines={1}>
+                  Follow - 6 days ago
+                </SkeletonText>
+              </Flex>
+            </Flex>
+          ))}
+        </Join>
+      </Flex>
+    </Flex>
+  )
+}
+
 export const HomeViewSectionActivityQueryRenderer: React.FC<{
   sectionID: string
 }> = withSuspense((props) => {
@@ -143,4 +183,4 @@ export const HomeViewSectionActivityQueryRenderer: React.FC<{
   }
 
   return <HomeViewSectionActivity section={data.homeView.section} />
-})
+}, HomeViewSectionActivityPlaceholder)
