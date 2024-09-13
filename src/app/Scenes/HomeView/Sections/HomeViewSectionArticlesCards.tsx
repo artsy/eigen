c4989f203b5a24deb3603c24b374ae2b4bd20911
Mounir Dhahri <soussouheros@gmail.com>
2024-09-13 15:59:19 +0200

    feat: migrate HomeViewSectionArticles section placeholder

--- src/app/Scenes/HomeView/Sections/HomeViewSectionArticlesCards.tsx
+++ src/app/Scenes/HomeView/Sections/HomeViewSectionArticlesCards.tsx
@@ -1,10 +1,20 @@
-import { Flex, Separator, Text, Touchable, useSpace } from "@artsy/palette-mobile"
+import {
+  Flex,
+  Separator,
+  Skeleton,
+  SkeletonBox,
+  SkeletonText,
+  Text,
+  Touchable,
+  useSpace,
+} from "@artsy/palette-mobile"
 import { HomeViewSectionArticlesCardsQuery } from "__generated__/HomeViewSectionArticlesCardsQuery.graphql"
 import { HomeViewSectionArticlesCards_section$key } from "__generated__/HomeViewSectionArticlesCards_section.graphql"
 import { HOME_VIEW_SECTIONS_SEPARATOR_HEIGHT } from "app/Scenes/HomeView/HomeView"
 import { navigate } from "app/system/navigation/navigate"
 import { extractNodes } from "app/utils/extractNodes"
 import { withSuspense } from "app/utils/hooks/withSuspense"
+import { times } from "lodash"
 import { graphql, useFragment, useLazyLoadQuery } from "react-relay"
 
 interface HomeViewSectionArticlesCardsProps {
@@ -94,6 +104,40 @@ const fragment = graphql`
   }
 `
 
+const HomeViewSectionArticlesCardsPlaceholder: React.FC = () => {
+  const space = useSpace()
+  return (
+    <Skeleton>
+      <Flex
+        my={HOME_VIEW_SECTIONS_SEPARATOR_HEIGHT}
+        mx={2}
+        p={2}
+        border="1px solid"
+        borderColor="black30"
+        gap={space(2)}
+      >
+        <Flex flexDirection="row" justifyContent="space-between" alignItems="center">
+          <SkeletonText variant="lg-display">title</SkeletonText>
+          <SkeletonText variant="lg-display">2021-12-31</SkeletonText>
+        </Flex>
+        {times(3).map((_, index) => (
+          <SkeletonBox key={index} gap={space(2)}>
+            <Flex flexDirection="row" alignItems="center">
+              <SkeletonText variant="sm-display" numberOfLines={3}>
+                Larry Gagosian and Peter Doig to collaborate on a new exhibition.
+              </SkeletonText>
+            </Flex>
+            {index !== 2 && <Separator />}
+          </SkeletonBox>
+        ))}
+        <Flex flexDirection="row" justifyContent="flex-end">
+          <SkeletonText variant="sm-display">More News</SkeletonText>
+        </Flex>
+      </Flex>
+    </Skeleton>
+  )
+}
+
 const homeViewSectionArticlesCardsQuery = graphql`
   query HomeViewSectionArticlesCardsQuery($id: String!) {
     homeView {
@@ -119,4 +163,4 @@ export const HomeViewSectionArticlesCardsQueryRenderer: React.FC<{
   }
 
   return <HomeViewSectionArticlesCards section={data.homeView.section} />
-})
+}, HomeViewSectionArticlesCardsPlaceholder)
