e5516251e340c46882bba252985eeda3d1183fc7
Mounir Dhahri <soussouheros@gmail.com>
2024-09-13 13:14:22 +0200

    feat: migration HomeViewSectionAuctionResults section placeholder

--- src/app/Scenes/HomeView/Sections/HomeViewSectionAuctionResults.tsx
+++ src/app/Scenes/HomeView/Sections/HomeViewSectionAuctionResults.tsx
@@ -1,26 +1,34 @@
-import { Flex, useScreenDimensions } from "@artsy/palette-mobile"
+import { Flex, Join, Skeleton, SkeletonBox, SkeletonText, Spacer } from "@artsy/palette-mobile"
 import { HomeViewSectionAuctionResultsQuery } from "__generated__/HomeViewSectionAuctionResultsQuery.graphql"
 import { HomeViewSectionAuctionResults_section$key } from "__generated__/HomeViewSectionAuctionResults_section.graphql"
 import { BrowseMoreRailCard } from "app/Components/BrowseMoreRailCard"
-import { AuctionResultListItemFragmentContainer } from "app/Components/Lists/AuctionResultListItem"
+import {
+  AUCTION_RESULT_CARD_IMAGE_HEIGHT,
+  AUCTION_RESULT_CARD_IMAGE_WIDTH,
+  AuctionResultListItemFragmentContainer,
+} from "app/Components/Lists/AuctionResultListItem"
 import { SectionTitle } from "app/Components/SectionTitle"
 import { HOME_VIEW_SECTIONS_SEPARATOR_HEIGHT } from "app/Scenes/HomeView/HomeView"
 import { useHomeViewTracking } from "app/Scenes/HomeView/useHomeViewTracking"
 import { navigate } from "app/system/navigation/navigate"
 import { extractNodes } from "app/utils/extractNodes"
 import { withSuspense } from "app/utils/hooks/withSuspense"
-import { FlatList } from "react-native"
+import { useMemoizedRandom } from "app/utils/placeholders"
+import { times } from "lodash"
+import { Dimensions, FlatList } from "react-native"
 import { graphql, useFragment, useLazyLoadQuery } from "react-relay"
 
 interface HomeViewSectionAuctionResultsProps {
   section: HomeViewSectionAuctionResults_section$key
 }
 
+// Avoid the card width to be too wide on tablets
+const AUCTION_RESULT_CARD_WIDTH = Math.min(400, Dimensions.get("window").width * 0.9)
+
 export const HomeViewSectionAuctionResults: React.FC<HomeViewSectionAuctionResultsProps> = (
   props
 ) => {
   const section = useFragment(sectionFragment, props.section)
-  const { width: screenWidth } = useScreenDimensions()
   const tracking = useHomeViewTracking()
 
   if (!section || !section.auctionResultsConnection?.totalCount) {
@@ -48,7 +56,7 @@ export const HomeViewSectionAuctionResults: React.FC<HomeViewSectionAuctionResul
             <AuctionResultListItemFragmentContainer
               showArtistName
               auctionResult={item}
-              width={screenWidth * 0.9}
+              width={AUCTION_RESULT_CARD_WIDTH}
               onTrack={() => {
                 tracking.tappedAuctionResultGroup(
                   item.internalID,
@@ -100,6 +108,52 @@ const sectionFragment = graphql`
   }
 `
 
+const HomeViewSectionAuctionResultsPlaceholder: React.FC = () => {
+  const randomValue = useMemoizedRandom()
+  return (
+    <Skeleton>
+      <Flex mx={2} my={HOME_VIEW_SECTIONS_SEPARATOR_HEIGHT}>
+        <SkeletonText variant="lg-display">List of Auction Results</SkeletonText>
+
+        <Spacer y={1} />
+
+        <Flex flexDirection="row">
+          <Join separator={<Spacer x="15px" />}>
+            {times(3 + randomValue * 10).map((index) => (
+              <Flex
+                key={index}
+                maxWidth={AUCTION_RESULT_CARD_WIDTH}
+                flexDirection="row"
+                overflow="hidden"
+              >
+                <SkeletonBox
+                  key={index}
+                  height={AUCTION_RESULT_CARD_IMAGE_HEIGHT}
+                  width={AUCTION_RESULT_CARD_IMAGE_WIDTH}
+                />
+                <Spacer x={1} />
+                <Flex>
+                  <SkeletonText variant="xs">Katherine Bernhardt</SkeletonText>
+                  <SkeletonText variant="xs">Shower Power</SkeletonText>
+                  <SkeletonText variant="xs" numberOfLines={2}>
+                    Unique lithograph in colors, on somerset tub sized paper, the full sheet.
+                  </SkeletonText>
+                  <SkeletonText variant="xs" numberOfLines={1} mt={1}>
+                    Set 11, 2024 - Phillips
+                  </SkeletonText>
+                  <SkeletonText variant="xs" numberOfLines={1}>
+                    $10,000
+                  </SkeletonText>
+                </Flex>
+              </Flex>
+            ))}
+          </Join>
+        </Flex>
+      </Flex>
+    </Skeleton>
+  )
+}
+
 const homeViewSectionAuctionResultsQuery = graphql`
   query HomeViewSectionAuctionResultsQuery($id: String!) {
     homeView {
@@ -125,4 +179,4 @@ export const HomeViewSectionAuctionResultsQueryRenderer: React.FC<{
   }
 
   return <HomeViewSectionAuctionResults section={data.homeView.section} />
-})
+}, HomeViewSectionAuctionResultsPlaceholder)
