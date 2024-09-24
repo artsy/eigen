03eb1986328027676ea314c5fe822b6f07c048a6
Adam Iskounen <adam.iskounen@artsymail.com>
2024-09-24 12:05:25 -0400

    begins to add social sign-in and -up

--- src/app/Scenes/Onboarding/AuthenticationDialog/WelcomeStep.tsx
+++ src/app/Scenes/Onboarding/AuthenticationDialog/WelcomeStep.tsx
@@ -1,19 +1,55 @@
-import { Flex, LinkText, Text, useTheme } from "@artsy/palette-mobile"
+import { Button, Flex, LinkText, Text, useTheme } from "@artsy/palette-mobile"
 import { BottomSheetScrollView } from "@gorhom/bottom-sheet"
 import { StackScreenProps } from "@react-navigation/stack"
 import { BottomSheetInput } from "app/Components/BottomSheetInput"
 import { OnboardingHomeNavigationStack } from "app/Scenes/Onboarding/OnboardingHome"
+import { AuthPromiseRejectType, AuthPromiseResolveType } from "app/store/AuthModel"
+import { GlobalStore } from "app/store/GlobalStore"
 import { navigate } from "app/system/navigation/navigate"
-import { Image } from "react-native"
+import { osMajorVersion } from "app/utils/platformUtil"
+import { useState } from "react"
+import { Image, InteractionManager, Platform } from "react-native"
 
 type WelcomeStepProps = StackScreenProps<OnboardingHomeNavigationStack, "WelcomeStep">
 
 export const WelcomeStep: React.FC<WelcomeStepProps> = ({ navigation }) => {
+  const [mode, _setMode] = useState<"login" | "signup">("login")
+
   const { space } = useTheme()
 
   const handleEmailInputFocus = () => {
     navigation.navigate("EmailStep")
   }
+
+  const handleSocialLogin = async (callback: () => Promise<AuthPromiseResolveType>) => {
+    GlobalStore.actions.auth.setSessionState({ isLoading: true })
+    InteractionManager.runAfterInteractions(() => {
+      callback().catch((error: AuthPromiseRejectType) => {
+        InteractionManager.runAfterInteractions(() => {
+          GlobalStore.actions.auth.setSessionState({ isLoading: false })
+          InteractionManager.runAfterInteractions(() => {
+            // TODO: handle error like OnboardingSocialPick does
+            console.error(error)
+          })
+        })
+      })
+    })
+  }
+
+  const continueWithApple = () =>
+    GlobalStore.actions.auth.authApple({ agreedToReceiveEmails: true })
+
+  const continueWithGoogle = () =>
+    GlobalStore.actions.auth.authGoogle({
+      signInOrUp: mode === "login" ? "signIn" : "signUp",
+      agreedToReceiveEmails: mode === "signup",
+    })
+
+  const continueWithFacebook = () =>
+    GlobalStore.actions.auth.authFacebook({
+      signInOrUp: mode === "login" ? "signIn" : "signUp",
+      agreedToReceiveEmails: mode === "signup",
+    })
 
   return (
     <BottomSheetScrollView>
@@ -30,16 +66,33 @@ export const WelcomeStep: React.FC<WelcomeStepProps> = ({ navigation }) => {
           <Text variant="xs" textAlign="center">
             Or continue with
           </Text>
+
           <Flex flexDirection="row" justifyContent="space-evenly">
-            <Flex>
-              <Image source={require("images/apple.webp")} resizeMode="contain" />
-            </Flex>
-            <Flex>
-              <Image source={require("images/google.webp")} resizeMode="contain" />
-            </Flex>
-            <Flex>
-              <Image source={require("images/facebook.webp")} resizeMode="contain" />
-            </Flex>
+            {Platform.OS === "ios" && osMajorVersion() >= 13 && (
+              <Button
+                variant="fillDark"
+                width="100%"
+                onPress={() => handleSocialLogin(continueWithApple)}
+              >
+                <Image source={require("images/apple.webp")} />
+              </Button>
+            )}
+
+            <Button
+              variant="outline"
+              width="100%"
+              onPress={() => handleSocialLogin(continueWithGoogle)}
+            >
+              <Image source={require("images/google.webp")} />
+            </Button>
+
+            <Button
+              variant="outline"
+              width="100%"
+              onPress={() => handleSocialLogin(continueWithFacebook)}
+            >
+              <Image source={require("images/facebook.webp")} />
+            </Button>
           </Flex>
         </Flex>
 
