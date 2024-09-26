42b7c68a92d3462ec9d17cb976e7b36191caccb4
Mounir Dhahri <soussouheros@gmail.com>
2024-09-26 09:54:19 +0200

    chore: better DX around for @cacheable

--- src/app/system/relay/middlewares/cacheHeaderMiddleware.ts
+++ src/app/system/relay/middlewares/cacheHeaderMiddleware.ts
@@ -3,6 +3,7 @@ import {
   hasNoCacheParamPresent,
   hasPersonalizedArguments,
   isRequestCacheable,
+  SKIP_CACHE_ARGUMENTS,
 } from "app/system/relay/helpers/cacheHeaderMiddlewareHelpers"
 import { GraphQLRequest } from "app/system/relay/middlewares/types"
 import { Middleware } from "react-relay-network-modern"
@@ -16,18 +17,34 @@ export const shouldSkipCDNCache = (req: GraphQLRequest) => {
   //     - a known personalized argument is present in the query (for example `include_artworks_by_followed_artists`)
   //     - nocache param is present in the url
 
-  if (req.cacheConfig?.force === true) {
-    return true
-  }
-
   if (isRequestCacheable(req)) {
+    if (req.cacheConfig?.force === true) {
+      if (__DEV__) {
+        console.warn(
+          "You are setting force: true on a cacheable request, CDN cache will be ignored."
+        )
+      }
+      return true
+    }
+
     const url = getCurrentURL()
     if (url && hasNoCacheParamPresent(url)) {
+      if (__DEV__) {
+        console.warn(
+          "You are setting nocache param in the url on a cacheable request, CDN cache will be ignored."
+        )
+      }
       // Don't use CDN cache if the url has the nocache param
       return true
     }
 
     if (hasPersonalizedArguments(req.variables)) {
+      if (__DEV__) {
+        console.warn(
+          "You are setting a personalized argument on a cacheable request, CDN cache will be ignored.\nList of personalized arguments: ",
+          SKIP_CACHE_ARGUMENTS.join(", ")
+        )
+      }
       // Don't use CDN cache if the query has a personalized argument
       return true
     }
