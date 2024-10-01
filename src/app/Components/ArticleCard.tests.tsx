697bfef81b2b8effac32bbba383fde67a7958e7d
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-10-01 11:08:12 -0400

    test: update associated test

--- src/app/Components/ArticleCard.tests.tsx
+++ src/app/Components/ArticleCard.tests.tsx
@@ -1,5 +1,5 @@
+import { Touchable } from "@artsy/palette-mobile"
 import { renderWithWrappersLEGACY } from "app/utils/tests/renderWithWrappers"
-import { TouchableWithoutFeedback } from "react-native"
 import { ArticleCard } from "./ArticleCard"
 
 it("renders without throwing an error", () => {
@@ -14,6 +14,6 @@ it("renders without throwing an error", () => {
   }
 
   const tree = renderWithWrappersLEGACY(<ArticleCard article={article as any} onPress={onPress} />)
-  tree.root.findAllByType(TouchableWithoutFeedback)[0].props.onPress()
+  tree.root.findAllByType(Touchable)[0].props.onPress()
   expect(onPress).toHaveBeenCalled()
 })
