741734fecabc394bc3b0316d612feeae41cdaa17
brainbicycle <brian.beckerle@artsymail.com>
2024-10-07 15:40:27 -0400

    explicit fallback for MyCollectionArtworkFormDeleteArtworkModal

--- src/app/Scenes/MyCollection/Screens/ArtworkForm/Components/MyCollectionArtworkFormDeleteArtworkModal.tsx
+++ src/app/Scenes/MyCollection/Screens/ArtworkForm/Components/MyCollectionArtworkFormDeleteArtworkModal.tsx
@@ -8,7 +8,7 @@ import {
   Text,
 } from "@artsy/palette-mobile"
 import { MyCollectionArtworkFormDeleteArtworkModalQuery } from "__generated__/MyCollectionArtworkFormDeleteArtworkModalQuery.graphql"
-import { withSuspense } from "app/utils/hooks/withSuspense"
+import { strictWithSuspense } from "app/utils/hooks/withSuspense"
 import { useState } from "react"
 import { Modal } from "react-native"
 import { graphql, useLazyLoadQuery } from "react-relay"
@@ -34,73 +34,77 @@ const myCollectionArtworkFormDeleteArtworkModalQuery = graphql`
 `
 
 export const MyCollectionArtworkFormDeleteArtworkModal: React.FC<MyCollectionArtworkFormDeleteArtworkModalProps> =
-  withSuspense(({ visible, hideModal, deleteArtwork, artistID }) => {
-    const data = useLazyLoadQuery<MyCollectionArtworkFormDeleteArtworkModalQuery>(
-      myCollectionArtworkFormDeleteArtworkModalQuery,
-      {
-        artistID: artistID,
-      }
-    )
+  strictWithSuspense(
+    ({ visible, hideModal, deleteArtwork, artistID }) => {
+      const data = useLazyLoadQuery<MyCollectionArtworkFormDeleteArtworkModalQuery>(
+        myCollectionArtworkFormDeleteArtworkModalQuery,
+        {
+          artistID: artistID,
+        }
+      )
 
-    const [shouldDeleteArtist, setShouldDeleteArtist] = useState(false)
+      const [shouldDeleteArtist, setShouldDeleteArtist] = useState(false)
 
-    // If the query fails, disable artist deletion. It can be done separetely from the home screen
-    const isLastArtwork = (data.me?.myCollectionConnection?.totalCount || 10) === 1
+      // If the query fails, disable artist deletion. It can be done separetely from the home screen
+      const isLastArtwork = (data.me?.myCollectionConnection?.totalCount || 10) === 1
 
-    return (
-      <Modal
-        visible={visible}
-        onDismiss={hideModal}
-        presentationStyle="pageSheet"
-        onRequestClose={hideModal}
-        animationType="slide"
-      >
-        <Flex p={2}>
-          <Text variant="lg-display">Delete this artwork?</Text>
-          <Spacer y={2} />
-          <Text variant="sm">This artwork will be removed from My Collection.</Text>
-          <Spacer y={4} />
-          <Checkbox
-            onPress={() => setShouldDeleteArtist(!shouldDeleteArtist)}
-            disabled={!isLastArtwork}
-            justifyContent="center"
-            flex={undefined}
-            checked={shouldDeleteArtist}
-            accessibilityLabel={`Remove ${data.artist?.name} from the artists you collect`}
-            accessibilityState={{ checked: shouldDeleteArtist }}
-            accessibilityHint="If you remove the artist from your collection, they will no longer appear in your profile as a collected artist."
-          >
-            <Text variant="xs" color={isLastArtwork ? "black100" : "black60"}>
-              Remove {data.artist?.name} from the artists you collect
-            </Text>
-          </Checkbox>
+      return (
+        <Modal
+          visible={visible}
+          onDismiss={hideModal}
+          presentationStyle="pageSheet"
+          onRequestClose={hideModal}
+          animationType="slide"
+        >
+          <Flex p={2}>
+            <Text variant="lg-display">Delete this artwork?</Text>
+            <Spacer y={2} />
+            <Text variant="sm">This artwork will be removed from My Collection.</Text>
+            <Spacer y={4} />
+            <Checkbox
+              onPress={() => setShouldDeleteArtist(!shouldDeleteArtist)}
+              disabled={!isLastArtwork}
+              justifyContent="center"
+              flex={undefined}
+              checked={shouldDeleteArtist}
+              accessibilityLabel={`Remove ${data.artist?.name} from the artists you collect`}
+              accessibilityState={{ checked: shouldDeleteArtist }}
+              accessibilityHint="If you remove the artist from your collection, they will no longer appear in your profile as a collected artist."
+            >
+              <Text variant="xs" color={isLastArtwork ? "black100" : "black60"}>
+                Remove {data.artist?.name} from the artists you collect
+              </Text>
+            </Checkbox>
 
-          {!isLastArtwork && (
-            <>
-              <Spacer y={1} />
-              <Message
-                variant="default"
-                title="To remove this artist, please remove all their other artworks from My Collection first."
-                IconComponent={() => <InfoCircleIcon width={18} height={18} fill="black100" />}
-              />
-            </>
-          )}
-          <Spacer y={4} />
-          <Button
-            block
-            onPress={() => {
-              deleteArtwork(shouldDeleteArtist)
-            }}
-            haptic="impactHeavy"
-            accessibilityLabel="Delete Artwork"
-          >
-            Delete Artwork
-          </Button>
-          <Spacer y={2} />
-          <Button block variant="outline" onPress={hideModal} haptic accessibilityLabel="Cancel">
-            Cancel
-          </Button>
-        </Flex>
-      </Modal>
-    )
-  })
+            {!isLastArtwork && (
+              <>
+                <Spacer y={1} />
+                <Message
+                  variant="default"
+                  title="To remove this artist, please remove all their other artworks from My Collection first."
+                  IconComponent={() => <InfoCircleIcon width={18} height={18} fill="black100" />}
+                />
+              </>
+            )}
+            <Spacer y={4} />
+            <Button
+              block
+              onPress={() => {
+                deleteArtwork(shouldDeleteArtist)
+              }}
+              haptic="impactHeavy"
+              accessibilityLabel="Delete Artwork"
+            >
+              Delete Artwork
+            </Button>
+            <Spacer y={2} />
+            <Button block variant="outline" onPress={hideModal} haptic accessibilityLabel="Cancel">
+              Cancel
+            </Button>
+          </Flex>
+        </Modal>
+      )
+    },
+    undefined,
+    undefined
+  )
