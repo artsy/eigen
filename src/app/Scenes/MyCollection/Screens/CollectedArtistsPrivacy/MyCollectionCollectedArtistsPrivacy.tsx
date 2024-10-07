7e468423ab02d6af2e137095781bb2714545fc98
brainbicycle <brian.beckerle@artsymail.com>
2024-10-07 15:09:39 -0400

    explicit fallback for MyCollectionCollectedArtistsPrivacy

--- src/app/Scenes/MyCollection/Screens/CollectedArtistsPrivacy/MyCollectionCollectedArtistsPrivacy.tsx
+++ src/app/Scenes/MyCollection/Screens/CollectedArtistsPrivacy/MyCollectionCollectedArtistsPrivacy.tsx
@@ -4,6 +4,7 @@ import {
   MyCollectionCollectedArtistsPrivacyQuery$data,
 } from "__generated__/MyCollectionCollectedArtistsPrivacyQuery.graphql"
 import { ArtistListItemPlaceholder } from "app/Components/ArtistListItem"
+import { LoadFailureView } from "app/Components/LoadFailureView"
 import { useToast } from "app/Components/Toast/toastHook"
 import {
   HeaderComponent,
@@ -15,7 +16,7 @@ import {
 } from "app/Scenes/MyCollection/Screens/CollectedArtistsPrivacy/UserInterestsStore"
 import { updateUserInterests } from "app/Scenes/MyCollection/Screens/CollectedArtistsPrivacy/updateUserInterests"
 import { popToRoot } from "app/system/navigation/navigate"
-import { withSuspense } from "app/utils/hooks/withSuspense"
+import { strictWithSuspense } from "app/utils/hooks/withSuspense"
 import { times } from "lodash"
 import { useState } from "react"
 import { SafeAreaView } from "react-native-safe-area-context"
@@ -28,10 +29,15 @@ interface MyCollectionCollectedArtistsPrivacyProps {
 export const MyCollectionCollectedArtistsPrivacy: React.FC<
   MyCollectionCollectedArtistsPrivacyProps
 > = ({ me }) => {
+  console.warn("MyCollectionArtistsPrivacy")
+  if (!me) {
+    return null
+  }
+
   return (
     <SafeAreaView edges={["bottom"]} style={{ flex: 1 }}>
       <Flex flexGrow={1} flex={1}>
-        <MyCollectionCollectedArtistsPrivacyArtistsList me={me!} />
+        <MyCollectionCollectedArtistsPrivacyArtistsList me={me} />
       </Flex>
 
       <SubmitButton />
@@ -89,19 +95,38 @@ const ShareSettingsScreenPlaceholder: React.FC<{}> = () => (
   </Flex>
 )
 
-export const MyCollectionCollectedArtistsPrivacyQueryRenderer: React.FC<{}> = withSuspense(() => {
-  const data = useLazyLoadQuery<MyCollectionCollectedArtistsPrivacyQuery>(
-    myCollectionCollectedArtistsPrivacyQuery,
-    {},
-    { fetchPolicy: "store-and-network" }
-  )
+export const MyCollectionCollectedArtistsPrivacyQueryRenderer: React.FC<{}> = strictWithSuspense(
+  () => {
+    const data = useLazyLoadQuery<MyCollectionCollectedArtistsPrivacyQuery>(
+      myCollectionCollectedArtistsPrivacyQuery,
+      {},
+      { fetchPolicy: "store-and-network" }
+    )
 
-  return (
-    <UserInterestsStoreProvider>
-      <MyCollectionCollectedArtistsPrivacy me={data.me!} />
-    </UserInterestsStoreProvider>
-  )
-}, ShareSettingsScreenPlaceholder)
+    console.warn("MyCollectionArtistsPrivacyQueryRenderer")
+
+    if (!data.me) {
+      return null
+    }
+
+    return (
+      <UserInterestsStoreProvider>
+        <MyCollectionCollectedArtistsPrivacy me={data.me} />
+      </UserInterestsStoreProvider>
+    )
+  },
+  ShareSettingsScreenPlaceholder,
+  (fallbackProps) => {
+    return (
+      <LoadFailureView
+        error={fallbackProps.error}
+        trackErrorBoundary={false}
+        showCloseButton={true}
+        useSafeArea={false}
+      />
+    )
+  }
+)
 
 const myCollectionCollectedArtistsPrivacyQuery = graphql`
   query MyCollectionCollectedArtistsPrivacyQuery {
