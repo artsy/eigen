b1fcf9da3be551193484320d44161b698c898ca4
Devon Blandin <dblandin@gmail.com>
2024-10-11 12:54:32 +0200

    feat: update spacing between home view section from 60 to 40px

--- src/app/Scenes/HomeView/Sections/HomeViewSectionArticles.tsx
+++ src/app/Scenes/HomeView/Sections/HomeViewSectionArticles.tsx
@@ -1,18 +1,10 @@
 import { ContextModule, ScreenOwnerType } from "@artsy/cohesion"
-import {
-  Flex,
-  FlexProps,
-  Join,
-  Skeleton,
-  SkeletonText,
-  Spacer,
-} from "@artsy/palette-mobile"
+import { Flex, FlexProps, Join, Skeleton, SkeletonText, Spacer } from "@artsy/palette-mobile"
 import { HomeViewSectionArticlesQuery } from "__generated__/HomeViewSectionArticlesQuery.graphql"
 import { HomeViewSectionArticles_section$key } from "__generated__/HomeViewSectionArticles_section.graphql"
 import { SkeletonArticleCard } from "app/Components/ArticleCard"
 import { ArticlesRailFragmentContainer } from "app/Scenes/Home/Components/ArticlesRail"
 import { HomeViewSectionSentinel } from "app/Scenes/HomeView/Components/HomeViewSectionSentinel"
-import { HOME_VIEW_SECTIONS_SEPARATOR_HEIGHT } from "app/Scenes/HomeView/HomeView"
 import { SectionSharedProps } from "app/Scenes/HomeView/Sections/Section"
 import { useHomeViewTracking } from "app/Scenes/HomeView/useHomeViewTracking"
 import { navigate } from "app/system/navigation/navigate"
@@ -107,19 +99,17 @@ const HomeViewSectionArticlesPlaceholder: React.FC<FlexProps> = (flexProps) => {
   const randomValue = useMemoizedRandom()
   return (
     <Skeleton>
-      <Flex {...flexProps}>
-        <Flex mx={2} my={HOME_VIEW_SECTIONS_SEPARATOR_HEIGHT}>
-          <SkeletonText variant="sm-display">Artsy Editorial</SkeletonText>
+      <Flex {...flexProps} mx={2}>
+        <SkeletonText variant="sm-display">Artsy Editorial</SkeletonText>
 
-          <Spacer y={2} />
+        <Spacer y={2} />
 
-          <Flex flexDirection="row">
-            <Join separator={<Spacer x="15px" />}>
-              {times(3 + randomValue * 10).map((index) => (
-                <SkeletonArticleCard key={index} />
-              ))}
-            </Join>
-          </Flex>
+        <Flex flexDirection="row">
+          <Join separator={<Spacer x="15px" />}>
+            {times(3 + randomValue * 10).map((index) => (
+              <SkeletonArticleCard key={index} />
+            ))}
+          </Join>
         </Flex>
       </Flex>
     </Skeleton>
