78865891c055ea5e011e2cab1c36a5f71b542b02
Sultan <sultan.al-maari@artsymail.com>
2024-10-21 16:40:43 +0200

    add log to build a new version

--- android/app/src/main/java/net/artsy/app/MainActivity.kt
+++ android/app/src/main/java/net/artsy/app/MainActivity.kt
@@ -109,6 +109,7 @@ class MainActivity : ReactActivity() {
     }
 
     private fun checkForAppUpdate() {
+        Log.d(TAG, "here we go!")
         Log.d(TAG, "checkForAppUpdate: started checking for update!")
         val appUpdateInfoTask = appUpdateManager.appUpdateInfo
 
