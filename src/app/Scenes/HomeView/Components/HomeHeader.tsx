86485d7f6d4828f62b078d6fac30c3228d79239b
rquartararo <rquartararo@gmail.com>
2024-10-22 19:05:17 -0400

    feat: add payment banner banner component

--- src/app/Scenes/HomeView/Components/HomeHeader.tsx
+++ src/app/Scenes/HomeView/Components/HomeHeader.tsx
@@ -1,4 +1,5 @@
 import { ArtsyLogoBlackIcon, Flex, Box, useSpace } from "@artsy/palette-mobile"
+import { PaymentFailureBanner } from "app/Scenes/HomeView/Components/PaymentFailureBanner"
 import { GlobalStore } from "app/store/GlobalStore"
 import { ActivityIndicator } from "./ActivityIndicator"
 
@@ -10,16 +11,19 @@ export const HomeHeader: React.FC = () => {
   const space = useSpace()
 
   return (
-    <Box style={{ paddingTop: space(2), paddingBottom: space(2) }}>
-      <Flex flexDirection="row" px={2} justifyContent="space-between" alignItems="center">
-        <Box flex={1} />
-        <Box>
-          <ArtsyLogoBlackIcon scale={0.75} />
-        </Box>
-        <Box flex={1} alignItems="flex-end">
-          <ActivityIndicator hasUnseenNotifications={hasUnseenNotifications} />
-        </Box>
-      </Flex>
-    </Box>
+    <>
+      <PaymentFailureBanner />
+      <Box style={{ paddingTop: space(2), paddingBottom: space(2) }}>
+        <Flex flexDirection="row" px={2} justifyContent="space-between" alignItems="center">
+          <Box flex={1} />
+          <Box>
+            <ArtsyLogoBlackIcon scale={0.75} />
+          </Box>
+          <Box flex={1} alignItems="flex-end">
+            <ActivityIndicator hasUnseenNotifications={hasUnseenNotifications} />
+          </Box>
+        </Flex>
+      </Box>
+    </>
   )
 }
