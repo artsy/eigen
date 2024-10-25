3df7e58a043c6b361f0aeee6fe47ef4ad69d6190
Mounir Dhahri <soussouheros@gmail.com>
2024-10-25 13:19:36 +0200

    feat: better bottom tabs

--- src/app/utils/bottomTabsHelper.tsx
+++ src/app/utils/bottomTabsHelper.tsx
@@ -1,5 +1,7 @@
 import EventEmitter from "events"
+import { useScrollToTop } from "@react-navigation/native"
 import { BottomTabType } from "app/Scenes/BottomTabs/BottomTabType"
+import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
 import { useEffect, useRef } from "react"
 import { FlatList, ScrollView } from "react-native"
 
@@ -13,7 +15,11 @@ export const scrollTabToTop = (tab: BottomTabType) => {
 }
 
 export const useBottomTabsScrollToTop = (tab: BottomTabType, onScrollToTop?: () => void) => {
+  const enableNewNavigation = useFeatureFlag("AREnableNewNavigation")
+
   const ref = useRef(null)
+
+  useScrollToTop(ref)
 
   const handleScrollToTopEvent = () => {
     const flatListRef = ref as React.RefObject<FlatList> | null
@@ -26,6 +32,10 @@ export const useBottomTabsScrollToTop = (tab: BottomTabType, onScrollToTop?: () 
   }
 
   useEffect(() => {
+    if (enableNewNavigation) {
+      return
+    }
+
     BottomTabsEvents.addListener(`${SCROLL_TO_TOP_EVENT}-${tab}`, handleScrollToTopEvent)
 
     return () => {
