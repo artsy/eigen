d56f04d052a5934396e8657cdc029dadb29deb1b
Ole <ole.richter91@gmail.com>
2024-10-28 12:45:42 +0100

    chore: Add screen tracking to Saves screen

--- src/app/Scenes/SavedArtworks/SavedArtworks.tsx
+++ src/app/Scenes/SavedArtworks/SavedArtworks.tsx
@@ -1,24 +1,31 @@
+import { OwnerType } from "@artsy/cohesion"
 import { BackButton, Flex, Screen, useSpace } from "@artsy/palette-mobile"
 import { ArtworkLists } from "app/Scenes/ArtworkLists/ArtworkLists"
 import { goBack } from "app/system/navigation/navigate"
+import { ProvideScreenTrackingWithCohesionSchema } from "app/utils/track"
+import { screen } from "app/utils/track/helpers"
 
 export const SavedArtworks = () => {
   const space = useSpace()
 
   return (
-    <Screen>
-      <Screen.AnimatedHeader
-        leftElements={
-          <Flex flexDirection="row" alignItems="center" gap={space(1)}>
-            <BackButton onPress={goBack} />
-          </Flex>
-        }
-        title="Saves"
-      />
-      <Screen.StickySubHeader title="Saves" />
-      <Screen.Body fullwidth>
-        <ArtworkLists isTab={false} />
-      </Screen.Body>
-    </Screen>
+    <ProvideScreenTrackingWithCohesionSchema
+      info={screen({ context_screen_owner_type: OwnerType.saves })}
+    >
+      <Screen>
+        <Screen.AnimatedHeader
+          leftElements={
+            <Flex flexDirection="row" alignItems="center" gap={space(1)}>
+              <BackButton onPress={goBack} />
+            </Flex>
+          }
+          title="Saves"
+        />
+        <Screen.StickySubHeader title="Saves" />
+        <Screen.Body fullwidth>
+          <ArtworkLists isTab={false} />
+        </Screen.Body>
+      </Screen>
+    </ProvideScreenTrackingWithCohesionSchema>
   )
 }
