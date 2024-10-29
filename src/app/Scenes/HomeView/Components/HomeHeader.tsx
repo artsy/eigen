49210fed01185093d545eaa4d313e170927d9d34
Mounir Dhahri <soussouheros@gmail.com>
2024-10-29 17:01:42 +0100

    wip

--- src/app/Scenes/HomeView/Components/HomeHeader.tsx
+++ src/app/Scenes/HomeView/Components/HomeHeader.tsx
@@ -1,4 +1,5 @@
-import { ArtsyLogoBlackIcon, Flex, Box } from "@artsy/palette-mobile"
+import { ArtsyLogoBlackIcon, Flex, Box, Button, Spacer, Text } from "@artsy/palette-mobile"
+import SearchIcon from "app/Components/Icons/SearchIcon"
 import { PaymentFailureBanner } from "app/Scenes/HomeView/Components/PaymentFailureBanner"
 import { GlobalStore } from "app/store/GlobalStore"
 import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
@@ -6,10 +7,37 @@ import { Suspense } from "react"
 import { ActivityIndicator } from "./ActivityIndicator"
 
 export const HomeHeader: React.FC = () => {
+  const enableNewSearchModal = useFeatureFlag("AREnableNewSearchModal")
   const showPaymentFailureBanner = useFeatureFlag("AREnablePaymentFailureBanner")
   const hasUnseenNotifications = GlobalStore.useAppState(
     (state) => state.bottomTabs.hasUnseenNotifications
   )
+
+  if (enableNewSearchModal) {
+    return (
+      <>
+        {!!showPaymentFailureBanner && (
+          <Suspense fallback={null}>
+            <PaymentFailureBanner />
+          </Suspense>
+        )}
+        <Flex py={2}>
+          <Flex flexDirection="row" px={2} justifyContent="space-around" alignItems="center">
+            <Flex flex={1}>
+              <Button block variant="fillGray" alignItems="flex-start" disabled>
+                <SearchIcon />
+                <Spacer x={2} />
+                <Text variant="sm-display">Search Artsy</Text>
+              </Button>
+            </Flex>
+            <Flex width={50} alignItems="flex-end">
+              <ActivityIndicator hasUnseenNotifications={hasUnseenNotifications} />
+            </Flex>
+          </Flex>
+        </Flex>
+      </>
+    )
+  }
 
   return (
     <>
