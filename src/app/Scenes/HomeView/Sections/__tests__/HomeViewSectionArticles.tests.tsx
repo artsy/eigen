bbee3a5b8bcae4792572e6d543ad3027364f249e
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-11-06 15:44:43 -0500

    test: improve coverage for HomeViewSectionArticles

--- src/app/Scenes/HomeView/Sections/__tests__/HomeViewSectionArticles.tests.tsx
+++ src/app/Scenes/HomeView/Sections/__tests__/HomeViewSectionArticles.tests.tsx
@@ -31,10 +31,60 @@ describe("HomeViewSectionArticles", () => {
     `,
   })
 
+  it("does not render the section if no articles are available", () => {
+    renderWithRelay({
+      HomeViewSectionArticles: () => ({
+        internalID: "home-view-section-latest-articles",
+        component: {
+          title: "Latest Articles",
+        },
+        articlesConnection: null,
+      }),
+    })
+
+    expect(screen.queryByText("Latest Articles")).not.toBeOnTheScreen()
+    expect(screen.toJSON()).toBeNull()
+  })
+
   it("renders a list of articles", () => {
     renderWithRelay({
       HomeViewSectionArticles: () => ({
         internalID: "home-view-section-latest-articles",
+        contextModule: "articleRail",
+        component: {
+          title: "Latest Articles",
+        },
+        articlesConnection: {
+          edges: [
+            {
+              node: {
+                thumbnailTitle: "Article 1",
+                slug: "article-1",
+                internalID: "article-1-id",
+              },
+            },
+            {
+              node: {
+                thumbnailTitle: "Article 2",
+                slug: "article-2",
+                internalID: "article-2-id",
+              },
+            },
+          ],
+        },
+      }),
+    })
+
+    expect(screen.getByText("Latest Articles")).toBeOnTheScreen()
+    expect(screen.getByText("Article 1")).toBeOnTheScreen()
+    expect(screen.getByText("Article 2")).toBeOnTheScreen()
+  })
+
+  it("tracks article navigation", () => {
+    renderWithRelay({
+      HomeViewSectionArticles: () => ({
+        internalID: "home-view-section-latest-articles",
+        contextModule: "articleRail",
         component: {
           title: "Latest Articles",
         },
@@ -64,20 +114,61 @@ describe("HomeViewSectionArticles", () => {
     expect(screen.getByText("Article 2")).toBeOnTheScreen()
 
     fireEvent.press(screen.getByText("Article 2"))
-    expect(mockTrackEvent.mock.calls[0]).toMatchInlineSnapshot(`
-        [
-          {
-            "action": "tappedArticleGroup",
-            "context_module": "<mock-value-for-field-"contextModule">",
-            "context_screen_owner_type": "home",
-            "destination_screen_owner_id": "article-2-id",
-            "destination_screen_owner_slug": "article-2",
-            "destination_screen_owner_type": "article",
-            "horizontal_slide_position": 1,
-            "module_height": "double",
-            "type": "thumbnail",
+
+    expect(mockTrackEvent).toHaveBeenCalledWith({
+      action: "tappedArticleGroup",
+      context_module: "articleRail",
+      context_screen_owner_type: "home",
+      destination_screen_owner_id: "article-2-id",
+      destination_screen_owner_slug: "article-2",
+      destination_screen_owner_type: "article",
+      horizontal_slide_position: 1,
+      module_height: "double",
+      type: "thumbnail",
+    })
+  })
+
+  it("tracks view-all navigation", () => {
+    renderWithRelay({
+      HomeViewSectionArticles: () => ({
+        internalID: "home-view-section-latest-articles",
+        contextModule: "articleRail",
+        component: {
+          title: "Latest Articles",
+          behaviors: {
+            viewAll: {
+              ownerType: "articles",
+              href: "/articles",
+            },
           },
-        ]
-      `)
+        },
+        articlesConnection: {
+          edges: [
+            {
+              node: {
+                thumbnailTitle: "Article 1",
+                internalID: "article-1-id",
+              },
+            },
+            {
+              node: {
+                thumbnailTitle: "Article 2",
+                internalID: "article-2-id",
+              },
+            },
+          ],
+        },
+      }),
+    })
+
+    fireEvent.press(screen.getByText("Latest Articles"))
+
+    expect(mockTrackEvent).toHaveBeenCalledWith({
+      action: "tappedArticleGroup",
+      context_module: "articleRail",
+      context_screen_owner_type: "home",
+      destination_screen_owner_type: "articles",
+      type: "viewAll",
+    })
   })
 })
