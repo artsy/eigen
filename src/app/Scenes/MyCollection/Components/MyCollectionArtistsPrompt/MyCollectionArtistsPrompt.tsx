54aa466b6b58b97b935c0648c76a740e9da92441
George Kartalis <g.kartalis7@gmail.com>
2024-11-06 17:19:34 +0100

    fix moti broken animation

--- src/app/Scenes/MyCollection/Components/MyCollectionArtistsPrompt/MyCollectionArtistsPrompt.tsx
+++ src/app/Scenes/MyCollection/Components/MyCollectionArtistsPrompt/MyCollectionArtistsPrompt.tsx
@@ -1,8 +1,11 @@
 import { Flex, Text } from "@artsy/palette-mobile"
+import { useBottomSheet } from "@gorhom/bottom-sheet"
 import { MyCollectionArtistsPromptBody } from "app/Scenes/MyCollection/Components/MyCollectionArtistsPrompt/MyCollectionArtistsPromptBody"
+import { SNAP_POINTS } from "app/Scenes/MyCollection/Components/MyCollectionBottomSheetModals/MyCollectionBottomSheetModalArtistsPrompt"
 import { MyCollectionAddCollectedArtistsStore } from "app/Scenes/MyCollection/Screens/MyCollectionAddCollectedArtists/MyCollectionAddCollectedArtistsStore"
 import { MotiView } from "moti"
 import { FC } from "react"
+import { useAnimatedStyle, withTiming } from "react-native-reanimated"
 
 export interface MyCollectionArtistsPromptProps {
   title: string
@@ -13,19 +16,18 @@ export const MyCollectionArtistsPrompt: FC<MyCollectionArtistsPromptProps> = ({
   title,
   subtitle,
 }) => {
-  // const { animatedIndex } = useBottomSheet()
+  const { animatedIndex } = useBottomSheet()
+
+  const style = useAnimatedStyle(() => {
+    return {
+      height: withTiming(animatedIndex.value === 1 ? SNAP_POINTS[1] : SNAP_POINTS[0], {
+        duration: 200,
+      }),
+    }
+  })
 
   return (
-    <MotiView
-      // TODO: fix this to not violate animate rules
-      // animate={useDerivedValue(() => ({
-      //   height: animatedIndex.value === 1 ? SNAP_POINTS[1] : SNAP_POINTS[0],
-      // }))}
-      transition={{
-        type: "timing",
-        duration: 200,
-      }}
-    >
+    <MotiView style={style}>
       <Flex px={2} pt={1} pb={2} flex={1}>
         <Text variant="md">{title}</Text>
 
