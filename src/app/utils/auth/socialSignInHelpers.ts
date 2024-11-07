4cfb728046ad7e9dbf83198c1d8d2fb151b3e2f7
Adam Iskounen <44589599+iskounen@users.noreply.github.com>
2024-11-07 12:13:05 -0500

    feat(DIA-973): add social account linking to auth2 (#11076)

--- src/app/utils/auth/socialSignInHelpers.ts
+++ src/app/utils/auth/socialSignInHelpers.ts
@@ -1,4 +1,5 @@
 import { statusCodes } from "@react-native-google-signin/google-signin"
+import { NavigationProp, useNavigation } from "@react-navigation/native"
 import { captureMessage } from "@sentry/react-native"
 import { OnboardingNavigationStack } from "app/Scenes/Onboarding/Onboarding"
 import { AppleToken, GoogleOrFacebookToken } from "app/Scenes/Onboarding/OnboardingSocialLink"
@@ -8,123 +9,125 @@ import { showBlockedAuthError } from "app/utils/auth/authHelpers"
 import { capitalize } from "lodash"
 import { Alert, InteractionManager } from "react-native"
 
-export const handleSocialLogin = async (callback: () => Promise<AuthPromiseResolveType>) => {
-  GlobalStore.actions.auth.setSessionState({ isLoading: true })
-  InteractionManager.runAfterInteractions(() => {
-    callback().catch((error: AuthPromiseRejectType) => {
-      InteractionManager.runAfterInteractions(() => {
-        GlobalStore.actions.auth.setSessionState({ isLoading: false })
+export const useSocialLogin = () => {
+  const navigation = useNavigation<NavigationProp<OnboardingNavigationStack>>()
+
+  const handleSocialLogin = async (callback: () => Promise<AuthPromiseResolveType>) => {
+    GlobalStore.actions.auth.setSessionState({ isLoading: true })
+    InteractionManager.runAfterInteractions(() => {
+      callback().catch((error: AuthPromiseRejectType) => {
         InteractionManager.runAfterInteractions(() => {
-          handleError(error)
+          GlobalStore.actions.auth.setSessionState({ isLoading: false })
+          InteractionManager.runAfterInteractions(() => {
+            handleError(error)
+          })
         })
       })
     })
-  })
-}
+  }
 
-const handleError = (error: AuthPromiseRejectType) => {
-  if (error.message === statusCodes.SIGN_IN_CANCELLED) {
-    return
-  } else {
-    captureMessage("AUTH_FAILURE: " + error.message)
+  const handleError = (error: AuthPromiseRejectType) => {
+    if (error.message === statusCodes.SIGN_IN_CANCELLED) {
+      return
+    } else {
+      captureMessage("AUTH_FAILURE: " + error.message)
 
-    const canBeLinked =
-      error.error === "User Already Exists" && error.meta && error.meta.existingProviders
-    if (canBeLinked) {
-      handleErrorWithAlternativeProviders(error.meta)
+      const canBeLinked =
+        error.error === "User Already Exists" && error.meta && error.meta.existingProviders
+      if (canBeLinked) {
+        handleErrorWithAlternativeProviders(error.meta)
+        return
+      }
+      GlobalStore.actions.auth.setSessionState({ isLoading: false })
+
+      InteractionManager.runAfterInteractions(() => {
+        const errorMode = error.message === "Attempt blocked" ? "attempt blocked" : "no account"
+        showErrorAlert(errorMode, error)
+        return
+      })
+    }
+  }
+
+  const handleErrorWithAlternativeProviders = (meta: AuthPromiseRejectType["meta"]) => {
+    if (!meta) {
       return
     }
-    GlobalStore.actions.auth.setSessionState({ isLoading: false })
 
-    InteractionManager.runAfterInteractions(() => {
-      const errorMode = error.message === "Attempt blocked" ? "attempt blocked" : "no account"
-      showErrorAlert(errorMode, error)
-      return
-    })
-  }
-}
+    const titleizedProvider = capitalize(meta.provider)
 
-const handleErrorWithAlternativeProviders = (meta: AuthPromiseRejectType["meta"]) => {
-  if (!meta) {
-    return
-  }
+    const {
+      email,
+      name,
+      existingProviders: providers,
+      provider: providerToBeLinked,
+      oauthToken,
+      idToken,
+      appleUid,
+    } = meta
 
-  const titleizedProvider = capitalize(meta.provider)
-
-  const {
-    email,
-    name,
-    existingProviders: providers,
-    provider: providerToBeLinked,
-    oauthToken,
-    idToken,
-    appleUid,
-  } = meta
-
-  const navParams: Omit<
-    OnboardingNavigationStack["OnboardingSocialLink"],
-    "tokenForProviderToBeLinked"
-  > = {
-    email,
-    name: name ?? "",
-    providers: providers ?? [],
-    providerToBeLinked,
-  }
-
-  let tokenForProviderToBeLinked: GoogleOrFacebookToken | AppleToken
-
-  if (["google", "facebook"].includes(providerToBeLinked)) {
-    if (!oauthToken) {
-      console.warn(`Error: No oauthToken provided for ${titleizedProvider}`)
-      return
+    const navParams: Omit<
+      OnboardingNavigationStack["OnboardingSocialLink"],
+      "tokenForProviderToBeLinked"
+    > = {
+      email,
+      name: name ?? "",
+      providers: providers ?? [],
+      providerToBeLinked,
     }
-    tokenForProviderToBeLinked = oauthToken
 
-    console.log({ navParams, tokenForProviderToBeLinked })
+    let tokenForProviderToBeLinked: GoogleOrFacebookToken | AppleToken
 
-    // TODO:
-    // navigation.navigate("OnboardingSocialLink", {
-    //   ...navParams,
-    //   tokenForProviderToBeLinked,
-    // })
-  } else if (providerToBeLinked === "apple") {
-    if (!idToken || !appleUid) {
-      console.warn(`Error: idToken and appleUid must be provided for ${titleizedProvider}`)
-      return
+    if (["google", "facebook"].includes(providerToBeLinked)) {
+      if (!oauthToken) {
+        console.warn(`Error: No oauthToken provided for ${titleizedProvider}`)
+        return
+      }
+      tokenForProviderToBeLinked = oauthToken
+
+      navigation.navigate("OnboardingSocialLink", {
+        ...navParams,
+        tokenForProviderToBeLinked,
+      })
+    } else if (providerToBeLinked === "apple") {
+      if (!idToken || !appleUid) {
+        console.warn(`Error: idToken and appleUid must be provided for ${titleizedProvider}`)
+        return
+      }
+      tokenForProviderToBeLinked = { idToken, appleUid }
+
+      navigation.navigate("OnboardingSocialLink", {
+        ...navParams,
+        tokenForProviderToBeLinked,
+      })
     }
-    tokenForProviderToBeLinked = { idToken, appleUid }
-
-    console.log({ navParams, tokenForProviderToBeLinked })
-
-    // TODO:
-    // navigation.navigate("OnboardingSocialLink", {
-    //   ...navParams,
-    //   tokenForProviderToBeLinked,
-    // })
   }
-}
 
-const showErrorAlert = (
-  errorMode: "no account" | "attempt blocked",
-  error: AuthPromiseRejectType
-) => {
-  if (errorMode === "no account") {
-    Alert.alert("No Artsy account found", error.message, [
-      {
-        text: "Cancel",
-        style: "cancel",
-      },
-      {
-        text: "OK",
-        onPress: () => {
-          // @ts-expect-error
-          navigation.replace(mode === "login" ? "OnboardingCreateAccount" : "OnboardingLogin", {
-            withFadeAnimation: true,
-          })
+  const showErrorAlert = (
+    errorMode: "no account" | "attempt blocked",
+    error: AuthPromiseRejectType
+  ) => {
+    if (errorMode === "no account") {
+      Alert.alert("No Artsy account found", error.message, [
+        {
+          text: "Cancel",
+          style: "cancel",
         },
-      },
-    ])
-  } else {
-    showBlockedAuthError("social sign in")
+        {
+          text: "OK",
+          onPress: () => {
+            // TODO: How should this be handled in auth2?
+            // navigation.replace(mode === "login" ? "OnboardingCreateAccount" : "OnboardingLogin", {
+            //   withFadeAnimation: true,
+            // })
+          },
+        },
+      ])
+    } else {
+      showBlockedAuthError("social sign in")
+    }
+  }
+
+  return {
+    handleSocialLogin,
   }
 }
