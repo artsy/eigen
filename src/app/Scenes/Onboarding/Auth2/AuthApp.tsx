2d84683207b29ad331ea534ad8a3f2a9ed2a1140
Adam Iskounen <adam.iskounen@artsymail.com>
2024-11-08 11:20:26 -0500

    fix: auth2 QA omnibus

--- src/app/Scenes/Onboarding/Auth2/AuthApp.tsx
+++ src/app/Scenes/Onboarding/Auth2/AuthApp.tsx
@@ -3,11 +3,11 @@ import { AuthContext } from "app/Scenes/Onboarding/Auth2/AuthContext"
 import { AuthScenes } from "app/Scenes/Onboarding/Auth2/AuthScenes"
 import { AuthBackground } from "app/Scenes/Onboarding/Auth2/components/AuthBackground"
 import { AuthModal } from "app/Scenes/Onboarding/Auth2/components/AuthModal"
-import { useAndroidStatusBarColor } from "app/Scenes/Onboarding/Auth2/hooks/useAndroidStatusBarColor"
+import { useSwitchStatusBarStyle } from "app/utils/useStatusBarStyle"
 import React from "react"
 
 export const AuthApp: React.FC = () => {
-  useAndroidStatusBarColor()
+  useSwitchStatusBarStyle("light-content", "dark-content")
 
   return (
     <Screen safeArea={false}>
