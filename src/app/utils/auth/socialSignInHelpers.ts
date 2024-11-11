82ae24e345833b1ab0e2c27cc435446e24afc348
Adam Iskounen <adam.iskounen@artsymail.com>
2024-11-11 07:15:05 -0500

    Finished catch-all error message

--- src/app/utils/auth/socialSignInHelpers.ts
+++ src/app/utils/auth/socialSignInHelpers.ts
@@ -109,17 +109,7 @@ export const useSocialLogin = () => {
     if (errorMode === "no account") {
       Alert.alert("No Artsy account found", error.message, [
         {
-          text: "Cancel",
-          style: "cancel",
-        },
-        {
           text: "OK",
-          onPress: () => {
-            // TODO: How should this be handled in auth2?
-            // navigation.replace(mode === "login" ? "OnboardingCreateAccount" : "OnboardingLogin", {
-            //   withFadeAnimation: true,
-            // })
-          },
         },
       ])
     } else {
