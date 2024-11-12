e3f211f4f5f12365fa571b73b86c38aeba8633f8
Adam Iskounen <adam.iskounen@artsymail.com>
2024-11-12 09:51:54 -0500

    Listed the things I want to test.

---
+++ src/app/Scenes/Onboarding/Auth2/scenes/__tests__/LoginWelcomeStep.tests.tsx
@@ -0,0 +1,11 @@
+describe("LoginWelcomeStep", () => {
+  it("expands the modal when the input field is tapped", () => {})
+  describe("when the modal is expanded", () => {
+    it("closes the modal when the back icon is tapped", () => {})
+    it("navigates to the sign-up password step if a user account doesn't exist", () => {})
+    it("navigates to the login password step if a user account exists", () => {})
+  })
+  it("initiates Sign in with Apple flow when the Apple icon is tapped", () => {})
+  it("initiates Google Sign-In flow when the Google icon is tapped", () => {})
+  it("initiates Facebook Login flow when the Facebook icon is tapped", () => {})
+})
