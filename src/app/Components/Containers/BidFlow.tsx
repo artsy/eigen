c357713a0a79e49a83b1883d3967f80b7cbc0da5
Mounir Dhahri <soussouheros@gmail.com>
2024-11-15 19:07:33 +0100

    fix: broken padding inside max bid

--- src/app/Components/Containers/BidFlow.tsx
+++ src/app/Components/Containers/BidFlow.tsx
@@ -1,6 +1,7 @@
 import { TimeOffsetProvider } from "app/Components/Bidding/Context/TimeOffsetProvider"
 import { SelectMaxBidQueryRenderer } from "app/Components/Bidding/Screens/SelectMaxBid"
 import NavigatorIOS from "app/utils/__legacy_do_not_use__navigator-ios-shim"
+import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
 import { Platform, View } from "react-native"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 
@@ -8,9 +9,15 @@ export const BidFlow: React.FC<
   Omit<React.ComponentProps<typeof SelectMaxBidQueryRenderer>, "navigator">
 > = (props) => {
   const insets = useSafeAreaInsets()
+  const enableNewNavigation = useFeatureFlag("AREnableNewNavigation")
 
   return (
-    <View style={{ flex: 1, paddingTop: Platform.OS === "android" ? insets.top : 0 }}>
+    <View
+      style={{
+        flex: 1,
+        paddingTop: Platform.OS === "android" && !enableNewNavigation ? insets.top : 0,
+      }}
+    >
       <TimeOffsetProvider>
         <NavigatorIOS initialRoute={{ component: SelectMaxBidQueryRenderer, passProps: props }} />
       </TimeOffsetProvider>
