4fb7e84d8a8d7ec1504cba8b4d427400a79fe65d
Adam Iskounen <adam.iskounen@artsymail.com>
2024-11-21 08:01:27 -0500

    removed flag from onboarding social pick

--- src/app/Scenes/Onboarding/OnboardingSocialPick.tests.tsx
+++ src/app/Scenes/Onboarding/OnboardingSocialPick.tests.tsx
@@ -1,6 +1,5 @@
 import { screen } from "@testing-library/react-native"
 import { OnboardingSocialPick } from "app/Scenes/Onboarding/OnboardingSocialPick"
-import { __globalStoreTestUtils__ } from "app/store/GlobalStore"
 import { getMockRelayEnvironment } from "app/system/relay/defaultEnvironment"
 import { renderWithWrappers } from "app/utils/tests/renderWithWrappers"
 import { Platform } from "react-native"
@@ -27,32 +26,12 @@ describe("OnboardingSocialPick", () => {
         mode = "login"
       })
 
-      describe("when the new disclaimer is disabled", () => {
-        beforeEach(() => {
-          __globalStoreTestUtils__?.injectFeatureFlags({ AREnableNewTermsAndConditions: false })
-        })
+      it("renders the new disclaimer text", () => {
+        renderWithWrappers(<TestRenderer />)
 
-        it("renders the disclaimer text", () => {
-          renderWithWrappers(<TestRenderer />)
-
-          expect(screen.getByTestId("disclaimer")).toHaveTextContent(
-            "By tapping Continue with Facebook, Google or Apple, you agree to Artsy's Terms of Use and Privacy Policy"
-          )
-        })
-      })
-
-      describe("when the new disclaimer is enabled", () => {
-        beforeEach(() => {
-          __globalStoreTestUtils__?.injectFeatureFlags({ AREnableNewTermsAndConditions: true })
-        })
-
-        it("renders the new disclaimer text", () => {
-          renderWithWrappers(<TestRenderer />)
-
-          expect(screen.getByTestId("disclaimer")).toHaveTextContent(
-            "By tapping Continue with Email, Facebook, Google or Apple, you agree to Artsy's Terms and Conditions and Privacy Policy"
-          )
-        })
+        expect(screen.getByTestId("disclaimer")).toHaveTextContent(
+          "By tapping Continue with Email, Facebook, Google or Apple, you agree to Artsy's Terms and Conditions and Privacy Policy"
+        )
       })
     })
 
@@ -61,32 +40,12 @@ describe("OnboardingSocialPick", () => {
         mode = "signup"
       })
 
-      describe("when the new disclaimer is disabled", () => {
-        beforeEach(() => {
-          __globalStoreTestUtils__?.injectFeatureFlags({ AREnableNewTermsAndConditions: false })
-        })
+      it("renders the new disclaimer text", () => {
+        renderWithWrappers(<TestRenderer />)
 
-        it("renders the disclaimer text", () => {
-          renderWithWrappers(<TestRenderer />)
-
-          expect(screen.getByTestId("disclaimer")).toHaveTextContent(
-            "By tapping Continue with Facebook, Google or Apple, you agree to Artsy's Terms of Use and Privacy Policy"
-          )
-        })
-      })
-
-      describe("when the new disclaimer is enabled", () => {
-        beforeEach(() => {
-          __globalStoreTestUtils__?.injectFeatureFlags({ AREnableNewTermsAndConditions: true })
-        })
-
-        it("renders the new disclaimer text", () => {
-          renderWithWrappers(<TestRenderer />)
-
-          expect(screen.getByTestId("disclaimer")).toHaveTextContent(
-            "By tapping Continue with Email, Facebook, Google or Apple, you agree to Artsy's Terms and Conditions and Privacy Policy"
-          )
-        })
+        expect(screen.getByTestId("disclaimer")).toHaveTextContent(
+          "By tapping Continue with Email, Facebook, Google or Apple, you agree to Artsy's Terms and Conditions and Privacy Policy"
+        )
       })
     })
   })
@@ -101,32 +60,12 @@ describe("OnboardingSocialPick", () => {
         mode = "login"
       })
 
-      describe("when the new disclaimer is disabled", () => {
-        beforeEach(() => {
-          __globalStoreTestUtils__?.injectFeatureFlags({ AREnableNewTermsAndConditions: false })
-        })
+      it("renders the new disclaimer text", () => {
+        renderWithWrappers(<TestRenderer />)
 
-        it("renders the disclaimer text", () => {
-          renderWithWrappers(<TestRenderer />)
-
-          expect(screen.getByTestId("disclaimer")).toHaveTextContent(
-            "By tapping Continue with Facebook, Google, you agree to Artsy's Terms of Use and Privacy Policy"
-          )
-        })
-      })
-
-      describe("when the new disclaimer is enabled", () => {
-        beforeEach(() => {
-          __globalStoreTestUtils__?.injectFeatureFlags({ AREnableNewTermsAndConditions: true })
-        })
-
-        it("renders the new disclaimer text", () => {
-          renderWithWrappers(<TestRenderer />)
-
-          expect(screen.getByTestId("disclaimer")).toHaveTextContent(
-            "By tapping Continue with Email, Facebook, Google, you agree to Artsy's Terms and Conditions and Privacy Policy"
-          )
-        })
+        expect(screen.getByTestId("disclaimer")).toHaveTextContent(
+          "By tapping Continue with Email, Facebook, Google, you agree to Artsy's Terms and Conditions and Privacy Policy"
+        )
       })
     })
 
@@ -135,32 +74,12 @@ describe("OnboardingSocialPick", () => {
         mode = "signup"
       })
 
-      describe("when the new disclaimer is disabled", () => {
-        beforeEach(() => {
-          __globalStoreTestUtils__?.injectFeatureFlags({ AREnableNewTermsAndConditions: false })
-        })
+      it("renders the new disclaimer text", () => {
+        renderWithWrappers(<TestRenderer />)
 
-        it("renders the disclaimer text", () => {
-          renderWithWrappers(<TestRenderer />)
-
-          expect(screen.getByTestId("disclaimer")).toHaveTextContent(
-            "By tapping Continue with Facebook, Google, you agree to Artsy's Terms of Use and Privacy Policy"
-          )
-        })
-      })
-
-      describe("when the new disclaimer is enabled", () => {
-        beforeEach(() => {
-          __globalStoreTestUtils__?.injectFeatureFlags({ AREnableNewTermsAndConditions: true })
-        })
-
-        it("renders the new disclaimer text", () => {
-          renderWithWrappers(<TestRenderer />)
-
-          expect(screen.getByTestId("disclaimer")).toHaveTextContent(
-            "By tapping Continue with Email, Facebook, Google, you agree to Artsy's Terms and Conditions and Privacy Policy"
-          )
-        })
+        expect(screen.getByTestId("disclaimer")).toHaveTextContent(
+          "By tapping Continue with Email, Facebook, Google, you agree to Artsy's Terms and Conditions and Privacy Policy"
+        )
       })
     })
   })
