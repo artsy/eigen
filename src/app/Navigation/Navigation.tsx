6951d8e9d81a5c273259d1ca8e52498b21bebc84
Mounir Dhahri <soussouheros@gmail.com>
2024-11-22 11:35:24 +0100

    chore: remove unused routes

--- src/app/Navigation/Navigation.tsx
+++ src/app/Navigation/Navigation.tsx
@@ -8,7 +8,6 @@ import {
   AuthenticatedRoutes,
   AuthenticatedRoutesParams,
 } from "app/Navigation/AuthenticatedRoutes/Tabs"
-import { UnauthenticatedRoutesParams } from "app/Navigation/UnauthenticatedRoutes"
 import { OnboardingWelcomeScreens } from "app/Scenes/Onboarding/Onboarding"
 import { GlobalStore } from "app/store/GlobalStore"
 import { routingInstrumentation } from "app/system/errorReporting/setupSentry"
@@ -22,7 +21,7 @@ import { Fragment } from "react"
 import { Platform } from "react-native"
 import SiftReactNative from "sift-react-native"
 
-export type NavigationRoutesParams = UnauthenticatedRoutesParams & AuthenticatedRoutesParams
+export type NavigationRoutesParams = AuthenticatedRoutesParams
 
 export const MainStackNavigator = createNativeStackNavigator<NavigationRoutesParams>()
 
