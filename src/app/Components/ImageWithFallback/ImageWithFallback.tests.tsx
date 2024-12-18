011b976b04402edff4e0cf434b788d695972601d
brainbicycle <brian.beckerle@artsymail.com>
2024-12-18 09:53:52 -0500

    preferred pattern for image tests

--- src/app/Components/ImageWithFallback/ImageWithFallback.tests.tsx
+++ src/app/Components/ImageWithFallback/ImageWithFallback.tests.tsx
@@ -1,3 +1,4 @@
+import { screen } from "@testing-library/react-native"
 import { ImageWithFallback } from "app/Components/ImageWithFallback/ImageWithFallback"
 import { renderWithWrappers } from "app/utils/tests/renderWithWrappers"
 
@@ -7,17 +8,17 @@ describe("ImageWithFallback", () => {
   })
 
   it("renders the image when a url is passed", async () => {
-    const view = renderWithWrappers(
+    renderWithWrappers(
       <ImageWithFallback src="https://example.com/image.jpg" width={100} height={100} />
     )
-    const image = await view.findByTestId("image")
+    const image = await screen.findByTestId("image")
     expect(image).toBeTruthy()
   })
 
   it("renders the fallback when a url is not passed", async () => {
-    const view = renderWithWrappers(<ImageWithFallback width={100} height={100} />)
+    renderWithWrappers(<ImageWithFallback width={100} height={100} />)
 
-    const fallbackImage = await view.findByTestId("fallback-image")
+    const fallbackImage = await screen.findByTestId("fallback-image")
     expect(fallbackImage).toBeTruthy()
   })
 })
