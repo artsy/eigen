c84e27888dbb4e09a057094cd937ee2b007f6084
brainbicycle <brian.beckerle@artsymail.com>
2024-12-19 15:38:20 -0500

    react native upgrade diff

--- android/app/src/main/java/net/artsy/app/MainApplication.kt
+++ android/app/src/main/java/net/artsy/app/MainApplication.kt
@@ -15,6 +15,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.google.firebase.messaging.FirebaseMessaging
 import com.microsoft.codepush.react.CodePush
 import com.segment.analytics.Analytics
@@ -46,7 +47,7 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, /* native exopackage */ false)
+        SoLoader.init(this, OpenSourceMergedSoMapping)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
             load()
