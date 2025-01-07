ededf25bc9b418c1272a35e1f0be3089e7f917d9
dariakoko <nargle30@gmail.com>
2025-01-07 19:55:26 +0100

    chore: add tests

---
+++ src/app/Components/Artist/ArtistArtworks/CreateAlertPromptMessage.tests.tsx
@@ -0,0 +1,63 @@
+import { screen } from "@testing-library/react-native"
+import { __globalStoreTestUtils__ } from "app/store/GlobalStore"
+import { renderWithWrappers } from "app/utils/tests/renderWithWrappers"
+import { Text } from "react-native"
+import { CreateAlertPromptMessage } from "./CreateAlertPromptMessage"
+
+jest.mock("@artsy/palette-mobile", () => ({
+  ...jest.requireActual("@artsy/palette-mobile"),
+}))
+
+jest.mock("@react-navigation/native", () => ({
+  ...jest.requireActual("@react-navigation/native"),
+  useNavigation: () => ({
+    navigate: jest.fn(),
+  }),
+}))
+
+describe("CreateAlertPromptMessage", () => {
+  const wrapper = () =>
+    renderWithWrappers(
+      <CreateAlertPromptMessage onPress={() => jest.fn()}>
+        <Text>Test Children</Text>
+      </CreateAlertPromptMessage>
+    )
+
+  it("renders correctly first time", () => {
+    __globalStoreTestUtils__?.injectState({
+      createAlertPrompt: { promptState: { timesShown: 0, dismisDate: 0 } },
+    })
+    wrapper()
+
+    expect(screen.getByText("Searching for a particular artwork?")).toBeOnTheScreen()
+  })
+
+  it("is not rendered on the third time", () => {
+    __globalStoreTestUtils__?.injectState({
+      createAlertPrompt: { promptState: { timesShown: 2, dismisDate: 0 } },
+    })
+    wrapper()
+
+    expect(screen.queryByText("Searching for a particular artwork?")).not.toBeOnTheScreen()
+  })
+
+  it("renders correctly second time after 3 days", () => {
+    __globalStoreTestUtils__?.injectState({
+      // 259200000 is 3 days
+      createAlertPrompt: { promptState: { timesShown: 1, dismisDate: Date.now() - 259200000 } },
+    })
+    wrapper()
+
+    expect(screen.getByText("Searching for a particular artwork?")).toBeOnTheScreen()
+  })
+
+  it("is not rendered second time after less than 3 days", () => {
+    __globalStoreTestUtils__?.injectState({
+      // 259200 is less than 3 days
+      createAlertPrompt: { promptState: { timesShown: 1, dismisDate: Date.now() - 259200 } },
+    })
+    wrapper()
+
+    expect(screen.queryByText("Searching for a particular artwork?")).not.toBeOnTheScreen()
+  })
+})
