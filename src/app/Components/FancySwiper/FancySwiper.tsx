062932cd3478e8b7c13dc1f968a2941fd3baf185
Adam Iskounen <adam.iskounen@artsymail.com>
2025-01-09 05:45:14 -0500

    refactor

--- src/app/Components/FancySwiper/FancySwiper.tsx
+++ src/app/Components/FancySwiper/FancySwiper.tsx
@@ -1,6 +1,6 @@
 import { Flex } from "@artsy/palette-mobile"
 import { FancySwiperIcons } from "app/Components/FancySwiper/FancySwiperIcons"
-import { useRef, useCallback } from "react"
+import { useRef } from "react"
 import { PanResponder, Animated } from "react-native"
 import { Card, FancySwiperCard } from "./FancySwiperCard"
 
@@ -9,7 +9,7 @@ export const OFFSET_X = 100
 interface FancySwiperProps {
   cards: Card[]
   hideActionButtons?: boolean
-  onSwipeLeft: () => void
+  onSwipeLeft?: () => void
   onSwipeRight?: () => void
 }
 
@@ -22,20 +22,6 @@ export const FancySwiper = ({
   const remainingCards = cards.reverse()
   const swiper = useRef<Animated.ValueXY>(new Animated.ValueXY()).current
 
-  const removeCardFromTop = useCallback(
-    (swipeDirection: "right" | "left") => {
-      // Revert the pan responder to its initial position
-      swiper.setValue({ x: 0, y: 0 })
-
-      if (onSwipeRight && swipeDirection === "right") {
-        onSwipeRight()
-      } else {
-        onSwipeLeft()
-      }
-    },
-    [remainingCards, swiper]
-  )
-
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (_, { dx, dy }) => {
@@ -43,36 +29,54 @@ export const FancySwiper = ({
       swiper.setValue({ x: dx, y: dy })
     },
     onPanResponderRelease: (_, { dx, dy }) => {
-      // if the card was swiped enough, attempt to remove it from the top
-      if (Math.abs(dx) >= OFFSET_X) {
-        const sign = Math.sign(dx)
-        const swipeDirection = sign > 0 ? "right" : "left"
+      const isFullSwipe = Math.abs(dx) >= OFFSET_X
+      const isRightSwipe = dx > 0
+      const isLeftSwipe = dx < 0
 
-        // only allow the swipe if the swipe direction is supported
-        if ((onSwipeRight && swipeDirection === "right") || swipeDirection === "left") {
-          onSwipeHandler(swipeDirection, dy)
-          return
-        }
+      if (isFullSwipe && isRightSwipe && onSwipeRight) {
+        handleRightSwipe(dy)
+      } else if (isFullSwipe && isLeftSwipe && onSwipeLeft) {
+        handleLeftSwipe(dy)
+      } else {
+        // move the card to its original position
+        Animated.spring(swiper, {
+          toValue: { x: 0, y: 0 },
+          friction: 5,
+          useNativeDriver: true,
+        }).start()
       }
-
-      // move the card to its original position if it wasn't swiped enough or the swipe was invalid
-      Animated.spring(swiper, {
-        toValue: { x: 0, y: 0 },
-        friction: 5,
-        useNativeDriver: true,
-      }).start()
     },
   })
 
-  const onSwipeHandler = (swipeDirection: "right" | "left", toValueY?: number) => {
-    const sign = swipeDirection === "left" ? -1 : 1
-    // Move the card off the screen
+  const handleLeftSwipe = (toValueY?: number) => {
+    // move the card off the screen
     Animated.timing(swiper, {
-      toValue: { x: 1000 * sign, y: toValueY || 0 },
+      toValue: { x: -1000, y: toValueY || 0 },
       duration: 300,
       useNativeDriver: true,
     }).start(() => {
-      removeCardFromTop(swipeDirection)
+      // revert the pan responder to its initial position
+      swiper.setValue({ x: 0, y: 0 })
+
+      if (onSwipeLeft) {
+        onSwipeLeft()
+      }
+    })
+  }
+
+  const handleRightSwipe = (toValueY?: number) => {
+    // move the card off the screen
+    Animated.timing(swiper, {
+      toValue: { x: 1000, y: toValueY || 0 },
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => {
+      // Revert the pan responder to its initial position
+      swiper.setValue({ x: 0, y: 0 })
+
+      if (onSwipeRight) {
+        onSwipeRight()
+      }
     })
   }
 
@@ -96,7 +100,9 @@ export const FancySwiper = ({
           )
         })}
       </Flex>
-      {!hideActionButtons && <FancySwiperIcons swiper={swiper} OnPress={onSwipeHandler} />}
+      {!hideActionButtons && (
+        <FancySwiperIcons onDislike={handleLeftSwipe} onLike={handleRightSwipe} swiper={swiper} />
+      )}
     </>
   )
 }
