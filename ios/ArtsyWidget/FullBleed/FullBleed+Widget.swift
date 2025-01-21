e7e465b72b5715453ffbd946d163c674a8e3ac2c
Jon Allured <jon@jonallured.com>
2025-01-21 10:30:28 -0600

    Extract properties for widget config

--- ios/ArtsyWidget/FullBleed/FullBleed+Widget.swift
+++ ios/ArtsyWidget/FullBleed/FullBleed+Widget.swift
@@ -4,14 +4,16 @@ import WidgetKit
 
 extension FullBleed {
     struct Widget: SwiftUI.Widget {
+        static let description: String = "A curated selection of newly uploaded works from galleries, fairs, and auctions."
+        static let displayName: String = "New This Week"
         static let kind: String = "FullBleedWidget"
         
         var body: some WidgetConfiguration {
             StaticConfiguration(kind: Widget.kind, provider: Provider()) { entry in
                 View(entry: entry)
             }
-            .configurationDisplayName("New This Week")
-            .description("A curated selection of newly uploaded works from galleries, fairs, and auctions.")
+            .configurationDisplayName(Widget.displayName)
+            .description(Widget.description)
             .supportedFamilies(View.supportedFamilies)
         }
     }
--- ios/ArtsyWidget/LatestArticles/LatestArticles+Widget.swift
+++ ios/ArtsyWidget/LatestArticles/LatestArticles+Widget.swift
@@ -4,14 +4,16 @@ import WidgetKit
 
 extension LatestArticles {
     struct Widget: SwiftUI.Widget {
+        static let description: String = "The latest articles from Artsy Editorial."
+        static let displayName: String = "Editorial"
         static let kind: String = "LatestArticlesWidget"
         
         var body: some WidgetConfiguration {
             StaticConfiguration(kind: Widget.kind, provider: Provider()) { entry in
                 View(entry: entry)
             }
-            .configurationDisplayName("Editorial")
-            .description("The latest articles from Artsy Editorial.")
+            .configurationDisplayName(Widget.displayName)
+            .description(Widget.description)
             .supportedFamilies(View.supportedFamilies)
         }
     }
