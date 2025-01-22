520274f33614d4e07f0a82f352034fef9bcc165c
George Kartalis <g.kartalis7@gmail.com>
2025-01-22 10:27:55 +0100

    refactor: remove FancyModal remains and replace with RNModal

--- src/app/Scenes/SellWithArtsy/SubmitArtwork/ArtworkDetails/InfoModal/InfoModal.tsx
+++ src/app/Scenes/SellWithArtsy/SubmitArtwork/ArtworkDetails/InfoModal/InfoModal.tsx
@@ -1,7 +1,14 @@
-import { Spacer, Flex, Text, Button, ButtonProps, useSpace } from "@artsy/palette-mobile"
-import { FancyModal } from "app/Components/FancyModal/FancyModal"
-import { NavigationHeader } from "app/Components/NavigationHeader"
-import { ViewStyle } from "react-native"
+import {
+  Spacer,
+  Flex,
+  Button,
+  ButtonProps,
+  useSpace,
+  Screen,
+  Touchable,
+  CloseIcon,
+} from "@artsy/palette-mobile"
+import { Modal, ViewStyle } from "react-native"
 
 interface Props {
   buttonVariant?: ButtonProps["variant"]
@@ -19,22 +26,30 @@ export const InfoModal: React.FC<Props> = ({
   onDismiss,
   title,
   visible,
-  fullScreen = false,
 }) => {
   const space = useSpace()
   return (
-    <FancyModal visible={visible} onBackgroundPressed={onDismiss} fullScreen={fullScreen}>
-      <NavigationHeader onLeftButtonPress={onDismiss} hideBottomDivider useXButton>
-        {!!title && <Text fontSize={24}>{title}</Text>}
-      </NavigationHeader>
+    <Modal visible={visible} onDismiss={onDismiss}>
+      <Screen>
+        <Screen.Header
+          title={title}
+          leftElements={
+            <Touchable hitSlop={{ bottom: 40, right: 40, left: 40, top: 40 }} onPress={onDismiss}>
+              <CloseIcon fill="black100" />
+            </Touchable>
+          }
+        />
 
-      <Flex style={[{ margin: space(2) }, containerStyle]}>{children}</Flex>
-      <Spacer y={2} />
-      <Flex justifyContent="center" alignSelf="center" px={2}>
-        <Button block haptic onPress={onDismiss} variant={buttonVariant}>
-          Close
-        </Button>
-      </Flex>
-    </FancyModal>
+        <Flex bg="red10" style={[{ margin: space(2) }, containerStyle]}>
+          {children}
+        </Flex>
+        <Spacer y={2} />
+        <Flex justifyContent="center" alignSelf="center" px={2}>
+          <Button block haptic onPress={onDismiss} variant={buttonVariant}>
+            Close
+          </Button>
+        </Flex>
+      </Screen>
+    </Modal>
   )
 }
