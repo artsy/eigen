55421e9fd56c0e04f862dc3a5e9cd58fa22379b6
dariakoko <nargle30@gmail.com>
2025-01-22 14:17:01 +0100

    fix useOnSaveArtwork

--- src/app/Components/ProgressiveOnboarding/useOnSaveArtwork.ts
+++ src/app/Components/ProgressiveOnboarding/useOnSaveArtwork.ts
@@ -1,22 +1,21 @@
-import { useSetActivePopover } from "app/Components/ProgressiveOnboarding/useSetActivePopover"
 import { GlobalStore } from "app/store/GlobalStore"
 import { useCallback } from "react"
 
 export const useOnSaveArtwork = () => {
   const { setTabProps } = GlobalStore.actions.bottomTabs
-  const { dismiss } = GlobalStore.actions.progressiveOnboarding
+  const { dismiss, setActivePopover, setIsReady } = GlobalStore.actions.progressiveOnboarding
   const { tabProps, isDismissed } = GlobalStore.useAppState((state) => ({
     ...state.bottomTabs.sessionState,
     ...state.progressiveOnboarding,
   }))
 
-  const dismissed = isDismissed("save-artwork").status
-  const { clearActivePopover } = useSetActivePopover(!dismissed)
-
   const setProfileTabSavedArtwork = useCallback(() => {
-    if (!dismissed) {
+    if (!isDismissed("save-artwork").status) {
       dismiss("save-artwork")
-      clearActivePopover()
+      // same as clearActivePopover
+      // imported clearActivePopover cannot be used inside the hook
+      setActivePopover(undefined)
+      setIsReady(true)
     }
     setTabProps({ tab: "profile", props: { ...tabProps.profile, savedArtwork: true } })
   }, [tabProps.profile?.savedArtwork, setTabProps, isDismissed, dismiss])
