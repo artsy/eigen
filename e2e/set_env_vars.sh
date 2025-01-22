b1eb0f4b6058bb71451187a9164fa4573d510443
brainbicycle <brian.beckerle@artsymail.com>
2025-01-22 15:22:45 +0100

    update login flow

---
+++ e2e/set_env_vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+# TODO: make work for android as well 
+export MAESTRO_APP_ID=net.artsy.artsy
+export MAESTRO_TEST_EMAIL=detox+test@example.com
+export MAESTRO_TEST_PASSWORD='$###$someT0ughP4$$'
