3188af7fbcbb00b8afeb9c1983f66e9038cb9649
dariakoko <nargle30@gmail.com>
2025-01-22 17:16:55 +0100

    add tests

--- src/app/Components/ProgressiveOnboarding/useDismissAlertReminder.tsx
+++ src/app/Components/ProgressiveOnboarding/useDismissAlertReminder.tsx
@@ -1,15 +1,9 @@
 import { useIsFocused } from "@react-navigation/native"
 import { GlobalStore } from "app/store/GlobalStore"
-import {
-  PROGRESSIVE_ONBOARDING_ALERT_FINISH,
-  PROGRESSIVE_ONBOARDING_ALERT_REMINDER_1,
-  PROGRESSIVE_ONBOARDING_ALERT_REMINDER_2,
-} from "app/store/ProgressiveOnboardingModel"
 
 export const CREATE_ALERT_REMINDER_ARTWORK_THRESHOLD = 40
 const DAYS = 1000 * 60 * 60 * 24
 const MINUTES = 1000 * 60
-const SECONDS = 1000
 
 export const useDismissAlertReminder = () => {
   const isFocused = useIsFocused()
@@ -19,27 +13,23 @@ export const useDismissAlertReminder = () => {
     sessionState: { isReady },
   } = GlobalStore.useAppState((state) => state.progressiveOnboarding)
 
-  const interval = __DEV__ ? 10 * SECONDS : 7 * DAYS
-
-  const displayFirstTime =
-    isDismissed(PROGRESSIVE_ONBOARDING_ALERT_FINISH).status &&
-    Date.now() - isDismissed(PROGRESSIVE_ONBOARDING_ALERT_FINISH).timestamp >= interval
+  const displayFirstTime = isDismissed("alert-finish").status
 
   const displaySecondTime =
-    !isDismissed(PROGRESSIVE_ONBOARDING_ALERT_REMINDER_2).status &&
-    isDismissed(PROGRESSIVE_ONBOARDING_ALERT_REMINDER_1).status &&
+    !isDismissed("alert-create-reminder-2").status &&
+    isDismissed("alert-create-reminder-1").status &&
     // display second time 7 days after the first reminder was dismissed
-    Date.now() - isDismissed(PROGRESSIVE_ONBOARDING_ALERT_REMINDER_1).timestamp >= interval
+    Date.now() - isDismissed("alert-create-reminder-1").timestamp >= 7 * DAYS
 
   const isDisplayable = isReady && (displayFirstTime || displaySecondTime) && isFocused
 
   const dismissSingleReminder = () => {
     setIsReady(false)
 
-    if (!isDismissed(PROGRESSIVE_ONBOARDING_ALERT_REMINDER_1).status) {
-      dismiss(PROGRESSIVE_ONBOARDING_ALERT_REMINDER_1)
+    if (!isDismissed("alert-create-reminder-1").status) {
+      dismiss("alert-create-reminder-1")
     } else {
-      dismiss(PROGRESSIVE_ONBOARDING_ALERT_REMINDER_2)
+      dismiss("alert-create-reminder-2")
     }
   }
 
@@ -48,9 +38,9 @@ export const useDismissAlertReminder = () => {
      * Dismiss the second reminder if the user clicks the Create Alert
      * button within two minutes of dismissing the first reminder.
      */
-    if (Date.now() - isDismissed(PROGRESSIVE_ONBOARDING_ALERT_REMINDER_1).timestamp < 2 * MINUTES) {
+    if (Date.now() - isDismissed("alert-create-reminder-1").timestamp < 2 * MINUTES) {
       setIsReady(false)
-      dismiss(PROGRESSIVE_ONBOARDING_ALERT_REMINDER_2)
+      dismiss("alert-create-reminder-2")
     }
   }
 
