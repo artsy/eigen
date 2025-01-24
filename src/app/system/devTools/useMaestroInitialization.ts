4ebaf9fa8325fc41a62e790dbe6d7638fbf4ef6a
brainbicycle <brian.beckerle@artsymail.com>
2025-01-24 13:18:07 +0100

    only sign out if currently signed in to prevent ui breakage

--- src/app/system/devTools/useMaestroInitialization.ts
+++ src/app/system/devTools/useMaestroInitialization.ts
@@ -11,6 +11,8 @@ interface MaestroLaunchArguments {
 // TODO: We should not have this installed as a dependency in the app
 // figure out how to get this conditionally working for maestro builds
 export const useMaestroInitialization = () => {
+  const isLoggedIn = GlobalStore.useAppState((state) => !!state.auth.userAccessToken)
+
   useEffect(() => {
     const args = LaunchArguments.value<MaestroLaunchArguments>()
     console.log("LaunchArgs: ", args)
@@ -26,9 +28,9 @@ export const useMaestroInitialization = () => {
         email,
         password,
       })
-    } else if (shouldSignOut) {
+    } else if (shouldSignOut && isLoggedIn) {
       console.log("LaunchArgs: Signing out")
       GlobalStore.actions.auth.signOut()
     }
-  }, [])
+  }, [isLoggedIn])
 }
