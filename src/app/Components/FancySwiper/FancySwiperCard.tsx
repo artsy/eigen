811985661ac33f470d73a44794d5b22185e34d8e
Adam Iskounen <adam.iskounen@artsymail.com>
2025-02-03 19:56:33 -0500

    cast a shadow while an artwork card is being swiped

--- src/app/Components/FancySwiper/FancySwiperCard.tsx
+++ src/app/Components/FancySwiper/FancySwiperCard.tsx
@@ -10,22 +10,49 @@ interface FancySwiperCardProps extends GestureResponderHandlers {
 
 export const FancySwiperCard = memo(
   ({ card, swiper, isTopCard, ...rest }: FancySwiperCardProps) => {
-    /**
-     * Give the top card a small rotation as it is being swiped so that it gives users the
-     * impression that they are moving a real card.
-     */
+    // tilt the top card ever so slightly as it is being swiped
     const rotate = swiper.x.interpolate({
       inputRange: [-SWIPE_MAGNITUDE, 0, SWIPE_MAGNITUDE],
       outputRange: ["-10deg", "0deg", "10deg"],
     })
 
-    const animatedStyle = {
+    const transformStyle = {
       transform: [...swiper.getTranslateTransform(), { rotate }],
+    }
+
+    // drop a shadow from the top card and make it more intense as it is swiped
+    const shadowOpacityX = swiper.x.interpolate({
+      inputRange: [-SWIPE_MAGNITUDE, 0, SWIPE_MAGNITUDE],
+      outputRange: [0.13, 0, 0.13],
+    })
+
+    const shadowOpacityY = swiper.y.interpolate({
+      inputRange: [-SWIPE_MAGNITUDE, 0, SWIPE_MAGNITUDE],
+      outputRange: [0.13, 0, 0.13],
+    })
+
+    const shadowOpacity = Animated.add(shadowOpacityX, shadowOpacityY).interpolate({
+      inputRange: [0, 0.26],
+      outputRange: [0, 1],
+      extrapolate: "clamp",
+    })
+
+    const shadowStyle = {
+      shadowOffset: {
+        width: 0,
+        height: 0,
+      },
+      shadowOpacity,
+      shadowRadius: 12,
     }
 
     return (
       <Animated.View
-        style={[{ position: "absolute", zIndex: -1, width: "100%" }, isTopCard && animatedStyle]}
+        style={[
+          { position: "absolute", zIndex: -1, width: "100%" },
+          isTopCard && transformStyle,
+          isTopCard && shadowStyle,
+        ]}
         testID={isTopCard ? "top-fancy-swiper-card" : undefined}
         {...rest}
       >
