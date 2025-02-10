e0a81d9647dd882427c35fb8a358b6fc0ecef93d
George Kartalis <g.kartalis7@gmail.com>
2025-02-10 18:43:15 +0100

    chore: remove codepush dep

--- android/app/src/main/java/net/artsy/app/MainApplication.kt
+++ android/app/src/main/java/net/artsy/app/MainApplication.kt
@@ -20,7 +20,6 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.google.firebase.messaging.FirebaseMessaging
-import com.microsoft.codepush.react.CodePush
 import com.segment.analytics.Analytics
 import io.sentry.react.RNSentryPackage
 import com.reactnativekeysjsi.KeysModule.getSecureFor
--- ios/Artsy/AppDelegate.mm
+++ ios/Artsy/AppDelegate.mm
@@ -6,10 +6,6 @@
 #import <BrazeUI/BrazeUI-Swift.h>
 #import "BrazeReactBridge.h"
 #import "BrazeReactUtils.h"
-
-#import <CodePush/CodePush.h>
-#import <AppCenterReactNative.h>
-
 #import "AppDelegate.h"
 #import "AppDelegate+Emission.h"
 #import "AppDelegate+Echo.h"
@@ -102,9 +98,6 @@ static ARAppDelegate *_sharedInstance = nil;
 
     [self setupSharedEmission];
 
-    [AppCenterReactNative register];
-
-
     self.moduleName = @"eigen";
 
     // You can add your custom initial props in the dictionary below.
@@ -123,7 +116,7 @@ static ARAppDelegate *_sharedInstance = nil;
 {
     [self setupForAppLaunch:launchOptions];
     [self setupAnalytics:application withLaunchOptions:launchOptions];
-    
+
     UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
     center.delegate = self;
 
@@ -253,7 +246,7 @@ static ARAppDelegate *_sharedInstance = nil;
 
     [[NSUserDefaults standardUserDefaults] setInteger:numberOfRuns forKey:ARAnalyticsAppUsageCountProperty];
     [[NSUserDefaults standardUserDefaults] synchronize];
-    
+
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
@@ -266,7 +259,7 @@ static ARAppDelegate *_sharedInstance = nil;
 #if DEBUG
     return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry"];
 #else
-    return [CodePush bundleURL];
+    return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
