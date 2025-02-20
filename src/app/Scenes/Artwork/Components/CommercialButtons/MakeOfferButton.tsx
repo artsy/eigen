899e102ee8339c27b127c76c016ede8df5d5c0f0
Mounir Dhahri <soussouheros@gmail.com>
2025-02-20 17:21:09 +0100

    chore: refactor make offer button (#11540)
    fix: make offer button warning

--- src/app/Scenes/Artwork/Components/CommercialButtons/MakeOfferButton.tsx
+++ src/app/Scenes/Artwork/Components/CommercialButtons/MakeOfferButton.tsx
@@ -1,13 +1,13 @@
-import { ActionType, OwnerType, TappedMakeOffer } from "@artsy/cohesion"
-import { ButtonProps, Button } from "@artsy/palette-mobile"
+import { ActionType, OwnerType } from "@artsy/cohesion"
+import { Button, ButtonProps } from "@artsy/palette-mobile"
 import { MakeOfferButtonOrderMutation } from "__generated__/MakeOfferButtonOrderMutation.graphql"
 import { MakeOfferButton_artwork$data } from "__generated__/MakeOfferButton_artwork.graphql"
 import { navigate } from "app/system/navigation/navigate"
-import { Track, track as _track } from "app/utils/track"
 
-import React from "react"
+import React, { useState } from "react"
 import { Alert } from "react-native"
 import { commitMutation, createFragmentContainer, graphql, RelayProp } from "react-relay"
+import { useTracking } from "react-tracking"
 
 interface PartnerOffer {
   internalID: string
@@ -23,25 +23,14 @@ export interface MakeOfferButtonProps {
   partnerOffer?: PartnerOffer
 }
 
-export interface State {
-  isCommittingCreateOfferOrderMutation: boolean
-  showCheckoutFlowModal: boolean
-  orderUrl: string | null
-}
+export const MakeOfferButton: React.FC<MakeOfferButtonProps> = (props) => {
+  const [isCommittingCreateOfferOrderMutation, setIsCommittingCreateOfferOrderMutation] =
+    useState(false)
 
-// @ts-expect-error STRICTNESS_MIGRATION --- 🚨 Unsafe legacy code 🚨 Please delete this and fix any type errors if you have time 🙏
-const track: Track<MakeOfferButtonProps, State, TappedMakeOffer> = _track
-
-@track()
-export class MakeOfferButton extends React.Component<MakeOfferButtonProps, State> {
-  state = {
-    isCommittingCreateOfferOrderMutation: false,
-    showCheckoutFlowModal: false,
-    orderUrl: null,
-  }
+  const tracking = useTracking()
 
   // @ts-expect-error STRICTNESS_MIGRATION --- 🚨 Unsafe legacy code 🚨 Please delete this and fix any type errors if you have time 🙏
-  onMutationError(error) {
+  const onMutationError = (error) => {
     Alert.alert(
       "Sorry, we couldn't process the request.",
       "Please try again or contact orders@artsy.net for help.",
@@ -53,7 +42,7 @@ export class MakeOfferButton extends React.Component<MakeOfferButtonProps, State
         {
           text: "Retry",
           onPress: () => {
-            this.handleCreateOfferOrder()
+            handleCreateOfferOrder()
           },
         },
       ]
@@ -61,97 +50,89 @@ export class MakeOfferButton extends React.Component<MakeOfferButtonProps, State
     console.log("src/app/Scenes/Artwork/Components/MakeOfferButton.tsx", error)
   }
 
-  @track(
-    (props): TappedMakeOffer => ({
+  const handleCreateOfferOrder = () => {
+    tracking.trackEvent({
       action: ActionType.tappedMakeOffer,
       context_owner_type: OwnerType.artwork,
       context_owner_id: props.artwork.internalID,
     })
-  )
-  handleCreateOfferOrder() {
-    const { relay, artwork, editionSetID, partnerOffer } = this.props
-    const { isCommittingCreateOfferOrderMutation } = this.state
+
+    const { relay, artwork, editionSetID, partnerOffer } = props
     const { internalID } = artwork
 
     if (isCommittingCreateOfferOrderMutation) {
       return
     }
 
-    this.setState({ isCommittingCreateOfferOrderMutation: true }, () => {
-      if (relay && relay.environment) {
-        commitMutation<MakeOfferButtonOrderMutation>(relay.environment, {
-          mutation: graphql`
-            mutation MakeOfferButtonOrderMutation(
-              $input: CommerceCreateOfferOrderWithArtworkInput!
-            ) {
-              commerceCreateOfferOrderWithArtwork(input: $input) {
-                orderOrError {
-                  __typename
-                  ... on CommerceOrderWithMutationSuccess {
-                    order {
-                      internalID
-                      mode
-                    }
+    setIsCommittingCreateOfferOrderMutation(true)
+
+    if (relay && relay.environment) {
+      commitMutation<MakeOfferButtonOrderMutation>(relay.environment, {
+        mutation: graphql`
+          mutation MakeOfferButtonOrderMutation($input: CommerceCreateOfferOrderWithArtworkInput!) {
+            commerceCreateOfferOrderWithArtwork(input: $input) {
+              orderOrError {
+                __typename
+                ... on CommerceOrderWithMutationSuccess {
+                  order {
+                    internalID
+                    mode
                   }
-                  ... on CommerceOrderWithMutationFailure {
-                    error {
-                      type
-                      code
-                      data
-                    }
+                }
+                ... on CommerceOrderWithMutationFailure {
+                  error {
+                    type
+                    code
+                    data
                   }
                 }
               }
             }
-          `,
-          variables: {
-            input: {
-              artworkId: internalID,
-              editionSetId: editionSetID,
-              partnerOfferId: partnerOffer?.isActive ? partnerOffer.internalID : null,
-            },
+          }
+        `,
+        variables: {
+          input: {
+            artworkId: internalID,
+            editionSetId: editionSetID,
+            partnerOfferId: partnerOffer?.isActive ? partnerOffer.internalID : null,
           },
-          onCompleted: (data) => {
-            this.setState({ isCommittingCreateOfferOrderMutation: false }, () => {
-              const {
-                // @ts-expect-error STRICTNESS_MIGRATION --- 🚨 Unsafe legacy code 🚨 Please delete this and fix any type errors if you have time 🙏
-                commerceCreateOfferOrderWithArtwork: { orderOrError },
-              } = data
-              if (orderOrError.__typename === "CommerceOrderWithMutationFailure") {
-                this.onMutationError(orderOrError.error)
-              } else if (orderOrError.__typename === "CommerceOrderWithMutationSuccess") {
-                navigate(`/orders/${orderOrError.order.internalID}`, {
-                  passProps: { orderID: orderOrError.order.internalID, title: "Make Offer" },
-                })
-              }
+        },
+        onCompleted: (data) => {
+          setIsCommittingCreateOfferOrderMutation(false)
+
+          const {
+            // @ts-expect-error STRICTNESS_MIGRATION --- 🚨 Unsafe legacy code 🚨 Please delete this and fix any type errors if you have time 🙏
+            commerceCreateOfferOrderWithArtwork: { orderOrError },
+          } = data
+          if (orderOrError.__typename === "CommerceOrderWithMutationFailure") {
+            onMutationError(orderOrError.error)
+          } else if (orderOrError.__typename === "CommerceOrderWithMutationSuccess") {
+            navigate(`/orders/${orderOrError.order.internalID}`, {
+              passProps: { orderID: orderOrError.order.internalID, title: "Make Offer" },
             })
-          },
-          onError: (error) =>
-            this.setState({ isCommittingCreateOfferOrderMutation: false }, () =>
-              this.onMutationError(error)
-            ),
-        })
-      }
-    })
+          }
+        },
+        onError: (error) => {
+          setIsCommittingCreateOfferOrderMutation(false)
+          onMutationError(error)
+        },
+      })
+    }
   }
 
-  render() {
-    const { isCommittingCreateOfferOrderMutation } = this.state
-
-    return (
-      <Button
-        onPress={() => this.handleCreateOfferOrder()}
-        loading={isCommittingCreateOfferOrderMutation}
-        size="large"
-        block
-        width={100}
-        variant={this.props.variant}
-        haptic
-      >
-        {this.props.buttonText ? this.props.buttonText : "Make an Offer"}
-      </Button>
-    )
-  }
+  return (
+    <Button
+      onPress={handleCreateOfferOrder}
+      loading={isCommittingCreateOfferOrderMutation}
+      size="large"
+      block
+      width={100}
+      variant={props.variant}
+      haptic
+    >
+      {props.buttonText || "Make an Offer"}
+    </Button>
+  )
 }
 
 export const MakeOfferButtonFragmentContainer = createFragmentContainer(MakeOfferButton, {
