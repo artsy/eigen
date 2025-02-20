a943ee5be819abb703af69c486c944943dee3892
Mounir Dhahri <soussouheros@gmail.com>
2025-02-20 18:05:13 +0100

    chore: add stepper

--- src/app/Scenes/InfiniteDiscovery/Components/InfiniteDiscoveryOnboarding.tsx
+++ src/app/Scenes/InfiniteDiscovery/Components/InfiniteDiscoveryOnboarding.tsx
@@ -1,9 +1,42 @@
-import { Button, Flex, Spacer, Text, useSpace } from "@artsy/palette-mobile"
+import {
+  Button,
+  Flex,
+  HeartIcon,
+  Spacer,
+  Text,
+  useScreenDimensions,
+  useSpace,
+} from "@artsy/palette-mobile"
+import { FlatList } from "react-native-gesture-handler"
 import LinearGradient from "react-native-linear-gradient"
 import { SafeAreaView } from "react-native-safe-area-context"
 
+const STEPS = [
+  {
+    key: "introduction",
+    title: (
+      <Text variant="sm-display" color="black60" mb={0.5}>
+        Welcome to Discover Daily
+      </Text>
+    ),
+    description: <Text variant="lg-display">A new way of browsing works on Artsy.</Text>,
+  },
+  {
+    key: "swipeArtworks",
+    description: <Text variant="lg-display">Swipe artworks to the left to see the next work</Text>,
+  },
+  {
+    key: "favouriteArtworks",
+    description: (
+      <Text variant="lg-display">
+        Press <HeartIcon /> when you like an artwork you see
+      </Text>
+    ),
+  },
+]
 export const InfiniteDiscoveryOnboarding: React.FC<{}> = () => {
   const space = useSpace()
+  const { width } = useScreenDimensions()
 
   return (
     <Flex flex={1} backgroundColor="white100">
@@ -19,20 +52,25 @@ export const InfiniteDiscoveryOnboarding: React.FC<{}> = () => {
           }}
         />
         <SafeAreaView style={{ flex: 1, justifyContent: "flex-end", paddingHorizontal: space(2) }}>
-          <Flex>
-            <Text variant="sm-display" color="black60">
-              Welcome to Discover Daily
-            </Text>
+          <FlatList
+            data={STEPS}
+            style={{ marginHorizontal: -space(2), flexGrow: 0 }}
+            renderItem={({ item }) => (
+              <Flex width={width} px={2} justifyContent="flex-end">
+                {item.title}
+                {item.description}
+              </Flex>
+            )}
+            keyExtractor={(item) => item.key}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled
+          />
 
-            <Spacer y={0.5} />
+          <Spacer y={2} />
 
-            <Text variant="lg-display">A new way of browsing works on Artsy.</Text>
-
-            <Spacer y={2} />
-
-            <Flex alignItems="flex-end">
-              <Button variant="outline">Next</Button>
-            </Flex>
+          <Flex alignItems="flex-end">
+            <Button variant="outline">Next</Button>
           </Flex>
         </SafeAreaView>
       </Flex>
