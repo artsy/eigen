f58fbefc80eafdd39e2b06660b0b6e6eef600215
Sultan <sultan.al-maari@artsymail.com>
2025-03-13 17:33:32 +0100

    remove formatUserAgent

--- src/app/utils/formatUserAgent.ts
+++
@@ -1,6 +0,0 @@
-import DeviceInfo from "react-native-device-info"
-import { version } from "../../../app.json"
-
-export function formatUserAgent(userAgent: string): string {
-  return `${userAgent} ${DeviceInfo.getSystemName()}/${DeviceInfo.getSystemVersion()} Artsy-Mobile/${version} Eigen/${DeviceInfo.getBuildNumber()}/${version}`
-}
