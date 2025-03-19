c52f38f8775a7eecdcf2f995cde6fc59551cce23
Mounir Dhahri <soussouheros@gmail.com>
2025-03-19 15:04:57 +0100

    feat: add bottom sheet modal

--- src/app/Scenes/Favorites/FollowsTab.tsx
+++ src/app/Scenes/Favorites/FollowsTab.tsx
@@ -1,10 +1,101 @@
-import { Flex } from "@artsy/palette-mobile"
+import { ArrowDownIcon, Flex, Join, RadioButton, Spacer, Text } from "@artsy/palette-mobile"
+import { BottomSheetScrollView } from "@gorhom/bottom-sheet"
+import { AutomountedBottomSheetModal } from "app/Components/BottomSheet/AutomountedBottomSheetModal"
 import { FollowedArtistsQueryRenderer } from "app/Scenes/Favorites/Components/FollowedArtists"
+import { SNAP_POINTS } from "app/Scenes/MyCollection/Components/MyCollectionBottomSheetModals/MyCollectionBottomSheetModalArtistsPrompt"
+import { useState } from "react"
+import { TouchableOpacity } from "react-native"
+import Haptic from "react-native-haptic-feedback"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+
+type FollowOption = "artists" | "shows" | "galleries" | "categories"
+
+const FOLLOW_OPTIONS: {
+  value: FollowOption
+  label: string
+}[] = [
+  {
+    value: "artists",
+    label: "Artists",
+  },
+  {
+    value: "shows",
+    label: "Shows",
+  },
+  {
+    value: "galleries",
+    label: "Galleries",
+  },
+  {
+    value: "categories",
+    label: "Categories",
+  },
+]
 
 export const FollowsTab = () => {
+  const [followOption, setfollowOption] = useState<FollowOption>("artists")
+  const { bottom } = useSafeAreaInsets()
+
+  const [showBottomSheet, setShowBottomSheet] = useState(false)
+
   return (
     <Flex flex={1}>
-      <FollowedArtistsQueryRenderer />
+      <Flex p={2}>
+        <TouchableOpacity
+          style={{ flexDirection: "row", alignItems: "center" }}
+          onPress={() => {
+            Haptic.trigger("impactLight")
+            setShowBottomSheet(true)
+          }}
+        >
+          <Text variant="sm-display" mr={0.5}>
+            {FOLLOW_OPTIONS.find(({ value }) => value === followOption)?.label}
+          </Text>
+          <ArrowDownIcon />
+        </TouchableOpacity>
+      </Flex>
+
+      {followOption === "artists" && <FollowedArtistsQueryRenderer />}
+
+      <AutomountedBottomSheetModal
+        visible={showBottomSheet}
+        snapPoints={SNAP_POINTS}
+        enableDynamicSizing
+        onDismiss={() => {
+          setShowBottomSheet(false)
+        }}
+        name="FollowsBottomSheet"
+      >
+        <BottomSheetScrollView keyboardShouldPersistTaps="always">
+          <Flex p={2}>
+            <Text variant="lg-display" mb={1}>
+              Follows
+            </Text>
+
+            <Spacer y={2} />
+
+            <Join separator={<Spacer y={2} />}>
+              {FOLLOW_OPTIONS.map(({ value, label }) => (
+                <RadioButton
+                  key={value}
+                  onPress={() => {
+                    setfollowOption(value)
+                    // Dismiss after a short delay to make sure the user can verify their choice
+                    setTimeout(() => {
+                      setShowBottomSheet(false)
+                    }, 500)
+                  }}
+                  selected={followOption === value}
+                  text={label}
+                />
+              ))}
+            </Join>
+          </Flex>
+
+          {/* This is a spacer to make sure the bottom sheet is not covered by the system bottom insets */}
+          <Spacer y={`${bottom}px`} />
+        </BottomSheetScrollView>
+      </AutomountedBottomSheetModal>
     </Flex>
   )
 }
