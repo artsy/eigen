f65450f95646f504bfb7e36df18de02668318d6c
dariakoko <nargle30@gmail.com>
2025-03-19 18:08:04 +0100

    chore: remove required and the char limit

--- src/app/Components/ArtworkLists/components/CreateOrEditArtworkListForm.tsx
+++ src/app/Components/ArtworkLists/components/CreateOrEditArtworkListForm.tsx
@@ -93,7 +93,6 @@ export const CreateOrEditArtworkListForm: FC<CreateOrEditArtworkListFormProps> =
               }}
               returnKeyType="done"
               autoFocus
-              required
             />
 
             {!!AREnableArtworkListOfferability && (
