888c96498869d94191238c8de1499c1de8a1eea9
Nikita Skalkin <nikskalkin@gmail.com>
2025-03-20 14:05:14 +0100

    feat: correctly resize images + placeholder improvement

--- src/app/Scenes/Favorites/Alerts/AlertsListPlaceholder.tsx
+++ src/app/Scenes/Favorites/Alerts/AlertsListPlaceholder.tsx
@@ -1,10 +1,15 @@
-import { Flex, Box, SkeletonBox, SkeletonText } from "@artsy/palette-mobile"
+import { Flex, Box, SkeletonBox, SkeletonText, ArrowDownIcon, Text } from "@artsy/palette-mobile"
 import { times } from "lodash"
 
 export const AlertsListPlaceholder: React.FC = () => {
   return (
     <>
-      <Box py={1}>
+      <Box>
+        <Flex flexDirection="row" alignItems="center" mx={2} mt={2}>
+          <ArrowDownIcon />
+          <Text ml={0.5}>Sort By</Text>
+        </Flex>
+
         {times(20).map((index: number) => (
           <Box key={index} px={2} py={1} backgroundColor="white100">
             <Flex flexDirection="row" alignItems="center" justifyContent="flex-start">
