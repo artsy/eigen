0e36ec47053af108d972130187ae4bc6908e4639
George Kartalis <g.kartalis7@gmail.com>
2025-03-27 15:31:34 +0100

    refactor(ContextGridCTA): convert class component to functional component and fix tracking
    Co-authored-by: brainbicycle <brian.beckerle@artsymail.com>

--- src/app/Scenes/Artwork/Components/OtherWorks/ContextGridCTA.tsx
+++ src/app/Scenes/Artwork/Components/OtherWorks/ContextGridCTA.tsx
@@ -1,7 +1,8 @@
 import { ArrowRightIcon, Flex, Text } from "@artsy/palette-mobile"
 import { RouterLink } from "app/system/navigation/RouterLink"
-import { Schema, track } from "app/utils/track"
+import { Schema } from "app/utils/track"
 import React from "react"
+import { useTracking } from "react-tracking"
 
 interface ContextGridCTAProps {
   href?: string
@@ -9,32 +10,35 @@ interface ContextGridCTAProps {
   label: string
 }
 
-@track()
-export class ContextGridCTA extends React.Component<ContextGridCTAProps> {
-  @track((props) => ({
-    action_name: Schema.ActionNames.ViewAll,
-    action_type: Schema.ActionTypes.Tap,
-    flow: Schema.Flow.RecommendedArtworks,
-    context_module: props.contextModule,
-  }))
-  render() {
-    const { href, label } = this.props
+export const ContextGridCTA: React.FC<ContextGridCTAProps> = (props) => {
+  const { trackEvent } = useTracking()
+  const { href, label } = props
 
-    if (href && label) {
-      return (
-        <RouterLink to={href} accessibilityLabel="Context Grid CTA">
-          <Flex flexDirection="row" alignContent="center">
-            <Text variant="sm" textAlign="left" weight="medium">
-              {label}
-            </Text>
-            <Flex alignSelf="center">
-              <ArrowRightIcon fill="black30" ml={1} />
-            </Flex>
+  if (href && label) {
+    return (
+      <RouterLink
+        to={href}
+        accessibilityLabel="Context Grid CTA"
+        onPress={() =>
+          trackEvent({
+            action_name: Schema.ActionNames.ViewAll,
+            action_type: Schema.ActionTypes.Tap,
+            flow: Schema.Flow.RecommendedArtworks,
+            context_module: props.contextModule,
+          })
+        }
+      >
+        <Flex flexDirection="row" alignContent="center">
+          <Text variant="sm" textAlign="left" weight="medium">
+            {label}
+          </Text>
+          <Flex alignSelf="center">
+            <ArrowRightIcon fill="black30" ml={1} />
           </Flex>
-        </RouterLink>
-      )
-    } else {
-      return null
-    }
+        </Flex>
+      </RouterLink>
+    )
+  } else {
+    return null
   }
 }
