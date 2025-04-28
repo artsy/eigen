9c573566d9f60c9067e31197fe4b00005ffa763b
brainbicycle <brian.beckerle@artsymail.com>
2025-04-28 15:41:39 -0400

    use expo command on android

--- scripts/hermes/hermes-android-bundle.sh
+++ scripts/hermes/hermes-android-bundle.sh
@@ -3,12 +3,11 @@
 NODE_OPTIONS=--max_old_space_size=8192
 BUNDLE_DIR="android/app/src/main/assets"
 
-react-native bundle \
---dev false \
---platform android \
---entry-file index.js \
---reset-cache \
---assets-dest android/app/src/main/res \
---bundle-output $BUNDLE_DIR/index.android.bundle \
---sourcemap-output $BUNDLE_DIR/index.android.bundle.packager.map \
---minify false
+npx expo export:embed \
+  --dev false \
+  --platform android \
+  --entry-file index.js \
+  --reset-cache \
+  --assets-dest android/app/src/main/res \
+  --bundle-output $BUNDLE_DIR/index.android.bundle \
+  --sourcemap-output $BUNDLE_DIR/index.android.bundle.map
