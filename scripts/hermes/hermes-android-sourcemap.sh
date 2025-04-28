981e172b3b3e1b200ec5eed089f775069dfe6714
brainbicycle <brian.beckerle@artsymail.com>
2025-04-28 16:02:03 -0400

    add missing mv to android sourcemap script

--- scripts/hermes/hermes-android-sourcemap.sh
+++ scripts/hermes/hermes-android-sourcemap.sh
@@ -3,6 +3,8 @@
 NODE_OPTIONS=--max_old_space_size=8192
 BUNDLE_DIR="android/app/src/main/assets"
 
+mv $BUNDLE_DIR/index.android.bundle.map $BUNDLE_DIR/index.android.bundle.packager.map
+
 node ./node_modules/react-native/scripts/compose-source-maps.js \
 $BUNDLE_DIR/index.android.bundle.packager.map \
 $BUNDLE_DIR/index.android.bundle.hbc.map -o \
