0dbce7eb8e8a512473d972064ca2ebaef8cf175a
Mounir Dhahri <soussouheros@gmail.com>
2025-04-29 17:26:05 +0200

    chore: show content on empty follow option state (#12014)
    * chore: show content on empty state
    
    * chore: hide back buttons on follows
    
    (cherry picked from commit db6b61589144f9f10f1aef96fd9ae73bf1ad72ab)

--- src/app/Scenes/Favorites/Components/FollowedArtists.tsx
+++ src/app/Scenes/Favorites/Components/FollowedArtists.tsx
@@ -36,6 +36,7 @@ export const FollowedArtists: React.FC<Props> = ({ me }) => {
   if (data.followsAndSaves?.artistsConnection?.totalCount === 0) {
     return (
       <Screen.ScrollView refreshControl={RefreshControl}>
+        <FollowOptionPicker />
         <ZeroState
           title="You haven’t followed any artists yet"
           subtitle="When you’ve found an artist you like, follow them to get updates on new works that become available."
@@ -136,7 +137,7 @@ export const FollowedArtistsQueryRenderer = withSuspense({
     return (
       <LoadFailureView
         onRetry={fallbackProps.resetErrorBoundary}
-        showBackButton={true}
+        showBackButton={false}
         useSafeArea={false}
         error={fallbackProps.error}
         trackErrorBoundary={false}
