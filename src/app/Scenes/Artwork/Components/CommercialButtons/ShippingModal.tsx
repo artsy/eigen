e10d8adac4b927f08503a1aa96b91db62550286e
Ole <ole.richter91@gmail.com>
2025-05-02 14:17:36 +0200

    fix: Shipping modal dark mode background (#12038)

--- src/app/Scenes/Artwork/Components/CommercialButtons/ShippingModal.tsx
+++ src/app/Scenes/Artwork/Components/CommercialButtons/ShippingModal.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from "@artsy/palette-mobile"
+import { Flex, Text, useColor } from "@artsy/palette-mobile"
 import { LocationAutocomplete } from "app/Components/LocationAutocomplete"
 import { NavigationHeader } from "app/Components/NavigationHeader"
 import { LocationWithDetails } from "app/utils/googleMaps"
@@ -14,7 +14,7 @@ interface ShippingModalProps {
 
 export const ShippingModal: React.FC<ShippingModalProps> = (props) => {
   const { location, toggleVisibility, modalIsVisible, setLocation } = props
-
+  const color = useColor()
   const [locationDetails, setLocationDetails] = useState<LocationWithDetails | null>(null)
 
   return (
@@ -23,38 +23,41 @@ export const ShippingModal: React.FC<ShippingModalProps> = (props) => {
       onRequestClose={toggleVisibility}
       presentationStyle="formSheet"
       animationType="slide"
+      transparent={false}
     >
-      <NavigationHeader
-        leftButtonText="Cancel"
-        onLeftButtonPress={() => {
-          toggleVisibility()
-        }}
-        rightButtonText="Apply"
-        onRightButtonPress={() => {
-          setLocation(locationDetails as LocationWithDetails)
-          toggleVisibility()
-        }}
-        rightButtonDisabled={!locationDetails}
-      >
-        Add Location
-      </NavigationHeader>
-      <ScrollView keyboardShouldPersistTaps="always" contentContainerStyle={{ flex: 1 }}>
-        <Flex m={2} flex={1}>
-          <LocationAutocomplete
-            title="Location"
-            placeholder="Add Location"
-            onChange={setLocationDetails}
-            initialLocation={location}
-            floating
-            FooterComponent={() => (
-              <Text mt={1} color="mono60">
-                Sharing your location with galleries helps them provide fast and accurate shipping
-                quotes.
-              </Text>
-            )}
-          />
-        </Flex>
-      </ScrollView>
+      <Flex flex={1} backgroundColor={color("background")}>
+        <NavigationHeader
+          leftButtonText="Cancel"
+          onLeftButtonPress={() => {
+            toggleVisibility()
+          }}
+          rightButtonText="Apply"
+          onRightButtonPress={() => {
+            setLocation(locationDetails as LocationWithDetails)
+            toggleVisibility()
+          }}
+          rightButtonDisabled={!locationDetails}
+        >
+          Add Location
+        </NavigationHeader>
+        <ScrollView keyboardShouldPersistTaps="always" contentContainerStyle={{ flex: 1 }}>
+          <Flex m={2} flex={1}>
+            <LocationAutocomplete
+              title="Location"
+              placeholder="Add Location"
+              onChange={setLocationDetails}
+              initialLocation={location}
+              floating
+              FooterComponent={() => (
+                <Text mt={1} color="mono60">
+                  Sharing your location with galleries helps them provide fast and accurate shipping
+                  quotes.
+                </Text>
+              )}
+            />
+          </Flex>
+        </ScrollView>
+      </Flex>
     </Modal>
   )
 }
