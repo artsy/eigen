ce78fee8c68f41f4d92da7121b997e72ead07c00
Daria Kozlova <daria.kozlova@artsymail.com>
2025-05-02 13:37:51 +0200

    fix: fix my collection empty state banner (#12027)

--- src/app/Scenes/MyProfile/Components/UserAccountHeader/MyCollectionBannerEmptyState.tsx
+++ src/app/Scenes/MyProfile/Components/UserAccountHeader/MyCollectionBannerEmptyState.tsx
@@ -4,21 +4,25 @@ import { Image } from "react-native"
 
 export const MyCollectionBannerEmptyState: React.FC = () => {
   return (
-    <RouterLink to="my-collection" testID="my-collection-banner-empty-state">
-      <Flex width="100%" flexDirection="row" p={1} backgroundColor="mono5" borderRadius={10}>
-        <Join separator={<Spacer x={1} />}>
-          <Flex flex={1} flexDirection="column" justifyContent="center">
-            <Text>Build Your Collection</Text>
-            <Text color="mono60">Manage, track, and gain insights into your art collection. </Text>
-          </Flex>
-          <Image
-            source={require("images/user_account_mc_empty_state.webp")}
-            resizeMode="contain"
-            height={80}
-            width={80}
-          />
-        </Join>
-      </Flex>
-    </RouterLink>
+    <Flex width="100%">
+      <RouterLink to="my-collection" testID="my-collection-banner-empty-state">
+        <Flex flexDirection="row" p={1} backgroundColor="mono5" borderRadius={10}>
+          <Join separator={<Spacer x={1} />}>
+            <Flex flex={1} flexDirection="column" justifyContent="center">
+              <Text>Build Your Collection</Text>
+              <Text color="mono60">
+                Manage, track, and gain insights into your art collection.{" "}
+              </Text>
+            </Flex>
+            <Image
+              source={require("images/user_account_mc_empty_state.webp")}
+              resizeMode="contain"
+              height={80}
+              width={80}
+            />
+          </Join>
+        </Flex>
+      </RouterLink>
+    </Flex>
   )
 }
