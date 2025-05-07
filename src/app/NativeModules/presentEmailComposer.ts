5841d7a0ecef3714154debaace8466cf5e7679b4
dariakoko <nargle30@gmail.com>
2025-05-07 12:13:11 +0200

    fix: do not use legacy native modules

--- src/app/NativeModules/presentEmailComposer.ts
+++ src/app/NativeModules/presentEmailComposer.ts
@@ -1,20 +1,16 @@
 import { stringify } from "qs"
 import { Linking, Platform } from "react-native"
-import { LegacyNativeModules } from "./LegacyNativeModules"
 
 export function presentEmailComposer(toAddress: string, subject: string, body?: string) {
   if (Platform.OS !== "ios") {
     Linking.openURL(`mailto:${toAddress}?${stringify({ subject, body })}`)
   } else if (body) {
-    LegacyNativeModules.ARTNativeScreenPresenterModule.presentEmailComposerWithBody(
-      body,
-      subject,
-      toAddress
-    )
+    const url = `mailto:${toAddress}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`
+    Linking.openURL(url).catch((err) => console.error("Error opening email client:", err))
   } else {
-    LegacyNativeModules.ARTNativeScreenPresenterModule.presentEmailComposerWithSubject(
-      subject,
-      toAddress
-    )
+    const url = `mailto:${toAddress}?subject=${encodeURIComponent(subject)}`
+    Linking.openURL(url).catch((err) => console.error("Error opening email client:", err))
   }
 }
