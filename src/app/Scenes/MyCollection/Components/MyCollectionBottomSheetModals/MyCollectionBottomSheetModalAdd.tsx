aeecd9d1e3f00a4a053baa5762268553a814629f
Mounir Dhahri <soussouheros@gmail.com>
2025-05-07 09:44:03 +0200

    fix: gray header on my collection bottomsheet (#12069)

--- src/app/Scenes/MyCollection/Components/MyCollectionBottomSheetModals/MyCollectionBottomSheetModalAdd.tsx
+++ src/app/Scenes/MyCollection/Components/MyCollectionBottomSheetModals/MyCollectionBottomSheetModalAdd.tsx
@@ -1,15 +1,27 @@
-import { ArtworkIcon, Flex, Text, UserMultiIcon } from "@artsy/palette-mobile"
+import { ArtworkIcon, Flex, Text, UserMultiIcon, useSpace } from "@artsy/palette-mobile"
+import { BOTTOM_TABS_HEIGHT } from "@artsy/palette-mobile/dist/elements/Screen/StickySubHeader"
 import { BottomSheetView } from "@gorhom/bottom-sheet"
 import { MenuItem } from "app/Components/MenuItem"
 import { MyCollectionTabsStore } from "app/Scenes/MyCollection/State/MyCollectionTabsStore"
 import { Tab } from "app/Scenes/MyProfile/MyProfileHeaderMyCollectionAndSavedWorks"
 import { navigate } from "app/system/navigation/navigate"
+import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 export const MyCollectionBottomSheetModalAdd: React.FC<{}> = () => {
   const setViewKind = MyCollectionTabsStore.useStoreActions((state) => state.setViewKind)
 
+  const { bottom } = useSafeAreaInsets()
+  const space = useSpace()
+
+  const enableRedesignedSettings = useFeatureFlag("AREnableRedesignedSettings")
+
   return (
-    <BottomSheetView>
+    <BottomSheetView
+      style={{
+        paddingBottom: enableRedesignedSettings ? bottom + BOTTOM_TABS_HEIGHT + space(2) : 0,
+      }}
+    >
       <Flex pb={1}>
         <Text textAlign="center" variant="sm" pt={2} pb={2}>
           Add to My Collection
