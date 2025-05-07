66a5e2759ec070d0be0d0f41bcdfef28f91bc175
Daria Kozlova <daria.kozlova@artsymail.com>
2025-05-07 16:58:55 +0200

    fix: fix Send Feedback functionality  (#12079)
    * fix: do not use legacy native modules
    
    * fix: tests
    
    * chore: remove platform check

--- src/app/NativeModules/presentEmailComposer.ts
+++ src/app/NativeModules/presentEmailComposer.ts
@@ -1,20 +1,13 @@
-import { stringify } from "qs"
-import { Linking, Platform } from "react-native"
-import { LegacyNativeModules } from "./LegacyNativeModules"
+import { Linking } from "react-native"
 
 export function presentEmailComposer(toAddress: string, subject: string, body?: string) {
-  if (Platform.OS !== "ios") {
-    Linking.openURL(`mailto:${toAddress}?${stringify({ subject, body })}`)
-  } else if (body) {
-    LegacyNativeModules.ARTNativeScreenPresenterModule.presentEmailComposerWithBody(
-      body,
-      subject,
-      toAddress
-    )
+  if (body) {
+    const url = `mailto:${toAddress}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`
+    Linking.openURL(url).catch((err) => console.error("Error opening email client:", err))
   } else {
-    LegacyNativeModules.ARTNativeScreenPresenterModule.presentEmailComposerWithSubject(
-      subject,
-      toAddress
-    )
+    const url = `mailto:${toAddress}?subject=${encodeURIComponent(subject)}`
+    Linking.openURL(url).catch((err) => console.error("Error opening email client:", err))
   }
 }
