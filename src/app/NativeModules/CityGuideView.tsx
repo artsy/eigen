b7f50fa54cb8331c543f9347e9d3ecce0f3519ac
Mounir Dhahri <soussouheros@gmail.com>
2025-05-21 17:28:40 +0200

    feat: cross platform city guide: WIP 1

--- src/app/NativeModules/CityGuideView.tsx
+++ src/app/NativeModules/CityGuideView.tsx
@@ -1,9 +1,34 @@
+import { Flex } from "@artsy/palette-mobile"
+import { CityGuide } from "app/Scenes/Map/CityGuide"
+import { MapContainer } from "app/Scenes/Map/MapContainer"
+import { unsafe_getFeatureFlag } from "app/store/GlobalStore"
+import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
 import { requireNativeComponent } from "react-native"
 
-const ARTCityGuideView = requireNativeComponent("ARTCityGuideView")
+const ARTCityGuideView = !unsafe_getFeatureFlag("AREnableCrossPlatformCityGuide")
+  ? requireNativeComponent("ARTCityGuideView")
+  : Flex
 
-export const CityGuideView: React.FC = () => (
-  <ARTCityGuideView // @ts-ignore
-    style={{ flex: 1 }}
-  />
-)
+export const CityGuideView: React.FC = () => {
+  const enableCrossPlatformCityGuide = useFeatureFlag("AREnableCrossPlatformCityGuide")
+
+  if (enableCrossPlatformCityGuide) {
+    return (
+      <MapContainer
+        citySlug="new-york-ny-usa"
+        hideMapButtons={false}
+        safeAreaInsets={{ top: 0, right: 0, bottom: 0, left: 0 }}
+        initialCoordinates={NEW_YORK_COORDINATES}
+        userLocationWithinCity={true}
+      />
+    )
+  }
+
+  return (
+    <ARTCityGuideView // @ts-ignore
+      style={{ flex: 1 }}
+    />
+  )
+}
+
+const NEW_YORK_COORDINATES = { lat: 40.713, lng: -74.006 }
---
+++ src/app/Scenes/Map/CityGuide.tsx
@@ -0,0 +1,5 @@
+import { Flex } from "@artsy/palette-mobile"
+
+export const CityGuide: React.FC = () => {
+  return <Flex flex={1} backgroundColor="red" />
+}
