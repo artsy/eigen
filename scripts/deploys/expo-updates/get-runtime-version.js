976fc2aef966edf9957b4ddd060993c641686436
brainbicycle <brian.beckerle@artsymail.com>
2025-05-21 09:58:09 -0300

    review comments

--- scripts/deploys/expo-updates/get-runtime-version.js
+++ scripts/deploys/expo-updates/get-runtime-version.js
@@ -4,4 +4,6 @@ const path = require("path")
 
 const appJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../../app.json")))
 const version = appJson.expo.runtimeVersion
+
+// Output the runtime version so we can access it in build.gradle
 console.log(version)
