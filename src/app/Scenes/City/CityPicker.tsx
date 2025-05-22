6a4d9fdd7c8d1614d8d15c33560f9b247ce6cb49
Mounir Dhahri <soussouheros@gmail.com>
2025-05-22 10:03:21 +0200

    feat: cross platform city guide: WIP 5 add city picker

--- src/app/Scenes/City/CityPicker.tsx
+++ src/app/Scenes/City/CityPicker.tsx
@@ -1,12 +1,11 @@
-import { Flex, Box, Text, TextProps, Separator, Join } from "@artsy/palette-mobile"
-import { themeGet } from "@styled-system/theme-get"
+import { Box, Flex, Join, Separator, Text, TextProps, useSpace } from "@artsy/palette-mobile"
 import { CircleWhiteCheckIcon } from "app/Components/Icons/CircleWhiteCheckIcon"
 import { LegacyNativeModules } from "app/NativeModules/LegacyNativeModules"
 import { useScreenDimensions } from "app/utils/hooks"
 import { ProvideScreenTracking, Schema } from "app/utils/track"
 import React, { useEffect, useState } from "react"
 import { ScrollView, TouchableOpacity } from "react-native"
-import styled from "styled-components/native"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 import cities from "../../../../data/cityDataSortedByDisplayPreference.json"
 
 interface Props {
@@ -15,9 +14,13 @@ interface Props {
 
 const cityList = cities.map((city) => city.name)
 
+const BORDER_RADIUS = 10
+
 export const CityPicker: React.FC<Props> = (props) => {
   const [selectedCity, setSelectedCity] = useState<string | null>(props.selectedCity)
   const { size } = useScreenDimensions()
+  const space = useSpace()
+  const insets = useSafeAreaInsets()
 
   const selectCity = (city: string, index: number) => {
     setSelectedCity(city)
@@ -33,33 +36,43 @@ export const CityPicker: React.FC<Props> = (props) => {
     if (selectedCity === null) {
       return
     }
-    setSelectedCity(null)
   }, [selectedCity])
 
   const { height: screenHeight } = useScreenDimensions()
 
   // @TODO: Implement test for this component https://artsyproduct.atlassian.net/browse/LD-562
   return (
-    <ProvideScreenTracking
-      info={{
-        context_screen: Schema.PageNames.CityPicker,
-        context_screen_owner_type: Schema.OwnerEntityTypes.CityGuide,
+    <Flex
+      position="absolute"
+      zIndex={1000}
+      style={{
+        marginTop: insets.top + space(6),
       }}
+      backgroundColor="mono0"
+      borderRadius={BORDER_RADIUS}
+      maxHeight="80%"
+      minWidth="90%"
+      alignSelf="center"
     >
-      <Overlay bounces={false} showsVerticalScrollIndicator={false}>
-        <Box ml={2}>
-          <Text variant="sm" weight="medium">
-            Fairs and Shows by City
-          </Text>
-        </Box>
-        <ScrollView contentContainerStyle={{ paddingHorizontal: 20 }}>
+      <ProvideScreenTracking
+        info={{
+          context_screen: Schema.PageNames.CityPicker,
+          context_screen_owner_type: Schema.OwnerEntityTypes.CityGuide,
+        }}
+      >
+        <ScrollView contentContainerStyle={{ padding: space(2) }}>
+          <Box>
+            <Text variant="sm" weight="medium">
+              Fairs and Shows by City
+            </Text>
+          </Box>
           <Join separator={<Separator />}>
             {cityList.map((city, i) => (
               <Box key={i}>
                 <TouchableOpacity accessibilityRole="button" onPress={() => selectCity(city, i)}>
                   <Flex flexDirection="row" justifyContent="space-between" alignItems="center">
                     <Text
-                      mt={1}
+                      selectable={false}
                       variant={dimensions(screenHeight)[size].cityFontSize}
                       lineHeight={`${dimensions(screenHeight)[size].lineHeight}px`}
                     >
@@ -72,25 +85,14 @@ export const CityPicker: React.FC<Props> = (props) => {
                     )}
                   </Flex>
                 </TouchableOpacity>
-                <Separator />
               </Box>
             ))}
           </Join>
         </ScrollView>
-      </Overlay>
-    </ProvideScreenTracking>
+      </ProvideScreenTracking>
+    </Flex>
   )
 }
-
-const Overlay = styled.ScrollView`
-  flex: 1;
-  background-color: ${themeGet("colors.background")};
-  padding-top: ${themeGet("space.2")};
-  padding-left: ${themeGet("space.2")};
-  padding-right: ${themeGet("space.2")};
-  border-radius: 10;
-  flex-direction: column;
-`
 
 const dimensions = (
   size: number
