d011a2c55483f206612a7332c96e3bd271a1fb1f
Mounir Dhahri <soussouheros@gmail.com>
2025-05-23 10:55:38 +0200

    chore: adjust native screens registration

--- src/app/Navigation/AuthenticatedRoutes/NativeScreens.tsx
+++ src/app/Navigation/AuthenticatedRoutes/NativeScreens.tsx
@@ -1,9 +1,5 @@
-import { CityGuideView } from "app/NativeModules/CityGuideView"
 import { LiveAuctionView } from "app/NativeModules/LiveAuctionView"
 import { Providers } from "app/Providers"
-import { CityView } from "app/Scenes/City/City"
-import { CityPicker } from "app/Scenes/City/CityPicker"
-import { MapContainer } from "app/Scenes/Map/MapContainer"
 import { AppRegistry, Platform } from "react-native"
 
 function register(moduleName: string, Component: React.ComponentType<any>) {
@@ -17,24 +13,8 @@ function register(moduleName: string, Component: React.ComponentType<any>) {
 
 const modules: { moduleName: string; module: React.FC<any> }[] = [
   {
-    moduleName: "LocalDiscovery",
-    module: CityGuideView,
-  },
-  {
     moduleName: "LiveAuction",
     module: LiveAuctionView,
-  },
-  {
-    moduleName: "CityPicker",
-    module: CityPicker,
-  },
-  {
-    moduleName: "Map",
-    module: MapContainer,
-  },
-  {
-    moduleName: "City",
-    module: CityView as any,
   },
 ]
 
