c9da9a124b304f1fc8248077b0dd350e39dd1ea5
Mounir Dhahri <soussouheros@gmail.com>
2025-05-26 12:06:27 +0200

    chore: address review feedback

--- src/app/Scenes/PrivacyRequest/PrivacyRequest.tsx
+++ src/app/Scenes/PrivacyRequest/PrivacyRequest.tsx
@@ -10,49 +10,44 @@ export const PrivacyRequest: React.FC = () => {
       title="Personal Data Request"
       contentContainerStyle={{ paddingHorizontal: 0 }}
     >
-      <Content />
-    </MyProfileScreenWrapper>
-  )
-}
-
-export const Content: React.FC = () => {
-  return (
-    <Flex flex={1}>
-      <Spacer y={1} />
-      <Box mx={2}>
-        <Join separator={<Spacer y={2} />}>
-          <Text variant="sm" textAlign="left">
-            Please see Artsy's{" "}
-            <RouterLink to="/privacy" hasChildTouchable>
-              <LinkText>Privacy Policy</LinkText>
-            </RouterLink>{" "}
-            for more information about the information we collect, how we use it, and why we use it.
-          </Text>
-          <Text variant="sm" textAlign="left">
-            To submit a personal data request tap the button below or email{" "}
-            <LinkText
-              onPress={() => presentEmailComposer("privacy@artsy.net", "Personal Data Request")}
+      <Flex flex={1}>
+        <Spacer y={1} />
+        <Box mx={2}>
+          <Join separator={<Spacer y={2} />}>
+            <Text variant="sm" textAlign="left">
+              Please see Artsy's{" "}
+              <RouterLink to="/privacy" hasChildTouchable>
+                <LinkText>Privacy Policy</LinkText>
+              </RouterLink>{" "}
+              for more information about the information we collect, how we use it, and why we use
+              it.
+            </Text>
+            <Text variant="sm" textAlign="left">
+              To submit a personal data request tap the button below or email{" "}
+              <LinkText
+                onPress={() => presentEmailComposer("privacy@artsy.net", "Personal Data Request")}
+              >
+                privacy@artsy.net.
+              </LinkText>{" "}
+            </Text>
+            <Button
+              variant="fillGray"
+              block
+              size="large"
+              mt={1}
+              onPress={() =>
+                presentEmailComposer(
+                  "privacy@artsy.net",
+                  "Personal Data Request",
+                  "Hello, I'm contacting you to ask that..."
+                )
+              }
             >
-              privacy@artsy.net.
-            </LinkText>{" "}
-          </Text>
-          <Button
-            variant="fillGray"
-            block
-            size="large"
-            mt={1}
-            onPress={() =>
-              presentEmailComposer(
-                "privacy@artsy.net",
-                "Personal Data Request",
-                "Hello, I'm contacting you to ask that..."
-              )
-            }
-          >
-            Do not sell my personal information
-          </Button>
-        </Join>
-      </Box>
-    </Flex>
+              Do not sell my personal information
+            </Button>
+          </Join>
+        </Box>
+      </Flex>
+    </MyProfileScreenWrapper>
   )
 }
