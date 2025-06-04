44ff4d1b12df20c35e14874e8f2f7f9634ed3331
brainbicycle <brian.beckerle@artsymail.com>
2025-06-04 10:12:39 -0400

    add purple banner to test updates

--- src/app/Scenes/HomeView/HomeView.tsx
+++ src/app/Scenes/HomeView/HomeView.tsx
@@ -1,5 +1,5 @@
 import { ContextModule, OwnerType } from "@artsy/cohesion"
-import { Flex, Screen, Spinner } from "@artsy/palette-mobile"
+import { Flex, Screen, Spinner, Text } from "@artsy/palette-mobile"
 import { PortalHost } from "@gorhom/portal"
 import { useFocusEffect } from "@react-navigation/native"
 import * as Sentry from "@sentry/react-native"
@@ -171,6 +171,16 @@ export const HomeView: React.FC = memo(() => {
   return (
     <Screen safeArea={true}>
       <Screen.Body fullwidth>
+        <Flex
+          backgroundColor={"purple"}
+          alignContent={"center"}
+          justifyContent="center"
+          alignItems={"center"}
+          p={2}
+          mb={2}
+        >
+          <Text color={"white"}>Expo Updates Works!</Text>
+        </Flex>
         <FlatList
           automaticallyAdjustKeyboardInsets
           keyboardDismissMode="on-drag"
