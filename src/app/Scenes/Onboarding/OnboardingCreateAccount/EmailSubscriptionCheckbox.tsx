96a72d9480cba54d2d7efd9ca6474853928f9fb0
George <g.kartalis7@gmail.com>
2025-06-05 16:48:16 +0200

    chore: adjust checkbox a11y labels to work with maestro and improve a11y (#12226)
    * feat: separate checkbox from children
    
    * fix: improve accessibility for checkboxes in signup flow
    
    * fix: tests

--- src/app/Scenes/Onboarding/OnboardingCreateAccount/EmailSubscriptionCheckbox.tsx
+++ src/app/Scenes/Onboarding/OnboardingCreateAccount/EmailSubscriptionCheckbox.tsx
@@ -1,4 +1,4 @@
-import { Text, Checkbox } from "@artsy/palette-mobile"
+import { Text, Checkbox, Flex } from "@artsy/palette-mobile"
 import { useFeatureFlag } from "app/utils/hooks/useFeatureFlag"
 
 interface EmailSubscriptionCheckboxProps {
@@ -15,25 +15,31 @@ export const EmailSubscriptionCheckbox: React.FC<EmailSubscriptionCheckboxProps>
   const signupLoginFusionEnabled = useFeatureFlag("AREnableSignupLoginFusion")
 
   return (
-    <Checkbox
-      error={error}
-      checked={checked}
-      onPress={() => setChecked(!checked)}
-      mt={0.5}
-      accessibilityLabel="Agree to receive Artsy's emails"
-      accessibilityHint="Check this element to receive Artsy's emails"
-    >
-      {signupLoginFusionEnabled ? (
-        <Text variant="xs">
-          Get Artsy's emails on the art market, products, services, editorial, and promotional
-          content. Unsubscribe at any time.
-        </Text>
-      ) : (
-        <Text variant="xs">
-          Dive deeper into the art market with Artsy emails. Subscribe to hear about our products,
-          services, editorials, and other promotional content. Unsubscribe at any time.
-        </Text>
-      )}
-    </Checkbox>
+    <Flex pr={2} flexDirection="row">
+      <Flex>
+        <Checkbox
+          hitSlop={{ top: 25, bottom: 25, left: 25, right: 25 }}
+          error={error}
+          checked={checked}
+          onPress={() => setChecked(!checked)}
+          mt={0.5}
+          accessibilityLabel="Agree to receive Artsy's emails checkbox"
+          accessibilityHint="Check this element to receive Artsy's emails"
+        />
+      </Flex>
+      <Flex paddingLeft={1} pt={0.5}>
+        {signupLoginFusionEnabled ? (
+          <Text variant="xs">
+            Get Artsy's emails on the art market, products, services, editorial, and promotional
+            content. Unsubscribe at any time.
+          </Text>
+        ) : (
+          <Text variant="xs">
+            Dive deeper into the art market with Artsy emails. Subscribe to hear about our products,
+            services, editorials, and other promotional content. Unsubscribe at any time.
+          </Text>
+        )}
+      </Flex>
+    </Flex>
   )
 }
--- src/app/Scenes/Onboarding/OnboardingCreateAccount/TermsOfServiceCheckbox.tsx
+++ src/app/Scenes/Onboarding/OnboardingCreateAccount/TermsOfServiceCheckbox.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Text } from "@artsy/palette-mobile"
+import { Checkbox, Flex, Text } from "@artsy/palette-mobile"
 import { NavigationProp } from "@react-navigation/native"
 
 interface TermsOfServiceCheckboxProps {
@@ -15,33 +15,39 @@ export const TermsOfServiceCheckbox: React.FC<TermsOfServiceCheckboxProps> = ({
   navigation,
 }) => {
   return (
-    <Checkbox
-      error={error}
-      checked={checked}
-      onPress={() => setChecked(!checked)}
-      mt={0.5}
-      accessibilityLabel="Accept terms and privacy policy"
-      accessibilityHint="Check this element to accept Artsy's terms and privacy policy"
-    >
-      <Text variant="xs" testID="disclaimer">
-        I accept Artsy's{" "}
-        <Text
-          onPress={() => navigation.navigate("OnboardingWebView", { url: "/terms" })}
-          variant="xs"
-          style={{ textDecorationLine: "underline" }}
-        >
-          General Terms and Conditions of Sale
-        </Text>{" "}
-        and{" "}
-        <Text
-          onPress={() => navigation.navigate("OnboardingWebView", { url: "/privacy" })}
-          variant="xs"
-          style={{ textDecorationLine: "underline" }}
-        >
-          Privacy Policy
+    <Flex flexDirection="row" alignItems="center" mr={2}>
+      <Flex>
+        <Checkbox
+          hitSlop={{ top: 25, bottom: 25, left: 25, right: 25 }}
+          error={error}
+          checked={checked}
+          onPress={() => setChecked(!checked)}
+          mt={0.5}
+          accessibilityLabel="Accept terms and privacy policy checkbox"
+          accessibilityHint="Check this element to accept Artsy's terms and privacy policy"
+        />
+      </Flex>
+      <Flex paddingLeft={1} pt={0.5}>
+        <Text variant="xs" testID="disclaimer">
+          I accept Artsy's{" "}
+          <Text
+            onPress={() => navigation.navigate("OnboardingWebView", { url: "/terms" })}
+            variant="xs"
+            style={{ textDecorationLine: "underline" }}
+          >
+            General Terms and Conditions of Sale
+          </Text>{" "}
+          and{" "}
+          <Text
+            onPress={() => navigation.navigate("OnboardingWebView", { url: "/privacy" })}
+            variant="xs"
+            style={{ textDecorationLine: "underline" }}
+          >
+            Privacy Policy
+          </Text>
+          .
         </Text>
-        .
-      </Text>
-    </Checkbox>
+      </Flex>
+    </Flex>
   )
 }
