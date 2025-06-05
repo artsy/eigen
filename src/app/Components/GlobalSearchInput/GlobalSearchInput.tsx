f6cf57392092d6688336452571f890d841a2f028
Mounir Dhahri <soussouheros@gmail.com>
2025-06-05 11:37:44 +0200

    feat: focus on search input after double tap

--- src/app/Components/GlobalSearchInput/GlobalSearchInput.tsx
+++ src/app/Components/GlobalSearchInput/GlobalSearchInput.tsx
@@ -3,55 +3,71 @@ import { Flex, RoundSearchInput, Touchable } from "@artsy/palette-mobile"
 import { GlobalSearchInputOverlay } from "app/Components/GlobalSearchInput/GlobalSearchInputOverlay"
 import { useDismissSearchOverlayOnTabBarPress } from "app/Components/GlobalSearchInput/utils/useDismissSearchOverlayOnTabBarPress"
 import { ICON_HIT_SLOP } from "app/Components/constants"
-import { Fragment, useState } from "react"
+import { useDebouncedValue } from "app/utils/hooks/useDebouncedValue"
+import { forwardRef, Fragment, useImperativeHandle, useState } from "react"
 import { useTracking } from "react-tracking"
 
-export const GlobalSearchInput: React.FC<{
-  ownerType: OwnerType
-}> = ({ ownerType }) => {
-  const [isVisible, setIsVisible] = useState(false)
-  const tracking = useTracking()
+export type GlobalSearchInput = {
+  focus: () => void
+}
 
-  useDismissSearchOverlayOnTabBarPress({ isVisible, ownerType, setIsVisible })
+export const GlobalSearchInput = forwardRef<GlobalSearchInput, { ownerType: OwnerType }>(
+  ({ ownerType }, ref) => {
+    const [isVisible, setIsVisible] = useState(false)
 
-  return (
-    <Fragment>
-      <Touchable
-        accessibilityRole="button"
-        onPress={() => {
-          tracking.trackEvent(
-            tracks.tappedGlobalSearchBar({
-              ownerType,
-            })
-          )
+    const debouncedIsVisible = useDebouncedValue({ value: isVisible })
+
+    const tracking = useTracking()
+
+    useDismissSearchOverlayOnTabBarPress({ isVisible, ownerType, setIsVisible })
+
+    useImperativeHandle(ref, () => ({
+      focus: () => {
+        if (!debouncedIsVisible.debouncedValue) {
           setIsVisible(true)
-        }}
-        hitSlop={ICON_HIT_SLOP}
-        testID="search-button"
-      >
-        {/* In order to make the search input behave like a button here, we wrapped it with a
+        }
+      },
+    }))
+
+    return (
+      <Fragment>
+        <Touchable
+          accessibilityRole="button"
+          onPress={() => {
+            tracking.trackEvent(
+              tracks.tappedGlobalSearchBar({
+                ownerType,
+              })
+            )
+            setIsVisible(true)
+          }}
+          hitSlop={ICON_HIT_SLOP}
+          testID="search-button"
+        >
+          {/* In order to make the search input behave like a button here, we wrapped it with a
          Touchable and set pointerEvents to none. This will prevent the input from receiving
          touch events and make sure they are being handled by the Touchable.
         */}
-        <Flex pointerEvents="none">
-          <RoundSearchInput
-            placeholder="Search Artsy"
-            accessibilityHint="Search artists, artworks, galleries etc."
-            accessibilityLabel="Search artists, artworks, galleries etc."
-            maxLength={55}
-            numberOfLines={1}
-            multiline={false}
-          />
-        </Flex>
-      </Touchable>
-      <GlobalSearchInputOverlay
-        ownerType={ownerType}
-        visible={isVisible}
-        hideModal={() => setIsVisible(false)}
-      />
-    </Fragment>
-  )
-}
+          <Flex pointerEvents="none">
+            <RoundSearchInput
+              placeholder="Search Artsy"
+              accessibilityHint="Search artists, artworks, galleries etc."
+              accessibilityLabel="Search artists, artworks, galleries etc."
+              maxLength={55}
+              numberOfLines={1}
+              multiline={false}
+            />
+          </Flex>
+        </Touchable>
+        <GlobalSearchInputOverlay
+          ownerType={ownerType}
+          visible={isVisible}
+          hideModal={() => setIsVisible(false)}
+        />
+      </Fragment>
+    )
+  }
+)
 
 const tracks = {
   tappedGlobalSearchBar: ({ ownerType }: { ownerType: OwnerType }) => ({
