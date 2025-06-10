8ab935fb5b5c4a11b50e298a7d1ce8a058490446
brainbicycle <brian.beckerle@artsymail.com>
2025-06-10 14:52:14 -0400

    remove test change

--- src/app/Scenes/HomeView/HomeView.tsx
+++ src/app/Scenes/HomeView/HomeView.tsx
@@ -1,5 +1,5 @@
 import { ContextModule, OwnerType } from "@artsy/cohesion"
-import { Flex, Screen, Spinner, Text } from "@artsy/palette-mobile"
+import { Flex, Screen, Spinner } from "@artsy/palette-mobile"
 import { PortalHost } from "@gorhom/portal"
 import { useFocusEffect } from "@react-navigation/native"
 import * as Sentry from "@sentry/react-native"
@@ -171,15 +171,6 @@ export const HomeView: React.FC = memo(() => {
   return (
     <Screen safeArea={true}>
       <Screen.Body fullwidth>
-        <Flex
-          alignContent="center"
-          alignItems="center"
-          justifyContent="center"
-          height={100}
-          backgroundColor="blue"
-        >
-          <Text color="white">Expo Updates QA! Has anything changed?</Text>
-        </Flex>
         <FlatList
           automaticallyAdjustKeyboardInsets
           keyboardDismissMode="on-drag"
