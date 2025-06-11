32f8ed73359902b29e01e32bb079bf4d76ee74e4
brainbicycle <brian.beckerle@artsymail.com>
2025-06-11 15:39:01 -0400

    remove sentry install

--- fastlane/sentry_fastlane.rb
+++ fastlane/sentry_fastlane.rb
@@ -5,8 +5,7 @@ lane :upload_sentry_artifacts do |options|
   sentry_release_name = options[:sentry_release_name]
   platform = options[:platform]
   dist_version = options[:dist_version]
-  sh("yarn add @sentry/cli -D")
-  sentry_cli_path="node_modules/@sentry/cli/bin/sentry-cli"
+  sentry_cli_path="./bin/node_modules/@sentry/cli/bin/sentry-cli"
 
   project_slug = 'eigen'
   org_slug = 'artsynet'
