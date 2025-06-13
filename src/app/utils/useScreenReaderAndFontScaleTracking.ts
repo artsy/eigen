2b2fc6f66c2f660342f26aebc78fa1fcf9a284f2
George Kartalis <g.kartalis7@gmail.com>
2025-06-13 14:25:54 +0200

    refactor: consolidate screenReader and fontscale tracking

--- src/app/utils/useScreenReaderAndFontScaleTracking.ts
+++ src/app/utils/useScreenReaderAndFontScaleTracking.ts
@@ -7,33 +7,21 @@ import { AnalyticsConstants } from "./track/constants"
 // the purpose of this hook is to track the screen reader status of the user's device
 export const useScreenReaderAndFontScaleTracking = () => {
   useEffect(() => {
-    const getScreenReaderStatus = async () => {
+    const getScreenReaderAndFontScaleStatus = async () => {
       const isEnabled = await AccessibilityInfo.isScreenReaderEnabled()
+      const fontScale = await DeviceInfo.getFontScale()
+
       SegmentTrackingProvider.identify?.(undefined, {
         [AnalyticsConstants.ScreenReaderStatus.key]: (() => {
           return isEnabled
             ? AnalyticsConstants.ScreenReaderStatus.value.Enabled
             : AnalyticsConstants.ScreenReaderStatus.value.Disabled
         })(),
+
+        fontScale: fontScale,
       })
     }
 
-    const getFontScale = async () => {
-      try {
-        const fontScale = await DeviceInfo.getFontScale()
-
-        SegmentTrackingProvider.identify?.(undefined, {
-          fontScale: fontScale,
-        })
-
-        return fontScale
-      } catch (error) {
-        console.error("Error getting font scale: ", error)
-      }
-      return
-    }
-
-    getFontScale()
-    getScreenReaderStatus()
+    getScreenReaderAndFontScaleStatus()
   }, [])
 }
