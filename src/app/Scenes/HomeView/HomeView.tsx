f6dc1e16af93027af7c4814398bcb448ef584373
brainbicycle <brian.beckerle@artsymail.com>
2025-06-30 09:32:48 -0400

    add purple banner to test sourcemaps

--- src/app/Scenes/HomeView/HomeView.tsx
+++ src/app/Scenes/HomeView/HomeView.tsx
@@ -1,5 +1,5 @@
 import { ContextModule, OwnerType } from "@artsy/cohesion"
-import { Flex, Screen, Spinner } from "@artsy/palette-mobile"
+import { Flex, Screen, Spinner, Text } from "@artsy/palette-mobile"
 import { PortalHost } from "@gorhom/portal"
 import { useFocusEffect } from "@react-navigation/native"
 import * as Sentry from "@sentry/react-native"
@@ -176,6 +176,9 @@ export const HomeView: React.FC = memo(() => {
   return (
     <Screen safeArea={true}>
       <Screen.Body fullwidth>
+        <Flex height={100} backgroundColor="purple">
+          <Text color="white">Do sourcemaps work?</Text>
+        </Flex>
         <FlatList
           automaticallyAdjustKeyboardInsets
           keyboardDismissMode="on-drag"
