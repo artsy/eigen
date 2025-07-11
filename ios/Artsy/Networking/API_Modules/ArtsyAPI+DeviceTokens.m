6f8325223082768feef5a5f608ab626017f164ca
George Kartalis <g.kartalis7@gmail.com>
2025-07-11 14:29:01 +0200

    fix(attempt): attempt to fix flashy splashscreen
    Co-authored-by: dariakoko <nargle30@gmail.com>

--- ios/Artsy/Networking/API_Modules/ArtsyAPI+DeviceTokens.m
+++ ios/Artsy/Networking/API_Modules/ArtsyAPI+DeviceTokens.m
@@ -10,15 +10,9 @@
 + (AFHTTPRequestOperation *)setAPNTokenForCurrentDevice:(NSString *)token success:(void (^)(id response))success failure:(void (^)(NSError *error))failure
 {
     NSString *name = [[UIDevice currentDevice] name];
-    
+
     if (token && name) {
         NSURLRequest *request = [ARRouter newSetDeviceAPNTokenRequest:token forDevice:name];
-
-        // TODO: Is it a problem that this can be nil?
-        // Should we fail loudly? Wait for the httpClient to be ready?
-        if (!request) {
-            return nil;
-        }
 
         return [ArtsyAPI performRequest:request success:success failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
             if (failure) {
@@ -26,7 +20,7 @@
             }
         }];
     }
-    
+
     return nil;
 }
 
@@ -39,10 +33,6 @@
     }
     NSURLRequest *request = [ARRouter newDeleteDeviceRequest:token];
 
-    if (!request) {
-        completion();
-        return nil;
-    }
     return [ArtsyAPI performRequest:request success:^ (id _) {
         completion();
     } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
