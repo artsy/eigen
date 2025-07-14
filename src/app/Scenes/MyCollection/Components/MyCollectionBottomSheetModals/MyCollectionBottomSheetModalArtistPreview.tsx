b2638bea56b89808aca1944cbf47f988af749964
Daria Kozlova <daria.kozlova@artsymail.com>
2025-07-14 12:28:11 +0200

    fix(PBRW-994, PBRW-974, ONYX-1566): use auto-height bottom sheet for the artist preview modal (#12453)
    * fix: refactor and fix the artist preview bottom sheet component
    
    * chore: cleanup
    
    * chore: dismiss then navigating to the artist screen

--- src/app/Scenes/MyCollection/Components/MyCollectionBottomSheetModals/MyCollectionBottomSheetModalArtistPreview.tsx
+++ src/app/Scenes/MyCollection/Components/MyCollectionBottomSheetModals/MyCollectionBottomSheetModalArtistPreview.tsx
@@ -1,15 +1,24 @@
 import { InfoIcon } from "@artsy/icons/native"
-import { Checkbox, Flex, Join, Message, Spacer, Text, useSpace } from "@artsy/palette-mobile"
-import { BOTTOM_TABS_HEIGHT } from "@artsy/palette-mobile/dist/elements/Screen/StickySubHeader"
+import {
+  Checkbox,
+  Flex,
+  Join,
+  Message,
+  SkeletonBox,
+  SkeletonText,
+  Spacer,
+  Text,
+} from "@artsy/palette-mobile"
 import { useActionSheet } from "@expo/react-native-action-sheet"
 import { BottomSheetView } from "@gorhom/bottom-sheet"
 import { MyCollectionBottomSheetModalArtistPreviewQuery } from "__generated__/MyCollectionBottomSheetModalArtistPreviewQuery.graphql"
 import { MyCollectionBottomSheetModalArtistPreview_artist$data } from "__generated__/MyCollectionBottomSheetModalArtistPreview_artist.graphql"
 import { MyCollectionBottomSheetModalArtistPreview_me$data } from "__generated__/MyCollectionBottomSheetModalArtistPreview_me.graphql"
-import { ArtistListItemContainer, ArtistListItemPlaceholder } from "app/Components/ArtistListItem"
+import { ArtistListItemContainer } from "app/Components/ArtistListItem"
+import { AutomountedBottomSheetModal } from "app/Components/BottomSheet/AutomountedBottomSheetModal"
 import { useToast } from "app/Components/Toast/toastHook"
 import { ArtistKindPills } from "app/Scenes/MyCollection/Components/MyCollectionBottomSheetModals/MyCollectionBottomSheetModalArtistPreview/ArtistKindPills"
-import { MyCollectionTabsStore } from "app/Scenes/MyCollection/State/MyCollectionTabsStore"
+import { SNAP_POINTS } from "app/Scenes/MyCollection/Components/MyCollectionBottomSheetModals/MyCollectionBottomSheetModalArtistsPrompt"
 import { deleteUserInterest } from "app/Scenes/MyCollection/mutations/deleteUserInterest"
 import { updateUserInterest } from "app/Scenes/MyCollection/mutations/updateUserInterest"
 import { getRelayEnvironment } from "app/system/relay/defaultEnvironment"
@@ -25,24 +34,21 @@ interface MyCollectionBottomSheetModalArtistPreviewProps {
   artist: MyCollectionBottomSheetModalArtistPreview_artist$data
   me: MyCollectionBottomSheetModalArtistPreview_me$data
   interestId: string
+  onDismiss: () => void
 }
 
 export const MyCollectionBottomSheetModalArtistPreview: React.FC<
   MyCollectionBottomSheetModalArtistPreviewProps
-> = ({ artist, me, interestId }) => {
-  const artworksCountWithinMyCollection = me?.myCollectionConnection?.totalCount ?? 0
+> = ({ artist, me, interestId, onDismiss }) => {
+  const artworksCountWithinMyCollection = me.myCollectionConnection?.totalCount ?? 0
   const canBeRemoved = artworksCountWithinMyCollection === 0
   const { showActionSheetWithOptions } = useActionSheet()
   const androidCustomSheetStyles = useAndroidActionSheetStyles()
 
   const [isPrivate, setIsPrivate] = useState(me.userInterest?.private ?? false)
 
-  const setViewKind = MyCollectionTabsStore.useStoreActions((actions) => actions.setViewKind)
-
   const toast = useToast()
-  const { bottom } = useSafeAreaInsets()
-
-  const space = useSpace()
+  const safeAreaInset = useSafeAreaInsets()
 
   useDebounce(
     () => {
@@ -70,7 +76,7 @@ export const MyCollectionBottomSheetModalArtistPreview: React.FC<
           backgroundColor: "green100",
         })
         // Hide modal after removing artist
-        setViewKind({ viewKind: null })
+        onDismiss()
 
         refreshMyCollection()
       })
@@ -83,77 +89,72 @@ export const MyCollectionBottomSheetModalArtistPreview: React.FC<
   }
 
   return (
-    <BottomSheetView
-      style={{
-        paddingBottom: bottom + BOTTOM_TABS_HEIGHT + space(2),
-      }}
-    >
-      <Flex px={2} pt={2}>
-        <Join separator={<Spacer y={2} />}>
-          <ArtistListItemContainer
-            artist={artist}
-            disableNavigation={!!artist?.isPersonalArtist}
-            uploadsCount={artworksCountWithinMyCollection}
-            isPrivate={me.userInterest?.private}
-            showFollowButton={!artist?.isPersonalArtist}
-          />
-          <ArtistKindPills artist={artist} />
+    <Flex px={2} pt={2} mb={`${safeAreaInset.bottom}px`}>
+      <Join separator={<Spacer y={2} />}>
+        <ArtistListItemContainer
+          artist={artist}
+          disableNavigation={!!artist?.isPersonalArtist}
+          uploadsCount={artworksCountWithinMyCollection}
+          isPrivate={me.userInterest?.private}
+          showFollowButton={!artist?.isPersonalArtist}
+          onPress={onDismiss}
+        />
+        <ArtistKindPills artist={artist} />
 
-          <Flex flexDirection="row" alignItems="flex-start">
-            <Checkbox
-              checked={!isPrivate}
-              accessibilityState={{ checked: isPrivate }}
-              onPress={() => {
-                setIsPrivate(!isPrivate)
-              }}
-            >
-              <>
-                <Text variant="xs">Share this artist with galleries during inquiries.</Text>
-                <Text variant="xs" color="mono60">
-                  Galleries are more likely to respond if they can see the artists you collect.
-                </Text>
-              </>
-            </Checkbox>
-          </Flex>
-
-          {canBeRemoved ? (
-            <Text
-              onPress={() => {
-                showActionSheetWithOptions(
-                  {
-                    title: "This artist will be removed from My Collection.",
-                    options: ["Remove Artist", "Keep Artist"],
-                    destructiveButtonIndex: 0,
-                    cancelButtonIndex: 1,
-                    useModal: true,
-                    ...androidCustomSheetStyles,
-                  },
-                  (buttonIndex) => {
-                    if (buttonIndex === 0) {
-                      deleteArtist()
-                    }
-                  }
-                )
-              }}
-              color="red100"
-              variant="xs"
-              underline
-            >
-              Remove Artist
-            </Text>
-          ) : (
+        <Flex flexDirection="row" alignItems="flex-start">
+          <Checkbox
+            checked={!isPrivate}
+            accessibilityState={{ checked: isPrivate }}
+            onPress={() => {
+              setIsPrivate(!isPrivate)
+            }}
+          >
             <>
-              <Spacer y={1} />
-              <Message
-                variant="default"
-                title="To remove this artist, please remove their artworks from My Collection first."
-                IconComponent={() => <InfoIcon width={18} height={18} fill="mono100" />}
-              />
+              <Text variant="xs">Share this artist with galleries during inquiries.</Text>
+              <Text variant="xs" color="mono60">
+                Galleries are more likely to respond if they can see the artists you collect.
+              </Text>
             </>
-          )}
-        </Join>
-      </Flex>
-    </BottomSheetView>
+          </Checkbox>
+        </Flex>
+
+        {canBeRemoved ? (
+          <Text
+            onPress={() => {
+              showActionSheetWithOptions(
+                {
+                  title: "This artist will be removed from My Collection.",
+                  options: ["Remove Artist", "Keep Artist"],
+                  destructiveButtonIndex: 0,
+                  cancelButtonIndex: 1,
+                  useModal: true,
+                  ...androidCustomSheetStyles,
+                },
+                (buttonIndex) => {
+                  if (buttonIndex === 0) {
+                    deleteArtist()
+                  }
+                }
+              )
+            }}
+            color="red100"
+            variant="xs"
+            underline
+          >
+            Remove Artist
+          </Text>
+        ) : (
+          <>
+            <Spacer y={1} />
+            <Message
+              variant="default"
+              title="To remove this artist, please remove their artworks from My Collection first."
+              IconComponent={() => <InfoIcon width={18} height={18} fill="mono100" />}
+            />
+          </>
+        )}
+      </Join>
+    </Flex>
   )
 }
 
@@ -184,7 +185,8 @@ export const MyCollectionBottomSheetModalArtistPreviewFragmentContainer = create
 export const MyCollectionBottomSheetModalArtistPreviewQueryRenderer: React.FC<{
   artistID: string
   interestId: string
-}> = ({ artistID, interestId }) => {
+  onDismiss: () => void
+}> = ({ artistID, interestId, onDismiss }) => {
   return (
     <QueryRenderer<MyCollectionBottomSheetModalArtistPreviewQuery>
       environment={getRelayEnvironment()}
@@ -211,6 +213,7 @@ export const MyCollectionBottomSheetModalArtistPreviewQueryRenderer: React.FC<{
         renderFallback: () => null,
         initialProps: {
           interestId,
+          onDismiss,
         },
       })}
     />
@@ -219,14 +222,53 @@ export const MyCollectionBottomSheetModalArtistPreviewQueryRenderer: React.FC<{
 
 const LoadingSkeleton = () => {
   return (
-    <>
-      <BottomSheetView>
-        <Flex px={2} pt={2}>
-          <Join separator={<Spacer y={4} />}>
-            <ArtistListItemPlaceholder />
+    <Flex px={2} pt={2} mb={2}>
+      <Flex flexDirection="row" py={1}>
+        <SkeletonBox height={70} width={70} borderRadius={35} />
+        <Flex pl={1} justifyContent="center">
+          <Join separator={<Spacer y={0.5} />}>
+            <SkeletonText variant="xxs">Artist Name</SkeletonText>
+            <SkeletonText variant="xxs">Artist Data</SkeletonText>
+            <SkeletonText variant="xxs">N artworks uploaded</SkeletonText>
           </Join>
         </Flex>
-      </BottomSheetView>
-    </>
+      </Flex>
+      <Spacer y={1} />
+      <Flex flexDirection="row" gap={1}>
+        <SkeletonBox height={20} borderRadius={10} width={200} />
+        <SkeletonBox height={20} borderRadius={10} width={130} />
+      </Flex>
+
+      {/* Fake other elements to reserve height. We need this to avoid the
+      AutomountedBottomSheetModal blinking when adjusting the height a lot */}
+      <Spacer y={4} />
+      <Spacer y={4} />
+      <Spacer y={4} />
+      <Spacer y={4} />
+      <Spacer y={4} />
+      <Spacer y={4} />
+    </Flex>
   )
 }
+
+export const MyCollectionBottomSheetModal: React.FC<{
+  artistID: string
+  interestId: string
+  onDismiss: () => void
+  visible: boolean
+}> = ({ artistID, interestId, onDismiss, visible }) => (
+  <AutomountedBottomSheetModal
+    visible={visible}
+    snapPoints={SNAP_POINTS}
+    enableDynamicSizing
+    onDismiss={onDismiss}
+  >
+    <BottomSheetView>
+      <MyCollectionBottomSheetModalArtistPreviewQueryRenderer
+        artistID={artistID}
+        interestId={interestId}
+        onDismiss={onDismiss}
+      />
+    </BottomSheetView>
+  </AutomountedBottomSheetModal>
+)
