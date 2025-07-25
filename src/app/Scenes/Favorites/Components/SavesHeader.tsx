741c11317a84a48d3496a3ed45bec8236874dae6
Sultan <sultan.al-maari@artsymail.com>
2025-07-25 15:46:20 +0200

    migrate AddIcon

--- src/app/Scenes/Favorites/Components/SavesHeader.tsx
+++ src/app/Scenes/Favorites/Components/SavesHeader.tsx
@@ -1,4 +1,5 @@
-import { Flex, AddIcon, TagIcon, Touchable, Text, Spacer, Join } from "@artsy/palette-mobile"
+import { AddIcon } from "@artsy/icons/native"
+import { Flex, TagIcon, Touchable, Text, Spacer, Join } from "@artsy/palette-mobile"
 import { ArtworkListsStore } from "app/Components/ArtworkLists/ArtworkListsStore"
 import { ProgressiveOnboardingOfferSettings } from "app/Components/ProgressiveOnboarding/ProgressiveOnboardingOfferSettings"
 import { useFavoritesTracking } from "app/Scenes/Favorites/useFavoritesTracking"
