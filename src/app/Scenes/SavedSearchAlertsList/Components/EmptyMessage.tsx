b37d4b70e2a174a6c30fe69faaa15db518accc5f
Sultan <sultan.al-maari@artsymail.com>
2025-07-25 15:32:15 +0200

    migrate FilterIcon

--- src/app/Scenes/SavedSearchAlertsList/Components/EmptyMessage.tsx
+++ src/app/Scenes/SavedSearchAlertsList/Components/EmptyMessage.tsx
@@ -1,14 +1,5 @@
-import { BellStrokeIcon } from "@artsy/icons/native"
-import {
-  ArtworkIcon,
-  Box,
-  Button,
-  FilterIcon,
-  Flex,
-  Join,
-  Spacer,
-  Text,
-} from "@artsy/palette-mobile"
+import { BellStrokeIcon, FilterIcon } from "@artsy/icons/native"
+import { ArtworkIcon, Box, Button, Flex, Join, Spacer, Text } from "@artsy/palette-mobile"
 import SearchIcon from "app/Components/Icons/SearchIcon"
 import { RouterLink } from "app/system/navigation/RouterLink"
 import { ScrollView } from "react-native"
