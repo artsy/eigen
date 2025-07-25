79e4054f5990545dc911079d759e5522e2dd35b4
Sultan <sultan.al-maari@artsymail.com>
2025-07-25 17:16:26 +0200

    migrate FacebookIcon

--- src/app/Scenes/Fair/Components/FairEditorialShare.tsx
+++ src/app/Scenes/Fair/Components/FairEditorialShare.tsx
@@ -1,5 +1,5 @@
-import { EnvelopeIcon } from "@artsy/icons/native"
-import { FacebookIcon, TwitterIcon, Box, BoxProps, Touchable } from "@artsy/palette-mobile"
+import { EnvelopeIcon, FacebookIcon } from "@artsy/icons/native"
+import { TwitterIcon, Box, BoxProps, Touchable } from "@artsy/palette-mobile"
 import { Linking } from "react-native"
 
 interface FairEditorialShareProps extends BoxProps {
