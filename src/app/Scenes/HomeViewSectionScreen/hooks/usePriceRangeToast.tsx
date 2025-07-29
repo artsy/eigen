9e19c6a608772c1923498b380326133ca6187598
dariakoko <nargle30@gmail.com>
2025-07-29 14:38:33 +0200

    feat: add price range availability check

--- src/app/Scenes/HomeViewSectionScreen/hooks/usePriceRangeToast.tsx
+++ src/app/Scenes/HomeViewSectionScreen/hooks/usePriceRangeToast.tsx
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import {
+  fetchPriceRange,
   PROGRESSIVE_ONBOARDING_PRICE_RANGE_CONTENT,
   PROGRESSIVE_ONBOARDING_PRICE_RANGE_TITLE,
 } from "app/Components/ProgressiveOnboarding/ProgressiveOnboardingPriceRangeHome"
@@ -25,48 +26,52 @@ export function usePriceRangeToast({
   const hasShownPriceRangeToast = useRef(false)
 
   useEffect(() => {
-    const hasScrolledEnough = totalCount > 0 && artworksLength / totalCount > 0.8
-
-    if (
-      sectionInternalID !== "home-view-section-recommended-artworks" ||
-      totalCount === 0 ||
-      !hasScrolledEnough ||
-      hasShownPriceRangeToast.current
-    ) {
+    // display toast only for the "Recommended Artworks" section
+    if (sectionInternalID !== "home-view-section-recommended-artworks") {
       return
     }
 
-    hasShownPriceRangeToast.current = true // prevent parallel triggers
+    fetchPriceRange().then((result) => {
+      if (result === false) {
+        const hasScrolledEnough = totalCount > 0 && artworksLength / totalCount > 0.8
 
-    const maybeShowToast = async () => {
-      try {
-        const lastShown = await AsyncStorage.getItem(PRICE_RANGE_TOAST_KEY)
-        const lastShownDate = new Date(Number(lastShown || "0"))
-        const now = new Date()
-
-        const hasThreeMonthsPassed = now.getTime() - lastShownDate.getTime() > THREE_MONTHS_MS
-
-        if (!hasThreeMonthsPassed) {
+        if (totalCount === 0 || !hasScrolledEnough || hasShownPriceRangeToast.current) {
           return
         }
 
-        await AsyncStorage.setItem(PRICE_RANGE_TOAST_KEY, String(now.getTime()))
+        hasShownPriceRangeToast.current = true // prevent parallel triggers
 
-        toast.show(PROGRESSIVE_ONBOARDING_PRICE_RANGE_TITLE, "bottom", {
-          description: PROGRESSIVE_ONBOARDING_PRICE_RANGE_CONTENT,
-          duration: "long",
-          onPress: () => {
-            InteractionManager.runAfterInteractions(() => {
-              navigate("my-account/edit-price-range")
+        const maybeShowToast = async () => {
+          try {
+            const lastShown = await AsyncStorage.getItem(PRICE_RANGE_TOAST_KEY)
+            const lastShownDate = new Date(Number(lastShown || "0"))
+            const now = new Date()
+
+            const hasThreeMonthsPassed = now.getTime() - lastShownDate.getTime() > THREE_MONTHS_MS
+
+            if (!hasThreeMonthsPassed) {
+              return
+            }
+
+            await AsyncStorage.setItem(PRICE_RANGE_TOAST_KEY, String(now.getTime()))
+
+            toast.show(PROGRESSIVE_ONBOARDING_PRICE_RANGE_TITLE, "bottom", {
+              description: PROGRESSIVE_ONBOARDING_PRICE_RANGE_CONTENT,
+              duration: "long",
+              onPress: () => {
+                InteractionManager.runAfterInteractions(() => {
+                  navigate("my-account/edit-price-range")
+                })
+              },
+              hideOnPress: true,
             })
-          },
-          hideOnPress: true,
-        })
-      } catch (e) {
-        console.error("Failed to maybe show price range toast", e)
-      }
-    }
+          } catch (e) {
+            console.error("Failed to maybe show price range toast", e)
+          }
+        }
 
-    maybeShowToast()
+        maybeShowToast()
+      } else return
+    })
   }, [artworksLength, totalCount, sectionInternalID, toast])
 }
