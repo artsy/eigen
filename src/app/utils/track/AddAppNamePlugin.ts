bff549c30dbc8946f4295fd3bd4e378e358d76b4
brainbicycle <brian.beckerle@artsymail.com>
2025-07-30 11:23:58 -0300

    pr review: cleaner name setting

--- src/app/utils/track/AddAppNamePlugin.ts
+++ src/app/utils/track/AddAppNamePlugin.ts
@@ -4,16 +4,9 @@ export class AddAppNamePlugin extends Plugin {
   type = PluginType.enrichment
 
   async execute(event: SegmentEvent) {
-    if (!event.context) {
-      event.context = {}
-    }
-
-    if (!event.context.app) {
-      event.context.app = {}
-    }
-
+    event.context = event.context || {}
+    event.context.app = event.context.app || {}
     event.context.app.name = "eigen"
-
     return event
   }
 }
