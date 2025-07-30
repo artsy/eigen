c828f1ea645cb812ad3a840d281c27f66ccc0f96
araujobarret <araujobarret@gmail.com>
2025-07-30 15:54:52 +0200

    fix: remove withSuspense overlapping the screen

--- src/app/Scenes/InfiniteDiscovery/Components/InfiniteDiscoveryNegativeSignalsBottomSheet.tsx
+++ src/app/Scenes/InfiniteDiscovery/Components/InfiniteDiscoveryNegativeSignalsBottomSheet.tsx
@@ -1,15 +1,13 @@
 import { useColor } from "@artsy/palette-mobile"
 import BottomSheet from "@gorhom/bottom-sheet"
 import { InfiniteDiscoveryNegativeSignalsBottomSheetQuery } from "__generated__/InfiniteDiscoveryNegativeSignalsBottomSheetQuery.graphql"
-import { LoadFailureView } from "app/Components/LoadFailureView"
 import { InfiniteDiscoveryBottomSheetBackdrop } from "app/Scenes/InfiniteDiscovery/Components/InfiniteDiscoveryBottomSheetBackdrop"
 import {
   InfiniteDiscoveryNegativeSignals,
   InfiniteDiscoveryNegativeSignalsPlaceholder,
 } from "app/Scenes/InfiniteDiscovery/Components/InfiniteDiscoveryNegativeSignals"
 import { GlobalStore } from "app/store/GlobalStore"
-import { withSuspense } from "app/utils/hooks/withSuspense"
-import { FC, useRef } from "react"
+import { FC, Suspense, useRef } from "react"
 import { graphql, useLazyLoadQuery } from "react-relay"
 
 interface NegativeSignalsBottomSheetProps {
@@ -43,51 +41,44 @@ interface InfiniteDiscoveryNegativeSignalsBottomSheetProps {
   artworkID: string
 }
 
-export const InfiniteDiscoveryNegativeSignalsBottomSheet =
-  withSuspense<InfiniteDiscoveryNegativeSignalsBottomSheetProps>({
-    Component: ({ artworkID }) => {
-      const color = useColor()
-      const ref = useRef<BottomSheet>(null)
-      const moreInfoSheetVisible = GlobalStore.useAppState(
-        (state) => state.infiniteDiscovery.sessionState.moreInfoSheetVisible
-      )
-      const { setMoreInfoSheetVisible } = GlobalStore.actions.infiniteDiscovery
+export const InfiniteDiscoveryNegativeSignalsBottomSheet: FC<
+  InfiniteDiscoveryNegativeSignalsBottomSheetProps
+> = ({ artworkID }) => {
+  const color = useColor()
+  const ref = useRef<BottomSheet>(null)
+  const moreInfoSheetVisible = GlobalStore.useAppState(
+    (state) => state.infiniteDiscovery.sessionState.moreInfoSheetVisible
+  )
+  const { setMoreInfoSheetVisible } = GlobalStore.actions.infiniteDiscovery
 
-      const handleOnSheetChange = (index: number) => {
-        if (index === 0 && moreInfoSheetVisible) {
-          setMoreInfoSheetVisible(false)
-        }
-      }
+  const handleOnSheetChange = (index: number) => {
+    if (index === 0 && moreInfoSheetVisible) {
+      setMoreInfoSheetVisible(false)
+    }
+  }
 
-      if (!moreInfoSheetVisible) {
-        return null
-      }
+  if (!moreInfoSheetVisible) {
+    return null
+  }
 
-      return (
-        <BottomSheet
-          ref={ref}
-          enableDynamicSizing={false}
-          enablePanDownToClose={true}
-          snapPoints={[1, 260]}
-          onChange={handleOnSheetChange}
-          index={1}
-          backgroundStyle={{
-            backgroundColor: color("mono0"),
-          }}
-          backdropComponent={(props) => (
-            <InfiniteDiscoveryBottomSheetBackdrop
-              {...props}
-              disappearsOnIndex={0}
-              appearsOnIndex={1}
-            />
-          )}
-        >
-          <NegativeSignalsBottomSheet artworkID={artworkID} />
-        </BottomSheet>
-      )
-    },
-    LoadingFallback: InfiniteDiscoveryNegativeSignalsPlaceholder,
-    ErrorFallback: () => {
-      return <LoadFailureView />
-    },
-  })
+  return (
+    <BottomSheet
+      ref={ref}
+      enableDynamicSizing={false}
+      enablePanDownToClose={true}
+      snapPoints={[1, 260]}
+      onChange={handleOnSheetChange}
+      index={1}
+      backgroundStyle={{
+        backgroundColor: color("mono0"),
+      }}
+      backdropComponent={(props) => (
+        <InfiniteDiscoveryBottomSheetBackdrop {...props} disappearsOnIndex={0} appearsOnIndex={1} />
+      )}
+    >
+      <Suspense fallback={<InfiniteDiscoveryNegativeSignalsPlaceholder />}>
+        <NegativeSignalsBottomSheet artworkID={artworkID} />
+      </Suspense>
+    </BottomSheet>
+  )
+}
