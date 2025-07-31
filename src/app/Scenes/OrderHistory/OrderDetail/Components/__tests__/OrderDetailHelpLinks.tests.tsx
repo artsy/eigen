20fcf4dc644cdb16f6976c65267186d4dfce6f4c
Sultan <sultan.al-maari@artsymail.com>
2025-07-31 19:05:30 +0200

    feat(EMI-2508): Add ask a question modal in Order Details

--- src/app/Scenes/OrderHistory/OrderDetail/Components/__tests__/OrderDetailHelpLinks.tests.tsx
+++ src/app/Scenes/OrderHistory/OrderDetail/Components/__tests__/OrderDetailHelpLinks.tests.tsx
@@ -1,4 +1,5 @@
 import { fireEvent, screen } from "@testing-library/react-native"
+import { OrderDetailHelpLinksTestsQuery } from "__generated__/OrderDetailHelpLinksTestsQuery.graphql"
 import { OrderDetailHelpLinks } from "app/Scenes/OrderHistory/OrderDetail/Components/OrderDetailHelpLinks"
 import { navigate } from "app/system/navigation/navigate"
 import { mockTrackEvent } from "app/utils/tests/globallyMockedStuff"
@@ -6,11 +7,12 @@ import { setupTestWrapper } from "app/utils/tests/setupTestWrapper"
 import { graphql } from "react-relay"
 
 describe("OrderDetailHelpLinks", () => {
-  const { renderWithRelay } = setupTestWrapper({
-    Component: (props: any) => <OrderDetailHelpLinks order={props.me.order} {...props} />,
+  const { renderWithRelay } = setupTestWrapper<OrderDetailHelpLinksTestsQuery>({
+    Component: (props) => <OrderDetailHelpLinks order={props.me!.order!} me={props.me!} />,
     query: graphql`
       query OrderDetailHelpLinksTestsQuery @relay_test_operation {
         me {
+          ...OrderDetailHelpLinks_me
           order(id: "order-id") {
             ...OrderDetailHelpLinks_order
           }
@@ -19,7 +21,7 @@ describe("OrderDetailHelpLinks", () => {
     `,
   })
 
-  it("renders the help link section", () => {
+  it("navigates to the help link", () => {
     renderWithRelay({
       Order: () => ({ mode: "BUY", source: "PARTNER_OFFER", internalID: "order-id" }),
     })
@@ -40,6 +42,31 @@ describe("OrderDetailHelpLinks", () => {
         "destination_screen_owner_slug": "0TO3b000000UessGAC/buy",
         "destination_screen_owner_type": "articles",
         "flow": "Partner offer",
+      },
+    ]
+  `)
+  })
+
+  it("opens ask specialist modal", () => {
+    renderWithRelay({ Order: () => ({ mode: "OFFER", internalID: "order-id" }) })
+
+    expect(screen.getByText("ask a question")).toBeOnTheScreen()
+    fireEvent.press(screen.getByText("ask a question"))
+
+    expect(
+      screen.getByText(
+        "An Artsy Specialist is available to answer your questions and help you collect through Artsy."
+      )
+    ).toBeOnTheScreen()
+    expect(mockTrackEvent).toHaveBeenCalledTimes(1)
+    expect(mockTrackEvent.mock.calls[0]).toMatchInlineSnapshot(`
+    [
+      {
+        "action": "tappedAskSpecialist",
+        "context_module": "ordersDetail",
+        "context_screen_owner_id": "order-id",
+        "context_screen_owner_type": "orders-detail",
+        "flow": "Make offer",
       },
     ]
   `)
