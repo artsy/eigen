a093e4a62d7c5914605441479f7f395b6013ce4b
Mounir Dhahri <soussouheros@gmail.com>
2025-08-04 12:20:58 +0200

    fix: bring back missing artworks from collection screen (#12576)
    chore: remove missing artworks from collection screen

--- src/app/utils/masonryHelpers/index.tsx
+++ src/app/utils/masonryHelpers/index.tsx
@@ -8,7 +8,7 @@ export const NUM_COLUMNS_MASONRY = isTablet() ? 3 : 2
 
 export const ON_END_REACHED_THRESHOLD_MASONRY = 2
 
-export const MASONRY_LIST_PAGE_SIZE = 15
+export const MASONRY_LIST_PAGE_SIZE = 10
 
 export interface masonryRenderItemProps {
   item: MasonryArtworkItem
