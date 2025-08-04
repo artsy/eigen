5cea5c8f88847fddab2093eff5e9c4af7b0bf204
George Kartalis <g.kartalis7@gmail.com>
2025-08-04 15:58:25 +0200

    fix: remove unnecessary Screen wrapper from onboardingwebview component

--- src/app/Scenes/Onboarding/OnboardingWebView.tsx
+++ src/app/Scenes/Onboarding/OnboardingWebView.tsx
@@ -1,4 +1,3 @@
-import { Screen } from "@artsy/palette-mobile"
 import { StackScreenProps } from "@react-navigation/stack"
 import { ArtsyWebViewPage } from "app/Components/ArtsyWebView"
 import { OnboardingNavigationStack } from "./Onboarding"
@@ -11,9 +10,5 @@ export type OnboardingWebViewProps = StackScreenProps<
 export type OnboardingWebViewRoute = "/privacy" | "/terms" | "/conditions-of-sale"
 
 export const OnboardingWebView: React.FC<OnboardingWebViewProps> = ({ route, navigation }) => {
-  return (
-    <Screen>
-      <ArtsyWebViewPage url={route.params.url} backAction={() => navigation.goBack()} />
-    </Screen>
-  )
+  return <ArtsyWebViewPage url={route.params.url} backAction={() => navigation.goBack()} />
 }
