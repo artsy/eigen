f3c7fb89479abebc532bd3cd7b8e545832a899c5
George Kartalis <g.kartalis7@gmail.com>
2025-09-02 18:30:12 +0200

    fix(prelogin): fix modal screens navigationBar color to match theme

--- src/app/Scenes/Onboarding/Auth2/AuthApp.tsx
+++ src/app/Scenes/Onboarding/Auth2/AuthApp.tsx
@@ -3,14 +3,8 @@ import { AuthContext } from "app/Scenes/Onboarding/Auth2/AuthContext"
 import { AuthScenes } from "app/Scenes/Onboarding/Auth2/AuthScenes"
 import { AuthBackground } from "app/Scenes/Onboarding/Auth2/components/AuthBackground"
 import { AuthModal } from "app/Scenes/Onboarding/Auth2/components/AuthModal"
-import { useAndroidStatusBarColor } from "app/Scenes/Onboarding/Auth2/hooks/useAndroidStatusBarColor"
-import { useSwitchStatusBarStyle } from "app/utils/useStatusBarStyle"
-import React from "react"
 
 export const AuthApp: React.FC = () => {
-  useSwitchStatusBarStyle("light-content", "dark-content")
-  useAndroidStatusBarColor()
-
   return (
     <Screen safeArea={false}>
       <AuthContext.Provider>
