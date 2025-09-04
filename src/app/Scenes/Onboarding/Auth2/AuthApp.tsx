807fba6e367680a81874d19347b754dca21075ba
George <g.kartalis7@gmail.com>
2025-09-04 15:47:13 +0200

    fix: android statusbar pre login (#12701)
    * chore: bring back useAndroidStatusBar hook
    
    * fix: android statusbar prelogin

--- src/app/Scenes/Onboarding/Auth2/AuthApp.tsx
+++ src/app/Scenes/Onboarding/Auth2/AuthApp.tsx
@@ -3,10 +3,12 @@ import { AuthContext } from "app/Scenes/Onboarding/Auth2/AuthContext"
 import { AuthScenes } from "app/Scenes/Onboarding/Auth2/AuthScenes"
 import { AuthBackground } from "app/Scenes/Onboarding/Auth2/components/AuthBackground"
 import { AuthModal } from "app/Scenes/Onboarding/Auth2/components/AuthModal"
+import { StatusBar } from "react-native"
 
 export const AuthApp: React.FC = () => {
   return (
     <Screen safeArea={false}>
+      <StatusBar translucent />
       <AuthContext.Provider>
         <Screen.Background>
           <AuthBackground />
