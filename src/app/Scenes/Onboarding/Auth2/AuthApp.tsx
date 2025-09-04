0da17b892c22f914f49a8812410d2f18840d7a01
George Kartalis <g.kartalis7@gmail.com>
2025-09-04 14:50:44 +0200

    chore: bring back useAndroidStatusBar hook

--- src/app/Scenes/Onboarding/Auth2/AuthApp.tsx
+++ src/app/Scenes/Onboarding/Auth2/AuthApp.tsx
@@ -3,8 +3,11 @@ import { AuthContext } from "app/Scenes/Onboarding/Auth2/AuthContext"
 import { AuthScenes } from "app/Scenes/Onboarding/Auth2/AuthScenes"
 import { AuthBackground } from "app/Scenes/Onboarding/Auth2/components/AuthBackground"
 import { AuthModal } from "app/Scenes/Onboarding/Auth2/components/AuthModal"
+import { useAndroidStatusBarColor } from "app/Scenes/Onboarding/Auth2/hooks/useAndroidStatusBarColor"
 
 export const AuthApp: React.FC = () => {
+  useAndroidStatusBarColor()
+
   return (
     <Screen safeArea={false}>
       <AuthContext.Provider>
