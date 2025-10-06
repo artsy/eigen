eb6434dfcc4ea5d99aba4d06463e281a0d1c9e06
brainbicycle <brian.beckerle@artsymail.com>
2025-10-06 14:09:30 -0400

    move buttons outside scrollview to fix keyboard adjustment, adjust spacing

--- src/app/Scenes/MyAccount/MyAccountDeleteAccount.tsx
+++ src/app/Scenes/MyAccount/MyAccountDeleteAccount.tsx
@@ -1,6 +1,6 @@
 import { OwnerType } from "@artsy/cohesion"
 import { GavelIcon, GenomeIcon } from "@artsy/icons/native"
-import { Box, Button, Flex, Input, Spacer, Text, useSpace } from "@artsy/palette-mobile"
+import { Button, Flex, Input, Spacer, Text, useSpace } from "@artsy/palette-mobile"
 import { useNavigation } from "@react-navigation/native"
 import { MyAccountDeleteAccountQuery } from "__generated__/MyAccountDeleteAccountQuery.graphql"
 import { MyAccountDeleteAccount_me$data } from "__generated__/MyAccountDeleteAccount_me.graphql"
@@ -45,15 +45,13 @@ const MyAccountDeleteAccount: React.FC<MyAccountDeleteAccountProps> = ({ me: { h
         behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
         <ScrollView
-          contentContainerStyle={{ padding: space(2), paddingBottom: space(12) }}
+          contentContainerStyle={{ padding: space(2), paddingBottom: space(2) }}
           keyboardDismissMode="on-drag"
           keyboardShouldPersistTaps="handled"
         >
           <Flex>
-            <Text variant="lg-display" mt="6">
-              Delete My Account
-            </Text>
-            <Spacer y={4} />
+            <Text variant="lg-display">Delete My Account</Text>
+            <Spacer y={2} />
             <Text>Are you sure you want to delete your account?</Text>
             <Spacer y={2} />
             <Text>If you delete your account:</Text>
@@ -103,50 +101,53 @@ const MyAccountDeleteAccount: React.FC<MyAccountDeleteAccountProps> = ({ me: { h
                 <Spacer y={2} />
               </>
             )}
-            <Button
-              block
-              disabled={!enableDelete}
-              loading={loading}
-              onPress={async () => {
-                try {
-                  setLoading(true)
-                  setError("")
-                  const input: DeleteAccountInput = {
-                    explanation,
-                    password,
-                  }
-                  const result = await deleteUserAccount(input)
-                  const mutationError =
-                    result.deleteMyAccountMutation?.userAccountOrError?.mutationError
-
-                  //  Failed to request account deletion
-                  if (mutationError?.message) {
-                    setError(mutationError?.message)
-                    return
-                  }
-                  //  Account deleted request submitted successfully
-                  Alert.alert(
-                    "Account deletion request submitted!",
-                    "It may take up to 7 days to fully delete and remove all of your data."
-                  )
-                  InteractionManager.runAfterInteractions(() => {
-                    GlobalStore.actions.auth.signOut()
-                  })
-                } catch (e: any) {
-                  setError("Something went wrong")
-                } finally {
-                  setLoading(false)
-                }
-              }}
-            >
-              Delete My Account
-            </Button>
-            <Spacer y={1} />
-            <Button block variant="outline" onPress={() => navigation.goBack()}>
-              Cancel
-            </Button>
           </Flex>
         </ScrollView>
+        <Flex m={1}>
+          <Button
+            block
+            disabled={!enableDelete}
+            loading={loading}
+            onPress={async () => {
+              try {
+                setLoading(true)
+                setError("")
+                const input: DeleteAccountInput = {
+                  explanation,
+                  password,
+                }
+                const result = await deleteUserAccount(input)
+                const mutationError =
+                  result.deleteMyAccountMutation?.userAccountOrError?.mutationError
+
+                //  Failed to request account deletion
+                if (mutationError?.message) {
+                  setError(mutationError?.message)
+                  return
+                }
+                //  Account deleted request submitted successfully
+                Alert.alert(
+                  "Account deletion request submitted!",
+                  "It may take up to 7 days to fully delete and remove all of your data."
+                )
+                InteractionManager.runAfterInteractions(() => {
+                  GlobalStore.actions.auth.signOut()
+                })
+              } catch (e: any) {
+                setError("Something went wrong")
+              } finally {
+                setLoading(false)
+              }
+            }}
+          >
+            Delete My Account
+          </Button>
+
+          <Spacer y={1} />
+          <Button block variant="outline" onPress={() => navigation.goBack()}>
+            Cancel
+          </Button>
+        </Flex>
       </KeyboardAvoidingView>
     </ProvideScreenTrackingWithCohesionSchema>
   )
