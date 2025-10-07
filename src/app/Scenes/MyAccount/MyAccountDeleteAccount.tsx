52672f82c6453930d5f1d8f1f4816250309019c2
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2025-10-07 08:24:16 -0400

    fix: keyboard blocks password field in account delete (#12818)
    * add props from credit card form
    
    * move buttons outside scrollview to fix keyboard adjustment, adjust spacing
    
    * remove redundant space
    
    Co-authored-by: Sultan Al-Maari <sltun2oo9@gmail.com>
    
    ---------
    
    Co-authored-by: Sultan Al-Maari <sltun2oo9@gmail.com>

--- src/app/Scenes/MyAccount/MyAccountDeleteAccount.tsx
+++ src/app/Scenes/MyAccount/MyAccountDeleteAccount.tsx
@@ -1,6 +1,6 @@
 import { OwnerType } from "@artsy/cohesion"
 import { GavelIcon, GenomeIcon } from "@artsy/icons/native"
-import { Box, Button, Flex, Input, Spacer, Text } from "@artsy/palette-mobile"
+import { Button, Flex, Input, Spacer, Text, useSpace } from "@artsy/palette-mobile"
 import { useNavigation } from "@react-navigation/native"
 import { MyAccountDeleteAccountQuery } from "__generated__/MyAccountDeleteAccountQuery.graphql"
 import { MyAccountDeleteAccount_me$data } from "__generated__/MyAccountDeleteAccount_me.graphql"
@@ -11,7 +11,7 @@ import renderWithLoadProgress from "app/utils/renderWithLoadProgress"
 import { ProvideScreenTrackingWithCohesionSchema } from "app/utils/track"
 import { screen } from "app/utils/track/helpers"
 import React, { useState } from "react"
-import { Alert, InteractionManager, KeyboardAvoidingView, ScrollView } from "react-native"
+import { Alert, InteractionManager, KeyboardAvoidingView, Platform, ScrollView } from "react-native"
 import { createFragmentContainer, graphql, QueryRenderer } from "react-relay"
 import { deleteUserAccount } from "./deleteUserAccount"
 
@@ -26,6 +26,7 @@ const MyAccountDeleteAccount: React.FC<MyAccountDeleteAccountProps> = ({ me: { h
   const [error, setError] = useState<string>("")
   const [explanation, setExplanation] = useState<string>("")
   const [password, setPassword] = useState<string>("")
+  const space = useSpace()
 
   const navigation = useNavigation()
 
@@ -39,13 +40,18 @@ const MyAccountDeleteAccount: React.FC<MyAccountDeleteAccountProps> = ({ me: { h
         context_screen_owner_type: OwnerType.accountDeleteMyAccount,
       })}
     >
-      <KeyboardAvoidingView style={{ flex: 1 }}>
-        <ScrollView>
-          <Box pr={2} pl={2}>
-            <Text variant="lg-display" mt="6">
-              Delete My Account
-            </Text>
-            <Spacer y={4} />
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <ScrollView
+          contentContainerStyle={{ padding: space(2) }}
+          keyboardDismissMode="on-drag"
+          keyboardShouldPersistTaps="handled"
+        >
+          <Flex>
+            <Text variant="lg-display">Delete My Account</Text>
+            <Spacer y={2} />
             <Text>Are you sure you want to delete your account?</Text>
             <Spacer y={2} />
             <Text>If you delete your account:</Text>
@@ -95,50 +101,53 @@ const MyAccountDeleteAccount: React.FC<MyAccountDeleteAccountProps> = ({ me: { h
                 <Spacer y={2} />
               </>
             )}
-            <Button
-              block
-              disabled={!enableDelete}
-              loading={loading}
-              onPress={async () => {
-                try {
-                  setLoading(true)
-                  setError("")
-                  const input: DeleteAccountInput = {
-                    explanation,
-                    password,
-                  }
-                  const result = await deleteUserAccount(input)
-                  const mutationError =
-                    result.deleteMyAccountMutation?.userAccountOrError?.mutationError
-
-                  //  Failed to request account deletion
-                  if (mutationError?.message) {
-                    setError(mutationError?.message)
-                    return
-                  }
-                  //  Account deleted request submitted successfully
-                  Alert.alert(
-                    "Account deletion request submitted!",
-                    "It may take up to 7 days to fully delete and remove all of your data."
-                  )
-                  InteractionManager.runAfterInteractions(() => {
-                    GlobalStore.actions.auth.signOut()
-                  })
-                } catch (e: any) {
-                  setError("Something went wrong")
-                } finally {
-                  setLoading(false)
-                }
-              }}
-            >
-              Delete My Account
-            </Button>
-            <Spacer y={1} />
-            <Button block variant="outline" onPress={() => navigation.goBack()}>
-              Cancel
-            </Button>
-          </Box>
+          </Flex>
         </ScrollView>
+        <Flex m={1}>
+          <Button
+            block
+            disabled={!enableDelete}
+            loading={loading}
+            onPress={async () => {
+              try {
+                setLoading(true)
+                setError("")
+                const input: DeleteAccountInput = {
+                  explanation,
+                  password,
+                }
+                const result = await deleteUserAccount(input)
+                const mutationError =
+                  result.deleteMyAccountMutation?.userAccountOrError?.mutationError
+
+                //  Failed to request account deletion
+                if (mutationError?.message) {
+                  setError(mutationError?.message)
+                  return
+                }
+                //  Account deleted request submitted successfully
+                Alert.alert(
+                  "Account deletion request submitted!",
+                  "It may take up to 7 days to fully delete and remove all of your data."
+                )
+                InteractionManager.runAfterInteractions(() => {
+                  GlobalStore.actions.auth.signOut()
+                })
+              } catch (e: any) {
+                setError("Something went wrong")
+              } finally {
+                setLoading(false)
+              }
+            }}
+          >
+            Delete My Account
+          </Button>
+
+          <Spacer y={1} />
+          <Button block variant="outline" onPress={() => navigation.goBack()}>
+            Cancel
+          </Button>
+        </Flex>
       </KeyboardAvoidingView>
     </ProvideScreenTrackingWithCohesionSchema>
   )
