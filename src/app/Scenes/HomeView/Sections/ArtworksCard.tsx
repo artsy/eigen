f7034be6019d301e7ca4e6f08e723e140e090c70
dariakoko <nargle30@gmail.com>
2025-10-10 11:21:36 +0200

    feat: adjust for teblets, refactor

--- src/app/Scenes/HomeView/Sections/ArtworksCard.tsx
+++ src/app/Scenes/HomeView/Sections/ArtworksCard.tsx
@@ -1,103 +1,94 @@
 import { Flex, Image, useSpace, useScreenDimensions } from "@artsy/palette-mobile"
 import { RouterLink } from "app/system/navigation/RouterLink"
 import { ArtworkActionTrackingProps } from "app/utils/track/ArtworkActions"
+import { isTablet } from "react-native-device-info"
 import { graphql, useFragment } from "react-relay"
 
 interface ArtworksCardProps extends ArtworkActionTrackingProps {
   artworks: any
 }
+
 export const ArtworksCard: React.FC<ArtworksCardProps> = ({ artworks }) => {
   const artworksData = useFragment(artworksCard, artworks)
   const space = useSpace()
   const { width, height } = useScreenDimensions()
+  const isIPad = isTablet()
 
-  const layout = artworksData[0].image.aspectRatio < 1 ? "horizontal" : "vertical"
+  const isLeadingImageVertical = artworksData[0].image.aspectRatio < 1 || isIPad
 
-  const cardHeight = height / 2.5
-  const cardWidth = width - space(2) - space(2)
+  const cardHeight = isIPad ? height / 2 : height / 2.5
+  const cardWidth = width - space(4)
 
-  const smallArtworksDimentions = cardHeight / 2
-  const mainArtworkDimantions =
-    layout === "horizontal"
-      ? {
-          width: cardWidth / 2 - space(0.5) / 2,
-          height: cardHeight + space(0.5),
-        }
-      : {
-          width: cardWidth,
-          height: cardHeight,
-        }
+  const dimensions = calculateDimensions(
+    isLeadingImageVertical,
+    cardWidth,
+    cardHeight,
+    isIPad,
+    space(0.5)
+  )
 
-  // Helper function to get artwork configuration
-  const getArtworkConfig = (index: number) => {
-    const artwork = artworksData[index]
-
-    if (index === 0) {
-      return {
-        artwork,
-        imageURL: artwork?.image?.url,
-        width: mainArtworkDimantions.width,
-        height: mainArtworkDimantions.height,
-        aspectRatio: 1,
-        href: artwork?.href,
-      }
-    } else {
-      return {
-        artwork,
-        imageURL: artwork?.image?.url,
-        width: cardWidth / 2 - space(0.5) / 2,
-        height: smallArtworksDimentions,
-        aspectRatio: artwork?.image?.aspectRatio,
-        href: artwork?.href,
-      }
-    }
-  }
-
-  // Get configurations for each artwork
-  const mainArtwork = getArtworkConfig(0)
-  const leftArtwork = getArtworkConfig(1)
-  const rightArtwork = getArtworkConfig(2)
+  const renderArtwork = (artwork: any, width: number, height: number) => (
+    <RouterLink>
+      <Image
+        src={artwork?.image?.url || ""}
+        width={width}
+        height={height}
+        blurhash={artwork?.image?.blurhash}
+        resizeMode="cover"
+      />
+    </RouterLink>
+  )
 
   return (
     <Flex
       width={cardWidth}
-      flexDirection={layout === "horizontal" ? "row" : "column"}
+      flexDirection={isLeadingImageVertical ? "row" : "column"}
       gap={0.5}
       alignSelf="center"
+      backgroundColor="red"
     >
-      <RouterLink>
-        <Image
-          src={mainArtwork.imageURL || ""}
-          width={mainArtwork.width}
-          height={mainArtwork.height}
-          aspectRatio={mainArtwork.aspectRatio}
-          blurhash={mainArtwork.artwork?.image?.blurhash}
-        />
-      </RouterLink>
+      {renderArtwork(artworksData[0], dimensions.main.width, dimensions.main.height)}
 
-      <Flex flexDirection={layout === "horizontal" ? "column" : "row"} gap={0.5}>
-        <RouterLink>
-          <Image
-            src={leftArtwork.imageURL || ""}
-            width={leftArtwork.width}
-            height={leftArtwork.height}
-            aspectRatio={leftArtwork.aspectRatio}
-            blurhash={leftArtwork.artwork?.image?.blurhash}
-          />
-        </RouterLink>
-
-        <RouterLink>
-          <Image
-            src={rightArtwork.imageURL || ""}
-            width={rightArtwork.width}
-            height={rightArtwork.height}
-            aspectRatio={rightArtwork.aspectRatio}
-            blurhash={rightArtwork.artwork?.image?.blurhash}
-          />
-        </RouterLink>
+      <Flex flexDirection={isLeadingImageVertical ? "column" : "row"} gap={0.5}>
+        {renderArtwork(artworksData[1], dimensions.small.width, dimensions.small.height)}
+        {renderArtwork(artworksData[2], dimensions.small.width, dimensions.small.height)}
       </Flex>
     </Flex>
   )
+}
+
+// helper function to get artworks dimentions
+function calculateDimensions(
+  isVertical: boolean,
+  cardWidth: number,
+  cardHeight: number,
+  isIPad: boolean,
+  gap: number
+) {
+  if (isVertical) {
+    const mainWidth = isIPad ? (cardWidth * 2) / 3 : cardWidth / 2
+    return {
+      main: {
+        width: mainWidth,
+        height: cardHeight,
+      },
+      small: {
+        width: cardWidth - mainWidth - gap,
+        height: cardHeight / 2 - gap / 2,
+      },
+    }
+  }
+
+  return {
+    main: {
+      width: cardWidth,
+      height: cardWidth,
+    },
+    small: {
+      width: cardWidth / 2 - gap / 2,
+      height: cardWidth / 2 - gap / 2,
+    },
+  }
 }
 
 const artworksCard = graphql`
