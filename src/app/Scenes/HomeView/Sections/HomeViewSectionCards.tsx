73a3e1c6afd74c0b6c281d361316280b0c494b1b
dariakoko <nargle30@gmail.com>
2025-10-15 16:14:15 +0200

    feat: add HomeViewSectionCards

---
+++ src/app/Scenes/HomeView/Sections/HomeViewSectionCards.tsx
@@ -0,0 +1,162 @@
+import { ContextModule } from "@artsy/cohesion"
+import { Flex, FlexProps, Skeleton, SkeletonBox, Text } from "@artsy/palette-mobile"
+import { HomeViewSectionCardsQuery } from "__generated__/HomeViewSectionCardsQuery.graphql"
+import { HomeViewSectionCards_section$key } from "__generated__/HomeViewSectionCards_section.graphql"
+import { CARD_WIDTH, CardRailCard } from "app/Components/CardRail/CardRailCard"
+import { CardRailFlatList } from "app/Components/CardRail/CardRailFlatList"
+import { SectionTitle } from "app/Components/SectionTitle"
+import { ThreeUpImageLayout } from "app/Components/ThreeUpImageLayout"
+import { HomeViewSectionSentinel } from "app/Scenes/HomeView/Components/HomeViewSectionSentinel"
+import { SectionSharedProps } from "app/Scenes/HomeView/Sections/Section"
+import { HORIZONTAL_FLATLIST_WINDOW_SIZE } from "app/Scenes/HomeView/helpers/constants"
+import { RouterLink } from "app/system/navigation/RouterLink"
+import { extractNodes } from "app/utils/extractNodes"
+import { NoFallback, withSuspense } from "app/utils/hooks/withSuspense"
+import { graphql, useFragment, useLazyLoadQuery } from "react-relay"
+
+interface HomeViewSectionCardsProps {
+  section: HomeViewSectionCards_section$key
+  index: number
+}
+
+export const HomeViewSectionCards: React.FC<HomeViewSectionCardsProps> = ({
+  section: sectionProp,
+  index,
+  ...flexProps
+}) => {
+  const section = useFragment(HomeViewSectionCardsFragment, sectionProp)
+  const cards = extractNodes(section.cardsConnection)
+
+  if (!cards) {
+    return null
+  }
+
+  const viewAll = section.component?.behaviors?.viewAll
+
+  return (
+    <Flex {...flexProps}>
+      <SectionTitle
+        href={viewAll?.href || "/auctions"}
+        mx={2}
+        title={section.component?.title}
+        onPress={() => {
+          /* track */
+        }}
+      />
+
+      <CardRailFlatList
+        data={cards}
+        initialNumToRender={cards.length}
+        windowSize={HORIZONTAL_FLATLIST_WINDOW_SIZE}
+        renderItem={({ item, index }) => {
+          if (!item) {
+            return null
+          }
+          const images = item.images || []
+          const imageURLs = images.map((img) => img?.imageURL).filter((url): url is string => !!url)
+
+          return (
+            <Flex flexDirection="column">
+              <CardRailCard key={index}>
+                <RouterLink
+                  to={item.href || "/"}
+                  onPress={() => {
+                    /* track */
+                  }}
+                >
+                  <ThreeUpImageLayout imageURLs={imageURLs} />
+                  <Text key={index} py={1} pl={0.5}>
+                    {item?.title}
+                  </Text>
+                </RouterLink>
+              </CardRailCard>
+            </Flex>
+          )
+        }}
+      />
+
+      <HomeViewSectionSentinel
+        contextModule={section.contextModule as ContextModule}
+        index={index}
+      />
+    </Flex>
+  )
+}
+
+const HomeViewSectionCardsFragment = graphql`
+  fragment HomeViewSectionCards_section on HomeViewSectionCards {
+    __typename
+    internalID
+    contextModule
+    component {
+      title
+      description
+      behaviors {
+        viewAll {
+          href
+          ownerType
+        }
+      }
+    }
+    cardsConnection(first: 3) {
+      edges {
+        node {
+          entityID
+          entityType
+          href
+          title
+          images {
+            blurhash
+            imageURL
+          }
+        }
+      }
+    }
+  }
+`
+
+const HomeViewSectionCardsPlaceholder: React.FC<FlexProps> = (flexProps) => {
+  return (
+    <Skeleton>
+      <Flex {...flexProps} flexDirection="row" gap={2} testID="HomeViewSectionCardsPlaceholder">
+        {Array.from({ length: 2 }).map((_, index) => (
+          <SkeletonBox key={index} width={CARD_WIDTH} height={CARD_WIDTH + 20 + 20} />
+        ))}
+      </Flex>
+    </Skeleton>
+  )
+}
+
+const homeViewSectionCardsQuery = graphql`
+  query HomeViewSectionCardsQuery($id: String!) {
+    homeView {
+      section(id: $id) {
+        ...HomeViewSectionCards_section
+      }
+    }
+  }
+`
+
+export const HomeViewSectionCardsQueryRenderer: React.FC<SectionSharedProps> = withSuspense({
+  Component: ({ sectionID, index, ...flexProps }) => {
+    const data = useLazyLoadQuery<HomeViewSectionCardsQuery>(
+      homeViewSectionCardsQuery,
+      {
+        id: sectionID,
+      },
+      {
+        networkCacheConfig: {
+          force: false,
+        },
+      }
+    )
+
+    if (!data.homeView.section) {
+      return null
+    }
+
+    return <HomeViewSectionCards section={data.homeView.section} index={index} {...flexProps} />
+  },
+  LoadingFallback: HomeViewSectionCardsPlaceholder,
+  ErrorFallback: NoFallback,
+})
