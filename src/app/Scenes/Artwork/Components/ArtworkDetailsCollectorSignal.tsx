9189da19a6fb92a6b36a575ab55f9a8eaa9b0f98
Carlos <araujobarret@gmail.com>
2025-10-16 06:27:37 -0300

    feat(ONYX-1942): add collector signals to dd (#12851)
    * feat: add collector signals to dd
    
    * feat: address requests

--- src/app/Scenes/Artwork/Components/ArtworkDetailsCollectorSignal.tsx
+++ src/app/Scenes/Artwork/Components/ArtworkDetailsCollectorSignal.tsx
@@ -1,64 +1,16 @@
-import { ArrowUpRightIcon, FireIcon, FairIcon } from "@artsy/icons/native"
-import { bullet, Flex, LinkText, Text } from "@artsy/palette-mobile"
-import { ArtworkDetailsCollectorSignal_artwork$key } from "__generated__/ArtworkDetailsCollectorSignal_artwork.graphql"
+import { Flex, LinkText, Text } from "@artsy/palette-mobile"
+import { useCollectorSignal_artwork$key } from "__generated__/useCollectorSignal_artwork.graphql"
 import { RouterLink } from "app/system/navigation/RouterLink"
-import { DateTime } from "luxon"
-import { graphql, useFragment } from "react-relay"
+import { useCollectorSignal } from "app/utils/artwork/useCollectorSignal"
 
 interface Props {
-  artwork: ArtworkDetailsCollectorSignal_artwork$key
+  artwork: useCollectorSignal_artwork$key
 }
 
 export const ArtworkDetailsCollectorSignal: React.FC<Props> = ({ artwork }) => {
-  const { collectorSignals } = useFragment(fragment, artwork)
+  const { signalTitle, signalDescription, href, SignalIcon } = useCollectorSignal({ artwork })
 
-  if (
-    !collectorSignals ||
-    (!collectorSignals.curatorsPick &&
-      !collectorSignals.increasedInterest &&
-      !collectorSignals.runningShow)
-  ) {
-    return null
-  }
-
-  const { curatorsPick, increasedInterest, runningShow } = collectorSignals
-
-  let singalTitle: string | null = null
-  let signalDescription: string | null = null
-  let href: string | null = null
-  let SignalIcon = FairIcon
-
-  switch (true) {
-    case curatorsPick: {
-      singalTitle = "Curators’ Pick"
-      signalDescription = "Hand selected by Artsy curators this week"
-      SignalIcon = FireIcon
-      break
-    }
-    case increasedInterest: {
-      singalTitle = "Increased Interest"
-      signalDescription = "Based on collector activity in the past 14 days"
-      SignalIcon = ArrowUpRightIcon
-      break
-    }
-    case !!runningShow: {
-      if (
-        !runningShow?.startAt ||
-        !runningShow?.endAt ||
-        !runningShow?.href ||
-        !runningShow?.name
-      ) {
-        break
-      }
-      const showStartsAt = DateTime.fromISO(runningShow.startAt).toFormat("MMM d")
-      const showEndAt = DateTime.fromISO(runningShow.endAt).toFormat("MMM d")
-      singalTitle = `Showing now ${bullet} ${showStartsAt}-${showEndAt}`
-      href = runningShow?.href
-      signalDescription = runningShow?.name
-    }
-  }
-
-  if (!singalTitle) {
+  if (!signalTitle) {
     return null
   }
 
@@ -68,7 +20,7 @@ export const ArtworkDetailsCollectorSignal: React.FC<Props> = ({ artwork }) => {
 
       <Flex flex={1} flexDirection="column">
         <Text variant="sm-display" color="mono100">
-          {singalTitle}
+          {signalTitle}
         </Text>
 
         {href ? (
@@ -86,18 +38,3 @@ export const ArtworkDetailsCollectorSignal: React.FC<Props> = ({ artwork }) => {
     </Flex>
   )
 }
-
-const fragment = graphql`
-  fragment ArtworkDetailsCollectorSignal_artwork on Artwork {
-    collectorSignals {
-      curatorsPick
-      increasedInterest
-      runningShow {
-        endAt
-        href
-        name
-        startAt
-      }
-    }
-  }
-`
