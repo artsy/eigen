5111b8cf687ff8ba2d508fbcfd8e763bf2177999
dariakoko <nargle30@gmail.com>
2025-11-05 15:43:36 +0100

    chore: track item_viewed

--- src/app/Scenes/HomeView/hooks/useImpressionsTracking.ts
+++ src/app/Scenes/HomeView/hooks/useImpressionsTracking.ts
@@ -105,8 +105,16 @@ export const useItemsImpressionsTracking = ({
     [enableItemsViewsTracking, isInViewport, contextScreenOwnerType, contextModule]
   )
 
+  const trackViewsFromCards = useCallback(
+    (items: Array<TrackableItem>) => {
+      trackItems(items)
+    },
+    [trackItems]
+  )
+
   return {
     onViewableItemsChanged,
     viewabilityConfig,
+    trackViewsFromCards,
   }
 }
