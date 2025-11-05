7b94e626277593d9450f80da62ed5e94fef9c04c
Daria Kozlova <daria.kozlova@artsymail.com>
2025-11-05 18:11:43 +0100

    fix: track the new NWFY homeview section and its items  (#12939)
    * fix: track the new homeview section
    
    * chore: track item_viewed
    
    * chore: remove unused props

--- src/app/Scenes/HomeView/Sections/HomeViewSectionArtworks.tsx
+++ src/app/Scenes/HomeView/Sections/HomeViewSectionArtworks.tsx
@@ -39,6 +39,8 @@ interface HomeViewSectionArtworksProps extends FlexProps {
   index: number
 }
 
+const NUMBER_OF_ARTWORKS_FOR_ARTWORKS_CARD = 3
+
 export const HomeViewSectionArtworks: React.FC<HomeViewSectionArtworksProps> = ({
   section: sectionProp,
   index,
@@ -50,13 +52,14 @@ export const HomeViewSectionArtworks: React.FC<HomeViewSectionArtworksProps> = (
   const section = useFragment(fragment, sectionProp)
   const viewableSections = HomeViewStore.useStoreState((state) => state.viewableSections)
 
-  const { onViewableItemsChanged, viewabilityConfig } = useItemsImpressionsTracking({
-    // It is important here to tell if the rail is visible or not, because the viewability config
-    // default behavior, doesn't take into account the fact that the rail could be not visible
-    // on the screen because it's within a scrollable container.
-    isInViewport: viewableSections.includes(section.internalID) && section.trackItemImpressions,
-    contextModule: section.contextModule as ContextModule,
-  })
+  const { onViewableItemsChanged, viewabilityConfig, trackViewsFromCards } =
+    useItemsImpressionsTracking({
+      // It is important here to tell if the rail is visible or not, because the viewability config
+      // default behavior, doesn't take into account the fact that the rail could be not visible
+      // on the screen because it's within a scrollable container.
+      isInViewport: viewableSections.includes(section.internalID) && section.trackItemImpressions,
+      contextModule: section.contextModule as ContextModule,
+    })
 
   let artworks = extractNodes(section.artworksConnection)
 
@@ -104,10 +107,11 @@ export const HomeViewSectionArtworks: React.FC<HomeViewSectionArtworksProps> = (
 
   const showHomeViewCardRail = enableNewHomeViewCardRailType && section.showArtworksCardView
 
+  const artowrksForArtworksCards = artworks.slice(0, NUMBER_OF_ARTWORKS_FOR_ARTWORKS_CARD)
+
   const subTitle =
     showHomeViewCardRail && artworks.length > 0
-      ? artworks
-          .slice(0, 3)
+      ? artowrksForArtworksCards
           .map((artwork) => artwork?.artistNames)
           .filter(Boolean)
           .join(", ")
@@ -125,7 +129,21 @@ export const HomeViewSectionArtworks: React.FC<HomeViewSectionArtworksProps> = (
       {!!isFirstArtworkSection && <ProgressiveOnboardingLongPressContextMenu />}
 
       {showHomeViewCardRail ? (
-        <ArtworksCard artworks={artworks} />
+        <ArtworksCard
+          artworks={artworks}
+          {...(section.trackItemImpressions
+            ? {
+                trackViewsFromCards: trackViewsFromCards(
+                  artowrksForArtworksCards
+                    .map((artwork, index) => ({
+                      id: artwork.internalID,
+                      index: index,
+                    }))
+                    .filter((item) => !!item.id)
+                ),
+              }
+            : {})}
+        />
       ) : (
         <ArtworkRail
           contextModule={section.contextModule as ContextModule}
@@ -179,6 +197,7 @@ const fragment = graphql`
         node {
           isDisliked @include(if: $enableHidingDislikedArtworks)
           artistNames @include(if: $includeArtistNames)
+          internalID
           ...ArtworkRail_artworks
           ...ArtworksCard_artworks
         }
