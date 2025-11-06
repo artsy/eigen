4ff36ab9067eac06d1425e199487cf335a07aece
George Kartalis <g.kartalis7@gmail.com>
2025-11-06 11:49:42 +0100

    chore: eigen<>echo test integration testing

--- src/app/store/config/features.ts
+++ src/app/store/config/features.ts
@@ -230,6 +230,12 @@ export const features = {
     showInDevMenu: true,
     echoFlagKey: "AREnableAuctionsHubOnHomeView",
   },
+  AREnableFailingTestEcho: {
+    description: "Enable a failing test echo flag",
+    readyForRelease: true,
+    showInDevMenu: true,
+    echoFlagKey: "AREnableFailingTestEcho",
+  },
 } satisfies { [key: string]: FeatureDescriptor }
 
 export interface DevToggleDescriptor {
