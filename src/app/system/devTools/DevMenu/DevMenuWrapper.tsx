8ac5a53ad8c1f5d0ebf4e301517bba43b7f2abf9
Mounir Dhahri <soussouheros@gmail.com>
2025-12-10 17:10:57 +0100

    fix: increase duration between taps

--- src/app/system/devTools/DevMenu/DevMenuWrapper.tsx
+++ src/app/system/devTools/DevMenu/DevMenuWrapper.tsx
@@ -2,11 +2,12 @@ import { Flex } from "@artsy/palette-mobile"
 import { GlobalStore } from "app/store/GlobalStore"
 import { navigateToDevMenu } from "app/system/devTools/DevMenu/utils/navigateToDevMenu"
 // eslint-disable-next-line no-restricted-imports
-import React, { useRef } from "react"
+import React, { useCallback, useRef } from "react"
 import { Platform } from "react-native"
 
 const MAX_DURATION_BETWEEN_TAPS = 300
-const MIN_DURATION_BETWEEN_TAPS = 5
+// No human can tap faster than that
+const MIN_DURATION_BETWEEN_TAPS = 70
 
 export const DevMenuWrapper: React.FC<React.PropsWithChildren> = ({ children }) => {
   const userIsDev = GlobalStore.useAppState((store) => store.artsyPrefs.userIsDev.value)
@@ -20,33 +21,33 @@ export const DevMenuWrapper: React.FC<React.PropsWithChildren> = ({ children }) 
     return <Flex flex={1}>{children}</Flex>
   }
 
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const handleStartShouldSetResponderCapture = useCallback(() => {
+    const now = Date.now()
+    const state = gestureState.current
+
+    if (
+      now - state.lastTapTimestamp < MAX_DURATION_BETWEEN_TAPS &&
+      // We need to set a minimum duration between taps to avoid triggering the dev menu on mouse scroll
+      now - state.lastTapTimestamp > MIN_DURATION_BETWEEN_TAPS
+    ) {
+      state.numTaps += 1
+    } else {
+      state.numTaps = 1
+    }
+
+    state.lastTapTimestamp = now
+
+    if (state.numTaps >= 5 && !isDeepZoomModalVisible) {
+      state.numTaps = 0
+
+      navigateToDevMenu({ isLoggedIn })
+    }
+    return false
+  }, [isDeepZoomModalVisible])
+
   return (
-    <Flex
-      flex={1}
-      onStartShouldSetResponderCapture={() => {
-        const now = Date.now()
-        const state = gestureState.current
-
-        if (
-          now - state.lastTapTimestamp < MAX_DURATION_BETWEEN_TAPS &&
-          // We need to set a minimum duration between taps to avoid triggering the dev menu on mouse scroll
-          now - state.lastTapTimestamp > MIN_DURATION_BETWEEN_TAPS
-        ) {
-          state.numTaps += 1
-        } else {
-          state.numTaps = 1
-        }
-
-        state.lastTapTimestamp = now
-
-        if (state.numTaps >= 5 && !isDeepZoomModalVisible) {
-          state.numTaps = 0
-
-          navigateToDevMenu({ isLoggedIn })
-        }
-        return false
-      }}
-    >
+    <Flex flex={1} onStartShouldSetResponderCapture={handleStartShouldSetResponderCapture}>
       {children}
     </Flex>
   )
