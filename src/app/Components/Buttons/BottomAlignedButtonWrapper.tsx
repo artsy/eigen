452eddde2477ac9d95fadc8e38f89ce9ca07f5e6
Sultan <sultan.al-maari@artsymail.com>
2025-12-23 18:58:45 +0100

    more visual fixes

--- src/app/Components/Buttons/BottomAlignedButtonWrapper.tsx
+++ src/app/Components/Buttons/BottomAlignedButtonWrapper.tsx
@@ -1,5 +1,7 @@
+import { useSpace } from "@artsy/palette-mobile"
 import { View } from "react-native"
 import { KeyboardStickyView } from "react-native-keyboard-controller"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 export interface BottomAlignedProps {
   onPress?: () => void
@@ -8,12 +10,19 @@ export interface BottomAlignedProps {
 
 export const BottomAlignedButtonWrapper: React.FC<React.PropsWithChildren<BottomAlignedProps>> = (
   props
-) => (
-  <View style={{ flex: 1 }}>
-    <View key="space-eater" style={{ flexGrow: 1 }}>
-      {props.children}
-    </View>
+) => {
+  const { bottom } = useSafeAreaInsets()
+  const space = useSpace()
 
-    <KeyboardStickyView>{props.buttonComponent}</KeyboardStickyView>
-  </View>
-)
+  return (
+    <View style={{ flex: 1 }}>
+      <View key="space-eater" style={{ flexGrow: 1 }}>
+        {props.children}
+      </View>
+
+      <KeyboardStickyView offset={{ opened: bottom - space(1) }}>
+        {props.buttonComponent}
+      </KeyboardStickyView>
+    </View>
+  )
+}
