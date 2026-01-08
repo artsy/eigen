3ba2e37401ffa039bd06c37819fe704a1d9a8951
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2026-01-07 22:11:50 -0500

    feat: headings

--- src/app/Scenes/HTMLTest/HTMLTest.tsx
+++ src/app/Scenes/HTMLTest/HTMLTest.tsx
@@ -1,4 +1,5 @@
 import { Screen, Spacer, Text } from "@artsy/palette-mobile"
+import { HTML } from "app/Components/HTML"
 import { goBack } from "app/system/navigation/navigate"
 
 export const HTMLTest: React.FC = () => {
@@ -8,35 +9,27 @@ export const HTMLTest: React.FC = () => {
       <Screen.Body>
         <Screen.ScrollView>
           <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
-          <Text variant="lg">HTML Test Screen</Text>
-          <Spacer y={2} />
+
+          <Text my={1}>
+            This screen provides a kitchen sink test of the HTML component used throughout the app.
+          </Text>
+
+          <Text variant="xl" color="brand">
+            Headings
+          </Text>
+
+          <HTML html={headings} />
         </Screen.ScrollView>
       </Screen.Body>
     </Screen>
   )
 }
+
+const headings = `
+<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+<h5>Heading 5</h5>
+<h6>Heading 6</h6>
+`
