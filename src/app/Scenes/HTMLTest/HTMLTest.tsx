2a88d106e2fbcf75c5757c1706877c71b52555cf
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2026-01-07 22:12:02 -0500

    feat: inline elements

--- src/app/Scenes/HTMLTest/HTMLTest.tsx
+++ src/app/Scenes/HTMLTest/HTMLTest.tsx
@@ -35,6 +35,7 @@ export const HTMLTest: React.FC = () => {
           <Section title="Horizontal Rule" html={rule} />
           <Section title="Table" html={table} />
           <Section title="Code" html={code} />
+          <Section title="Inline elements" html={inline} />
         </Screen.ScrollView>
       </Screen.Body>
     </Screen>
@@ -224,4 +225,25 @@ P Q R S T U V W X Y Z [ \ ] ^ _
 a b c d e f g h i j k l m n o
 p q r s t u v w x y z { | } ~ </pre>
 </div>
+`
+
+const inline = `
+<p><a href="https://www.github.com">This is a text link</a> (to github.com)</p>
+<p><strong>Strong is used to indicate strong importance.</strong></p>
+<p><em>This text has added emphasis.</em></p>
+<p>The <b>b element</b> is stylistically different text from normal text, without any special importance.</p>
+<p>The <i>i element</i> is text that is offset from the normal text.</p>
+<p>The <u>u element</u> is text with an unarticulated, though explicitly rendered, non-textual annotation.</p>
+<p><del>This text is deleted</del> and <ins>This text is inserted</ins>.</p>
+<p><s>This text has a strikethrough</s>.</p>
+<p>Superscript<sup>42</sup>.</p>
+<p>Subscript<sub>42</sub>.</p>
+<p><small>This small text is small for fine print, etc.</small></p>
+<p>Abbreviation: <abbr title="HyperText Markup Language">HTML</abbr></p>
+<p><q cite="https://developer.mozilla.org/en-US/docs/HTML/Element/q">This text is a short inline quotation.</q></p>
+<p><cite>This is a citation.</cite></p>
+<p>The <dfn>dfn element</dfn> indicates a definition.</p>
+<p>The <mark>mark element</mark> indicates a highlight.</p>
+<p>The <var>variable element</var>, such as <var>x</var> = <var>y</var>.</p>
+<p>The time element: <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></p>
 `
