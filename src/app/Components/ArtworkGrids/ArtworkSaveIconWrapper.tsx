02ab5933be30ce79f423dcc167038e983bdb1f0e
Mounir Dhahri <soussouheros@gmail.com>
2026-01-12 18:11:02 +0100

    chore: disable save animation

--- src/app/Components/ArtworkGrids/ArtworkSaveIconWrapper.tsx
+++ src/app/Components/ArtworkGrids/ArtworkSaveIconWrapper.tsx
@@ -1,7 +1,6 @@
 import { HeartFillIcon, HeartStrokeIcon } from "@artsy/icons/native"
 import { HEART_ICON_SIZE } from "app/Components/constants"
-import { useEffect, useRef } from "react"
-import { Animated, useAnimatedValue } from "react-native"
+import { View } from "react-native"
 
 interface ArtworkSaveIconWrapperProps {
   isSaved: boolean
@@ -16,46 +15,8 @@ export const ArtworkSaveIconWrapper: React.FC<ArtworkSaveIconWrapperProps> = ({
   accessibilityLabel,
   fill,
 }) => {
-  const scaleAnimation = useAnimatedValue(1)
-  const didMount = useRef(false)
-
-  useEffect(() => {
-    if (!didMount.current) {
-      didMount.current = true
-      return
-    }
-
-    if (isSaved) {
-      Animated.sequence([
-        Animated.spring(scaleAnimation, {
-          toValue: 0.7,
-          mass: 0.01,
-          stiffness: 300,
-          useNativeDriver: true,
-        }),
-        Animated.timing(scaleAnimation, {
-          toValue: 1.1,
-          duration: 300,
-          useNativeDriver: true,
-        }),
-        Animated.timing(scaleAnimation, {
-          toValue: 1,
-          duration: 200,
-          useNativeDriver: true,
-        }),
-      ]).start()
-    } else {
-      // We don't want to animation the dislike
-      scaleAnimation.setValue(1)
-    }
-  }, [isSaved, scaleAnimation])
-
   return (
-    <Animated.View
-      style={{ transform: [{ scale: scaleAnimation }] }}
-      testID={testID}
-      accessibilityLabel={accessibilityLabel}
-    >
+    <View testID={testID} accessibilityLabel={accessibilityLabel}>
       {!!isSaved ? (
         <HeartFillIcon
           height={HEART_ICON_SIZE}
@@ -71,6 +32,6 @@ export const ArtworkSaveIconWrapper: React.FC<ArtworkSaveIconWrapperProps> = ({
           testID="empty-heart-icon"
         />
       )}
-    </Animated.View>
+    </View>
   )
 }
