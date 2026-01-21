57277a7bdb8668f0d22c824846a6ea30b2cc170d
brainbicycle <brian.beckerle@artsymail.com>
2026-01-21 11:46:25 +0100

    add close icon

--- src/app/Scenes/LiveSale/LiveSale.tsx
+++ src/app/Scenes/LiveSale/LiveSale.tsx
@@ -1,5 +1,25 @@
-import { Text } from "@artsy/palette-mobile"
+import { CloseIcon } from "@artsy/icons/native"
+import { DEFAULT_HIT_SLOP, Screen, Text, Touchable } from "@artsy/palette-mobile"
+import { goBack } from "app/system/navigation/navigate"
 
 export const LiveSale: React.FC<{ slug: string }> = ({ slug }) => {
-  return <Text>This is where I would put my Live Sale Screen for {slug}</Text>
+  return (
+    <Screen>
+      <Screen.Header
+        leftElements={
+          // TODO: Is this the preferred way to have close icon? should we use NavigationHeader?
+          // update Screen.Header to more easily support close icon?
+          <Touchable
+            accessibilityRole="button"
+            accessibilityLabel="Exit Sale"
+            hitSlop={DEFAULT_HIT_SLOP}
+            onPress={() => goBack()}
+          >
+            <CloseIcon />
+          </Touchable>
+        }
+      />
+      <Text>This is where I would put my Live Sale Screen for {slug}</Text>
+    </Screen>
+  )
 }
