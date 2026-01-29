9eb8c279d86257bd37ed36658e2e17e60aa526e2
Mounir Dhahri <soussouheros@gmail.com>
2026-01-29 11:38:23 +0100

    fix: dev menu issues (#13188)

--- src/app/system/devTools/DevMenu/DevMenuWrapper.tsx
+++ src/app/system/devTools/DevMenu/DevMenuWrapper.tsx
@@ -1,52 +1,19 @@
 import { Flex } from "@artsy/palette-mobile"
-import { __unsafe__onboardingNavigationRef } from "app/Scenes/Onboarding/Screens/Onboarding"
 import { GlobalStore } from "app/store/GlobalStore"
-// eslint-disable-next-line no-restricted-imports
-import { navigate } from "app/system/navigation/navigate"
-import React, { useRef } from "react"
+import { useCaptureTaps } from "app/system/devTools/DevMenu/hooks/useCaptureTaps"
 import { Platform } from "react-native"
-
-const MAX_DURATION_BETWEEN_TAPS = 300
 
 export const DevMenuWrapper: React.FC<React.PropsWithChildren> = ({ children }) => {
   const userIsDev = GlobalStore.useAppState((store) => store.artsyPrefs.userIsDev.value)
-  const isLoggedIn = GlobalStore.useAppState((state) => !!state.auth.userAccessToken)
-  const isDeepZoomModalVisible = GlobalStore.useAppState(
-    (store) => store.devicePrefs.sessionState.isDeepZoomModalVisible
-  )
-  const gestureState = useRef({ lastTapTimestamp: 0, numTaps: 0 })
+
+  const { handleStartShouldSetResponderCapture } = useCaptureTaps()
 
   if (!userIsDev || Platform.OS === "ios") {
     return <Flex flex={1}>{children}</Flex>
   }
 
   return (
-    <Flex
-      flex={1}
-      onStartShouldSetResponderCapture={() => {
-        const now = Date.now()
-        const state = gestureState.current
-
-        if (now - state.lastTapTimestamp < MAX_DURATION_BETWEEN_TAPS) {
-          state.numTaps += 1
-        } else {
-          state.numTaps = 1
-        }
-
-        state.lastTapTimestamp = now
-
-        if (state.numTaps >= 5 && !isDeepZoomModalVisible) {
-          state.numTaps = 0
-
-          if (!isLoggedIn) {
-            __unsafe__onboardingNavigationRef.current?.navigate("DevMenu")
-          } else {
-            navigate("/dev-menu")
-          }
-        }
-        return false
-      }}
-    >
+    <Flex flex={1} onStartShouldSetResponderCapture={handleStartShouldSetResponderCapture}>
       {children}
     </Flex>
   )
