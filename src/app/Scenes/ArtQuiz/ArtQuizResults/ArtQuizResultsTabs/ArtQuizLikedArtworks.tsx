b732ab6940d1c78a36e77f589d8a6e69c6ad81d8
Mounir Dhahri <soussouheros@gmail.com>
2026-01-29 15:22:51 +0100

    chore: refactor generic grid (#13193)

--- src/app/Scenes/ArtQuiz/ArtQuizResults/ArtQuizResultsTabs/ArtQuizLikedArtworks.tsx
+++ src/app/Scenes/ArtQuiz/ArtQuizResults/ArtQuizResultsTabs/ArtQuizLikedArtworks.tsx
@@ -2,8 +2,6 @@ import { Tabs, useSpace } from "@artsy/palette-mobile"
 import { ArtQuizLikedArtworks_artworks$key } from "__generated__/ArtQuizLikedArtworks_artworks.graphql"
 import { ArtQuizResultsTabs_me$data } from "__generated__/ArtQuizResultsTabs_me.graphql"
 import GenericGrid from "app/Components/ArtworkGrids/GenericGrid"
-
-import { useScreenDimensions } from "app/utils/hooks"
 import { graphql, useFragment } from "react-relay"
 
 export const ArtQuizLikedArtworks = ({
@@ -17,19 +15,16 @@ export const ArtQuizLikedArtworks = ({
   )
 
   const space = useSpace()
-  const dimensions = useScreenDimensions()
 
   return (
     <Tabs.ScrollView
       contentContainerStyle={{
         paddingBottom: space(4),
-        marginVertical: space(2),
         paddingHorizontal: space(1),
       }}
     >
       <GenericGrid
         artworks={artworks}
-        width={dimensions.width}
         hidePartner
         artistNamesTextStyle={{ weight: "regular" }}
         saleInfoTextStyle={{ weight: "medium", color: "mono100" }}
