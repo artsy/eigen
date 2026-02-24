20d7f599dbda72dbc049edc0340ff7e7553a32de
George Kartalis <g.kartalis7@gmail.com>
2026-02-24 11:28:45 +0100

    adjust paths

--- expo/plugins/build-cache-s3/index.js
+++ expo/plugins/build-cache-s3/index.js
@@ -30,7 +30,8 @@ function formatBytes(bytes) {
 
 function getS3Path(options, key) {
   const bucket = options?.bucket || "eigen-ci-cache"
-  return `s3://${bucket}/${key}`
+  const prefix = options?.prefix || "eigen/cached-builds"
+  return `s3://${bucket}/${prefix}/${key}`
 }
 
 function getCacheKey(fingerprintHash, platform, isDevClient) {
