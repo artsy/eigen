8672acb9a628cc88c4f8def498da997f75a7d52b
brainbicycle <brian.beckerle@artsymail.com>
2026-03-02 15:57:06 -0500

    revist: remove unused system time api sync and models

--- ios/Artsy/Networking/ARSystemTime.h
+++ ios/Artsy/Networking/ARSystemTime.h
@@ -4,9 +4,9 @@
 @interface ARSystemTime : NSObject
 
 /**
- *  Current server-side date and time based on a previously retrieved interval.
- *
- *  @return Server-side date and time.
+ *  Legacy: Current server-side date and time based on a previously retrieved interval.
+ *  TODO: This class previously was responsible for syncing server and client times, it has been broken a long while possibly can be removed entirely.
+ *  @return Current date and time.
  */
 + (NSDate *)date;
 
