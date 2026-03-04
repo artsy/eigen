86d4f5b98db4d0381ff27cbb688e08cfade2f818
dariakoko <nargle30@gmail.com>
2026-03-04 18:03:59 +0100

    chore

--- src/app/utils/masonryHelpers/index.tsx
+++ src/app/utils/masonryHelpers/index.tsx
@@ -15,7 +15,7 @@ export interface masonryRenderItemProps {
 
 export interface MasonryArtworkItem {
   readonly id: string
-  readonly internalID: string
+  readonly internalID?: string | null
   readonly image:
     | {
         readonly aspectRatio: number
