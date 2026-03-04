a8d7fa449371d4b447d6e03b56f1ec8ccb5eb808
Mounir Dhahri <soussouheros@gmail.com>
2026-03-04 11:35:40 +0100

    chore: bump react native google sign in

--- src/app/store/AuthModel.ts
+++ src/app/store/AuthModel.ts
@@ -626,11 +626,16 @@ export const getAuthModel = (): AuthModel => ({
         const userInfo = await GoogleSignin.signIn()
         const accessToken = (await GoogleSignin.getTokens()).accessToken
 
+        if (userInfo.type === "cancelled") {
+          reject(new AuthError("Cancelled Google Auth"))
+          return
+        }
+
         if (options.signInOrUp === "signUp") {
-          const resultGravitySignUp = userInfo.user.name
+          const resultGravitySignUp = userInfo.data.user.name
             ? await actions.signUp({
-                email: userInfo.user.email,
-                name: userInfo.user.name,
+                email: userInfo.data.user.email,
+                name: userInfo.data.user.name,
                 oauthMode: "accessToken",
                 accessToken,
                 oauthProvider: "google",
@@ -738,10 +743,15 @@ export const getAuthModel = (): AuthModel => ({
         const userInfo = await GoogleSignin.signIn()
         const accessToken = (await GoogleSignin.getTokens()).accessToken
 
-        const resultGravitySignUp = userInfo.user.name
+        if (userInfo.type === "cancelled") {
+          reject(new AuthError("Cancelled Google Auth"))
+          return
+        }
+
+        const resultGravitySignUp = userInfo.data.user.name
           ? await actions.signUp({
-              email: userInfo.user.email,
-              name: userInfo.user.name,
+              email: userInfo.data.user.email,
+              name: userInfo.data.user.name,
               oauthMode: "accessToken",
               accessToken,
               oauthProvider: "google",
@@ -977,7 +987,7 @@ export const getAuthModel = (): AuthModel => ({
   signOut: thunk(async (actions, _) => {
     const signOutGoogle = async () => {
       try {
-        const isSignedIn = await GoogleSignin.isSignedIn()
+        const isSignedIn = GoogleSignin.hasPreviousSignIn()
         if (isSignedIn) {
           await GoogleSignin.revokeAccess()
           await GoogleSignin.signOut()
