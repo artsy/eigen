bbf85cba56bd190fe7155283cb3ec5c6b36a1047
Mounir Dhahri <soussouheros@gmail.com>
2026-03-05 15:19:57 +0100

    chore: update google sign in inialization logic

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -68,6 +68,12 @@ if (Platform.OS === "android") {
   }
 }
 
+if (Keys.OSS !== "true") {
+  GoogleSignin.configure({
+    webClientId: "673710093763-hbj813nj4h3h183c4ildmu8vvqc0ek4h.apps.googleusercontent.com",
+  })
+}
+
 const Main = () => {
   // Rozenite plugins
   useNetworkActivityDevTools()
@@ -82,10 +88,6 @@ const Main = () => {
     if (oss === "true") {
       return
     }
-    GoogleSignin.configure({
-      webClientId: "673710093763-hbj813nj4h3h183c4ildmu8vvqc0ek4h.apps.googleusercontent.com",
-      iosClientId: "673710093763-mjnb33vmcd74p4io0kbc2d8mkpj9jnh7.apps.googleusercontent.com",
-    })
     Settings.initializeSDK()
   }, [])
   const isHydrated = GlobalStore.useAppState((state) => state.sessionState.isHydrated)
