d7de32fe80dbd7995066a128071f1a5c57875dcc
George <g.kartalis7@gmail.com>
2026-03-27 12:15:30 +0100

    fix: empty state on myC artists (#13423)

--- src/app/Scenes/MyCollection/Components/MyCollectionZeroArtists.tsx
+++ src/app/Scenes/MyCollection/Components/MyCollectionZeroArtists.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, useSpace } from "@artsy/palette-mobile"
+import { Button, Flex, Tabs, useSpace } from "@artsy/palette-mobile"
 import { ZeroState } from "app/Components/States/ZeroState"
 import { ModalCarousel } from "app/Scenes/HomeView/Components/ModalCarouselComponents/ModalCarousel"
 import { useZeroStateDimensions } from "app/Scenes/MyCollection/utils/zeroStateWidth"
@@ -14,56 +14,58 @@ export const MyCollectionZeroArtists: React.FC<{}> = () => {
   const { width: zeroStateWidth } = useZeroStateDimensions()
 
   return (
-    <Flex>
-      <ModalCarousel
-        isVisible={isModalVisible}
-        toggleModal={(isVisible) => setIsModalVisible(isVisible)}
-      />
-      <ZeroState
-        minHeight={0}
-        showBorder
-        bigTitle="Keep track of artists you collect"
-        subtitle="Discover more about the artists you collect, with latest career news and auction results."
-        image={
-          <Image
-            source={require("images/my-collection-artists-empty-state.webp")}
-            resizeMode="cover"
-            style={{
-              width: zeroStateWidth,
-              minHeight: 150,
-              marginTop: space(2),
-            }}
-          />
-        }
-        callToAction={
-          <Flex
-            gap={2}
-            flexDirection={isFontScaleLarge() ? "column" : "row"}
-            justifyContent="space-evenly"
-          >
-            <RouterLink
-              to="/my-collection/collected-artists/new"
-              onPress={() => {
-                // TODO: Implement analytics
+    <Tabs.ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <Flex>
+        <ModalCarousel
+          isVisible={isModalVisible}
+          toggleModal={(isVisible) => setIsModalVisible(isVisible)}
+        />
+        <ZeroState
+          minHeight={0}
+          showBorder
+          bigTitle="Keep track of artists you collect"
+          subtitle="Discover more about the artists you collect, with latest career news and auction results."
+          image={
+            <Image
+              source={require("images/my-collection-artists-empty-state.webp")}
+              resizeMode="cover"
+              style={{
+                width: zeroStateWidth,
+                minHeight: 150,
+                marginTop: space(2),
               }}
-              hasChildTouchable
+            />
+          }
+          callToAction={
+            <Flex
+              gap={2}
+              flexDirection={isFontScaleLarge() ? "column" : "row"}
+              justifyContent="space-evenly"
             >
-              <Button testID="add-artwork-button-zero-state" block={isFontScaleLarge()}>
-                Add Artists
+              <RouterLink
+                to="/my-collection/collected-artists/new"
+                onPress={() => {
+                  // TODO: Implement analytics
+                }}
+                hasChildTouchable
+              >
+                <Button testID="add-artwork-button-zero-state" block={isFontScaleLarge()}>
+                  Add Artists
+                </Button>
+              </RouterLink>
+              <Button
+                variant="outline"
+                onPress={() => {
+                  setIsModalVisible(true)
+                }}
+                block={isFontScaleLarge()}
+              >
+                Learn More
               </Button>
-            </RouterLink>
-            <Button
-              variant="outline"
-              onPress={() => {
-                setIsModalVisible(true)
-              }}
-              block={isFontScaleLarge()}
-            >
-              Learn More
-            </Button>
-          </Flex>
-        }
-      />
-    </Flex>
+            </Flex>
+          }
+        />
+      </Flex>
+    </Tabs.ScrollView>
   )
 }
