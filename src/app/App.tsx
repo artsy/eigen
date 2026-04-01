366d5b156f57f9f142652a0d3ba735fa2ff2c089
Mounir Dhahri <soussouheros@gmail.com>
2026-04-01 16:12:06 +0200

    remove app tsx changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -22,6 +22,7 @@ import { useIdentifyUser } from "app/utils/useIdentifyUser"
 import { useListenToThemeChange } from "app/utils/useListenToThemeChange"
 import { useSiftConfig } from "app/utils/useSiftConfig"
 import { useStripeConfig } from "app/utils/useStripeConfig"
+import { useTrackAppState } from "app/utils/useTrackAppState"
 import { useEffect } from "react"
 import { NativeModules, Platform, UIManager, View } from "react-native"
 import { Settings } from "react-native-fbsdk-next"
@@ -113,6 +114,7 @@ const Main = () => {
   useHideSplashScreen()
   useAndroidAppStyling()
   useListenToThemeChange()
+  useTrackAppState()
 
   if (!isHydrated || !isUserIdentified) {
     return <View />
