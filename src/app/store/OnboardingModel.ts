ff2886b6c09c02ada2d75625b4572582f4d2844d
Janae Edwards <edwards.janae@gmail.com>
2026-06-17 16:54:10 -0400

    add state for artist save bottom sheet. Add showArtistSaveBottomSheet boolean to track if the experienced collector onboarding bottom sheet should be displayed on home screen.

--- src/app/store/OnboardingModel.ts
+++ src/app/store/OnboardingModel.ts
@@ -8,15 +8,21 @@ export interface OnboardingModel {
   onboardingArtQuizState: OnboardingArtQuizState
   setArtQuizState: Action<this, OnboardingArtQuizState>
   setOnboardingState: Action<this, OnboardingState>
+  showArtistSaveBottomSheet: boolean
+  setShowArtistSaveBottomSheet: Action<this, boolean>
 }
 
 export const getOnboardingModel = (): OnboardingModel => ({
   onboardingState: "incomplete",
   onboardingArtQuizState: "none",
+  showArtistSaveBottomSheet: false,
   setArtQuizState: action((state, artQuizState) => {
     state.onboardingArtQuizState = artQuizState
   }),
   setOnboardingState: action((state, onboardingState) => {
     state.onboardingState = onboardingState
+  }),
+  setShowArtistSaveBottomSheet: action((state, show) => {
+    state.showArtistSaveBottomSheet = show
   }),
 })
