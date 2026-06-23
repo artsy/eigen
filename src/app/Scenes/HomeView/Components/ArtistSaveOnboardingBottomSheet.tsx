dc99ee5e35b676019410594a020e236eb66b7658
Janae Edwards <edwards.janae@gmail.com>
2026-06-23 09:38:12 -0400

    style and layout changes

--- src/app/Scenes/HomeView/Components/ArtistSaveOnboardingBottomSheet.tsx
+++ src/app/Scenes/HomeView/Components/ArtistSaveOnboardingBottomSheet.tsx
@@ -1,7 +1,6 @@
 import { Button, Flex, Image, useSpace, Spacer, Text, useColor } from "@artsy/palette-mobile"
 import { BottomSheetView } from "@gorhom/bottom-sheet"
 import { AutomountedBottomSheetModal } from "app/Components/BottomSheet/AutomountedBottomSheetModal"
-import { useScreenDimensions } from "app/utils/hooks"
 import { useState, useRef } from "react"
 import { Platform } from "react-native"
 import PagerView, { PagerViewOnPageScrollEvent } from "react-native-pager-view"
@@ -23,9 +22,7 @@ export const ArtistSaveOnboardingBottomSheet = ({
   const pagerViewRef = useRef<PagerView>(null)
   const space = useSpace()
   const color = useColor()
-  const { width: screenWidth } = useScreenDimensions()
   const numberOfPages = 2
-  const barWidth = (screenWidth - 40) / numberOfPages
 
   const handleButtonPress = () => {
     if (activeStep === 0) {
@@ -51,7 +48,7 @@ export const ArtistSaveOnboardingBottomSheet = ({
       <BottomSheetView style={bottomSheetViewStyles}>
         <Flex mb={4} mx={2} alignItems="center">
           <PagerView
-            style={{ width: "100%", height: 350 }}
+            style={{ width: "100%", height: 300 }}
             initialPage={0}
             onPageScroll={handleIndexChange}
             ref={pagerViewRef}
@@ -60,22 +57,22 @@ export const ArtistSaveOnboardingBottomSheet = ({
             offscreenPageLimit={1}
           >
             {/* PAGE 1: Artists + Favorites */}
-            <Flex key="page-0" alignItems="center">
+            <Flex key="page-0" alignItems="center" width="100%">
               {/* Artist Images - Overlapping Circles */}
               <Flex flexDirection="row" justifyContent="center" alignItems="center">
                 {artists.slice(0, 3).map((artist, index) => (
                   <Flex
                     key={artist.id}
                     style={{
-                      marginLeft: index > 0 ? -16 : 0,
+                      marginLeft: index > 0 ? -10 : 0,
                       zIndex: artists.length - index,
                     }}
                   >
                     <Image
                       src={artist.imageUrl}
-                      width={64}
-                      height={64}
-                      style={{ borderRadius: 32 }}
+                      width={75}
+                      height={75}
+                      style={{ borderRadius: 37.5 }}
                     />
                   </Flex>
                 ))}
@@ -84,49 +81,46 @@ export const ArtistSaveOnboardingBottomSheet = ({
               <Spacer y={2} />
 
               {/* Title */}
-              <Text variant="lg-display" textAlign="center">
+              <Text variant="sm-display" weight="medium" textAlign="center">
                 Your followed artists are saved to Favorites.
               </Text>
 
               <Spacer y={1} />
 
               {/* Body Text */}
-              <Text variant="sm" color="black60" textAlign="center">
+              <Text variant="xs" color="black60" textAlign="center">
                 Find them anytime in the Favorites tab at the bottom of your screen.
               </Text>
 
               <Spacer y={2} />
 
-              {/* Nav bar image placeholder */}
-              <Flex
-                height={60}
-                backgroundColor="black10"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Text variant="xs" color="black60">
-                  [Nav Bar Image Placeholder]
-                </Text>
-              </Flex>
+              {/* Nav bar image */}
+
+              <Image
+                src="https://files.artsy.net/images/nav-bar.png"
+                height={75}
+                aspectRatio={350 / 75}
+                resizeMode="contain"
+              />
             </Flex>
 
             {/* PAGE 2: Alerts Notification */}
-            <Flex key="page-1" alignItems="center">
+            <Flex key="page-1" alignItems="center" width="100%">
               {/* Artist Images - Overlapping Circles */}
               <Flex flexDirection="row" justifyContent="center" alignItems="center">
                 {artists.slice(0, 3).map((artist, index) => (
                   <Flex
                     key={artist.id}
                     style={{
-                      marginLeft: index > 0 ? -16 : 0,
+                      marginLeft: index > 0 ? -10 : 0,
                       zIndex: artists.length - index,
                     }}
                   >
                     <Image
                       src={artist.imageUrl}
-                      width={64}
-                      height={64}
-                      style={{ borderRadius: 32 }}
+                      width={75}
+                      height={75}
+                      style={{ borderRadius: 37.5 }}
                     />
                   </Flex>
                 ))}
@@ -135,49 +129,41 @@ export const ArtistSaveOnboardingBottomSheet = ({
               <Spacer y={2} />
 
               {/* Title */}
-              <Text variant="lg-display" textAlign="center">
+              <Text variant="sm-display" weight="medium" textAlign="center">
                 We'll let you know when new works arrive.
               </Text>
 
               <Spacer y={1} />
 
               {/* Body */}
-              <Text variant="sm" color="black60" textAlign="center">
+              <Text variant="xs" color="black60" textAlign="center">
                 When a new work by an artist you follow is added to Artsy, you'll see a notification
                 on the Alerts icon at the top of your For You page.
               </Text>
 
               <Spacer y={2} />
 
-              {/* Search bar image placeholder */}
-              <Flex
-                height={60}
-                backgroundColor="black10"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Text variant="xs" color="black60">
-                  [Search Bar Image Placeholder]
-                </Text>
-              </Flex>
+              {/* Search bar image */}
+              <Image
+                src="https://files.artsy.net/images/search-bar.png"
+                height={75}
+                aspectRatio={350 / 75}
+                resizeMode="contain"
+              />
 
               <Spacer y={2} />
             </Flex>
           </PagerView>
 
-          {/* Page indicator - horizontal bars */}
-          <Flex flexDirection="row" justifyContent="center" mb={2}>
+          {/* Page indicator */}
+          <Flex flexDirection="row" justifyContent="center" mb={2} gap={0.5} py={0.5}>
             {Array.from(Array(numberOfPages).keys()).map((index) => (
               <Flex
                 key={index}
-                style={{
-                  height: 1,
-                  width: barWidth - space(1),
-                  marginRight: index < numberOfPages - 1 ? space(1) : 0,
-                  borderRadius: 2,
-                  backgroundColor: color("mono100"),
-                  opacity: activeStep === index ? 1 : 0.2,
-                }}
+                flex={1}
+                height={1}
+                borderRadius={2}
+                backgroundColor={activeStep === index ? color("mono60") : "#d9d9d9"}
               />
             ))}
           </Flex>
