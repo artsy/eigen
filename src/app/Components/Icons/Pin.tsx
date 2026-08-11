5628ab59f66a3e33d5b0619aba81a6d426d4576a
Mounir Dhahri <soussouheros@gmail.com>
2026-08-11 15:47:51 +0200

    fix: gity guide warning

--- src/app/Components/Icons/Pin.tsx
+++ src/app/Components/Icons/Pin.tsx
@@ -1,3 +1,4 @@
+import { View } from "react-native"
 import Svg, { Path, SvgProps } from "react-native-svg"
 /* SVGR has dropped some elements not supported by react-native-svg: title */
 
@@ -11,16 +12,18 @@ interface PinProps extends SvgProps {
 }
 
 export const Pin = (props: PinProps) => (
-  <Svg
-    width={props.pinWidth ? props.pinWidth : 45}
-    height={props.pinHeight ? props.pinHeight : 45}
-    viewBox="0 0 54 54"
-    {...props}
-  >
-    <Path
-      d="M28.597 48.207a2.006 2.006 0 0 1-3.194 0C19.424 40.322 11 30.422 11 21c0-8.837 7.163-16 16-16s16 7.163 16 16c0 9.422-8.424 19.322-14.403 27.207zM21 21a6 6 0 1 0 12 0 6 6 0 0 0-12 0z"
-      fill={props.color ? props.color : props.selected ? "#6E1EFF" : "#000"}
-      fillRule="nonzero"
-    />
-  </Svg>
+  <View collapsable={false} style={{ width: props.pinWidth ?? 45, height: props.pinHeight ?? 45 }}>
+    <Svg
+      width={props.pinWidth ? props.pinWidth : 45}
+      height={props.pinHeight ? props.pinHeight : 45}
+      viewBox="0 0 54 54"
+      {...props}
+    >
+      <Path
+        d="M28.597 48.207a2.006 2.006 0 0 1-3.194 0C19.424 40.322 11 30.422 11 21c0-8.837 7.163-16 16-16s16 7.163 16 16c0 9.422-8.424 19.322-14.403 27.207zM21 21a6 6 0 1 0 12 0 6 6 0 0 0-12 0z"
+        fill={props.color ? props.color : props.selected ? "#6E1EFF" : "#000"}
+        fillRule="nonzero"
+      />
+    </Svg>
+  </View>
 )
